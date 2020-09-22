6d693d53c75aa34a745a0eca02598bb843e28728
Rajiv Shah <rajivshah1@icloud.com>
2020-09-22 01:27:55 -0400

    feature(frontend/shared): Write safe version of Object.assign
    The native Object.assign can be exploited to cause prototype pollution when used with untrusted input. We can avoid prototype pollution by not merging prototype, __proto__, or constructor properties.

--- frontend/src/shared/lib/helpers.ts
+++ frontend/src/shared/lib/helpers.ts
@@ -23,3 +23,32 @@ export const persistent = <T>(key: string, initialValue: T): Writable<T> => {
 
     return state
 }
+
+export const safeAssign = (target: { [key: string]: any }, ...sources: { [key: string]: any }[]): Object => {
+    // Based on Object.assign polyfill
+    // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/assign#Polyfill
+    'use strict';
+    if (target === null || target === undefined) {
+        throw new TypeError('Cannot convert undefined or null to object');
+    }
+
+    const to = Object(target);
+
+    for (let index = 0; index < sources.length; index++) {
+        const nextSource = sources[index];
+
+        if (nextSource !== null && nextSource !== undefined) {
+            for (const nextKey in nextSource) {
+                // Avoid prototype pollution by not merging prototype, __proto__, or constructor properties
+                if (nextKey === 'prototype' || nextKey === '__proto__' || nextKey === 'constructor') {
+                    continue;
+                }
+                // Avoid bugs when hasOwnProperty is shadowed
+                if (Object.prototype.hasOwnProperty.call(nextSource, nextKey)) {
+                    to[nextKey] = nextSource[nextKey];
+                }
+            }
+        }
+    }
+    return to;
+};
