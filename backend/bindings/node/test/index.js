026e5a0efcd8f08099fb09d72765e116e71a1b9f
Lucas Nogueira <lucas@tauri.studio>
2020-10-01 13:10:22 -0300

    feat(backend) add test for `backup_and_restore`

--- backend/bindings/node/test/index.js
+++ backend/bindings/node/test/index.js
@@ -1,17 +1,30 @@
 const lib = require('../lib')
 const assert = require('assert')
+const fs = require('fs')
 
 describe('binding', () => {
-  it('sends a message and gets the response', () => {
+  it('creates an account, backup and restore it', async () => {
     lib.init()
-    lib.createAccount({
+    lib.setStrongholdPassword('password')
+    const createResponse = await lib.createAccount({
       clientOptions: {
         node: 'https://nodes.devnet.iota.org:443'
       }
-    }).then(response => {
-      assert.deepStrictEqual(response, {
-        type: "CreatedAccount"
-      })
     })
+    assert.strictEqual(createResponse.type, 'CreatedAccount')
+    try {
+      const backupResponse = await lib.backup('./backup')
+      assert.strictEqual(backupResponse.type, 'BackupSuccessful')
+      fs.unlinkSync('./example-database/snapshot')
+      lib.setStrongholdPassword('password') // since we removed the snapshot, reload stronghold
+      const restoreResponse = await lib.restoreBackup('./backup')
+      assert.strictEqual(restoreResponse.type, 'BackupRestored')
+    } finally {
+      try {
+        fs.rmdirSync('./backup', {
+          recursive: true
+        })
+      } catch {}
+    }
   })
 })
