0e800e0751e06d7524b269456a7f68a2be8b4c54
Lucas Nogueira <lucas@tauri.studio>
2020-10-09 13:19:32 -0300

    fix(bindings) throw error if the API response is an Error

--- backend/bindings/node/lib/src/index.ts
+++ backend/bindings/node/lib/src/index.ts
@@ -24,11 +24,17 @@ const addon = require('../native')
 
 function sendMessage(message: BridgeMessage): Promise<any> {
   return new Promise((resolve, reject) => {
-    addon.sendMessage(typeof message === 'string' ? message : JSON.stringify(message), (err: any, res: string) => {
+    addon.sendMessage(JSON.stringify(message), (err: any, res: string) => {
       if (err) {
         reject(err)
       } else {
-        resolve(JSON.parse(res))
+        const response = JSON.parse(res)
+        if (response && response.type === 'Error') {
+          reject(response)
+        }
+        else {
+          resolve(response)
+        }
       }
     })
   })
