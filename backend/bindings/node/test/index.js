8ce68fee26181845fe205fcac2e5ad75cf16d291
Lucas Nogueira <lucas@tauri.studio>
2020-10-09 13:20:10 -0300

    feat(bindings) improve nodejs test error checking

--- backend/bindings/node/test/index.js
+++ backend/bindings/node/test/index.js
@@ -4,21 +4,31 @@ const fs = require('fs')
 
 describe('binding', () => {
   it('creates an account, backup and restore it', async () => {
+    fs.rmdirSync('./example-database', {
+      recursive: true
+    })
     lib.init()
-    lib.setStrongholdPassword('password')
+    await lib.setStrongholdPassword('password')
     const createResponse = await lib.createAccount({
       clientOptions: {
         node: 'https://nodes.devnet.iota.org:443'
       }
     })
-    assert.strictEqual(createResponse.type, 'CreatedAccount')
+    assert.deepStrictEqual(createResponse, {
+      type: 'CreatedAccount',
+      payload: createResponse.payload
+    })
     try {
       const backupResponse = await lib.backup('./backup')
-      assert.strictEqual(backupResponse.type, 'BackupSuccessful')
+      assert.deepStrictEqual(backupResponse, {
+        type: 'BackupSuccessful'
+      })
       fs.unlinkSync('./example-database/snapshot')
-      lib.setStrongholdPassword('password') // since we removed the snapshot, reload stronghold
+      await lib.setStrongholdPassword('password') // since we removed the snapshot, reload stronghold
       const restoreResponse = await lib.restoreBackup('./backup')
-      assert.strictEqual(restoreResponse.type, 'BackupRestored')
+      assert.deepStrictEqual(restoreResponse, {
+        type: 'BackupRestored'
+      })
     } finally {
       try {
         fs.rmdirSync('./backup', {
