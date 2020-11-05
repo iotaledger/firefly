cb7cdef4e5aff10595648a85b82f80a5875eafa8
Lucas Nogueira <lucas@tauri.studio>
2020-11-05 10:29:26 -0300

    chore(desktop) remove test code

--- frontend/src/desktop/electron/preload.js
+++ frontend/src/desktop/electron/preload.js
@@ -3,9 +3,3 @@ const binding = require('wallet-actor-system-nodejs-binding')
 binding.init()
 
 window.__WALLET__ = binding
-
-// TODO this is only for the test; should be removed later
-const fs = require('fs')
-window.__deleteStrongholdSnapshot = () => {
-    fs.rmdirSync('./example-database', { recursive: true })
-}
