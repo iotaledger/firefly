dce8a67e800c11fb6ae93be75eb730b1e64079f9
Umair Sarfraz <aquadestructor@icloud.com>
2020-12-03 23:45:40 +0500

    feat: fetch market data on load

--- frontend/src/desktop/App.svelte
+++ frontend/src/desktop/App.svelte
@@ -1,5 +1,6 @@
 <script lang="typescript">
     import { onMount } from 'svelte'
+    import { fetchMarketData } from '@shared-lib/marketData'
     import { setupI18n, isLocaleLoaded, dir, _ } from '@shared-lib/i18n'
     import { darkMode, mobile, logged } from '@shared-lib/app'
     import { goto } from '@shared-lib/helpers'
@@ -29,11 +30,14 @@
     let splash = true
 
     setupI18n()
-    onMount(() => {
+    onMount(async () => {
         setTimeout(() => {
             splash = false
             initRouter()
+
         }, 2000)
+
+        await fetchMarketData()
     })
 </script>
 
