dacff0bd3e7625e4bfee21dd03d0cd8621b90ff4
Umair Sarfraz <aquadestructor@icloud.com>
2020-12-04 00:06:10 +0500

    chore: remove unused functions

--- frontend/src/shared/components/charts/Index.svelte
+++ frontend/src/shared/components/charts/Index.svelte
@@ -1,7 +1,7 @@
 <script lang="typescript">
     import { onDestroy } from 'svelte'
     import { LineChart, BarChart } from '@shared-components'
-    import { chartData, priceData, chartTimeframe, chartCurrency } from '@shared-lib/marketData'
+    import { chartData } from '@shared-lib/marketData'
 
     enum ChartType {
         Line = 'line',
@@ -16,43 +16,6 @@
     export let currency: string = 'usd'
 
     export let type: ChartType
-
-    function prepareProps(priceData) {
-        const monthNames = [
-            'January',
-            'February',
-            'March',
-            'April',
-            'May',
-            'June',
-            'July',
-            'August',
-            'September',
-            'October',
-            'November',
-            'December'
-        ]
-
-        return priceData[currency][timeframe]
-            .sort((a, b) => a[0] - b[0])
-            .reduce(
-                (acc, values) => {
-                    acc.labels.push(new Date(values[0] * 1000).toLocaleString('default', { month: 'short', day: 'numeric' }))
-                    acc.data.push(parseFloat(values[1]))
-
-                    return acc
-                },
-                { labels: [], data: [] }
-            )
-    }
-
-    function updateProps(_priceData) {
-        console.log('Called', _priceData)
-        const props = prepareProps(_priceData)
-
-        labels = props.labels
-        data = props.data
-    }
 
     const unsubscribe = chartData.subscribe((_chartData) => {
         labels = _chartData.labels;
