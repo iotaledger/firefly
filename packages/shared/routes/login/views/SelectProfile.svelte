11eb78385ddc3945773b70e261ebf8fcc5bd4e3b
Umair Sarfraz <aquadestructor@icloud.com>
2021-01-05 17:37:58 +0500

    feat: add login screens

---
+++ packages/shared/routes/login/views/SelectProfile.svelte
@@ -0,0 +1,33 @@
+<script>
+    import { createEventDispatcher } from 'svelte'
+    import { OnboardingLayout, Illustration, Text, Button, Logo, Profile, Icon } from 'shared/components'
+
+    export let locale
+    export let mobile
+
+    const dispatch = createEventDispatcher()
+
+    function handleContinueClick() {
+        dispatch('next')
+    }
+</script>
+
+{#if mobile}
+    <div>foo</div>
+{:else}
+    <section class="flex flex-col h-screen">
+        <Logo width="64px" logo="logo-firefly" classes="mt-28 mx-auto" />
+        <div class="px-48 flex-1 flex justify-evenly items-center">
+            {#each [
+                'Charlie Varley',
+                'Umair Sarfraz',
+                'Begoña Álvarez'
+            ] as name}
+                <Profile onClick={handleContinueClick} name={name} />
+            {/each}
+            <Profile name="Add Profile" bgColor="#fff" classes="border-solid border-2 border-gray-400">
+                <Icon icon="plus" classes="text-blue-500" width={16} height={19} />
+            </Profile>
+        </div>
+    </section>
+{/if}
