22e7c1924c6285744772e226fdcadfa2e3fae5d2
Lucas Fernandes Nogueira <lucas@tauri.studio>
2021-02-05 05:01:31 -0300

    refactor(desktop): use Webpack (#134)

--- packages/desktop/postcss.config.js
+++ packages/desktop/postcss.config.js
@@ -1,4 +1,13 @@
 // TODO purge
+const path = require('path')
+
 module.exports = {
-    plugins: [require('tailwindcss')('./../shared/tailwind.config.js')],
+    plugins: [
+        require('tailwindcss')('./../shared/tailwind.config.js'),
+        require('postcss-url')({
+            url(asset) {
+                return path.relative(__dirname, asset.absolutePath).replace('..', '../shared')
+            }
+        })
+    ],
 }
