ee246d9d4bc26619e63d673fc84941baf70a7623
Martyn Janes <martyn@obany.com>
2021-02-25 18:18:29 +0100

    feature(shared): Update checkbox to match standard appearance/behavior (#196)

--- packages/shared/components/icon/icons.js
+++ packages/shared/components/icon/icons.js
@@ -88,8 +88,16 @@ export const icons = {
             {
                 d: 'M7 12L10.5 15.5L17 9',
                 strokeWidth: '2',
-                strokeLinecap: 'round',
-                strokeColor: 'white',
+                strokeLinecap: 'round'
+            },
+        ],
+    },
+    "checkbox-unchecked": {
+        width: 24,
+        height: 24,
+        path: [
+            {
+                d: 'm0.55394,5.52478c0,-2.71689 2.28311,-5 5,-5l13.10204,0c2.71689,0 5,2.28311 5,5l0,13.0204c0,2.71689 -2.28311,5 -5,5l-13.10204,0c-2.71689,0 -5,-2.28311 -5,-5l0,-13.0204z'
             },
         ],
     },
