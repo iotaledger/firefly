e3d70148174e31feeccbea700915899522778b61
Martyn Janes <martyn@obany.com>
2021-03-03 17:07:23 +0100

    feat: Radio button style and state management

--- packages/shared/components/inputs/Radio.svelte
+++ packages/shared/components/inputs/Radio.svelte
@@ -1,17 +1,44 @@
 <script lang="typescript">
+    import { Icon } from 'shared/components'
+
     export let value = undefined
     export let group = undefined
     export let label = ''
     export let classes = ''
+
+    console.log({ value }, { group })
 </script>
 
 <style type="text/scss">
-    input {
-        @apply mr-7;
+    label {
+        position: relative;
+        :global(svg path) {
+            stroke: var(--text-disabled-color);
+            fill: white;
+        }
+        :global(svg.active path) {
+            stroke: var(--text-disabled-color);
+            @apply text-blue-500;
+            @apply fill-current;
+        }
+
+        &:hover,
+        &:focus-within {
+            :global(svg path) {
+                stroke: #9aadce;
+            }
+        }
+
+        input {
+            position: absolute;
+            left: 0;
+            opacity: 0;
+        }
     }
 </style>
 
-<label class={`w-full flex items-center mb-4 text-12 leading-160 text-gray-800 dark:text-white ${classes}`}>
-    <input class="h-4 w-4" type="radio" bind:group {value} />
+<label class={`w-full flex items-center mb-4 text-12 leading-160 cursor-pointer text-gray-800 dark:text-white ${classes}`}>
+    <input class="h-4 w-4 cursor-pointer" type="radio" bind:group {value} />
+    <Icon icon={value === group ? 'radio' : 'radio-unchecked'} classes={`mr-3 ${value === group ? 'active' : ''}`} />
     {label}
 </label>
