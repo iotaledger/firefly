b50290ca811717bf3ac5725f20d01d749ce59ded
Martyn Janes <martyn@obany.com>
2021-03-04 15:37:04 +0100

    feat: Address add disabled state

--- packages/shared/components/inputs/Address.svelte
+++ packages/shared/components/inputs/Address.svelte
@@ -1,10 +1,14 @@
 <script lang="typescript">
-    import { Icon } from 'shared/components'
+    import { Text } from 'shared/components'
+    import { activeProfile } from 'shared/lib/profile'
     import Error from './Error'
+
     export let address = undefined
     export let classes = ''
-    export let locale = undefined
+    export let disabled = false
+    export let label = undefined
     export let error = ''
+    let prefix = $activeProfile.isDeveloperProfile ? 'atoi...' : 'iota...'
 </script>
 
 <style type="text/scss">
@@ -17,17 +21,28 @@
             border-radius: 10px;
             box-shadow: -2px -2px 4px rgba(255, 255, 255, 0.2), 0px 4px 8px rgba(65, 114, 248, 0.08);
             resize: none;
+
+            &.disabled {
+                @apply pointer-events-none;
+                @apply bg-gray-100;
+            }
         }
     }
 </style>
 
 <div>
+    {#if label}
+        <Text type="p" classes="mb-2" smaller>{label}</Text>
+    {/if}
+
     <address-input class={classes}>
-        <textarea 
-            bind:value={address} 
-            placeholder={locale('general.add_address')} 
+        <textarea
+            bind:value={address}
+            placeholder={prefix}
             class={`w-full relative text-12 leading-140 py-4 pr-8 pl-4 bg-white border border-solid ${classes} ${error ? 'border-red-300 hover:border-red-500 focus:border-red-500' : 'border-gray-300 hover:border-gray-500 focus:border-gray-500'} rounded-xl text-gray`}
-        />
+            class:disabled
+            {disabled}
+            spellcheck={false} />
     </address-input>
-    <Error {error}/>
+    <Error {error} />
 </div>
