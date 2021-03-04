5c4773dbd0f66ccfadbfb6d2085573e57a036567
Martyn Janes <martyn@obany.com>
2021-03-04 09:00:12 +0100

    fix: Correct compilation errors

--- packages/shared/components/popups/AddNode.svelte
+++ packages/shared/components/popups/AddNode.svelte
@@ -1,9 +1,9 @@
 <script lang="typescript">
     import type { ClientOptions, Node } from 'lib/typings/client'
-    import { Text, Input, Button, Password, Checkbox } from 'shared/components'
+    import { Button, Checkbox, Input, Password, Text } from 'shared/components'
+    import { DEFAULT_NODE, DEFAULT_NODES, isNodeValid } from 'shared/lib/network'
     import { closePopup } from 'shared/lib/popup'
     import { activeProfile, updateProfile } from 'shared/lib/profile'
-    import { isNodeValid, DEFAULT_NODES as nodes } from 'shared/lib/network'
     import { api, wallet, WalletAccount } from 'shared/lib/wallet'
 
     export let locale
@@ -18,7 +18,7 @@
     let authError = ''
 
     function addCustomNode(node: Node, primary = false) {
-        if (isNewNodeValid([...$activeProfile.settings.customNodes, ...DEFAULT_NODES], node)) {
+        if (isNodeValid([...$activeProfile.settings.customNodes, ...DEFAULT_NODES], node)) {
             const options: ClientOptions = primary
                 ? {
                       ...$accounts[0].clientOptions,
@@ -86,14 +86,13 @@
 <Text type="h4" classes="mb-5">{locale('popups.node.title_add')}</Text>
 <div class="w-full h-full">
     <Input bind:value={url} placeholder={locale('popups.node.node_address')} error={addressError} />
-    <Input classes="mt-3" bind:value={username} placeholder={locale('popups.node.optional_username')}
-        error={authError} />
+    <Input classes="mt-3" bind:value={username} placeholder={locale('popups.node.optional_username')} error={authError} />
     <Password classes="mt-3" bind:value={password} placeholder={locale('popups.node.optional_password')} />
     <Checkbox classes="my-8" label={locale('popups.node.set_as_primary_node')} bind:checked={primary} />
 </div>
 <div class="flex flex-row justify-between space-x-4 w-full px-8 ">
-    <Button secondary classes="w-1/2" onClick={()=> closePopup()}>{locale('actions.cancel')}</Button>
-    <Button disabled={!url} classes="w-1/2" onClick={()=> addCustomNode({ url, username, password }, primary)}>
+    <Button secondary classes="w-1/2" onClick={() => closePopup()}>{locale('actions.cancel')}</Button>
+    <Button disabled={!url} classes="w-1/2" onClick={() => addCustomNode({ url, auth: { username, password } }, primary)}>
         {locale('actions.add_node')}
     </Button>
 </div>
