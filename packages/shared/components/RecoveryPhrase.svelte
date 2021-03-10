03f0b251010dce7f9be2c0f8b4a102dcf9e2083d
Martyn Janes <martyn@obany.com>
2021-03-10 11:19:50 +0100

    feat: Recover phrase can undo last click and reset

--- packages/shared/components/RecoveryPhrase.svelte
+++ packages/shared/components/RecoveryPhrase.svelte
@@ -8,6 +8,7 @@
     export let isVerification = false
     export let hide = false
     export let classes = ''
+    export let disabled = false
 
     let visibleRecoveryPhrase
 
@@ -15,15 +16,30 @@
         visibleRecoveryPhrase = isVerification ? recoveryPhrase.slice().sort() : recoveryPhrase
     })
 
+    $: {
+        if (recoveryPhraseInput.length === 0) {
+            recoveryPhraseIndexes = []
+        }
+    }
+
     const handleClick = (word, idx) => {
-        if (!isVerification || (recoveryPhraseInput.includes(word) && recoveryPhraseIndexes.includes(idx))) {
+        if (!isVerification) {
             return
         }
 
-        recoveryPhraseInput.push(word)
-        recoveryPhraseIndexes.push(idx)
-
-        recoveryPhraseInput = recoveryPhraseInput // needed
+        if (
+            recoveryPhraseInput.length > 0 &&
+            recoveryPhraseInput[recoveryPhraseInput.length - 1] === word &&
+            recoveryPhraseIndexes.length > 0 &&
+            recoveryPhraseIndexes[recoveryPhraseIndexes.length - 1] === idx
+        ) {
+            recoveryPhraseInput.pop()
+            recoveryPhraseIndexes.pop()
+        } else if (!recoveryPhraseInput.includes(word) && !recoveryPhraseIndexes.includes(idx)) {
+            recoveryPhraseInput.push(word)
+            recoveryPhraseIndexes.push(idx)
+        }
+        recoveryPhraseInput = recoveryPhraseInput
         recoveryPhraseIndexes = recoveryPhraseIndexes
     }
 </script>
@@ -35,6 +51,10 @@
         button {
             &.disabled {
                 @apply pointer-events-none;
+
+                &.selected {
+                    @apply opacity-30;
+                }
             }
             &.selected {
                 @apply bg-blue-500;
@@ -52,12 +72,12 @@
                 on:click|preventDefault={() => handleClick(word, i)}
                 class="px-6 py-4 flex flex-row items-center rounded-2xl bg-gray-50"
                 class:selected={isVerification && recoveryPhraseInput.indexOf(word) !== -1 && recoveryPhraseIndexes.indexOf(i) !== -1}
-                class:disabled={!isVerification}>
+                class:disabled={!isVerification || disabled}>
                 {#if !isVerification}<span class="text-gray-500 whitespace-pre">{`${i + 1}. `}</span>{/if}
-                <span class={hide ? 'text-gray-500' : 'text-gray-800'}>{hide ? '********' : word}</span>
-                {#if recoveryPhraseIndexes.indexOf(i) !== -1}
+                {#if isVerification && recoveryPhraseInput.indexOf(word) !== -1 && recoveryPhraseIndexes.indexOf(i) !== -1}
+                    <span class="text-gray-300">{word}</span>
                     <span class="font-bold text-16 leading-3 text-white">{recoveryPhraseIndexes.indexOf(i) + 1}</span>
-                {/if}
+                {:else}<span class={hide ? 'text-gray-500' : 'text-gray-800'}>{hide ? '********' : word}</span>{/if}
             </button>
         {/each}
     </div>
