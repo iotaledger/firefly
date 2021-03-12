d715f5236402f8ee871c28feeff88f466313c915
Diego <dfernandez@boxfish.studio.com>
2021-03-12 11:25:03 +0100

    Build Congratulations view

--- packages/shared/routes/setup/Congratulations.svelte
+++ packages/shared/routes/setup/Congratulations.svelte
@@ -1,24 +1,35 @@
 <script lang="typescript">
     import { onMount, createEventDispatcher } from 'svelte'
-    import { OnboardingLayout, Illustration, Text, Button } from 'shared/components'
+    import { Box, OnboardingLayout, Illustration, Text, Button } from 'shared/components'
+    import { AvailableExchangeRates, convertToFiat, currencies, CurrencyTypes, exchangeRates } from 'shared/lib/currency'
     import { newProfile, saveProfile, setActiveProfile } from 'shared/lib/profile'
+    import { formatUnit } from 'shared/lib/units'
+    import { get } from 'svelte/store'
 
     export let locale
     export let mobile
 
-    onMount(() => {
-        // This is the last screen in onboarding for all flows i.e., if you create a new wallet or import stronghold
-        // When this component mounts, ensure that the profile is persisted in the local storage.
-        saveProfile($newProfile)
-        setActiveProfile($newProfile.id)
+    // onMount(() => {
+    //     // This is the last screen in onboarding for all flows i.e., if you create a new wallet or import stronghold
+    //     // When this component mounts, ensure that the profile is persisted in the local storage.
+    //     saveProfile($newProfile)
+    //     setActiveProfile($newProfile.id)
 
-        newProfile.set(null)
-    })
+    //     newProfile.set(null)
+    // })
 
     const dispatch = createEventDispatcher()
 
+    let balance = Math.floor(Math.random() * 2000000) // TODO: dummy
+    let fiatbalance = `${convertToFiat(
+        balance,
+        get(currencies)[CurrencyTypes.USD],
+        get(exchangeRates)[AvailableExchangeRates.USD]
+    )} ${CurrencyTypes.USD}`
+
     const handleContinueClick = () => {
-        dispatch('next')
+        console.log("Continue clicked")
+        //dispatch('next')
     }
 </script>
 
@@ -27,11 +38,19 @@
 {:else}
     <OnboardingLayout allowBack={false}>
         <div slot="leftpane__content">
-            <Text type="h1" classes="mb-5">{locale('views.congratulations.title')}</Text>
-            <Text type="p" secondary classes="mb-4">{locale('views.congratulations.body')}</Text>
+            <Box classes="bg-gray-50 dark:bg-gray-900 dark:bg-opacity-50 rounded-lg p-10">
+                <balance class="flex flex-col flex-grow items-center">
+                    <Text type="h1" classes="mb-5 text-center">{locale('views.congratulations.title')}</Text>
+                    <Text type="p" secondary classes="mb-4 text-center">{locale('views.congratulations.body')}</Text>
+                    <div class="flex mb-2 my-6">
+                        <Text type="h2">{formatUnit(balance)}</Text>
+                    </div>
+                    <Text type="p" highlighted classes="py-1 uppercase">{fiatbalance}</Text>
+                </balance>
+            </Box>
         </div>
         <div slot="leftpane__action">
-            <Button classes="w-full" onClick={() => handleContinueClick()}>{locale('actions.continue')}</Button>
+            <Button classes="w-full" onClick={() => handleContinueClick()}>{locale('views.congratulations.export_migration')}</Button>
         </div>
         <div slot="rightpane" class="w-full h-full flex justify-end items-center">
             <Illustration width="100%" illustration="congratulations-desktop" />
