47a1253c6a8ac6ae656416f271667c589640df3c
Diego <dfernandez@boxfish.studio.com>
2021-03-12 13:56:22 +0100

    fix: Migrate view - illustration to have full height

--- packages/shared/routes/setup/Migrate.svelte
+++ packages/shared/routes/setup/Migrate.svelte
@@ -16,16 +16,13 @@
         get(currencies)[CurrencyTypes.USD],
         get(exchangeRates)[AvailableExchangeRates.USD]
     )} ${CurrencyTypes.USD}`
-
-    //let error = balance < 1000000 // TODO: dummy 1 MIOTA
     
     let loading = false
-    let migrated = false
     let migratingFundsMessage = ""
 
-    function handleMigrateClick() {
+    function handleContinueClick() {
         loading = true
-        migratingFundsMessage = "Migrating funds..."
+        migratingFundsMessage = locale('views.migrate.migrating')
         setTimeout(() => {
             //dispatch('next')
         }, 2000)
@@ -37,47 +34,40 @@
     function handleBackClick() {
         dispatch('previous')
     }
-
-
 </script>
 
 {#if mobile}
     <div>foo</div>
 {:else}
-    <OnboardingLayout onBackClick={() => dispatch('previous')}>
-        <div slot="leftpane__content">
-            <Text on:click={learnAboutMigrationsClick} type="h1" classes="mb-5 mt-5">{locale('views.migrate.title')}</Text>
-            <Text type="p" secondary classes="mb-4">This is an automated process to transfer your existing funds to the new network.</Text>
-            <Text type="p" secondary highlighted classes="mb-8 font-bold">Your funds will be locked until the Chrysalis network goes live in Aril 2021</Text>
-            <Box classes="bg-gray-50 dark:bg-gray-900 dark:bg-opacity-50 rounded-lg">
-                <balance class="flex flex-col flex-grow items-center py-12">
-                    <div class="flex mb-2">
-                        <Text type="h2">{formatUnit(balance)}</Text>
-                    </div>
-                    <Text type="p" highlighted classes="py-1 uppercase">{fiatbalance}</Text>
-                </balance>
-            </Box>
-            <!--
-            {#if error}
-                <Toast classes="mt-4" type="error" message={locale('views.balance.error')} />
-            {/if}
-            -->
-        </div>
-        <div slot="leftpane__action" class="flex flex-col items-center space-x-4">
-            <Button
-            disabled={loading}
-            classes="w-full" onClick={() => handleMigrateClick()}>
-                <Spinner busy={loading} message={migratingFundsMessage} classes="justify-center" />
-                {#if !loading && !migrated}
-                    Begin migration
-                {/if}
-            </Button>
-            <div on:click={learnAboutMigrationsClick}>
-                <Text type="p" secondary highlighted classes="m-7 font-bold cursor-pointer" >Learn about the migration</Text>
+    <OnboardingLayout onBackClick={() => dispatch('previous')} class="">
+            <div slot="leftpane__content">
+                <Text on:click={learnAboutMigrationsClick} type="h1" classes="mb-5 mt-5">{locale('views.migrate.title')}</Text>
+                <Text type="p" secondary classes="mb-4">{locale('views.migrate.body_1')}</Text>
+                <Text type="p" secondary highlighted classes="mb-8 font-bold">{locale('views.migrate.body_2')}</Text>
+                <Box classes="bg-gray-50 dark:bg-gray-900 dark:bg-opacity-50 rounded-lg ">
+                    <balance class="flex flex-col flex-grow items-center py-12">
+                        <div class="flex mb-2">
+                            <Text type="h2">{formatUnit(balance)}</Text>
+                        </div>
+                        <Text type="p" highlighted classes="py-1 uppercase">{fiatbalance}</Text>
+                    </balance>
+                </Box>
             </div>
-        </div>
-        <div slot="rightpane" class="w-full h-full flex">
-            <Illustration illustration="migrate-desktop" />
+            <div slot="leftpane__action" class="flex flex-col items-center space-x-4">
+                <Button
+                disabled={loading}
+                classes="w-full" onClick={() => handleContinueClick()}>
+                    <Spinner busy={loading} message={migratingFundsMessage} classes="justify-center" />
+                    {#if !loading}
+                        Begin migration
+                    {/if}
+                </Button>
+                <div on:click={learnAboutMigrationsClick}>
+                    <Text type="p" secondary highlighted classes="m-7 font-bold cursor-pointer">{locale('views.migrate.learn')}</Text>
+                </div>
+            </div>
+        <div slot="rightpane" class="h-full flex">
+            <Illustration illustration="migrate-desktop" height="100%" width="auto" classes="h-full object-cover object-left" />
         </div>
     </OnboardingLayout>
 {/if}
