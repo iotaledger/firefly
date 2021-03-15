f731f147d1fe091ed92f725db238c13298633010
Martyn Janes <martyn@obany.com>
2021-03-15 12:26:27 +0100

    fix: Don't reset settings router on locale loading

--- packages/shared/routes/dashboard/settings/Settings.svelte
+++ packages/shared/routes/dashboard/settings/Settings.svelte
@@ -1,6 +1,7 @@
 <script lang="typescript">
     import { appSettings } from 'shared/lib/appSettings'
     import { deepLinkRequestActive } from 'shared/lib/deepLinking'
+    import { isLocaleLoaded } from 'shared/lib/i18n'
     import { settingsRoute } from 'shared/lib/router'
     import { SettingsRoutes } from 'shared/lib/typings/routes'
     import { onDestroy } from 'svelte'
@@ -17,7 +18,11 @@
     }
 
     onDestroy(() => {
-        settingsRoute.set(SettingsRoutes.Init)
+        // When a new locale is loaded the pages are reloaded
+        // so don't reset the router in this case
+        if (!$isLocaleLoaded) {
+            settingsRoute.set(SettingsRoutes.Init)
+        }
     })
 </script>
 
