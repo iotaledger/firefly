02f0cc01e332ea2b1a4b6ada48cf67085cdaf69d
Charlie Varley <charlie.varley@iota.org>
2021-03-17 01:04:51 +0000

    fix: adjust settings home viewer to grey out disabled settings on logout

--- packages/shared/routes/dashboard/settings/views/SettingsHome.svelte
+++ packages/shared/routes/dashboard/settings/views/SettingsHome.svelte
@@ -30,38 +30,37 @@
     <div class="h-full w-full flex flex-col">
         <div class="flex flex-row justify-between mb-14">
             <Text type="h2">{locale('views.settings.settings')}</Text>
-            {#if !$loggedIn}
-                <button on:click={closeSettingsHome}>
-                    <Icon icon="close" classes="text-gray-800 dark:text-white" />
-                </button>
-            {/if}
+            <button on:click={closeSettingsHome}>
+                <Icon icon="close" classes="text-gray-800 dark:text-white" />
+            </button>
         </div>
         <div class="flex flex-row items-start space-x-10">
             <SettingsMenu
                 icon="settings"
                 iconColor="bg-blue-500"
                 icons={SettingsIcons}
-                settings={$loggedIn ? GeneralSettings : GeneralSettingsNoProfile}
+                settings={GeneralSettings}
+                activeSettings={$loggedIn ? GeneralSettings : GeneralSettingsNoProfile }
                 title={locale('views.settings.generalSettings.title')}
                 description={locale('views.settings.generalSettings.description')}
                 onClick={() => settingsRoute.set(SettingsRoutes.GeneralSettings)}
                 {locale} />
-            {#if $loggedIn}
-                <SettingsMenu
-                    icon="security"
-                    iconColor="bg-yellow-500"
-                    icons={SettingsIcons}
-                    settings={SecuritySettings}
-                    title={locale('views.settings.security.title')}
-                    description={locale('views.settings.security.description')}
-                    onClick={() => settingsRoute.set(SettingsRoutes.Security)}
-                    {locale} />
-            {/if}
+            <SettingsMenu
+                icon="security"
+                iconColor="bg-yellow-500"
+                icons={SettingsIcons}
+                settings={SecuritySettings}
+                activeSettings={$loggedIn ? SecuritySettings : undefined }
+                title={locale('views.settings.security.title')}
+                description={locale('views.settings.security.description')}
+                onClick={() => settingsRoute.set(SettingsRoutes.Security)}
+                {locale} />
             <SettingsMenu
                 icon="tools"
                 iconColor="bg-green-600"
                 icons={SettingsIcons}
-                settings={$loggedIn ? AdvancedSettings : AdvancedSettingsNoProfile}
+                settings={AdvancedSettings}
+                activeSettings={$loggedIn ? AdvancedSettings : AdvancedSettingsNoProfile}
                 title={locale('views.settings.advancedSettings.title')}
                 description={locale('views.settings.advancedSettings.description')}
                 onClick={() => settingsRoute.set(SettingsRoutes.AdvancedSettings)}
@@ -71,6 +70,7 @@
                 iconColor="bg-purple-500"
                 icons={SettingsIcons}
                 settings={HelpAndInfo}
+                activeSettings={HelpAndInfo}
                 title={locale('views.settings.helpAndInfo.title')}
                 description={locale('views.settings.helpAndInfo.description')}
                 onClick={() => settingsRoute.set(SettingsRoutes.HelpAndInfo)}
