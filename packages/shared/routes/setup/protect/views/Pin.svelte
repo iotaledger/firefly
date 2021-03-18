4475d0784096b6843a6995db94fb6c0e131b9060
Martyn Janes <martyn@obany.com>
2021-03-18 11:15:41 +0100

    feat: Onboarding pin code with glimpse mode

--- packages/shared/routes/setup/protect/views/Pin.svelte
+++ packages/shared/routes/setup/protect/views/Pin.svelte
@@ -35,22 +35,21 @@
                 <Text type="h2" classes="mb-5">{locale('views.pin.title')}</Text>
                 <Text type="p" secondary classes="mb-4">{locale('views.pin.body1')}</Text>
                 <Text type="p" secondary highlighted classes="mb-8 font-bold">{locale('views.pin.body2')}</Text>
-                <Pin bind:value={pinInput} classes="w-full mx-auto block" on:submit={onSubmit} autofocus />
+                <Pin bind:value={pinInput} glimpse classes="w-full mx-auto block" on:submit={onSubmit} autofocus />
             {:else}
                 <Text type="h2" classes="mb-5">{locale('views.confirmPin.title')}</Text>
                 <Text type="p" secondary classes="mb-8">{locale('views.confirmPin.body')}</Text>
-                <Pin bind:value={pinInput} classes="w-full mx-auto block" on:submit={onSubmit} autofocus />
+                <Pin bind:value={pinInput} glimpse classes="w-full mx-auto block" on:submit={onSubmit} autofocus />
             {/if}
         </div>
         <div slot="leftpane__action" class="flex flex-row flex-wrap justify-between items-center space-x-4">
-            <Button secondary classes="flex-1" onClick={() => handleBackClick()}>{locale('actions.back')}</Button>
-            <Button classes="flex-1" disabled={!valid || loading} onClick={() => onSubmit()}>{locale('actions.setPin')}</Button>
+            <Button classes="flex-1" disabled={!valid || loading} onClick={() => onSubmit()}>{locale(confirmInput ? 'actions.confirmPin' : 'actions.setPin')}</Button>
         </div>
-        <div slot="rightpane" class="w-full h-full flex justify-end items-center">
+        <div slot="rightpane" class="w-full h-full flex justify-center p-16" style="background-color: #FFF3F8;">
             {#if !confirmInput}
-                <Illustration width="100%" illustration="pin-desktop" />
+                <Illustration width="auto" height="auto" illustration="pin-desktop" />
             {:else}
-                <Illustration width="100%" illustration="repeat-pin-desktop" />
+                <Illustration width="auto" height="auto" illustration="repeat-pin-desktop" />
             {/if}
         </div>
     </OnboardingLayout>
