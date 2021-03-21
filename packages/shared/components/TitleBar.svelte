834b03acfbc7ce8cf0cba155bcafd7fe8306011a
Martyn Janes <martyn@obany.com>
2021-03-21 07:37:03 +0100

    fix: Sidebar full height

--- packages/shared/components/TitleBar.svelte
+++ packages/shared/components/TitleBar.svelte
@@ -12,26 +12,26 @@
 
 <div class="h-full w-full">
     {#if os === 'win32'}
-        <nav class="fixed z-50 left-0 right-0 flex flex-row h-10 justify-between" style="-webkit-app-region: drag">
-            <button on:click={() => Electron.popupMenu()} class="ml-5 p-2 stroke-current text-gray-900 dark:text-gray-100" style="-webkit-app-region: none">
+        <nav class="fixed z-50 left-0 right-0 flex flex-row h-12 justify-between" style="-webkit-app-region: drag">
+            <button on:click={() => Electron.popupMenu()} class="ml-6 p-2 stroke-current text-gray-900 dark:text-gray-100" style="-webkit-app-region: none">
                 <svg width="15" height="15" viewBox="0 0 15 15">
                     <path d="M0 1h15v1h-15z" fill="currentColor" />
                     <path d="M0 7h15v1h-15z" fill="currentColor" />
                     <path d="M0 13h15v1h-15z" fill="currentColor" />
                 </svg>
             </button>
-            <div class="flex flex-row">
-                <button on:click={() => Electron.minimize()} class="p-2 stroke-current text-gray-900 dark:text-gray-100" style="-webkit-app-region: none">
+            <div class="flex flex-row mr-2">
+                <button on:click={() => Electron.minimize()} class="p-2 mr-2 stroke-current text-gray-900 dark:text-gray-100" style="-webkit-app-region: none">
                     <svg width="15" height="15" viewBox="0 0 15 15">
                         <path d="M0 12h15v1h-15z" fill="currentColor" />
                     </svg>
                 </button>
-                <button on:click={() => Electron.maximize()} class="p-2 stroke-current text-gray-900 dark:text-gray-100" style="-webkit-app-region: none">
+                <button on:click={() => Electron.maximize()} class="p-2 mr-2 stroke-current text-gray-900 dark:text-gray-100" style="-webkit-app-region: none">
                     <svg width="15" height="15" viewBox="0 0 15 15">
                         <path d="M1 1v13h13v-13h-13zm-1-1h15v15h-15v-15z" fill="currentColor" />
                     </svg>
                 </button>
-                <button on:click={() => Electron.close()} class="p-2 stroke-current text-gray-900 dark:text-gray-100" style="-webkit-app-region: none">
+                <button on:click={() => Electron.close()} class="p-2 mr-2 stroke-current text-gray-900 dark:text-gray-100" style="-webkit-app-region: none">
                     <svg width="15" height="15" viewBox="0 0 15 15">
                         <path
                             d="M7.425 6.718l6.718-6.718.707.707-6.718 6.718 6.718 6.718-.707.707-6.718-6.718-6.718 6.718-.707-.707 6.718-6.718-6.718-6.718.707-.707 6.718 6.718z"
@@ -42,9 +42,9 @@
         </nav>
     {/if}
     {#if os === 'darwin'}
-        <div style="-webkit-app-region: drag" class="w-full h-8 fixed left-20" />
+        <div style="-webkit-app-region: drag" class="w-full h-12 fixed left-20" />
     {/if}
-    <div class={`fixed ${os === 'win32' ? 'top-10' : 'top-0'} left-0 right-0 bottom-0`}>
+    <div class={`fixed ${os === 'win32' ? 'top-12' : 'top-0'} left-0 right-0 bottom-0`}>
         <slot />
     </div>
 </div>
