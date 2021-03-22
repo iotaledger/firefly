1c9c92880ddbbb5597d96a1750b594dbde27d249
Begoña Álvarez de la Cruz <balvarez@boxfish.studio>
2021-03-22 05:03:38 +0100

    Feat: new security UI (#593)
    * remove scrollbar bg color
    
    * polish ui: font sizes, send cancel button, floating labels, scrollbars
    
    * update dashboard security panel to new design
    
    * invert stronghold toggle status

--- packages/shared/routes/dashboard/wallet/views/Security.svelte
+++ packages/shared/routes/dashboard/wallet/views/Security.svelte
@@ -1,7 +1,7 @@
 <script lang="typescript">
     import { SecurityTile, Text } from 'shared/components'
     import { versionDetails } from 'shared/lib/appUpdater'
-    import { diffDates, getBackupWarningColor } from 'shared/lib/helpers'
+    import { diffDates, getBackupWarningColor, isRecentDate } from 'shared/lib/helpers'
     import { showAppNotification } from 'shared/lib/notifications'
     import { openPopup } from 'shared/lib/popup'
     import { activeProfile, isStrongholdLocked, profiles } from 'shared/lib/profile'
@@ -13,6 +13,7 @@
 
     let lastBackupDate
     let lastBackupDateFormatted
+    let backupSafe
     let color
 
     function setup() {
@@ -20,6 +21,7 @@
         const lastStrongholdBackupTime = ap?.lastStrongholdBackupTime
         lastBackupDate = lastStrongholdBackupTime ? new Date(lastStrongholdBackupTime) : null
         lastBackupDateFormatted = diffDates(lastBackupDate, new Date())
+        backupSafe = lastBackupDate && isRecentDate(lastBackupDate)?.lessThanAMonth
         color = getBackupWarningColor(lastBackupDate)
     }
 
@@ -55,30 +57,9 @@
     onDestroy(unsubscribe)
 </script>
 
-<div data-label="security" class="py-6 px-8 flex-grow flex flex-col">
+<div data-label="security" class="py-6 px-8 flex-grow flex flex-col h-full">
     <Text type="h5" classes="mb-5">{locale('general.security')}</Text>
-    <div class="grid grid-cols-2 gap-2">
-        <!-- Firefly version -->
-        <SecurityTile
-            title={locale('views.dashboard.security.version.title', { values: { version: $versionDetails.currentVersion } })}
-            message={locale(`views.dashboard.security.version.${$versionDetails.upToDate ? 'upToDate' : 'outOfDate'}`)}
-            color={$versionDetails.upToDate ? 'blue' : 'yellow'}
-            icon="firefly"
-            onClick={() => handleSecurityTileClick('version')} />
-        <!-- Hardware Device -->
-        <SecurityTile
-            title={locale('views.dashboard.security.hardwareDevice.title')}
-            message={locale('views.dashboard.security.hardwareDevice.noneDetected')}
-            color="gray"
-            icon="chip"
-            classes="pointer-events-none" />
-        <!-- Stronghold status -->
-        <SecurityTile
-            title={locale('views.dashboard.security.strongholdStatus.title')}
-            message={locale(`views.dashboard.security.strongholdStatus.${$isStrongholdLocked ? 'locked' : 'unlocked'}`)}
-            color={$isStrongholdLocked ? 'blue' : 'yellow'}
-            icon={$isStrongholdLocked ? 'lock' : 'unlock'}
-            onClick={() => ($isStrongholdLocked ? handleSecurityTileClick('password') : lockStronghold())} />
+    <div class="grid grid-cols-2 gap-3 auto-rows-max w-full overflow-y-auto flex-auto h-1 -mr-2 pr-2">
         <!-- Stronghold backup -->
         <SecurityTile
             title={locale('views.dashboard.security.strongholdBackup.title')}
@@ -87,6 +68,25 @@
                   }) : locale('popups.backup.notBackedUp')}
             onClick={() => handleSecurityTileClick('backup')}
             icon="shield"
+            warning={!backupSafe}
             {color} />
+        <!-- Firefly version -->
+        <SecurityTile
+            title={locale('views.dashboard.security.version.title', { values: { version: $versionDetails.currentVersion } })}
+            message={locale(`views.dashboard.security.version.${$versionDetails.upToDate ? 'upToDate' : 'outOfDate'}`)}
+            color={$versionDetails.upToDate ? 'blue' : 'yellow'}
+            warning={!$versionDetails.upToDate}
+            icon="firefly"
+            onClick={() => handleSecurityTileClick('version')} />
+        <!-- Stronghold status -->
+        <SecurityTile
+            title={locale('views.dashboard.security.strongholdStatus.title')}
+            message={locale(`views.dashboard.security.strongholdStatus.${$isStrongholdLocked ? 'locked' : 'unlocked'}`)}
+            color="yellow"
+            icon={$isStrongholdLocked ? 'lock' : 'unlock'}
+            onClick={() => ($isStrongholdLocked ? handleSecurityTileClick('password') : lockStronghold())}
+            classes="col-span-2"
+            toggle
+            toggleActive={!$isStrongholdLocked} />
     </div>
 </div>
