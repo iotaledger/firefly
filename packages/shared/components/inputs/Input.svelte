2713784ac215cd752d706e8e74573aa236a7f8e6
Martyn Janes <martyn@obany.com>
2021-03-29 12:14:36 +0100

    fix: Get decimal separator from locale (#742)
    * fix: Get decimal separator from locale
    
    * fix: Allow overwrite when completely selected at max length
    
    * Update packages/shared/lib/i18n.ts
    
    Co-authored-by: Rajiv Shah <rajivshah1@icloud.com>
    
    * fix: Disable parsing non numbers
    
    * feat: Add max decimals support for amount input
    
    Co-authored-by: Rajiv Shah <rajivshah1@icloud.com>

--- packages/shared/components/inputs/Input.svelte
+++ packages/shared/components/inputs/Input.svelte
@@ -1,5 +1,6 @@
 <script lang="typescript">
     import { Error } from 'shared/components'
+    import { getDecimalSeparator } from 'shared/lib/i18n'
     import { onMount } from 'svelte'
 
     export let value = ''
@@ -14,32 +15,78 @@
     export let autofocus = false
     export let submitHandler = undefined
     export let disabled = false
+    export let maxDecimals = undefined
     export let disableContextMenu = false
 
     let inputElement
+    let decimalSeparator = getDecimalSeparator()
 
     const handleInput = (e) => {
         value = e.target.value
     }
 
     const onKeyPress = (e) => {
-        if (e.keyCode !== 8) {
-            const isReturn = e.keyCode === 13
-            if (isReturn && submitHandler) {
+        if (e.key !== 'Tab') {
+            const isEnter = e.key === 'Enter'
+            if (isEnter && submitHandler) {
                 submitHandler()
             }
-            if (maxlength && value.length >= maxlength) {
-                e.preventDefault()
-            }
-            if ((float || integer) && !isReturn) {
-                // if the input is float, we accept one dot
-                if (float && (e.keyCode === 46 || e.keyCode === 190)) {
-                    if (value.indexOf('.') >= 0) {
+            if ((float || integer) && !isEnter) {
+                // if the input is float, we accept one dot or comma depending on localization
+                if (float && e.key === decimalSeparator) {
+                    if (value.indexOf(decimalSeparator) >= 0) {
                         e.preventDefault()
                     }
-                } else if (e.keyCode < 48 || e.keyCode > 57) {
+                } else if ('0123456789'.indexOf(e.key) < 0) {
                     // if float or interger we accept numbers
                     e.preventDefault()
+                } else if (float && maxDecimals !== undefined && '0123456789'.indexOf(e.key) >= 0) {
+                    // If max decimals are set only allow certain number after decimal separator
+                    const sepPos = value.indexOf(decimalSeparator)
+                    if (sepPos >= 0) {
+                        // If caret position is after the separator then check
+                        if (e.target.selectionEnd > sepPos) {
+                            // If sel start and end are different that means
+                            // the text has been highlighted for overwrite
+                            // if they are the same then it single insertion
+                            if (e.target.selectionStart === e.target.selectionEnd) {
+                                const numDecimals = value.length - sepPos - 1
+                                if (numDecimals >= maxDecimals) {
+                                    e.preventDefault()
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    const onPaste = (e) => {
+        if (e.clipboardData && (float || integer)) {
+            const pasteVal = e.clipboardData.getData('text')
+            // Discard scientific notation or negative
+            if (pasteVal.indexOf('e') >= 0 || pasteVal.indexOf('-') >= 0) {
+                e.preventDefault()
+            } else if (float) {
+                const val = Number.parseFloat(pasteVal)
+                // Discard any number we can't parse as floats
+                if (Number.isNaN(val)) {
+                    e.preventDefault()
+                } else if (maxDecimals !== undefined) {
+                    value = Number(val.toFixed(maxDecimals)).toString()
+                    e.preventDefault()
+                }
+            } else if (integer) {
+                // Dicard anything with a decimal separator
+                if (pasteVal.indexOf('.') >= 0 || pasteVal.indexOf(',') >= 0) {
+                    e.preventDefault()
+                } else {
+                    const val = Number.parseInt(pasteVal, 10)
+                    // Discard any number we can't parse as integers
+                    if (Number.isNaN(val)) {
+                        e.preventDefault()
+                    }
                 }
             }
         }
@@ -134,6 +181,7 @@
             class:floating-active={value && label}
             on:input={handleInput}
             on:keypress={onKeyPress}
+            on:paste={onPaste}
             on:contextmenu={handleContextMenu}
             {disabled}
             {...$$restProps}
