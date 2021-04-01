55018b7e78508fe6b2a31e069eb481dedfec5b02
Martyn Janes <martyn@obany.com>
2021-04-01 21:45:50 +0100

    fix: Delete profile requests wallet password (#783)

--- packages/shared/components/popups/DeleteProfile.svelte
+++ packages/shared/components/popups/DeleteProfile.svelte
@@ -1,5 +1,5 @@
 <script lang="typescript">
-    import { Button, Text } from 'shared/components'
+    import { Button, Password, Text } from 'shared/components'
     import { logout } from 'shared/lib/app'
     import { Electron } from 'shared/lib/electron'
     import { showAppNotification } from 'shared/lib/notifications'
@@ -11,48 +11,70 @@
     export let locale
 
     let isBusy = false
+    let error = ''
+    let password
 
     function deleteProfile() {
         isBusy = true
-        const ap = get(activeProfile)
-        if (!ap) {
-            logout()
-            return
-        }
-
-        Electron.PincodeManager.remove(ap.id).then((isRemoved) => {
-            if (!isRemoved) {
-                console.error('Something went wrong removing pincode entry.')
-            }
-
-            // Remove storage
-            api.removeStorage({
-                onSuccess(res) {
+        error = ''
+        api.setStrongholdPassword(password, {
+            onSuccess() {
+                const ap = get(activeProfile)
+                if (!ap) {
                     logout()
+                    return
+                }
 
-                    removeProfile(ap.id)
+                Electron.PincodeManager.remove(ap.id).then((isRemoved) => {
+                    if (!isRemoved) {
+                        console.error('Something went wrong removing pincode entry.')
+                    }
 
-                    isBusy = false
-                    closePopup()
-                },
-                onError(err) {
-                    isBusy = false
-                    closePopup()
-                    showAppNotification({
-                        type: 'error',
-                        message: locale(err.error),
+                    // Remove storage
+                    api.removeStorage({
+                        onSuccess(res) {
+                            logout()
+
+                            removeProfile(ap.id)
+
+                            isBusy = false
+                            closePopup()
+                        },
+                        onError(err) {
+                            isBusy = false
+                            closePopup()
+                            showAppNotification({
+                                type: 'error',
+                                message: locale(err.error),
+                            })
+                        },
                     })
-                },
-            })
+                })
+            },
+            onError(err) {
+                isBusy = false
+                error = locale(err.error)
+            },
         })
     }
 </script>
 
 <Text type="h4" classes="mb-5">{locale('popups.deleteProfile.title')}</Text>
 <div class="w-full h-full mb-5">
-    <Text>{locale('popups.deleteProfile.confirmation')}</Text>
+    <Text classes="mb-3">{locale('popups.deleteProfile.confirmation')}</Text>
+    <Text type="p" secondary classes="mb-3">{locale('popups.deleteProfile.typePassword')}</Text>
+    <Password
+        {error}
+        classes="w-full mb-8"
+        bind:value={password}
+        showRevealToggle
+        {locale}
+        placeholder={locale('general.password')}
+        autofocus
+        submitHandler={() => deleteProfile()}
+        disabled={isBusy} />
 </div>
 <div class="flex flex-row justify-between space-x-4 w-full px-8 ">
     <Button secondary classes="w-1/2" onClick={() => closePopup()} disabled={isBusy}>{locale('actions.no')}</Button>
-    <Button disabled={isBusy} classes="w-1/2" onClick={() => deleteProfile()} warning>{locale('actions.yes')}</Button>
+    <Button disabled={!password || isBusy} classes="w-1/2" onClick={() => deleteProfile()} warning>{locale('actions.yes')}</Button>
 </div>
