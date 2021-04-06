32c9b5a414f680e149067ce7587861b60811ea5a
Martyn Janes <martyn@obany.com>
2021-04-06 14:00:46 +0100

    fix: Comment

--- packages/desktop/electron/preload.js
+++ packages/desktop/electron/preload.js
@@ -22,7 +22,7 @@ const Electron = {
     removeProfileFolder(profilePath) {
         ipcRenderer.invoke('get-path', 'userData').then((userDataPath) => {
             // Check that the removing profile path matches the user data path
-            // so that we don't try and remove things outside out scope
+            // so that we don't try and remove things outside our scope
             if (profilePath.startsWith(userDataPath)) {
                 fs.rmdirSync(profilePath, { recursive: true })
             }
