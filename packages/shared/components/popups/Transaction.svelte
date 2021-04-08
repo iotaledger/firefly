d9ffefba5ce340f66af5a2b82febbaf57d8c624a
Charlie Varley <charlie.varley@iota.org>
2021-04-09 00:56:59 +0100

    fix: transaction popup spacing

--- packages/shared/components/popups/Transaction.svelte
+++ packages/shared/components/popups/Transaction.svelte
@@ -1,6 +1,7 @@
 <script lang="typescript">
     import { Button, Illustration, Text } from 'shared/components'
     import { AvailableExchangeRates, convertToFiat, currencies, CurrencyTypes, exchangeRates } from 'shared/lib/currency'
+    import { appSettings } from 'shared/lib/appSettings'
     import { closePopup } from 'shared/lib/popup'
     import { activeProfile } from 'shared/lib/profile'
     import { formatUnit } from 'shared/lib/units'
@@ -30,20 +31,22 @@
 
 <style type="text/scss">
     .illustration {
+        height: 250px;
         :global(img) {
-            max-height: 260px;
-            object-position: 0 10px;
+            min-height: 280px;
+            max-width: 100%;
+            object-position: 0 -3px;
         }
     }
 </style>
 
-<Text type="h4" classes="mb-5">{locale('popups.transaction.title')}</Text>
+<Text type="h4" classes="mb-6">{locale('popups.transaction.title')}</Text>
 <div class="flex w-full flex-row flex-wrap">
-    <div class="illustration w-full bg-pastel-yellow dark:bg-gray-900 flex justify-center content-center">
-        <Illustration height="260px" illustration="balance-desktop" />
+    <div class="illustration w-full bg-pastel-yellow dark:bg-gray-900 flex justify-center">
+        <Illustration illustration="balance-desktop" />
     </div>
-    <div class="w-full text-center my-6 px-10">
-        <Text type="h4" highlighted classes="mb-2">
+    <div class="w-full text-center my-9 px-11">
+        <Text type="h4" highlighted classes="mb-3">
             {locale('popups.transaction.body', { values: { amount: displayedAmount } })}
         </Text>
         <Text type={internal ? 'p' : 'pre'} secondary bigger>{to}</Text>
