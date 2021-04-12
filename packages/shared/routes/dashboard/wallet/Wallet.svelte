b8a647e212560c02975595175b3d722f9c52ab62
Martyn Janes <martyn@obany.com>
2021-04-12 18:10:15 +0100

    fix: Internal address transfer (#857)
    * feat: Address and Dropdown allow autofocus
    
    * fix: On Send check to see if address is an internal account

--- packages/shared/routes/dashboard/wallet/Wallet.svelte
+++ packages/shared/routes/dashboard/wallet/Wallet.svelte
@@ -419,7 +419,7 @@
         })
     }
 
-    function onInternalTransfer(senderAccountId, receiverAccountId, amount) {
+    function onInternalTransfer(senderAccountId, receiverAccountId, amount, internal) {
         const _internalTransfer = () => {
             isTransferring.set(true)
             api.internalTransfer(senderAccountId, receiverAccountId, amount, {
@@ -469,7 +469,7 @@
                     transferState.set('Complete')
 
                     setTimeout(() => {
-                        clearSendParams(true)
+                        clearSendParams(internal)
                         isTransferring.set(false)
                     }, 3000)
                 },
