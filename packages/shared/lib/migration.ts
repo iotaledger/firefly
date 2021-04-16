3e02fbc12e499aef21093e16e5a4afde56f5bcd1
Umair Sarfraz <aquadestructor@icloud.com>
2021-04-17 00:28:23 +0200

    hotfix: fix Array.isArray() condition

--- packages/shared/lib/migration.ts
+++ packages/shared/lib/migration.ts
@@ -462,7 +462,7 @@ export const bundlesWithUnspentAddresses = derived(get(migration).bundles, (_bun
 ))
 
 export const hasAnySpentAddressWithNoBundleHashes = derived(get(migration).bundles, (_bundles) => _bundles.length &&
-    _bundles.some((bundle) => bundle.inputs.some((input) => input.spent && (Array.isArray(input.spentBundleHashes && !input.spentBundleHashes.length) || input.spentBundleHashes === null))))
+    _bundles.some((bundle) => bundle.inputs.some((input) => input.spent && (Array.isArray(input.spentBundleHashes) && !input.spentBundleHashes.length) || input.spentBundleHashes === null)))
 
 
 export const spentAddressesWithNoBundleHashes = derived([get(migration).data, get(migration).bundles], ([data]) => data.inputs.filter((input) => input.spent && input.balance >= MINIMUM_MIGRATION_BALANCE && (Array.isArray(input.spentBundleHashes && !input.spentBundleHashes.length) || input.spentBundleHashes === null)))
