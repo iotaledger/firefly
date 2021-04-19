96744f5e8385936c88794e5dbd130b9730818774
Begoña Alvarez <balvarez@boxfish.studio>
2021-04-19 13:08:54 +0200

    update migration progress bar

--- packages/shared/components/ProgressBar.svelte
+++ packages/shared/components/ProgressBar.svelte
@@ -3,24 +3,42 @@
     export let classes = ''
     export let message = ''
     export let secondary = false
+    export let narrow = false
 </script>
 
 <style type="text/scss">
-    div,
-    .progress {
-        height: 48px;
-    }
-    .progress {
-        transition: width 0.25s;
-    }
-    .message {
-        z-index: 1;
+    .progress-wrapper {
+        div,
+        .progress {
+            @apply h-12;
+        }
+        .progress {
+            transition: width 0.25s;
+        }
+        .message {
+            z-index: 1;
+            @apply absolute;
+        }
+        &.narrow {
+            @apply pb-8;
+            div,
+            .progress {
+                @apply h-4;
+            }
+            .message {
+                @apply bottom-0;
+            }
+        }
     }
 </style>
 
-<div class="relative flex justify-center items-center overflow-hidden  { secondary ? 'border border-solid border-gray-300' : 'bg-blue-500'} rounded-2xl {classes}">
+<div class="relative progress-wrapper flex flex-col justify-center items-center w-full" class:narrow>
+    <div
+        class="relative w-full flex justify-center items-center overflow-hidden  {secondary ? 'border border-solid border-gray-300' : 'bg-blue-500'} rounded-2xl {classes}">
+        <span
+            class="absolute left-0 inline-block {secondary ? 'bg-blue-200' : 'bg-yellow-500'} progress rounded-2xl"
+            style={`width:${Math.max(Math.min(percent, 100), 0)}%`} />
+    </div>
     <span
-        class="absolute left-0 inline-block {secondary ? 'bg-blue-200' : 'bg-yellow-500'} progress rounded-2xl"
-        style={`width:${Math.max(Math.min(percent, 100), 0)}%`} />
-    <span class="font-bold text-12 {secondary ? 'text-blue-500' : 'text-white'} message">{message}</span>
+        class="font-bold text-12 {narrow ? 'text-gray-800 dark:text-white' : secondary ? 'text-blue-500' : 'text-white'} message">{message}</span>
 </div>
