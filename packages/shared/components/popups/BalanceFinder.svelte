d9f2531b42d3b2e961676439fdcd7eb50b5c19f8
Martyn Janes <martyn@obany.com>
2021-04-20 18:35:21 +0100

    Fix: Balance finder password (#949)
    * fix: Only show balance finder password when required
    
    * fix: Remove additional horizontal separator
    
    * fix: Add missing busy flag

--- packages/shared/components/popups/BalanceFinder.svelte
+++ packages/shared/components/popups/BalanceFinder.svelte
@@ -1,8 +1,9 @@
 <script lang="typescript">
     import { Button, Password, Spinner, Text } from 'shared/components'
     import { closePopup } from 'shared/lib/popup'
-    import { asyncSetStrongholdPassword, asyncSyncAccounts, updateAccounts, wallet } from 'shared/lib/wallet'
-
+    import { asyncSetStrongholdPassword, asyncSyncAccounts, wallet } from 'shared/lib/wallet'
+    import { isStrongholdLocked } from 'shared/lib/profile'
+    
     export let locale
 
     const { balanceOverview } = $wallet
@@ -10,7 +11,7 @@
     let addressIndex = 0
     let gapIndex = 25
     let accountDiscoveryThreshold = 10
-    let password
+    let password = ''
     let error = ''
     let isBusy = false
 
@@ -18,7 +19,9 @@
         try {
             error = ''
             isBusy = true
-            await asyncSetStrongholdPassword(password)
+            if ($isStrongholdLocked) {
+                await asyncSetStrongholdPassword(password)
+            }
             await asyncSyncAccounts(addressIndex, gapIndex, accountDiscoveryThreshold, false)
             addressIndex += gapIndex
         } catch (err) {
@@ -40,21 +43,23 @@
         <Text type="p">{locale('popups.balanceFinder.totalWalletBalance')}</Text>
         <Text type="p" highlighted>{$balanceOverview.balance}</Text>
         <Text type="p" secondary classes="mb-6">{$balanceOverview.balanceFiat}</Text>
-        <Text type="p" secondary classes="mb-3">{locale('popups.balanceFinder.typePassword')}</Text>
-        <Password
-            {error}
-            classes="w-full mb-2"
-            bind:value={password}
-            showRevealToggle
-            {locale}
-            placeholder={locale('general.password')}
-            autofocus
-            submitHandler={() => handleFindBalances()}
-            disabled={isBusy} />
+        {#if $isStrongholdLocked}
+            <Text type="p" secondary classes="mb-3">{locale('popups.balanceFinder.typePassword')}</Text>
+            <Password
+                {error}
+                classes="w-full mb-2"
+                bind:value={password}
+                showRevealToggle
+                {locale}
+                placeholder={locale('general.password')}
+                autofocus
+                submitHandler={() => handleFindBalances()}
+                disabled={isBusy} />
+        {/if}
     </div>
     <div class="flex flex-row flex-nowrap w-full space-x-4">
         <Button classes="w-full" secondary onClick={handleCancelClick} disabled={isBusy}>{locale('actions.cancel')}</Button>
-        <Button classes="w-full" onClick={handleFindBalances} disabled={!password || isBusy}>
+        <Button classes="w-full" onClick={handleFindBalances} disabled={($isStrongholdLocked && password.length === 0) || isBusy}>
             {#if isBusy}
                 <Spinner busy={true} message={locale(`actions.searching`)} classes="justify-center" />
             {:else}{locale(`actions.${addressIndex ? 'searchAgain' : 'searchBalances'}`)}{/if}
