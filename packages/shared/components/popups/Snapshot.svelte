3ff0c06cc482350cfcf24b2050594d176350703a
Begoña Alvarez <balvarez@boxfish.studio>
2021-04-20 16:18:28 +0200

    add close app button to snapshot popup

--- packages/shared/components/popups/Snapshot.svelte
+++ packages/shared/components/popups/Snapshot.svelte
@@ -1,8 +1,12 @@
 <script lang="typescript">
-    import { Text } from 'shared/components'
+    import { Button, Text } from 'shared/components'
+    import { Electron } from 'shared/lib/electron'
 
     export let locale
 </script>
 
 <Text type="h4" classes="mb-5">{locale('popups.snapshot.title')}</Text>
 <Text type="p" secondary>{locale('popups.snapshot.body')}</Text>
+<div class="flex w-full justify-center pt-8">
+    <Button classes="w-1/2" onClick={() => Electron.close()}>{locale('actions.closeFirefly')}</Button>
+</div>
