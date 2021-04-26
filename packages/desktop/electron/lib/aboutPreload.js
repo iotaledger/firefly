e43a7c28815d06fe9cf34403ff71816cc596e9df
Martyn Janes <martyn@obany.com>
2021-04-26 07:54:31 +0100

    Update packages/desktop/electron/lib/aboutPreload.js
    Co-authored-by: Rajiv Shah <rajivshah1@icloud.com>

--- packages/desktop/electron/lib/aboutPreload.js
+++ packages/desktop/electron/lib/aboutPreload.js
@@ -2,15 +2,15 @@ const { contextBridge, ipcRenderer } = require('electron')
 const { version, build: { productName } } = require('../../package.json')
 
 contextBridge.exposeInMainWorld('about', {
-    getData: (callback) => {
-        ipcRenderer.invoke('menu-data').then(data => {
+    getData: () => {
+        return ipcRenderer.invoke('menu-data').then((data) => {
             const aboutData = {
                 appName: productName,
                 version: data.strings.version.replace('{version}', version),
                 iconPath: './assets/logos/firefly_logo.svg'
             }
             
-            callback(aboutData)
+            return aboutData
         })
     }
-})
+)
