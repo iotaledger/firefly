457657ea8d6e8594d3db3aa369856c6a1d2419e2
Martyn Janes <martyn@obany.com>
2021-04-26 08:07:07 +0100

    Update packages/desktop/electron/lib/errorPreload.js
    Co-authored-by: Rajiv Shah <rajivshah1@icloud.com>

--- packages/desktop/electron/lib/errorPreload.js
+++ packages/desktop/electron/lib/errorPreload.js
@@ -2,15 +2,15 @@ const { contextBridge, ipcRenderer } = require('electron')
 const { version } = require('../../package.json')
 
 contextBridge.exposeInMainWorld('error', {
-    getData: (callback) => {
-        ipcRenderer.invoke('error-data').then(data => {
-            callback({
+    getData: () => {
+        return ipcRenderer.invoke('error-data').then((data) => {
+            return {
                 iconPath: './assets/logos/firefly_logo.svg',
                 version,
                 diagnostics: data.diagnostics.map((d) => `${d.label.replace('popups.diagnostics.', '')}: ${d.value}`).join('\r\n'),
                 errorType: data.errorType,
                 error: data.error
-            })
+            }
         })
     },
     openUrl: (url) => {
