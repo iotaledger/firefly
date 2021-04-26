c0a2a62aa7c77f7c48a69c1ceeebae2c1dcd5fe7
Martyn Janes <martyn@obany.com>
2021-04-26 13:47:39 +0100

    fix: Handle exceptions and show a dialog (#1008)
    * fix: Handle exceptions and show a dialog
    
    * fix: Additional error handling
    
    * Update packages/desktop/electron/lib/errorPreload.js
    
    Co-authored-by: Rajiv Shah <rajivshah1@icloud.com>
    
    * Update packages/desktop/public/error.html
    
    Co-authored-by: Rajiv Shah <rajivshah1@icloud.com>
    
    * fix: Remove poop
    
    Co-authored-by: Rajiv Shah <rajivshah1@icloud.com>

--- packages/desktop/main.js
+++ packages/desktop/main.js
@@ -1,4 +1,17 @@
+import { Electron } from 'shared/lib/electron'
 import App from './App.svelte'
+
+window.addEventListener('error', event => {
+    Electron.unhandledException("Render Context Error", event.error || event)
+    event.preventDefault();
+    console.error(event.error || event)
+})
+
+window.addEventListener('unhandledrejection', event => {
+    Electron.unhandledException("Render Context Unhandled Rejection", event.reason)
+    event.preventDefault();
+    console.error(event.reason)
+});
 
 const app = new App({
     target: document.body,
--- packages/shared/lib/electron.ts
+++ packages/shared/lib/electron.ts
@@ -77,6 +77,8 @@ export interface IElectron {
     updateCancel(): Promise<void>
     updateDownload(): Promise<void>
 
+    unhandledException(title: string, err: Error): Promise<void>
+
     // SeedVault API methods
     importLegacySeed(buffer: any, password: string): Promise<string>;
     validateSeedVault(buffer: any): Promise<boolean>;
