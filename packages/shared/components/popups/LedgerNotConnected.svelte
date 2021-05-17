47c266f179a67ad13e3f7160acd5b8627528d247
Begoña Alvarez <balvarez@boxfish.studio>
2021-05-17 17:53:34 +0200

    Update Ledger popup style

--- packages/shared/components/popups/LedgerNotConnected.svelte
+++ packages/shared/components/popups/LedgerNotConnected.svelte
@@ -1,40 +1,22 @@
 <script>
-    import { getContext } from 'svelte'
     import { Text, Button } from 'shared/components'
     import { closePopup } from 'shared/lib/popup'
-    export let locale
-    export let message
 
-    const popupState = getContext('popupState')
+    export let message
+    export let handleClose
+    export let locale
 
     function handleCancelClick() {
-        closePopup()
+        if ('function' === typeof handleClose) {
+            handleClose()
+        } else {
+            closePopup()
+        }
     }
 </script>
 
-<style type="text/scss">
-    .rectangle {
-        width: 48px;
-        height: 48px;
-        background: #DC8989;
-        border-radius: 16px;
-    }
-
-    .message-container {
-        width: 220px;
-    }
-
-    .action-container {
-        width: 220px;
-    }
-</style>
-
-<div class="flex flex-col items-center">
-    <div class="rectangle mt-5"></div>
-    <div class="message-container">
-        <Text type="h4" classes="mb-5 mt-5 text-center">{message}</Text>
-    </div>
-    <div class="flex flex-row justify-between px-2 w-full action-container">
-        <Button secondary classes="w-full" onClick={handleCancelClick}>{locale('actions.cancel')}</Button>
-    </div>
+<div class="p-8 flex flex-col w-full items-center justify-center text-center">
+    <div class="w-12 h-12 bg-orange-300 rounded-lg mb-6" />
+    <Text type="p" classes="mb-6 px-16">{message}</Text>
+    <Button secondary classes="w-1/2" onClick={handleCancelClick}>{locale('actions.cancel')}</Button>
 </div>
