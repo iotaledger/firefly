3ab31cfa3b625731363dca7c827521c4e40c8488
Matthew Maxwell <maxwellmattryan@gmail.com>
2021-06-21 16:48:34 -0500

    Perform media query only once to handle inconsistent OS behaviour

--- packages/shared/lib/appSettings.ts
+++ packages/shared/lib/appSettings.ts
@@ -25,14 +25,21 @@ export const appSettings = persistent<AppSettings>('settings', {
 })
 
 /**
+ * NOTE: This media query is performed only once to help make the UX
+ * consistent across OS platforms. To be specific, it ensures that users
+ * are required to restart Firefly for system theme changes to take effect.
+ * This is because the behaviour of this media query is different among platforms,
+ * e.g. MacOS allows for Firefly to update the app theme without the restart whereas
+ * Linux-based platforms do require the app restart.
+ */
+const _shouldBeDarkMode = window.matchMedia('(prefers-color-scheme: dark)').matches
+
+/**
  * Determines if the theme is dark or not based on the current
  * application settings
  * @param theme the theme to check against
  * @returns true if the app should be in dark mode according to the theme
  */
 export const shouldBeDarkMode = (theme: AppTheme): boolean => {
-    if(theme === 'system')
-        return window.matchMedia('(prefers-color-scheme: dark)').matches
-    else
-        return theme === 'dark'
+    return theme === 'system' ? _shouldBeDarkMode : theme === 'dark'
 }
