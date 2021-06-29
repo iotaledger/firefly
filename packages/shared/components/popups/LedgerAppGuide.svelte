8f8c6913c882819ed1f7c9e85c653c54a979b326
Matthew Maxwell <44885822+maxwellmattryan@users.noreply.github.com>
2021-06-29 12:23:27 +0000

    feat: Ledger migration popup guide (#1227)
    * feat: Add onboarding layout for Ledger app guide
    
    * feat: Add functional popup guide
    
    * feat: Add locale JSON data for new next
    
    * fix: Adjust styling and remove unnecessary locale data

---
+++ packages/shared/components/popups/LedgerAppGuide.svelte
@@ -0,0 +1,66 @@
+<script>
+    import { Button, Illustration, Text } from 'shared/components';
+    import { closePopup } from 'shared/lib/popup';
+
+    export let locale
+
+    let stepIndex = 0
+    let stepAnimations = [
+        'import-from-firefly-ledger-desktop',
+        'import-from-ledger-desktop',
+        'balance-desktop',
+        'import-from-firefly-ledger-desktop',
+        'import-from-ledger-desktop'
+    ]
+
+    function changeIndex(increment) {
+        stepIndex += increment
+    }
+
+    function handleCloseClick() {
+        closePopup()
+    }
+</script>
+
+<style type="text/scss">
+    .illustration {
+        height: 320px;
+
+        :global(img) {
+            min-height: 280px;
+            max-width: 100%;
+            object-position: 0 -3px;
+        }
+    }
+</style>
+
+<Text type="h4" classes="mb-6">{locale('popups.ledgerAppGuide.title')}</Text>
+<div class="w-full flex flex-row flex-wrap">
+    <div class="illustration w-full bg-white dark:bg-gray-900 flex justify-center">
+        <Illustration illustration={stepAnimations[stepIndex]} />
+    </div>
+    <div class="w-full text-center my-9 px-10">
+        <Text secondary>{locale('popups.ledgerAppGuide.steps')[stepIndex]}</Text>
+    </div>
+    <div class="w-full flex flex-row flex-nowrap space-x-4">
+        <Button
+            classes="w-1/2"
+            secondary
+            onClick={() => changeIndex(-1)}
+            disabled={stepIndex === 0}
+        >{locale('actions.previous')}</Button>
+        {#if stepIndex < locale('popups.ledgerAppGuide.steps').length - 1}
+            <Button
+                classes="w-1/2"
+                secondary
+                onClick={() => changeIndex(1)}
+            >{locale('actions.next')}</Button>
+        {:else}
+            <Button
+                classes="w-1/2"
+                primary
+                onClick={handleCloseClick}
+            >{locale('actions.close')}</Button>
+        {/if}
+    </div>
+</div>
