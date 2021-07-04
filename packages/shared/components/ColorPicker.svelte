a8ee041402758daf64440284f0b362bdaa6ecc51
Jean Ribeiro <jeanmendesrib@gmail.com>
2021-07-04 14:47:19 -0300

    adds ring and active color to ColorPicker

--- packages/shared/components/ColorPicker.svelte
+++ packages/shared/components/ColorPicker.svelte
@@ -2,17 +2,18 @@
     import { Text } from 'shared/components'
 
     export let title = 'Color picker'
+    export let active = 'turquoise'
 
-    const colors = ['blue', 'turquoise', 'lightblue', 'green', 'yellow', 'orange', 'red', 'purple']
+    const colors = ['blue', 'lightblue', 'turquoise', 'green', 'yellow', 'orange', 'red', 'purple']
 </script>
 
 <div>
-    <div class="flex flex-row mb-6">
+    <div class="flex flex-row mb-4">
         <Text type="h5">{title}</Text>
     </div>
     <div class="flex flex-row justify-between">
         {#each colors as color}
-            <div class='w-6 h-6 bg-{color}-500 rounded-lg'></div>
+            <div class='w-6 h-6 bg-{color}-500 hover:bg-{color}-600 rounded-lg ring-{color}-100 hover:ring-{color}-200 cursor-pointer' class:ring-4="{color === active}"></div>
         {/each}
     </div>
 </div>
