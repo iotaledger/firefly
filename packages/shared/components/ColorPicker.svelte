60f9c73debcab538193ead6958363ed377b99ad4
Jean Ribeiro <jeanmendesrib@gmail.com>
2021-07-06 23:14:02 -0300

    adds color change in ColorPicker component

--- packages/shared/components/ColorPicker.svelte
+++ packages/shared/components/ColorPicker.svelte
@@ -2,9 +2,13 @@
     import { Text } from 'shared/components'
 
     export let title = 'Color picker'
-    export let active = 'turquoise'
+    export let active = 'green'
 
     const colors = ['blue', 'lightblue', 'turquoise', 'green', 'yellow', 'orange', 'red', 'purple']
+
+    const handleClick = color => {
+        active = color;
+    }
 </script>
 
 <div>
@@ -13,7 +17,7 @@
     </div>
     <div class="flex flex-row justify-between">
         {#each colors as color}
-            <div class='w-8 h-8 bg-{color}-500 hover:bg-{color}-600 rounded-lg ring-{color}-100 hover:ring-{color}-200 cursor-pointer' class:ring-4="{color === active}"></div>
+            <div class='w-8 h-8 bg-{color}-500 hover:bg-{color}-600 rounded-lg ring-{color}-500 ring-opacity-30 hover:ring-opacity-40 cursor-pointer' class:ring-4="{color === active}" on:click={() => handleClick(color)}></div>
         {/each}
     </div>
 </div>
