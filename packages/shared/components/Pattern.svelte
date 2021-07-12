22fcf804da213e255e49d7f71f4a88f91b0f84b9
Jean Ribeiro <jeanmendesrib@gmail.com>
2021-07-11 23:02:14 -0300

    removes Pattern width and height

--- packages/shared/components/Pattern.svelte
+++ packages/shared/components/Pattern.svelte
@@ -1,10 +1,8 @@
 <script lang="typescript">
-    export let pattern, classes, width, height, onClick
+    export let pattern, classes, onClick
 </script>
 
 <div
     class="{classes} bg-no-repeat bg-center bg-cover bg cursor-pointer"
-    width={width || '100%'}
-    height={height || '100%'}
     style={`background-image: url("assets/patterns/${pattern}.svg");`}
     on:click={onClick}></div>
