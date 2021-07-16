66cbdbd724e6e4fe905a677a95293a10cd22ca9c
Jean Ribeiro <jeanmendesrib@gmail.com>
2021-07-15 23:01:55 -0300

    improves accessibility

--- packages/shared/components/ColorPicker.svelte
+++ packages/shared/components/ColorPicker.svelte
@@ -7,9 +7,12 @@
 
     const colors = ['blue', 'lightblue', 'turquoise', 'green', 'yellow', 'orange', 'red', 'purple']
 
-    const handleClick = color => {
-        active = color;
+    const changeActive = color => {
+        active = color
     }
+
+    const handleKeyPress = (event, color) => event.key === 'Enter' && changeActive(color)
+    const handleClick = (color) => changeActive(color)
 </script>
 
 <div>
@@ -18,7 +21,9 @@
     </div>
     <div class="flex flex-row justify-between">
         {#each colors as color}
-            <div class='w-8 h-8 bg-{color}-500 hover:bg-{color}-600 rounded-lg ring-{color}-500 ring-opacity-30 hover:ring-opacity-40 cursor-pointer flex justify-center items-center' class:ring-4="{color === active}" on:click={() => handleClick(color)}>
+            <div tabindex="0" class='w-8 h-8 bg-{color}-500 hover:bg-{color}-600 focus:bg-{color}-600 rounded-lg ring-{color}-500 ring-opacity-30 hover:ring-opacity-40 cursor-pointer flex justify-center items-center' class:ring-4="{color === active}"
+            on:click={() => handleClick(color)} on:keypress={(event) => handleKeyPress(event, color)}
+            aria-label={color}>
                 {#if color === active}<Icon icon="checkmark" classes="text-white" />{/if}
             </div>
         {/each}
--- packages/shared/components/PatternPicker.svelte
+++ packages/shared/components/PatternPicker.svelte
@@ -8,9 +8,12 @@
 
     const patterns = ["", "clover", "circles", "clouds", "shapes", "wind", "rain", "organic"]
 
-    const handleClick = pattern => {
-        active = pattern;
+    const changeActive = pattern => {
+        active = pattern
     }
+
+    const handleKeyPress = (event, pattern) => event.key === 'Enter' && changeActive(pattern)
+    const handleClick = (pattern) => changeActive(pattern)
 </script>
 
 <div>
@@ -20,9 +23,10 @@
     <div class="grid grid-cols-4 grid-rows-2 gap-4">
         {#each patterns as pattern}
             <div
-                class="h-20 rounded-lg {pattern === active ? `bg-${color}-500 opacity-100 ring-${color}-500 ring-4 ring-opacity-30 hover:ring-opacity-40` : 'bg-gray-300 hover:bg-{color}-500 opacity-80 hover:opacity-100'} bg-no-repeat bg-center bg-cover bg cursor-pointer flex justify-center items-center"
+                class="h-20 rounded-lg {pattern === active ? `bg-${color}-500 opacity-100 ring-${color}-500 ring-4 ring-opacity-30 hover:ring-opacity-40 focus:ring-opacity-40` : 'bg-gray-300 hover:bg-{color}-500 focus:bg-{color}-500 opacity-80 hover:opacity-100 focus:opacity-100'} bg-no-repeat bg-center bg-cover bg cursor-pointer flex justify-center items-center"
                 style={pattern ? `background-image: url("assets/patterns/${pattern}.svg");` : null}
-                on:click={() => handleClick(pattern)}>
+                on:click={() => handleClick(pattern)} on:keypress={(event) => handleKeyPress(event, pattern)}
+                tabindex="0" aria-label={pattern}>
                 {#if pattern === active}<Icon icon="checkmark" classes="bg-green-600 text-white rounded-full" />{/if}
             </div>
         {/each}
