83c7c10ea8bd2e1405a04301278a0492d306bef6
Charlie Varley <charlie.varley@iota.org>
2021-07-16 19:45:21 +0100

    chore: add legacy permanode

--- packages/shared/lib/migration.ts
+++ packages/shared/lib/migration.ts
@@ -18,7 +18,7 @@ export const LOG_FILE_NAME = 'migration.log'
 
 // TODO: Change back temp migration nodes (previously https://nodes.devnet.iota.org)
 export const MIGRATION_NODES = ['https://nodes-legacy.ledgermigration1.net']
-export const PERMANODE = undefined
+export const PERMANODE = "http://permanode.ledgermigration1.net:4000/api"
 
 export const ADDRESS_SECURITY_LEVEL = 2
 
