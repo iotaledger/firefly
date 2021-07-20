369062ad571a7fe5188bd1a09b354d356fdd270e
Rajiv Shah <rajivshah1@icloud.com>
2021-07-20 17:47:15 -0400

    chore: Remove remote and IPC usage from Ledger (#1254)
    * chore: Remove remote and IPC usage from Ledger
    
    * chore: Disable remote module
    
    * chore: remove unused code
    
    Co-authored-by: Charlie Varley <charlie.varley@iota.org>

--- packages/desktop/electron/main.js
+++ packages/desktop/electron/main.js
@@ -85,7 +85,7 @@ let paths = {
 const defaultWebPreferences = {
     nodeIntegration: false,
     contextIsolation: true,
-    enableRemoteModule: true,
+    enableRemoteModule: false,
     worldSafeExecuteJavaScript: true,
     disableBlinkFeatures: 'Auxclick',
     webviewTag: false,
