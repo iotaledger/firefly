dddbb1c5f731659d27114530507cd1edc4ddcc22
Umair Sarfraz <umair.sarfraz@iota.org>
2021-07-20 21:06:45 +0200

    feat: remove unnecessary code related to ledger simulator (#1269)

--- packages/desktop/electron/lib/Ledger.js
+++ packages/desktop/electron/lib/Ledger.js
@@ -3,7 +3,7 @@ const TransportSpeculos = require('@ledgerhq/hw-transport-node-speculos').defaul
 const Iota = require('hw-app-iota').default
 const { ipcRenderer, remote } = require('electron')
 
-const USE_SIMULATOR = false
+const USE_SIMULATOR = true
 const SIMULATOR_PORT = 9999;
 
 const ipc = ipcRenderer;
@@ -11,6 +11,16 @@ const Wallet = remote.getCurrentWindow().webContents;
 
 const Errors = {
     LEDGER_CANCELLED: 'Transaction cancelled on Ledger device.',
+}
+
+async function createTransport() {
+    if (USE_SIMULATOR) {
+        return await TransportSpeculos.open({
+            apduPort: SIMULATOR_PORT
+        })
+    }
+
+    return await TransportHid.create();
 }
 
 class Ledger {
@@ -100,7 +110,7 @@ class Ledger {
 
             const callback = async () => {
                 try {
-                    this.transport = await TransportHid.create();
+                    this.transport = await createTransport();
 
                     this.iota = new Iota(this.transport);
 
@@ -204,92 +214,10 @@ class Ledger {
 }
 
 class LedgerSpeculos extends Ledger {
-    async selectSeed(index, page, security) {
-        if (!this.connected) {
-            Wallet.send('ledger', { awaitConnection: true });
-            await this.awaitConnection();
-
-            Wallet.send('ledger', { awaitConnection: false });
-        }
-
-        if (!this.connected) {
-            throw new Error('Ledger connection error');
-        }
-
-        if (this.iota) {
-            this.transport.close();
-            this.iota = null;
-        }
-
-        await this.awaitApplication(index, page, security);
-
-        return this.iota;
-    }
-
     async awaitConnection() {
         return new Promise((resolve) => {
             this.connected = true;
             resolve()
-        });
-    }
-
-    async awaitApplication(index, page, security) {
-        return new Promise((resolve, reject) => {
-            let timeout = null;
-            let rejected = false;
-
-            const callback = async () => {
-                try {
-                    this.transport = await TransportSpeculos.open({
-                        apduPort: SIMULATOR_PORT
-                    })
-
-                    this.iota = new Iota(this.transport);
-
-                    await this.iota.setActiveSeed(`44'/4218'/${index}'/${page}'`, security || 2);
-
-                    Wallet.send('ledger', { awaitApplication: false });
-                    clearTimeout(timeout);
-
-                    resolve(true);
-                } catch (error) {
-                    if (this.transport) {
-                        this.transport.close();
-                    }
-                    this.iota = null;
-
-                    Wallet.send('ledger', { awaitApplication: true });
-
-                    if (rejected) {
-                        return;
-                    }
-
-                    // Retry application await on error 0x6e00 - IOTA application not open
-                    if (error.statusCode === 0x6e00) {
-                        timeout = setTimeout(() => callback(), 4000);
-                    } else {
-                        Wallet.send('ledger', { awaitApplication: false });
-                        reject(error);
-                    }
-                }
-            };
-
-            callback();
-
-            const callbackAbort = (_e, message) => {
-                if (message && message.abort) {
-                    rejected = true;
-
-                    ipc.removeListener('ledger', callbackAbort);
-
-                    if (timeout) {
-                        clearTimeout(timeout);
-                    }
-                    reject(Errors.LEDGER_CANCELLED);
-                }
-            };
-
-            ipc.on('ledger', callbackAbort);
         });
     }
 }
