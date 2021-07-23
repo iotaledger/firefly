9dd34ee3f8391cb660731a95e5315b43018fe5c4
Begoña Alvarez <balvarez@boxfish.studio>
2021-07-23 17:13:15 +0200

    Update address pre sizes

--- packages/shared/routes/setup/ledger/views/GenerateNewAddress.svelte
+++ packages/shared/routes/setup/ledger/views/GenerateNewAddress.svelte
@@ -123,7 +123,7 @@
                 <Text type="p" secondary classes="mb-10">{locale('views.generateNewLedgerAddress.confirmBody')}</Text>
                 <div class="rounded-lg bg-gray-50 dark:bg-gray-800 p-5 text-center">
                     <Text type="h5" highlighted classes="mb-2">{locale('general.newAddress')}</Text>
-                    <Text type="pre">{formatAddressForLedger(newAddress)}</Text>
+                    <Text type="pre" bigger>{formatAddressForLedger(newAddress)}</Text>
                 </div>
             {:else}
                 <Text type="h2" classes="mb-5">{locale('views.generateNewLedgerAddress.confirmedTitle')}</Text>
@@ -133,7 +133,7 @@
                         <Icon icon="success-check" classes="text-white" />
                     </div>
                     <Text type="h5" highlighted classes="mb-2">{locale('general.newAddress')}</Text>
-                    <Text type="pre">{formatAddressForLedger(newAddress)}</Text>
+                    <Text type="pre" bigger>{formatAddressForLedger(newAddress)}</Text>
                 </div>
             {/if}
         </div>
