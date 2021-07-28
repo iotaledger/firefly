bed96a32ab2d1cb96b8a09b652e9a0b5c018a854
Begoña Alvarez <balvarez@boxfish.studio>
2021-07-28 20:08:44 +0200

    add missing import

--- packages/shared/lib/ledger.ts
+++ packages/shared/lib/ledger.ts
@@ -1,6 +1,6 @@
 import { removeAddressChecksum } from 'shared/lib/migration'
 import { closePopup, openPopup, popupState } from 'shared/lib/popup'
-import { forceNextRoute, walletSetupType } from 'shared/lib/router'
+import { forceNextRoute, resetWalletRoute, walletSetupType } from 'shared/lib/router'
 import { AppRoute, SetupType } from 'shared/lib/typings/routes'
 import { api } from 'shared/lib/wallet'
 import { get, writable } from 'svelte/store'
@@ -16,6 +16,7 @@ import {
     LegacyLedgerErrorName
 } from './typings/ledger'
 import type { NotificationType } from './typings/notification'
+
 
 const LEDGER_STATUS_POLL_INTERVAL_ON_DISCONNECT = 1500
 
@@ -38,7 +39,7 @@ export function getLedgerDeviceStatus(
 
             const state = get(ledgerDeviceState)
             const isConnected = (legacy && state === LedgerDeviceState.LegacyConnected)
-                            || (!legacy && state === LedgerDeviceState.Connected)
+                || (!legacy && state === LedgerDeviceState.Connected)
             if (isConnected) {
                 onConnected()
             } else {
@@ -61,9 +62,9 @@ export function calculateLedgerDeviceState(status: LedgerStatus): LedgerDeviceSt
     if (locked) {
         return LedgerDeviceState.Locked
     } else {
-        switch(app?.name) {
+        switch (app?.name) {
             default:
-                if(connected) {
+                if (connected) {
                     /**
                      * NOTE: "BOLOS" is the name of the Ledger operating system and is
                      * sometimes registered as an app.
@@ -134,7 +135,7 @@ export function displayNotificationForLedgerProfile(
         const allowedToNotify = allowMultiple ? true : isNewNotification(notificationType)
         const canNotify = allowedToNotify && (ignoreNotDetected ? state !== LedgerDeviceState.NotDetected : true)
 
-        const isConnected =  (!legacy && state === LedgerDeviceState.Connected)
+        const isConnected = (!legacy && state === LedgerDeviceState.Connected)
         const isLegacyConnected = (legacy && state === LedgerDeviceState.LegacyConnected)
         const shouldNotify = (!isConnected && !isLegacyConnected) || error
 
@@ -153,7 +154,7 @@ export function displayNotificationForLedgerProfile(
     if (checkDeviceStatus) {
         getLedgerDeviceStatus(
             false,
-            () => {},
+            () => { },
             () => _notify(),
             () => _notify()
         )
@@ -165,10 +166,10 @@ export function displayNotificationForLedgerProfile(
 }
 
 export function isLedgerError(error: any): boolean {
-    if(!error) return false
+    if (!error) return false
 
     let errorType: string = ''
-    switch(typeof error) {
+    switch (typeof error) {
         case 'object':
             errorType = error.type || error.name
             break
