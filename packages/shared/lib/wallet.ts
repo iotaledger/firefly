e4b7558343a6f7f6fcd6376c3c35e4e21917e025
Matthew Maxwell <maxwellmattryan@gmail.com>
2021-10-04 13:26:14 -0500

    chore: Reduce duplicated code in wallet proxy logic

--- packages/shared/lib/wallet.ts
+++ packages/shared/lib/wallet.ts
@@ -391,40 +391,36 @@ export const api: IWalletApi = new Proxy(
     { ...window['__WALLET_API__'] },
     {
         get: (target, propKey) => {
-            const _handleCallbackError = (err) => {
+            /* eslint-disable @typescript-eslint/no-explicit-any */
+            const _handleCallbackError = (err: any) => {
                 const title = `Callback Error ${propKey.toString()}`
 
                 console.error(title, err)
                 void Electron.unhandledException(title, { message: err?.message, stack: err?.stack })
             }
 
+            /* eslint-disable @typescript-eslint/no-explicit-any */
+            const _handleCallbackResult = (args: any[], idx: number, result: 'onSuccess' | 'onError'): any[] => {
+                const originalResultFn = args[idx][result]
+
+                args[idx][result] = (payload) => {
+                    try {
+                        originalResultFn(payload)
+                    } catch (err) {
+                        _handleCallbackError(err)
+                    }
+                }
+
+                return args
+            }
+
             const originalMethod = target[propKey]
             return (...args) => {
                 for (let i = args.length - 1; i >= 0; i--) {
                     if (args[i]?.onSuccess) {
-                        const originalSuccess = args[i].onSuccess
-                        args[i].onSuccess = (payload) => {
-                            try {
-                                originalSuccess(payload)
-                            } catch (err) {
-                                _handleCallbackError(err)
-                            }
-                        }
-                    }
-
-                    if (args[i]?.onError) {
-                        const originalError = args[i].onError
-                        args[i].onError = (payload) => {
-                            try {
-                                originalError(payload)
-                            } catch (err) {
-                                _handleCallbackError(err)
-                            }
-                        }
-                    }
-
-                    if (args[i]?.onSuccess || args[i]?.onError) {
-                        break
+                        args = _handleCallbackResult(args, i, 'onSuccess')
+                    } else if (args[i]?.onError) {
+                        args = _handleCallbackResult(args, i, 'onError')
                     }
                 }
 
