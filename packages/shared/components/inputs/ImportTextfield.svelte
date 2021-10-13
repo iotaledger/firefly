c141a414364bf9a057ff637ea16212e0daa4cf44
Begoña Alvarez <balvarez@boxfish.studio>
2021-10-13 17:17:53 +0200

    fix textfield debounce

--- packages/shared/components/inputs/ImportTextfield.svelte
+++ packages/shared/components/inputs/ImportTextfield.svelte
@@ -62,6 +62,7 @@
         }
     }
 
+    /* eslint-disable @typescript-eslint/no-misused-promises */
     const handleKeyDown = async () => {
         value = ''
         statusMessage = ''
@@ -124,10 +125,7 @@
         class="text-14 leading-140 resize-none w-full p-4 pb-3 rounded-xl border border-solid {error ? 'border-red-300 hover:border-red-500 focus:border-red-500' : 'border-gray-300 hover:border-gray-500 dark:border-gray-700 dark:hover:border-gray-700'}
         text-gray-500 dark:text-white bg-white dark:bg-gray-800 scroll-secondary"
         bind:value={content}
-        on:keydown={() => {
-            /* eslint-disable @typescript-eslint/no-misused-promises */
-            debounce(handleKeyDown)
-        }}
+        on:keydown={debounce(handleKeyDown)}
         placeholder=""
         spellcheck={false}
         autofocus />
