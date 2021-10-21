3425bdee4fac1e4d64f580020bb727522178c57d
Matthew Maxwell <maxwellmattryan@gmail.com>
2021-10-20 20:25:40 -0500

    chore: Change some strings and make fine adjustments

--- packages/shared/components/ButtonCheckbox.svelte
+++ packages/shared/components/ButtonCheckbox.svelte
@@ -19,7 +19,7 @@
     on:click={() => (value = !value)}
     bind:this={buttonElement}
     type="button"
-    class="w-full flex flex-row p-4 mb-4 rounded-2xl border border-1 border-solid items-center justify-between border-gray-300 dark:border-gray-700 hover:border-gray-500 dark:hover:border-gray-700 focus:border-gray-500 focus:hover:border-gray-700"
+    class="w-full h-auto flex flex-row p-4 pt-10 pb-10 mb-4 rounded-xl border border-1 border-solid items-center justify-between border-gray-300 dark:border-gray-700 hover:border-gray-500 dark:hover:border-gray-700 focus:border-gray-500 focus:hover:border-gray-700"
     style="height: 72px">
     <div class="flex flex-row items-center">
         <Icon {icon} classes="text-blue-500" />
@@ -27,5 +27,5 @@
             <slot />
         </Text>
     </div>
-    <Checkbox bind:checked={value} classes="mb-0 pointer-events-none" tabindex={-1} />
+    <Checkbox bind:checked={value} classes="ml-1 mb-0 pointer-events-none" tabindex={-1} />
 </button>
