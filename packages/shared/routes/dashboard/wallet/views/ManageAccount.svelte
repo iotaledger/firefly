9ce892f7baf149f1eecaee9f116ff1d579f08c40
annika <annika.kaiser@gmail.com>
2021-10-24 15:57:09 +0200

    task: add logic to manage wallet

--- packages/shared/routes/dashboard/wallet/views/ManageAccount.svelte
+++ packages/shared/routes/dashboard/wallet/views/ManageAccount.svelte
@@ -1,5 +1,5 @@
 <script lang="typescript">
-    import { Button, Input, Text } from 'shared/components'
+    import { Button, Input, Text, Icon } from 'shared/components'
     import { getTrimmedLength } from 'shared/lib/helpers'
     import { accountRoute, walletRoute } from 'shared/lib/router'
     import { AccountRoutes, WalletRoutes } from 'shared/lib/typings/routes'
@@ -26,11 +26,22 @@
     ]
     let availablePatterns = ['default', 'circles', 'clouds', 'clover', 'organic', 'rain', 'shapes', 'wind']
 
+    let updatedColor
+    let updatedPattern
+
     let accountAlias = alias
     let isBusy = false
 
     // This looks odd but sets a reactive dependency on accountAlias, so when it changes the error will clear
     $: accountAlias, (error = '')
+
+    const handleColorClick = (selectedColor) => {
+        updatedColor = selectedColor
+    }
+
+    const handlePatternClick = (selectedPattern) => {
+        updatedPattern = selectedPattern
+    }
 
     const handleSaveClick = () => {
         const trimmedAccountAlias = accountAlias.trim()
@@ -108,19 +119,42 @@
             </div>
             <div class="w-full h-full grid md:grid-cols-8 sm:grid-cols-4 gap-6">
                 {#each availableColors as availableColor}
-                    <div class="bg-{availableColor} rounded-lg w-8 h-8 cursor-pointer hover:opacity-50 focus:ring-4" />
+                    <div
+                        class="bg-{availableColor} rounded-lg w-8 h-8 cursor-pointer hover:opacity-50 focus:ring-2 focus:ring-blue-600"
+                        on:click={() => handleColorClick(availableColor)}>
+                        {#if updatedColor === availableColor}
+                            <Icon icon="checkmark" classes="h-full w-full text-white justify-self-center" />
+                        {/if}
+                    </div>
                 {/each}
             </div>
         </div>
         <!-- set wallet pattern -->
-        <div class="w-full h-auto flex flex-col justify-center">
+        <div class="w-full h-auto flex flex-col justify-between">
             <div class="flex flex-row mb-6">
                 <Text type="h5">Wallet pattern</Text>
             </div>
-            <div class="grid grid-cols-4 gap-4 justify-center">
+            <div class="flex-col grid grid-cols-4 gap-4">
                 {#each availablePatterns as availablePattern}
                     <div
-                        class="h-20 w-20 rounded-lg bg-cover bg-blue-500 bg-{availablePattern} cursor-pointer hover:opacity-50" />
+                        class="rounded-lg bg-cover bg-{updatedPattern === availablePattern ? updatedColor : 'blue-50'} cursor-pointer hover:opacity-50 relative"
+                        style="height: 100%; width: 100%"
+                        on:click={() => handlePatternClick(availablePattern)}>
+                        <img
+                            class="object-cover z-0"
+                            style={updatedPattern === availablePattern ? updatedColor : 'filter: invert(.5) sepia(1) saturate(5) hue-rotate(175deg);'}
+                            width="100%"
+                            height="100%"
+                            src={`assets/patterns/wallet-backgrounds/${availablePattern}.svg`}
+                            alt="" />
+                        {#if updatedPattern === availablePattern}
+                            <div
+                                class="bg-green-800 rounded-full absolute bottom-5 left-9 w-full h-full"
+                                style="height: 25px; width: 25px">
+                                <Icon icon="checkmark" classes="text-white justify-self-center" />
+                            </div>
+                        {/if}
+                    </div>
                 {/each}
             </div>
         </div>
