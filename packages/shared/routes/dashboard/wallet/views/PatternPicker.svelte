78263fb2c471081e8bf366cd56c728e0e440509e
Fogha <collinslord50@gmail.com>
2021-10-25 09:40:47 +0100

    displaying only available svgs

--- packages/shared/routes/dashboard/wallet/views/PatternPicker.svelte
+++ packages/shared/routes/dashboard/wallet/views/PatternPicker.svelte
@@ -3,7 +3,7 @@
 
     export let walletPatterns
     export let selectedPattern
-    export let selectedColor
+    export let selectedColor = 'blue'
 
     const patternSelection = (pattern) => {
         selectedPattern = pattern
@@ -27,7 +27,10 @@
         <div
             class="h-14 w-16 bg-{selectedPattern === pattern ? selectedColor : ''}-500 rounded m- flex justify-center items-center colorPicker"
             on:click={() => patternSelection(pattern)}>
-            <img class="object-cover" width="100%" height="100%" src={`assets/patterns/${pattern}.svg`} alt="" />
+            {#if pattern !== '' }
+                <img class="object-cover" width="100%" height="100%" src={`assets/patterns/${pattern}.svg`} alt="" />
+            {/if}
+
             {#if selectedPattern === pattern}
                 <div class="bg-green-100 h-5 w-5 rounded-2xl flex justify-center items-center z-10 absolute">
                     <Icon icon="success-check" classes="h-5  w-5 text-white" />
