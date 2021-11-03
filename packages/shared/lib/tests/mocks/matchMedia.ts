13633903d86948118ec570123b19515064a5ec32
Matthew Maxwell <44885822+maxwellmattryan@users.noreply.github.com>
2021-11-03 10:52:24 -0500

    feat: System default theme (#1211)
    * Add capability for system default theme selection
    
    * Change variable name to remove changed files
    
    * Add update on splash page and locale support (only en and ja right now)
    
    * Remove ja punctuation for consistency in settings pages
    
    * Review and add code comments
    
    * Change icon, fix comments, and fix JA typos
    
    * Perform media query only once to handle inconsistent OS behaviour
    
    * Add more specific comment and variable name
    
    * test: Add necessary mock to test

---
+++ packages/shared/lib/tests/mocks/matchMedia.ts
@@ -0,0 +1,17 @@
+/**
+ * NOTE: matchMedia is not implemented in JSDOM, so it must be mocked
+ * here.
+ */
+Object.defineProperty(window, 'matchMedia', {
+    writable: true,
+    value: jest.fn().mockImplementation((query) => ({
+        matches: false,
+        media: query,
+        onchange: null,
+        addListener: jest.fn(), // deprecated
+        removeListener: jest.fn(), // deprecated
+        addEventListener: jest.fn(),
+        removeEventListener: jest.fn(),
+        dispatchEvent: jest.fn(),
+    })),
+})
