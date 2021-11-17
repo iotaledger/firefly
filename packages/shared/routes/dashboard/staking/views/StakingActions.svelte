1c9c7890f6cd49447e2edab851858f8c786623a8
Matthew Maxwell <maxwellmattryan@gmail.com>
2021-11-17 09:07:04 +0000

    fix: Change balance if staked funds present

--- packages/shared/routes/dashboard/staking/views/StakingActions.svelte
+++ packages/shared/routes/dashboard/staking/views/StakingActions.svelte
@@ -1,9 +1,13 @@
 <script lang="typescript">
-    import { Button, StakingIndicator, Text } from 'shared/components'
+    import { BalanceSummary, Button, StakingIndicator, Text } from 'shared/components'
     import { Locale } from 'shared/lib/typings/i18n'
     import { stakedAccounts, stakingEventStatus } from 'shared/lib/participation'
-    import { openPopup, popupState } from 'shared/lib/popup'
+    import { openPopup } from 'shared/lib/popup'
     import { StakingEventStatus } from 'shared/lib/typings/participation'
+    import { wallet } from '../../../../lib/wallet'
+    import { get } from 'svelte/store'
+    import { formatUnitBestMatch } from '../../../../lib/units'
+    import { WalletAccount } from '../../../../lib/typings/wallet'
 
     export let locale: Locale
 
@@ -11,6 +15,12 @@
 
     // TODO: Remove later once polling handles automatically updating the stake
     stakingEventStatus.set(StakingEventStatus.PreStake)
+
+    const sumRawAccountBalances = (accounts: WalletAccount[]): number =>
+        accounts.map((a) => a.rawIotaBalance).reduce((amt, cur) => amt + cur, 0)
+
+    const calculateBalanceFromAccounts = (accounts: WalletAccount[]): string =>
+        formatUnitBestMatch(accounts.map((a) => a.rawIotaBalance).reduce((amt, cur) => amt + cur, 0))
 
     const handleStakeFundsClick = () => {
         const isPreStake = $stakingEventStatus === StakingEventStatus.PreStake
@@ -25,21 +35,30 @@
         <Text type="h5" classes="text-2xl">{locale('views.staking.title')}</Text>
         <StakingIndicator fundsStaked={true} />
     </div>
-    <div class="h-2/6">
-        <Text type="h5" classes="text-3xl">
-            7.38 Gi
-        </Text>
-        <Text type="p" secondary>
-            {locale('views.staking.manager.availableForStaking')}
-        </Text>
-    </div>
     {#if hasStakedAccounts}
+        <div class="h-2/6">
+            <Text type="h5" classes="text-3xl">
+                {calculateBalanceFromAccounts($stakedAccounts)}
+            </Text>
+<!--            <BalanceSummary balanceRaw={sumRawAccountBalances($stakedAccounts)} />-->
+            <Text type="p" secondary>
+                Staked funds
+            </Text>
+        </div>
         <Button classes="w-full" secondary onClick={handleStakeFundsClick}>
             Manage stake
         </Button>
     {:else}
+        <div class="h-2/6">
+            <Text type="h5" classes="text-3xl">
+                {calculateBalanceFromAccounts(get($wallet.accounts))}
+            </Text>
+            <Text type="p" secondary>
+                Available for staking
+            </Text>
+        </div>
         <Button classes="w-full" onClick={handleStakeFundsClick}>
-            {locale('actions.stakeFunds')}
+            Stake funds
         </Button>
     {/if}
 </div>
