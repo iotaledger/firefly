8de07dee30832aea1cc1ce98178bf94f4203a8ee
Amadeo Marchioni <amadeuz@github.com>
2021-11-25 13:19:37 -0300

    update type definitions

--- packages/backend/bindings/capacitor/src/definitions.ts
+++ packages/backend/bindings/capacitor/src/definitions.ts
@@ -1,15 +1,6 @@
-declare module "@capacitor/core" {
-  interface PluginRegistry {
-    WalletPlugin: WalletPlugin;
-  }
-}
-
-export interface InitOptions {
-  storagePath?: string
-}
-
 export interface WalletPlugin {
-  initialize(options?: InitOptions): void;
-  sendMessage(message: { [k: string]: any }): Promise<void>;
-  addListener(event: 'walletMessageReceived', cb: (message: any) => void): void;
+  initialize(actorId: String): Promise<void>;
+  destroy(actorId: String): Promise<void>;
+  sendMessage(message: { [key: string]: any }): Promise<void>;
+  addListener(actorId: String, id: String, event: { [key: string]: any }): Promise<void>;
 }
--- packages/backend/bindings/capacitor/src/index.ts
+++ packages/backend/bindings/capacitor/src/index.ts
@@ -1,1 +1,8 @@
+import { registerPlugin } from '@capacitor/core';
+
+import type { WalletPlugin } from './definitions';
+
+const Wallet = registerPlugin<WalletPlugin>('Wallet');
+
 export * from './definitions';
+export { Wallet };
