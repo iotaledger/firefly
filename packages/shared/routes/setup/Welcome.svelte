ec7c27f2737634b3480be2dd3f163a9d82c49f55
Begoña Alvarez <balvarez@boxfish.studio>
2021-11-28 19:38:21 +0100

    Prevent scrollbar to show on mobile welcome view

--- packages/shared/routes/setup/Welcome.svelte
+++ packages/shared/routes/setup/Welcome.svelte
@@ -3,12 +3,14 @@
     import { mobile } from 'shared/lib/app'
     import { appSettings } from 'shared/lib/appSettings'
     import { locales, setLanguage, _ } from 'shared/lib/i18n'
-    import { createEventDispatcher } from 'svelte'
     import { Locale } from 'shared/lib/typings/i18n'
+    import { createEventDispatcher } from 'svelte'
 
     export let locale: Locale
 
     const dispatch = createEventDispatcher()
+
+    $: languageList = Object.values(locales).map((locale) => ({ value: locale, label: locale }))
 
     function handleContinueClick() {
         dispatch('next')
@@ -20,18 +22,42 @@
     }
 </script>
 
+<style type="text/scss">
+    .languages {
+        max-height: calc(100vh - 100vw - 150px);
+        @screen md {
+            max-height: inherit;
+        }
+    }
+</style>
+
 <OnboardingLayout allowBack={false}>
     <div slot="leftpane__content">
         <div class="flex flex-col {$mobile && 'items-center text-center px-10'} space-y-4 mb-8">
-            <Logo width="64px" logo="logo-firefly" classes="mb-6" />
+            {#if !$mobile}
+                <Logo width="64px" logo="logo-firefly" classes="mb-6" />
+            {/if}
             <Text type={$mobile ? 'h3' : 'h1'}>{locale('views.onboarding1.title')}</Text>
             <Text type="p" secondary>{locale('views.onboarding1.body')}</Text>
         </div>
-        <Dropdown
-            sortItems={true}
-            onSelect={handleLanguage}
-            value={locales[$appSettings.language]}
-            items={Object.values(locales).map((locale) => ({ value: locale, label: locale }))} />
+        {#if $mobile}
+            <div class="languages flex flex-wrap space-y-2 overflow-y-auto">
+                {#each languageList as language}
+                    <button
+                        class="relative flex items-center p-2 w-full whitespace-nowrap rounded-md"
+                        on:click={() => handleLanguage(language)}
+                        class:active={language?.label === locales[$appSettings.language]}>
+                        <Text type="p" smaller>{language?.label}</Text>
+                    </button>
+                {/each}
+            </div>
+        {:else}
+            <Dropdown
+                sortItems={true}
+                onSelect={handleLanguage}
+                value={locales[$appSettings.language]}
+                items={languageList} />
+        {/if}
     </div>
     <div slot="leftpane__action">
         <Button onClick={() => handleContinueClick()} classes="w-full">{locale('actions.continue')}</Button>
