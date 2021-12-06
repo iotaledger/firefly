babcc71390d08e12b48e55a153d47fdfea115d5e
Amadeo Marchioni <amadeuz@github.com>
2021-12-06 08:59:31 -0300

    fix wallet plugin type definitions

--- packages/backend/bindings/capacitor/src/definitions.ts
+++ packages/backend/bindings/capacitor/src/definitions.ts
@@ -1,8 +1,9 @@
 import type { PluginListenerHandle } from "@capacitor/core";
+//import type { MessageResponse } from '../../../../shared/lib/typings/bridge'
 export interface WalletPluginTypes {
   initialize(options: { actorId: string }): Promise<void>
   listen(options: { actorId: string, id: string, event: string }): Promise<string>
   destroy(options: { actorId: string }): Promise<void>
   sendMessage(message: { [key: string]: any }): Promise<void>
-  addListener(walletEvent: 'walletEvent', cb: (jsonResponse: string) => void): PluginListenerHandle
+  addListener(walletEvent: 'walletEvent', cb: (message: {walletResponse: any}) => void): PluginListenerHandle
 }
