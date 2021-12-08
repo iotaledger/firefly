a4e95cbd84dbc1ef22617ec9effa18848a3823da
Charlie Varley <charlievarley@MacBook-Pro.local>
2021-12-08 00:47:17 +0000

    update: change balance finder string

--- packages/shared/components/popups/BalanceFinder.svelte
+++ packages/shared/components/popups/BalanceFinder.svelte
@@ -82,7 +82,7 @@
         {/if}
     </div>
     <div class="flex flex-row flex-nowrap w-full space-x-4">
-        <Button classes="w-full" secondary onClick={handleCancelClick} disabled={isBusy}>{locale('actions.cancel')}</Button>
+        <Button classes="w-full" secondary onClick={handleCancelClick} disabled={isBusy}>{locale('actions.done')}</Button>
         <Button classes="w-full" onClick={handleFindBalances} disabled={($isSoftwareProfile && $isStrongholdLocked && password.length === 0) || isBusy}>
             {#if isBusy}
                 <Spinner busy={true} message={locale('actions.searching')} classes="justify-center" />
