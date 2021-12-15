4d877cdc8d6e4321b59b55ee6e5e83cc35c7c491
Begoña Alvarez <balvarez@boxfish.studio>
2021-12-15 18:38:22 +0100

    replace inline css in account tile for scss

--- packages/shared/components/AccountTile.svelte
+++ packages/shared/components/AccountTile.svelte
@@ -1,6 +1,6 @@
 <script lang="typescript">
-    import { get } from 'svelte/store'
     import { Icon, Text, Tooltip } from 'shared/components'
+    import { appSettings } from 'shared/lib/appSettings'
     import { localize } from 'shared/lib/i18n'
     import {
         formatStakingAirdropReward,
@@ -21,10 +21,9 @@
     import { ParticipationEventState, ParticipationOverview, StakingAirdrop } from 'shared/lib/participation/types'
     import { openPopup } from 'shared/lib/popup'
     import { getBestTimeDuration } from 'shared/lib/time'
+    import type { WalletAccount } from 'shared/lib/typings/wallet'
     import { formatUnitBestMatch } from 'shared/lib/units'
     import { capitalize } from 'shared/lib/utils'
-    import { wallet } from 'shared/lib/wallet'
-    import type { WalletAccount } from 'shared/lib/typings/wallet'
 
     export let name = ''
     export let balance = ''
@@ -37,6 +36,8 @@
     export let disabled = false
 
     export let onClick = (): void | string => ''
+
+    $: darkModeEnabled = $appSettings.darkMode
 
     if (airdrop) {
         disabled = true
@@ -173,59 +174,117 @@
         &.size-l {
             min-height: 140px;
         }
+        &.partial-stake {
+            @apply border;
+            @apply border-solid;
+            @apply border-orange-500;
+            &:not(:hover) {
+                @apply bg-transparent;
+            }
+            &:hover {
+                @apply border-transparent;
+            }
+            &.darkmode {
+                @apply border;
+                @apply border-solid;
+                @apply border-orange-500;
+                &:hover {
+                    @apply border-transparent;
+                }
+            }
+        }
+        &.staked:not(.partial-stake) {
+            @apply border;
+            @apply border-solid;
+            @apply border-gray-200;
+            &:not(:hover) {
+                @apply bg-transparent;
+            }
+            &:hover {
+                @apply border-transparent;
+            }
+            &.darkmode {
+                @apply border-gray-900;
+            }
+        }
+        &.airdrop {
+            @apply opacity-50;
+            @apply border;
+            @apply border-solid;
+            @apply border-gray-200;
+            &:not(:hover) {
+                @apply bg-transparent;
+            }
+            &:hover {
+                @apply bg-gray-500;
+            }
+            &.darkmode {
+                @apply bg-gray-900;
+                @apply border-transparent;
+                &:hover {
+                    @apply bg-gray-600;
+                }
+            }
+        }
+        &.hidden-wallet {
+            @apply opacity-50;
+        }
     }
 </style>
 
 <button
     on:click={handleTileClick}
-    class="size-{size} group rounded-xl {showWarningState ? 'bg-yellow-100 hover:bg-yellow-400' : `border-gray-100 dark:border-gray-900 hover:bg-${color}-500 ${isStaked ? `border border-1 border-solid border-gray-200 dark:border-gray-900 hover:border-${color}-500` : 'bg-gray-100 dark:bg-gray-900'}`} font-400 flex flex-col justify-between text-left p-{size === 's' ? '3' : '6'} {hidden ? 'opacity-50' : ''} {airdrop ? 'opacity-50 bg-gray-100 dark:bg-gray-900 hover:bg-gray-700 dark:hover:bg-gray-600' : ''}">
+    class="bg-gray-100 dark:bg-gray-900 hover:bg-{color}-500 size-{size} group rounded-xl font-400 flex flex-col justify-between text-left p-{size === 's' ? '3' : '6'}"
+    class:staked={isStaked}
+    class:partial-stake={showWarningState}
+    class:airdrop
+    class:hidden-wallet={hidden}
+    class:darkmode={darkModeEnabled}>
     <div class="mb-2 w-full flex flex-row justify-between items-start space-x-1.5">
         <div class="flex flex-row space-x-1.5 items-start">
             {#if showWarningState}
                 <div bind:this={tooltipAnchor} on:mouseenter={toggleTooltip} on:mouseleave={toggleTooltip}>
-                    <Icon icon="exclamation" width="16" height="16" classes="mt-0.5 fill-current text-gray-800" />
+                    <Icon
+                        icon="exclamation"
+                        width="16"
+                        height="16"
+                        classes="mt-0.5 fill-current text-orange-500 group-hover:text-white" />
                 </div>
             {:else if isStaked}
                 <Icon
                     icon="tokens"
                     width="16"
                     height="16"
-                    classes="fill-current mt-0.5 text-gray-800 dark:text-white" />
+                    classes="fill-current mt-0.5 text-gray-800 dark:text-white group-hover:text-white" />
             {/if}
             <Text
                 bold
                 smaller={size === 's'}
                 overrideColor
-                classes="inline text-gray-800 {showWarningState ? '' : 'dark:text-white group-hover:text-white'} overflow-hidden overflow-ellipsis">
+                classes="inline text-gray-800 dark:text-white group-hover:text-white overflow-hidden overflow-ellipsis">
                 {getName()}
             </Text>
         </div>
         {#if airdrop}
             <Icon
                 icon={airdrop}
-                classes="fill-current text-gray-{disabled ? '500' : '400'} dark:text-gray-700"
+                classes="fill-current text-gray-{disabled ? '500' : '400'} dark:text-gray-700 group-hover:text-white"
                 width={size === 's' ? 13 : 18}
                 height={size === 's' ? 13 : 18} />
         {:else if ledger}
             <Icon
                 icon="ledger"
-                classes="fill-current text-gray-400 dark:text-gray-700"
+                classes="fill-current text-gray-800 dark:text-white group-hover:text-white"
                 width={size === 's' ? 13 : 18}
                 height={size === 's' ? 13 : 18} />
         {/if}
     </div>
     <div
         class="flex {size === 'l' ? 'flex-row space-x-4' : 'flex-col space-y-1'} justify-between w-full flex-{size === 'l' ? 'nowrap' : 'wrap'}">
-        <Text
-            smaller
-            overrideColor
-            classes="block text-gray-800 {showWarningState ? '' : 'dark:text-white group-hover:text-white'}">
+        <Text smaller overrideColor classes="block text-gray-800 dark:text-white group-hover:text-white">
             {#if airdrop}{formatStakingAirdropReward(airdrop, Number(balance), 6)}{:else}{balance}{/if}
         </Text>
-        <Text
-            smaller
-            overrideColor
-            classes="block text-blue-500 dark:text-gray-600 {showWarningState ? '' : 'group-hover:text-white'}">
+        <Text smaller overrideColor classes="block text-blue-500 dark:text-gray-600 group-hover:text-white">
             {balanceEquiv}
         </Text>
     </div>
