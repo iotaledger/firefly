3b46330c75683b91506655099eddb0df77e00304
Begoña Alvarez <balvarez@boxfish.studio>
2021-12-15 18:36:40 +0100

    hotfix svg slow css transition

--- packages/shared/components/icon/Index.svelte
+++ packages/shared/components/icon/Index.svelte
@@ -14,6 +14,10 @@
 <style type="text/scss">
     .boxed {
         border-radius: 0.625rem; // TODO: add to tailwind
+    } 
+    /* Hotfix to avoid the SVG slow transition */
+    svg, svg path {
+        transition: background 0.05s, color 0.05s, border-color 0.05s, opacity 0.05s;
     }
 </style>
 
