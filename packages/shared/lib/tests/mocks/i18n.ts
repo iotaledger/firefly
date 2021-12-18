bfe05f4e908fbd4717dad7d5dd7095f954878c70
Rajiv Shah <rajivshah1@icloud.com>
2021-12-18 05:54:21 -0500

    chore(shared): Remove unused strings from i18n mock

--- packages/shared/lib/tests/mocks/i18n.ts
+++ packages/shared/lib/tests/mocks/i18n.ts
@@ -7,13 +7,9 @@
 // TODO: Write a function later to flatten all of the object
 //  property paths in locales/en.json.
 const LOCALE_DATA = {
-    'general.time.day': 'day',
     'general.time.days': 'days',
-    'general.time.hour': 'hour',
     'general.time.hours': 'hours',
-    'general.time.minute': 'minute',
     'general.time.minutes': 'minutes',
-    'general.time.second': 'second',
     'general.time.seconds': 'seconds',
 }
 
