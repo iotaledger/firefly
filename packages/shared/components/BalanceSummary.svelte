e3155bb03cd3b8ac432832683f0a0f63e685aec2
Nicole O'Brien <nicole.obrien@iota.org>
2021-12-19 22:52:33 +0000

    fix: remove tooltip on balance summary (#1808)

--- packages/shared/components/BalanceSummary.svelte
+++ packages/shared/components/BalanceSummary.svelte
@@ -1,6 +1,6 @@
 <script lang="typescript">
     import { Unit } from '@iota/unit-converter'
-    import { Text, Tooltip } from 'shared/components'
+    import { Text } from 'shared/components'
     import { formatUnitBestMatch, formatUnitPrecision } from 'shared/lib/units'
 
     export let color = 'blue' // TODO: profiles will have different colors
@@ -8,14 +8,7 @@
     export let balanceRaw = 0
     export let balanceFiat = 0
 
-    let tooltipAnchor
-
-    let showTooltip = false
     let showPreciseBalance = false
-
-    function toggleTooltip() {
-        showTooltip = !showTooltip
-    }
 
     function togglePreciseBalance() {
         showPreciseBalance = !showPreciseBalance
@@ -24,9 +17,6 @@
 
 <div class="flex items-start flex-col flex-wrap space-y-1.5">
     <balance-box
-        bind:this={tooltipAnchor}
-        on:mouseenter={toggleTooltip}
-        on:mouseleave={toggleTooltip}
         on:click={togglePreciseBalance}>
         <Text type="h2" overrideColor classes="text-white">
             {showPreciseBalance ? formatUnitPrecision(balanceRaw, Unit.Mi) : formatUnitBestMatch(balanceRaw, true, 3)}
@@ -34,12 +24,5 @@
     </balance-box>
     {#if balanceFiat}
         <Text type="p" overrideColor smaller classes="text-{color}-200 dark:text-blue-300">{balanceFiat}</Text>
-    {/if}
-    {#if showTooltip}
-        <Tooltip anchor={tooltipAnchor} refresh={showPreciseBalance} position="top">
-            <Text type="p">
-                {!showPreciseBalance ? formatUnitPrecision(balanceRaw, Unit.Mi) : formatUnitBestMatch(balanceRaw, true, 3)}
-            </Text>
-        </Tooltip>
     {/if}
 </div>
