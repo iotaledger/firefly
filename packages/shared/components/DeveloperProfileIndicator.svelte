d5f810d31bfd2b365390ba173dec1b792ad458de
Nicole O'Brien <nicole.obrien@iota.org>
2021-12-30 15:37:19 +0000

    fix: remove warning signs from dev profile indicator

--- packages/shared/components/DeveloperProfileIndicator.svelte
+++ packages/shared/components/DeveloperProfileIndicator.svelte
@@ -1,6 +1,6 @@
 <script lang="typescript">
     import { activeProfile } from 'shared/lib/profile';
-    import { Text , Icon } from 'shared/components'
+    import { Text } from 'shared/components'
     import type  { Locale } from 'shared/lib/typings/i18n';
 
     export let locale: Locale
@@ -11,10 +11,8 @@
 
 {#if $activeProfile?.isDeveloperProfile}
 <div class="flex flex roww-full justify-center content-center items-center bg-yellow-700 space-x-4 p-2">
-    <Icon icon="warning" classes="text-white" width={20} height={20}/>
     <Text type="p">
         {locale('indicators.developerProfileIndicator.warningText', { values: { networkName: $activeProfile.settings.networkConfig.network.name }})}
     </Text>
-    <Icon icon="warning" classes="text-white" width={20} height={20}/>
 </div>
 {/if}
