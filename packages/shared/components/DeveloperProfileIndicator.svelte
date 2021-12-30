0ae5d283238a97b3c29ad9bff0c3f306e91a9ab3
Nicole O'Brien <nicole.obrien@iota.org>
2021-12-30 16:16:49 +0000

    feat: add dismiss to dev profile indicator

--- packages/shared/components/DeveloperProfileIndicator.svelte
+++ packages/shared/components/DeveloperProfileIndicator.svelte
@@ -1,18 +1,24 @@
 <script lang="typescript">
     import { activeProfile } from 'shared/lib/profile';
-    import { Text } from 'shared/components'
+    import { Link, Text } from 'shared/components'
     import type  { Locale } from 'shared/lib/typings/i18n';
 
     export let locale: Locale
+
+    let showIndicator = true
+
+    const handleDismiss = () => {
+        showIndicator = false
+    }
 </script>
 
-<style type="text/scss">
-</style>
-
-{#if $activeProfile?.isDeveloperProfile}
-<div class="flex flex roww-full justify-center content-center items-center bg-yellow-700 space-x-4 p-2">
+{#if $activeProfile?.isDeveloperProfile && showIndicator}
+<div class="flex flex justify-center content-center items-center bg-yellow-700 space-x-4 p-2">
     <Text type="p">
         {locale('indicators.developerProfileIndicator.warningText', { values: { networkName: $activeProfile.settings.networkConfig.network.name }})}
     </Text>
+    <Link onClick={handleDismiss} classes="text-13 hover:underline cursor-pointer">
+        {locale('actions.dismiss')}
+    </Link>
 </div>
 {/if}
