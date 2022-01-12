10516eff8b1f9d7ecc1cda5a1380221c4e61403a
Rajiv Shah <rajivshah1@icloud.com>
2022-01-12 15:16:08 -0500

    Re-export Sentry.captureException
    This is necessary for us to manually capture exceptions that are usually captured by our own error handlers

--- packages/desktop/sentry.js
+++ packages/desktop/sentry.js
@@ -1,14 +1,16 @@
 // SENTRY_MAIN is replaced by Webpack DefinePlugin
 // eslint-disable-next-line no-undef
-const { init } = SENTRY_MAIN ? require('@sentry/electron/dist/main') : require('@sentry/electron/dist/renderer')
+const Sentry = SENTRY_MAIN ? require('@sentry/electron/dist/main') : require('@sentry/electron/dist/renderer')
 
 const SENTRY_DSN = process.env.SENTRY_DSN || ''
 const SENTRY_ORG_NAME = 'iota-foundation-h4'
-const SENTRY_APP_NAME = 'firefly'
+const SENTRY_APP_NAME = 'Firefly'
 
-init({
+Sentry.init({
     dsn: SENTRY_DSN,
     debug: true,
     appName: SENTRY_APP_NAME,
     environment: process.env.NODE_ENV || 'production',
 })
+
+export const captureException = Sentry.captureException
