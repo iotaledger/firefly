c1e4ad4907e71ca19d10ee7b0afc2d4931ef4145
Amadeo Marchioni <amadeo.marchioni@iota.org>
2022-01-19 19:11:50 -0300

    fix double import

--- packages/shared/routes/login/views/EnterPin.svelte
+++ packages/shared/routes/login/views/EnterPin.svelte
@@ -4,7 +4,6 @@
     import { ongoingSnapshot, openSnapshotPopup } from 'shared/lib/migration'
     import { mobile } from 'shared/lib/app'
     import { showAppNotification } from 'shared/lib/notifications'
-    import { Platform } from 'shared/lib/platform'
     import { activeProfile,clearActiveProfile } from 'shared/lib/profile'
     import type { Locale } from 'shared/lib/typings/i18n'
     import { validatePinFormat } from 'shared/lib/utils'
