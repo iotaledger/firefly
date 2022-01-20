3893f82d4264b3bc1f24a1cfa61938cfbae6306f
Begoña Alvarez <balvarez@boxfish.studio>
2022-01-20 11:55:26 +0100

    cleanup lock settings

--- packages/shared/routes/dashboard/settings/views/security/AppLock.svelte
+++ packages/shared/routes/dashboard/settings/views/security/AppLock.svelte
@@ -3,6 +3,10 @@
     import { localize } from 'shared/lib/i18n'
     import { activeProfile, updateProfile } from 'shared/lib/profile'
 
+    function updateLockTimeout(option) {
+        updateProfile('settings.lockScreenTimeout', option.value)
+    }
+
     function assignTimeoutOptionLabel(timeInMinutes) {
         if (timeInMinutes >= 60) {
             return localize('times.hour', { values: { time: timeInMinutes / 60 } })
@@ -11,17 +15,19 @@
         return localize('times.minute', { values: { time: timeInMinutes } })
     }
 
-    const lockScreenTimeoutOptions = [1, 5, 10, 30, 60].map((time) => ({
-        value: time,
-        label: assignTimeoutOptionLabel(time),
-    }))
+    function lockScreenTimeoutOptions() {
+        return [1, 5, 10, 30, 60].map((time) => ({
+            value: time,
+            label: assignTimeoutOptionLabel(time),
+        }))
+    }
+
+    const lockOptions = lockScreenTimeoutOptions()
 </script>
 
 <Text type="h4" classes="mb-3">{localize('views.settings.appLock.title')}</Text>
 <Text type="p" secondary classes="mb-5">{localize('views.settings.appLock.description')}</Text>
 <Dropdown
-    onSelect={(option) => {
-        updateProfile('settings.lockScreenTimeout', option.value)
-    }}
+    onSelect={updateLockTimeout}
     value={assignTimeoutOptionLabel($activeProfile?.settings.lockScreenTimeout)}
-    items={lockScreenTimeoutOptions} />
+    items={lockOptions} />
