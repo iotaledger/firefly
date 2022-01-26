4b78b308eea42156549805620d94ced7430c6fbe
Tuditi <daviddetroch@pm.me>
2022-01-26 09:56:00 +0100

    Added change profile name component + english locale

---
+++ packages/shared/routes/dashboard/settings/views/general/ChangeProfileName.svelte
@@ -0,0 +1,28 @@
+<script lang="typescript">
+    import { Button, Input, Text } from 'shared/components'
+    import { localize } from 'shared/lib/i18n'
+    import { activeProfile, updateProfile } from 'shared/lib/profile'
+
+    let newName: string
+
+    function changeProfileName(): void {
+        updateProfile('name', newName)
+    }
+</script>
+
+<form id="form-change-profile-name" on:submit={changeProfileName}>
+    <Text type="h4" classes="mb-3">
+        {localize('views.settings.changeProfileName.title')}
+    </Text>
+    <Text type="p" secondary classes="mb-5">
+        {localize('views.settings.changeProfileName.description')}
+    </Text>
+    <Input
+        placeholder={$activeProfile.name}
+        bind:value={newName}
+        classes="mb-5"
+    />
+    <Button medium form="form-change-profile-name" type="submit">
+        {localize('views.settings.changeProfileName.title')}
+    </Button>   
+</form>
