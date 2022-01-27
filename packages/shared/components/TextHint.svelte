f94d552f36eceb7aab0223778ce47f553c97f928
Matthew Maxwell <maxwellmattryan@gmail.com>
2022-01-27 16:10:51 -0600

    feat: add `TextHint` components for hints and advice

---
+++ packages/shared/components/TextHint.svelte
@@ -0,0 +1,16 @@
+<script lang="typescript">
+    import { Icon, Text } from 'shared/components'
+
+    export let classes: string = ''
+    export let icon: 'info' | 'exclamation' | 'none' = 'none'
+    export let hint: string = ''
+</script>
+
+{#if hint !== ''}
+    <div class="flex flex-row justify-center items-center {classes}">
+        {#if icon !== 'none'}
+            <Icon {icon} classes="mr-3 fill-current text-black dark:text-white" />
+        {/if}
+        <Text type="p" classes="text-black dark:text-white">{hint}</Text>
+    </div>
+{/if}
