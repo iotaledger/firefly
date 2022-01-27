1bad84866fc1626f7aa6cf5b4531f5c7360dc4cf
Tuditi <daviddetroch@pm.me>
2022-01-27 12:16:36 +0100

    Added validation and folder renaming

--- packages/shared/routes/dashboard/settings/views/general/ChangeProfileName.svelte
+++ packages/shared/routes/dashboard/settings/views/general/ChangeProfileName.svelte
@@ -1,16 +1,33 @@
 <script lang="typescript">
     import { Button, Input, Text } from 'shared/components'
     import { localize } from 'shared/lib/i18n'
-    import { activeProfile, updateProfile } from 'shared/lib/profile'
+    import { activeProfile, updateProfile, validateProfileName } from 'shared/lib/profile'
+    import { Electron } from 'shared/lib/electron';
+    import { getStoragePath } from 'shared/lib/wallet';
 
     let newName: string
+    let error = ''
 
-    function changeProfileName(): void {
-        updateProfile('name', newName)
+    async function onSubmitClick(): Promise<void> {
+        const trimmedProfileName = newName.trim();
+        try {
+            validateProfileName(trimmedProfileName)
+            await renameProfileFolder(trimmedProfileName)
+            updateProfile('name', newName)
+        } catch (err) {
+            return (error = err.message)
+        }
+    }
+
+    async function renameProfileFolder(newName: string): Promise<void> {
+        const userDataPath = await Electron.getUserDataPath();
+        const oldPath = getStoragePath(userDataPath, $activeProfile.name)
+        const newPath = getStoragePath(userDataPath, newName)
+        await Electron.renameProfileFolder(oldPath, newPath);
     }
 </script>
 
-<form id="form-change-profile-name" on:submit={changeProfileName}>
+<form id="form-change-profile-name" on:submit={onSubmitClick}>
     <Text type="h4" classes="mb-3">
         {localize('views.settings.changeProfileName.title')}
     </Text>
@@ -18,6 +35,7 @@
         {localize('views.settings.changeProfileName.description')}
     </Text>
     <Input
+        {error}
         placeholder={$activeProfile.name}
         bind:value={newName}
         classes="mb-5"
