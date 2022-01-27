933f3ce7ca49428c0a8e61c03cb0bec7e2106016
Rajiv Shah <rajivshah1@icloud.com>
2022-01-27 15:46:27 -0500

    feat: Pass machine ID to Sentry JS client

--- packages/desktop/sentry.js
+++ packages/desktop/sentry.js
@@ -2,6 +2,7 @@
 
 /* eslint-disable no-undef */
 const Sentry = SENTRY_MAIN_PROCESS ? require('@sentry/electron/dist/main') : require('@sentry/electron/dist/renderer')
+const { machineIdSync } = require('node-machine-id')
 
 const appName = 'Firefly'
 const debug = true
@@ -10,6 +11,15 @@ const dsn = SENTRY_DSN || ''
 /* eslint-disable no-undef */
 const environment = SENTRY_ENVIRONMENT || ''
 
+let machineId = ''
+
+try {
+    machineId = machineIdSync()
+} catch (error) {
+    console.error(error)
+}
+
 Sentry.init({ appName, debug, dsn, environment })
+Sentry.setUser({ id: machineId })
 
 export const captureException = Sentry.captureException
