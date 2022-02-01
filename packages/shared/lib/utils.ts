789f7a6239507b517d32273e6d1cfd6c34bd33c6
Jean Ribeiro <iamjeanribeiro@gmail.com>
2022-01-31 22:00:32 -0300

    feat: transaction search and filtering (#1857)
    * adds transaction counter
    
    * adds search icon
    
    * adds search icon
    
    * refactoring
    
    * update search icon
    
    * localize tx filter
    
    * add missing new localized string
    
    * cleanup imports
    
    * retouch css
    
    * adds optional chaining in transaction object
    
    * adds message id to search and improves typing a little bit
    
    * fixes from review
    
    * fixes focus error
    
    * adds adapted trinity transaction search
    
    * fixes transaction search by adding search result length condition
    
    * adds search by unit and returns search result even when empty
    
    * adds value search condition
    
    * fix: allow search on lowercase
    
    Co-authored-by: Begoña Alvarez <balvarez@boxfish.studio>
    Co-authored-by: Nicole O'Brien <nicole.obrien@iota.org>

--- packages/shared/lib/utils.ts
+++ packages/shared/lib/utils.ts
@@ -386,3 +386,63 @@ export const clamp = (n: number, min: number, max: number): number => {
 
     return Math.min(Math.max(n, min), max)
 }
+
+/**
+ * Converts iota value-unit string to int value
+ *
+ * @method unitToValue
+ * @param {string}
+ *
+ * @returns {number}
+ */
+export const unitToValue = (str: string): number => {
+    const value = parseInt(str, 10)
+    const unit = str.substring(value.toString().length).toLowerCase()
+
+    switch (unit) {
+        case 'ki':
+            return value * 1000
+        case 'mi':
+            return value * 1000000
+        case 'gi':
+            return value * 1000000000
+        case 'ti':
+            return value * 1000000000000
+        case 'pi':
+            return value * 1000000000000000
+        default:
+            return value
+    }
+}
+
+/**
+ * Check if value is in unit range
+ *
+ * @method isValueInUnitRange
+ * @param {number} value
+ * @param {string} unit
+ *
+ * @returns {boolean}
+ */
+export const isValueInUnitRange = (value: number, unit: string): boolean => {
+    const ki = 1000,
+        mi = 1000000,
+        gi = 1000000000,
+        ti = 1000000000000,
+        pi = 1000000000000000
+
+    switch (unit) {
+        case 'ki':
+            return value >= ki && value <= mi
+        case 'mi':
+            return value >= mi && value <= gi
+        case 'gi':
+            return value >= gi && value <= ti
+        case 'ti':
+            return value >= ti && value <= pi
+        case 'pi':
+            return value >= pi
+        default:
+            return false
+    }
+}
