000e7e133261bdc62a9f99743576452261fb6498
Jean Ribeiro <iamjeanribeiro@gmail.com>
2022-02-02 22:47:29 -0300

    adds submitting of event participation

--- packages/shared/routes/dashboard/governance/Governance.svelte
+++ packages/shared/routes/dashboard/governance/Governance.svelte
@@ -1,17 +1,46 @@
 <script lang="typescript">
+    import { WalletPill } from 'shared/components'
     import { GovernanceHeader, GovernanceDashboard, GovernanceEventDetails } from './views'
     import { GovernanceRoutes } from 'shared/lib/typings/routes'
     import { governanceRoute } from 'shared/lib/router'
     import { participationEvents } from 'shared/lib/participation/stores'
+    import { wallet } from 'shared/lib/wallet'
+    import { get } from 'svelte/store'
+    import { selectedAccountId } from 'shared/lib/wallet'
+    import type { Locale } from 'shared/lib/typings/i18n'
+
+    export let locale: Locale
+
+    const { accounts } = get(wallet)
+
+    selectedAccountId.set($accounts[0]?.id)
+
+    function handleAccountClick(accountId) {
+        selectedAccountId.set(accountId)
+    }
 
     $: event = $participationEvents[0]
 </script>
 
 <div class="staking-wrapper w-full h-full flex flex-col flex-nowrap px-10 py-8 flex-1 bg-gray-50 dark:bg-gray-900">
     {#if $governanceRoute === GovernanceRoutes.Init}
-        <GovernanceHeader />
+        <div class='flex justify-between'>
+            <GovernanceHeader />
+            <div>
+                {#each $accounts as acc}
+                    <WalletPill active={acc.id === $selectedAccountId} 
+                        name={acc.alias} color={acc.color} onClick={() => handleAccountClick(acc.id)} />
+                {/each}
+            </div>
+        </div>
         <GovernanceDashboard {event} />
     {:else if $governanceRoute === GovernanceRoutes.EventDetails}
-        <GovernanceEventDetails {event} />
+        <div class='ml-auto'>
+            {#each $accounts as acc}
+                <WalletPill active={acc.id === $selectedAccountId}
+                    name={acc.alias} color={acc.color} onClick={() => handleAccountClick(acc.id)} />
+            {/each}
+        </div>
+        <GovernanceEventDetails {event} {locale} />
     {/if}
 </div>
--- packages/shared/routes/dashboard/governance/views/GovernanceEventDetails.svelte
+++ packages/shared/routes/dashboard/governance/views/GovernanceEventDetails.svelte
@@ -1,15 +1,58 @@
 <script lang="typescript">
-    import { DashboardPane } from 'shared/components'
-    import { Text, Icon, Link, Button } from 'shared/components'
+    import { Text, Icon, Button, DashboardPane } from 'shared/components'
     import { localize } from 'shared/lib/i18n'
     import { GovernanceRoutes } from 'shared/lib/typings/routes'
     import { governanceRoute } from 'shared/lib/router'
-    import { ParticipationEventState } from 'shared/lib/participation/types'
-    import type { ParticipationEvent } from 'shared/lib/participation/types'
-
+    import { ParticipationEventState, ParticipationEvent } from 'shared/lib/participation/types'
+    import { participate } from 'shared/lib/participation/api';
+    import { api, selectedAccountId } from 'shared/lib/wallet'
+    import { isSoftwareProfile } from 'shared/lib/profile'
+    import type { Locale } from 'shared/lib/typings/i18n'
+    import { showAppNotification } from 'shared/lib/notifications'
+    import { openPopup } from 'shared/lib/popup'
+    
     export let event: ParticipationEvent;
+    export let locale: Locale
 
     const handleBackClick = () => governanceRoute.set(GovernanceRoutes.Init)
+
+    const castVote = async (answer: string) => {
+        try {
+            await participate($selectedAccountId, [{ eventId: event.eventId, answers: [answer] }])
+        } catch (err) {
+            showAppNotification({
+                type: 'error',
+                message: locale(err.error),
+            })
+        }
+    }
+
+    const handleCastClick = (answer: string) => {
+        if ($isSoftwareProfile) {
+            api.getStrongholdStatus({
+                onSuccess(strongholdStatusResponse) {
+                    if (strongholdStatusResponse.payload.snapshot.status === 'Locked') {
+                        openPopup({
+                            type: 'password',
+                            props: {
+                                onSuccess: () => castVote(answer),
+                            },
+                        })
+                    } else {
+                        void castVote(answer)
+                    }
+                },
+                onError(err) {
+                    showAppNotification({
+                        type: 'error',
+                        message: locale(err.error),
+                    })
+                },
+            })
+        } else {
+            void castVote(answer)
+        }
+    }
 </script>
 
 <div
@@ -22,19 +65,22 @@
 
 <div class="w-full h-full grid grid-cols-3 gap-x-4 min-h-0">
     <DashboardPane classes="w-full h-full p-6 col-span-2 flex flex-col">
-        <Text type="p" classes="mr-auto uppercase px-2 py-1 mb-2 text-blue-500 bg-blue-100 rounded-lg" smaller bold overrideColor>{event.status.status}</Text>
-        <Text type="h2" classes="mb-4">{event.information.name}</Text>
-        <Text type="p" classes="mb-2">{event.information.additionalInfo}</Text>
-        <Text type="p" classes="mb-2">{event.information.payload.questions[0].text}</Text>
-        <Text type="p" classes="mb-6">{event.information.payload.questions[0].additionalInfo}</Text>
-        {#each event.information.payload.questions[0].answers as answer, i}
+        <Text type="p" classes="mr-auto uppercase px-2 py-1 mb-2 text-blue-500 bg-blue-100 rounded-lg" smaller bold overrideColor>{event?.status?.status}</Text>
+        <Text type="h2" classes="mb-4">{event?.information?.name}</Text>
+        <Text type="p" classes="mb-2">{event?.information?.additionalInfo}</Text>
+        <Text type="p" classes="mb-2">{event?.information?.payload?.questions[0]?.text}</Text>
+        <Text type="p" classes="mb-6">{event?.information?.payload?.questions[0]?.additionalInfo}</Text>
+        {#each event?.information?.payload?.questions[0]?.answers as answer, i}
             <div class="py-4 px-6 bg-gray-50 border border-solid border-gray-100 rounded-lg flex mb-4">
                 <div>
-                    <Text type="p" classes="uppercase text-blue-500 mb-2" overrideColor smaller bold>{`Option ${i + 1}`}</Text>
-                    <Text type="h3" classes="mb-2">{answer.text}</Text>
-                    <Text type="p">{answer.additionalInfo}</Text>
+                    <Text type="p" classes="uppercase text-blue-500 mb-2" overrideColor smaller bold>{`Option ${answer?.value}`}</Text>
+                    <Text type="h3" classes="mb-2">{answer?.text}</Text>
+                    <Text type="p">{answer?.additionalInfo}</Text>
                 </div>
-                <Button medium classes="my-auto ml-44" disabled={event.status.status === ParticipationEventState.Upcoming}>Cast votes</Button>
+                <Button medium classes="my-auto ml-44" disabled={event.status.status === ParticipationEventState.Upcoming}
+                    onClick={() => handleCastClick(answer?.value)}>
+                    Cast votes
+                </Button>
             </div>
         {/each}
     </DashboardPane>
