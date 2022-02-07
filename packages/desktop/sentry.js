9efa38fa6092985d744542285d5440b12142b20f
Matthew Maxwell <maxwellmattryan@gmail.com>
2022-02-07 16:35:08 -0600

    fix: remove incorrect platform usage

--- packages/desktop/sentry.js
+++ packages/desktop/sentry.js
@@ -1,7 +1,6 @@
 /** NOTE: SENTRY_MAIN_PROCESS, SENTRY_DSN, SENTRY_ENVIRONMENT, and PRELOAD_SCRIPT are replaced by Webpack at compile-time. */
 
 /* eslint-disable no-undef */
-const { Platform } = require('shared/lib/platform')
 const Sentry = SENTRY_MAIN_PROCESS ? require('@sentry/electron/dist/main') : require('@sentry/electron/dist/renderer')
 
 const appName = 'Firefly'
@@ -22,7 +21,7 @@ if (SENTRY_MAIN_PROCESS || PRELOAD_SCRIPT) {
         console.error(error)
     }
 } else {
-    Platform.getMachineId().then((id) => {
+    Electron.getMachineId().then((id) => {
         machineId = id
     })
 }
