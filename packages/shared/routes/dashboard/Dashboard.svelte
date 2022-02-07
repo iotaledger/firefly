1e16f8b21ca18b63f43343183686e88196def6eb
Amadeo Marchioni <amadeo.marchioni@iota.org>
2022-02-06 21:33:21 -0300

    change style tag correct order

--- packages/shared/routes/dashboard/Dashboard.svelte
+++ packages/shared/routes/dashboard/Dashboard.svelte
@@ -261,12 +261,6 @@
     }
 </script>
 
-<style type="text/scss">
-    :global(:not(body.platform-win32)) .dashboard-wrapper {
-        margin-top: calc(env(safe-area-inset-top) / 2);
-    }
-</style>
-
 <Idle />
 <div class="dashboard-wrapper flex flex-row w-full h-full">
     <Sidebar {locale} />
@@ -276,3 +270,9 @@
         <DeveloperProfileIndicator {locale} classes="absolute top-0" />
     </div>
 </div>
+
+<style type="text/scss">
+    :global(:not(body.platform-win32)) .dashboard-wrapper {
+        margin-top: calc(env(safe-area-inset-top) / 2);
+    }
+</style>
