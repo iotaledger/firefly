cbd2755babcffd04a425ebf1270380f99b18c20a
Tuditi <daviddetroch@pm.me>
2022-02-09 17:42:10 +0100

    chore: removed unused import

--- packages/shared/lib/participation/participation.ts
+++ packages/shared/lib/participation/participation.ts
@@ -12,7 +12,7 @@ import {
     participationOverview,
     pendingParticipations,
 } from './stores'
-import { AccountParticipationAbility, Participation, ParticipationEventState, StakingAirdrop } from './types'
+import { AccountParticipationAbility, ParticipationEventState, StakingAirdrop } from './types'
 
 let participationPollInterval
 
