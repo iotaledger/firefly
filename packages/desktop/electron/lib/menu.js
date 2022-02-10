298621af21bfbf1a7d9b9dee5f7a1cdbe4b4baca
Jason Kraft <developer.kraft@gmail.com>
2022-02-09 19:17:53 -0800

    chore: fix windows shortcut typo

--- packages/desktop/electron/lib/menu.js
+++ packages/desktop/electron/lib/menu.js
@@ -140,7 +140,7 @@ const buildTemplate = () => {
     template[0].submenu = template[0].submenu.concat([
         {
             label: state.strings.quit,
-            accelerator: 'Command+Q',
+            accelerator: process.platform === 'win32' ? 'Alt+F4' : 'Cmd+Q',
             click: function () {
                 app.quit()
             },
