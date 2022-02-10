4aa819b23148567e5348e37530b39387c3c79848
Nicole O'Brien <nicole.obrien@iota.org>
2022-02-10 10:58:27 +0000

    refactor: qr scanner into Platform.BarcodeManager  (#2278)
    * refactor: put barcode scanner code in barcode manager behind the platform api
    
    * chore: improve comments

--- packages/shared/lib/platform.ts
+++ packages/shared/lib/platform.ts
@@ -87,6 +87,13 @@ export const Platform: IPlatform = {
             ? CapacitorApi.NotificationManager
             : undefined,
 
+    BarcodeManager:
+        PLATFORM == Platforms.DESKTOP
+            ? undefined
+            : PLATFORM == Platforms.MOBILE
+            ? CapacitorApi.BarcodeManager
+            : undefined,
+
     getStrongholdBackupDestination: (defaultPath) => {
         switch (PLATFORM) {
             case Platforms.DESKTOP:
