f95f89c1ec3416de830acf7c6197cdf8dde0ccfa
Tuditi <daviddetroch@pm.me>
2022-02-15 09:34:24 +0100

    chore: removed hardcoded staking action, unnecessary comments and duplicate type annotations

--- packages/shared/lib/participation/api.ts
+++ packages/shared/lib/participation/api.ts
@@ -2,10 +2,8 @@ import type { Event } from '../typings/events'
 import { localize } from '../i18n'
 import { showAppNotification } from '../notifications'
 import { api, saveNewMessage } from '../wallet'
-
-import { resetParticipation } from './participation'
 import { addNewPendingParticipation, participationEvents, participationOverview } from './stores'
-import {
+import type {
     ParticipationAction,
     ParticipateResponsePayload,
     Participation,
@@ -13,15 +11,8 @@ import {
     ParticipationOverviewResponse,
 } from './types'
 
-/**
- * Gets participation overview.
- *
- * @method getParticipationOverview
- *
- * @returns {Promise<void>}
- */
 export function getParticipationOverview(): Promise<void> {
-    return new Promise<void>((resolve, reject) => {
+    return new Promise((resolve, reject) => {
         api.getParticipationOverview({
             onSuccess(overview: Event<ParticipationOverviewResponse>) {
                 participationOverview.set(overview?.payload.accounts)
@@ -37,15 +28,8 @@ export function getParticipationOverview(): Promise<void> {
     })
 }
 
-/**
- * Gets participation event details.
- *
- * @method getParticipationEvents
- *
- * @returns {Promise<ParticipationEvent[]>}
- */
 export function getParticipationEvents(): Promise<ParticipationEvent[]> {
-    return new Promise<ParticipationEvent[]>((resolve, reject) => {
+    return new Promise((resolve, reject) => {
         api.getParticipationEvents({
             onSuccess(response: Event<ParticipationEvent[]>) {
                 participationEvents.set(response?.payload)
@@ -61,32 +45,25 @@ export function getParticipationEvents(): Promise<ParticipationEvent[]> {
     })
 }
 
-/**
- * Participate in events.
- *
- * @method participate
- *
- * @param {string} accountId
- * @param {Participation[]} participations
- *
- * @returns {Promise<void>}
- */
-export function participate(accountId: string, participations: Participation[]): Promise<string[]> {
+export function participate(
+    accountId: string,
+    participations: Participation[],
+    action: ParticipationAction
+): Promise<string[]> {
     if (!accountId) {
         showAppNotification({
             type: 'error',
             message: localize('error.participation.cannotUseAccount'),
         })
-
         return
     }
 
-    return new Promise<string[]>((resolve, reject) => {
+    return new Promise((resolve, reject) => {
         api.participate(accountId, participations, {
             onSuccess(response: Event<ParticipateResponsePayload>) {
                 response.payload.forEach((message) => saveNewMessage(accountId, message))
 
-                addNewPendingParticipation(response.payload, accountId, ParticipationAction.Stake)
+                addNewPendingParticipation(response.payload, accountId, action)
                 resolve(response.payload.map((message) => message.id))
             },
             onError(error) {
@@ -98,17 +75,11 @@ export function participate(accountId: string, participations: Participation[]):
     })
 }
 
-/**
- * Stop paticipating in events.
- *
- * @method stopParticipating
- *
- * @param {string} accountId
- * @param {string[]} eventIds
- *
- * @returns {Promise<void>}
- */
-export function stopParticipating(accountId: string, eventIds: string[]): Promise<string[]> {
+export function stopParticipating(
+    accountId: string,
+    eventIds: string[],
+    action: ParticipationAction
+): Promise<string[]> {
     if (!accountId) {
         showAppNotification({
             type: 'error',
@@ -118,12 +89,12 @@ export function stopParticipating(accountId: string, eventIds: string[]): Promis
         return
     }
 
-    return new Promise<string[]>((resolve, reject) => {
+    return new Promise((resolve, reject) => {
         api.stopParticipating(accountId, eventIds, {
             onSuccess(response: Event<ParticipateResponsePayload>) {
                 response.payload.forEach((message) => saveNewMessage(accountId, message))
 
-                addNewPendingParticipation(response.payload, accountId, ParticipationAction.Unstake)
+                addNewPendingParticipation(response.payload, accountId, action)
 
                 resolve(response.payload.map((message) => message.id))
             },
@@ -136,23 +107,17 @@ export function stopParticipating(accountId: string, eventIds: string[]): Promis
     })
 }
 
-/**
- * Participate with remaining funds in events.
- *
- * @method participateWithRemainingFunds
- *
- * @param {string} accountId
- * @param {Participation[]} participations
- *
- * @returns {Promise<void>}
- */
-export function participateWithRemainingFunds(accountId: string, participations: Participation[]): Promise<string[]> {
-    return new Promise<string[]>((resolve, reject) => {
+export function participateWithRemainingFunds(
+    accountId: string,
+    participations: Participation[],
+    action: ParticipationAction
+): Promise<string[]> {
+    return new Promise((resolve, reject) => {
         api.participateWithRemainingFunds(accountId, participations, {
             onSuccess(response: Event<ParticipateResponsePayload>) {
                 response.payload.forEach((message) => saveNewMessage(accountId, message))
 
-                addNewPendingParticipation(response.payload, accountId, ParticipationAction.Stake)
+                addNewPendingParticipation(response.payload, accountId, action)
 
                 resolve(response.payload.map((message) => message.id))
             },
