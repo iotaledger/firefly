193f5606965a1025ea1dd144762b0fc521d425f2
Amadeo Marchioni <amadeo.marchioni@iota.org>
2022-02-16 11:10:00 -0300

    adapt CrashReporting to mobile

--- packages/shared/routes/setup/CrashReporting.svelte
+++ packages/shared/routes/setup/CrashReporting.svelte
@@ -27,22 +27,18 @@
     }
 </script>
 
-{#if mobile}
-    <div>foo</div>
-{:else}
-    <OnboardingLayout onBackClick={handleBackClick} {busy}>
-        <div slot="leftpane__content">
-            <Text type="h2" classes="mb-5">{locale('views.crashReporting.title')}</Text>
-            <Text type="p" secondary classes="mb-8">{locale('views.crashReporting.body')}</Text>
-        </div>
-        <div slot="leftpane__action">
-            <Checkbox label={locale('views.crashReporting.checkbox')} bind:checked={sendCrashReports} classes="mb-8" />
-            <Button classes="w-full" onClick={handleContinueClick}>
-                {locale('actions.continue')}
-            </Button>
-        </div>
-        <div slot="rightpane" class="w-full h-full flex justify-center bg-pastel-orange dark:bg-gray-900">
-            <Animation animation="secure-desktop"/>
-        </div>
-    </OnboardingLayout>
-{/if}
+<OnboardingLayout onBackClick={handleBackClick} {busy}>
+    <div slot="leftpane__content">
+        <Text type="h2" classes="mb-5">{locale('views.crashReporting.title')}</Text>
+        <Text type="p" secondary classes="mb-8">{locale('views.crashReporting.body')}</Text>
+    </div>
+    <div slot="leftpane__action">
+        <Checkbox label={locale('views.crashReporting.checkbox')} bind:checked={sendCrashReports} classes="mb-8" />
+        <Button classes="w-full" onClick={handleContinueClick}>
+            {locale('actions.continue')}
+        </Button>
+    </div>
+    <div slot="rightpane" class="w-full h-full flex justify-center bg-pastel-orange dark:bg-gray-900">
+        <Animation animation="secure-desktop"/>
+    </div>
+</OnboardingLayout>
