e85b2c051914add8c8c3f9f25f7b40bf5a7a6cc4
Tuditi <daviddetroch@pm.me>
2022-02-17 15:29:22 +0100

    feat: hardcode staking event id

--- packages/shared/routes/dashboard/governance/Governance.svelte
+++ packages/shared/routes/dashboard/governance/Governance.svelte
@@ -5,18 +5,18 @@
     import { governanceRoute } from 'shared/lib/router'
     import { participationEvents } from 'shared/lib/participation/stores'
     import { wallet } from 'shared/lib/wallet'
-    import { get } from 'svelte/store'
     import { selectedAccountId } from 'shared/lib/wallet'
+    import { TREASURY_VOTE_EVENT_ID } from 'shared/lib/participation/constants';
 
-    const { accounts } = get(wallet)
+    const { accounts } = $wallet
 
     selectedAccountId.set($accounts[0]?.id)
 
-    function handleAccountClick(accountId) {
+    function handleAccountClick(accountId: string): void {
         selectedAccountId.set(accountId)
     }
 
-    $: event = $participationEvents[0]
+    $: event = $participationEvents.find((p) => p.eventId === TREASURY_VOTE_EVENT_ID)
 </script>
 
 <div class="staking-wrapper w-full h-full flex flex-col flex-nowrap px-10 py-8 flex-1 bg-gray-50 dark:bg-gray-900">
