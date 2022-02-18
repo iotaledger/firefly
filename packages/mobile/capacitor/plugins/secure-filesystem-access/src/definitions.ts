f524857d8ccc9f14e444044b6fa0de571410d5f5
Amadeo Marchioni <amadeo.marchioni@iota.org>
2022-02-18 13:36:13 -0300

    update plugin definitions

--- packages/mobile/capacitor/plugins/secure-filesystem-access/src/definitions.ts
+++ packages/mobile/capacitor/plugins/secure-filesystem-access/src/definitions.ts
@@ -1,30 +1,7 @@
-export type FilePickerOptions = {
-  limit?: number;
-  allowedExtensions?: Array<string>;
-  showHiddenFiles?: boolean;
-}
-
-export type FolderPickerOptions = {
-  limit?: number;
-  showHiddenFiles?: boolean;
-}
-
-export type CreateFileOptions = {
-  directory: string;
-  filename: string;
-  content: string;
-}
-
-export type WriteToFileOptions = {
-  filepath: string;
-  content: string;
+export type PickerOptions = {
+  type: 'file' | 'folder';
 }
 
 export interface SecureFilesystemAccessTypes {
-  launchFilePicker(options: FilePickerOptions): Promise<{ files: Array<string> }>;
-  launchFolderPicker(options: FolderPickerOptions): Promise<{ folders: Array<string> }>;
-  shareFile(options: { filepath: string }): Promise<void>;
-  getFileUrlForUri(options: { uri: string }): Promise<{ filepath: string }>;
-  createFile(options: CreateFileOptions): Promise<{ filepath: string }>;
-  writeToFile(options: WriteToFileOptions): Promise<{ filepath: string }>;
+  showPicker(options: PickerOptions): Promise<{ selected: Array<string> }>;
 }
