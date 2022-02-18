47c1b99ba6a7f49be58009f6f08fd7ea00df279c
Amadeo Marchioni <amadeo.marchioni@iota.org>
2022-02-18 13:35:55 -0300

    update swift plugin headers

--- packages/mobile/capacitor/plugins/secure-filesystem-access/ios/Plugin/Plugin.m
+++ packages/mobile/capacitor/plugins/secure-filesystem-access/ios/Plugin/Plugin.m
@@ -2,6 +2,5 @@
 #import <Capacitor/Capacitor.h>
 
 CAP_PLUGIN(SecureFilesystemAccess, "SecureFilesystemAccess",
-    CAP_PLUGIN_METHOD(launchFolderPicker, CAPPluginReturnPromise);
-    CAP_PLUGIN_METHOD(launchFilePicker, CAPPluginReturnPromise);
+    CAP_PLUGIN_METHOD(showPicker, CAPPluginReturnPromise);
 )
