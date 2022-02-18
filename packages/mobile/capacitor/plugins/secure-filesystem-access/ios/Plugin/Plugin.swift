61b912f95bf621d9caabd3c447c5953a0cfec5fb
Amadeo Marchioni <amadeo.marchioni@iota.org>
2022-02-18 13:35:25 -0300

    update swift code

--- packages/mobile/capacitor/plugins/secure-filesystem-access/ios/Plugin/Plugin.swift
+++ packages/mobile/capacitor/plugins/secure-filesystem-access/ios/Plugin/Plugin.swift
@@ -1,101 +1,65 @@
 import Foundation
 import Capacitor
-import MobileCoreServices
+import UniformTypeIdentifiers
 
+@available(iOS 13.0, *)
 @objc(SecureFilesystemAccess)
 public class SecureFilesystemAccess: CAPPlugin, UIDocumentPickerDelegate {
 
+    private var _call: CAPPluginCall? = nil
     private var pickerType: String = ""
-    public var _call: CAPPluginCall? = nil
     
-    @objc func launchFilePicker(_ call: CAPPluginCall){
-        let allowMultipleSelection = ((call.getInt("limit") ?? -1) == -1) ? true: false
-        
-        let this = self
-        
-        self._call = call
-        self.pickerType = "files"
-         
-        DispatchQueue.main.async { [weak self] in
-            let documentPicker = UIDocumentPickerViewController(
-                documentTypes: [String(kUTTypeItem)],
-                in: UIDocumentPickerMode.open
-            )
-            
-            documentPicker.allowsMultipleSelection = allowMultipleSelection
-            
-            if #available(iOS 13.0, *) {
-                documentPicker.shouldShowFileExtensions = true
-            }
-            
-            documentPicker.delegate = this
-            documentPicker.modalPresentationStyle = UIModalPresentationStyle.fullScreen
-            
-            self?.bridge?.viewController?.present(
-                documentPicker,
-                animated: true,
-                completion: nil
-            )
-        }
-    }
-    
-    @objc func launchFolderPicker(_ call: CAPPluginCall){
-        let this = self
+    @objc func showPicker(_ call: CAPPluginCall){
         call.keepAlive = true
         self._call = call
+        pickerType = call.getString("type") ?? "folder"
         
-        self.pickerType = "folders"
-        
-        DispatchQueue.main.async { [weak self] in
-            let documentPicker = UIDocumentPickerViewController(
-                documentTypes: [String(kUTTypeFolder)],
-                in:
-                    UIDocumentPickerMode.open
-            )
-            
-            documentPicker.allowsMultipleSelection = true
-            documentPicker.delegate = this
-            documentPicker.modalPresentationStyle = UIModalPresentationStyle.popover
-            
-            self!.bridge?.viewController?.present(
-                documentPicker,
-                animated: true,
-                completion: nil
-            )
+        DispatchQueue.main.async { [self] in
+            var documentPicker: UIDocumentPickerViewController? = nil
+            if #available(iOS 14.0, *) {
+                if (pickerType == "file") {
+                    let strongholdType: UTType = UTType("org.iota.firefly-stronghold")!
+                    documentPicker = UIDocumentPickerViewController(
+                        forOpeningContentTypes: [strongholdType], asCopy: true)
+                } else if (pickerType == "folder") {
+                    documentPicker = UIDocumentPickerViewController(
+                        documentTypes: ["public.folder"],
+                        in: UIDocumentPickerMode.open)
+                }
+            } else {
+                documentPicker = UIDocumentPickerViewController(
+                    documentTypes: pickerType == "file" 
+                        ? ["org.iota.firefly-stronghold"]
+                        : ["public.folder"],
+                    in: UIDocumentPickerMode.open) // for export pdf! in: UIDocumentPickerMode.exportToService
+            }
+            documentPicker!.allowsMultipleSelection = false
+            documentPicker!.shouldShowFileExtensions = true
+            documentPicker!.delegate = self
+            documentPicker!.modalPresentationStyle = UIModalPresentationStyle.formSheet
+            self.bridge?.viewController?.present(documentPicker!, animated: true, completion: nil)
         }
     }
     
-    /// This method responds when the user selects the file(s) or folder(s).
-    /// If the user selects something, the Capacitor plugin will return the array with the defined key to the view.
-    /// - Parameters:
-    ///   - controller: <#controller description#>
-    ///   - urls: urls contains an array of URL objects with the selected elements (files or folders).
-    public func documentPicker(
-        _ controller: UIDocumentPickerViewController,
-        didPickDocumentsAt url: [URL]
-    ){
-        //var items: [String] = []
-        //let url = urls[0].absoluteString
-        //for url in urls {
-          //  items.append(url.absoluteString)
-        print("starting...")
-        // Start accessing a security-scoped resource.
-        guard url[0].startAccessingSecurityScopedResource() else {
-            // Handle the failure here.
-            print("failed to access the security-scoped resource!")
+    // This method responds when the user selects the file or folder.
+    public func documentPicker(_ controller: UIDocumentPickerViewController, didPickDocumentsAt url: [URL]) {
+        self._call?.resolve(["selected": [url[0].relativePath]])
+        
+        if (self.pickerType == "file") {
+            // iOS 14 with `asCopy: true` doesn't need permission
             return
         }
-        let fileURL = URL(string: url[0].absoluteString)!
-        print(fileURL)
-        self._call?.resolve([
-            self.pickerType: [url[0].relativePath]
-        ])
         
-        DispatchQueue.main.asyncAfter(deadline: .now() + 34.0) {
-            // Make sure you release the security-scoped resource when you finish.
-            url[0].stopAccessingSecurityScopedResource()
-            print("stopped!C")
+        guard url[0].startAccessingSecurityScopedResource() else {
+            self._call?.reject("failed to access the security-scoped resource!")
+            return
         }
         
+        DispatchQueue.main.asyncAfter(deadline: .now() + 60.0) {
+            url[0].stopAccessingSecurityScopedResource()
+            self._call?.keepAlive = false
+            self._call?.resolve()
+            return
+        }
     }
 }
