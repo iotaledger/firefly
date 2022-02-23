a8ea8165f00e5e73d96110ce3f6cdd477910af4a
Begoña Alvarez <balvarez@boxfish.studio>
2022-02-23 17:43:45 +0100

    feat: add missing top nav border

--- packages/shared/routes/dashboard/TopNavigation.svelte
+++ packages/shared/routes/dashboard/TopNavigation.svelte
@@ -9,7 +9,9 @@
     const viewableAccounts = getContext<Readable<WalletAccount[]>>('viewableAccounts')
 </script>
 
-<div class="bg-gray-200 dark:bg-gray-800 flex flex-row justify-center py-2 w-full">
+<div
+    class="bg-gray-20 dark:bg-gray-800 border-solid border-b border-gray-300 dark:border-gray-700 flex flex-row justify-center py-2 w-full"
+>
     <!-- <button data-label="back-button" class="flex-1 mt-1" on:click={() => {}}>
         <div class="flex items-center space-x-3">
             <Icon icon="arrow-left" classes="text-blue-500" />
