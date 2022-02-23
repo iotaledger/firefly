04041d69daae1c100f991b110d48ff78518ed8fb
Rajiv Shah <rajivshah1@icloud.com>
2022-02-23 17:16:32 -0500

    feature: Add script to fix productName in package.json

---
+++ packages/desktop/scripts/fix-productName.js
@@ -0,0 +1,21 @@
+/**
+ * Electron reads the app name from the productName property of the package.json. For alpha and beta releases,
+ * we need to rename the app so that it stores user data in the correct folders.
+ */
+
+const { readFileSync, writeFileSync } = require('fs')
+const path = require('path')
+
+if (!process.env.CI) {
+    console.warn('Warning: These changes should not be checked into Git!')
+}
+
+const stage = process.env.RELEASE || 'prod'
+const appName = stage === 'prod' ? 'Firefly' : `Firefly ${stage[0].toUpperCase()}${stage.slice(1)}`
+const packageJsonPath = path.resolve(__dirname, '../package.json')
+
+const packageJson = JSON.parse(readFileSync(packageJsonPath, { encoding: 'utf-8' }))
+packageJson.productName = appName
+
+// Write out package.json with 4 spaces indentation and a trailing newline
+writeFileSync(packageJsonPath, JSON.stringify(packageJson, null, 4).concat('\n'))
