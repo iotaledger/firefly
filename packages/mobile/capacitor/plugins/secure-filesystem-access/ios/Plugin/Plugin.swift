0c8197781cdd28cc447bc0813376f6cbbf91c27c
Amadeo Marchioni <amadeo.marchioni@iota.org>
2022-02-27 18:53:34 -0300

    update iOS code

--- packages/mobile/capacitor/plugins/secure-filesystem-access/ios/Plugin/Plugin.swift
+++ packages/mobile/capacitor/plugins/secure-filesystem-access/ios/Plugin/Plugin.swift
@@ -1,65 +1,97 @@
 import Foundation
 import Capacitor
-import UniformTypeIdentifiers
+import MobileCoreServices
 
-@available(iOS 13.0, *)
 @objc(SecureFilesystemAccess)
 public class SecureFilesystemAccess: CAPPlugin, UIDocumentPickerDelegate {
 
-    private var _call: CAPPluginCall? = nil
-    private var pickerType: String = ""
-    
+    public var _call: CAPPluginCall? = nil
+    public var _url: URL? = nil
+
     @objc func showPicker(_ call: CAPPluginCall){
         call.keepAlive = true
         self._call = call
-        pickerType = call.getString("type") ?? "folder"
+        guard let pickerType = call.getString("type") else {
+            return call.reject("actorId is required")
+        }
         
-        DispatchQueue.main.async { [self] in
-            var documentPicker: UIDocumentPickerViewController? = nil
-            if #available(iOS 14.0, *) {
-                if (pickerType == "file") {
-                    let strongholdType: UTType = UTType("org.iota.firefly-stronghold")!
-                    documentPicker = UIDocumentPickerViewController(
-                        forOpeningContentTypes: [strongholdType], asCopy: true)
-                } else if (pickerType == "folder") {
-                    documentPicker = UIDocumentPickerViewController(
-                        documentTypes: ["public.folder"],
-                        in: UIDocumentPickerMode.open)
-                }
-            } else {
-                documentPicker = UIDocumentPickerViewController(
-                    documentTypes: pickerType == "file" 
-                        ? ["org.iota.firefly-stronghold"]
-                        : ["public.folder"],
-                    in: UIDocumentPickerMode.open) // for export pdf! in: UIDocumentPickerMode.exportToService
+        DispatchQueue.main.async {
+            let documentPicker = UIDocumentPickerViewController(
+                documentTypes: pickerType == "file" 
+                    ? ["org.iota.firefly-stronghold"] // [String(kUTTypeItem)] 
+                    : [String(kUTTypeFolder)],
+                in: UIDocumentPickerMode.open
+            )
+            documentPicker.allowsMultipleSelection = false
+            if #available(iOS 13.0, *) {
+                documentPicker.shouldShowFileExtensions = true
             }
-            documentPicker!.allowsMultipleSelection = false
-            documentPicker!.shouldShowFileExtensions = true
-            documentPicker!.delegate = self
-            documentPicker!.modalPresentationStyle = UIModalPresentationStyle.formSheet
-            self.bridge?.viewController?.present(documentPicker!, animated: true, completion: nil)
+            documentPicker.delegate = self
+            documentPicker.modalPresentationStyle = UIModalPresentationStyle.formSheet
+            self.bridge?.viewController?.present(documentPicker, animated: true, completion: nil)
         }
     }
     
-    // This method responds when the user selects the file or folder.
     public func documentPicker(_ controller: UIDocumentPickerViewController, didPickDocumentsAt url: [URL]) {
-        self._call?.resolve(["selected": [url[0].relativePath]])
-        
-        if (self.pickerType == "file") {
-            // iOS 14 with `asCopy: true` doesn't need permission
+        _url = url[0]
+        self._call?.resolve([
+            "selected": [url[0].relativePath]
+        ])
+    }
+
+    @objc func allowAccess(_ call: CAPPluginCall) {
+        guard _url!.startAccessingSecurityScopedResource() else {
+            call.reject("failed to access the security-scoped resource!")
             return
         }
-        
-        guard url[0].startAccessingSecurityScopedResource() else {
-            self._call?.reject("failed to access the security-scoped resource!")
-            return
+        call.resolve()
+    }
+
+    @objc func revokeAccess(_ call: CAPPluginCall) {
+        _url!.stopAccessingSecurityScopedResource()
+        _url = nil
+        _call?.keepAlive = false
+        call.resolve()
+    }
+
+    public func getAppPath(folder: String) -> URL {
+        let fm = FileManager.default
+        let documents = fm.urls(for: .documentDirectory, in: .userDomainMask).first!
+        let appPath = documents.appendingPathComponent("__storage__/" + folder, isDirectory: true)
+        return appPath
+    }
+    
+    @objc func removeProfileFolder(_ call: CAPPluginCall) {
+        guard let folder = call.getString("folder") else {
+            return call.reject("folder is required")
         }
         
-        DispatchQueue.main.asyncAfter(deadline: .now() + 60.0) {
-            url[0].stopAccessingSecurityScopedResource()
-            self._call?.keepAlive = false
-            self._call?.resolve()
-            return
+        let fileUrl = getAppPath(folder: folder)
+        if FileManager.default.fileExists(atPath: fileUrl.path) {
+            try? FileManager.default.removeItem(atPath: fileUrl.path)
         }
+        call.resolve()
+    }
+
+    @objc func renameProfileFolder(_ call: CAPPluginCall) {
+        guard let oldName = call.getString("oldName") else {
+            return call.reject("oldName is required")
+        }
+        guard let newName = call.getString("newName") else {
+            return call.reject("newName is required")
+        }
+        let srcUrl = getAppPath(folder: oldName)
+        let dstUrl = getAppPath(folder: newName)
+        try? FileManager.default.moveItem(at: srcUrl, to: dstUrl)
+        call.resolve()
+    }
+
+    @objc func listProfileFolders(_ call: CAPPluginCall) {
+        guard let folder = call.getString("folder") else {
+            return call.reject("folder is required")
+        }
+        let fileUrl = getAppPath(folder: folder)
+        let result = try? FileManager.default.contentsOfDirectory(at: fileUrl, includingPropertiesForKeys: nil, options: [])
+        call.resolve(["result": result!])
     }
 }
