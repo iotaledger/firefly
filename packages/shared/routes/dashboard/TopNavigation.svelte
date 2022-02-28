84a5926bd04c675a33aa9d848347aed7760f0bd1
Begoña Alvarez <balvarez@boxfish.studio>
2022-02-28 13:29:13 +0100

    fix: remove commented code

--- packages/shared/routes/dashboard/TopNavigation.svelte
+++ packages/shared/routes/dashboard/TopNavigation.svelte
@@ -12,11 +12,5 @@
 <div
     class="bg-gray-200 dark:bg-gray-800 border-solid border-b border-gray-300 dark:border-gray-700 flex flex-row justify-center py-2 w-full"
 >
-    <!-- <button data-label="back-button" class="flex-1 mt-1" on:click={() => {}}>
-        <div class="flex items-center space-x-3">
-            <Icon icon="arrow-left" classes="text-blue-500" />
-            <Text type="h5">{localize('actions.back')}</Text>
-        </div>
-    </button> -->
     <AccountSwitcher {onCreateAccount} accounts={$viewableAccounts} />
 </div>
