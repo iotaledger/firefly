fab4c7f76dad3e3676ce040f5521b2a52f5909ea
Amadeo Marchioni <amadeo.marchioni@iota.org>
2022-02-28 11:13:26 -0300

    remove @capacitor/filesystem plugin

--- packages/mobile/capacitor/capacitorApi.ts
+++ packages/mobile/capacitor/capacitorApi.ts
@@ -1,5 +1,4 @@
 import { Capacitor } from '@capacitor/core'
-import { Filesystem, Directory, Encoding } from '@capacitor/filesystem'
 import { CapacitorNativeFilePicker } from 'capacitor-native-filepicker'
 import { DeepLinkManager } from '../../mobile/capacitor/lib/deepLinkManager'
 import { NotificationManager } from '../../mobile/capacitor/lib/notificationManager'
