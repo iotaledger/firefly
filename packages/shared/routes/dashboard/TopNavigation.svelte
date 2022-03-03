d9d4b09a085117ec230196ac6a89c51ecee02e2e
Tuditi <daviddetroch@pm.me>
2022-03-03 13:08:59 +0100

    feat: add back button

--- packages/shared/routes/dashboard/TopNavigation.svelte
+++ packages/shared/routes/dashboard/TopNavigation.svelte
@@ -1,8 +1,9 @@
 <script lang="typescript">
-    import { AccountSwitcher } from 'shared/components'
-    import type { WalletAccount } from 'shared/lib/typings/wallet'
     import { getContext } from 'svelte'
+    import { localize } from 'shared/lib/i18n';
+    import { AccountSwitcher, Icon, Text } from 'shared/components'
     import type { Readable } from 'svelte/store'
+    import type { WalletAccount } from 'shared/lib/typings/wallet'
 
     export let onCreateAccount = (..._: any[]): void => {}
 
@@ -12,5 +13,11 @@
 <div
     class="bg-gray-200 dark:bg-gray-800 border-solid border-b border-gray-300 dark:border-gray-700 flex flex-row justify-center py-2 w-full"
 >
+    <button on:click={() => console.log('back')} class="absolute left-6">
+        <div class="flex items-center space-x-3">
+            <Icon icon="arrow-left" classes="text-gray-500" />
+            <Text bigger overrideColor classes="text-gray-500">{localize('actions.back')}</Text>
+        </div>
+    </button>
     <AccountSwitcher {onCreateAccount} accounts={$viewableAccounts} />
 </div>
