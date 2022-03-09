fd0bafa2758976d3fb43df9a51046cedec693a55
Tuditi <daviddetroch@pm.me>
2022-03-09 21:12:12 +0100

    chore: removed dead code + sorted imports

--- packages/shared/lib/core/router/helper.ts
+++ packages/shared/lib/core/router/helper.ts
@@ -1,42 +1,23 @@
-import { get, readable, writable } from 'svelte/store'
+import { get } from 'svelte/store'
 import { selectedAccountId, selectedMessage } from 'shared/lib/wallet'
-import { AppRouter } from 'shared/lib/core/router/appRouter'
 import { isDeepLinkRequestActive } from '@common/deep-links'
-import { DashboardRouter, dashboardRouter } from 'shared/lib/core/router/dashboardRouter'
-import { ledgerRouter, LedgerRouter } from 'shared/lib/core/router/ledgerRouter'
-import { walletRouter, WalletRouter } from 'shared/lib/core/router/walletRouter'
-import { accountRouter, AccountRouter } from 'shared/lib/core/router/accountRouter'
-import { appRouter } from 'shared/lib/core/router/appRouter'
-import { SettingsRouter, settingsRouter } from 'shared/lib/core/router/settingsRouter'
-import { DashboardRoutes } from '@core/router/enum/routes'
+import {
+    appRouter,
+    AppRouter,
+    accountRouter,
+    AccountRouter,
+    DashboardRoutes,
+    DashboardRouter,
+    dashboardRouter,
+    ledgerRouter,
+    LedgerRouter,
+    walletRouter,
+    WalletRouter,
+    SettingsRouter,
+    settingsRouter,
+} from '@core/router'
 import { closePopup } from 'shared/lib/popup'
-import { SetupType } from 'shared/lib/typings/setup'
 
-/**
- * Application path based on location hash
- */
-export const path = readable<string>(null, (set) => {
-    const updatePath = (): void => {
-        const pathName = window.location.hash.substr(1)
-        set(pathName)
-    }
-
-    window.addEventListener('hashchange', updatePath)
-    updatePath()
-
-    return (): void => {
-        window.removeEventListener('hashchange', updatePath)
-    }
-})
-
-/**
- * Onboarding/setup type
- */
-export const walletSetupType = writable<SetupType>(null)
-
-/**
- * Navigate to initial route
- */
 export const initRouters = (): void => {
     appRouter.set(new AppRouter())
     dashboardRouter.set(new DashboardRouter())
@@ -46,12 +27,10 @@ export const initRouters = (): void => {
     settingsRouter.set(new SettingsRouter())
 }
 
-// TODO: only handle route changes, not app variables
 export const routerNext = (event: CustomEvent): void => {
     get(appRouter).next(event)
 }
 
-// TODO: only handle route changes, not app variables
 export const routerPrevious = (): void => {
     get(appRouter).previous()
 }
