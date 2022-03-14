590e7183dbfb954e7a2576d2c415c81eef811ab5
Matthew Maxwell <44885822+maxwellmattryan@users.noreply.github.com>
2022-03-14 08:08:48 -0700

    fix: cross platform dependency leaks (#2472)
    * fix: initialize `Platform` differently to avoid direct mobile imports
    
    * Update packages/shared/lib/platform.ts
    
    Co-authored-by: Rajiv Shah <rajivshah1@icloud.com>
    
    * fix: set global property for wallet bindings on mobile
    
    * fix: Import capacitorApi at launch and fix TS errors
    
    * chore: Fix Webpack chunk filename
    
    Co-authored-by: Rajiv Shah <rajivshah1@icloud.com>

--- packages/mobile/capacitor/capacitorApi.ts
+++ packages/mobile/capacitor/capacitorApi.ts
@@ -10,9 +10,13 @@ import { AppSettings } from 'shared/lib/typings/app'
 import { VersionDetails } from 'shared/lib/typings/appUpdater'
 import { IPlatform } from 'shared/lib/typings/platform'
 
+import * as WalletBindings from './walletPluginApi'
+
+window['__WALLET__'] = WalletBindings
+
 let activeProfileId = null
 
-export const CapacitorApi: IPlatform = {
+const CapacitorApi: IPlatform = {
     updateAppSettings(settings: Partial<AppSettings>) {
         return new Promise((resolve) => resolve())
     },
@@ -255,3 +259,5 @@ export const CapacitorApi: IPlatform = {
     hookErrorLogger,
     ledger: undefined,
 }
+
+window['__CAPACITOR__'] = CapacitorApi
--- packages/mobile/webpack.config.js
+++ packages/mobile/webpack.config.js
@@ -31,7 +31,7 @@ const output = {
     publicPath: prod ? '../' : '/',
     path: path.join(__dirname, '/public'),
     filename: '[name].js',
-    chunkFilename: '[name].[id].js',
+    chunkFilename: 'build/[name].[id].js',
 }
 
 // ------------------------ Module rules ------------------------
