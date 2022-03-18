a5b9f7cee9d4c59f8701c3ffba5651ac09b9907e
Tuditi <daviddetroch@pm.me>
2022-03-18 12:19:13 +0100

    chore: use export from barrel file

--- packages/desktop/electron/lib/menu.js
+++ packages/desktop/electron/lib/menu.js
@@ -1,5 +1,5 @@
 import { app, ipcMain, Menu, shell } from 'electron'
-import { WalletRoutes, ExternalRoute } from '@core/router/enum/routes'
+import { WalletRoutes, ExternalRoute } from '@core/router'
 import { closeAboutWindow, getOrInitWindow, openAboutWindow } from '../main'
 import { menuState } from './menuState'
 
