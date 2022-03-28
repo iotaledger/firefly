7213de4f9a228de669a7fee838ecc6fa0dfaea9e
Matthew Maxwell <maxwellmattryan@gmail.com>
2022-03-28 17:11:45 -0500

    fix: change URL for reporting an issue

--- packages/shared/lib/typings/routes.ts
+++ packages/shared/lib/typings/routes.ts
@@ -129,5 +129,5 @@ export enum ExternalRoute {
     Documentation = 'https://wiki.iota.org/learn/wallets/firefly/general',
     Discord = 'https://discord.iota.org',
     FAQ = 'https://wiki.iota.org/learn/wallets/firefly/faq-and-troubleshooting',
-    IssueReport = 'https://github.com/iotaledger/firefly/issues',
+    IssueReport = 'https://github.com/iotaledger/firefly/issues/new/choose',
 }
