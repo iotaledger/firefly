c81be69dd85e9d993793bbaf8dc50749f0e86321
Amadeo Marchioni <amadeo.marchioni@iota.org>
2022-03-28 22:53:41 -0300

    chore: disable PrivacyScreen plugin to allow better testing
    Signed-off-by: Amadeo Marchioni <amadeo.marchioni@iota.org>

--- packages/mobile/capacitor-sample.config.ts
+++ packages/mobile/capacitor-sample.config.ts
@@ -7,7 +7,7 @@ const config: CapacitorConfig = {
     bundledWebRuntime: false,
     plugins: {
         PrivacyScreen: {
-            enable: true,
+            enable: false,
         },
         SplashScreen: {
             launchAutoHide: false,
