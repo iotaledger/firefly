d63734343ca91063b6c01a6180fe1ab2189dddcd
Tuditi <daviddetroch@pm.me>
2022-03-31 15:30:26 +0200

    feat: show notification if account switch not ready yet

--- packages/shared/components/AccountSwitcher.svelte
+++ packages/shared/components/AccountSwitcher.svelte
@@ -1,20 +1,42 @@
 <script lang="typescript">
-    import type { WalletAccount } from 'lib/typings/wallet'
+    import { localize } from '@core/i18n'
+    import { showAppNotification } from '@lib/notifications'
+    import { participationAction } from '@lib/participation/stores'
+    import { WalletAccount } from 'lib/typings/wallet'
     import { AccountSwitcherModal, Icon, Text } from 'shared/components'
     import { activeProfile, getColor } from 'shared/lib/profile'
-    import { selectedAccount } from 'shared/lib/wallet'
+    import { isSyncing, isTransferring, selectedAccount } from 'shared/lib/wallet'
 
     export let accounts: WalletAccount[] = []
     export let onCreateAccount = (..._: any[]): void => {}
 
     let showModal = false
 
+    function onClick(): void {
+        let message: string
+        if ($isSyncing) {
+            message = localize('popups.notification.syncing')
+        } else if ($isTransferring) {
+            message = localize('popups.notification.transferring')
+        } else if ($participationAction) {
+            message = localize('popups.notification.participating')
+        } else {
+            toggleModal()
+            return
+        }
+        showAppNotification({
+            type: 'warning',
+            message,
+        })
+        showModal = false
+    }
+
     function toggleModal(): void {
         showModal = !showModal
     }
 </script>
 
-<button on:click={toggleModal} class="flex flex-row justify-center items-center space-x-2">
+<button on:click={onClick} class="flex flex-row justify-center items-center space-x-2">
     <div class="circle" style="--account-color: {getColor($activeProfile, $selectedAccount?.id)};" />
     <Text type="h4">{$selectedAccount?.alias}</Text>
     <div class="transform transition-all {showModal ? 'rotate-180' : 'rotate-0'}">
