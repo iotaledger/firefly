5fcebf4600e4e4d3f43730edb79f82847bd70d61
Nicole O'Brien <nicole.obrien@iota.org>
2022-04-06 20:57:14 +0100

    chore: merge desktop release 1.4.0 into develop (#2733)
    * chore: incremenet version to 1.4.0-beta-1
    
    * fix: improve wording for theme settings hint (#2654)
    
    * fix: change URL for reporting an issue
    
    * fix: minimum rewards popup warning (#2684)
    
    * chore: manually sync new translations from Crowdin
    
    * fix: cancel any requests to select a Bluetooth device (#2693)
    
    * fix: cancel any requests to select a Bluetooth device
    
    * chore: Ignore vulnerability in Snyk
    
    * chore: update wallet.rs to fix missing addresses (#2695)
    
    Co-authored-by: Thoralf-M <46689931+Thoralf-M@users.noreply.github.com>
    
    * chore: increment version to 1.4.0-beta-2
    
    * chore: sync latest translations from Crowdin
    
    * chore: upload PDB files when building Windows (#2721)
    
    * chore: increase network timeout for Yarn on CI (#2722)
    
    * chore: add comment explaining network timeout threshold increase
    
    * fix: rename account issues for 1.4.0 (#2720)
    
    * fix: rename profile issues
    
    * Update return type for onSubmitClick
    
    * Update packages/desktop/App.svelte
    
    Co-authored-by: Rajiv Shah <rajivshah1@icloud.com>
    
    * Update packages/shared/lib/app.ts
    
    Co-authored-by: Rajiv Shah <rajivshah1@icloud.com>
    
    * fix: make hasRenamedProfileFoldersToId persistent
    
    Co-authored-by: Rajiv Shah <rajivshah1@icloud.com>
    
    * chore: increment version to 1.4.0-beta-3
    
    * chore: fix workflow
    
    * chore: increment desktop version to 1.4.0
    
    * fix: retain original error structure when sending error to Sentry (#2727)
    
    * chore: Use Sectigo timestamp server on Windows
    
    * chore: use Sectigo RFC 3161 timestamp server on Windows
    
    * chore: update wallet.rs to fix transaction history
    
    * chore: increment desktop version to 1.4.1-beta
    
    * fix: missing autosubscription in dashboard
    
    * chore: increment desktop version to 1.4.1
    
    Co-authored-by: Matthew Maxwell <maxwellmattryan@gmail.com>
    Co-authored-by: Matthew Maxwell <44885822+maxwellmattryan@users.noreply.github.com>
    Co-authored-by: Rajiv Shah <rajivshah1@icloud.com>
    Co-authored-by: Thoralf-M <46689931+Thoralf-M@users.noreply.github.com>

--- packages/desktop/App.svelte
+++ packages/desktop/App.svelte
@@ -2,7 +2,7 @@
     import { onDestroy, onMount } from 'svelte'
     import { Popup, Route, TitleBar, ToastContainer } from 'shared/components'
     import { stage, loggedIn } from 'shared/lib/app'
-    import { appSettings, initAppSettings } from 'shared/lib/appSettings'
+    import { appSettings, hasRenamedProfileFoldersToId, initAppSettings } from 'shared/lib/appSettings'
     import { getVersionDetails, pollVersion, versionDetails } from 'shared/lib/appUpdater'
     import { addError } from 'shared/lib/errors'
     import { goto } from 'shared/lib/helpers'
@@ -11,7 +11,7 @@
     import { showAppNotification } from 'shared/lib/notifications'
     import { Electron } from 'shared/lib/electron'
     import { openPopup, popupState } from 'shared/lib/popup'
-    import { cleanupEmptyProfiles, cleanupInProgressProfiles } from 'shared/lib/profile'
+    import { cleanupEmptyProfiles, cleanupInProgressProfiles, renameAllProfileFoldersToId } from 'shared/lib/profile'
     import { AppRoute, DashboardRoute, dashboardRouter, accountRouter, initRouters, openSettings } from '@core/router'
     import {
         Appearance,
@@ -72,6 +72,12 @@
             splash = false
             initRouters()
         }, 3000)
+
+        // This is added to migrate all profile folder names from using the profile name to the id
+        if (!get(hasRenamedProfileFoldersToId)) {
+            await renameAllProfileFoldersToId()
+            hasRenamedProfileFoldersToId.set(true)
+        }
 
         initAppSettings.set($appSettings)
 
