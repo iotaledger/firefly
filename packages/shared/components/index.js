837009e6bd55e38cf09808d509c43a2be0ec557a
Jean Ribeiro <iamjeanribeiro@gmail.com>
2022-04-07 06:44:41 -0300

    feat: adds profile icon notification for backup and version update (#2770)
    * feat: adds profile icon notification for backup and version update
    
    * fix: dismiss the badge after user has viewed the profile menu in that session
    
    * feat: componentizes pinging badge

--- packages/shared/components/index.js
+++ packages/shared/components/index.js
@@ -62,6 +62,7 @@ export { default as ColorPicker } from './ColorPicker.svelte'
 export { default as AccountSwitcher } from './AccountSwitcher.svelte'
 export { default as SidebarTab } from './SidebarTab.svelte'
 export { default as StakingAssetTile } from './StakingAssetTile.svelte'
+export { default as PingingBadge } from './PingingBadge.svelte'
 
 export * from './modals'
 // Charts
