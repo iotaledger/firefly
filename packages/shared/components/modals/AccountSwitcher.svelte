8b2a40b0f69138f1afcffaa3184ec0262edfe293
Begoña Alvarez <balvarez@boxfish.studio>
2022-04-07 13:35:06 +0200

    feat: add border to account color circle in wallet switcher

--- packages/shared/components/modals/AccountSwitcher.svelte
+++ packages/shared/components/modals/AccountSwitcher.svelte
@@ -54,10 +54,28 @@
     }
     button {
         .circle {
+            @apply relative;
             @apply rounded-full;
             @apply w-3;
             @apply h-3;
             background-color: var(--account-color);
+            &:after {
+                @apply absolute;
+                @apply rounded-full;
+                @apply w-3;
+                @apply h-3;
+                @apply border-2;
+                @apply border-solid;
+                @apply border-gray-700;
+                @apply bg-transparent;
+                @apply opacity-10;
+                @apply top-1/2;
+                @apply left-1/2;
+                @apply transform;
+                @apply -translate-x-1/2;
+                @apply -translate-y-1/2;
+                content: '';
+            }
         }
     }
 </style>
