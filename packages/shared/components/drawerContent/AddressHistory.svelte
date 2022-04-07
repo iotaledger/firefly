c8b01e5df0d298b501a6a2c0b685a81d2131bdd2
Amadeo Marchioni <amadeo.marchioni@iota.org>
2022-04-07 11:38:24 -0300

    fix: comp AddressHistory css polish
    Signed-off-by: Amadeo Marchioni <amadeo.marchioni@iota.org>

--- packages/shared/components/drawerContent/AddressHistory.svelte
+++ packages/shared/components/drawerContent/AddressHistory.svelte
@@ -23,11 +23,11 @@
     }
 </script>
 
-<div class="flex flex-col px-6 py-10">
+<div class="flex flex-col px-6 {$mobile ? 'safe-area pt-7 pb-3' : 'py-10'}">
     <div class="mb-5">
         <Text type="h4">{localize('popups.addressHistory.title', { values: { name: account.alias } })}</Text>
     </div>
-    <div class="history scrollable-y flex flex-row flex-wrap space-y-7">
+    <div class="history flex flex-row flex-wrap space-y-7 {$mobile ? 'overflow-y-auto' : 'scrollable-y'}">
         {#each addresses as _addr}
             <div class="flex flex-row flex-wrap space-y-1">
                 <Text type="p">
@@ -62,5 +62,8 @@
         @screen md {
             max-height: 30vh;
         }
+    }
+    .safe-area {
+        margin-bottom: calc(env(safe-area-inset-top) / 2);
     }
 </style>
