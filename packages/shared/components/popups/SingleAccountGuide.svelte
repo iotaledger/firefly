bad62d5e90172615a084a04987e46fe8802b4582
Nicole O'Brien <nicole.obrien@iota.org>
2022-04-09 23:04:46 +0100

    update: text on single account guide add: hint to single account guide

--- packages/shared/components/popups/SingleAccountGuide.svelte
+++ packages/shared/components/popups/SingleAccountGuide.svelte
@@ -1,5 +1,5 @@
 <script lang="typescript">
-    import { Button, Text, Illustration } from 'shared/components'
+    import { Button, Text, Illustration, TextHint } from 'shared/components'
     import { closePopup } from '@lib/popup'
     import { localize } from '@core/i18n'
     import { updateProfile } from '@lib/profile'
@@ -15,5 +15,12 @@
     <Illustration illustration="single-account-guide-desktop" classes="w-full rounded-xl" />
     <Text type="h3" classes="pt-5">{localize('popups.singleAccountGuide.title')}</Text>
     <Text type="p" classes="pt-3 pb-5">{localize('popups.singleAccountGuide.body')}</Text>
+    <TextHint
+        classes="p-4 rounded-2xl bg-blue-50 dark:bg-gray-800 mb-4"
+        icon="info"
+        iconClasses="fill-current text-blue-500 dark:text-blue-500"
+        hint={localize('popups.singleAccountGuide.hint')}
+        hintClasses="text-gray-500 dark:text-gray-500"
+    />
     <Button {onClick}>{localize('actions.okIUnderstand')}</Button>
 </div>
