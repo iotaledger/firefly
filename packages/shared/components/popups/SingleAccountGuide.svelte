913276fdd768036ae30c54192d2aefccea111050
Nicole O'Brien <nicole.obrien@iota.org>
2022-04-11 22:18:56 +0100

    fix: padding on single account guide

--- packages/shared/components/popups/SingleAccountGuide.svelte
+++ packages/shared/components/popups/SingleAccountGuide.svelte
@@ -12,9 +12,9 @@
 
 <div class="flex flex-col">
     <Illustration illustration="white-arrow" classes="absolute block w-12 h-12 top-14 left-0 right-0 mx-auto" />
-    <Illustration illustration="single-account-guide-desktop" classes="w-full rounded-xl" />
-    <Text type="h3" classes="pt-5">{localize('popups.singleAccountGuide.title')}</Text>
-    <Text type="p" classes="pt-3 pb-5">{localize('popups.singleAccountGuide.body')}</Text>
+    <Illustration illustration="single-account-guide-desktop" classes="w-full rounded-xl mb-4" />
+    <Text type="h3" classes="mb-2">{localize('popups.singleAccountGuide.title')}</Text>
+    <Text type="p" classes="mb-4">{localize('popups.singleAccountGuide.body')}</Text>
     <TextHint
         classes="p-4 rounded-2xl bg-blue-50 dark:bg-gray-800 mb-4"
         icon="info"
