47cafda8ed11881086cec416815b928d43c18986
Begoña Álvarez de la Cruz <balvarez@boxfish.studio>
2022-04-13 20:25:26 +0200

    Feat: UI polish voting welcome view  (#2886)
    * feat: remove voting heading
    
    * feat: unify css paddings
    
    * feat: polish css voting power
    
    * feat: polish css voting info
    
    * feat: polish css voting dashboard treasury event
    
    * feat: allow iotatreasury how to endpoint

--- packages/shared/routes/dashboard/governance/views/index.js
+++ packages/shared/routes/dashboard/governance/views/index.js
@@ -1,4 +1,3 @@
-export { default as GovernanceHeader } from './GovernanceHeader.svelte'
 export { default as GovernanceVotingPower } from './GovernanceVotingPower.svelte'
 export { default as GovernanceInfo } from './GovernanceInfo.svelte'
 export { default as GovernanceEvents } from './GovernanceEvents.svelte'
