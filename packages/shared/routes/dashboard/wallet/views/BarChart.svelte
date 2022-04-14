dd9db377ca4507da0269a5e9d1bee9efc05355d8
Begoña Alvarez <balvarez@boxfish.studio>
2022-04-14 18:45:35 +0200

    feat: make dashboard charts agnostics to wallet color

--- packages/shared/routes/dashboard/wallet/views/BarChart.svelte
+++ packages/shared/routes/dashboard/wallet/views/BarChart.svelte
@@ -2,7 +2,6 @@
     import { Chart, Text } from 'shared/components'
     import { getAccountActivityData } from 'shared/lib/chart'
     import { localize } from '@core/i18n'
-    import { activeProfile, getColor } from 'shared/lib/profile'
     import { selectedAccount } from 'shared/lib/wallet'
 
     let chartData = {
@@ -13,7 +12,6 @@
     $: incoming = chartData.incoming
     $: outgoing = chartData.outgoing
     $: labels = chartData.labels
-    $: color = getColor($activeProfile, $selectedAccount?.id)
 
     $: {
         if (localize || $selectedAccount) {
@@ -24,5 +22,5 @@
 
 <div data-label="bar-chart" class="flex flex-col justify-between w-full h-full p-6 pb-4">
     <Text type="h5" classes="mb-4">{localize('charts.accountActivity')}</Text>
-    <Chart type="bar" {labels} datasets={[incoming, outgoing]} {color} formatYAxis={(value) => value.toFixed(2)} />
+    <Chart type="bar" {labels} datasets={[incoming, outgoing]} formatYAxis={(value) => value.toFixed(2)} />
 </div>
