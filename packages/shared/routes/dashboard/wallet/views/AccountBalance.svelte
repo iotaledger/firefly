e3a0acada12fa4cdb9247e391700d8c05419fd9e
Begoña Alvarez <balvarez@boxfish.studio>
2022-04-14 17:24:48 +0200

    feat: revert wallet balance pane to be wallet color agnostic

--- packages/shared/routes/dashboard/wallet/views/AccountBalance.svelte
+++ packages/shared/routes/dashboard/wallet/views/AccountBalance.svelte
@@ -2,9 +2,7 @@
     import { Unit } from '@iota/unit-converter'
     import { Text } from 'shared/components'
     import { mobile } from 'shared/lib/app'
-    import { isBright } from 'shared/lib/helpers'
     import { localize } from '@core/i18n'
-    import { activeProfile, getColor } from 'shared/lib/profile'
     import { accountRoute, accountRouter } from '@core/router'
     import { AccountRoute } from '@core/router/enums'
     import { formatUnitBestMatch, formatUnitPrecision } from 'shared/lib/units'
@@ -13,9 +11,6 @@
     export let classes = ''
 
     export let onMenuClick = (): void => {}
-
-    $: color = getColor($activeProfile, $selectedAccount?.id) as string
-    $: textColor = isBright(color) ? 'gray-800' : 'white'
 
     let showPreciseBalance = false
 
@@ -32,23 +27,26 @@
 </script>
 
 <div
-    style="--account-color: {color};"
-    class="relative account-color p-6 {$mobile ? 'pb-0 bg-transparent' : 'pb-12'} {classes}"
+    class="bg-gradient-to-t from-gray-100 via-white to-white dark:from-gray-900 dark:via-gray-800 dark:to-gray-800 relative p-6 {$mobile
+        ? 'pb-0 bg-transparent'
+        : 'pb-12'} {classes}"
 >
     <!-- Balance -->
     <div data-label="total-balance" class="flex flex-col flex-wrap space-y-1.5">
         {#if !$mobile}
-            <p class="text-11 leading-120 text-{textColor} uppercase tracking-widest">{localize('general.balance')}</p>
+            <p class="text-11 leading-120 text-gray-800 dark:text-white uppercase tracking-widest">
+                {localize('general.balance')}
+            </p>
         {/if}
         <div class="flex flex-col flex-wrap items-start space-y-1.5 mr-12">
             <div on:click={togglePreciseBalance}>
-                <Text type="h2" overrideColor classes="text-{textColor}">
+                <Text type="h2">
                     {showPreciseBalance
                         ? formatUnitPrecision($selectedAccount?.rawIotaBalance, Unit.Mi)
                         : formatUnitBestMatch($selectedAccount?.rawIotaBalance, true, 3)}
                 </Text>
             </div>
-            <Text type="p" overrideColor smaller classes="text-{textColor} dark:text-{textColor}">
+            <Text type="p" smaller>
                 {$selectedAccount?.balanceEquiv}
             </Text>
         </div>
@@ -57,13 +55,13 @@
         <!-- Action Send / Receive -->
         <div class="flex flex-row justify-between space-x-4 mt-7">
             <button
-                class="action p-3 w-full text-center rounded-lg font-semibold text-14 bg-{textColor}"
+                class="action p-3 w-full text-center rounded-lg font-semibold text-14 bg-blue-500 text-white"
                 on:click={handleReceiveClick}
             >
                 {localize('actions.receive')}
             </button>
             <button
-                class="action p-3 w-full text-center rounded-lg font-semibold text-14 bg-{textColor}"
+                class="action p-3 w-full text-center rounded-lg font-semibold text-14 bg-blue-500 text-white"
                 on:click={handleSendClick}
             >
                 {localize('actions.send')}
@@ -72,7 +70,7 @@
     {/if}
     <button
         on:click={() => onMenuClick()}
-        class="px-2 py-3 flex flex-row space-x-1 bg-opacity-10 bg-black rounded-lg text-{textColor} absolute top-6 right-6"
+        class="px-2 py-3 flex flex-row space-x-1 bg-gray-50 dark:bg-gray-900 text-gray-900 dark:text-white dark:bg-opacity-50 rounded-lg absolute top-6 right-6"
     >
         {#each Array(3) as _}
             <svg width="4" height="4" viewBox="0 0 4 4">
@@ -81,12 +79,3 @@
         {/each}
     </button>
 </div>
-
-<style type="text/scss">
-    .account-color {
-        background-color: var(--account-color);
-        button.action {
-            color: var(--account-color);
-        }
-    }
-</style>
