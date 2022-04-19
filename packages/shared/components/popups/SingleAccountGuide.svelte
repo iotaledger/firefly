b217b195a288d2643d78b1bcbb90a4093e920d6d
Nicole O'Brien <nicole.obrien@iota.org>
2022-04-19 13:22:48 +0100

    enhancement: add video to single account guide

--- packages/shared/components/popups/SingleAccountGuide.svelte
+++ packages/shared/components/popups/SingleAccountGuide.svelte
@@ -3,16 +3,34 @@
     import { closePopup } from '@lib/popup'
     import { localize } from '@core/i18n'
     import { updateProfile } from '@lib/profile'
+    import { onMount } from 'svelte'
+
+    let video = null
 
     function onClick() {
         closePopup()
         updateProfile('hasFinishedSingleAccountGuide', true)
     }
+
+    onMount(() => {
+        video.play()
+    })
 </script>
 
 <div class="flex flex-col">
     <Illustration illustration="white-arrow" classes="absolute block w-12 h-12 top-14 left-0 right-0 mx-auto" />
-    <Illustration illustration="single-account-guide-desktop" classes="w-full rounded-xl mb-4" />
+    <video
+        class="w-full rounded-xl mb-4 h-auto"
+        width="100%"
+        height="auto"
+        controls={false}
+        muted
+        playsinline
+        loop
+        bind:this={video}
+    >
+        <source src="assets/videos/single-account-guide.mp4" type="video/mp4" />
+    </video>
     <Text type="h3" classes="mb-2">{localize('popups.singleAccountGuide.title')}</Text>
     <Text type="p" classes="mb-4">{localize('popups.singleAccountGuide.body')}</Text>
     <TextHint
