0ad8588ce1b9d79a3df1d1e8665c8f07be233458
Charlie Varley <charlievarley@macbook-pro-2.lan>
2022-04-21 12:33:22 +0100

    fix: duplicate constant imports

--- packages/shared/lib/participation/participation.ts
+++ packages/shared/lib/participation/participation.ts
@@ -1,19 +1,18 @@
+import { get } from 'svelte/store'
 import { WalletAccount } from '../typings/wallet'
 import { DUST_THRESHOLD, hasValidPendingTransactions } from '../wallet'
 import { canAccountReachMinimumAirdrop } from './account'
 import { getParticipationOverview } from './api'
-import { ASSEMBLY_EVENT_ID, PARTICIPATION_POLL_DURATION } from './constants'
+import { ASSEMBLY_EVENT_ID, PARTICIPATION_POLL_DURATION, SHIMMER_EVENT_ID } from './constants'
 import {
     isPerformingParticipation,
     participationAction,
     participationEvents,
+    participationHistory,
     participationOverview,
     pendingParticipations,
-    participationHistory,
 } from './stores'
 import { AccountParticipationAbility, ParticipationAction, ParticipationEventState, StakingAirdrop } from './types'
-import { get } from 'svelte/store'
-import { ASSEMBLY_EVENT_ID, SHIMMER_EVENT_ID } from './constants'
 
 let participationPollInterval
 
