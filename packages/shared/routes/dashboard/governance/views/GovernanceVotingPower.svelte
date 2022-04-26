7364b6b282cc92af7f9865578e673209d0562c4b
Charlie Varley <charlievarley@macbook-pro-2.lan>
2022-04-26 22:29:05 +0100

    enhancement: move voting power popup to tooltip

--- packages/shared/routes/dashboard/governance/views/GovernanceVotingPower.svelte
+++ packages/shared/routes/dashboard/governance/views/GovernanceVotingPower.svelte
@@ -1,16 +1,32 @@
 <script lang="typescript">
-    import { Text, Icon } from 'shared/components'
     import { localize } from '@core/i18n'
-    import { openPopup } from 'shared/lib/popup'
+    import GovernanceInfoTooltip from 'shared/components/GovernanceInfoTooltip.svelte'
+    import { Icon, Text } from 'shared/components'
     import { WalletAccount } from 'shared/lib/typings/wallet'
 
     export let account: WalletAccount
+
+    const tooltip = { anchor: null as HTMLElement, show: false }
+
+    function toggleTooltip(type: string, show: boolean): void {
+        tooltip.show = show
+    }
 </script>
 
-<div class="flex items-center mb-3 cursor-pointer" on:click={() => openPopup({ type: 'governanceVotingPowerInfo' })}>
+<div class="flex items-center mb-3 cursor-pointer">
     <Text type="p" smaller overrideColor classes="mr-1 text-gray-700 dark:text-gray-500">
         {localize('views.governance.votingPower.title')}
     </Text>
-    <Icon width="12" height="12" icon="info-filled" classes="text-gray-700 dark:text-gray-500" />
+    <button
+        on:mouseenter={() => toggleTooltip('votingWeight', true)}
+        on:mouseleave={() => toggleTooltip('votingWeight', false)}
+        bind:this={tooltip.anchor}
+    >
+        <Icon width="12" height="12" icon="info-filled" classes="text-gray-700 dark:text-gray-500" />
+    </button>
 </div>
 <Text type="h1">{account?.balance}</Text>
+
+{#if tooltip.show}
+    <GovernanceInfoTooltip type="votingWeight" anchor={tooltip.anchor} position="bottom" />
+{/if}
