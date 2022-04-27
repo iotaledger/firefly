2a01707bdf9f99a0df31df0c88d2480942e21a7f
Amadeo Marchioni <amadeo.marchioni@iota.org>
2022-04-27 20:59:44 -0300

    feat: implement webview settings plugin
    Signed-off-by: Amadeo Marchioni <amadeo.marchioni@iota.org>

---
+++ packages/mobile/android/app/src/main/java/org/iota/firefly/mobile/alpha/WebViewSettingsPlugin.java
@@ -0,0 +1,25 @@
+package org.iota.firefly.mobile.alpha;
+
+import android.util.Log;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+
+import com.getcapacitor.Plugin;
+import com.getcapacitor.annotation.CapacitorPlugin;
+
+@CapacitorPlugin(name = "WebViewSettings")
+public class WebViewSettingsPlugin extends Plugin {
+    @Override
+    public void load() {
+        WebView webView = getBridge().getWebView();
+        WebSettings settings = webView.getSettings();
+
+        settings.setGeolocationEnabled(false);
+        settings.setAllowFileAccess(false);
+        settings.setAllowContentAccess(false);
+        settings.setDatabaseEnabled(false);
+        settings.setAppCacheEnabled(false);
+        settings.setJavaScriptCanOpenWindowsAutomatically(false);
+        settings.setMixedContentMode(WebSettings.MIXED_CONTENT_NEVER_ALLOW);
+    }
+}
