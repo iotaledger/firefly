0125eb3e6ef2934d9d02a1a82e1320d4090e7f14
Nicole O'Brien <nicole.obrien@iota.org>
2022-04-27 02:08:35 +0100

    refactor: remove old ActivityDetail code

--- packages/shared/components/index.js
+++ packages/shared/components/index.js
@@ -1,5 +1,4 @@
 export { default as AccountTile } from './AccountTile.svelte'
-export { default as ActivityDetail } from './ActivityDetail.svelte'
 export { default as ActivityRow } from './ActivityRow.svelte'
 export { default as Address } from './inputs/Address.svelte'
 export { default as Amount } from './inputs/Amount.svelte'
