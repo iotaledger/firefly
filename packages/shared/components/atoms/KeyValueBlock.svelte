8d90e558cc9f18498bdf2ea9344eb7de675c97fc
Nicole O'Brien <nicole.obrien@iota.org>
2022-04-28 01:52:48 +0100

    feat: basic transaction details popup (#3049)
    * refactor: text component (#3042)
    
    * feat: add all font weights for inter and fira mono fonts
    
    * increment svelte version to make use of new element
    
    * update: add font familys to tailwind theme
    fix: add more tags to tailwind safelist
    
    * refactor: update text component to be more customizable
    
    Co-authored-by: Begoña Alvarez <balvarez@boxfish.studio>
    
    * chore: increment eslint-plugin-svelte3 to 3.4.1
    chore: prettier-plugin-svelte to 2.7.0
    
    * feat: basic activity details popup
    
    * refactor: remove old ActivityDetail code
    
    Co-authored-by: Begoña Alvarez <balvarez@boxfish.studio>

---
+++ packages/shared/components/atoms/KeyValueBlock.svelte
@@ -0,0 +1,33 @@
+<script lang="typescript">
+    import { FontWeightText } from 'shared/components/Text.svelte'
+    import { Text } from 'shared/components'
+
+    export let keyText: string = ''
+    export let valueText: string = ''
+    export let textColor: string = 'gray-600'
+    export let darkTextColor: string = 'gray-600'
+    export let backgroundColor: string = 'gray-50'
+    export let darkBackgrounColor: string = 'gray-50'
+    export let classes: string = ''
+</script>
+
+<block
+    class={`w-full flex justify-between px-4 py-3.5 rounded-lg ${backgroundColor ? 'bg-' + backgroundColor : ''}  ${
+        darkBackgrounColor ? 'dark:bg-' + darkBackgrounColor : ''
+    } ${classes}`}
+>
+    {#if keyText}
+        <Text fontSize="sm" color={textColor} {darkTextColor}>
+            {keyText}
+        </Text>
+    {:else}
+        <slot name="key" />
+    {/if}
+    {#if valueText}
+        <Text fontSize="sm" color={textColor} {darkTextColor}>
+            {valueText}
+        </Text>
+    {:else}
+        <slot name="value" />
+    {/if}
+</block>
