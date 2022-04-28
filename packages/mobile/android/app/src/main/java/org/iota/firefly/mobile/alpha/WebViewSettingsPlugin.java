45b52ad336107fa9e218446f84f6728002ee9b9a
Amadeo Marchioni <amadeo.marchioni@iota.org>
2022-04-28 12:49:08 -0300

    chore: implement delete main app cache folder
    Signed-off-by: Amadeo Marchioni <amadeo.marchioni@iota.org>

--- packages/mobile/android/app/src/main/java/org/iota/firefly/mobile/alpha/WebViewSettingsPlugin.java
+++ packages/mobile/android/app/src/main/java/org/iota/firefly/mobile/alpha/WebViewSettingsPlugin.java
@@ -31,12 +31,40 @@ public class WebViewSettingsPlugin extends Plugin {
      * Clean the RAM cache used by the webView including cache files on:
      * app/cache/WebView/* folder. Called when the app is no longer visible
      * to the user, as onDestroy is not called every time the app exit.
+     * TODO Clean app cache folder too? test the whole app with this change!
      */
     @Override
     public void handleOnStop() {
         super.handleOnStop();
         WebView webView = getBridge().getWebView();
         webView.clearCache(true);
+//        deleteCache(getContext());
+        Log.e("CLEANCACHE", "done!");
     }
 
+    public static void deleteCache(Context context) {
+        try {
+            File dir = context.getCacheDir();
+            deleteDir(dir);
+        } catch (Exception ignored) {}
+    }
+
+    public static boolean deleteDir(File dir) {
+        if (dir != null && dir.isDirectory()) {
+            String[] children = dir.list();
+            if (children != null) {
+                for (String child : children) {
+                    boolean success = deleteDir(new File(dir, child));
+                    if (!success) {
+                        return false;
+                    }
+                }
+            }
+            return dir.delete();
+        } else if (dir!= null && dir.isFile()) {
+            return dir.delete();
+        } else {
+            return false;
+        }
+    }
 }
