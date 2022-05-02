78e3e63358cf0788dff969a105ef873ed5e6417c
Tuditi <daviddetroch@pm.me>
2022-05-02 14:59:06 +0200

    chore: added ProfileManager interface

--- packages/shared/lib/typings/profile.ts
+++ packages/shared/lib/typings/profile.ts
@@ -2,6 +2,7 @@ import { AvailableExchangeRates } from './currency'
 import { ChartSelectors } from './chart'
 import { NetworkConfig } from './network'
 import { AccountStakingRewards } from '@lib/participation/types'
+import { Account, AccountId, CreateAccountPayload, EventType } from '@iota/wallet'
 
 export interface MigratedTransaction {
     address: string
@@ -90,4 +91,17 @@ export enum ImportType {
 export interface ProfileAccount {
     id: string
     color: string
+}
+
+export interface ProfileManager {
+    getAccount(accountId: AccountId): Promise<Account>
+    getAccounts(): Promise<Account[]>
+    createAccount(account: CreateAccountPayload): Promise<Account>
+    setStrongholdPassword(password: string): Promise<string>
+    generateMnemonic(): Promise<string>
+    storeMnemonic(mnemonic: string): Promise<string>
+    verifyMnemonic(mnemonic: string): Promise<string>
+    backup(destination: string, password: string): Promise<string>
+    importAccounts(backupPath: string, password: string): Promise<string>
+    listen(eventTypes: EventType[], callback: (error: Error, result: string) => void): void
 }
