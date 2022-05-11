4e857d9c4701f4e4a6a4067162e4ac96057f54fa
Nicole O'Brien <nicole.obrien@iota.org>
2022-05-11 22:41:21 +0100

    fix: type issue

--- packages/shared/lib/core/profile/stores/new-profile.store.ts
+++ packages/shared/lib/core/profile/stores/new-profile.store.ts
@@ -1,4 +1,4 @@
 import { writable } from 'svelte/store'
 import { IPersistedProfile } from '../interfaces'
 
-export const newProfile = writable<Partial<IPersistedProfile>>(null)
+export const newProfile = writable<IPersistedProfile>(null)
