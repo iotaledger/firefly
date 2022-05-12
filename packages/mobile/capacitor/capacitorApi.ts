4859de26453fd2c6d0a4864871111c0caf5fbf93
Rajiv Shah <rajivshah1@icloud.com>
2022-05-12 13:48:03 -0400

    chore: fix prettier warnings in mobile (#3185)

--- packages/mobile/capacitor/capacitorApi.ts
+++ packages/mobile/capacitor/capacitorApi.ts
@@ -58,7 +58,7 @@ export const CapacitorApi: IPlatform = {
         const type = defaultPath === null ? 'file' : 'folder'
         const { selected } = await SecureFilesystemAccess.showPicker({
             type,
-            defaultPath
+            defaultPath,
         })
         return `${selected}`
     },
@@ -288,7 +288,7 @@ export const CapacitorApi: IPlatform = {
         const os: string = Capacitor.getPlatform()
         const { selected } = await SecureFilesystemAccess.showPicker({
             type: 'folder',
-            defaultPath: ''
+            defaultPath: '',
         })
         if (os === 'ios') {
             void (await SecureFilesystemAccess.allowAccess())
