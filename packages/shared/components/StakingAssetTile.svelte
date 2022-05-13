758af8f496f1d4356d279bbb19eb7a7f3cdfff32
Matthew Maxwell <44885822+maxwellmattryan@users.noreply.github.com>
2022-05-12 19:58:51 -0500

    fix: add reactivity for cached staking rewards (#3032)
    * fix: add reactive declaration for cached `profile.stakingRewards`
    
    * chore: cleanup import statement
    
    Co-authored-by: Charlie Varley <charlie.varley@iota.org>

--- packages/shared/components/StakingAssetTile.svelte
+++ packages/shared/components/StakingAssetTile.svelte
@@ -27,6 +27,7 @@
     import { getBestTimeDuration } from 'shared/lib/time'
     import { formatUnitBestMatch } from 'shared/lib/units'
     import { capitalize } from 'shared/lib/utils'
+    import { activeProfile } from 'shared/lib/profile'
     import { selectedAccountStore } from 'shared/lib/wallet'
 
     export let asset: Asset
@@ -49,6 +50,7 @@
 
     const FIAT_PLACEHOLDER = '---'
 
+    $: $activeProfile.stakingRewards
     $: isDarkModeEnabled = $appSettings.darkMode
     $: isActivelyStaking = getAccount($stakedAccounts) && isStakingPossible(stakingEventState)
     $: isPartiallyStakedAndCanStake = $isPartiallyStaked && isStakingPossible(stakingEventState)
