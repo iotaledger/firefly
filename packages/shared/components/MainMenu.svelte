a33167026b1d81d19682de87f87e2257725723ab
Amadeo Marchioni <amadeo.marchioni@iota.org>
2022-05-16 00:28:50 -0300

    fix: format
    Signed-off-by: Amadeo Marchioni <amadeo.marchioni@iota.org>

--- packages/shared/components/MainMenu.svelte
+++ packages/shared/components/MainMenu.svelte
@@ -5,11 +5,7 @@
     import { activeProfile, getColor } from 'shared/lib/profile'
     import { selectedAccount } from 'shared/lib/wallet'
     import { Settings } from 'shared/routes'
-    import {
-        settingsRoute,
-        settingsRouter,
-        SettingsRoute,
-    } from '@core/router'
+    import { settingsRoute, settingsRouter, SettingsRoute } from '@core/router'
     import { Locale } from '@core/i18n'
 
     export let locale: Locale
@@ -67,7 +63,10 @@
                     <span class="text-20 text-center text-white uppercase font-semibold">{profileInitial}</span>
                 </div>
                 <Text type="h4" classes="col-start-2 row-start-2">{$activeProfile?.name}</Text>
-                <button class="col-start-2 row-start-3 flex items-center w-max px-1 pr-2 rounded-xl" on:click={() => logout()}>
+                <button
+                    class="col-start-2 row-start-3 flex items-center w-max px-1 pr-2 rounded-xl"
+                    on:click={() => logout()}
+                >
                     <Icon width="16" height="16" classes="mr-1 text-gray-500 -ml-2" icon="logout" />
                     <Text type="p" secondary classes="text-center">Logout</Text>
                 </button>
