554d0eafe1aba9e001d5d0774dca24f590b4d1a6
Amadeo Marchioni <amadeo.marchioni@iota.org>
2022-05-16 00:24:29 -0300

    fix: mainmenu comp old code
    Signed-off-by: Amadeo Marchioni <amadeo.marchioni@iota.org>

--- packages/shared/components/MainMenu.svelte
+++ packages/shared/components/MainMenu.svelte
@@ -4,10 +4,13 @@
     import { isBright, getInitials } from 'shared/lib/helpers'
     import { activeProfile, getColor } from 'shared/lib/profile'
     import { selectedAccount } from 'shared/lib/wallet'
-    import { settingsRoute, settingsChildRoute } from 'shared/lib/router'
-    import { SettingsRoutes } from 'shared/lib/typings/routes'
     import { Settings } from 'shared/routes'
-    import { Locale } from 'shared/lib/typings/i18n'
+    import {
+        settingsRoute,
+        settingsRouter,
+        SettingsRoute,
+    } from '@core/router'
+    import { Locale } from '@core/i18n'
 
     export let locale: Locale
 
@@ -15,60 +18,58 @@
     $: textColor = isBright(color) ? 'gray-800' : 'white'
 
     let drawer: Drawer
-    const profileColor = getColor($activeProfile, $activeProfile.id)
 
     $: profileInitial = getInitials($activeProfile?.name, 1)
 
     function handleBackClick() {
-        if ($settingsRoute === SettingsRoutes.Init) {
+        if ($settingsRoute === SettingsRoute.Init) {
             drawer?.close()
         } else {
-            settingsRoute.set(SettingsRoutes.Init)
+            $settingsRouter.goTo(SettingsRoute.Init)
         }
     }
 
     function handleClick() {
-        settingsRoute.set(SettingsRoutes.Init)
-        settingsChildRoute.set(null)
+        $settingsRouter.goTo(SettingsRoute.Init)
         drawer.open()
     }
 </script>
 
 <button
-    class="absolute menu-button r-30 z-10 w-11 h-11 flex items-center justify-center rounded-full leading-100"
-    style="background-color: {profileColor};"
+    class="menu-button fixed top-5 left-6 z-10 w-11 h-11 flex items-center justify-center rounded-full leading-100"
+    style="background-color: {color};"
     on:click={handleClick}
 >
-    <Text type="h4" classes="uppercase text-{textColor}">{profileInitial || 'A'}</Text>
+    <Text type="h4" overrideColor classes="z-10 uppercase text-{textColor}">{profileInitial || 'A'}</Text>
     <div class="w-11 h-11 flex rounded-full bg-white leading-100 opacity-20 absolute" />
 </button>
 <Drawer bind:this={drawer} fromRight dimLength={0} fullScreen classes="flex">
     <div class="flex flex-col flex-1">
         <header
-            class="header w-full px-8 py-3 mb-6 flex items-centers justify-center bg-white dark:bg-gray-800"
+            class="w-full mt-3 py-3 px-9 mb-5 flex items-centers justify-center bg-white dark:bg-gray-800"
             on:click={handleBackClick}
         >
-            <Icon icon="arrow-left" classes="absolute left-6 text-gray-500 text-blue-500" />
+            <Icon icon="arrow-left" classes="absolute mb-5 left-8 text-gray-500 text-blue-500" />
             <Text type="h4" classes="text-center">
                 {locale(
-                    $settingsRoute === SettingsRoutes.Init
+                    $settingsRoute === SettingsRoute.Init
                         ? 'general.yourWallets'
                         : `views.settings.${$settingsRoute}.title`
                 )}
             </Text>
         </header>
-        {#if $settingsRoute === SettingsRoutes.Init}
-            <div class="grid profile-block space-x-5 px-6 w-full">
+        {#if $settingsRoute === SettingsRoute.Init}
+            <div class="grid profile-block space-x-5 px-6 w-full mb-6">
                 <div
                     class="row-span-4 w-16 h-16 flex items-center justify-center rounded-full leading-100"
-                    style="background-color: {profileColor};"
+                    style="background-color: {color};"
                 >
                     <span class="text-20 text-center text-white uppercase font-semibold">{profileInitial}</span>
                 </div>
                 <Text type="h4" classes="col-start-2 row-start-2">{$activeProfile?.name}</Text>
-                <button class="col-start-2 row-start-3 flex items-center" on:click={() => logout()}>
-                    <Icon width="16" height="16" classes="mr-1 text-gray-500" icon="refresh" />
-                    <Text type="p" secondary classes="text-center">Switch Accounts</Text>
+                <button class="col-start-2 row-start-3 flex items-center w-max px-1 pr-2 rounded-xl" on:click={() => logout()}>
+                    <Icon width="16" height="16" classes="mr-1 text-gray-500 -ml-2" icon="logout" />
+                    <Text type="p" secondary classes="text-center">Logout</Text>
                 </button>
             </div>
         {/if}
@@ -77,13 +78,11 @@
 </Drawer>
 
 <style>
-    .header {
-        padding-top: env(safe-area-inset-top);
+    header {
+        border-top: solid transparent calc(env(safe-area-inset-top) / 1.5);
     }
     .menu-button {
-        position: absolute;
-        top: env(safe-area-inset-top);
-        left: 25px;
+        padding-top: env(safe-area-inset-top);
     }
     .profile-block {
         grid-template-columns: auto 1fr;
