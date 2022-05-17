fb0d4f6ed3c39e6cac71a68a9220d67fc303fd6e
Nicole O'Brien <nicole.obrien@iota.org>
2022-05-17 23:48:56 +0100

    refactor: remove old ManageAccount component

--- packages/shared/components/panes/wallet/AccountSummaryAndAssetsPane.svelte
+++ packages/shared/components/panes/wallet/AccountSummaryAndAssetsPane.svelte
@@ -1,16 +1,8 @@
 <script lang="typescript">
-    import { AccountRoute, accountRoute } from '@core/router'
-    import { ManageAccount } from 'shared/routes/dashboard/wallet/views'
-    import { selectedAccount } from '@core/account'
-    import { Pane, AccountSummary, AssetsPane } from 'shared/components'
+    import { AccountSummary, AssetsPane, Pane } from 'shared/components'
 </script>
 
 <Pane classes=" h-full flex flex-auto flex-col flex-shrink-0">
     <AccountSummary />
-    {#if $accountRoute === AccountRoute.Init}
-        <AssetsPane />
-    {:else if $accountRoute === AccountRoute.Manage}
-        <!-- // TODO: turn this into a modal -->
-        <ManageAccount alias={$selectedAccount.getAlias()} account={$selectedAccount} />
-    {/if}
+    <AssetsPane />
 </Pane>
