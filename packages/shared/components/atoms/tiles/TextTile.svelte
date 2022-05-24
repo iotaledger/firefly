60a57653d37107e164902ba15a4784b24c49470b
Jason Kraft <developer.kraft@gmail.com>
2022-05-23 19:28:18 -0700

    Separate StorageDepositBreakdown components into a list of TextTile components

---
+++ packages/shared/components/atoms/tiles/TextTile.svelte
@@ -0,0 +1,63 @@
+<script lang="typescript">
+    import { FontWeightText } from 'shared/components/Text.svelte'
+    import { Text } from 'shared/components'
+
+    export let leftPrimaryText = ''
+    export let leftPrimaryColor = 'gray-800'
+    export let leftPrimaryFontSize = '15'
+    export let leftPrimaryFontWeight: FontWeightText.normal
+    export let leftPrimaryLineHeight = '20'
+
+    export let leftSecondaryText = ''
+    export let leftSecondaryColor = 'gray-600'
+    export let leftSecondaryFontSize = '13'
+    export let leftSecondaryFontWeight = FontWeightText.medium
+    export let leftSecondaryLineHeight = '16'
+
+    export let rightPrimaryText = ''
+    export let rightPrimaryColor = 'gray-800'
+    export let rightPrimaryFontSize = '15'
+    export let rightPrimaryFontWeight = FontWeightText.normal
+    export let rightPrimaryLineHeight = '20'
+
+    export let rightSecondaryText = ''
+    export let rightSecondaryColor = 'gray-600'
+    export let rightSecondaryFontSize = '13'
+    export let rightSecondaryFontWeight = FontWeightText.medium
+    export let rightSecondaryLineHeight = '16'
+</script>
+
+<div class="flex justify-between">
+    <div class={leftSecondaryText ? 'flex flex-col space-y-0.5' : ''}>
+        <Text
+            color={leftPrimaryColor}
+            fontSize={leftPrimaryFontSize}
+            fontWeight={leftPrimaryFontWeight}
+            lineHeight={leftPrimaryLineHeight}>{leftPrimaryText}</Text
+        >
+        {#if leftSecondaryText}
+            <Text
+                color={leftSecondaryColor}
+                fontSize={leftSecondaryFontSize}
+                fontWeight={leftSecondaryFontWeight}
+                lineHeight={leftSecondaryLineHeight}>{leftSecondaryText}</Text
+            >
+        {/if}
+    </div>
+    <div class={rightSecondaryText ? 'flex flex-col items-end space-y-0.5' : ''}>
+        <Text
+            color={rightPrimaryColor}
+            fontSize={rightPrimaryFontSize}
+            fontWeight={rightPrimaryFontWeight}
+            lineHeight={rightPrimaryLineHeight}>{rightPrimaryText}</Text
+        >
+        {#if rightSecondaryText}
+            <Text
+                color={rightSecondaryColor}
+                fontSize={rightSecondaryFontSize}
+                fontWeight={rightSecondaryFontWeight}
+                lineHeight={rightSecondaryLineHeight}>{rightSecondaryText}</Text
+            >
+        {/if}
+    </div>
+</div>
--- packages/shared/components/popups/StorageDepositBreakdown.svelte
+++ packages/shared/components/popups/StorageDepositBreakdown.svelte
@@ -1,55 +1,37 @@
 <script lang="typescript">
     import { FontWeightText } from 'shared/components/Text.svelte'
-    import { HR, Text } from 'shared/components'
+    import { HR, TextTile, Text } from 'shared/components'
 </script>
 
 <div class="flex flex-col space-y-6">
     <Text type="h3" fontWeight={FontWeightText.semibold} lineHeight="24">Storage deposit breakdown</Text>
     <HR overrideColor classes="border-gray-100" />
-    <div class="flex justify-between">
-        <div class="flex flex-col space-y-0.5">
-            <Text fontSize="15" lineHeight="20">Locked in pending transactions</Text>
-            <Text color="gray-600" fontWeight={FontWeightText.medium} fontSize="13" lineHeight="16">
-                For sending native assets and small IOTA amounts
-            </Text>
-        </div>
-        <div class="flex flex-col items-end space-y-0.5">
-            <Text fontSize="15" lineHeight="20">1.83 Mi</Text>
-            <Text color="gray-600" fontWeight={FontWeightText.medium} fontSize="13" lineHeight="16">$ 2.45</Text>
-        </div>
-    </div>
+    <TextTile
+        leftPrimaryText="Locked in pending transactions"
+        leftSecondaryText="For sending native assets and small IOTA amounts"
+        rightPrimaryText="1.83 Mi"
+        rightSecondaryText="$ 2.45"
+    />
     <HR overrideColor classes="border-gray-100" />
-    <div class="flex justify-between">
-        <div class="flex flex-col space-y-0.5">
-            <Text fontSize="15" lineHeight="20">Reserved for Native Tokens</Text>
-            <Text color="gray-600" fontWeight={FontWeightText.medium} fontSize="13" lineHeight="16">
-                For storing native tokens
-            </Text>
-        </div>
-        <div class="flex flex-col items-end space-y-0.5">
-            <Text fontSize="15" lineHeight="20">1.83 Mi</Text>
-            <Text color="gray-600" fontWeight={FontWeightText.medium} fontSize="13" lineHeight="16">$ 2.45</Text>
-        </div>
-    </div>
+    <TextTile
+        leftPrimaryText="Reserved for Native Tokens"
+        leftSecondaryText="For storing native tokens"
+        rightPrimaryText="1.83 Mi"
+        rightSecondaryText="$ 2.45"
+    />
     <HR overrideColor classes="border-gray-100" />
-    <div class="flex justify-between">
-        <div class="flex flex-col space-y-0.5">
-            <Text fontSize="15" lineHeight="20">Reserved for NFTs</Text>
-            <Text color="gray-600" fontWeight={FontWeightText.medium} fontSize="13" lineHeight="16">
-                For storing NFTs
-            </Text>
-        </div>
-        <div class="flex flex-col items-end space-y-0.5">
-            <Text fontSize="15" lineHeight="20">1.83 Mi</Text>
-            <Text color="gray-600" fontWeight={FontWeightText.medium} fontSize="13" lineHeight="16">$ 2.45</Text>
-        </div>
-    </div>
+    <TextTile
+        leftPrimaryText="Reserved for NFTs"
+        leftSecondaryText="For storing NFTs"
+        rightPrimaryText="1.83 Mi"
+        rightSecondaryText="$ 2.45"
+    />
     <HR overrideColor classes="border-gray-100" />
-    <div class="flex justify-between">
-        <Text color="gray-800" fontWeight={FontWeightText.semibold} fontSize="15">Total storage deposit</Text>
-        <div class="flex flex-col items-end space-y-0.5">
-            <Text fontWeight={FontWeightText.semibold} fontSize="15" lineHeight="20">1.83 Mi</Text>
-            <Text color="gray-600" fontWeight={FontWeightText.medium} fontSize="13" lineHeight="16">$ 2.45</Text>
-        </div>
-    </div>
+    <TextTile
+        leftPrimaryText="Total storage deposit"
+        leftPrimaryFontWeight={FontWeightText.semibold}
+        rightPrimaryText="1.83 Mi"
+        rightPrimaryFontWeight={FontWeightText.semibold}
+        rightSecondaryText="$ 2.45"
+    />
 </div>
