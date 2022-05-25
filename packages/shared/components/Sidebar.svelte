b2cdeadf3fc5d6cb9a913a2bef3067c131f9f91e
Begoña Alvarez <balvarez@boxfish.studio>
2022-05-25 18:40:13 +0200

    feat: reset gov router only if you are already in gov, like a home button

--- packages/shared/components/Sidebar.svelte
+++ packages/shared/components/Sidebar.svelte
@@ -146,6 +146,10 @@
     }
 
     function openGovernance() {
+        // reset router only if you are already on governance, act like a home button
+        if ($dashboardRoute === DashboardRoute.Governance) {
+            $governanceRouter.reset()
+        }
         $dashboardRouter.goTo(DashboardRoute.Governance)
     }
 </script>
