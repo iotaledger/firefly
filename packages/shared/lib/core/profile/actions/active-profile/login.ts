cfcaaba01704eee2afe8cd62897eae77aaef5e43
Nicole O'Brien <nicole.obrien@iota.org>
2022-05-26 13:31:42 +0100

    refactor: initial sendAmount logic for stardust (#3329)
    * temp: stop polling market data and chrysalis snapshot
    
    * enhancement: clean up sendForm and assetAmountInput
    
    * refactor: remove old unit dependency
    
    * refactor: extract recipient type
    
    * remove: temp extra asset
    
    * fix: localise available balance string
    
    * refactor: initial sendAmount logic
    
    * refactor: checkStronghold for stardust
    
    * fix: resolve pr review comments
    
    * fix: tests
    
    * fix: resolve pr review comments

--- packages/shared/lib/core/profile/actions/active-profile/login.ts
+++ packages/shared/lib/core/profile/actions/active-profile/login.ts
@@ -1,12 +1,15 @@
 import { activeProfile } from '../../stores'
 import { get } from 'svelte/store'
 import { loadAccounts } from './loadAccounts'
+import { isStrongholdUnlocked } from '@core/profile-manager'
 
-export function login(): void {
-    const { loggedIn, lastActiveAt, id } = get(activeProfile)
+export async function login(): Promise<void> {
+    const { loggedIn, lastActiveAt, id, isStrongholdLocked } = get(activeProfile)
     if (id) {
         void loadAccounts()
         loggedIn.set(true)
         lastActiveAt.set(new Date())
+        const response = await isStrongholdUnlocked()
+        isStrongholdLocked.set(!response)
     }
 }
