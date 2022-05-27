408f95bb31278c6f9a39f71dfb5211d6d1dc2d9b
Amadeo Marchioni <amadeo.marchioni@iota.org>
2022-05-26 23:52:41 -0300

    fix: issues on account navigation
    Signed-off-by: Amadeo Marchioni <amadeo.marchioni@iota.org>

--- packages/shared/components/AccountNavigation.svelte
+++ packages/shared/components/AccountNavigation.svelte
@@ -1,17 +1,12 @@
 <script lang="typescript">
     import type { WalletAccount } from 'lib/typings/wallet'
-    import { AccountSwitcherModal, Icon, Text, Drawer } from 'shared/components'
+    import { Drawer, Icon, Text } from 'shared/components'
     import { AccountSwitcher, AccountCreation } from 'shared/components/drawerContent'
-    import { mobile } from 'shared/lib/app'
     import { activeProfile, getColor } from 'shared/lib/profile'
     import { selectedAccount } from 'shared/lib/wallet'
-    import { isBright } from 'shared/lib/helpers'
 
     export let accounts: WalletAccount[] = []
     export let onCreateAccount = (..._: any[]): void => {}
-
-    $: color = getColor($activeProfile, $selectedAccount?.id) as string
-    $: textColor = isBright(color) ? 'gray-800' : 'white'
 
     enum DrawerRoutes {
         Init = 'init',
@@ -19,17 +14,13 @@
     }
 
     let drawer: Drawer
-    let showModal = false
     let showDrawer = false
     let drawerRoute = DrawerRoutes.Init
 
     function toggleAccountSwitcher(): void {
-        if ($mobile) {
-            setDrawerRoute(DrawerRoutes.Init)
-            showDrawer = !showDrawer
-            return drawer?.open()
-        }
-        showModal = !showModal
+        setDrawerRoute(DrawerRoutes.Init)
+        showDrawer = !showDrawer
+        drawer?.open()
     }
 
     function setDrawerRoute(route: DrawerRoutes): void {
@@ -40,14 +31,13 @@
 <div class="flex flex-auto flex-col">
     <button
         on:click={toggleAccountSwitcher}
-        class=" {$mobile &&
-            'ml-5 mt-3 p-2 fixed left-1/3 rounded-xl'} flex flex-row justify-center items-center space-x-2"
+        class="mt-3 p-2 pl-4 fixed rounded-xl flex flex-row justify-center items-center space-x-2 {showDrawer
+            ? 'bg-gray-100 dark:bg-gray-900'
+            : ''}"
     >
-        {#if !$mobile}
-            <div class="circle" style="--account-color: {getColor($activeProfile, $selectedAccount?.id)};" />
-        {/if}
+        <span class="circle" style="--account-color: {getColor($activeProfile, $selectedAccount?.id)}" />
         <Text type="h4">{$selectedAccount?.alias}</Text>
-        <div class="transform transition-all {showModal ? 'rotate-180' : 'rotate-0'}">
+        <div class="transform transition-transform {showDrawer ? 'rotate-180' : 'rotate-0'}">
             <Icon height="18" width="18" icon="chevron-down" classes="text-gray-800 dark:text-white" />
         </div>
     </button>
@@ -56,18 +46,19 @@
             <AccountCreation {onCreateAccount} onCancel={() => drawer.close()} />
         {:else if (drawerRoute = DrawerRoutes.Init)}
             <AccountSwitcher
+                {accounts}
                 {onCreateAccount}
                 handleCreateAccountPress={() => setDrawerRoute(DrawerRoutes.Create)}
                 onAccountSelection={() => drawer.close()}
-                {accounts}
             />
         {/if}
     </Drawer>
 </div>
-<AccountSwitcherModal {onCreateAccount} {accounts} bind:isActive={showModal} />
 
 <style type="text/scss">
     button {
+        left: 50%;
+        transform: translatex(-50%);
         .circle {
             @apply rounded-full;
             @apply w-3;
