54cdbe439535b3aa524fff84bee9faf02e0f2186
Amadeo Marchioni <amadeo.marchioni@iota.org>
2022-05-27 04:05:04 -0300

    chore: remove duplicated file and code
    Signed-off-by: Amadeo Marchioni <amadeo.marchioni@iota.org>

--- packages/shared/components/drawerContent/index.js
+++ packages/shared/components/drawerContent/index.js
@@ -1,6 +1,5 @@
 export { default as AccountSwitcher } from './AccountSwitcher.svelte'
 export { default as AccountActions } from './AccountActions.svelte'
-export { default as AccountCreation } from './AccountCreation.svelte'
 export { default as AddressHistory } from './AddressHistory.svelte'
 export { default as ExportTransactionHistory } from './ExportTransactionHistory.svelte'
 export { default as HideAccount } from './HideAccount.svelte'
