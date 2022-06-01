eabfccda402bc77ac4b53973923be507f3b627bb
Jean Ribeiro <iamjeanribeiro@gmail.com>
2022-06-01 16:59:30 -0300

    feat: updates asset dropdown list

--- packages/shared/components/inputs/AssetDropdown.svelte
+++ packages/shared/components/inputs/AssetDropdown.svelte
@@ -1,32 +1,65 @@
 <script lang="typescript">
     import { FontWeightText } from 'shared/components/Text.svelte'
-    import { Icon, Text } from 'shared/components'
+    import { Icon, Text, AssetTile } from 'shared/components'
     import { assets } from '@core/wallet'
 
     export let asset = $assets?.[0]
+
+    const hasMultipleAssets = $assets.length > 1
+
+    let collapsed = false
+
+    function handleClick() {
+        if (hasMultipleAssets) {
+            collapsed = !collapsed
+        }
+    }
+
+    function handleAssetClick(_asset) {
+        asset = _asset
+    }
 </script>
 
 {#if asset}
-    <div
-        class="flex flex-row items-center p-2 space-x-2 text-left bg-gray-100 dark:bg-gray-700 rounded-md cursor-pointer"
-    >
+    <div on:click={handleClick} class="flex flex-col relative">
         <div
-            class="icon icon-bg h-6 w-6 rounded-full flex items-center justify-center p-0.5"
-            style="--icon-bg-color: {asset?.metadata?.primaryColor}"
+            class="flex flex-row items-center p-2 space-x-2 text-left bg-gray-100 dark:bg-gray-700 rounded-md cursor-default"
+            class:cursor-pointer={hasMultipleAssets}
         >
-            <Icon classes="text-white" icon={asset?.metadata.name.toLocaleLowerCase()} height="100%" width="100%" />
-        </div>
-        <Text color="gray-600" darkColor="gray-500" fontWeight={FontWeightText.semibold} fontSize="15">
-            {asset?.metadata.name}
-        </Text>
-        <div class="transform rotate-0">
-            <Icon height="18" width="18" icon="chevron-down" classes="text-gray-600 dark:text-gray-500" />
+            <div
+                class="icon icon-bg h-6 w-6 rounded-full flex items-center justify-center p-0.5"
+                style="--icon-bg-color: {asset?.metadata?.primaryColor}"
+            >
+                <Icon classes="text-white" icon={asset?.metadata.name.toLocaleLowerCase()} height="100%" width="100%" />
+            </div>
+            <Text color="gray-600" darkColor="gray-500" fontWeight={FontWeightText.semibold} fontSize="15">
+                {asset?.metadata.name}
+            </Text>
+            {#if hasMultipleAssets}
+                <div class="transform rotate-0">
+                    <Icon height="18" width="18" icon="chevron-down" classes="text-gray-600 dark:text-gray-500" />
+                </div>
+            {/if}
         </div>
     </div>
+    {#if collapsed && hasMultipleAssets}
+        <ul class="bg-gray-50 absolute top-12 asset-list-sizing border border-solid border-gray-300 rounded-2xl z-10" on:click={handleClick}>
+            {#each $assets as _asset}
+                <li on:click={() => handleAssetClick(_asset)}>
+                    <AssetTile asset={_asset} />
+                </li>
+            {/each}
+        </ul>
+    {/if}
 {/if}
 
 <style>
     .icon-bg {
         background-color: var(--icon-bg-color);
+    }
+
+    .asset-list-sizing {
+        left: -1.25rem;
+        right: -1.125rem;
     }
 </style>
--- packages/shared/lib/core/wallet/stores/assets.store.ts
+++ packages/shared/lib/core/wallet/stores/assets.store.ts
@@ -4,10 +4,10 @@ import { activeProfile } from '@core/profile'
 import { AvailableExchangeRates, CurrencyTypes } from 'shared/lib/typings/currency'
 import { UNIT_MAP, Unit } from 'shared/lib/units'
 import { selectedAccount } from '@core/account'
-import { derived } from 'svelte/store'
+import { derived, Readable } from 'svelte/store'
 import { IAsset } from '@core/wallet'
 
-export const assets = derived(
+export const assets: Readable<IAsset[]> = derived(
     [exchangeRates, currencies, activeProfile, selectedAccount],
     ([$exchangeRates, $currencies, $activeProfile, $selectedAccount]) => {
         if (!$activeProfile || !$selectedAccount) return []
