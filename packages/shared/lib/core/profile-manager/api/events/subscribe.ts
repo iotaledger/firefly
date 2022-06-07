8011d5d6c4c95ae446a34c1c9208e14367829dbe
Tuditi <daviddetroch@pm.me>
2022-06-07 19:48:46 +0200

    chore: remove profile.ts

--- packages/shared/lib/core/profile-manager/api/events/subscribe.ts
+++ packages/shared/lib/core/profile-manager/api/events/subscribe.ts
@@ -19,7 +19,7 @@ export function subscribe(): void {
             }
 
             // do we need to handle the parsing?
-            const { accountIndex, event } = JSON.parse(result)
+            const { event } = JSON.parse(result)
 
             const eventNames = Object.keys(event)
 
