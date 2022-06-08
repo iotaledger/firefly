fb82d8bf2b833c6c8610291da8c331ddfbd176b7
Jason Kraft <developer.kraft@gmail.com>
2022-06-08 15:12:41 -0700

    Improve white-space border around notificiation badge

--- packages/shared/components/NotificationBadge.svelte
+++ packages/shared/components/NotificationBadge.svelte
@@ -5,7 +5,5 @@
 </script>
 
 <span class="absolute top-0 right-0 flex justify-center items-center h-3 w-3">
-    <span
-        class="h-2 w-2 rounded-full box-content border-2 border-solid border-{borderColor} dark:border-{darkBorderColor} bg-{innerColor}"
-    />
+    <span class="h-2 w-2 rounded-full ring-2 ring-{borderColor} dark:ring-{darkBorderColor} bg-{innerColor}" />
 </span>
