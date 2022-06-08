a3fdb88195cce243ee37c38d36326783548e2717
Jason Kraft <developer.kraft@gmail.com>
2022-06-08 14:26:34 -0700

    Change account notification badge to match Figma design

---
+++ packages/shared/components/NotificationBadge.svelte
@@ -0,0 +1,11 @@
+<script lang="typescript">
+    export let innerColor = 'red-600'
+    export let borderColor = 'white'
+    export let darkBorderColor = 'gray-800'
+</script>
+
+<span class="absolute top-0 right-0 flex justify-center items-center h-3 w-3">
+    <span
+        class="h-2 w-2 rounded-full box-content border-2 border-solid border-{borderColor} dark:border-{darkBorderColor} bg-{innerColor}"
+    />
+</span>
