0fe22e7fa960aedb142aa370f11aa72eb604ee17
Amadeo Marchioni <amadeo.marchioni@iota.org>
2022-06-08 10:42:58 -0300

    fix: rename correct class folder
    Signed-off-by: Amadeo Marchioni <amadeo.marchioni@iota.org>

--- packages/mobile/android/app/src/main/java/org/iota/firefly/mobile/alpha/MainActivity.java
+++ packages/mobile/android/app/src/main/java/org/iota/firefly/mobile/alpha/MainActivity.java
@@ -7,5 +7,6 @@ public class MainActivity extends BridgeActivity {
   @Override
   public void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
+    registerPlugin(WebViewSettingsPlugin.class);
   }
 }
