361aa09d091bc3d371caa72b6a2756ee3941fd3a
Nicole O'Brien <nicole.obrien@iota.org>
2022-06-10 00:42:59 +0100

    fix: build issues to do with types (#3423)
    * fix: build issues to doe with types
    
    * fix: tests

--- packages/shared/lib/core/wallet/utils/isAsyncActivity.ts
+++ packages/shared/lib/core/wallet/utils/isAsyncActivity.ts
@@ -3,7 +3,11 @@ import { OutputData } from '@iota/wallet'
 import { ASYNC_UNLOCK_CONDITION_TYPES } from '../constants'
 
 export function isAsyncActivity(output: OutputData): boolean {
-    return output.output.unlockConditions.some((unlockCondition) => isAsyncUnlockCondition(unlockCondition))
+    if (output.output.type !== 2) {
+        return output.output.unlockConditions.some((unlockCondition) => isAsyncUnlockCondition(unlockCondition))
+    } else {
+        return false
+    }
 }
 
 export function isAsyncUnlockCondition(unlockCondition: UnlockConditionTypes): boolean {
