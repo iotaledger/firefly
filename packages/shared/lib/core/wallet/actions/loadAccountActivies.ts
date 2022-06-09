06071437bba019780f71b9b5c46851f0988115c5
MarkNerdi <mark.nardi@iota.org>
2022-06-09 13:46:44 +0200

    feat: load activities from outputs

--- packages/shared/lib/core/wallet/actions/loadAccountActivies.ts
+++ packages/shared/lib/core/wallet/actions/loadAccountActivies.ts
@@ -4,6 +4,7 @@ import {
     addActivityToAccountActivitiesInAllAccountActivities,
     addEmptyAccountActivitiesToAllAccountActivities,
 } from '../stores'
+import { isActivityHiddenForAccountId } from '../stores/hidden-activities.store'
 
 export function loadAccountActivities(account: IAccountState): void {
     addEmptyAccountActivitiesToAllAccountActivities(account.id)
@@ -12,5 +13,16 @@ export function loadAccountActivities(account: IAccountState): void {
             account.id,
             new Activity().setFromTransaction(transactionId, account.meta.transactions?.[transactionId])
         )
+    })
+    Object.keys(account.meta.outputs).forEach((outputId) => {
+        const output = account.meta.outputs?.[outputId]
+        const claimed = !!account.meta.lockedOutputs[outputId]
+        if (!output.remainder) {
+            const hidden = isActivityHiddenForAccountId(account.id, outputId)
+            addActivityToAccountActivitiesInAllAccountActivities(
+                account.id,
+                new Activity().setFromOutput(outputId, output, account.depositAddress, hidden, claimed)
+            )
+        }
     })
 }
