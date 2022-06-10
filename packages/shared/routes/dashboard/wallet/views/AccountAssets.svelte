a58b1fe97da976bd134d00d1634968c22c288ba6
Paul Bögelsack <pboegelsack@googlemail.com>
2022-06-10 18:10:52 +0200

    fix: bottom navigation for account history

--- packages/shared/routes/dashboard/wallet/views/AccountAssets.svelte
+++ packages/shared/routes/dashboard/wallet/views/AccountAssets.svelte
@@ -6,9 +6,11 @@
     export let classes = ''
 </script>
 
-<div class="w-full h-full space-y-6 flex flex-auto flex-col flex-shrink-0 p-6 {classes}">
-    <Text classes="text-left" type="h5">{localize('general.myAssets')}</Text>
-    <div class="flex flex-auto flex-col overflow-y-auto h-1 -mr-2 pr-2 scroll-secondary scrollable-y">
+<div class="h-full w-full space-y-4 flex flex-auto flex-col px-9 pt-6 {classes}">
+    <Text type="h5">
+        {localize('general.myAssets')}
+    </Text>
+    <div class="flex flex-auto flex-col overflow-y-auto h-1 pr-2">
         {#each $assets as asset}
             <div class="w-full mb-2.5">
                 <AssetTile {asset} />
