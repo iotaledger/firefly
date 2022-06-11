be51e75556e3218f4c0da163c56cc280910be173
Paul Bögelsack <pboegelsack@googlemail.com>
2022-06-11 16:43:37 +0200

    enhancement: updates account balance component for mobile scrolling animation

--- packages/shared/routes/dashboard/wallet/views/AccountBalance.svelte
+++ packages/shared/routes/dashboard/wallet/views/AccountBalance.svelte
@@ -7,9 +7,10 @@
     import { AccountRoute } from '@core/router/enums'
     import { formatUnitBestMatch, formatUnitPrecision } from 'shared/lib/units'
     import { selectedAccount } from 'shared/lib/wallet'
+    import { spring } from 'svelte/motion'
 
     export let classes = ''
-
+    export let scale = spring(1)
     export let onMenuClick = (): void => {}
 
     let showPreciseBalance = false
@@ -21,39 +22,79 @@
         $accountRouter.goTo(AccountRoute.Receive)
     }
 
+    function scaleWrapper(node) {
+        const paddingTop = 3.75
+        scale.subscribe((curr) => {
+            const scaleUp = curr * curr
+            node.style.paddingTop = `${paddingTop * scaleUp + 0.75}rem`
+        })
+    }
+
+    function scaleBalance(node) {
+        scale.subscribe((curr) => {
+            const scaleUp = curr * curr
+            node.style.transform = `scale(${0.4 * scaleUp + 0.6}, ${0.4 * scaleUp + 0.6}) translateX(${
+                380 * (1 - scaleUp)
+            }%)`
+        })
+    }
+
+    function scaleDollar(node) {
+        const opacity = 1
+        scale.subscribe((curr) => {
+            const speedUp = curr - (1 - curr)
+            node.style.opacity = `${opacity * speedUp}`
+        })
+    }
+
+    function scaleButtons(node) {
+        const opacity = 1
+        const marginTop = 1.75
+        scale.subscribe((curr) => {
+            const speedUp = curr - (1 - curr)
+            node.style.marginTop = `${marginTop * speedUp}rem`
+            node.style.transform = `scale(${curr}, ${curr})`
+            node.style.opacity = `${opacity * speedUp}`
+        })
+    }
+
+    function scaleMobileMenu(node) {
+        const opacity = 1
+        scale.subscribe((curr) => {
+            const speedUp = curr - (1 - curr)
+            node.style.opacity = `${opacity * speedUp}`
+        })
+    }
+
     function togglePreciseBalance() {
         showPreciseBalance = !showPreciseBalance
     }
 </script>
 
-<div
-    class="bg-gradient-to-t from-gray-100 via-white to-white dark:from-gray-900 dark:via-gray-800 dark:to-gray-800 relative p-6 {$mobile
-        ? 'pb-0 pt-20 bg-transparent'
-        : 'pb-12'} {classes}"
->
-    <!-- Balance -->
-    <div data-label="total-balance" class="flex flex-col flex-wrap space-y-1.5">
-        {#if !$mobile}
-            <p class="text-11 leading-120 text-gray-800 dark:text-white uppercase tracking-widest">
-                {localize('general.balance')}
-            </p>
-        {/if}
-        <div class="flex flex-col flex-wrap {$mobile ? 'items-center' : 'items-start space-y-1.5 mr-12'}">
-            <div on:click={togglePreciseBalance}>
-                <Text type={$mobile ? 'h1' : 'h2'}>
-                    {showPreciseBalance
-                        ? formatUnitPrecision($selectedAccount?.rawIotaBalance, Unit.Mi)
-                        : formatUnitBestMatch($selectedAccount?.rawIotaBalance, true, 3)}
-                </Text>
+{#if $mobile}
+    <div
+        class="bg-gradient-to-t from-gray-100 via-white to-white dark:from-gray-900 dark:via-gray-800 dark:to-gray-800 relative p-6 pb-0 pt-20 bg-transparent {classes}"
+        use:scaleWrapper
+    >
+        <!-- Balance -->
+        <div data-label="total-balance" class="flex flex-col flex-wrap space-y-1.5">
+            <div class="flex flex-col flex-wrap items-center">
+                <div on:click={togglePreciseBalance} use:scaleBalance>
+                    <Text type="h1">
+                        {showPreciseBalance
+                            ? formatUnitPrecision($selectedAccount?.rawIotaBalance, Unit.Mi)
+                            : formatUnitBestMatch($selectedAccount?.rawIotaBalance, true, 3)}
+                    </Text>
+                </div>
+                <div use:scaleDollar>
+                    <Text type="h4" smaller overrideColor="true" classes={mobile && 'text-gray-500'}>
+                        {$selectedAccount?.balanceEquiv}
+                    </Text>
+                </div>
             </div>
-            <Text type={$mobile ? 'h4' : 'p'} smaller overrideColor={$mobile} classes={mobile && 'text-gray-500'}>
-                {$selectedAccount?.balanceEquiv}
-            </Text>
         </div>
-    </div>
-    {#if $accountRoute === AccountRoute.Init || $mobile}
         <!-- Action Send / Receive -->
-        <div class="flex flex-row justify-between space-x-4 mt-7 {$mobile && 'mb-10'}">
+        <div class="flex flex-row justify-between space-x-4 mt-7 {$mobile && 'mb-10'}" use:scaleButtons>
             <button
                 class="action p-3 w-full text-center rounded-lg font-semibold text-14 bg-blue-500 text-white"
                 on:click={handleReceiveClick}
@@ -67,20 +108,69 @@
                 {localize('actions.send')}
             </button>
         </div>
-    {/if}
-    <button
-        on:click={() => onMenuClick()}
-        class="{$mobile
-            ? 'menu-mobile'
-            : 'bg-opacity-10 bg-gray-50 dark:bg-gray-900 dark:bg-opacity-50 rounded-lg'} px-2 py-3 flex flex-row space-x-1 text-gray-900 dark:text-white absolute top-6 right-6"
+        <button
+            on:click={() => onMenuClick()}
+            use:scaleMobileMenu
+            class="menu-mobile px-2 py-3 flex flex-row space-x-1 text-gray-900 dark:text-white absolute top-6 right-6"
+        >
+            {#each Array(3) as _}
+                <svg width="4" height="4" viewBox="0 0 4 4">
+                    <circle cx="2" cy="2" r="2" class="fill-current" />
+                </svg>
+            {/each}
+        </button>
+    </div>
+{:else}
+    <div
+        class="bg-gradient-to-t from-gray-100 via-white to-white dark:from-gray-900 dark:via-gray-800 dark:to-gray-800 relative p-6 pb-12 {classes}"
     >
-        {#each Array(3) as _}
-            <svg width="4" height="4" viewBox="0 0 4 4">
-                <circle cx="2" cy="2" r="2" class="fill-current" />
-            </svg>
-        {/each}
-    </button>
-</div>
+        <!-- Balance -->
+        <div data-label="total-balance" class="flex flex-col flex-wrap space-y-1.5">
+            <p class="text-11 leading-120 text-gray-800 dark:text-white uppercase tracking-widest">
+                {localize('general.balance')}
+            </p>
+            <div class="flex flex-col flex-wrap 'items-start space-y-1.5 mr-12">
+                <div on:click={togglePreciseBalance}>
+                    <Text type="h2">
+                        {showPreciseBalance
+                            ? formatUnitPrecision($selectedAccount?.rawIotaBalance, Unit.Mi)
+                            : formatUnitBestMatch($selectedAccount?.rawIotaBalance, true, 3)}
+                    </Text>
+                </div>
+                <Text type="p" smaller overrideColor="false" classes={mobile && 'text-gray-500'}>
+                    {$selectedAccount?.balanceEquiv}
+                </Text>
+            </div>
+        </div>
+        {#if $accountRoute === AccountRoute.Init}
+            <!-- Action Send / Receive -->
+            <div class="flex flex-row justify-between space-x-4 mt-7 {$mobile && 'mb-10'}">
+                <button
+                    class="action p-3 w-full text-center rounded-lg font-semibold text-14 bg-blue-500 text-white"
+                    on:click={handleReceiveClick}
+                >
+                    {localize('actions.receive')}
+                </button>
+                <button
+                    class="action p-3 w-full text-center rounded-lg font-semibold text-14 bg-blue-500 text-white"
+                    on:click={handleSendClick}
+                >
+                    {localize('actions.send')}
+                </button>
+            </div>
+        {/if}
+        <button
+            on:click={() => onMenuClick()}
+            class="bg-opacity-10 bg-gray-50 dark:bg-gray-900 dark:bg-opacity-50 rounded-lg px-2 py-3 flex flex-row space-x-1 text-gray-900 dark:text-white absolute top-6 right-6"
+        >
+            {#each Array(3) as _}
+                <svg width="4" height="4" viewBox="0 0 4 4">
+                    <circle cx="2" cy="2" r="2" class="fill-current" />
+                </svg>
+            {/each}
+        </button>
+    </div>
+{/if}
 
 <style type="text/scss">
     .menu-mobile {
