c2a4ad87bda46a6c8383e6e6f06d9d4992c79e2f
Paul Bögelsack <pboegelsack@googlemail.com>
2022-06-11 16:42:14 +0200

    enhancement: Updates account assets component for scroll animation

--- packages/shared/routes/dashboard/wallet/views/AccountAssets.svelte
+++ packages/shared/routes/dashboard/wallet/views/AccountAssets.svelte
@@ -4,11 +4,16 @@
     import { localize } from '@core/i18n'
 
     export let classes = ''
+    export let scroll = true
+    export let scrollDetection = (node: Element): void => {}
 </script>
 
 <div class="w-full h-full space-y-6 flex flex-auto flex-col flex-shrink-0 p-6 {classes}">
     <Text classes="text-left" type="h5">{localize('general.myAssets')}</Text>
-    <div class="flex flex-auto flex-col overflow-y-auto h-1 -mr-2 pr-2 scroll-secondary scrollable-y">
+    <div
+        class="flex flex-auto flex-col h-1 -mr-2 pr-2 {scroll ? 'overflow-y-auto scroll-secondary scrollable-y' : ''}"
+        use:scrollDetection
+    >
         {#each $assets as asset}
             <div class="w-full mb-2.5">
                 <AssetTile {asset} />
