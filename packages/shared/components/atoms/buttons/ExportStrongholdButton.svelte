c0479e255a96ca81ed90ed5f8088fa0dbf43902c
Nicole O'Brien <nicole.obrien@iota.org>
2022-06-13 11:32:03 +0100

    refactor: export backup with stardust api (#3430)
    * refactor: app logic for exporting a backup
    
    * ui: style backup stronghold popup
    
    * refactor: remove old props
    
    * fix: resolve pr comments
    
    * fix: broken tests in initialiseProfileManager
    
    * fix: remove password field from initialiseProfileManager
    
    Co-authored-by: Charlie Varley <charlie.varley@iota.org>

---
+++ packages/shared/components/atoms/buttons/ExportStrongholdButton.svelte
@@ -0,0 +1,61 @@
+<script lang="typescript">
+    import { exportStronghold } from '@contexts/settings'
+    import { localize } from '@core/i18n'
+    import { showAppNotification } from '@lib/notifications'
+    import { openPopup } from '@lib/popup'
+    import { Button } from 'shared/components'
+
+    export let isBusy = false
+    export let message = ''
+    export let showNotification = false
+    export let overrideTitle = ''
+
+    function handleExportStrongholdResponse(cancelled, error): void {
+        setTimeout(
+            () => {
+                message = ''
+            },
+            cancelled ? 0 : 5000
+        )
+        isBusy = false
+        if (!cancelled) {
+            if (error) {
+                message = localize('general.exportingStrongholdFailed')
+                showNotification &&
+                    showAppNotification({
+                        type: 'error',
+                        message: localize(error),
+                    })
+            } else {
+                message = localize('general.exportingStrongholdSuccess')
+                showNotification &&
+                    showAppNotification({
+                        type: 'info',
+                        message: localize('general.exportingStrongholdSuccess'),
+                    })
+            }
+        }
+    }
+
+    function handleExportClick(): void {
+        isBusy = false
+        message = ''
+
+        openPopup({
+            type: 'password',
+            props: {
+                onSuccess: (password) => {
+                    isBusy = true
+                    message = localize('general.exportingStronghold')
+                    exportStronghold(password, handleExportStrongholdResponse)
+                },
+                returnPassword: true,
+                subtitle: localize('popups.password.backup'),
+            },
+        })
+    }
+</script>
+
+<Button medium inlineStyle="min-width: 156px;" onClick={handleExportClick} disabled={isBusy} {...$$restProps}>
+    {overrideTitle || localize('actions.export')}
+</Button>
