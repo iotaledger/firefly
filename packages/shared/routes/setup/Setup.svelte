4e93c0e6b728ac9775a8dff00f0923b4e73220c4
Jason Kraft <developer.kraft@gmail.com>
2022-06-14 13:14:30 -0700

    Update text in Setup.svelte

--- packages/shared/routes/setup/Setup.svelte
+++ packages/shared/routes/setup/Setup.svelte
@@ -3,8 +3,10 @@
     import { Animation, Button, Link, Logo, OnboardingLayout, Text } from 'shared/components'
     import { mobile } from '@core/app'
     import { Locale } from '@core/i18n'
+    import { newProfile } from '@core/profile'
     import { SetupType } from 'shared/lib/typings/setup'
     import { appRouter } from '@core/router'
+    import { NetworkProtocol } from '@core/network'
 
     export let locale: Locale
 
@@ -19,9 +21,9 @@
 
 <OnboardingLayout onBackClick={handleBackClick}>
     <div slot="title">
-        <Text type="h2">{locale('views.setup.title')}</Text>
+        <Text type="h2">{locale(`views.setup.title.${$newProfile?.networkProtocol}`)}</Text>
     </div>
-    <div slot="leftpane__content">
+    <div slot="leftpane__content" class:hidden={$newProfile?.networkProtocol !== NetworkProtocol.IOTA}>
         <div class="relative flex flex-col items-center bg-gray-100 dark:bg-gray-900 rounded-2xl mt-16 p-8 pt-16">
             <div class="absolute -top-14">
                 <Logo width="auto" height="auto" logo="logo-chrysalis-gem" />
@@ -42,15 +44,19 @@
             secondary
             onClick={() => handleContinueClick(SetupType.New)}
         >
-            {locale('actions.createWallet')}
+            {locale(`actions.createWallet.${$newProfile?.networkProtocol}`)}
             {#if !$mobile}
-                <Text type="p" secondary smaller>{locale('actions.createWalletDescription')}</Text>
+                <Text type="p" secondary smaller
+                    >{locale(`actions.createWalletDescription.${$newProfile?.networkProtocol}`)}</Text
+                >
             {/if}
         </Button>
         <Button icon="transfer" classes="w-full" secondary onClick={() => handleContinueClick(SetupType.Import)}>
-            {locale('actions.restoreWallet')}
+            {locale(`actions.restoreWallet.${$newProfile?.networkProtocol}`)}
             {#if !$mobile}
-                <Text type="p" secondary smaller>{locale('actions.restoreWalletDescription')}</Text>
+                <Text type="p" secondary smaller
+                    >{locale(`actions.restoreWalletDescription.${$newProfile?.networkProtocol}`)}</Text
+                >
             {/if}
         </Button>
     </div>
