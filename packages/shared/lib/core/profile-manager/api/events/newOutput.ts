52f16a7595ed5c239a33da78dd94216fa60b1e9b
MarkNerdi <105642810+MarkNerdi@users.noreply.github.com>
2022-06-14 11:16:11 +0200

    feat: handle newOutput event (#3463)
    * fix: timestamp in seconds to milliseconds
    
    * feat: add new ouput to activity list
    
    * refactor: create balance store
    
    * Revert "refactor: create balance store"
    
    This reverts commit 4835b94013d098fc4a6b497283f9913c58feeffc.
    
    * feat: sync balance after new output
    
    * chore: use store variable instead of api call for balance
    
    * fix: handle empty recipient
    
    * enhancement: use transactionId for consistency
    
    * fix: review fixes
    
    * feat: handle spent output event (#3478)
    
    * feat: handle spent output event
    
    * feat: handle transaction claiming
    
    * chore: add type checks
    
    * chore: move to constant file
    
    * chore: move to own file
    
    Co-authored-by: Umair Sarfraz <aquadestructor@icloud.com>

--- packages/shared/lib/core/profile-manager/api/events/newOutput.ts
+++ packages/shared/lib/core/profile-manager/api/events/newOutput.ts
@@ -1,3 +1,32 @@
-import { OutputData } from '@iota/wallet'
+import { syncBalance } from '@core/account/actions/syncBalance'
+import { activeAccounts } from '@core/profile/stores'
+import { Activity } from '@core/wallet/classes/activity.class'
+import { addActivityToAccountActivitiesInAllAccountActivities } from '@core/wallet/stores/all-account-activities.store'
+import { ADDRESS_TYPE_ED25519 } from '@core/wallet/constants'
+import { Bech32Helper } from '@lib/bech32Helper'
+import { Converter } from '@lib/converter'
+import { get } from 'svelte/store'
+import { NewOutputEvent } from '../types/newOutputEvent'
 
-export function handleNewOutputEvent(payload: OutputData): void {}
+export function handleNewOutputEvent(accountId: string, event: NewOutputEvent): void {
+    const account = get(activeAccounts).find((account) => account.id === accountId)
+
+    const address =
+        event.output.address?.type === ADDRESS_TYPE_ED25519
+            ? Bech32Helper.toBech32(0, Converter.hexToBytes(event.output.address.pubKeyHash.substring(2)), 'rms')
+            : ''
+    if (
+        event.output.address.type === ADDRESS_TYPE_ED25519 &&
+        account.depositAddress === address &&
+        !event.output.remainder
+    ) {
+        syncBalance(account.id)
+        const activity = {
+            output: event.output,
+            accountAddress: account.depositAddress,
+            hidden: false,
+            claimed: false,
+        }
+        addActivityToAccountActivitiesInAllAccountActivities(account.id, new Activity().setFromOutput(activity))
+    }
+}
