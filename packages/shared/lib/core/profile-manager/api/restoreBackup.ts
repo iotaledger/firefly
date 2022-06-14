01462b66b68d78440ba5f5eb201e57fadd80759e
Tuditi <daviddetroch@pm.me>
2022-06-14 14:22:56 +0200

    chore: removed comment

--- packages/shared/lib/core/profile-manager/api/restoreBackup.ts
+++ packages/shared/lib/core/profile-manager/api/restoreBackup.ts
@@ -2,7 +2,6 @@ import { get } from 'svelte/store'
 import { profileManager } from '../stores'
 
 export async function restoreBackup(importFilePath: string, password: string): Promise<void> {
-    // TODO: check this once Thoralf exposes this
     const manager = get(profileManager)
     await manager.restoreBackup(importFilePath, password)
 }
