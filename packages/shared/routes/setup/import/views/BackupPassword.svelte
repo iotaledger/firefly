9c81cd2bfadeb8b18bf25b69c628b7f26a7747ea
Tuditi <daviddetroch@pm.me>
2022-06-14 12:25:13 +0200

    chore: code conventions

--- packages/shared/routes/setup/import/views/BackupPassword.svelte
+++ packages/shared/routes/setup/import/views/BackupPassword.svelte
@@ -1,11 +1,9 @@
 <script lang="typescript">
     import { Animation, Button, OnboardingLayout, Password, Spinner, Text } from 'shared/components'
     import { mobile } from '@core/app'
-    import { Locale } from '@core/i18n'
+    import { localize } from '@core/i18n'
     import { createEventDispatcher, getContext } from 'svelte'
     import { ImportRouter } from '@core/router'
-
-    export let locale: Locale
 
     export let error = ''
     export let busy = false
@@ -32,20 +30,21 @@
 <OnboardingLayout onBackClick={handleBackClick} {busy}>
     <div slot="title">
         {#if $mobile}
-            <Text type="h2" classes="mb-4">{`${locale('general.import')} ${locale(`general.${$importType}`)}`}</Text>
+            <Text type="h2" classes="mb-4">
+                {`${localize('general.import')} ${localize(`general.${$importType}`)}`}
+            </Text>
         {:else}
-            <Text type="h2" classes="mb-4">{locale('general.import')}</Text>
-            <Text type="h3" highlighted>{locale(`general.${$importType}`)}</Text>
+            <Text type="h2" classes="mb-4">{localize('general.import')}</Text>
+            <Text type="h3" highlighted>{localize(`general.${$importType}`)}</Text>
         {/if}
     </div>
     <div slot="leftpane__content">
-        <Text type="p" secondary classes="mb-4">{locale('views.importBackupPassword.body1')}</Text>
-        <Text type="p" secondary classes="mb-8">{locale('views.importBackupPassword.body2')}</Text>
+        <Text type="p" secondary classes="mb-4">{localize('views.importBackupPassword.body1')}</Text>
+        <Text type="p" secondary classes="mb-8">{localize('views.importBackupPassword.body2')}</Text>
         <Password
             classes="mb-6"
             {error}
             bind:value={password}
-            {locale}
             showRevealToggle
             autofocus
             disabled={busy}
@@ -61,10 +60,10 @@
             {#if $isGettingMigrationData}
                 <Spinner
                     busy={$isGettingMigrationData}
-                    message={locale('views.migrate.restoringWallet')}
+                    message={localize('views.migrate.restoringWallet')}
                     classes="justify-center"
                 />
-            {:else}{locale('actions.continue')}{/if}
+            {:else}{localize('actions.continue')}{/if}
         </Button>
     </div>
     <div slot="rightpane" class="w-full h-full flex justify-center {!$mobile && 'bg-pastel-orange dark:bg-gray-900'}">
