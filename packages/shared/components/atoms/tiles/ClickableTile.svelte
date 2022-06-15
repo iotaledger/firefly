b175a7f56a27b2096eee21a062a7d6767c654792
Begoña Alvarez <balvarez@boxfish.studio>
2022-06-15 13:40:37 +0200

    refactor: remove default bg color from clickable tile

--- packages/shared/components/atoms/tiles/ClickableTile.svelte
+++ packages/shared/components/atoms/tiles/ClickableTile.svelte
@@ -8,7 +8,6 @@
 
 <Tile
     classes="
-        bg-gray-50 dark:bg-gray-900
         hover:bg-gray-200 dark:hover:bg-gray-700
         {disabled ? 'opacity-50 pointer-events-none' : 'cursor-pointer'}
         {classes}
