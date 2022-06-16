c6c2dec20f608a5556aa941dbe395cccdb97267a
MarkNerdi <mark.nardi@iota.org>
2022-06-16 15:16:03 +0200

    fix: error-handling on claim

--- packages/shared/lib/core/wallet/actions/claimActivity.ts
+++ packages/shared/lib/core/wallet/actions/claimActivity.ts
@@ -2,11 +2,15 @@ import { selectedAccount } from '@core/account/stores/selected-account.store'
 import { get } from 'svelte/store'
 import { queriedActivities } from '../stores/selected-account-activities.store'
 
-export function claimActivity(id: string): void {
-    queriedActivities.update((state) => {
-        const activity = state.find((activity) => activity.id === id)
-        get(selectedAccount).collectOutputs([activity.id])
-        activity.isClaimed = true
-        return state
-    })
+export async function claimActivity(id: string): Promise<void> {
+    try {
+        await get(selectedAccount).collectOutputs([id])
+        queriedActivities.update((state) => {
+            const activity = state.find((activity) => activity.id === id)
+            activity.isClaimed = true
+            return state
+        })
+    } catch (err) {
+        console.error(err)
+    }
 }
