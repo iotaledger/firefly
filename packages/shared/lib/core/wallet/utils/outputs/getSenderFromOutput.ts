e108eb395e8b0fe17bc91586a6600bf9a9a1e54f
MarkNerdi <mark.nardi@iota.org>
2022-06-16 09:57:35 +0200

    fix: use wallet name to sender as well

--- packages/shared/lib/core/wallet/utils/outputs/getSenderFromOutput.ts
+++ packages/shared/lib/core/wallet/utils/outputs/getSenderFromOutput.ts
@@ -1,4 +1,5 @@
 import { OutputTypes } from '@iota/types'
+import { findAccountWithAddress } from '@lib/wallet'
 import { OUTPUT_TYPE_TREASURY } from '../../constants'
 import { Sender } from '../../types'
 import { getSenderAddressFromUnlockCondition } from '../getSenderAddressFromUnlockCondition'
@@ -7,11 +8,12 @@ export function getSenderFromOutput(output: OutputTypes): Sender {
     if (output.type !== OUTPUT_TYPE_TREASURY) {
         for (const unlockCondition of output.unlockConditions) {
             const senderAddress = getSenderAddressFromUnlockCondition(unlockCondition)
-            if (senderAddress) {
-                return {
-                    type: 'address',
-                    address: senderAddress,
-                }
+            const senderAccount = findAccountWithAddress(senderAddress)
+
+            if (senderAccount) {
+                return { type: 'account', account: senderAccount }
+            } else if (senderAddress) {
+                return { type: 'address', address: senderAddress }
             }
         }
     } else {
