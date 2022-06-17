967607dff7cdb82cb8a46dd3cacaf25fec72f1f0
Nicole O'Brien <nicole.obrien@iota.org>
2022-06-17 15:05:50 +0100

    fix: push logic

--- packages/shared/lib/core/wallet/actions/loadAccountActivies.ts
+++ packages/shared/lib/core/wallet/actions/loadAccountActivies.ts
@@ -2,19 +2,15 @@ import { IAccountState } from '@core/account'
 import { addEmptyAccountActivitiesToAllAccountActivities } from '../stores'
 import { loadAccountActivitiesFromOutputs } from './loadAccountActivitiesFromOutputs'
 import { loadAccountActivitiesFromTransactions } from './loadAccountActivitiesFromTransactions'
-import {
-    updateActivityAsyncStateFromTransactions,
-    updateActivityAsyncStateFromOutputs,
-    updateActivitiesWithClaimedTransactionInfo,
-} from './'
+import { updateActivitiesWithClaimedTransactionInfo, updateActivityAsyncStateFromTransactions } from './'
 
 export function loadAccountActivities(account: IAccountState): void {
     addEmptyAccountActivitiesToAllAccountActivities(account.id)
     loadAccountActivitiesFromTransactions(account)
     loadAccountActivitiesFromOutputs(account)
-    // updateActivityAsyncStateFromTransactions(account)
+    updateActivityAsyncStateFromTransactions(account)
     // updateActivityAsyncStateFromOutputs(account)
 
     // removeClaimedTransactionsFromAccountActivities(account.id)
-    updateActivitiesWithClaimedTransactionInfo(account.id)
+    // updateActivitiesWithClaimedTransactionInfo(account.id)
 }
