f29c9db04efb59daa78fe7a2ba7b920512b66555
Nicole O'Brien <nicole.obrien@iota.org>
2022-06-17 14:15:11 +0100

    fix: update activity and temp hide the claiming activity

--- packages/shared/lib/core/wallet/actions/updateActivityAsyncStateFromTransactions.ts
+++ packages/shared/lib/core/wallet/actions/updateActivityAsyncStateFromTransactions.ts
@@ -1,7 +1,7 @@
 import { IAccountState } from '@core/account'
 import { get } from 'svelte/store'
 import { ActivityDirection } from '../enums'
-import { allAccountActivities } from '../stores'
+import { allAccountActivities, updateActivity } from '../stores'
 
 export function updateActivityAsyncStateFromTransactions(account: IAccountState): void {
     Object.keys(account.meta.transactions).forEach((transactionId) => {
@@ -14,12 +14,13 @@ export function updateActivityAsyncStateFromTransactions(account: IAccountState)
             inputs.some((input) => input.transactionId === activity.transactionId && activity.isAsync)
         )
         if (linkedActivities.length > 0) {
-            // TODO: hide activity with transactionsId
+            updateActivity({ id: transactionId, isHidden: true })
             linkedActivities.forEach((linkedActivity) => {
-                if (linkedActivity.isAsync && linkedActivity.direction === ActivityDirection.Out) {
+                if (linkedActivity.direction === ActivityDirection.Out) {
                     linkedActivity.claimingTransactionId = transactionId
                     linkedActivity.isClaimed = true
                     linkedActivity.claimedDate = new Date(Number(transaction.timestamp))
+                    updateActivity(linkedActivity)
                 }
             })
         }
