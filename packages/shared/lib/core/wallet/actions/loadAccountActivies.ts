ef04b7bc5b11a00db18bdb503b245a693efe8ab7
Nicole O'Brien <nicole.obrien@iota.org>
2022-06-17 16:18:22 +0100

    refactor: clean up unusued functions

--- packages/shared/lib/core/wallet/actions/loadAccountActivies.ts
+++ packages/shared/lib/core/wallet/actions/loadAccountActivies.ts
@@ -2,15 +2,11 @@ import { IAccountState } from '@core/account'
 import { addEmptyAccountActivitiesToAllAccountActivities } from '../stores'
 import { loadAccountActivitiesFromOutputs } from './loadAccountActivitiesFromOutputs'
 import { loadAccountActivitiesFromTransactions } from './loadAccountActivitiesFromTransactions'
-import { updateActivitiesWithClaimedTransactionInfo, updateActivityAsyncStateFromTransactions } from './'
+import { updateActivityAsyncStateFromTransactions } from './'
 
 export function loadAccountActivities(account: IAccountState): void {
     addEmptyAccountActivitiesToAllAccountActivities(account.id)
     loadAccountActivitiesFromTransactions(account)
     loadAccountActivitiesFromOutputs(account)
     updateActivityAsyncStateFromTransactions(account)
-    // updateActivityAsyncStateFromOutputs(account)
-
-    // removeClaimedTransactionsFromAccountActivities(account.id)
-    // updateActivitiesWithClaimedTransactionInfo(account.id)
 }
