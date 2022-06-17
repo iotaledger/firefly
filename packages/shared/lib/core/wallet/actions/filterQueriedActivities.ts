2ad0d219753ad5cac44e0da3c6c0e68f0415d681
Nicole O'Brien <nicole.obrien@iota.org>
2022-06-17 13:04:26 +0100

    temp: push broken code

--- packages/shared/lib/core/wallet/actions/filterQueriedActivities.ts
+++ packages/shared/lib/core/wallet/actions/filterQueriedActivities.ts
@@ -1,4 +1,6 @@
 import { ActivityDirection } from '@core/wallet'
+import { formatUnitBestMatch } from '@lib/units'
+import { unitToValue } from '@lib/utils'
 import { get } from 'svelte/store'
 import { selectedAccountActivities, queriedActivities } from '../stores'
 
@@ -10,5 +12,18 @@ export function filterQueriedActivities(filter: number): void {
     } else if (filter === 2) {
         activityList = activityList.filter((activity) => activity.direction === ActivityDirection.Out)
     }
+
+    activityList = activityList.filter(
+        (activity) =>
+            (activity.recipient.type === 'account' && activity.recipient?.account?.name === searchTerm) ||
+            (activity.recipient.type === 'address' && activity.recipient?.address === searchTerm) ||
+            activity?.id.toLowerCase() === searchTerm ||
+            (searchTerm[0] === '>' && unitToValue(searchTerm.substring(1)) < activity.rawAmount) ||
+            (searchTerm[0] === '<' && unitToValue(searchTerm.substring(1)) > activity.rawAmount) ||
+            (searchTerm[1] === 'i' && isValueInUnitRange(activity.rawAmount, searchTerm)) ||
+            activity.rawAmount === unitToValue(searchTerm) ||
+            formatUnitBestMatch(activity.rawAmount).toString().toLowerCase()?.includes(searchTerm)
+    )
+
     queriedActivities.set(activityList)
 }
