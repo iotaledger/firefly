4eb8a696ca64d972cb2e631ec261955e0872c3ab
MarkNerdi <mark.nardi@iota.org>
2022-06-17 16:51:02 +0200

    small performance improvement

--- packages/shared/lib/core/wallet/actions/updateActivityAsyncStateFromTransactions.ts
+++ packages/shared/lib/core/wallet/actions/updateActivityAsyncStateFromTransactions.ts
@@ -5,35 +5,33 @@ import { allAccountActivities, updateActivity } from '../stores'
 import { claimedActivities } from '../stores/claimed-activities.store'
 
 export function updateActivityAsyncStateFromTransactions(account: IAccountState): void {
+    const accountActivities = get(allAccountActivities).find(
+        (accountActivities) => accountActivities?.accountId === account.id
+    )
+    const activities = accountActivities.activities.filter((activity) => activity.direction === ActivityDirection.In)
+
     Object.keys(account.meta.transactions).forEach((transactionId) => {
         const transaction = account.meta.transactions?.[transactionId]
         const transactionInputs = transaction.payload.essence.inputs
-        const accountActivities = get(allAccountActivities).find(
-            (accountActivities) => accountActivities?.accountId === account.id
-        )
 
-        for (const activity of accountActivities.activities) {
-            if (activity.direction === ActivityDirection.In) {
-                const claimedActivity = get(claimedActivities)?.[account.id]?.[activity.transactionId]
-                if (claimedActivity && claimedActivity.claimingTransactionId === transactionId) {
-                    updateActivity(account.id, {
-                        ...claimedActivity,
-                        claimedDate: new Date(claimedActivity.claimedTimestamp),
-                    })
-                    updateActivity(account.id, { id: transactionId, isHidden: true })
-                } else if (
-                    transactionInputs.some(
-                        (input) => input.transactionId === activity.transactionId && activity.isAsync
-                    )
-                ) {
-                    updateActivity(account.id, {
-                        id: activity.id,
-                        isClaimed: true,
-                        claimedDate: new Date(Number(transaction.timestamp)),
-                        claimingTransactionId: transactionId,
-                    })
-                    updateActivity(account.id, { id: transactionId, isHidden: true })
-                }
+        for (const activity of activities) {
+            const claimedActivity = get(claimedActivities)?.[account.id]?.[activity.transactionId]
+            if (claimedActivity && claimedActivity.claimingTransactionId === transactionId) {
+                updateActivity(account.id, {
+                    ...claimedActivity,
+                    claimedDate: new Date(claimedActivity.claimedTimestamp),
+                })
+                updateActivity(account.id, { id: transactionId, isHidden: true })
+            } else if (
+                transactionInputs.some((input) => input.transactionId === activity.transactionId && activity.isAsync)
+            ) {
+                updateActivity(account.id, {
+                    id: activity.id,
+                    isClaimed: true,
+                    claimedDate: new Date(Number(transaction.timestamp)),
+                    claimingTransactionId: transactionId,
+                })
+                updateActivity(account.id, { id: transactionId, isHidden: true })
             }
         }
     })
