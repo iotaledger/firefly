c9efb9e0b0b3b0bd9d00a3205955ffaec226d133
Jean Ribeiro <iamjeanribeiro@gmail.com>
2022-06-19 19:12:26 -0300

    fix: import success layout

--- packages/shared/routes/setup/import/views/Success.svelte
+++ packages/shared/routes/setup/import/views/Success.svelte
@@ -18,8 +18,8 @@
         <Text type="h2">{localize('views.importFromFile.title')}</Text>
     </div>
     <div slot="leftpane__content">
-        <div class="flex flex-col items-center bg-gray-100 dark:bg-gray-900 rounded-2xl mt-10 p-5">
-            <div class="bg-green-500 rounded-2xl relative -top-10">
+        <div class="flex flex-col items-center bg-gray-100 dark:bg-gray-900 rounded-2xl mt-10 p-5 pt-10 relative">
+            <div class="bg-green-500 rounded-2xl absolute -top-5 p-1">
                 <Icon icon="success-check" classes="text-white" />
             </div>
             <Text type="h2" classes="mb-5 text-center">{localize('views.importSuccess.title')}</Text>
