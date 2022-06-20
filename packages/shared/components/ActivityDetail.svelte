4bce04d5d67117365c586210babf9d1e399f6956
Paul Bögelsack <pboegelsack@googlemail.com>
2022-06-20 20:23:20 +0200

    fix: view explorer button style

--- packages/shared/components/ActivityDetail.svelte
+++ packages/shared/components/ActivityDetail.svelte
@@ -214,7 +214,7 @@
                     class="mobile-explorer-button action p-3 w-full text-center rounded-lg font-semibold text-14 bg-white dark:bg-gray-800 text-blue-500"
                     on:click={() => Platform.openUrl(`${explorerLink}/message/${id}`)}
                 >
-                    <Text smaller highlighted>{localize('general.openExplorer')}</Text>
+                    <Text fontWeight="font-500" highlighted>{localize('general.openExplorer')}</Text>
                 </button>
             </div>
         </div>
