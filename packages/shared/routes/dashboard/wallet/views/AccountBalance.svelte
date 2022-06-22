4d1b153f79c8a73987245b4d720accdf580132f9
Paul Bögelsack <pboegelsack@googlemail.com>
2022-06-22 12:08:01 +0200

    fix: merge account balance component

--- packages/shared/routes/dashboard/wallet/views/AccountBalance.svelte
+++ packages/shared/routes/dashboard/wallet/views/AccountBalance.svelte
@@ -17,7 +17,6 @@
     let unsubscribeMobileBalanceAnimation = () => {}
     let unsubscribeMobileCurencyAnimation = () => {}
     let unsubscribeMobileButtonsAnimation = () => {}
-    let unsubscribeMobileWalletMenuAnimation = () => {}
 
     let showPreciseBalance = false
 
@@ -72,14 +71,6 @@
         })
     }
 
-    function animateMobileWalletMenu(node: HTMLElement): void {
-        const opacity = 1
-        unsubscribeMobileWalletMenuAnimation = scale.subscribe((curr) => {
-            const speedUp = curr - (1 - curr)
-            node.style.opacity = `${opacity * speedUp}`
-        })
-    }
-
     function togglePreciseBalance() {
         showPreciseBalance = !showPreciseBalance
     }
@@ -88,17 +79,16 @@
         unsubscribeMobileBalanceAnimation()
         unsubscribeMobileCurencyAnimation()
         unsubscribeMobileButtonsAnimation()
-        unsubscribeMobileWalletMenuAnimation()
     })
 </script>
 
 {#if $mobile}
     <div
-        class="bg-gradient-to-t from-gray-100 via-white to-white dark:from-gray-900 dark:via-gray-800 dark:to-gray-800 relative p-6 pb-0 pt-20 bg-transparent {classes}"
+        class="bg-gradient-to-t from-gray-100 via-white to-white dark:from-gray-900 dark:via-gray-800 dark:to-gray-800 relative p-6 'pb-0 pt-20 bg-transparent {classes}"
     >
         <!-- Balance -->
         <div data-label="total-balance" class="flex flex-col flex-wrap space-y-1.5">
-            <div class="flex flex-col flex-wrap items-center">
+            <div class="flex flex-col flex-wrap {$mobile ? 'items-center' : 'items-start space-y-1.5 mr-12'}">
                 <div on:click={togglePreciseBalance} use:animateMobileBalance>
                     <Text type="h1">
                         {showPreciseBalance
@@ -107,14 +97,14 @@
                     </Text>
                 </div>
                 <div use:animateMobileCurrency>
-                    <Text type="h4" smaller overrideColor="true" classes={mobile && 'text-gray-500'}>
+                    <Text type="h4" smaller overrideColor classes="text-gray-500">
                         {$selectedAccount?.balanceEquiv}
                     </Text>
                 </div>
             </div>
         </div>
         <!-- Action Send / Receive -->
-        <div class="flex flex-row justify-between space-x-4 mt-7 {$mobile && 'mb-10'}" use:animateMobileButtons>
+        <div class="flex flex-row justify-between space-x-4 mt-7 mb-10" use:animateMobileButtons>
             <button
                 class="action p-3 w-full text-center rounded-lg font-semibold text-14 bg-blue-500 text-white"
                 on:click={handleReceiveClick}
@@ -128,17 +118,6 @@
                 {localize('actions.send')}
             </button>
         </div>
-        <button
-            on:click={() => onMenuClick()}
-            use:animateMobileWalletMenu
-            class="menu-mobile px-2 py-3 flex flex-row space-x-1 text-gray-900 dark:text-white absolute top-6 right-6"
-        >
-            {#each Array(3) as _}
-                <svg width="4" height="4" viewBox="0 0 4 4">
-                    <circle cx="2" cy="2" r="2" class="fill-current" />
-                </svg>
-            {/each}
-        </button>
     </div>
 {:else}
     <div
@@ -149,7 +128,7 @@
             <p class="text-11 leading-120 text-gray-800 dark:text-white uppercase tracking-widest">
                 {localize('general.balance')}
             </p>
-            <div class="flex flex-col flex-wrap 'items-start space-y-1.5 mr-12">
+            <div class="flex flex-col flex-wrap items-start space-y-1.5 mr-12">
                 <div on:click={togglePreciseBalance}>
                     <Text type="h2">
                         {showPreciseBalance
@@ -157,14 +136,14 @@
                             : formatUnitBestMatch($selectedAccount?.rawIotaBalance, true, 3)}
                     </Text>
                 </div>
-                <Text type="p" smaller overrideColor="false" classes={mobile && 'text-gray-500'}>
+                <Text type="p" smaller overrideColor={false}>
                     {$selectedAccount?.balanceEquiv}
                 </Text>
             </div>
         </div>
         {#if $accountRoute === AccountRoute.Init}
             <!-- Action Send / Receive -->
-            <div class="flex flex-row justify-between space-x-4 mt-7 {$mobile && 'mb-10'}">
+            <div class="flex flex-row justify-between space-x-4 mt-7">
                 <button
                     class="action p-3 w-full text-center rounded-lg font-semibold text-14 bg-blue-500 text-white"
                     on:click={handleReceiveClick}
@@ -191,11 +170,3 @@
         </button>
     </div>
 {/if}
-
-<style type="text/scss">
-    .menu-mobile {
-        position: absolute;
-        margin-top: -8px;
-        right: 25px;
-    }
-</style>
