6ffcb6c71d7090dc2200d9ad7c6d5e2dcec91801
Tuditi <daviddetroch@pm.me>
2022-06-24 13:27:30 +0200

    chore: removed events & message types

--- packages/shared/lib/typings/index.ts
+++ packages/shared/lib/typings/index.ts
@@ -1,29 +1,12 @@
 import * as account from './account'
 import * as address from './address'
-import * as bridge from './bridge'
 import * as chart from './chart'
 import * as currency from './currency'
 import * as error from './error'
-import * as events from './events'
 import * as ledger from './ledger'
-import * as message from './message'
 import * as migration from './migration'
 import * as notification from './notification'
 import * as utils from './utils'
 import * as wallet from './wallet'
 
-export {
-    account,
-    address,
-    bridge,
-    chart,
-    currency,
-    error,
-    events,
-    ledger,
-    message,
-    migration,
-    notification,
-    utils,
-    wallet,
-}
+export { account, address, chart, currency, error, ledger, migration, notification, utils, wallet }
