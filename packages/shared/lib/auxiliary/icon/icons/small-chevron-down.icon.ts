24a82a4d9e602268d745e2efd921a17944465516
Begoña Alvarez <balvarez@boxfish.studio>
2022-06-24 14:38:16 +0200

    refactor: move each icon to a standalone file

---
+++ packages/shared/lib/auxiliary/icon/icons/small-chevron-down.icon.ts
@@ -0,0 +1,14 @@
+import { IIcon } from '../interfaces'
+
+export const SMALL_CHEVRON_DOWN_ICON: IIcon = {
+    width: 16,
+    height: 16,
+    path: [
+        {
+            d: 'M4 6L8 10L12 6',
+            strokeWidth: '2',
+            strokeLinecap: 'round',
+            fill: 'none',
+        },
+    ],
+}
