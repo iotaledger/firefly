d81231c5d5b852d490b93a3d894c49c975d12419
Jean Ribeiro <iamjeanribeiro@gmail.com>
2022-07-01 00:19:41 -0300

    fix: generate id if undefined before setting pin

--- packages/shared/lib/contexts/onboarding/actions/cleanupProtectionOnboarding.ts
+++ packages/shared/lib/contexts/onboarding/actions/cleanupProtectionOnboarding.ts
@@ -1,16 +1,20 @@
 import { get } from 'svelte/store'
 
 import { Platform } from '@lib/platform'
-import { newProfile } from '../stores'
+import { newProfile, updateNewProfile } from '../stores'
 import { SetupType } from '@lib/typings/setup'
 import { walletSetupType } from '@lib/wallet'
 
 import { storeAndCleanMnemonic } from './storeAndCleanMnemonic'
+import { generateRandomId } from '@lib/utils'
 
 /**
  * Handles the cleanup process for the protection segment of the onboarding flow.
  */
 export async function cleanupProtectionOnboarding(pinInput: string): Promise<void> {
+    if (!get(newProfile)?.id) {
+        updateNewProfile({ id: generateRandomId() })
+    }
     await Platform.PincodeManager.set(get(newProfile)?.id, pinInput)
     // TODO: replace with new api when it is implemented
     // await setStoragePassword(pinInput)
