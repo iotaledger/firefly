0ba186b5d3655354dee3a8b195109f262ea5e0a5
Jean Ribeiro <iamjeanribeiro@gmail.com>
2022-07-05 17:25:41 -0300

    fix: clears stronghold password when clicking back in setup pin

--- packages/shared/routes/onboarding/views/PasswordView.svelte
+++ packages/shared/routes/onboarding/views/PasswordView.svelte
@@ -1,9 +1,10 @@
 <script lang="typescript">
+    import { onMount } from 'svelte'
     import zxcvbn from 'zxcvbn'
     import { Animation, Button, OnboardingLayout, PasswordInput, Text } from 'shared/components'
     import { mobile } from '@core/app'
     import { localize } from '@core/i18n'
-    import { changeStrongholdPassword, setStrongholdPassword } from '@core/profile-manager'
+    import { changeStrongholdPassword, clearStrongholdPassword, setStrongholdPassword } from '@core/profile-manager'
     import { onboardingRouter } from '@core/router'
     import { strongholdPassword } from '@contexts/onboarding'
     import { showAppNotification } from '@lib/notifications'
@@ -76,6 +77,10 @@
             return zxcvbn(limitedPassword)
         }
     } // zxcvbn lib recommends to not validate long passwords because of performance issues https://github.com/dropbox/zxcvbn#user-content-performance
+
+    onMount(() => {
+        clearStrongholdPassword()
+    })
 </script>
 
 <OnboardingLayout onBackClick={handleBackClick} {busy}>
