84b1015cd2a4cc7dd04b3ed200e6764bcf387c2a
Nicole O'Brien <nicole.obrien@iota.org>
2022-07-06 13:29:19 +0100

    fix: change asset panel title

--- packages/shared/components/molecules/AssetList.svelte
+++ packages/shared/components/molecules/AssetList.svelte
@@ -9,7 +9,7 @@
 
 {#if assets && assets?.length > 0}
     <div class="w-full h-full space-y-6 flex flex-auto flex-col flex-shrink-0 p-6">
-        <Text classes="text-left" type={TextType.h5}>{localize('general.myAssets')}</Text>
+        <Text classes="text-left" type={TextType.h5}>{localize('general.assets')}</Text>
         <div class="flex flex-auto flex-col overflow-y-auto h-1 -mr-2 pr-2 space-y-2.5 scroll-secondary scrollable-y">
             {#each assets as asset}
                 <AssetTile {asset} />
