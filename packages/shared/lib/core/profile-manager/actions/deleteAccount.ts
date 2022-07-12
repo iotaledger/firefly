e8ad7a39b149b489562f2933868d1c4dc9f5136d
Jean Ribeiro <iamjeanribeiro@gmail.com>
2022-07-12 17:48:50 -0300

    fix: delete account flow UX (#3846)
    * fix: delete account flow ux
    
    * fix: code review
    
    * fix: adds password validation before deleting profile

--- packages/shared/lib/core/profile-manager/actions/deleteAccount.ts
+++ packages/shared/lib/core/profile-manager/actions/deleteAccount.ts
@@ -1,15 +1,30 @@
 import { get } from 'svelte/store'
-
 import { setSelectedAccount } from '@core/account'
 import { removeAccountFromActiveAccounts, visibleActiveAccounts } from '@core/profile'
-import { CannotRemoveAccountError, removeLatestAccount } from '@core/profile-manager'
+import {
+    CannotRemoveAccountError,
+    RemoveAccountWithBalanceError,
+    removeLatestAccount,
+    RemoveNotLastAccountError,
+} from '@core/profile-manager'
 import { resetWalletRoute } from '@core/router'
+import { parseCurrency } from '@lib/currency'
 
 export async function deleteAccount(id: string): Promise<void> {
+    const accountToBeDeleted = get(visibleActiveAccounts).find((account) => account?.id === id)
+    const accounts = get(visibleActiveAccounts)
+
+    if (accountToBeDeleted !== accounts[accounts.length - 1]) {
+        throw new RemoveNotLastAccountError()
+    }
+
+    if (parseCurrency(accountToBeDeleted?.balances?.baseCoin?.total) > 0) {
+        throw new RemoveAccountWithBalanceError()
+    }
+
     try {
         await removeLatestAccount()
         removeAccountFromActiveAccounts(id)
-
         setSelectedAccount(get(visibleActiveAccounts)?.[0]?.id ?? null)
         resetWalletRoute()
     } catch (err) {
