8c0fbb2d77e66f6a0144982294caf2308ec65279
MarkNerdi <mark.nardi@iota.org>
2022-07-12 22:02:48 +0200

    enhancement: use time constants instead of magic numbers

--- packages/shared/lib/core/wallet/classes/activity.class.ts
+++ packages/shared/lib/core/wallet/classes/activity.class.ts
@@ -7,7 +7,13 @@ import { OutputData, OutputOptions, Transaction } from '@iota/wallet'
 import { convertToFiat, formatCurrency } from '@lib/currency'
 import { truncateString } from '@lib/helpers'
 import { findAccountWithAddress } from '@lib/wallet'
-import { MILLISECONDS_PER_SECOND } from 'shared/lib/time'
+import {
+    HOURS_PER_DAY,
+    MILLISECONDS_PER_SECOND,
+    MINUTES_PER_HOUR,
+    SECONDS_PER_DAY,
+    SECONDS_PER_MINUTE,
+} from 'shared/lib/time'
 import { get } from 'svelte/store'
 import { ActivityAsyncStatus, ActivityDirection, ActivityType, InclusionState } from '../enums'
 import { IActivity } from '../interfaces'
@@ -209,10 +215,14 @@ export class Activity implements IActivity {
     getTimeDiffUntilExpirationTime(time: Date): string {
         if (this.isAsync && !this.isClaimed && this?.expirationDate) {
             const elapsedTime = this.expirationDate.getTime() - time.getTime()
-            const days = Math.floor(elapsedTime / (1000 * 60 * 60 * 24))
-            const hours = Math.floor((elapsedTime / (1000 * 60 * 60)) % 24)
-            const minutes = Math.floor((elapsedTime / (1000 * 60)) % 60)
-            const seconds = Math.floor((elapsedTime / 1000) % 60)
+            const days = Math.floor(elapsedTime / (MILLISECONDS_PER_SECOND * SECONDS_PER_DAY))
+            const hours = Math.floor(
+                (elapsedTime / (MILLISECONDS_PER_SECOND * SECONDS_PER_MINUTE * MINUTES_PER_HOUR)) % HOURS_PER_DAY
+            )
+            const minutes = Math.floor(
+                (elapsedTime / (MILLISECONDS_PER_SECOND * SECONDS_PER_MINUTE)) % MINUTES_PER_HOUR
+            )
+            const seconds = Math.floor((elapsedTime / MILLISECONDS_PER_SECOND) % SECONDS_PER_MINUTE)
 
             if (days > 0 || hours > 0) {
                 return `${days}d ${hours}h`
