c39f98fbd2c3338a27717365dd49981535e955b7
Umair Sarfraz <aquadestructor@icloud.com>
2022-07-18 03:43:25 +0200

    fix: make sure remainder balance does not hide while a transfer is in progress

--- packages/shared/lib/core/wallet/actions/setAsyncStatusOfAccountActivities.ts
+++ packages/shared/lib/core/wallet/actions/setAsyncStatusOfAccountActivities.ts
@@ -6,7 +6,9 @@ export function setAsyncStatusOfAccountActivities(time: Date): void {
     allAccountActivities.update((state) => {
         state.forEach((accountActivities, accountId) => {
             for (const activity of accountActivities) {
-                const oldAsyncStatus = activity.asyncStatus
+                // Fallback to null because getAsyncStatus(time) returns null for non-async activities
+                // Otherwise, activity.asyncStatus results undefined and therefore oldAsyncStatus !== activity.asyncStatus check would fail
+                const oldAsyncStatus = activity.asyncStatus || null
                 activity.asyncStatus = activity.getAsyncStatus(time)
                 if (!balancesToUpdate.includes(accountId) && oldAsyncStatus !== activity.asyncStatus) {
                     balancesToUpdate.push(accountId)
