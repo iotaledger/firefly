30cf9845776dbb378f9617eb83d4a24c03e04438
MarkNerdi <mark.nardi@iota.org>
2022-07-21 10:46:54 +0200

    feat: load metadata and display it in asset tile

--- packages/shared/lib/core/wallet/actions/refreshNativeTokens.ts
+++ packages/shared/lib/core/wallet/actions/refreshNativeTokens.ts
@@ -1,16 +1,20 @@
-import { selectedAccount } from '@core/account'
+import { activeAccounts } from '@core/profile'
 import { get } from 'svelte/store'
 import { addNativeTokenAsset, clearNativeTokenAssets } from '../stores'
+import { getTokenDataFromFoundryOutput } from '../utils'
 
-export function refreshNativeTokens(): void {
+export async function refreshNativeTokens(): Promise<void> {
     clearNativeTokenAssets()
-    get(selectedAccount)?.balances?.nativeTokens?.forEach((nativeToken) => {
+    const account = get(activeAccounts).find((_account) => _account.id === accountId)
+    for (const nativeToken of account?.balances?.nativeTokens) {
+        const metadata = await getTokenDataFromFoundryOutput(nativeToken.tokenId)
         addNativeTokenAsset({
             id: nativeToken.tokenId,
+            metadata,
             balance: {
                 total: Number(nativeToken.total),
                 available: Number(nativeToken.available),
             },
         })
-    })
+    }
 }
---
+++ packages/shared/lib/core/wallet/utils/getTokenDataFromFoundryOutput.ts
@@ -0,0 +1,28 @@
+import { ITokenMetadata } from '../interfaces'
+import { selectedAccount } from '@core/account'
+import { get } from 'svelte/store'
+import { IFoundryOutput } from '@iota/types'
+import { Converter } from '@lib/converter'
+
+export async function getTokenDataFromFoundryOutput(tokenId: string): Promise<ITokenMetadata> {
+    const foundry = await get(selectedAccount).getFoundryOutput(tokenId)
+    const data = getHexDataFromFoundryOutput(foundry)
+    const metadata = JSON.parse(Converter.hexToUtf8(data))
+
+    return {
+        name: metadata.name,
+        tickerSymbol: metadata.symbol,
+        unit: metadata.symbol,
+        decimals: metadata.decimals,
+        useMetricPrefix: false,
+    }
+}
+
+function getHexDataFromFoundryOutput(foundry: IFoundryOutput): string {
+    for (const feature of foundry.immutableFeatures) {
+        if (feature.type === 2) {
+            return feature.data
+        }
+    }
+    return undefined
+}
