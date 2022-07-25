ba974cacb8267e1f6704d5d202018a4f0a468fc3
Jean Ribeiro <iamjeanribeiro@gmail.com>
2022-07-25 15:50:18 -0300

    fix: truncate long tx metadata (#3885)
    * chore: remove brackets from app name
    
    * chore: update app description
    
    * chore: increment version to `2.0.0-alpha-0.1.4`
    
    * chore: set network config for public testnet
    
    * chore: enable public testnet profiles
    
    * chore: increment version to `2.0.0-beta-1.0.0`
    
    * fix: remove automatic and manual node selection for beta
    
    * fix: nodeInfo on dev indicator
    
    * chore: update network names
    
    * chore: increment desktop version to `2.0.0-beta-1.0.1`
    
    * fix: adds margin and truncate to KeyValueBox
    
    * feat: add tooltip for truncated texts
    
    * fix: tooltip triangle not showing due to overflow
    
    * feat: improve tooltip toggle
    
    Co-authored-by: Nicole O'Brien <nicole.obrien@iota.org>
    Co-authored-by: Begoña Alvarez <balvarez@boxfish.studio>

--- packages/shared/components/atoms/boxes/KeyValueBox.svelte
+++ packages/shared/components/atoms/boxes/KeyValueBox.svelte
@@ -1,5 +1,5 @@
 <script lang="typescript">
-    import { Text } from 'shared/components'
+    import { Text, Tooltip } from 'shared/components'
     import Box from './Box.svelte'
 
     export let keyText = ''
@@ -10,21 +10,52 @@
     export let darkBackgroundColor = 'gray-850'
     export let padding = 'px-4 py-3.5'
     export let classes = ''
+
+    let showTooltip = false
+    let tooltipAnchor: HTMLElement
+    let textContainerWidth: number
+
+    $: isValueTextTruncated = (tooltipAnchor?.firstChild as HTMLElement)?.scrollWidth > textContainerWidth
+
+    function toggleTooltip(show: boolean): void {
+        if (isValueTextTruncated) {
+            showTooltip = show
+        }
+    }
 </script>
 
-<Box row clearPadding {backgroundColor} {darkBackgroundColor} classes="justify-between {padding} {classes}">
-    {#if keyText}
-        <Text fontSize="14" lineHeight="5" color={textColor} darkColor={darkTextColor}>
-            {keyText}
-        </Text>
-    {:else}
-        <slot name="key" />
-    {/if}
-    {#if valueText}
-        <Text fontSize="14" lineHeight="5" color={textColor} darkColor={darkTextColor}>
-            {valueText}
-        </Text>
-    {:else}
-        <slot name="value" />
-    {/if}
-</Box>
+<div on:mouseleave={() => toggleTooltip(false)}>
+    <Box row clearPadding {backgroundColor} {darkBackgroundColor} classes="justify-between {padding} {classes}">
+        {#if keyText}
+            <Text fontSize="14" lineHeight="5" color={textColor} darkColor={darkTextColor} classes="mr-4">
+                {keyText}
+            </Text>
+        {:else}
+            <slot name="key" />
+        {/if}
+        {#if valueText}
+            <div
+                on:focus={() => toggleTooltip(true)}
+                on:mouseover={() => toggleTooltip(true)}
+                class="truncate"
+                bind:this={tooltipAnchor}
+                bind:clientWidth={textContainerWidth}
+            >
+                <Text fontSize="14" lineHeight="5" color={textColor} darkColor={darkTextColor} classes="truncate">
+                    {valueText}
+                </Text>
+            </div>
+            {#if showTooltip}
+                <Tooltip anchor={tooltipAnchor} position="top">
+                    <div class="max-h-40 overflow-y-scroll pr-0">
+                        <Text color={textColor} darkColor={darkTextColor} classes="text-left break-words"
+                            >{valueText}</Text
+                        >
+                    </div>
+                </Tooltip>
+            {/if}
+        {:else}
+            <slot name="value" />
+        {/if}
+    </Box>
+</div>
