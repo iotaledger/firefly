000ac948ae74177175058ce279cbc70877dd63f8
Nicole O'Brien <nicole.obrien@iota.org>
2022-07-26 15:30:30 +0100

    fix: tests

--- packages/shared/lib/tests/__mocks__/account.mock.ts
+++ packages/shared/lib/tests/__mocks__/account.mock.ts
@@ -20,7 +20,7 @@ import {
     TransactionOptions,
 } from '@iota/wallet'
 
-import { IAccount } from '@core/account'
+import { IAccount } from '../../core/account'
 
 import { MOCK_ACCOUNT_BALANCE } from './account-balance.mock'
 import { MOCK_ADDRESS } from './address.mock'
@@ -37,6 +37,7 @@ export class AccountMock implements IAccount {
         lockedOutputs: new Set<string>(),
         unspentOutputs: {},
         transactions: {},
+        incomingTransactions: {},
         pendingTransactions: new Set<string>(),
         accountOptions: {
             outputConsolidationThreshold: 0,
