afac9f5f4f9fdaa13ff4ae89a620eb22ebf46744
Jean Ribeiro <iamjeanribeiro@gmail.com>
2022-07-27 11:05:36 -0300

    feat: adds CollectibleDetailsMenu item

---
+++ packages/shared/components/modals/CollectibleDetailsMenu.svelte
@@ -0,0 +1,14 @@
+<script lang="typescript">
+    import { Modal, MenuItem } from 'shared/components'
+
+    export let modal: Modal
+</script>
+
+<Modal bind:this={modal} position={{ top: '100px', right: '60px' }}>
+    <div class="flex flex-col">
+        <MenuItem icon="receive" title="Download" first />
+        <MenuItem icon="profile" title="Set NFT as profile avatar" />
+        <MenuItem icon="export" title="View in IPFS" />
+        <MenuItem icon="delete" title="Burn NFT" />
+    </div>
+</Modal>
