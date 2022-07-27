098699f6d98b58be431be2879fa91b5907daeefd
Nicole O'Brien <nicole.obrien@iota.org>
2022-07-27 10:29:12 +0100

    refactor: abstract faucet url

--- packages/shared/components/atoms/buttons/FaucetRequestButton.svelte
+++ packages/shared/components/atoms/buttons/FaucetRequestButton.svelte
@@ -1,8 +1,9 @@
 <script lang="typescript">
     import { localize } from '@core/i18n'
-    import { nodeInfo } from '@core/network'
+    import { FAUCET_URLS, nodeInfo } from '@core/network'
     import { openPopup } from '@lib/popup'
     import { OnboardingButton } from 'shared/components'
+    import { activeProfile } from '@core/profile'
 
     function handleGetTokens() {
         openPopup({
@@ -11,12 +12,14 @@
     }
 </script>
 
-<OnboardingButton
-    primaryText={localize('actions.faucetRequest', {
-        values: { token: $nodeInfo?.baseToken?.name },
-    })}
-    secondaryText={localize('general.faucetRequestDescription', {
-        values: { network: $nodeInfo?.protocol?.networkName },
-    })}
-    onClick={handleGetTokens}
-/>
+{#if FAUCET_URLS?.[$activeProfile?.networkProtocol]?.[$activeProfile?.networkType]}
+    <OnboardingButton
+        primaryText={localize('actions.faucetRequest', {
+            values: { token: $nodeInfo?.baseToken?.name },
+        })}
+        secondaryText={localize('general.faucetRequestDescription', {
+            values: { network: $nodeInfo?.protocol?.networkName },
+        })}
+        onClick={handleGetTokens}
+    />
+{/if}
