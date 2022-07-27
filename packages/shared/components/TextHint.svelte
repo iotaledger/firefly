feb548dfd4c0943457760842aa63a6e93d392b06
Nicole O'Brien <nicole.obrien@iota.org>
2022-07-27 19:08:18 +0100

    ui: update colours of textHint

--- packages/shared/components/TextHint.svelte
+++ packages/shared/components/TextHint.svelte
@@ -14,7 +14,7 @@
     export let text: string = ''
     export let textClasses: string = ''
     export let textColor = 'gray-700'
-    export let textDarkColor = 'gray-700'
+    export let textDarkColor = 'gray-400'
 
     let _classes
     let _icon
@@ -26,21 +26,21 @@
         } else if (secondary) {
             _classes = 'bg-gray-50 dark:bg-gray-800'
         } else if (success) {
-            _classes = 'bg-green-50 dark:bg-green-50 border-green-500'
+            _classes = 'bg-green-50 dark:bg-green-500'
             _icon = 'checkmark'
-            _iconClasses = 'text-green-500 dark:text-green-500'
+            _iconClasses = 'text-green-700 dark:text-green-700'
         } else if (danger) {
-            _classes = 'bg-red-50 dark:bg-red-50 border-red-500'
+            _classes = 'bg-red-50 dark:bg-red-500'
             _icon = 'success-error'
             _iconClasses = 'text-red-500 dark:text-red-500'
         } else if (warning) {
-            _classes = 'bg-yellow-50 dark:bg-yellow-50 border-yellow-500'
+            _classes = 'bg-yellow-50 dark:bg-yellow-500'
             _icon = 'exclamation'
-            _iconClasses = 'text-yellow-500 dark:text-yellow-500'
+            _iconClasses = 'text-yellow-700 dark:text-yellow-700'
         } else if (info) {
-            _classes = 'bg-blue-50 dark:bg-blue-50 border-blue-500'
+            _classes = 'bg-blue-50 dark:bg-blue-500'
             _icon = 'info'
-            _iconClasses = 'text-blue-500 dark:text-blue-500'
+            _iconClasses = 'text-blue-600 dark:text-blue-600'
         }
     }
 
@@ -48,7 +48,7 @@
 </script>
 
 {#if text}
-    <div class="flex flex-row items-center p-4 rounded-lg border border-solid {_classes} {classes}">
+    <div class="flex flex-row items-center p-4 rounded-lg dark:bg-opacity-10 {_classes} {classes}">
         {#if icon}
             <Icon
                 icon={_icon}
