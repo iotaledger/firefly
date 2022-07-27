6b9a4c2854eaa2ee2c0253aabc64083a402ffa84
Begoña Alvarez <balvarez@boxfish.studio>
2022-07-27 12:52:00 +0200

    feat: move irc30 asset primary color to build action

--- packages/shared/lib/core/wallet/helpers/buildPersistedAssetFromIrc30Metadata.ts
+++ packages/shared/lib/core/wallet/helpers/buildPersistedAssetFromIrc30Metadata.ts
@@ -1,11 +1,13 @@
 import { VerificationStatus } from '../enums'
 import { IIrc30Metadata, IPersistedAsset } from '../interfaces'
+import { getIconColorFromString } from '@core/account'
 
 export function buildPersistedAssetFromIrc30Metadata(
     tokenId: string,
     metadata: IIrc30Metadata,
     verification = VerificationStatus.New
 ): IPersistedAsset {
+    const primaryColor = getIconColorFromString(metadata?.name)
     return {
         id: tokenId,
         standard: 'IRC30',
@@ -14,6 +16,7 @@ export function buildPersistedAssetFromIrc30Metadata(
             unit: metadata.symbol,
             decimals: metadata.decimals,
             useMetricPrefix: false,
+            primaryColor,
             ...(metadata?.description && { description: metadata?.description }),
             ...(metadata?.url && { description: metadata?.url }),
             ...(metadata?.logoUrl && { description: metadata?.logoUrl }),
