9eeff545f7bcc1bffa4d68f42002e0210479837d
Jean Ribeiro <iamjeanribeiro@gmail.com>
2022-07-27 11:09:22 -0300

    refactor: remove pane from wrapping the DetailsView component in Collectibles.svelte

--- packages/shared/routes/dashboard/collectibles/Collectibles.svelte
+++ packages/shared/routes/dashboard/collectibles/Collectibles.svelte
@@ -6,13 +6,13 @@
 
 <div class="w-full h-full flex flex-col flex-nowrap p-10 relative flex-1 bg-gray-50 dark:bg-gray-900">
     <div class="w-full h-full grid grid-cols-1 gap-x-4 min-h-0">
-        <Pane classes="p-6">
-            {#if $collectiblesRoute === CollectiblesRoute.Gallery}
+        {#if $collectiblesRoute === CollectiblesRoute.Gallery}
+            <Pane classes="p-6">
                 <GalleryView />
-            {/if}
-            {#if $collectiblesRoute === CollectiblesRoute.Details}
-                <DetailsView />
-            {/if}
-        </Pane>
+            </Pane>
+        {/if}
+        {#if $collectiblesRoute === CollectiblesRoute.Details}
+            <DetailsView />
+        {/if}
     </div>
 </div>
