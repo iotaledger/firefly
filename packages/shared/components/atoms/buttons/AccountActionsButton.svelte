d8daa2f4002488670b04e6978fc5c0aab73b9e39
Jean Ribeiro <iamjeanribeiro@gmail.com>
2022-07-27 11:04:36 -0300

    refactor: adds KebabButton component

--- packages/shared/components/atoms/buttons/AccountActionsButton.svelte
+++ packages/shared/components/atoms/buttons/AccountActionsButton.svelte
@@ -1,19 +1,11 @@
 <script lang="typescript">
     import { Modal, AccountActionsMenu } from 'shared/components'
+    import { KebabButton } from './'
 
     export let classes = ''
 
     let modal: Modal
 </script>
 
-<button
-    on:click={modal?.toggle}
-    class="px-2 py-3 flex flex-row space-x-1 bg-gray-50 dark:bg-gray-900 text-gray-900 dark:text-white dark:bg-opacity-50 rounded-lg {classes}"
->
-    {#each Array(3) as _}
-        <svg width="4" height="4" viewBox="0 0 4 4">
-            <circle cx="2" cy="2" r="2" class="fill-current" />
-        </svg>
-    {/each}
-</button>
+<KebabButton onClick={modal?.toggle} {classes} />
 <AccountActionsMenu bind:modal />
