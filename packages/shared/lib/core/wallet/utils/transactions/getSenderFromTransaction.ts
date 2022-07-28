380f500f9dc979dd173b782e1605a87f0334fd69
MarkNerdi <mark.nardi@iota.org>
2022-07-28 09:28:49 +0200

    chore: remove unused code

--- packages/shared/lib/core/wallet/utils/transactions/getSenderFromTransaction.ts
+++ packages/shared/lib/core/wallet/utils/transactions/getSenderFromTransaction.ts
@@ -1,24 +1,10 @@
 import { Subject } from '@core/wallet/types'
-import { ITransactionPayload, OutputTypes } from '@iota/types'
-import { getSubjectFromAddress } from '../getSubjectFromAddress'
-import { getRecipientAddressFromOutput, getSenderFromOutput } from '../outputs'
+import { OutputTypes } from '@iota/types'
+import { getSenderFromOutput } from '../outputs'
 
-export function getSenderFromTransaction(
-    isIncoming: boolean,
-    accountAddress: string,
-    output: OutputTypes,
-    transaction: ITransactionPayload
-): Subject {
+export function getSenderFromTransaction(isIncoming: boolean, accountAddress: string, output: OutputTypes): Subject {
     if (isIncoming) {
-        if (transaction) {
-            const remainderOutput = transaction.essence.outputs.find(
-                (output) => accountAddress !== getRecipientAddressFromOutput(output)
-            )
-            const address = getRecipientAddressFromOutput(remainderOutput)
-            return address ? getSubjectFromAddress(address) : getSenderFromOutput(output)
-        } else {
-            return getSenderFromOutput(output)
-        }
+        return getSenderFromOutput(output)
     } else {
         return { type: 'address', address: accountAddress }
     }
