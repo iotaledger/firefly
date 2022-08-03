88d81a17b14418e7307a570b1aecc30944d6f7ae
Nicole O'Brien <nicole.obrien@iota.org>
2022-08-03 10:47:56 +0100

    enhancement: add disable effect to toggle

--- packages/shared/components/inputs/Toggle.svelte
+++ packages/shared/components/inputs/Toggle.svelte
@@ -2,16 +2,26 @@
     export let active = false
     export let classes = ''
     export let color = 'blue'
+    export let disabled = false
 
     export let onClick = (): void => {}
+
+    function handleClick(): void {
+        if (!disabled && onClick) {
+            onClick()
+        }
+    }
 </script>
 
 <toggle
-    on:click={onClick}
+    on:click={handleClick}
     class:active
-    class="relative block w-10 flex-shrink-0 h-6 rounded-full border-solid border border-transparent dark:border-gray-700 {active
-        ? `bg-${color}-500`
-        : 'bg-gray-200 dark:bg-gray-900'} {classes}"
+    disabled
+    class="
+        relative block w-10 flex-shrink-0 h-6 rounded-full border-solid border border-transparent dark:border-gray-700 
+        {disabled ? 'opacity-40' : 'cursor-pointer'}
+        {active ? `bg-${color}-500` : 'bg-gray-200 dark:bg-gray-900'} 
+        {classes}"
 >
     <knob class="absolute top-1/2 transform -translate-y-1/2 left-1 h-4 w-4 rounded-full bg-white dark:bg-gray-800" />
 </toggle>
