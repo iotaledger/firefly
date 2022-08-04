6b70a2e5f7dbfc7e5fbd6100b71a793e6e113dac
Matthew Maxwell <maxwellmattryan@gmail.com>
2022-08-03 21:48:52 -0500

    fix: fix rename mistake

--- packages/shared/lib/core/network/constants/faucet-urls.constant.ts
+++ packages/shared/lib/core/network/constants/faucet-urls.constant.ts
@@ -1,6 +1,6 @@
 import { NetworkProtocol, NetworkType } from '../enums'
 
-export const FAUCET_URL: Readonly<{ [key in NetworkProtocol]?: { [key in NetworkType]?: string } }> = {
+export const FAUCET_URLS: Readonly<{ [key in NetworkProtocol]?: { [key in NetworkType]?: string } }> = {
     [NetworkProtocol.Shimmer]: {
         [NetworkType.Devnet]: 'https://faucet.testnet.shimmer.network/api/enqueue',
     },
