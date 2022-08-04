6852f830efbb7873daa1582294d7d9d13514b594
Nicole O'Brien <nicole.obrien@iota.org>
2022-08-04 10:32:56 +0100

    enhancement: gift storage deposit (#4150)
    * enhancement: add disable effect to toggle
    
    * enhancement: add gift storage deposit to send confirmation page
    
    * enhancement: add gift storage deposit to send deep link
    
    * enhancement: add calculation for gifted vs non gifted storage deposit
    
    * refactor: remove calculateStorageDepositFromOutput
    
    * refactor: return object instead of array in getStorageDepositFromOutput
    
    * fix: resolve pr comments
    
    * chore: removed dead variable
    
    Co-authored-by: Tuditi <daviddetroch@pm.me>

--- packages/shared/lib/core/wallet/utils/outputs/getStorageDepositFromOutput.ts
+++ packages/shared/lib/core/wallet/utils/outputs/getStorageDepositFromOutput.ts
@@ -1,13 +1,24 @@
-import { OutputTypes } from '@iota/types'
+import { OutputTypes, IStorageDepositReturnUnlockCondition } from '@iota/types'
 import { OUTPUT_TYPE_TREASURY, UNLOCK_CONDITION_STORAGE_DEPOSIT_RETURN } from '../../constants'
 
-export function getStorageDepositFromOutput(output: OutputTypes): number {
+export function getStorageDepositFromOutput(output: OutputTypes): {
+    storageDeposit: number
+    giftedStorageDeposit: number
+} {
     if (output && output?.type !== OUTPUT_TYPE_TREASURY) {
-        for (const unlockCondition of output.unlockConditions) {
-            if (unlockCondition?.type === UNLOCK_CONDITION_STORAGE_DEPOSIT_RETURN) {
-                return Number(unlockCondition.amount)
-            }
+        const storageDepositReturnUnlockCondition = <IStorageDepositReturnUnlockCondition>(
+            output?.unlockConditions?.find(
+                (unlockCondition) => unlockCondition?.type === UNLOCK_CONDITION_STORAGE_DEPOSIT_RETURN
+            )
+        )
+        if (storageDepositReturnUnlockCondition) {
+            return { storageDeposit: Number(storageDepositReturnUnlockCondition.amount), giftedStorageDeposit: 0 }
+        } else if (output?.nativeTokens?.length > 0 && Number(output?.amount) > 0) {
+            return { storageDeposit: 0, giftedStorageDeposit: Number(output?.amount) }
+        } else {
+            return { storageDeposit: 0, giftedStorageDeposit: 0 }
         }
+    } else {
+        return { storageDeposit: 0, giftedStorageDeposit: 0 }
     }
-    return 0
 }
