3a79de2d43b2259c42e08f17e08d645f4d7f83b8
MarkNerdi <mark.nardi@iota.org>
2022-08-12 17:20:10 +0200

    refactor: remove unused code

--- packages/shared/lib/core/wallet/utils/transactions/getRelevantOutputFromTransaction.ts
+++ packages/shared/lib/core/wallet/utils/transactions/getRelevantOutputFromTransaction.ts
@@ -7,16 +7,12 @@ export function getRelevantOutputFromTransaction(
     transaction: Transaction,
     accountAddress: string,
     isFoundry: boolean
-): { output: OutputTypes; outputIndex: number; isSelfTransaction: boolean } {
+): { output: OutputTypes; outputIndex: number } {
     const outputs = transaction.payload.essence.outputs
 
     if (isFoundry) {
         const outputIndex = outputs.findIndex((output) => output.type === OUTPUT_TYPE_FOUNDRY)
-        return {
-            output: outputs[outputIndex],
-            outputIndex: outputIndex,
-            isSelfTransaction: false,
-        }
+        return { output: outputs[outputIndex], outputIndex: outputIndex }
     }
 
     const nonRemainerOutputIndex = outputs.findIndex((output) => {
@@ -32,14 +28,13 @@ export function getRelevantOutputFromTransaction(
         return {
             output: outputs[nonRemainerOutputIndex],
             outputIndex: nonRemainerOutputIndex,
-            isSelfTransaction: false,
         }
     } else {
         if (outputs.every((output) => accountAddress === getRecipientAddressFromOutput(output))) {
             const outputIndex = outputs.findIndex((output) => accountAddress === getRecipientAddressFromOutput(output))
 
-            return { output: outputs[outputIndex], outputIndex: outputIndex, isSelfTransaction: true }
+            return { output: outputs[outputIndex], outputIndex: outputIndex }
         }
     }
-    return { output: undefined, outputIndex: undefined, isSelfTransaction: undefined }
+    return { output: undefined, outputIndex: undefined }
 }
