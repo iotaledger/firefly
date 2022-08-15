fb7d27865bca88267db5820f01e2e18a82d7ef4c
Matthew Maxwell <maxwellmattryan@gmail.com>
2022-08-14 20:16:10 -0500

    fix: adjust onboarding completion logic

--- packages/shared/lib/contexts/onboarding/actions/completeOnboardingProcess.ts
+++ packages/shared/lib/contexts/onboarding/actions/completeOnboardingProcess.ts
@@ -12,15 +12,18 @@ import { cleanupOnboarding } from './cleanupOnboarding'
 export async function completeOnboardingProcess(): Promise<void> {
     addOnboardingProfile()
 
-    /**
-     * NOTE: If recovering from a Stronghold that already contains
-     * at least one account, calling this function will cause
-     * an "AccountAliasAlreadyExists" error from wallet-rs. This
-     * error does NOT cause side-effects while creating a profile.
-     */
-    await createNewAccount()
+    const shouldCreateNewAccount = get(onboardingProfile)?.setupType !== ProfileSetupType.Claimed
+    if (shouldCreateNewAccount) {
+        /**
+         * NOTE: If recovering from a Stronghold that already contains
+         * at least one account, calling this function will cause
+         * an "AccountAliasAlreadyExists" error from wallet-rs. This
+         * error does NOT cause side-effects while creating a profile.
+         */
+        await createNewAccount()
+    }
 
-    const recoverAccounts = get(onboardingProfile)?.setupType !== ProfileSetupType.New
+    const recoverAccounts = get(onboardingProfile)?.setupType === ProfileSetupType.Recovered
     void login(recoverAccounts)
 
     void cleanupOnboarding()
