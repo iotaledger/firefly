c9aa57302f7e5c9a7db93c699e9c44abf9ce342a
Nicole O'Brien <nicole.obrien@iota.org>
2022-08-15 18:23:26 +0100

    refactor: split up account loading into separate functions

--- packages/shared/lib/core/profile/actions/active-profile/loadAccounts.ts
+++ packages/shared/lib/core/profile/actions/active-profile/loadAccounts.ts
@@ -1,13 +1,11 @@
-import { loadAccount, setSelectedAccount } from '@core/account'
+import { loadAccount } from '@core/account'
 import { getAccounts } from '@core/profile-manager'
-import { loadAccountActivities } from '@core/wallet'
-import { refreshAccountAssetsForActiveProfile } from '@core/wallet/actions/refreshAccountAssetsForActiveProfile'
 import { get } from 'svelte/store'
 import { activeAccounts, activeProfile } from '../../stores'
 
 export async function loadAccounts(): Promise<void> {
     try {
-        const { hasLoadedAccounts, lastUsedAccountId } = get(activeProfile)
+        const { hasLoadedAccounts } = get(activeProfile)
         const accountsResponse = await getAccounts()
         if (accountsResponse.length === 0) {
             hasLoadedAccounts.set(true)
@@ -18,9 +16,6 @@ export async function loadAccounts(): Promise<void> {
                 accountsResponse?.map((accountResponse) => loadAccount(accountResponse))
             )
             activeAccounts.set(loadedAccounts.sort((a, b) => a.meta.index - b.meta.index))
-            setSelectedAccount(lastUsedAccountId ?? get(activeAccounts)?.[0]?.id ?? null)
-            await refreshAccountAssetsForActiveProfile()
-            await Promise.all(get(activeAccounts)?.map((activeAccount) => loadAccountActivities(activeAccount)))
             hasLoadedAccounts.set(true)
         }
     } catch (err) {
