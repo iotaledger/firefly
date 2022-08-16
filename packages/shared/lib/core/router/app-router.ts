87bf16f71642120390d074c09059b9f1ac5c170d
Nicole O'Brien <nicole.obrien@iota.org>
2022-08-16 19:07:08 +0100

    feat: go to load profile view after onboarding
    Co-authored-by: Jason Kraft <jason.kraft@iota.org>
    Co-authored-by: Matthew Maxwell <maxwellmattryan@users.noreply.github.com>

--- packages/shared/lib/core/router/app-router.ts
+++ packages/shared/lib/core/router/app-router.ts
@@ -2,9 +2,10 @@ import { get, writable } from 'svelte/store'
 
 import { profiles } from '@core/profile'
 
-import { AppRoute } from './enums'
+import { AppRoute, LoginRoute } from './enums'
 import { Router } from './router'
 import { FireflyEvent } from './types'
+import { loginRoute } from './subrouters'
 
 export const appRoute = writable<AppRoute>(null)
 export const appRouter = writable<AppRouter>(null)
@@ -46,7 +47,8 @@ export class AppRouter extends Router<AppRoute> {
                 break
             }
             case AppRoute.Onboarding: {
-                nextRoute = AppRoute.Dashboard
+                loginRoute.set(LoginRoute.LoadProfile)
+                nextRoute = AppRoute.Login
                 break
             }
         }
