bcd6b050c8c34ae76f558b2a3e777a70788a9e38
Matthew Maxwell <maxwellmattryan@gmail.com>
2022-08-16 15:08:40 -0500

    feat: get stronghold restoration working for both profile managers

--- packages/shared/lib/contexts/onboarding/actions/restoreBackupForShimmerClaimingProfileManager.ts
+++ packages/shared/lib/contexts/onboarding/actions/restoreBackupForShimmerClaimingProfileManager.ts
@@ -1,17 +1,17 @@
 import { get } from 'svelte/store'
 
 import { COIN_TYPE, NetworkProtocol } from '@core/network'
+import { getStorageDirectoryOfProfile } from '@core/profile'
+import { getSecretManagerPath, profileManager } from '@core/profile-manager'
+import { Platform } from '@lib/platform'
 
 import { CannotRestoreWithMismatchedCoinTypeError } from '../errors'
 import { onboardingProfile, shimmerClaimingProfileManager } from '../stores'
 
 export async function restoreBackupForShimmerClaimingProfileManager(strongholdPassword: string): Promise<void> {
-    const { importFilePath } = get(onboardingProfile)
+    const { id, importFilePath } = get(onboardingProfile)
 
     await get(shimmerClaimingProfileManager)?.restoreBackup(importFilePath, strongholdPassword)
-
-    // copy stronghold to profile managers' paths (before any accounts have been loaded)
-    // set stronghold password afterwards
 
     /**
      * NOTE: We must check that the Stronghold was an IOTA-based backup and
@@ -21,4 +21,12 @@ export async function restoreBackupForShimmerClaimingProfileManager(strongholdPa
     if (accounts?.length > 0 && accounts[0]?.meta?.coinType !== COIN_TYPE[NetworkProtocol.IOTA]) {
         throw new CannotRestoreWithMismatchedCoinTypeError()
     }
+
+    // copy stronghold to profile managers' paths (before any accounts have been loaded, it is possible for 0 accounts to be "loaded")
+    const profileDirectory = await getStorageDirectoryOfProfile(id)
+    const secretManagerPath = getSecretManagerPath(profileDirectory)
+    await Platform.copyFile(importFilePath, secretManagerPath)
+
+    // set stronghold password afterwards
+    await get(profileManager)?.setStrongholdPassword(strongholdPassword)
 }
