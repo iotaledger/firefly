ea0e2ecbbe437f361f2a4d2d523d1f217fa7f2fa
Nicole O'Brien <nicole.obrien@iota.org>
2022-08-23 09:07:35 +0100

    fix: interface

--- packages/shared/lib/core/wallet/interfaces/send-confirmation-parameters.interface.ts
+++ packages/shared/lib/core/wallet/interfaces/send-confirmation-parameters.interface.ts
@@ -8,4 +8,6 @@ export interface ISendConfirmationParameters {
     metadata?: string
     tag?: string
     giftStorageDeposit?: boolean
+    disableToggleGift?: boolean
+    disableChangeExpiration?: boolean
 }
