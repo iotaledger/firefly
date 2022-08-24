4509a0c41f3141b07386517ca88e7e2ff6eec6d2
Matthew Maxwell <maxwellmattryan@gmail.com>
2022-08-24 10:17:20 -0500

    chore: change some var names

--- packages/shared/lib/core/account/utils/sumTotalFromOutputs.ts
+++ packages/shared/lib/core/account/utils/sumTotalFromOutputs.ts
@@ -1,5 +1,5 @@
 import { OutputData } from '@iota/wallet/types/output'
 
 export function sumTotalFromOutputs(outputs: OutputData[]): number {
-    return outputs?.reduce((total: number, curr: OutputData) => (total += Number(curr?.output?.amount)), 0)
+    return outputs?.reduce((total: number, outputData: OutputData) => (total += Number(outputData?.output?.amount)), 0)
 }
