8296bd42e8204320cea82a17ce9d1fac4c846dc1
Matthew Maxwell <maxwellmattryan@gmail.com>
2022-08-24 10:12:22 -0500

    fix: add missing locale entry

--- packages/shared/lib/core/profile/errors/unable-to-find-profile-setup-type.error.ts
+++ packages/shared/lib/core/profile/errors/unable-to-find-profile-setup-type.error.ts
@@ -3,7 +3,7 @@ import { BaseError, DEFAULT_APP_ERROR_PARAMETERS } from '@core/error'
 export class UnableToFindProfileSetupTypeError extends BaseError {
     constructor() {
         super({
-            message: '',
+            message: 'error.profile.setupType',
             ...DEFAULT_APP_ERROR_PARAMETERS,
         })
     }
