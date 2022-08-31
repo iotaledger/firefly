389b5638f7c615f501a783f914c087f88b012f65
Jean Ribeiro <iamjeanribeiro@gmail.com>
2022-08-31 11:22:42 -0300

    fix: disable keyboard autofocus on setup (#4372)
    * fix: disable keyboard autofocus on setup
    
    * fix: dont autofocus on repeat pin
    
    Co-authored-by: Charlie Varley <charlie.varley@iota.org>

--- packages/shared/routes/setup/backup/views/Backup.svelte
+++ packages/shared/routes/setup/backup/views/Backup.svelte
@@ -27,7 +27,8 @@
         <Text type="p" secondary highlighted classes="mb-4 font-bold">{locale('views.backup.body3')}</Text>
     </div>
     <div slot="leftpane__action">
-        <Button classes="w-full" onClick={() => handleContinueClick()} autofocus>{locale('actions.continue')}</Button>
+        <Button classes="w-full" onClick={handleContinueClick} autofocus={!$mobile}>{locale('actions.continue')}</Button
+        >
     </div>
     <div slot="rightpane" class="w-full h-full flex justify-center {!$mobile && 'bg-pastel-purple dark:bg-gray-900'}">
         <Animation
