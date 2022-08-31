a8b9b202dadf03b8d61c56765fd19a2369b30859
Tuditi <daviddetroch@pm.me>
2022-08-31 16:55:27 +0200

    chore: refactored destroyProfileManager

--- packages/shared/lib/core/profile-manager/actions/destroyProfileManager.ts
+++ packages/shared/lib/core/profile-manager/actions/destroyProfileManager.ts
@@ -1,14 +1,13 @@
-import { get, Writable } from 'svelte/store'
+import { get } from 'svelte/store'
 
 import { api } from '../api'
-import { IProfileManager } from '../interfaces'
 import { profileManager } from '../stores'
 
-export function destroyProfileManager(_profileManager: Writable<IProfileManager> = profileManager): void {
+export function destroyProfileManager(_profileManager = profileManager): void {
     const manager = get(_profileManager)
-    if (!manager) return
-
-    _profileManager.set(null)
-    api.deleteAccountManager(manager?.id)
-    manager.destroy()
+    if (manager) {
+        _profileManager.set(null)
+        api.deleteAccountManager(manager?.id)
+        manager.destroy()
+    }
 }
