c4f69a1c3a5fd5aa61312e3140e62aafc2354c56
Tuditi <daviddetroch@pm.me>
2022-08-31 11:19:39 +0200

    chore: remove deprecated allowRendererProcessReuse

--- packages/desktop/electron/main.js
+++ packages/desktop/electron/main.js
@@ -373,8 +373,6 @@ export const getOrInitWindow = (windowName) => {
  */
 initMenu()
 
-app.allowRendererProcessReuse = false
-
 // Quit when all windows are closed.
 app.on('window-all-closed', () => {
     // On macOS it is common for applications and their menu bar
