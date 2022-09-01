2e8e1fd30e7016010d2dc2b971545936af8b5555
MarkNerdi <105642810+MarkNerdi@users.noreply.github.com>
2022-09-01 22:18:39 +0200

    refactor: activity hiding step as preprocessing (#4337)
    * refactor: activity data types for transactions and foundries
    
    * refactor: fix claiming
    
    * update: activity detail popup
    
    * refactor: extract into own files
    
    * refactor: fixes
    
    * refactor: fix activity popup
    
    * fix: fix claim event for outgoing transactions
    
    * Fix: activity details popup
    
    * temp: refactors for ensuring type in type
    
    * refactor: own file per interface + use enum
    
    * refactor: split transaction and foundry tiles
    
    * refactor: hide claiming activities before generating them
    
    * chore: remove comment
    
    * refactor: improve naming
    
    * refactor: complexity improvement for linking transactions
    
    * refactor: naming
    
    * chore: add jsdoc comment to linkTransactionsWithClaimingTransactions
    
    * fix: only hide claiming transactions for incoming asyncs
    
    * fix: casing of function name
    
    Co-authored-by: Nicole O'Brien <nicole.obrien@iota.org>

--- packages/shared/lib/core/wallet/actions/activities/generateAndStoreActivitiesForAccount.ts
+++ packages/shared/lib/core/wallet/actions/activities/generateAndStoreActivitiesForAccount.ts
@@ -2,7 +2,7 @@ import { IAccountState } from '@core/account'
 import { setOutgoingAsyncActivitiesToClaimed } from '../setOutgoingAsyncActivitiesToClaimed'
 import { preprocessTransactionsForAccount } from './preprocessTransactionsForAccount'
 import { preprocessOutputsForAccount } from './preprocessOutputsForAccount'
-import { linkActivityAndClaimingTransaction } from './linkActivityAndClaimingTransaction'
+import { linkTransactionsWithClaimingTransactions } from './linkTransactionsWithClaimingTransactions'
 import { hideActivitiesForFoundries } from './hideActivitiesForFoundries'
 import { generateActivitiesFromProcessedTransactions } from './generateActivitiesFromProcessedTransactions'
 import { setAccountActivitiesInAllAccountActivities } from '@core/wallet/stores'
@@ -14,13 +14,15 @@ export async function generateAndStoreActivitiesForAccount(account: IAccountStat
         ...preprocessOutputsForAccount(account),
     ]
 
-    // Step 2: generate activities from processed transactions
-    const activities = generateActivitiesFromProcessedTransactions(processedTransactions, account)
+    // Step 2: link transactions with corresponding claiming transactions
+    const linkedProcessedTransactions = linkTransactionsWithClaimingTransactions(processedTransactions, account)
 
-    // Step 3: set account activities with generated activities
+    // Step 3: generate activities from processed transactions
+    const activities = generateActivitiesFromProcessedTransactions(linkedProcessedTransactions, account)
+
+    // Step 4: set account activities with generated activities
     setAccountActivitiesInAllAccountActivities(account.id, activities)
 
     hideActivitiesForFoundries(account)
     await setOutgoingAsyncActivitiesToClaimed(account)
-    linkActivityAndClaimingTransaction(account)
 }
---
+++ packages/shared/lib/core/wallet/utils/transactions/getDirectionFromTransaction.ts
@@ -0,0 +1,15 @@
+import { IProcessedTransaction } from '@core/wallet/interfaces'
+import { ActivityDirection } from '@core/wallet/enums/activity-direction.enum'
+import { getMainTransactionOutputFromTransaction } from './getMainTransactionOutputFromTransaction'
+
+export function getDirectionFromTransaction(
+    transaction: IProcessedTransaction,
+    accountAddress: string
+): ActivityDirection {
+    const { outputs, isIncoming } = transaction
+
+    const { isSelfTransaction } = getMainTransactionOutputFromTransaction(outputs, accountAddress, isIncoming)
+
+    const direction = isIncoming || isSelfTransaction ? ActivityDirection.In : ActivityDirection.Out
+    return direction
+}
