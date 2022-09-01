1b055ba8a24265eb4ea5cc232b5ceefcd75e210f
Matthew Maxwell <maxwellmattryan@gmail.com>
2022-09-01 10:24:27 -0500

    fix: add explicit error handling

--- packages/shared/lib/core/profile-manager/api/events/transaction-progress.event.ts
+++ packages/shared/lib/core/profile-manager/api/events/transaction-progress.event.ts
@@ -3,7 +3,11 @@ import { get } from 'svelte/store'
 import { selectedAccountId } from '@core/account'
 import { ledgerDeviceStatus, openLedgerConfirmationPopup } from '@core/ledger'
 import { isLedgerProfile } from '@core/profile'
-import { isPreparedTransaction, isPreparedTransactionEssenceHash } from '@core/profile-manager'
+import {
+    MissingTransactionProgressEventPayloadError,
+    isPreparedTransaction,
+    isPreparedTransactionEssenceHash,
+} from '@core/profile-manager'
 import { closePopup, openPopup } from '@lib/popup'
 
 import { TransactionProgressEventPayload } from '../types'
@@ -22,21 +26,25 @@ function handleTransactionProgressInternal(
     payload: TransactionProgressEventPayload,
     isDuringOnboarding?: boolean
 ): void {
-    if (payload === 'PerformingPow') {
-        /**
-         * NOTE: This is necessary so that the transaction
-         * confirmation popup is closed since we can assume
-         * the user confirmed the prompt on the actual Ledger
-         * device.
-         */
-        closePopup(true)
-    } else if (isPreparedTransaction(payload)) {
-        openLedgerConfirmationPopup(payload, isDuringOnboarding)
-    } else if (isPreparedTransactionEssenceHash(payload)) {
-        if (get(ledgerDeviceStatus)?.blindSigningEnabled) {
+    if (payload) {
+        if (payload === 'PerformingPow') {
+            /**
+             * NOTE: This is necessary so that the transaction
+             * confirmation popup is closed since we can assume
+             * the user confirmed the prompt on the actual Ledger
+             * device.
+             */
+            closePopup(true)
+        } else if (isPreparedTransaction(payload)) {
             openLedgerConfirmationPopup(payload, isDuringOnboarding)
-        } else {
-            openPopup({ type: 'enableLedgerBlindSigning' })
+        } else if (isPreparedTransactionEssenceHash(payload)) {
+            if (get(ledgerDeviceStatus)?.blindSigningEnabled) {
+                openLedgerConfirmationPopup(payload, isDuringOnboarding)
+            } else {
+                openPopup({ type: 'enableLedgerBlindSigning' })
+            }
         }
+    } else {
+        throw new MissingTransactionProgressEventPayloadError()
     }
 }
