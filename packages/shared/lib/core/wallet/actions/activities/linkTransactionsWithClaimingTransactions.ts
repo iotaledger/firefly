6755b8ea4f6c1f22ea084bd472c8c563b4bcf923
MarkNerdi <mark.nardi@iota.org>
2022-09-01 13:37:21 +0200

    fix: only hide claiming transactions for incoming asyncs

--- packages/shared/lib/core/wallet/actions/activities/linkTransactionsWithClaimingTransactions.ts
+++ packages/shared/lib/core/wallet/actions/activities/linkTransactionsWithClaimingTransactions.ts
@@ -1,5 +1,7 @@
 import { IAccountState } from '@core/account'
+import { ActivityDirection } from '@core/wallet/enums'
 import { IProcessedTransaction } from '@core/wallet/interfaces'
+import { getDirectionFromTransaction } from '@core/wallet/utils'
 import { isOutputAsync } from '@core/wallet/utils/outputs/isOutputAsync'
 import { get } from 'svelte/store'
 import { addClaimedActivity, claimedActivities } from '../../stores'
@@ -20,12 +22,16 @@ export function linkTransactionsWithClaimingTransactions(
 
     const claimedAccountActivities = get(claimedActivities)?.[account.id]
     const sortedTransactions = transactions.sort((t1, t2) => (t1.time > t2.time ? 1 : -1))
-    const asyncTransactions = []
+    const incomingAsyncTransactions = []
 
     for (const transaction of sortedTransactions) {
         if (transactionsIncludedAsClaimingTransactions.includes(transaction.transactionId)) {
             continue
-        } else if (transaction.outputs.some((_output) => isOutputAsync(_output))) {
+        } else if (
+            transaction.outputs.some((_output) => isOutputAsync(_output)) &&
+            getDirectionFromTransaction(transaction, account.depositAddress) === ActivityDirection.In
+        ) {
+            // If we have the corresponding claiming transaction cached in local storage, we get that data and update the async transaction
             const claimedActivity = claimedAccountActivities?.[transaction.transactionId]
             if (claimedActivity && claimedActivity.claimingTransactionId === transaction.transactionId) {
                 const claimingData = {
@@ -35,11 +41,17 @@ export function linkTransactionsWithClaimingTransactions(
                 transaction.claimingData = claimingData
                 transactionsIncludedAsClaimingTransactions.push(claimingData.claimingTransactionId)
             } else {
-                asyncTransactions.push(transaction)
+                // Else we store the async transaction in a list, which we later use to check if a transaction is the claiming transaction for another one
+                incomingAsyncTransactions.push(transaction)
             }
             resultingTransactions.push(transaction)
         } else {
-            const claimedTransaction = searchClaimedTransactionInAsyncTransactions(asyncTransactions, transaction)
+            // For 'normal' transactions we search through the async transactions to check if one is the claiming transaction from the other one
+            // If we find a match, we update the async transaction and ignore the current one
+            const claimedTransaction = searchClaimedTransactionInincomingAsyncTransactions(
+                incomingAsyncTransactions,
+                transaction
+            )
 
             if (claimedTransaction) {
                 claimedTransaction.claimingData = {
@@ -61,7 +73,7 @@ export function linkTransactionsWithClaimingTransactions(
     return resultingTransactions
 }
 
-function searchClaimedTransactionInAsyncTransactions(
+function searchClaimedTransactionInincomingAsyncTransactions(
     allAsyncTransaction: IProcessedTransaction[],
     transaction: IProcessedTransaction
 ): IProcessedTransaction {
