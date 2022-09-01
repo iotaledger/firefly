1131f9d9ccc3e1500eb21a5808639dbf70b19308
Matthew Maxwell <maxwellmattryan@gmail.com>
2022-09-01 13:42:20 -0500

    fix: add five seconds to sleep time

--- packages/shared/lib/contexts/onboarding/actions/claimShimmerRewards.ts
+++ packages/shared/lib/contexts/onboarding/actions/claimShimmerRewards.ts
@@ -103,7 +103,7 @@ async function claimShimmerRewardsForShimmerClaimingAccount(
     persistShimmerClaimingTransaction(claimingTransaction?.transactionId)
 
     // TODO: https://github.com/iotaledger/firefly/issues/4223
-    await sleep(SECONDS_PER_MILESTONE * MILLISECONDS_PER_SECOND)
+    await sleep((5 + SECONDS_PER_MILESTONE) * MILLISECONDS_PER_SECOND)
 
     const syncedShimmerClaimingAccount = await prepareShimmerClaimingAccount(
         shimmerClaimingAccount,
