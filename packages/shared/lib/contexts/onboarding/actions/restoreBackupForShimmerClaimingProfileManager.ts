76e7b02afbe92871b1f750c0fe9aa7327f4a11eb
Matthew Maxwell <maxwellmattryan@gmail.com>
2022-09-01 15:45:34 -0500

    fix: add file copying to logic

--- packages/shared/lib/contexts/onboarding/actions/restoreBackupForShimmerClaimingProfileManager.ts
+++ packages/shared/lib/contexts/onboarding/actions/restoreBackupForShimmerClaimingProfileManager.ts
@@ -1,14 +1,28 @@
-import { get } from 'svelte/store'
+import { get, Writable } from 'svelte/store'
 
 import { NetworkProtocol } from '@core/network'
-import { profileManager } from '@core/profile-manager'
+import { IProfileManager, profileManager } from '@core/profile-manager'
 
-import { copyStrongholdFileToProfileDirectory, validateStrongholdCoinType } from '../helpers'
+import {
+    copyStrongholdFileToProfileDirectory,
+    getShimmerClaimingProfileManagerStorageDirectory,
+    validateStrongholdCoinType,
+} from '../helpers'
 import { onboardingProfile, shimmerClaimingProfileManager } from '../stores'
+import { getStorageDirectoryOfProfile } from '@core/profile'
+import { ClientOptions } from '@iota/wallet'
 
 export async function restoreBackupForShimmerClaimingProfileManager(strongholdPassword: string): Promise<void> {
     const { id, importFilePath, clientOptions } = get(onboardingProfile)
-    await get(shimmerClaimingProfileManager)?.restoreBackup(importFilePath, strongholdPassword)
+
+    const tempProfileDirectory = await getShimmerClaimingProfileManagerStorageDirectory()
+    await restoreBackupForShimmerClaimingProfileManagerHelper(
+        importFilePath,
+        tempProfileDirectory,
+        strongholdPassword,
+        clientOptions,
+        shimmerClaimingProfileManager
+    )
 
     /**
      * NOTE: We must check that the Stronghold was an IOTA-based backup and
@@ -16,7 +30,24 @@ export async function restoreBackupForShimmerClaimingProfileManager(strongholdPa
      */
     await validateStrongholdCoinType(get(shimmerClaimingProfileManager), NetworkProtocol.IOTA)
 
-    await copyStrongholdFileToProfileDirectory(id, importFilePath)
-    await get(profileManager)?.setStrongholdPassword(strongholdPassword)
-    await get(profileManager)?.setClientOptions(clientOptions)
+    const profileDirectory = await getStorageDirectoryOfProfile(id)
+    await restoreBackupForShimmerClaimingProfileManagerHelper(
+        importFilePath,
+        profileDirectory,
+        strongholdPassword,
+        clientOptions,
+        profileManager
+    )
+}
+
+async function restoreBackupForShimmerClaimingProfileManagerHelper(
+    importFilePath: string,
+    storageDirectory: string,
+    strongholdPassword: string,
+    clientOptions: ClientOptions,
+    manager: Writable<IProfileManager>
+): Promise<void> {
+    await copyStrongholdFileToProfileDirectory(storageDirectory, importFilePath)
+    await get(manager)?.setStrongholdPassword(strongholdPassword)
+    await get(manager)?.setClientOptions(clientOptions)
 }
--- packages/shared/lib/contexts/onboarding/helpers/copyStrongholdFileToProfileDirectory.ts
+++ packages/shared/lib/contexts/onboarding/helpers/copyStrongholdFileToProfileDirectory.ts
@@ -1,9 +1,10 @@
-import { getStorageDirectoryOfProfile } from '@core/profile'
 import { getSecretManagerPath } from '@core/profile-manager'
 import { Platform } from '@lib/platform'
 
-export async function copyStrongholdFileToProfileDirectory(profileId: string, importFilePath: string): Promise<void> {
-    const profileDirectory = await getStorageDirectoryOfProfile(profileId)
+export async function copyStrongholdFileToProfileDirectory(
+    profileDirectory: string,
+    importFilePath: string
+): Promise<void> {
     const secretManagerPath = getSecretManagerPath(profileDirectory)
     await Platform.copyFile(importFilePath, secretManagerPath)
 }
