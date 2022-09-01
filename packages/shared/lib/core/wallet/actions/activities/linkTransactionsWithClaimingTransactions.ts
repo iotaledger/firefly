2e77f2aed2790b981e2c34d3fffcf501b2028d37
MarkNerdi <mark.nardi@iota.org>
2022-09-01 11:22:21 +0200

    refactor: complexity improvement for linking transactions

--- packages/shared/lib/core/wallet/actions/activities/linkTransactionsWithClaimingTransactions.ts
+++ packages/shared/lib/core/wallet/actions/activities/linkTransactionsWithClaimingTransactions.ts
@@ -12,56 +12,54 @@ export function linkTransactionsWithClaimingTransactions(
     const transactionsIncludedAsClaimingTransactions = []
 
     const claimedAccountActivities = get(claimedActivities)?.[account.id]
+    const sortedTransactions = transactions.sort((t1, t2) => (t1.time > t2.time ? 1 : -1))
+    const asyncTransactions = []
 
-    for (const transaction of transactions.sort((t1, t2) => (t1.time > t2.time ? 1 : -1))) {
+    for (const transaction of sortedTransactions) {
         if (transactionsIncludedAsClaimingTransactions.includes(transaction.transactionId)) {
             continue
-        } else if (transaction.outputs.some((_output) => !isOutputAsync(_output))) {
-            resultingTransactions.push(transaction)
-        } else {
-            let claimingData = undefined
-
+        } else if (transaction.outputs.some((_output) => isOutputAsync(_output))) {
             const claimedActivity = claimedAccountActivities?.[transaction.transactionId]
             if (claimedActivity && claimedActivity.claimingTransactionId === transaction.transactionId) {
-                claimingData = {
+                const claimingData = {
                     claimedDate: new Date(claimedActivity.claimedTimestamp),
                     claimingTransactionId: claimedActivity.transactionId,
                 }
-            } else {
-                const claimingTransaction = searchClaimingTransactionInAllTransactions(transactions, transaction)
-
-                if (claimingTransaction) {
-                    claimingData = {
-                        claimedDate: claimingTransaction.time,
-                        claimingTransactionId: claimingTransaction.transactionId,
-                    }
-
-                    addClaimedActivity(account.id, transaction.transactionId, {
-                        id: transaction.transactionId,
-                        claimedTimestamp: claimingTransaction.time.getTime(),
-                        claimingTransactionId: claimingTransaction.transactionId,
-                    })
-                }
-            }
-            if (claimingData) {
                 transaction.claimingData = claimingData
                 transactionsIncludedAsClaimingTransactions.push(claimingData.claimingTransactionId)
+            } else {
+                asyncTransactions.push(transaction)
             }
             resultingTransactions.push(transaction)
+        } else {
+            const claimedTransaction = searchClaimedTransactionInAllTransactions(asyncTransactions, transaction)
+
+            if (claimedTransaction) {
+                claimedTransaction.claimingData = {
+                    claimedDate: transaction.time,
+                    claimingTransactionId: transaction.transactionId,
+                }
+
+                addClaimedActivity(account.id, transaction.transactionId, {
+                    id: transaction.transactionId,
+                    claimedTimestamp: transaction.time.getTime(),
+                    claimingTransactionId: transaction.transactionId,
+                })
+            } else {
+                resultingTransactions.push(transaction)
+            }
         }
     }
 
     return resultingTransactions
 }
 
-function searchClaimingTransactionInAllTransactions(
-    allTransaction: IProcessedTransaction[],
+function searchClaimedTransactionInAllTransactions(
+    allAsyncTransaction: IProcessedTransaction[],
     transaction: IProcessedTransaction
 ): IProcessedTransaction {
     // TODO: add as many restrictions for candidates to optimize the time
-    return allTransaction.find(
-        (candidate) =>
-            candidate.time > transaction.time &&
-            candidate.transactionInputs?.some((input) => input.transactionId === transaction.transactionId)
+    return allAsyncTransaction.find((candidate) =>
+        transaction.transactionInputs?.some((input) => input.transactionId === candidate.transactionId)
     )
 }
