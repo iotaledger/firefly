c4a765d0ff3b735dd903ca90513129587d62e03e
Jean Ribeiro <iamjeanribeiro@gmail.com>
2022-09-02 10:55:13 -0300

    fix: adds mobile conditional in smaller Texts in ChangePinCode

--- packages/shared/routes/dashboard/settings/views/security/ChangePincode.svelte
+++ packages/shared/routes/dashboard/settings/views/security/ChangePincode.svelte
@@ -98,7 +98,7 @@
 >
     <Text type="h4" classes="mb-3">{localize('views.settings.changePincode.title')}</Text>
     <Text type="p" secondary classes="mb-5">{localize('views.settings.changePincode.description')}</Text>
-    <Text type="p" secondary smaller classes="mb-2">{localize('views.settings.changePincode.currentPincode')}</Text>
+    <Text type="p" secondary smaller={!$mobile} classes="mb-2">{localize('views.settings.changePincode.currentPincode')}</Text>
     <Pin
         smaller
         error={currentPincodeError}
@@ -107,7 +107,7 @@
         disabled={pinCodeBusy}
         on:submit={changePincode}
     />
-    <Text type="p" secondary smaller classes="mb-2">{localize('views.settings.changePincode.newPincode')}</Text>
+    <Text type="p" secondary smaller={!$mobile} classes="mb-2">{localize('views.settings.changePincode.newPincode')}</Text>
     <Pin
         smaller
         error={newPincodeError}
@@ -116,7 +116,7 @@
         disabled={pinCodeBusy}
         on:submit={changePincode}
     />
-    <Text type="p" secondary smaller classes="mb-2">{localize('views.settings.changePincode.confirmNewPincode')}</Text>
+    <Text type="p" secondary smaller={!$mobile} classes="mb-2">{localize('views.settings.changePincode.confirmNewPincode')}</Text>
     <Pin
         smaller
         error={confirmationPincodeError}
