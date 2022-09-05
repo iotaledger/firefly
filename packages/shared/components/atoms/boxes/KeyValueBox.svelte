89b8e889cf32f8b0b9c49c66d556357a59dbddd8
Nicole O'Brien <nicole.obrien@iota.org>
2022-09-05 12:59:16 +0100

    enhancement: add scroll to key value boc

--- packages/shared/components/atoms/boxes/KeyValueBox.svelte
+++ packages/shared/components/atoms/boxes/KeyValueBox.svelte
@@ -1,5 +1,5 @@
 <script lang="typescript">
-    import { Text, Tooltip } from 'shared/components'
+    import { Text } from 'shared/components'
     import { FontWeight } from 'shared/components/Text.svelte'
     import { onMount } from 'svelte'
     import CopyableBox from './CopyableBox.svelte'
@@ -16,22 +16,15 @@
     export let isCopyable: boolean = false
 
     let isVertical: boolean = false
-    let showTooltip = false
-    let tooltipAnchor: HTMLElement
-    let textContainerWidth: number
-
-    function toggleTooltip(show: boolean): void {
-        if (isVertical && !isCopyable) {
-            showTooltip = show
-        }
-    }
+    let valueContainer: HTMLElement
+    let valueContainerWidth: number
 
     onMount(() => {
-        isVertical = (tooltipAnchor?.firstChild as HTMLElement)?.scrollWidth > textContainerWidth
+        isVertical = (valueContainer?.firstChild as HTMLElement)?.scrollWidth > valueContainerWidth
     })
 </script>
 
-<div on:mouseleave={() => toggleTooltip(false)} class="w-full">
+<div class="w-full">
     <CopyableBox
         value={copyValue ? copyValue : valueText}
         {isCopyable}
@@ -40,50 +33,45 @@
         col={isVertical}
         {backgroundColor}
         {darkBackgroundColor}
-        classes="w-full text-left {padding} {classes} justify-between {isVertical ? 'space-y-1' : 'space-x-2 '}"
+        classes="w-full overflow-hidden {padding} {classes}"
     >
-        {#if keyText}
-            <Text
-                fontSize="14"
-                lineHeight="5"
-                color={textColor}
-                darkColor={darkTextColor}
-                fontWeight={FontWeight.medium}
-            >
-                {keyText}
-            </Text>
-        {:else}
-            <slot name="key" />
-        {/if}
-        {#if valueText}
-            <div
-                on:focus={() => toggleTooltip(true)}
-                on:mouseover={() => toggleTooltip(true)}
-                bind:this={tooltipAnchor}
-                bind:clientWidth={textContainerWidth}
-                class={isVertical ? 'max-h-10 break-words overflow-y-scroll overflow-hidden' : 'truncate'}
-            >
+        <div
+            class="w-full flex text-left  {isVertical
+                ? 'max-h-20 flex-col space-y-1 overflow-y-auto'
+                : 'flex-row space-x-2 justify-between'}"
+        >
+            {#if keyText}
                 <Text
                     fontSize="14"
                     lineHeight="5"
                     color={textColor}
                     darkColor={darkTextColor}
-                    classes={isVertical ? '' : 'truncate'}
+                    fontWeight={FontWeight.medium}
                 >
-                    {valueText}
+                    {keyText}
                 </Text>
-            </div>
-            {#if showTooltip}
-                <Tooltip anchor={tooltipAnchor} position="top">
-                    <div class="max-h-40 overflow-y-scroll pr-0 text-left break-words">
-                        <Text color={textColor} darkColor={darkTextColor} classes="text-left break-words"
-                            >{valueText}</Text
-                        >
-                    </div>
-                </Tooltip>
+            {:else}
+                <slot name="key" />
             {/if}
-        {:else}
-            <slot name="value" />
-        {/if}
+            {#if valueText}
+                <div
+                    bind:this={valueContainer}
+                    bind:clientWidth={valueContainerWidth}
+                    class={isVertical ? 'break-words' : 'truncate'}
+                >
+                    <Text
+                        fontSize="14"
+                        lineHeight="16"
+                        color={textColor}
+                        darkColor={darkTextColor}
+                        classes={isVertical ? '' : 'truncate'}
+                    >
+                        {valueText}
+                    </Text>
+                </div>
+            {:else}
+                <slot name="value" />
+            {/if}
+        </div>
     </CopyableBox>
 </div>
