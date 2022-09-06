69caee93bb7f80e203185cac0937c8e2bc7952da
Jean Ribeiro <iamjeanribeiro@gmail.com>
2022-09-06 15:07:14 -0300

    feat: adds filter badge count

--- packages/shared/components/filters/Filter.svelte
+++ packages/shared/components/filters/Filter.svelte
@@ -21,10 +21,18 @@
 
     $: isChanged = JSON.stringify($filterStore) !== JSON.stringify(filter)
     $: filterActive = modal?.isOpened()
+    $: activeFilterCount = Object.keys($filterStore).filter((f) => $filterStore[f].active).length
 </script>
 
 <div class="h-6 relative">
     <TogglableButton icon="filter" bind:active={filterActive} onClick={modal?.toggle} />
+    {#if activeFilterCount}
+        <filter-badge
+            class="inline-flex items-center justify-center h-3 w-3 -ml-2 -mt-0.5 absolute rounded-full bg-blue-500 text-white text-8"
+        >
+            {activeFilterCount}
+        </filter-badge>
+    {/if}
 
     <FilterModal bind:modal bind:filter {isChanged} {onSetFilters} {onClose}>
         {#each Object.keys(filter) as filterUnit}
