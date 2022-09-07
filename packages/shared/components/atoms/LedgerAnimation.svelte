6bf3d1a421449f3c2093431cea846c5c69711cf0
MarkNerdi <mark.nardi@iota.org>
2022-09-07 15:51:46 +0200

    fix: fix z-index

--- packages/shared/components/atoms/LedgerAnimation.svelte
+++ packages/shared/components/atoms/LedgerAnimation.svelte
@@ -10,9 +10,8 @@
 <ledger-animation class="w-full h-full relative flex {classes}">
     <Animation animation="ledger-bg-desktop" classes="w-full h-full z-0 absolute {bgClasses}" />
     {#if animation}
-        <Animation {animation} classes="z-10" />
-    {/if}
-    {#if illustration}
-        <Illustration {illustration} classes="z-10" />
+        <Animation {animation} classes="z-0" />
+    {:else if illustration}
+        <Illustration {illustration} classes="z-0" />
     {/if}
 </ledger-animation>
