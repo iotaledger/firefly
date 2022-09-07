dd3648579133bd1b87f0f783891353c4b97de7d3
Tuditi <daviddetroch@pm.me>
2022-09-07 13:02:48 +0200

    chore: default ledgerPreparedOutput to null

--- packages/shared/lib/core/ledger/stores/ledger-prepared-output.store.ts
+++ packages/shared/lib/core/ledger/stores/ledger-prepared-output.store.ts
@@ -1,4 +1,4 @@
 import { OutputTypes } from '@iota/types'
 import { writable } from 'svelte/store'
 
-export const ledgerPreparedOutput = writable<OutputTypes>()
+export const ledgerPreparedOutput = writable<OutputTypes>(null)
