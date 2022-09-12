e9cf6188acc054f700f1a960c75c85dd4412f640
MarkNerdi <mark.nardi@iota.org>
2022-09-12 10:01:37 +0200

    fix: open account switcher popup

--- packages/shared/lib/auxiliary/deep-link/handlers/handleDeepLink.ts
+++ packages/shared/lib/auxiliary/deep-link/handlers/handleDeepLink.ts
@@ -7,6 +7,7 @@ import { resetDeepLink } from '../actions'
 import { DeepLinkContext } from '../enums'
 import { isDeepLinkRequestActive } from '../stores'
 import { handleDeepLinkWalletContext } from './wallet/handleDeepLinkWalletContext'
+import { closePopup, openPopup } from '@lib/popup'
 
 /**
  * Parses an IOTA deep link, i.e. a URL that begins with the app protocol i.e "firefly://".
@@ -24,18 +25,27 @@ export function handleDeepLink(input: string): void {
     try {
         const url = new URL(input)
         if (url.protocol === `${process.env.APP_PROTOCOL}:`) {
-            switch (url.hostname) {
-                case DeepLinkContext.Wallet:
-                    get(dashboardRouter).goTo(DashboardRoute.Wallet)
-                    handleDeepLinkWalletContext(url)
-                    break
-                default:
-                    addError({
-                        time: Date.now(),
-                        type: 'deepLink',
-                        message: `Unrecognized context '${url.host}'`,
-                    })
-            }
+            openPopup({
+                type: 'accountSwitcher',
+                overflow: true,
+                props: {
+                    onConfirm: () => {
+                        closePopup()
+                        switch (url.hostname) {
+                            case DeepLinkContext.Wallet:
+                                get(dashboardRouter).goTo(DashboardRoute.Wallet)
+                                handleDeepLinkWalletContext(url)
+                                break
+                            default:
+                                addError({
+                                    time: Date.now(),
+                                    type: 'deepLink',
+                                    message: `Unrecognized context '${url.host}'`,
+                                })
+                        }
+                    },
+                },
+            })
         } else {
             resetDeepLink()
             addError({
--- packages/shared/routes/dashboard/Dashboard.svelte
+++ packages/shared/routes/dashboard/Dashboard.svelte
@@ -18,7 +18,6 @@
     import { Developer, Settings, Staking, Wallet } from 'shared/routes'
     import { onDestroy, onMount } from 'svelte'
     import TopNavigation from './TopNavigation.svelte'
-    import { closePopup, openPopup } from '@lib/popup'
 
     $: $activeProfile, saveActiveProfile()
 
@@ -72,16 +71,7 @@
     // TODO: handle deep link requests for new send form
     const handleDeepLinkRequest = (data: string): void => {
         if ($activeProfile?.hasLoadedAccounts) {
-            openPopup({
-                type: 'accountSwitcher',
-                overflow: true,
-                props: {
-                    onConfirm: () => {
-                        closePopup()
-                        handleDeepLink(data)
-                    },
-                },
-            })
+            handleDeepLink(data)
         }
     }
 
