26699f858b60587861cb159b95a0d9e8bac373fb
Matthew Maxwell <maxwellmattryan@gmail.com>
2022-09-14 16:28:21 -0500

    chore: separate error file

--- packages/shared/lib/core/profile-manager/errors/wallet-api-event.error.ts
+++ packages/shared/lib/core/profile-manager/errors/wallet-api-event.error.ts
@@ -9,13 +9,3 @@ export class WalletApiEventError extends BaseError {
         })
     }
 }
-
-export class WalletApiEventValidationError extends BaseError {
-    constructor(reason: string) {
-        super({
-            message: reason,
-            ...DEFAULT_APP_ERROR_PARAMETERS,
-            localizeMessage: false,
-        })
-    }
-}
