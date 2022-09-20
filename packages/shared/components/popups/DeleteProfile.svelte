ee430738cf4774c76f990c02f0b67dce8bca091b
Nicole O'Brien <nicole.obrien@iota.org>
2022-09-20 11:30:27 +0100

    fix: remove need for password when deleting stronghold profile

--- packages/shared/components/popups/DeleteProfile.svelte
+++ packages/shared/components/popups/DeleteProfile.svelte
@@ -1,24 +1,14 @@
 <script lang="typescript">
-    import { Button, PasswordInput, Text, Spinner } from 'shared/components'
-    import { isSoftwareProfile } from '@core/profile'
+    import { Button, Text, Spinner } from 'shared/components'
     import { localize } from '@core/i18n'
     import { closePopup } from '@lib/popup'
     import { deleteProfile } from '@contexts/settings'
 
     let isBusy = false
-    let error = ''
-    let password: string
 
     async function handleDeleteClick(): Promise<void> {
         isBusy = true
-        error = ''
-
-        try {
-            await deleteProfile()
-        } catch (err) {
-            error = localize(err.error)
-        }
-
+        await deleteProfile()
         isBusy = false
     }
 </script>
@@ -26,23 +16,10 @@
 <Text type="h4" classes="mb-5">{localize('popups.deleteProfile.title')}</Text>
 <div class="w-full h-full mb-5">
     <Text classes="mb-3">{localize('popups.deleteProfile.confirmation')}</Text>
-    {#if $isSoftwareProfile}
-        <Text type="p" secondary classes="mb-3">{localize('popups.deleteProfile.typePassword')}</Text>
-        <PasswordInput
-            {error}
-            classes="w-full mb-8"
-            bind:value={password}
-            showRevealToggle
-            placeholder={localize('general.password')}
-            autofocus
-            submitHandler={handleDeleteClick}
-            disabled={isBusy}
-        />
-    {/if}
 </div>
 <div class="flex flex-row justify-between space-x-4 w-full">
     <Button secondary classes="w-1/2" onClick={closePopup} disabled={isBusy}>{localize('actions.no')}</Button>
-    <Button disabled={(!password && $isSoftwareProfile) || isBusy} classes="w-1/2" onClick={handleDeleteClick} warning>
+    <Button disabled={isBusy} classes="w-1/2" onClick={handleDeleteClick} warning>
         {#if isBusy}
             <Spinner busy classes="justify-center" />
         {:else}
