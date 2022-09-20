dd5f12417e321d96d83e84006e352a0e39413e8f
MarkNerdi <mark.nardi@iota.org>
2022-09-20 10:53:13 +0200

    enhancement: add maximum supported decimals

--- packages/shared/lib/core/wallet/utils/convertToRawAmount.ts
+++ packages/shared/lib/core/wallet/utils/convertToRawAmount.ts
@@ -2,6 +2,7 @@ import Big from 'big.js'
 import { parseCurrency } from '@lib/currency'
 import { UNIT_MAP } from '@lib/units'
 import { ITokenMetadata } from '../interfaces'
+import { MAX_SUPPORTED_DECIMALS } from '../constants/max-supported-decimals.constants'
 
 export function convertToRawAmount(amount: string, unit: string, tokenMetadata?: ITokenMetadata): Big {
     if (amount) {
@@ -17,7 +18,7 @@ function convertToRawAmountFromMetadata(amount: number, unit: string, tokenMetad
         return Big(amount * UNIT_MAP?.[unit?.substring(0, 1)] ?? 0)
     } else {
         if (unit && unit === tokenMetadata.unit) {
-            const decimals = Math.min(tokenMetadata.decimals, 20)
+            const decimals = Math.min(tokenMetadata.decimals, MAX_SUPPORTED_DECIMALS)
             return Big(amount).mul(Big(10).pow(decimals))
         } else if (unit === tokenMetadata.subunit) {
             return Big(amount)
