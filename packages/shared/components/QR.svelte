5818d9e039f3bb5f3c8ea696ec7f128bbca173da
MarkNerdi <mark.nardi@iota.org>
2022-09-22 14:34:58 +0200

    ui: improve qr code performance

--- packages/shared/components/QR.svelte
+++ packages/shared/components/QR.svelte
@@ -1,52 +1,20 @@
 <script lang="typescript">
-    import QRCode from 'qr.js/lib/QRCode'
+    import QrCode from 'svelte-qrcode'
     import { appSettings } from '@core/app'
 
     export let data
     export let size = 150
     export let classes
 
-    let qr
-    let cells
-
     $: darkModeEnabled = $appSettings.darkMode
-    $: data, create()
-
-    function create() {
-        try {
-            qr = new QRCode(-1, 1)
-            qr.addData(data)
-            qr.make()
-            cells = qr.modules
-        } catch (e) {
-            console.error(e)
-        }
-    }
 </script>
 
-{#key data}
-    {#if cells}
-        <svg width={size} height={size} viewBox={`0 0 ${cells.length} ${cells.length}`} class={classes}>
-            {#each cells as row, rowIndex}
-                {#each row as cell, cellIndex}
-                    <rect
-                        height={1}
-                        key={cellIndex}
-                        style="fill: {cell ? (darkModeEnabled ? '#ffffff' : '#25395F') : 'none'};"
-                        width={1}
-                        x={cellIndex}
-                        y={rowIndex}
-                    />
-                {/each}
-            {/each}
-        </svg>
-    {/if}
-{/key}
-
-<style>
-    svg {
-        display: block;
-        position: relative;
-        margin: 0 auto;
-    }
-</style>
+<div class="flex justify-center {classes}">
+    <QrCode
+        value={data}
+        background={darkModeEnabled ? '#25395F' : '#ffffff'}
+        color={darkModeEnabled ? '#ffffff' : '#000000'}
+        {size}
+        padding="0"
+    />
+</div>
