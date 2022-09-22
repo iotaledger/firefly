a18708c4e12ea3a7c0121f77cd9a1aca83e4f639
Jean Ribeiro <iamjeanribeiro@gmail.com>
2022-09-22 15:12:48 -0300

    refactor: SendFormPopup optional inputs

--- packages/shared/components/popups/SendFormPopup.svelte
+++ packages/shared/components/popups/SendFormPopup.svelte
@@ -1,16 +1,18 @@
 <script lang="typescript">
-    import { Button, Text, RecipientInput, AssetAmountInput, ClosableInput, AddInputButton } from 'shared/components'
-    import { localize } from '@core/i18n'
-    import { closePopup, openPopup } from 'shared/lib/popup'
-    import { FontWeight } from 'shared/components/Text.svelte'
-    import { newTransactionDetails, updateNewTransactionDetails } from '@core/wallet'
     import { onMount } from 'svelte'
     import { get } from 'svelte/store'
+    import { localize } from '@core/i18n'
+    import { newTransactionDetails, updateNewTransactionDetails } from '@core/wallet'
+    import { Button, Text, RecipientInput, AssetAmountInput, OptionalInput } from 'shared/components'
+    import { FontWeight } from 'shared/components/Text.svelte'
+    import { closePopup, openPopup } from '@lib/popup'
     import { getByteLengthOfString } from '@lib/utils/getByteLengthOfString'
 
     let { asset, amount, unit, recipient, metadata, tag } = get(newTransactionDetails)
     let assetAmountInput: AssetAmountInput
     let recipientInput: RecipientInput
+    let metadataInput: OptionalInput
+    let tagInput: OptionalInput
 
     async function onSend(): Promise<void> {
         const valid = await validate()
@@ -23,13 +25,22 @@
         }
     }
 
+    function validateOptionalInput(value: string, byteLimit: number, errorMessage: string): Promise<void> {
+        return new Promise((resolve, reject) => {
+            if (getByteLengthOfString(value) > byteLimit) {
+                return reject(errorMessage)
+            }
+            resolve()
+        })
+    }
+
     async function validate(): Promise<boolean> {
         try {
             await Promise.all([
                 assetAmountInput?.validate(),
                 recipientInput?.validate(),
-                validateTag(),
-                validateMetadata(),
+                metadataInput?.validate(validateOptionalInput(metadata, 8192, localize('error.send.metadataTooLong'))),
+                tagInput?.validate(validateOptionalInput(tag, 64, localize('error.send.tagTooLong'))),
             ])
             return true
         } catch (error) {
@@ -42,44 +53,8 @@
         closePopup()
     }
 
-    let metadataButtonElement: HTMLButtonElement
-    let isMetadataInputOpen = false
-    function openMetadataInput() {
-        isMetadataInputOpen = true
-    }
-
-    let tagButtonElement: HTMLButtonElement
-    let isTagInputOpen = false
-    function openTagInput() {
-        isTagInputOpen = true
-    }
-
-    let tagError: string = ''
-    function validateTag(): Promise<void> {
-        tagError = ''
-        if (getByteLengthOfString(tag) > 64) {
-            tagError = localize('error.send.tagTooLong')
-            return Promise.reject(tagError)
-        }
-    }
-
-    let metadataError: string = ''
-    function validateMetadata(): Promise<void> {
-        metadataError = ''
-        if (getByteLengthOfString(metadata) > 8192) {
-            metadataError = localize('error.send.metadataTooLong')
-            return Promise.reject(metadataError)
-        }
-    }
-
     let sendButtonElement: HTMLButtonElement
     onMount(() => {
-        if (metadata) {
-            openMetadataInput()
-        }
-        if (tag) {
-            openTagInput()
-        }
         if (amount && recipient) {
             sendButtonElement.focus()
         }
@@ -87,46 +62,26 @@
 </script>
 
 <send-form-popup class="w-full h-full space-y-6 flex flex-auto flex-col flex-shrink-0">
-    <Text type="h3" fontWeight={FontWeight.semibold} classes="text-left">{localize('popups.sendForm.title')}</Text>
+    <Text type="h3" fontWeight={FontWeight.semibold} classes="text-left">
+        {localize('popups.sendForm.title')}
+    </Text>
     <send-form-inputs class="flex flex-col space-y-4">
         <AssetAmountInput bind:this={assetAmountInput} bind:asset bind:amount bind:unit />
         <RecipientInput bind:this={recipientInput} bind:recipient />
-        <ClosableInput
-            bind:buttonElement={metadataButtonElement}
-            bind:open={isMetadataInputOpen}
-            bind:value={metadata}
-            error={metadataError}
-            label={localize('general.metadata')}
-            placeholder={localize('general.metadata')}
-            fontSize="15"
-            fontWeight={FontWeight.medium}
-        />
-        <ClosableInput
-            bind:buttonElement={tagButtonElement}
-            bind:open={isTagInputOpen}
-            bind:value={tag}
-            error={tagError}
-            label={localize('general.tag')}
-            placeholder={localize('general.tag')}
-            fontSize="15"
-            fontWeight={FontWeight.medium}
-        />
-        {#if !isMetadataInputOpen || !isTagInputOpen}
-            <optional-input-buttons class="flex flex-row space-x-4">
-                <AddInputButton
-                    bind:buttonElement={metadataButtonElement}
-                    bind:open={isMetadataInputOpen}
-                    text={localize('general.metadata')}
-                    onClick={openMetadataInput}
-                />
-                <AddInputButton
-                    bind:buttonElement={tagButtonElement}
-                    bind:open={isTagInputOpen}
-                    text={localize('general.tag')}
-                    onClick={openTagInput}
-                />
-            </optional-input-buttons>
-        {/if}
+        <optional-inputs class="flex flex-row flex-wrap gap-2">
+            <OptionalInput
+                bind:this={metadataInput}
+                bind:value={metadata}
+                label={localize('general.metadata')}
+                description={localize('tooltips.optionalInput')}
+            />
+            <OptionalInput
+                bind:this={tagInput}
+                bind:value={tag}
+                label={localize('general.tag')}
+                description={localize('tooltips.optionalInput')}
+            />
+        </optional-inputs>
     </send-form-inputs>
     <popup-buttons class="flex flex-row flex-nowrap w-full space-x-4">
         <Button classes="w-full" outline onClick={onCancel}>
