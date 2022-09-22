3ded9ddb5132f386d313f88b35cb7b0505609caa
Tuditi <daviddetroch@pm.me>
2022-09-22 09:17:07 +0200

    chore: removed error thrown wallet API event

--- packages/shared/lib/core/profile-manager/enums/wallet-api-event.enum.ts
+++ packages/shared/lib/core/profile-manager/enums/wallet-api-event.enum.ts
@@ -4,7 +4,6 @@
  */
 export enum WalletApiEvent {
     ConsolidationRequired = 'ConsolidationRequired',
-    ErrorThrown = 'ErrorThrown',
     LedgerAddressGeneration = 'LedgerAddressGeneration',
     NewOutput = 'NewOutput',
     SpentOutput = 'SpentOutput',
