b1e19f9ed0f101a5fca1522b0c3d877f7064a56b
Tuditi <daviddetroch@pm.me>
2022-09-22 09:16:27 +0200

    fix: ammend error handling

--- packages/shared/lib/core/wallet/actions/claimActivity.ts
+++ packages/shared/lib/core/wallet/actions/claimActivity.ts
@@ -1,7 +1,6 @@
 import { selectedAccount } from '@core/account/stores/selected-account.store'
 import { handleError } from '@core/error/handlers/handleError'
-import { localize } from '@core/i18n'
-import { handleLedgerError, LedgerError } from '@core/ledger'
+import { handleLedgerError } from '@core/ledger'
 import { activeProfile, ProfileType } from '@core/profile'
 import { get } from 'svelte/store'
 import { ActivityType } from '../enums'
@@ -13,26 +12,15 @@ export async function claimActivity(activityId: string, data: ITransactionActivi
     const _activeProfile = get(activeProfile)
     try {
         updateActivityDataByActivityId(account.id, activityId, { type: ActivityType.Transaction, isClaiming: true })
-        const results = await account.claimOutputs([data.outputId])
-        if (results.length > 0) {
-            const transactionId = results[0].transactionId
-            updateActivityDataByActivityId(account.id, activityId, {
-                type: ActivityType.Transaction,
-                claimingTransactionId: transactionId,
-            })
-        } else if (_activeProfile.type === ProfileType.Ledger) {
-            // TODO: remove these clauses after wallet.rs implemented errors
-
-            // Wallet.rs doesn't throw an error for claiming (multiple) outputs.
-            // Multiple outputs could be claimed in multiple transactions and
-            // it's possible for some to fail and others to succeed.
-            throw new Error(LedgerError.DeniedByUser)
-        } else {
-            throw new Error(localize('error.send.cannotClaimTwice'))
-        }
+        const result = await account.claimOutputs([data.outputId])
+        const transactionId = result.transactionId
+        updateActivityDataByActivityId(account.id, activityId, {
+            type: ActivityType.Transaction,
+            claimingTransactionId: transactionId,
+        })
     } catch (err) {
-        if (err.message === LedgerError.DeniedByUser) {
-            handleLedgerError(err.message)
+        if (_activeProfile.type === ProfileType.Ledger) {
+            handleLedgerError(err.error)
         } else {
             handleError(err)
         }
