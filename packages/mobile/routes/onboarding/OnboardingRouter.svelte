b0360974bf16b66c029674932e20d61db07fc666
Paul Bögelsack <pboegelsack@googlemail.com>
2022-09-26 21:23:24 +0200

    feat: update onboarding routes

--- packages/mobile/routes/onboarding/OnboardingRouter.svelte
+++ packages/mobile/routes/onboarding/OnboardingRouter.svelte
@@ -1,11 +1,34 @@
 <script lang="typescript">
     import { Transition } from 'shared/components'
-    import { AppSetupRouter } from './views'
+    import {
+        // routers
+        AppSetupRouter,
+        NetworkSetupRouter,
+        ProfileSetupRouter,
+        StorageProtectionSetupRouter,
+        StrongholdSetupRouter,
+    } from './views'
     import { OnboardingRoute, onboardingRoute } from '@core/router'
 </script>
 
 {#if $onboardingRoute === OnboardingRoute.AppSetup}
     <Transition>
         <AppSetupRouter />
+    </Transition>
+{:else if $onboardingRoute === OnboardingRoute.NetworkSetup}
+    <Transition>
+        <NetworkSetupRouter />
+    </Transition>
+{:else if $onboardingRoute === OnboardingRoute.ProfileSetup}
+    <Transition>
+        <ProfileSetupRouter />
+    </Transition>
+{:else if $onboardingRoute === OnboardingRoute.StrongholdSetup}
+    <Transition>
+        <StrongholdSetupRouter />
+    </Transition>
+{:else if $onboardingRoute === OnboardingRoute.StorageProtectionSetup}
+    <Transition transition={false}>
+        <StorageProtectionSetupRouter />
     </Transition>
 {/if}
--- packages/mobile/routes/onboarding/views/index.js
+++ packages/mobile/routes/onboarding/views/index.js
@@ -1,1 +1,5 @@
 export * from './app-setup'
+export * from './network-setup'
+export * from './profile-setup'
+export * from './storage-protection-setup'
+export * from './stronghold-setup'
