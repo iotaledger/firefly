780b0dcb9fd9f1bfe2c882531a1ca70cc95383e0
Begoña Alvarez <balvarez@boxfish.studio>
2022-10-02 18:19:25 +0200

    feat: add missing logic in desktop base

--- packages/mobile/routes/onboarding/views/profile-setup/views/SetupView.svelte
+++ packages/mobile/routes/onboarding/views/profile-setup/views/SetupView.svelte
@@ -1,13 +1,19 @@
 <script lang="typescript">
-    import { onMount } from 'svelte'
-    import { OnboardingLayout } from '../../../../../components'
-    import { OnboardingButton } from 'shared/components'
-    import features from '@features/features'
-    import { onboardingProfile, ProfileSetupType, updateOnboardingProfile } from '@contexts/onboarding'
+    import {
+        initialiseOnboardingProfile,
+        onboardingProfile,
+        ProfileSetupType,
+        shouldBeDeveloperProfile,
+        updateOnboardingProfile,
+    } from '@contexts/onboarding'
     import { localize } from '@core/i18n'
-    import { formatProtocolName, getDefaultClientOptions } from '@core/network'
+    import { formatProtocolName, getDefaultClientOptions, NetworkProtocol, NetworkType } from '@core/network'
     import { destroyProfileManager } from '@core/profile-manager'
     import { profileSetupRouter } from '@core/router'
+    import features from '@features/features'
+    import { OnboardingButton } from 'shared/components'
+    import { onMount } from 'svelte'
+    import { OnboardingLayout } from '../../../../../components'
 
     const title = localize('views.onboarding.profileSetup.setup.title', {
         values: { protocol: formatProtocolName($onboardingProfile?.networkProtocol) },
@@ -24,6 +30,13 @@
     }
 
     onMount(() => {
+        if (!$onboardingProfile?.id) {
+            initialiseOnboardingProfile(
+                $onboardingProfile?.isDeveloperProfile ?? shouldBeDeveloperProfile(),
+                NetworkProtocol.Shimmer
+            )
+            updateOnboardingProfile({ networkType: NetworkType.Mainnet })
+        }
         if (!$onboardingProfile?.clientOptions) {
             const clientOptions = getDefaultClientOptions(
                 $onboardingProfile?.networkProtocol,
