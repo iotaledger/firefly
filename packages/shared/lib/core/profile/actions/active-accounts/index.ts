d870b7f4fbc16eac7b370e477e2859aaf63a9eb0
Nicole O'Brien <nicole.obrien@iota.org>
2022-10-04 21:08:02 +0100

    refactor: move function out of lib/wallet

--- packages/shared/lib/core/profile/actions/active-accounts/index.ts
+++ packages/shared/lib/core/profile/actions/active-accounts/index.ts
@@ -1,1 +1,2 @@
 export * from './updateActiveAccountMetadata'
+export * from './findActiveAccountWithAddress'
--- packages/shared/lib/wallet.ts
+++ packages/shared/lib/wallet.ts
@@ -1,21 +1,6 @@
-import { IAccountState } from '@core/account'
-import { activeAccounts } from '@core/profile'
-import { get, writable } from 'svelte/store'
+import { writable } from 'svelte/store'
 
 // TODO: move all this out of the file
 
 export const isTransferring = writable<boolean>(false)
 export const isSyncing = writable<boolean>(false)
-
-/**
- * Find an address in one of our accounts
- * @param address The address to find
- * @returns The wallet account matching the address or undefined if not found
- */
-export const findAccountWithAddress = (address: string): IAccountState | undefined => {
-    if (!address) {
-        return
-    }
-    const accounts = get(activeAccounts)
-    return accounts.find((account) => account.depositAddress === address)
-}
