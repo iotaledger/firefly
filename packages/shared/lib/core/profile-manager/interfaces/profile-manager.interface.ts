e6a4c1967fad675529a0eb0b2931d4534705f16f
Tuditi <daviddetroch@pm.me>
2022-10-05 13:58:45 +0200

    chore: remove methods that don't return complete objects

--- packages/shared/lib/core/profile-manager/interfaces/profile-manager.interface.ts
+++ packages/shared/lib/core/profile-manager/interfaces/profile-manager.interface.ts
@@ -1,15 +1,12 @@
 import {
-    AccountId,
     AccountSyncOptions,
     ClientOptions,
-    CreateAccountPayload,
     EventType,
     LedgerNanoStatus,
     NodeInfoWrapper,
     WalletEvent,
 } from '@iota/wallet'
 
-import { IAccount } from '@core/account'
 import { IAuth } from '@core/network'
 
 import { WalletApiEventHandler } from '../types'
@@ -20,25 +17,16 @@ export interface IProfileManager {
     bech32ToHex(bech32Address: string): Promise<string>
     changeStrongholdPassword(currentPassword: string, newPassword: string): Promise<void>
     clearStrongholdPassword(): Promise<void>
-    createAccount(payload: CreateAccountPayload): Promise<IAccount>
     destroy(): void
     emitTestEvent(event: WalletEvent): Promise<void>
     generateMnemonic(): Promise<string>
-    getAccount(accountId: AccountId): Promise<IAccount>
     getAccountIndexes(): Promise<number[]>
-    getAccounts(): Promise<IAccount[]>
     getNodeInfo(url?: string, auth?: IAuth): Promise<NodeInfoWrapper>
     getLedgerNanoStatus(): Promise<LedgerNanoStatus>
     hexToBech32(hex: string, bech32Hrp?: string): Promise<string>
     isStrongholdPasswordAvailable(): Promise<boolean>
     listen(eventTypes: EventType[], callback: WalletApiEventHandler): void
     clearListeners(eventTypes: EventType[]): void
-    recoverAccounts(
-        accountStartIndex: number,
-        accountGapLimit: number,
-        addressGapLimit: number,
-        syncOptions?: AccountSyncOptions
-    ): Promise<IAccount[]>
     removeLatestAccount(): Promise<void>
     restoreBackup(source: string, password: string): Promise<void>
     setClientOptions(options: ClientOptions): Promise<void>
