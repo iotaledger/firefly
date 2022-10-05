ab39091478afefb25a5b12355df2db8f091b80a9
Tuditi <daviddetroch@pm.me>
2022-10-05 12:59:10 +0200

    chore: camelCase error messages and remove duplication

--- packages/shared/lib/core/account/utils/getBoundAccount.ts
+++ packages/shared/lib/core/account/utils/getBoundAccount.ts
@@ -1,25 +1,24 @@
-import { get, Writable } from 'svelte/store'
-
 import { IAccount, UnableToGetBoundAccountError } from '@core/account'
-import { api, IProfileManager, profileManager as _profileManager } from '@core/profile-manager'
-import { ErrorType } from '@lib/typings/events'
+import { WalletRsError } from '@core/error'
+import { createAccount, getAccount, profileManager as _profileManager } from '@core/profile-manager'
 
 export async function getBoundAccount(
     accountIndex: number,
     createAccountsIfNotFound: boolean = false,
-    profileManager: Writable<IProfileManager> = _profileManager
+    profileManager = _profileManager
 ): Promise<IAccount> {
     try {
         /**
-         * CAUTION: Do NOT remove the unnecessary `await` keyword here.
+         * CAUTION: Do NOT remove the `await` keyword here.
          * It is needed in the case of handling an AccountNotFound
          * error by creating more accounts.
          */
-        return await api?.getAccount(get(profileManager)?.id, accountIndex ?? 0)
+        const account = await getAccount(accountIndex ?? 0, profileManager)
+        return account
     } catch (err) {
-        if (err?.type === ErrorType?.AccountNotFound && createAccountsIfNotFound) {
+        if (err?.type === WalletRsError?.AccountNotFound && createAccountsIfNotFound) {
             for (let indexToCreateAccount = 0; indexToCreateAccount < accountIndex; indexToCreateAccount++) {
-                const account = await get(profileManager)?.createAccount({})
+                const account = await createAccount({}, profileManager)
                 if (account?.getMetadata()?.index === accountIndex) {
                     return account
                 }
--- packages/shared/lib/typings/events.ts
+++ packages/shared/lib/typings/events.ts
@@ -22,82 +22,6 @@ export interface EventMap {
     'notification-activated': unknown
 }
 
-// Reference: https://github.com/iotaledger/wallet.rs/blob/develop/src/error.rs
-export enum ErrorType {
-    // Generic
-    IoError = 'IoError',
-    JsonError = 'JsonError',
-    ClientError = 'ClientError',
-    Panic = 'Panic',
-
-    // Account
-    LatestAccountIsEmpty = 'LatestAccountIsEmpty',
-    AccountNotEmpty = 'AccountNotEmpty',
-    AccountInitialiseRequiredField = 'AccountInitialiseRequiredField',
-    CannotUseIndexIdentifier = 'CannotUseIndexIdentifier',
-    AccountAliasAlreadyExists = 'AccountAliasAlreadyExists',
-    InvalidBackupFile = 'InvalidBackupFile',
-    InvalidBackupDestination = 'InvalidBackupDestination',
-    InsufficientFunds = 'InsufficientFunds',
-    MnemonicEncode = 'MnemonicEncode',
-    InvalidMnemonic = 'InvalidMnemonic',
-    AccountNotFound = 'AccountNotFound',
-
-    // Address
-    InvalidAddress = 'InvalidAddress',
-    InvalidAddressLength = 'InvalidAddressLength',
-    InvalidRemainderValueAddress = 'InvalidRemainderValueAddress',
-    AddressBuildRequiredField = 'AddressBuildRequiredField',
-
-    // Message
-    MessageNotFound = 'MessageNotFound',
-    InvalidMessageIdLength = 'InvalidMessageIdLength',
-    InvalidMessageId = 'InvalidMessageId',
-    InvalidOutputKind = 'InvalidOutputKind',
-    InvalidTransactionId = 'InvalidTransactionId',
-
-    // Stronghold
-    StrongholdError = 'StrongholdError',
-
-    // Database
-    StorageDoesntExist = 'StorageDoesntExist',
-    Storage = 'Storage',
-    StorageAdapterNotDefined = 'StorageAdapterNotDefined',
-    StorageExists = 'StorageExists',
-    StorageAdapterNotSet = 'StorageAdapterNotSet',
-    StorageIsEncrypted = 'StorageIsEncrypted',
-    RecordDecrypt = 'RecordDecrypt',
-    RecordEncrypt = 'RecordEncrypt',
-    RecordNotFound = 'RecordNotFound',
-
-    // Bee (https://github.com/iotaledger/bee)
-    BeeMessage = 'BeeMessage',
-
-    // Nodes
-    UrlError = 'UrlError',
-    NodesNotSynced = 'NodesNotSynced',
-
-    // Ledger
-    LedgerMiscError = 'LedgerMiscError',
-    LedgerDongleLocked = 'LedgerDongleLocked',
-    LedgerDeniedByUser = 'LedgerDeniedByUser',
-    LedgerDeviceNotFound = 'LedgerDeviceNotFound',
-    LedgerEssenceTooLarge = 'LedgerEssenceTooLarge',
-    WrongLedgerSeedError = 'WrongLedgerSeedError',
-
-    // Dust output
-    LeavingDustError = 'LeavingDustError',
-}
-
-export enum LedgerErrorType {
-    LedgerMiscError = 'LedgerMiscError',
-    LedgerDongleLocked = 'LedgerDongleLocked',
-    LedgerDeniedByUser = 'LedgerDeniedByUser',
-    LedgerDeviceNotFound = 'LedgerDeviceNotFound',
-    LedgerEssenceTooLarge = 'LedgerEssenceTooLarge',
-    WrongLedgerSeedError = 'WrongLedgerSeedError',
-}
-
 export type Callback<T> = (error: string, data: T) => void
 
 export interface Event<T> {
@@ -105,11 +29,6 @@ export interface Event<T> {
     id: string
     type: unknown
     payload: T
-}
-
-export interface ErrorEventPayload {
-    type: ErrorType
-    error: string
 }
 
 export interface BalanceChangeEventPayload {
