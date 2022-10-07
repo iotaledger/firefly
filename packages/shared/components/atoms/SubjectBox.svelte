7a84990d71e42df04e4b34c3426f547c6daa5f66
Tuditi <daviddetroch@pm.me>
2022-10-07 11:46:13 +0200

    chore: merge origin

---
+++ packages/shared/components/atoms/SubjectBox.svelte
@@ -0,0 +1,21 @@
+<script lang="typescript">
+    import { localize } from '@core/i18n'
+    import { Subject } from '@core/wallet'
+    import { Box, AddressBox, Text, AccountLabel, TextType, FontWeight } from 'shared/components'
+
+    export let subject: Subject = null
+</script>
+
+{#if subject.type === 'account'}
+    <Box row clearBackground clearPadding classes="justify-center">
+        <AccountLabel account={subject.account} />
+    </Box>
+{:else if subject.type === 'address'}
+    <AddressBox clearBackground clearPadding isCopyable address={subject.address} />
+{:else}
+    <Box row clearBackground clearPadding classes="justify-center">
+        <Text type={TextType.pre} fontSize="base" fontWeight={FontWeight.medium}>
+            {localize('general.unknownAddress')}
+        </Text>
+    </Box>
+{/if}
---
+++ packages/shared/components/enums/button-size.enum.ts
@@ -0,0 +1,5 @@
+export enum ButtonSize {
+    Large = 'lg',
+    Medium = 'md',
+    Small = 'sm',
+}
