5c4d1cefdb7fd8b0b046df6435495b85f046b8af
Jean Ribeiro <iamjeanribeiro@gmail.com>
2022-10-07 16:42:22 -0300

    feat: initial selected collectibles UI (#4047)
    * feat: initial nft tab
    
    * Add routing and modules
    
    * fix: removes non-existing export from
    
    * fix: changes the div for changing routes to Button
    
    * refactor: adds KebabButton component
    
    * feat: adds CollectibleDetailsMenu item
    
    * fix: adds KebabButton to button index
    
    * refactor: remove pane from wrapping the DetailsView component in Collectibles.svelte
    
    * [WIP] feat: adds DetailsView UI
    
    * fix merge
    
    * Update yarn.lock and delete empty AccountSummaryAndAssetsPane file
    
    * Set collectibles feature flag to false
    
    * remove duplicate collectibles sidebar tab
    
    * Remove unimplemented collections route from GalleryView
    
    * add tooltip text to DetailsView component and update Button component usage
    
    * remove error causing Illustration component and replace with placeholder orange div
    
    Co-authored-by: Nicole O'Brien <nicole.obrien@iota.org>
    Co-authored-by: Jason Kraft <developer.kraft@gmail.com>

---
+++ packages/shared/components/modals/CollectibleDetailsMenu.svelte
@@ -0,0 +1,15 @@
+<script lang="typescript">
+    import { Modal, MenuItem } from 'shared/components'
+    import { localize } from '@core/i18n'
+
+    export let modal: Modal
+</script>
+
+<Modal bind:this={modal} position={{ top: '100px', right: '60px' }}>
+    <div class="flex flex-col">
+        <MenuItem icon="receive" title={localize('views.collectibles.details.menu.download')} first />
+        <MenuItem icon="profile" title={localize('views.collectibles.details.menu.setAvatar')} />
+        <MenuItem icon="export" title={localize('views.collectibles.details.menu.view')} />
+        <MenuItem icon="delete" title={localize('views.collectibles.details.menu.burn')} />
+    </div>
+</Modal>
