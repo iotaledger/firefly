6ce268460ab92b6b00442b8ddd92e78d38c49e27
paul-boegelsack <paul.boegelsack@iota.org>
2022-10-10 11:59:33 +0200

    feat: add login views for stardust mobile (#4813)
    * feat: setup login routes
    
    * feat: adjust load profile view for mobile
    
    * fix: remove dark mode variable declaration and hidden prop
    
    * fix: polish css and house keeping
    
    * fix: import after merge with base
    
    * feat: move hasCompletedAppSetup to an earlier route
    
    Co-authored-by: Begoña Alvarez <balvarez@boxfish.studio>

---
+++ packages/mobile/components/buttons/ProfileButton.svelte
@@ -0,0 +1,90 @@
+<script lang="typescript">
+    import { appSettings } from '@core/app'
+    import { Icon, NetworkIcon, Text, FontWeight, TextType } from 'shared/components'
+    import { getInitials as _getInitials } from 'shared/lib/helpers'
+    import { NetworkProtocol, NetworkType } from '@core/network'
+
+    export let name = ''
+    export let id = ''
+    export let networkType: NetworkType
+    export let networkProtocol: NetworkProtocol
+    export let disabled = false
+
+    export let onClick: undefined | ((id: string) => void) = undefined
+
+    function handleOnClick() {
+        onClick && onClick(id)
+    }
+
+    $: darkModeEnabled = $appSettings.darkMode
+
+    // @TODO: move to shared lib
+    function getInitials() {
+        const initials = _getInitials(name, 1)
+        if (initials.length === 1) {
+            return initials
+        } else {
+            const letters = initials.split('')
+            return letters[0] + letters[letters.length - 1]
+        }
+    }
+</script>
+
+<button
+    type="button"
+    {disabled}
+    class="rounded-xl px-4 py-3 w-full"
+    on:click={handleOnClick}
+    class:darkmode={darkModeEnabled}
+>
+    <div class="flex flex-row">
+        <div class="relative">
+            <div class="h-14 w-14 rounded-full bg-blue-500 flex items-center justify-center">
+                <Text type="h5" classes="text-white">{getInitials()}</Text>
+            </div>
+            <div class="absolute right-0 bottom-0">
+                <NetworkIcon {networkType} {networkProtocol} height={14} width={14} />
+            </div>
+        </div>
+        <Text
+            type={TextType.p}
+            color="gray-800"
+            darkColor="white"
+            fontSize="14"
+            fontWeight={FontWeight.semibold}
+            lineHeight="5"
+            classes="px-4 my-auto flex-grow text-left truncate"
+            >{name}
+        </Text>
+        <div class="w-4 items-center my-auto">
+            <Icon icon="chevron-right" classes="text-gray-500 dark:text-white" />
+        </div>
+    </div>
+</button>
+
+<style type="text/scss">
+    button {
+        @apply border;
+        @apply border-solid;
+        @apply border-gray-300;
+        @apply bg-transparent;
+
+        &:disabled {
+            :global(svg) {
+                @apply text-gray-500;
+            }
+            @apply pointer-events-none;
+            @apply bg-gray-50;
+        }
+
+        &.darkmode {
+            @apply border-gray-700;
+            &:disabled {
+                @apply bg-gray-700;
+                @apply bg-opacity-10;
+                @apply border-gray-700;
+                @apply border-opacity-10;
+            }
+        }
+    }
+</style>
---
+++ packages/mobile/routes/login/views/SelectProfileView.svelte
@@ -0,0 +1,70 @@
+<script lang="typescript">
+    import { ProfileButton } from '../../../components'
+    import { Button, Logo, Text } from 'shared/components'
+    import { needsToAcceptLatestPrivacyPolicy, needsToAcceptLatestTermsOfService, appSettings } from '@core/app'
+    import { localize } from '@core/i18n'
+    import { NetworkProtocol, NetworkType } from '@core/network'
+    import { profiles, loadPersistedProfileIntoActiveProfile } from '@core/profile'
+    import { initialiseOnboardingRouters, loginRouter } from '@core/router'
+    import {
+        initialiseOnboardingProfile,
+        shouldBeDeveloperProfile,
+        updateOnboardingProfile,
+    } from '@contexts/onboarding'
+    import { openPopup } from '@lib/popup'
+
+    $: dark = $appSettings.darkMode
+
+    function onContinueClick(id: string) {
+        loadPersistedProfileIntoActiveProfile(id)
+        $loginRouter.next()
+    }
+
+    function onAddProfileClick() {
+        initialiseOnboardingProfile(shouldBeDeveloperProfile(), NetworkProtocol.Shimmer)
+        if (!shouldBeDeveloperProfile()) {
+            updateOnboardingProfile({ networkType: NetworkType.Mainnet })
+        }
+        initialiseOnboardingRouters()
+        $loginRouter.next({ shouldAddProfile: true })
+    }
+
+    $: if (needsToAcceptLatestPrivacyPolicy() || needsToAcceptLatestTermsOfService()) {
+        openPopup({
+            type: 'legalUpdate',
+            hideClose: true,
+            preventClose: true,
+        })
+    }
+</script>
+
+<div class="h-full px-5 pb-5 flex flex-col">
+    <section class="overlay-scrollbar flex flex-col items-center w-full h-full pt-16">
+        <Logo width="64px" logo="logo-firefly" />
+        <!-- @TODO: Add text to locals -->
+        <Text type="h3" bold classes="text-center mt-4">Welcome back!</Text>
+        <div class="w-full flex flex-col mt-10">
+            {#each $profiles as profile}
+                <div class="w-full item-center mb-4">
+                    <ProfileButton
+                        onClick={onContinueClick}
+                        name={profile.name}
+                        id={profile.id}
+                        networkType={profile?.networkType ?? NetworkType.Devnet}
+                        networkProtocol={profile?.networkProtocol ?? NetworkProtocol.IOTA}
+                    />
+                </div>
+            {/each}
+        </div>
+    </section>
+    <footer class="w-full pt-5">
+        <Button
+            classes="w-full text-blue-500 border border-solid 
+                {dark ? 'border-gray-700' : 'border-gray-300'}"
+            variant="none"
+            onClick={onAddProfileClick}
+        >
+            {localize('general.addProfile')}
+        </Button>
+    </footer>
+</div>
