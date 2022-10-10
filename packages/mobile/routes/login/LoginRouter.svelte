9f481185095c703f1c83b0a68e469371a485e935
Begoña Álvarez de la Cruz <balvarez@boxfish.studio>
2022-10-10 14:59:06 +0200

    refactor: remove mobile routes transitions (#4836)

--- packages/mobile/routes/login/LoginRouter.svelte
+++ packages/mobile/routes/login/LoginRouter.svelte
@@ -1,19 +1,12 @@
 <script lang="typescript">
-    import { Transition } from 'shared/components'
     import { EnterPinView, SelectProfileView, LoadProfileView } from './views'
     import { loginRoute, LoginRoute } from '@core/router'
 </script>
 
 {#if $loginRoute === LoginRoute.SelectProfile}
-    <Transition>
-        <SelectProfileView />
-    </Transition>
+    <SelectProfileView />
 {:else if $loginRoute === LoginRoute.EnterPin}
-    <Transition>
-        <EnterPinView />
-    </Transition>
+    <EnterPinView />
 {:else if $loginRoute === LoginRoute.LoadProfile}
-    <Transition>
-        <LoadProfileView />
-    </Transition>
+    <LoadProfileView />
 {/if}
