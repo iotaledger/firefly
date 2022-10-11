1f0431db43dc95e81005b93e39c0723059b23ec9
Tuditi <daviddetroch@pm.me>
2022-10-11 11:50:01 +0200

    fix: update error message

--- packages/shared/lib/auxiliary/deep-link/utils/getRawAmountFromSearchParam.ts
+++ packages/shared/lib/auxiliary/deep-link/utils/getRawAmountFromSearchParam.ts
@@ -1,10 +1,10 @@
-import { AmountNotANumberError, SendOperationParameter } from '@auxiliary/deep-link'
+import { AmountNotAnIntegerError, SendOperationParameter } from '@auxiliary/deep-link'
 
 export function getRawAmountFromSearchParam(searchParams: URLSearchParams): string {
     let rawAmount = searchParams.get(SendOperationParameter.Amount)
     const amount = Number(rawAmount)
     if (!Number.isInteger(amount)) {
-        throw new AmountNotANumberError(rawAmount)
+        throw new AmountNotAnIntegerError(rawAmount)
     }
     if (amount < 0) {
         rawAmount = Math.abs(amount).toString()
