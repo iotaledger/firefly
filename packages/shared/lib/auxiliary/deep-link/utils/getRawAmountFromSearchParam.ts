a7f9ce2fcaf9e393b384ec2bf6a5de0bb36f984d
Tuditi <daviddetroch@pm.me>
2022-10-11 11:05:30 +0200

    fix: add extra validation for negative numbers

--- packages/shared/lib/auxiliary/deep-link/utils/getRawAmountFromSearchParam.ts
+++ packages/shared/lib/auxiliary/deep-link/utils/getRawAmountFromSearchParam.ts
@@ -3,7 +3,7 @@ import { AmountNotANumberError, SendOperationParameter } from '@auxiliary/deep-l
 export function getRawAmountFromSearchParam(searchParams: URLSearchParams): string {
     const rawAmount = searchParams.get(SendOperationParameter.Amount)
     const amount = Number(rawAmount)
-    if (!Number.isInteger(amount)) {
+    if (!Number.isInteger(amount) || amount < 0) {
         throw new AmountNotANumberError(rawAmount)
     }
     return rawAmount
