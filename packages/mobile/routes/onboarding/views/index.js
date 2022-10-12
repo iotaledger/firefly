f186079db16f3aa9a54c7fed82c1ae3a8cee2cec
paul-boegelsack <paul.boegelsack@iota.org>
2022-10-12 11:25:54 +0200

    feat: add mnemonic import view for stardust mobile (#4851)
    * feat: setup recovery with mnemonics for onboarding
    
    * feat: setup import with mnemonic for onboarding
    
    * fix: adds missing text type
    
    * refactor: removes unnecessary artefacts and enables recovery for main net
    
    Co-authored-by: Begoña Alvarez <balvarez@boxfish.studio>

--- packages/mobile/routes/onboarding/views/index.js
+++ packages/mobile/routes/onboarding/views/index.js
@@ -2,6 +2,7 @@ export * from './app-setup'
 export * from './network-setup'
 export * from './profile-backup'
 export * from './profile-setup'
+export * from './profile-recovery'
 export * from './storage-protection-setup'
 export * from './stronghold-setup'
 
---
+++ packages/mobile/routes/onboarding/views/profile-recovery/views/index.js
@@ -0,0 +1,2 @@
+export { default as ImportMnemonicPhraseView } from './ImportMnemonicPhraseView.svelte'
+export { default as SuccessView } from './SuccessView.svelte'
