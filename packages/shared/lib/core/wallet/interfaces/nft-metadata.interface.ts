53f5be92a592bc0130d3442c67185b1acbd60890
Mark Nardi <mark.nardi@iota.org>
2022-10-13 12:04:46 +0200

    test: test2

--- packages/shared/lib/core/wallet/interfaces/nft-metadata.interface.ts
+++ packages/shared/lib/core/wallet/interfaces/nft-metadata.interface.ts
@@ -11,6 +11,5 @@ export interface INftMetadata {
     royalties: Record<string, number>
     issuerName: string
     description: string
-    description2: string
     attributes: { trait_type: string; value: string | number }[]
 }
