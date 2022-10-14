531d99770ce480cd4b3b8b8f097d442c873df27e
Jean Ribeiro <iamjeanribeiro@gmail.com>
2022-10-14 06:37:33 -0300

    enhancement: removes kdbx and all its related contents (#4890)
    Co-authored-by: Tuditi <45079109+Tuditi@users.noreply.github.com>

--- packages/shared/lib/contexts/onboarding/actions/setProfileRecoveryTypeFromFilename.ts
+++ packages/shared/lib/contexts/onboarding/actions/setProfileRecoveryTypeFromFilename.ts
@@ -1,11 +1,9 @@
-import { SEEDVAULT_REGEX, STRONGHOLD_REGEX } from '../constants'
+import { STRONGHOLD_REGEX } from '../constants'
 import { ProfileRecoveryType } from '../enums'
 import { updateOnboardingProfile } from '../stores'
 
 export function setProfileRecoveryTypeFromFilename(filename: string): void {
-    if (SEEDVAULT_REGEX.test(filename)) {
-        updateOnboardingProfile({ recoveryType: ProfileRecoveryType.SeedVault })
-    } else if (STRONGHOLD_REGEX.test(filename)) {
+    if (STRONGHOLD_REGEX.test(filename)) {
         updateOnboardingProfile({ recoveryType: ProfileRecoveryType.Stronghold })
     }
 }
