0cd473a8f6da85390626153d6786c89ba1f8b04b
MarkNerdi <105642810+MarkNerdi@users.noreply.github.com>
2022-10-14 02:46:00 +0200

    enhancement: add nfts to activity list (#4884)
    * enhancement: add nft activity type
    
    * enhancement: add subject
    
    * enhancement: add NFT activity details
    
    * enhancement: add more properties to nft details
    
    * enhancement: parse correct data from the nft
    
    * test: test commit
    
    * test: test2
    
    * patch: fix misc issues before approving and merging with nft dev tools
    
    Co-authored-by: Jason Kraft <jason.kraft@iota.org>

---
+++ packages/shared/lib/core/wallet/interfaces/nft-metadata.interface.ts
@@ -0,0 +1,16 @@
+import { MimeType } from '../types'
+
+export interface INftMetadata {
+    id: string
+    standard: string
+    version: string
+    type: MimeType
+    uri: string
+    name: string
+    collectionId?: string
+    collectionName?: string
+    royalties?: Record<string, number>
+    issuerName?: string
+    description?: string
+    attributes?: { trait_type: string; value: string | number }[]
+}
--- packages/shared/lib/core/wallet/utils/getActivityType.ts
+++ packages/shared/lib/core/wallet/utils/getActivityType.ts
@@ -1,10 +1,13 @@
 import { ActivityType } from '../enums'
-import { OUTPUT_TYPE_ALIAS, OUTPUT_TYPE_FOUNDRY } from '../constants'
+import { OUTPUT_TYPE_ALIAS, OUTPUT_TYPE_FOUNDRY, OUTPUT_TYPE_NFT } from '../constants'
 import { IWrappedOutput } from '../interfaces'
 
 export function getActivityType(outputs: IWrappedOutput[]): ActivityType {
     if (outputs.some((output) => output.output.type === OUTPUT_TYPE_FOUNDRY)) {
         return ActivityType.Foundry
+    }
+    if (outputs.some((output) => output.output.type === OUTPUT_TYPE_NFT)) {
+        return ActivityType.Nft
     }
     if (outputs.some((output) => output.output.type === OUTPUT_TYPE_ALIAS)) {
         return ActivityType.Alias
