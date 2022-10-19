a0f872a997a089b59326b3f8019f7d9b4d4b89d0
Paul Bögelsack <pboegelsack@googlemail.com>
2022-10-19 10:11:57 +0200

    fix: add missing dashboard export

--- packages/mobile/lib/core/router/index.ts
+++ packages/mobile/lib/core/router/index.ts
@@ -1,6 +1,7 @@
 export * from './enums'
 
 export * from './app-router'
+export * from './dashboard-router'
 export * from './onboarding-router'
 export * from './subrouters'
 export * from './helper'
