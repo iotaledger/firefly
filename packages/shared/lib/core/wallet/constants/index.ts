65d7494b1b5b5a72446547185556099a88e4511b
Mark Nardi <mark.nardi@iota.org>
2022-10-20 14:19:02 +0200

    fix: fix import

--- packages/shared/lib/core/wallet/constants/index.ts
+++ packages/shared/lib/core/wallet/constants/index.ts
@@ -1,6 +1,7 @@
 export * from './address-type.constants'
 export * from './animated-token-ids.constant'
 export * from './default-transaction-options.constant'
+export * from './empty-hex-id.constants'
 export * from './feature-type.constants'
 export * from './max-asset-displayed-initials.constant'
 export * from './max-supported-decimals.constants'
