6ac241895532aac0987499618b38f142d768ffd7
paul-boegelsack <paul.boegelsack@iota.org>
2022-10-20 16:43:46 +0200

    ui: dashboard header setup (#4990)
    * ref: merge with stardust-develop
    
    * feat: deactivate sendAndReceive
    
    * feat: setup features for top bar
    
    * fix: add missing lang attribute
    
    * ref: move feature flags to wallet
    
    * ui: add fixed positions for top bar elements
    
    * fix: change onClick of AccountActionsButton back

---
+++ packages/mobile/components/buttons/ProfileActionsButton.svelte
@@ -0,0 +1,12 @@
+<script lang="typescript">
+    import { activeProfile } from '@core/profile'
+    import { getInitials } from '@lib/helpers'
+
+    // @TODO fix the linting error that profileInitial isn't used
+    /* eslint-disable @typescript-eslint/no-unused-vars */
+    $: profileInitial = getInitials($activeProfile?.name, 1)
+</script>
+
+<button class="w-10 h-10 relative flex items-center justify-center rounded-full bg-blue-500 leading-100">
+    <span class="text-12 text-center text-white uppercase">{profileInitial}</span>
+</button>
