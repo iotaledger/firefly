e82bdbc975984154d1dc2f0bd5f15ac9c3079a19
Begoña Alvarez <balvarez@boxfish.studio>
2022-10-20 14:11:55 +0200

    chore: update feature flags for mobile

--- packages/mobile/lib/contexts/wallet/constants/initial-active-tab.constant.ts
+++ packages/mobile/lib/contexts/wallet/constants/initial-active-tab.constant.ts
@@ -4,9 +4,9 @@ import features from '../../../../features/features'
 export const INITIAL_ACTIVE_TAB: WalletTab | null = getInitialActiveTab()
 
 function getInitialActiveTab(): WalletTab | null {
-    if (features?.tokens?.enabled) {
+    if (features?.wallet?.tokens?.enabled) {
         return WalletTab.Tokens
-    } else if (features?.activity?.enabled) {
+    } else if (features?.wallet?.activity?.enabled) {
         return WalletTab.Activity
     } else {
         return null
