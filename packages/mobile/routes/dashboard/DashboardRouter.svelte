ff34a62093a8ea569e27fae42193db7a570e8276
Begoña Alvarez <balvarez@boxfish.studio>
2022-10-20 16:22:50 +0200

    feat: add receive drawer

--- packages/mobile/routes/dashboard/DashboardRouter.svelte
+++ packages/mobile/routes/dashboard/DashboardRouter.svelte
@@ -2,14 +2,17 @@
     import { selectedAccount } from '@core/account'
     import { TabPane } from '../../../mobile/components'
     import { activeWalletTab, WALLET_TAB_COMPONENT } from '../../lib/contexts/wallet'
+    import { dashboardRoute, DashboardRoute, dashboardRouter } from '../../lib/core/router'
+    import { ReceiveDrawer } from './wallet/drawers'
     import { TabNavigator } from './wallet/tabs'
 
     $: activeWalletTabComponent = WALLET_TAB_COMPONENT[$activeWalletTab]
 </script>
 
+<!-- Wallet base welcome screen -->
 {#if $selectedAccount}
     <div class="flex flex-col w-screen h-screen bg-white dark:bg-gray-800">
-        <div class="w-full h-18">BALANCE PLACEHOLDER</div>
+        <button on:click={() => $dashboardRouter.goTo(DashboardRoute.Receive)}>Receive</button>
         {#if activeWalletTabComponent}
             <div class="relative flex flex-col flex-auto w-full">
                 <TabPane>
@@ -19,4 +22,8 @@
             </div>
         {/if}
     </div>
+{/if}
+<!-- Routes -->
+{#if $dashboardRoute === DashboardRoute.Receive}
+    <ReceiveDrawer onClose={() => $dashboardRouter.previous()} />
 {/if}
