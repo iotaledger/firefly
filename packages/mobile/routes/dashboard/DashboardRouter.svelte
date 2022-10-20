35acd38319b94beeb5885aae43e1c8480750dec7
Begoña Alvarez <balvarez@boxfish.studio>
2022-10-20 12:06:09 +0200

    fix: render bottom navigation and active tab conditionally

--- packages/mobile/routes/dashboard/DashboardRouter.svelte
+++ packages/mobile/routes/dashboard/DashboardRouter.svelte
@@ -2,18 +2,22 @@
     import { selectedAccount } from '@core/account'
     import { activeWalletTab, WALLET_TAB_COMPONENT } from '../../lib/contexts/wallet'
     import { TabNavigator } from './wallet/tabs'
+
+    $: activeWalletTabComponent = WALLET_TAB_COMPONENT[$activeWalletTab]
 </script>
 
 {#if $selectedAccount}
     <div class="flex flex-col w-screen h-screen bg-white dark:bg-gray-800">
         <div class="w-full h-18">BALANCE PLACEHOLDER</div>
-        <div class="relative flex flex-col flex-auto w-full">
-            <div class="z-0 flex-auto">
-                <svelte:component this={WALLET_TAB_COMPONENT[$activeWalletTab]} />
+        {#if activeWalletTabComponent}
+            <div class="relative flex flex-col flex-auto w-full">
+                <div class="z-0 flex-auto">
+                    <svelte:component this={WALLET_TAB_COMPONENT[$activeWalletTab]} />
+                </div>
+                <div class="z-10">
+                    <TabNavigator />
+                </div>
             </div>
-            <div class="z-10">
-                <TabNavigator />
-            </div>
-        </div>
+        {/if}
     </div>
 {/if}
