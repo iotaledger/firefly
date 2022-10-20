66230d4d4b01ac89ceae6bdc6b07a8feb060a4b1
Tuditi <45079109+Tuditi@users.noreply.github.com>
2022-10-20 14:24:49 +0200

    feat: network selector on send (#4982)
    * feat: add destination network and addresses
    
    * feat: add network selector when sending a transaction
    
    * chore: show address and correct label
    
    * chore: truncate more characters
    
    * fix: RecipientInput labelLocale
    
    * fix: change value prop initial value to empty string
    
    * fix: adds error display in RecipientInput
    
    * fix: add network type to network address object
    
    * enhancement: pointer cursor on network input
    
    * fix: fix classes on selector
    
    Co-authored-by: Jean Ribeiro <iamjeanribeiro@gmail.com>
    Co-authored-by: Mark Nardi <mark.nardi@iota.org>

--- packages/shared/components/molecules/SelectorInput.svelte
+++ packages/shared/components/molecules/SelectorInput.svelte
@@ -1,8 +1,8 @@
 <script lang="typescript">
     import { localize } from '@core/i18n'
-    import { InputContainer, Modal, TextInput } from 'shared/components'
+    import { Modal, TextInput } from 'shared/components'
 
-    export let value: string
+    export let value: string = ''
     export let error: string = ''
     export let disabled: boolean = false
     export let labelLocale: string = ''
@@ -19,20 +19,17 @@
 </script>
 
 <selector-input class="w-full relative">
-    <InputContainer bind:inputElement clearPadding isFocused={hasFocus} {error}>
-        <TextInput
-            bind:inputElement
-            bind:value
-            bind:hasFocus
-            clearBackground
-            clearBorder
-            {disabled}
-            label={localize(labelLocale)}
-            placeholder={localize(labelLocale)}
-            fontSize="sm"
-            {...$$restProps}
-        />
-    </InputContainer>
+    <TextInput
+        bind:inputElement
+        bind:value
+        bind:hasFocus
+        {error}
+        {disabled}
+        label={localize(labelLocale)}
+        placeholder={localize(labelLocale)}
+        fontSize="sm"
+        {...$$restProps}
+    />
     <slot onClose={() => inputElement.blur()}>
         <!-- Contains Custom Selector -->
     </slot>
