a613cbb77019d83bd08061540d9879c3a2f832ad
MarkNerdi <105642810+MarkNerdi@users.noreply.github.com>
2022-10-24 16:46:17 +0200

    enhancement: add storage deposit to native token minting (#5000)
    * enhancement: add storage deposit to native token minting
    
    * Update packages/shared/components/popups/MintNativeTokenConfirmationPopup.svelte
    
    Co-authored-by: Tuditi <45079109+Tuditi@users.noreply.github.com>
    
    * fix: PR fixes
    
    Co-authored-by: Tuditi <45079109+Tuditi@users.noreply.github.com>

--- packages/shared/components/atoms/buttons/MintNativeTokenButton.svelte
+++ packages/shared/components/atoms/buttons/MintNativeTokenButton.svelte
@@ -3,10 +3,12 @@
     import { closePopup, openPopup } from '@auxiliary/popup'
     import { OnboardingButton } from 'shared/components'
     import { selectedAccount } from '@core/account'
+    import { resetMintTokenDetails } from '@core/wallet'
 
     $: hasAliases = $selectedAccount.balances?.aliases.length > 0
 
     function handleMintNativeToken(): void {
+        resetMintTokenDetails()
         if (hasAliases) {
             openPopup({
                 type: 'mintNativeTokenForm',
