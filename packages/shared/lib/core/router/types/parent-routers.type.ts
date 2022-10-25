f2ea3d162ec3a661e8b41f3e7aa756e2303ffafb
Begoña Alvarez <balvarez@boxfish.studio>
2022-10-25 11:37:41 +0200

    linter: add generic type

--- packages/shared/lib/core/router/types/parent-routers.type.ts
+++ packages/shared/lib/core/router/types/parent-routers.type.ts
@@ -1,4 +1,4 @@
 import { Router } from '../router'
 import { Subrouter } from '../subrouters'
 
-export type ParentRouter = Router | Subrouter
+export type ParentRouter<T> = Router<T> | Subrouter<T>
