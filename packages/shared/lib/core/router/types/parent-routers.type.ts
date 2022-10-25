d7fb15579a0e73b035199c644f99a2a95b99c5f7
Begoña Alvarez <balvarez@boxfish.studio>
2022-10-25 14:17:17 +0200

    revert: shared parent router changes

--- packages/shared/lib/core/router/types/parent-routers.type.ts
+++ packages/shared/lib/core/router/types/parent-routers.type.ts
@@ -1,5 +1,4 @@
 import { AppRouter } from '../app-router'
 import { OnboardingRouter } from '../onboarding-router'
-import { DashboardRouter } from '../dashboard-router'
 
-export type ParentRouter = AppRouter | OnboardingRouter | DashboardRouter
+export type ParentRouter = AppRouter | OnboardingRouter
