2ac22aef66fd62b0c2b6d5f13c9824a2bd5aa15a
MarkNerdi <105642810+MarkNerdi@users.noreply.github.com>
2022-11-04 14:17:38 +0100

    feat: collectible tab (#5092)
    * feat: add collectibles to own tab
    
    * enhancement: initial subroute logic
    
    * ui: update UI
    
    * ui: add nft placeholder to gallery view
    
    * ui: update text style
    
    * fix: disable feature + fix pr comments

--- packages/desktop/views/dashboard/collectibles/views/DetailsView.svelte
+++ packages/desktop/views/dashboard/collectibles/views/DetailsView.svelte
@@ -10,48 +10,19 @@
         Text,
     } from 'shared/components'
     import { localize } from '@core/i18n'
+    import { selectedNftId } from '../stores/selected-nft.store'
+    import { getNftByIdFromAllAccountNfts, INft } from '@core/nfts'
+    import { selectedAccountIndex } from '@core/account'
+    import { truncateString } from '@core/utils'
 
-    export let id: string = 'vt7ryeasd6f5hfh689w'
-    export let name: string = 'IOTABOTS #5'
-    export let collection: string = 'IOTABOTS'
-    export let collectionUrl: string = 'https://iotabots.io'
-    export let type: string = 'IRC27'
-    export let storageDeposit: string = '0.5 MI'
-    export let attributes: { title: string; value: string | number | boolean }[] = [
-        {
-            title: 'Background',
-            value: 'Purple',
-        },
-        {
-            title: 'Element',
-            value: 'Water',
-        },
-        {
-            title: 'Attack',
-            value: 150,
-        },
-        {
-            title: 'Super Long Attribute',
-            value: 2000,
-        },
-        {
-            title: 'Another Attribute',
-            value: 1000,
-        },
+    const nft: INft = getNftByIdFromAllAccountNfts($selectedAccountIndex, $selectedNftId)
 
-        {
-            title: 'Long Attribute',
-            value: 2000,
-        },
-        {
-            title: 'Another Attribute',
-            value: 1000,
-        },
-    ]
+    const { id, name } = nft
+    const { type, collectionName, attributes } = nft.parsedMetadata || {}
 
     let modal: Modal
 
-    $: displayedId = id.substring(0, 6) + '...' + id.substring(id.length - 6, id.length)
+    const storageDeposit = 0 // TODO: get storage deposit from corresponding activity
 </script>
 
 <div class="flex flex-row w-full space-x-4 overflow-auto">
@@ -67,12 +38,7 @@
         <div class="overflow-y-scroll h-full">
             <div class="space-y-2 mb-6">
                 <KeyValueBox keyText={localize('views.collectibles.details.collection')}>
-                    <a slot="value" href={collectionUrl} class="flex items-center" target="_blank">
-                        <Icon icon="export" classes="text-gray-600 dark:text-gray-500 mr-1 w-4 h-4" />
-                        <Text fontSize="14" lineHeight="5" color="gray-600" darkColor="gray-500" classes="truncate">
-                            {collection}
-                        </Text>
-                    </a>
+                    {collectionName}
                 </KeyValueBox>
                 <KeyValueBox
                     keyText={localize('views.collectibles.details.nftId')}
@@ -83,7 +49,7 @@
                     <div slot="value" class="flex items-center">
                         <Icon icon="copy" classes="text-gray-600 dark:text-gray-500 mr-1 w-4 h-4" />
                         <Text fontSize="14" lineHeight="5" color="gray-600" darkColor="gray-500" classes="truncate">
-                            {displayedId}
+                            {truncateString(id, 6, 6)}
                         </Text>
                     </div>
                 </KeyValueBox>
@@ -94,23 +60,21 @@
                     valueText={storageDeposit}
                 />
             </div>
-            <div>
-                <Text type="h3" classes="mb-4">{localize('views.collectibles.details.attributes')}</Text>
-                <div class="flex flex-wrap">
-                    {#each attributes as attribute}
-                        <div class="flex flex-col bg-gray-50 dark:bg-gray-850 rounded-2xl text-gray-800 p-3 mr-3 mb-3">
-                            <Text type="p" color="gray-500" darkColor="gray-500" bold>{attribute.title}</Text>
-                            <Text type="p" color="gray-800" darkColor="gray-400" bold>{attribute.value}</Text>
-                        </div>
-                    {/each}
-                    {#each attributes as attribute}
-                        <div class="flex flex-col bg-gray-50 dark:bg-gray-850 rounded-2xl text-gray-800 p-3 mr-3 mb-3">
-                            <Text type="p" color="gray-500" darkColor="gray-500" bold>{attribute.title}</Text>
-                            <Text type="p" color="gray-800" darkColor="gray-400" bold>{attribute.value}</Text>
-                        </div>
-                    {/each}
+            {#if attributes}
+                <div>
+                    <Text type="h3" classes="mb-4">{localize('views.collectibles.details.attributes')}</Text>
+                    <div class="flex flex-wrap">
+                        {#each Object.values(attributes) as attribute}
+                            <div
+                                class="flex flex-col bg-gray-50 dark:bg-gray-850 rounded-2xl text-gray-800 p-3 mr-3 mb-3"
+                            >
+                                <Text type="p" color="gray-500" darkColor="gray-500" bold>{attribute.trait_type}</Text>
+                                <Text type="p" color="gray-800" darkColor="gray-400" bold>{attribute.value}</Text>
+                            </div>
+                        {/each}
+                    </div>
                 </div>
-            </div>
+            {/if}
         </div>
         <div class="flex w-full space-x-4 self-end mt-auto pt-4">
             <Button outline classes="flex-1">{localize('general.viewOnExplorer')}</Button>
