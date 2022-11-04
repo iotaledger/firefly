f6d2c30e866fbab2f8216857894a3c9c2b9c2506
Begoña Alvarez <balvarez@boxfish.studio>
2022-11-04 14:59:28 +0100

    feat: kickoff send amount

--- packages/mobile/views/dashboard/drawers/send/views/AmountView.svelte
+++ packages/mobile/views/dashboard/drawers/send/views/AmountView.svelte
@@ -1,1 +1,25 @@
-Amount placeholder
+<script lang="typescript">
+    import { onMount } from 'svelte'
+
+    import { IotaUnit } from '@core/utils'
+    import { IAsset, newTransactionDetails, selectedAccountAssets, updateNewTransactionDetails } from '@core/wallet'
+
+    import { AssetList } from '../../../../../components'
+    import { sendRouter } from '../../../../../lib/routers'
+
+    function onAssetClick(asset: IAsset) {
+        updateNewTransactionDetails({ type: $newTransactionDetails.type, asset })
+        $sendRouter.next()
+    }
+
+    onMount(() => {
+        updateNewTransactionDetails({
+            type: $newTransactionDetails.type,
+            unit: $newTransactionDetails?.unit ?? IotaUnit.M,
+        })
+    })
+</script>
+
+<div class="w-full overflow-y-auto flex flex-auto h-1">
+    <AssetList onAssetTileClick={onAssetClick} assets={$selectedAccountAssets} />
+</div>
