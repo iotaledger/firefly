8f00db69ecb9d9e79887e1c2c79e36f31c3c4a4b
Matthew Maxwell <maxwellmattryan@gmail.com>
2022-11-09 15:02:32 -0600

    chore: remove last hard dep from core modules

--- packages/shared/lib/core/app/interfaces/platform-event-map.interface.ts
+++ packages/shared/lib/core/app/interfaces/platform-event-map.interface.ts
@@ -1,11 +1,8 @@
-import { DashboardRoute } from '@core/router'
-
 import { IAppUpdateDownloadProgress } from './app-update-download-progress.interface'
 import { IAppVersionDetails } from './app-version-details.interface'
 
 export interface IPlatformEventMap {
     'menu-logout': void
-    'menu-navigate-wallet': DashboardRoute
     'menu-navigate-settings': void
     'menu-check-for-update': void
     'menu-error-log': void
