b973e6af55c784648c6c65fbb0d0e112a631ca2a
Matthew Maxwell <maxwellmattryan@gmail.com>
2022-11-11 11:38:06 -0600

    chore: create helper file

--- packages/desktop/lib/routers/actions/goToAppContext.ts
+++ packages/desktop/lib/routers/actions/goToAppContext.ts
@@ -1,7 +1,9 @@
 import { get } from 'svelte/store'
 
 import { AppContext } from '@core/app/enums'
-import { AppRoute, appRouter } from '@core/router'
+import { appRouter } from '@core/router'
+
+import { getAppRouteForContext } from '../helpers'
 import { resetRouters } from './resetRouters'
 
 export function goToAppContext(context: AppContext): void {
@@ -12,19 +14,5 @@ export function goToAppContext(context: AppContext): void {
         get(appRouter).goTo(appRoute)
     } else {
         throw new Error(`No app-level route defined for the "${context}" context.`)
-    }
-}
-
-// TODO: Put this in helpers
-function getAppRouteForContext(context: AppContext): AppRoute {
-    switch (context) {
-        case AppContext.Dashboard:
-            return AppRoute.Dashboard
-        case AppContext.Login:
-            return AppRoute.Login
-        case AppContext.Onboarding:
-            return AppRoute.Onboarding
-        default:
-            return undefined
     }
 }
