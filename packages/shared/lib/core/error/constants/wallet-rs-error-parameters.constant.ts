2508df307bd4606a1d1e809e20a65565bf005e71
Nicole O'Brien <nicole.obrien@iota.org>
2022-11-15 20:30:26 +0000

    feat: add error handling to consolidation

--- packages/shared/lib/core/error/constants/wallet-rs-error-parameters.constant.ts
+++ packages/shared/lib/core/error/constants/wallet-rs-error-parameters.constant.ts
@@ -4,6 +4,7 @@ import { IErrorParameters } from '../interfaces'
 export const WALLET_RS_ERROR_PARAMETERS: Readonly<{
     [WalletRsError.ClientError]?: { [key in ClientError]?: Partial<IErrorParameters> }
     [WalletRsError.InsufficientFunds]?: Partial<IErrorParameters>
+    [WalletRsError.NoOutputsToConsolidate]?: Partial<IErrorParameters>
     [WalletRsError.IotaClientError]?: { [key in IotaClientError]?: Partial<IErrorParameters> }
 }> = {
     [WalletRsError.ClientError]: {
@@ -23,7 +24,13 @@ export const WALLET_RS_ERROR_PARAMETERS: Readonly<{
     [WalletRsError.InsufficientFunds]: {
         localizationKey: 'error.send.insufficientFundsStorageDeposit',
         logToConsole: true,
-        saveToErrorLog: true,
+        saveToErrorLog: false,
+        showNotification: true,
+    },
+    [WalletRsError.NoOutputsToConsolidate]: {
+        localizationKey: 'error.noOutputsToConsolidate',
+        logToConsole: true,
+        saveToErrorLog: false,
         showNotification: true,
     },
     [WalletRsError.IotaClientError]: {
--- packages/shared/lib/core/wallet/actions/consolidateOutputs.ts
+++ packages/shared/lib/core/wallet/actions/consolidateOutputs.ts
@@ -10,13 +10,13 @@ export async function consolidateOutputs(): Promise<void> {
     const account = get(selectedAccount)
     const _activeProfile = get(activeProfile)
     try {
-        const transaction = await account.consolidateOutputs(true)
+        const transaction = await account.consolidateOutputs(false, 2)
         const processedTransaction = preprocessTransaction(transaction)
         const activity = generateActivity(processedTransaction, account)
         addActivityToAccountActivitiesInAllAccountActivities(account.index, activity)
     } catch (err) {
         if (_activeProfile.type === ProfileType.Ledger) {
-            handleLedgerError(err.error)
+            handleLedgerError(err)
         } else {
             handleError(err)
         }
