f7d71419812c1b8868cd69b4a8ea57678361c9e7
Tuditi <daviddetroch@pm.me>
2022-11-15 17:50:27 +0100

    fix: show error correctly when of type string

--- packages/shared/lib/core/error/handlers/handleGenericError.ts
+++ packages/shared/lib/core/error/handlers/handleGenericError.ts
@@ -1,10 +1,20 @@
 import { IError } from '../interfaces'
 import { logAndNotifyError } from '../actions'
 
-export function handleGenericError(error: IError): void {
+export function handleGenericError(error: IError | string): void {
+    let message: string
+    let type: string
+    if (typeof error === 'string') {
+        message = error
+        type = 'Generic'
+    } else {
+        message = error?.message ?? error?.error
+        type = error?.type ?? 'Generic'
+    }
+
     logAndNotifyError({
-        type: error?.type ?? 'Generic',
-        message: error?.message ?? error?.error,
+        type,
+        message,
         logToConsole: true,
         saveToErrorLog: true,
         showNotification: true,
