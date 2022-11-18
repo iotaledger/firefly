cdcde6beb9a2bfe496482ed9e175964d1dbbfe7d
Mark Nardi <mark.nardi@iota.org>
2022-11-18 14:31:27 +0100

    enhancement: add alias address to foundry tab

--- packages/shared/components/molecules/activity-info/FoundryActivityInformation.svelte
+++ packages/shared/components/molecules/activity-info/FoundryActivityInformation.svelte
@@ -1,18 +1,16 @@
 <script lang="typescript">
     import { KeyValueBox } from 'shared/components'
     import { localize } from '@core/i18n'
-    import { FoundryActivity, getAssetFromPersistedAssets } from '@core/wallet'
+    import { FoundryActivity } from '@core/wallet'
 
     export let activity: FoundryActivity
 
-    $: asset = getAssetFromPersistedAssets(activity.assetId)
-
     let detailsList: { [key in string]: string }
     $: detailsList = {
-        tokenName: asset.metadata.name,
-        decimals: String(asset.metadata.decimals),
-        totalSupply: String(parseInt(activity.maximumSupply, 16)),
-        circulatingSupply: String(parseInt(activity.mintedTokens, 16)),
+        aliasAddress: activity.aliasAddress,
+        maximumSupply: String(parseInt(activity.maximumSupply, 16)),
+        mintedTokens: String(parseInt(activity.mintedTokens, 16)),
+        meltedTokens: String(parseInt(activity.meltedTokens, 16)),
     }
 </script>
 
