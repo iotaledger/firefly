22dd0de144a66e2e3faa45a1bd548c5112e005f6
Tuditi <daviddetroch@pm.me>
2022-11-18 16:30:25 +0100

    fix: correct keccak algorithm

--- packages/shared/lib/core/layer-2/utils/validateEthereumAddress.ts
+++ packages/shared/lib/core/layer-2/utils/validateEthereumAddress.ts
@@ -5,8 +5,10 @@ import { localize } from '@core/i18n'
 import { networkHrp } from '@core/network'
 import { validateBech32Address } from '@core/utils'
 
+const HASH_SIZE = 256
+
 export function validateEthereumAddress(address: string): string {
-    if (validateBech32Address(get(networkHrp), address)) {
+    if (!validateBech32Address(get(networkHrp), address)) {
         return localize('error.layer2.layer1Recipient')
     }
     // Check if it has the basic requirements of an address
@@ -24,8 +26,8 @@ export function validateEthereumAddress(address: string): string {
 // Check if EIP-55 mixed-case checksum address encoding is followed (https://eips.ethereum.org/EIPS/eip-55)
 function isChecksumAddress(address: string): string {
     address = address.replace('0x', '')
-    const keccak = new Keccak<256>()
-    const addressHash = keccak.update(address.toLowerCase()).digest('hex')
+    const keccak256 = new Keccak(HASH_SIZE)
+    const addressHash = keccak256.update(address.toLowerCase()).digest('hex')
     for (let i = 0; i < address.length; i++) {
         // The nth letter should be uppercase if the nth digit of casemap is 1
         if (
