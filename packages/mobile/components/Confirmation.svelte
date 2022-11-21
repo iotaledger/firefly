9cc89b4b9dabcd3d4d2322ec7248ed976fc3074e
Paul Bögelsack <pboegelsack@googlemail.com>
2022-11-21 09:20:53 +0100

    feat-mobile: add confirmation component

---
+++ packages/mobile/components/Confirmation.svelte
@@ -0,0 +1,31 @@
+<script lang="typescript">
+    import { localize } from '@core/i18n'
+    import { Button, ButtonVariant, Text, TextHint } from 'shared/components'
+
+    export let description: string = ''
+    export let hint: string = ''
+    export let info: boolean = false
+    export let success: boolean = false
+    export let warning: boolean = false
+    export let danger: boolean = false
+    export let confirmText: string = localize('actions.confirm')
+    export let onConfirm: () => void = undefined
+</script>
+
+<div class="w-full h-full space-y-6 flex flex-auto flex-col flex-shrink-0">
+    <div class="space-y-4">
+        {#if description}
+            <Text fontSize="14" classes="text-left">{description}</Text>
+        {/if}
+        {#if hint}
+            <TextHint {info} {success} {warning} {danger} text={hint} />
+        {/if}
+    </div>
+    <Button
+        classes="w-full"
+        variant={warning || danger ? ButtonVariant.Warning : ButtonVariant.Primary}
+        onClick={onConfirm}
+    >
+        {confirmText}
+    </Button>
+</div>
