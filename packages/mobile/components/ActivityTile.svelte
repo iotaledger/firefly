334da391ef18958730bbda44d9e606c96a8300eb
Paul Bögelsack <pboegelsack@googlemail.com>
2022-11-21 10:46:50 +0100

    feat-mobile: move activity router logic to activity tab

--- packages/mobile/components/ActivityTile.svelte
+++ packages/mobile/components/ActivityTile.svelte
@@ -1,6 +1,5 @@
 <script lang="typescript">
-    import { isStrongholdUnlocked } from '@core/profile-manager'
-    import { Activity, ActivityAsyncStatus, ActivityType, claimActivity, InclusionState } from '@core/wallet'
+    import { Activity, ActivityAsyncStatus, ActivityType, InclusionState } from '@core/wallet'
     import {
         AliasActivityTileContent,
         ClickableTile,
@@ -10,38 +9,14 @@
         TransactionActivityTileContent,
     } from 'shared/components'
     import { AsyncActivityTileFooter } from '../components'
-    import { activityRouter, ActivityRoute } from '../lib/routers'
-    import { selectedActivity } from '../lib/wallet'
 
     export let activity: Activity
     export let onClick: () => unknown = () => {}
-
-    function handleOnClick(): void {
-        $selectedActivity = activity
-        $activityRouter?.goTo(ActivityRoute.Details)
-        onClick()
-    }
-
-    function onReject(): void {
-        $selectedActivity = activity
-        $activityRouter?.goTo(ActivityRoute.Reject)
-    }
-
-    async function onClaim(): Promise<void> {
-        const isUnlocked = await isStrongholdUnlocked()
-        if (isUnlocked) {
-            claimActivity(activity)
-        } else {
-            $selectedActivity = activity
-            $activityRouter?.goTo(ActivityRoute.Password)
-        }
-    }
+    export let onReject: () => unknown = () => {}
+    export let onClaim: () => unknown = () => {}
 </script>
 
-<ClickableTile
-    onClick={handleOnClick}
-    classes={activity.inclusionState === InclusionState.Confirmed ? '' : 'opacity-50'}
->
+<ClickableTile {onClick} classes={activity.inclusionState === InclusionState.Confirmed ? '' : 'opacity-50'}>
     <activity-tile class="w-full flex flex-col space-y-4">
         <tile-content class="flex flex-row items-center text-left space-x-4">
             {#if activity.type === ActivityType.Transaction}
--- packages/mobile/views/dashboard/tabs/ActivityTab.svelte
+++ packages/mobile/views/dashboard/tabs/ActivityTab.svelte
@@ -1,5 +1,29 @@
 <script lang="typescript">
+    import { isStrongholdUnlocked } from '@core/profile-manager'
+    import { Activity, claimActivity } from '@core/wallet'
     import { ActivityList } from '../../../../mobile/components'
+    import { ActivityRoute, activityRouter } from '../../../lib/routers'
+    import { selectedActivity } from '../../../lib/wallet'
+
+    function onTileClick(activity: Activity): void {
+        $selectedActivity = activity
+        $activityRouter?.goTo(ActivityRoute.Details)
+    }
+
+    function onReject(activity: Activity): void {
+        $selectedActivity = activity
+        $activityRouter?.goTo(ActivityRoute.Reject)
+    }
+
+    async function onClaim(activity: Activity): Promise<void> {
+        const isUnlocked = await isStrongholdUnlocked()
+        if (isUnlocked) {
+            claimActivity(activity)
+        } else {
+            $selectedActivity = activity
+            $activityRouter?.goTo(ActivityRoute.Password)
+        }
+    }
 </script>
 
-<ActivityList />
+<ActivityList {onTileClick} {onClaim} {onReject} />
