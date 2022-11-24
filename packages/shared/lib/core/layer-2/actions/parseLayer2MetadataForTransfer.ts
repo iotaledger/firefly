41be6ea72383369042f046e226b1ca507592611b
Tuditi <daviddetroch@pm.me>
2022-11-24 11:39:59 +0100

    chore: refactore

--- packages/shared/lib/core/layer-2/actions/parseLayer2MetadataForTransfer.ts
+++ packages/shared/lib/core/layer-2/actions/parseLayer2MetadataForTransfer.ts
@@ -2,6 +2,7 @@ import { Converter } from '@core/utils'
 import { ReadStream } from '@iota/util.js'
 import { Allowance, FORCE_OPEN_ACCOUNT, ILayer2TransferAllowanceMetadata } from '@core/layer-2'
 import { NativeTokenAmount, TOKEN_ID_BYTE_LENGTH } from '@core/token'
+import { ILayer2Allowance } from '@core/layer-2/interfaces/layer2-allowance.interface'
 
 export function parseLayer2MetadataForTransfer(metadata: Uint8Array): ILayer2TransferAllowanceMetadata {
     const readStream = new ReadStream(metadata)
@@ -11,9 +12,28 @@ export function parseLayer2MetadataForTransfer(metadata: Uint8Array): ILayer2Tra
     const contractFunction = readStream.readUInt32('contractFunction')
     const gasBudget = readStream.readUInt64('gasBudget')
 
+    const smartContractParameters = parseSmartContractParameters(readStream)
+    const { baseTokenAmount, nativeTokens } = parseAllowance(readStream)
+
+    const ethereumAddress = '0x' + smartContractParameters['a'].substring(2)
+    const forceOpenAccount = smartContractParameters['c'] === FORCE_OPEN_ACCOUNT
+
+    return {
+        senderContract: Converter.decimalToHex(senderContract, true),
+        targetContract: Converter.decimalToHex(targetContract, true),
+        contractFunction: Converter.decimalToHex(contractFunction, true),
+        gasBudget: gasBudget.toString(),
+        ethereumAddress,
+        forceOpenAccount,
+        baseTokenAmount,
+        nativeTokens,
+    }
+}
+
+function parseSmartContractParameters(readStream: ReadStream): Record<string, string> {
     const smartContractParametersAmount = readStream.readUInt32('parametersLength')
     const smartContractParameters: Record<string, string> = {}
-    // How do we know what type of data is represented by the bytes?
+
     for (let index = 0; index < smartContractParametersAmount; index++) {
         const keyLength = readStream.readUInt16('keyLength')
         const keyBytes = readStream.readBytes('keyValue', keyLength)
@@ -27,37 +47,29 @@ export function parseLayer2MetadataForTransfer(metadata: Uint8Array): ILayer2Tra
         smartContractParameters[key] = value
     }
 
-    const remainingBytes = readStream.unused()
-    if (remainingBytes <= 2) {
-        console.error('End reached')
-        return
-    } else {
-        const allowance = readStream.readUInt8('allowance')
+    return smartContractParameters
+}
 
-        if (allowance === Allowance.Set) {
-            const baseTokenAmount = readStream.readUInt64('baseTokenAmount').toString()
-            const tokenBufferLength = readStream.readUInt16('tokenBufferBytes.length')
-            const tokenAmount = Math.floor(tokenBufferLength / (TOKEN_ID_BYTE_LENGTH + 32))
-            const nativeTokens: NativeTokenAmount[] = []
+function parseAllowance(readStream: ReadStream): ILayer2Allowance {
+    const allowance = readStream.readUInt8('allowance')
 
-            for (let token = 0; token < tokenAmount; token++) {
-                const tokenId = Converter.bytesToHex(readStream.readBytes('tokenId', TOKEN_ID_BYTE_LENGTH))
-                const amount = readStream.readUInt256('tokenAmount').toString()
-                nativeTokens.push({ tokenId, amount })
-            }
+    if (allowance === Allowance.Set) {
+        const baseTokenAmount = readStream.readUInt64('baseTokenAmount').toString()
+        const tokenBufferLength = readStream.readUInt16('tokenBufferBytes.length')
+        const tokenAmount = Math.floor(tokenBufferLength / (TOKEN_ID_BYTE_LENGTH + 32))
+        const nativeTokens: NativeTokenAmount[] = []
 
-            return {
-                senderContract: Converter.decimalToHex(senderContract, true),
-                targetContract: Converter.decimalToHex(targetContract, true),
-                contractFunction: Converter.decimalToHex(contractFunction, true),
-                gasBudget: gasBudget.toString(),
-                ethereumAddress: '0x' + smartContractParameters['a'].substring(2),
-                forceOpenAccount: smartContractParameters['c'] === FORCE_OPEN_ACCOUNT,
-                baseTokenAmount,
-                nativeTokens,
-            }
-        } else {
-            return
+        for (let token = 0; token < tokenAmount; token++) {
+            const tokenId = Converter.bytesToHex(readStream.readBytes('tokenId', TOKEN_ID_BYTE_LENGTH))
+            const amount = readStream.readUInt256('tokenAmount').toString()
+            nativeTokens.push({ tokenId, amount })
         }
+
+        return {
+            baseTokenAmount,
+            nativeTokens,
+        }
+    } else {
+        return
     }
 }
--- packages/shared/lib/core/layer-2/interfaces/layer2-smart-contract-call-data.interface.ts
+++ packages/shared/lib/core/layer-2/interfaces/layer2-smart-contract-call-data.interface.ts
@@ -1,10 +1,8 @@
-import { NativeTokenAmount } from '@core/token'
+import { ILayer2Allowance } from '@core/layer-2/interfaces/layer2-allowance.interface'
 
-export interface ILayer2SmartContractCallData {
+export interface ILayer2SmartContractCallData extends ILayer2Allowance {
     senderContract: string
     targetContract: string
     contractFunction: string
     gasBudget: string
-    baseTokenAmount: string
-    nativeTokens: NativeTokenAmount[]
 }
