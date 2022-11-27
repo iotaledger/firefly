b4507fe95ae48d5962b2189a4ad84665dcf3ea17
Jean Ribeiro <iamjeanribeiro@gmail.com>
2022-11-27 20:12:30 -0300

    feat: adds SmartContractActivityInformation component

---
+++ packages/shared/components/molecules/activity-info/SmartContractActivityInformation.svelte
@@ -0,0 +1,29 @@
+<script lang="typescript">
+    import { KeyValueBox } from 'shared/components'
+    import { localize } from '@core/i18n'
+    import { NftActivity } from '@core/wallet'
+    import { parseLayer2Metadata } from '@core/layer-2'
+
+    export let activity: NftActivity
+
+    $: layer2Metadata = parseLayer2Metadata(activity.metadata)
+    $: detailsList = createDetailsList(layer2Metadata)
+
+    function createDetailsList(metadata: typeof layer2Metadata): { [key: string]: { data: unknown } } {
+        return {
+            ...(metadata?.targetContract && {
+                targetContract: { data: metadata.targetContract },
+            }),
+            ...(metadata?.contractFunction && {
+                contractFunction: { data: metadata.contractFunction },
+            }),
+            ...(metadata?.gasBudget && {
+                gasBudget: { data: metadata.gasBudget },
+            }),
+        }
+    }
+</script>
+
+{#each Object.entries(detailsList) as [key, value]}
+    <KeyValueBox keyText={localize(`general.${key}`)} valueText={value.data} isCopyable />
+{/each}
