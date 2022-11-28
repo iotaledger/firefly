7056d90dd5422ca37826b10d9ebe9bf30ff05a48
Mark Nardi <mark.nardi@iota.org>
2022-11-28 16:09:47 +0100

    enhancement: add content if no nfts are owned

--- packages/desktop/views/dashboard/collectibles/Collectibles.svelte
+++ packages/desktop/views/dashboard/collectibles/Collectibles.svelte
@@ -4,12 +4,10 @@
 </script>
 
 <div class="w-full h-full flex flex-col flex-nowrap p-10 relative flex-1 bg-gray-50 dark:bg-gray-900">
-    <div class="w-full h-full grid grid-cols-1 gap-x-4 min-h-0">
-        {#if $collectiblesRoute === CollectiblesRoute.Gallery}
-            <GalleryView />
-        {/if}
-        {#if $collectiblesRoute === CollectiblesRoute.Details}
-            <DetailsView />
-        {/if}
-    </div>
+    {#if $collectiblesRoute === CollectiblesRoute.Gallery}
+        <GalleryView />
+    {/if}
+    {#if $collectiblesRoute === CollectiblesRoute.Details}
+        <DetailsView />
+    {/if}
 </div>
