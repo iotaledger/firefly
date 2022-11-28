5e91a39a6f30b44fda2a0de1788e5db75d274f8f
Mark Nardi <mark.nardi@iota.org>
2022-11-28 13:31:40 +0100

    enhancement: update gallery view ui

--- packages/desktop/views/dashboard/collectibles/views/GalleryView.svelte
+++ packages/desktop/views/dashboard/collectibles/views/GalleryView.svelte
@@ -3,6 +3,7 @@
     import { CollectiblesRoute, collectiblesRouter } from '@core/router'
     import { INft, selectedAccountNfts } from '@core/nfts'
     import { selectedNftId } from '../stores/selected-nft.store'
+    import { localize } from '@core/i18n'
 
     function handleNftClick(nft: INft): void {
         $selectedNftId = nft.id
@@ -10,7 +11,11 @@
     }
 </script>
 
-<Text type={TextType.h3}>Collectibles</Text>
+<div class="flex flex-row text-left space-x-1">
+    <Text type={TextType.h3}>{localize('views.collectibles.gallery.title')}</Text>
+    <Text type={TextType.h3} color="gray-500">• {$selectedAccountNfts.length}</Text>
+</div>
+
 <div class="h-full flex flex-wrap flex-row gap-6 mt-7 pb-7 scrollable-y">
     {#each $selectedAccountNfts as nft}
         <NftGalleryItem {nft} onClick={() => handleNftClick(nft)} />
