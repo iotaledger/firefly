18776c6122577a37187caee6ef80d32a39387189
Jean Ribeiro <iamjeanribeiro@gmail.com>
2022-11-28 16:22:06 -0300

    fix: adds formatted gas budget

--- packages/shared/components/molecules/activity-info/SmartContractActivityInformation.svelte
+++ packages/shared/components/molecules/activity-info/SmartContractActivityInformation.svelte
@@ -1,12 +1,18 @@
 <script lang="typescript">
     import { KeyValueBox } from 'shared/components'
     import { localize } from '@core/i18n'
-    import { BaseActivity } from '@core/wallet'
+    import { formatTokenAmountPrecise, TransactionActivity } from '@core/wallet'
     import { Layer2Metadata } from '@core/layer-2'
+    import { BASE_TOKEN } from '@core/network'
+    import { activeProfile } from '@core/profile'
 
-    export let activity: BaseActivity
+    export let activity: TransactionActivity
 
     $: detailsList = createDetailsList(activity.parsedLayer2Metadata)
+    $: formattedGasBudget = formatTokenAmountPrecise(
+        Number(activity.parsedLayer2Metadata?.gasBudget) ?? 0,
+        BASE_TOKEN[$activeProfile?.networkProtocol]
+    )
 
     function createDetailsList(metadata: Layer2Metadata): { [key: string]: { data: unknown } } {
         return {
@@ -17,7 +23,7 @@
                 contractFunction: { data: metadata.contractFunction },
             }),
             ...(metadata?.gasBudget && {
-                gasBudget: { data: metadata.gasBudget },
+                gasBudget: { data: formattedGasBudget },
             }),
         }
     }
