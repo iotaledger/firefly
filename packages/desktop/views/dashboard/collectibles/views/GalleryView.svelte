f92fca397656bfcaa58e6be4ace491e164bfbd84
Mark Nardi <mark.nardi@iota.org>
2022-11-29 16:25:21 +0100

    enhancement: update ui if nft list is empty because of filters

--- packages/desktop/views/dashboard/collectibles/views/GalleryView.svelte
+++ packages/desktop/views/dashboard/collectibles/views/GalleryView.svelte
@@ -32,49 +32,63 @@
 </script>
 
 <div class="flex flex-col w-full h-full space-y-4">
-    <div class="flex flex-row justify-between">
-        <div class="flex flex-row text-left space-x-1 items-center">
-            <Text fontSize="text-14" fontWeight={FontWeight.semibold}
-                >{localize('views.collectibles.gallery.title')}</Text
-            >
-            <Text fontSize="text-14" fontWeight={FontWeight.semibold} color="gray-500">• {$queriedNfts.length}</Text>
+    {#if $selectedAccountNfts.length}
+        <div class="flex flex-row justify-between">
+            <div class="flex flex-row text-left space-x-1 items-center">
+                <Text fontSize="text-14" fontWeight={FontWeight.semibold}
+                    >{localize('views.collectibles.gallery.title')}</Text
+                >
+                <Text fontSize="text-14" fontWeight={FontWeight.semibold} color="gray-500">• {$queriedNfts.length}</Text
+                >
+            </div>
+
+            <div class="flex items-center" style="height: 40px">
+                {#if searchActive}
+                    <TextInput
+                        bind:inputElement
+                        bind:value={searchValue}
+                        hasFocus={true}
+                        placeholder={localize('general.search')}
+                        fontSize="15"
+                        clearPadding
+                        containerClasses="py-2 px-3"
+                        fontWeight={FontWeight.medium}
+                        color="gray-500"
+                    >
+                        <Icon slot="left" icon="search" classes="mr-2 text-gray-500 dark:text-white" />
+                        <button on:click={() => (searchActive = false)} slot="right">
+                            <Icon
+                                icon="close"
+                                classes="cursor-pointer ml-2 text-gray-500 dark:text-white hover:text-gray-600 dark:hover:text-gray-200"
+                            />
+                        </button>
+                    </TextInput>
+                {:else}
+                    <TogglableButton icon="search" bind:active={searchActive} />
+                {/if}
+            </div>
         </div>
 
-        <div class="flex items-center" style="height: 40px">
-            {#if searchActive}
-                <TextInput
-                    bind:inputElement
-                    bind:value={searchValue}
-                    hasFocus={true}
-                    placeholder={localize('general.search')}
-                    fontSize="15"
-                    clearPadding
-                    containerClasses="py-2 px-3"
-                    fontWeight={FontWeight.medium}
-                    color="gray-500"
-                >
-                    <Icon slot="left" icon="search" classes="mr-2 text-gray-500 dark:text-white" />
-                    <button on:click={() => (searchActive = false)} slot="right">
-                        <Icon
-                            icon="close"
-                            classes="cursor-pointer ml-2 text-gray-500 dark:text-white hover:text-gray-600 dark:hover:text-gray-200"
-                        />
-                    </button>
-                </TextInput>
+        <div class="w-full h-full flex items-center justify-center grow-1">
+            {#if $queriedNfts.length}
+                <div class="h-full flex flex-wrap flex-row gap-6 scrollable-y">
+                    {#each $queriedNfts as nft}
+                        <NftGalleryItem {nft} onClick={() => handleNftClick(nft)} />
+                    {/each}
+                </div>
             {:else}
-                <TogglableButton icon="search" bind:active={searchActive} />
+                <div class="flex flex-col items-center space-y-8">
+                    <Illustration illustration="empty-collectibles" width="134" height="134" />
+                    <div class="flex flex-col items-center">
+                        <Text fontSize="text-14" fontWeight={FontWeight.semibold} color="gray-500"
+                            >{localize('views.collectibles.gallery.noResults')}</Text
+                        >
+                    </div>
+                </div>
             {/if}
         </div>
-    </div>
-
-    <div class="w-full h-full flex items-center justify-center grow-1">
-        {#if $queriedNfts.length}
-            <div class="h-full flex flex-wrap flex-row gap-6 scrollable-y">
-                {#each $queriedNfts as nft}
-                    <NftGalleryItem {nft} onClick={() => handleNftClick(nft)} />
-                {/each}
-            </div>
-        {:else}
+    {:else}
+        <div class="w-full h-full flex items-center justify-center grow-1">
             <div class="flex flex-col items-center space-y-8">
                 <Illustration illustration="empty-collectibles" width="134" height="134" />
                 <div class="flex flex-col items-center">
@@ -82,17 +96,11 @@
                         >{localize('views.collectibles.gallery.emptyTitle')}</Text
                     >
                     <Text fontSize="text-14" color="gray-500"
-                        >{localize(
-                            `views.collectibles.gallery.${
-                                $selectedAccountNfts.length === 0 ? 'emptyDescription' : 'emptyDescription2'
-                            }`
-                        )}</Text
+                        >{localize('views.collectibles.gallery.emptyDescription')}</Text
                     >
                 </div>
-                {#if $selectedAccountNfts.length === 0}
-                    <ReceiveButton text={localize('actions.depositNft')} />
-                {/if}
+                <ReceiveButton text={localize('actions.depositNft')} />
             </div>
-        {/if}
-    </div>
+        </div>
+    {/if}
 </div>
