2c07a35542f257384c06789e9ac583facbbbe79e
Mark Nardi <mark.nardi@iota.org>
2022-11-29 14:31:55 +0100

    chore: use already created function

--- packages/shared/lib/core/wallet/actions/burnAsset.ts
+++ packages/shared/lib/core/wallet/actions/burnAsset.ts
@@ -2,6 +2,7 @@ import { selectedAccount } from '@core/account/stores/selected-account.store'
 import { handleError } from '@core/error/handlers/handleError'
 import { handleLedgerError } from '@core/ledger'
 import { activeProfile, ProfileType } from '@core/profile'
+import { Converter } from '@core/utils'
 import { get } from 'svelte/store'
 import { selectedAccountAssets } from '../stores'
 
@@ -12,7 +13,7 @@ export async function burnAsset(assetId: string): Promise<void> {
     try {
         const balance = nativeTokens.find((_asset) => _asset.id === assetId)?.balance.available
         if (balance) {
-            await account.burnNativeToken(assetId, '0x' + balance.toString(16))
+            await account.burnNativeToken(assetId, Converter.decimalToHex(balance, true))
         }
     } catch (err) {
         if (_activeProfile.type === ProfileType.Ledger) {
