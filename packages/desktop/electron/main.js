52c633f03a5088612e26aa65cc582304a7939c3e
Tuditi <daviddetroch@pm.me>
2022-11-30 15:11:47 +0100

    chore: remove deprecated event handler

--- packages/desktop/electron/main.js
+++ packages/desktop/electron/main.js
@@ -296,7 +296,6 @@ function createWindow() {
      * Only allow external navigation to allowed domains
      */
     windows.main.webContents.on('will-navigate', handleNavigation)
-    windows.main.webContents.on('new-window', handleNavigation)
 
     windows.main.on('close', () => {
         closeAboutWindow()
