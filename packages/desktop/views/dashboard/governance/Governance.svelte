62f13a27c50243ebe23cb3fefd128d11d48315ae
Jean Ribeiro <iamjeanribeiro@gmail.com>
2022-12-01 15:37:26 -0300

    feat: adds proposals details initial ui

--- packages/desktop/views/dashboard/governance/Governance.svelte
+++ packages/desktop/views/dashboard/governance/Governance.svelte
@@ -1,14 +1,19 @@
 <script lang="typescript">
-    import { Pane, VotingPower } from '@ui'
+    import { Pane, ProposalsDetails, VotingPower } from '@ui'
     import { selectedAccount } from '@core/account'
 </script>
 
 {#if $selectedAccount}
     <div class="w-full h-full flex flex-nowrap p-8 relative flex-1 bg-gray-50 dark:bg-gray-900">
         {#key $selectedAccount?.index}
-            <Pane classes="w-1/3 p-6 h-fit">
-                <VotingPower />
-            </Pane>
+            <div class="w-1/3 flex flex-col space-y-4">
+                <Pane classes="p-6 h-fit">
+                    <VotingPower />
+                </Pane>
+                <Pane classes="p-6 h-fit">
+                    <ProposalsDetails />
+                </Pane>
+            </div>
         {/key}
     </div>
 {/if}
