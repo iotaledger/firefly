466b9c9678c0e877d59a1635d7b0a162cb74e63e
MarkNerdi <105642810+MarkNerdi@users.noreply.github.com>
2022-12-05 10:19:17 +0100

    enhancement: disable nft/asset select if using nft/asset send button (#5341)
    * enhancement: disable nft selection if using nft send button
    
    * enhancement: disable asset selection if using asset send button
    
    * fix: fix back on send confirmation
    
    * chore: rename to readonly
    
    * chore: improve conditional check

--- packages/shared/lib/core/wallet/types/new-transaction-details.type.ts
+++ packages/shared/lib/core/wallet/types/new-transaction-details.type.ts
@@ -14,6 +14,7 @@ type NewBaseTransactionDetails = {
     disableToggleGift?: boolean
     disableChangeExpiration?: boolean
     addSenderFeature?: boolean
+    disableAssetSelection?: boolean
 }
 
 export type NewTokenTransactionDetails = NewBaseTransactionDetails & {
