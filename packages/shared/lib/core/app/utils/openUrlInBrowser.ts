b3ce2901a5d1089450de95a926d1cdae811c9dc2
Mark Nardi <mark.nardi@iota.org>
2022-12-06 09:42:50 +0100

    enhancement: add confirmation popup

--- packages/shared/lib/core/app/utils/openUrlInBrowser.ts
+++ packages/shared/lib/core/app/utils/openUrlInBrowser.ts
@@ -1,5 +1,24 @@
+import { closePopup, openPopup } from '@auxiliary/popup'
+import { localize } from '@core/i18n'
 import { Platform } from '../classes/platform.class'
+import { externalAllowedLinks } from '../constants'
 
 export function openUrlInBrowser(url: string): void {
-    Platform.openUrl(url)
+    if (externalAllowedLinks.some((allowedLink) => url.includes(allowedLink))) {
+        Platform.openUrl(url)
+    } else {
+        openPopup({
+            type: 'confirmation',
+            props: {
+                title: localize('popups.deleteProfile.title'),
+                hint: localize('popups.deleteProfile.confirmation'),
+                warning: true,
+                confirmText: localize('actions.delete'),
+                onConfirm: () => {
+                    Platform.openUrl(url)
+                    closePopup()
+                },
+            },
+        })
+    }
 }
