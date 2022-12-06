e0b3f0a88de00144fda5235ed549392f6e517cc9
Mark Nardi <mark.nardi@iota.org>
2022-12-06 15:08:10 +0100

    fix: fix url validation

--- packages/shared/lib/core/app/utils/openUrlInBrowser.ts
+++ packages/shared/lib/core/app/utils/openUrlInBrowser.ts
@@ -3,20 +3,23 @@ import { localize } from '@core/i18n'
 import { Platform } from '../classes/platform.class'
 import { externalAllowedLinks } from '../constants'
 
-export function openUrlInBrowser(url: string): void {
-    if (externalAllowedLinks.some((allowedLink) => url.includes(allowedLink))) {
-        Platform.openUrl(url)
+export function openUrlInBrowser(targetUrl: string): void {
+    const url = new URL(targetUrl)
+    const domain = url.hostname.replace('www.', '').replace('mailto:', '')
+
+    if (externalAllowedLinks.includes(domain) || externalAllowedLinks.includes(domain + url.pathname)) {
+        Platform.openUrl(targetUrl)
     } else {
         openPopup({
             type: 'confirmation',
             props: {
                 title: localize('popups.externalUrl.title'),
-                description: localize('popups.externalUrl.body', { values: { url } }),
+                description: localize('popups.externalUrl.body', { values: { targetUrl } }),
                 hint: localize('popups.externalUrl.hint'),
                 warning: true,
                 confirmText: localize('popups.externalUrl.action'),
                 onConfirm: () => {
-                    Platform.openUrl(url)
+                    Platform.openUrl(targetUrl)
                     closePopup()
                 },
             },
