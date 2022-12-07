c5500fe111f0df9aedc5d8c1a9abd1cfb83dc46e
Mark Nardi <mark.nardi@iota.org>
2022-12-07 15:54:07 +0100

    ui: fix padding + scroll behavior

--- packages/desktop/views/dashboard/collectibles/Collectibles.svelte
+++ packages/desktop/views/dashboard/collectibles/Collectibles.svelte
@@ -3,7 +3,7 @@
     import { DetailsView, GalleryView } from './views'
 </script>
 
-<div class="w-full h-full flex flex-col flex-nowrap p-10 relative flex-1 bg-gray-50 dark:bg-gray-900">
+<div class="w-full h-full flex flex-col flex-nowrap p-8 relative flex-1 bg-gray-50 dark:bg-gray-900">
     {#if $collectiblesRoute === CollectiblesRoute.Gallery}
         <GalleryView />
     {/if}
--- packages/desktop/views/dashboard/collectibles/views/GalleryView.svelte
+++ packages/desktop/views/dashboard/collectibles/views/GalleryView.svelte
@@ -69,22 +69,20 @@
             </div>
         </div>
 
-        <div class="w-full h-full ">
-            {#if $queriedNfts.length}
-                <div class="h-full gallery-grid scrollable-y">
-                    {#each $queriedNfts as nft}
-                        <NftGalleryItem {nft} onClick={() => handleNftClick(nft)} />
-                    {/each}
-                </div>
-            {:else}
-                <div class="w-full h-full flex flex-col items-center justify-center space-y-8">
-                    <Illustration illustration="empty-collectibles" width="134" height="134" />
-                    <Text fontSize="text-14" fontWeight={FontWeight.semibold} color="gray-500"
-                        >{localize('views.collectibles.gallery.noResults')}</Text
-                    >
-                </div>
-            {/if}
-        </div>
+        {#if $queriedNfts.length}
+            <div class="h-full gallery-grid items-start scrollable-y">
+                {#each $queriedNfts as nft}
+                    <NftGalleryItem {nft} onClick={() => handleNftClick(nft)} />
+                {/each}
+            </div>
+        {:else}
+            <div class="w-full h-full flex flex-col items-center justify-center space-y-8">
+                <Illustration illustration="empty-collectibles" width="134" height="134" />
+                <Text fontSize="text-14" fontWeight={FontWeight.semibold} color="gray-500"
+                    >{localize('views.collectibles.gallery.noResults')}</Text
+                >
+            </div>
+        {/if}
     {:else}
         <div class="w-full h-full flex items-center justify-center grow-1">
             <div class="flex flex-col items-center space-y-8">
