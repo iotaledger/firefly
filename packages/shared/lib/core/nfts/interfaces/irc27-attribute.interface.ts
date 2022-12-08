7c744f657579d5acf8c8806d6e5ae47aa3ff3ae6
Nicole O'Brien <nicole.obrien@iota.org>
2022-12-08 15:39:45 +0000

    fix: add comment

--- packages/shared/lib/core/nfts/interfaces/irc27-attribute.interface.ts
+++ packages/shared/lib/core/nfts/interfaces/irc27-attribute.interface.ts
@@ -1,3 +1,5 @@
+// This interface does not follow the coding conventions as it is
+// following TIP27 metadata standards
 export interface IIrc27Attribute {
     trait_type: string
     value: string | number
