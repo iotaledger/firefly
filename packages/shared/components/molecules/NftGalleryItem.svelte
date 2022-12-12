239e435414786e05a8dc8b280a56d0877991c8aa
Begoña Alvarez <balvarez@boxfish.studio>
2022-12-12 14:11:43 +0100

    style: fix nft gallery image not being a perfect square

--- packages/shared/components/molecules/NftGalleryItem.svelte
+++ packages/shared/components/molecules/NftGalleryItem.svelte
@@ -14,15 +14,17 @@
     }
 </script>
 
-<button
-    on:click={openCollectiblesDetailsView}
-    class="flex flex-col rounded-2xl overflow-hidden"
-    style="height: {nftWrapperClientWidth}px;"
->
-    <div class="w-full overflow-hidden flex-grow" bind:clientWidth={nftWrapperClientWidth}>
-        <NftMedia nftId={nft.id} classes="bg-gray-200 dark:bg-gray-700 min-w-full min-h-full object-cover" />
-    </div>
-    <div class="w-full flex flex-col justify-center p-3.5 bg-white dark:bg-gray-800">
-        <Text fontWeight={FontWeight.semibold} fontSize="12" classes="text-left truncate">{nft.name}</Text>
+<button on:click={openCollectiblesDetailsView} class="flex flex-col items-center justify-center">
+    <div class="w-full rounded-2xl overflow-hidden flex flex-col">
+        <div
+            class="w-full overflow-hidden flex"
+            bind:clientWidth={nftWrapperClientWidth}
+            style="height: {nftWrapperClientWidth}px; "
+        >
+            <NftMedia nftId={nft.id} classes="bg-gray-200 dark:bg-gray-700 min-w-full min-h-full object-cover" />
+        </div>
+        <div class="w-full flex flex-col justify-center p-3.5 bg-white dark:bg-gray-800">
+            <Text fontWeight={FontWeight.semibold} fontSize="12" classes="text-left truncate">{nft.name}</Text>
+        </div>
     </div>
 </button>
