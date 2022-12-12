7c019f3fbe8ede74722cdf2e6badc7422492b96c
Nicole O'Brien <nicole.obrien@iota.org>
2022-12-12 11:41:44 +0000

    temp: add styling to nft gallery

--- packages/shared/components/molecules/NftGalleryItem.svelte
+++ packages/shared/components/molecules/NftGalleryItem.svelte
@@ -12,8 +12,10 @@
     }
 </script>
 
-<button on:click={openCollectiblesDetailsView} class="flex flex-col w-full aspect-square">
-    <NftMedia nftId={nft.id} classes="bg-gray-200 dark:bg-gray-700 rounded-t-2xl" />
+<button on:click={openCollectiblesDetailsView} class="flex flex-col w-full ">
+    <div class="bg-red-400 w-full flex-grow aspect-square overflow-hidden">
+        <NftMedia nftId={nft.id} classes="bg-gray-200 dark:bg-gray-700 rounded-t-2xl object-cover" />
+    </div>
     <div class="w-full flex flex-col justify-center p-3.5 bg-white dark:bg-gray-800 rounded-b-2xl">
         <Text fontWeight={FontWeight.semibold} fontSize="12" classes="text-left truncate">{nft.name}</Text>
     </div>
