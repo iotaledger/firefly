b3f0391f8855f137cfd2da500ce83a62411ffa09
Nicole O'Brien <nicole.obrien@iota.org>
2022-12-13 15:25:47 +0000

    remove: unused css

--- packages/desktop/views/dashboard/collectibles/views/CollectiblesGalleryView.svelte
+++ packages/desktop/views/dashboard/collectibles/views/CollectiblesGalleryView.svelte
@@ -90,26 +90,3 @@
         </div>
     {/if}
 </div>
-
-<style lang="scss">
-    .gallery-grid {
-        --grid-layout-gap: 1rem;
-        --grid-column-count: 9;
-        --grid-item--min-width: 240px;
-
-        /**
-        * Calculated values.
-        */
-        --gap-count: calc(var(--grid-column-count) - 1);
-        --total-gap-width: calc(var(--gap-count) * var(--grid-layout-gap));
-        --grid-item--max-width: calc((100% - var(--total-gap-width)) / var(--grid-column-count));
-
-        display: grid;
-        grid-template-columns: repeat(
-            auto-fill,
-            minmax(max(var(--grid-item--min-width), var(--grid-item--max-width)), 1fr)
-        );
-        grid-template-rows: auto;
-        grid-gap: var(--grid-layout-gap);
-    }
-</style>
