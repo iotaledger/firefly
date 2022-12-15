603bdc3f976706ce9d1705f23e4862025367a8d1
Begoña Alvarez <balvarez@boxfish.studio>
2022-12-15 15:58:59 +0100

    chore: remove debris

--- packages/mobile/views/dashboard/drawers/profile/views/settings/SettingsRouter.svelte
+++ packages/mobile/views/dashboard/drawers/profile/views/settings/SettingsRouter.svelte
@@ -1,42 +1,10 @@
 <script lang="typescript">
-    import { localize } from '@core/i18n'
-    import features from '@features/features'
     import { Drawer, StrongholdUnlock } from '../../../../../../components'
-    import { SettingsCategory, SETTINGS_ROUTE_META } from '../../../../../../lib/contexts/dashboard'
+    import { SETTINGS_ROUTE_META } from '../../../../../../lib/contexts/dashboard'
     import { settingsRoute, SettingsRoute, settingsRouter } from '../../../../../../lib/routers'
     import { SettingsIndexView } from './views'
 
     $: needsUnlockStore = $settingsRouter?.getNeedsUnlockStore()
-
-    const categories = {
-        [SettingsCategory.General]: {
-            name: localize('views.settings.general.title'),
-            enabled: features.settings.general.enabled,
-            settings: [],
-        },
-        [SettingsCategory.Security]: {
-            name: localize('views.settings.security.title'),
-            enabled: features.settings.security.enabled,
-            settings: [],
-        },
-        [SettingsCategory.Advanced]: {
-            name: localize('views.settings.advanced.title'),
-            enabled: features.settings.advanced.enabled,
-            settings: [],
-        },
-        [SettingsCategory.HelpAndInfo]: {
-            name: localize('views.settings.helpAndInfo.title'),
-            enabled: features.settings.helpAndInfo.enabled,
-            settings: [],
-        },
-    }
-
-    for (const [route, setting] of Object.entries(SETTINGS_ROUTE_META)) {
-        if (categories[setting.category].enabled) {
-            const listItem = { ...setting, route }
-            categories[setting.category].settings.push(listItem)
-        }
-    }
 
     function onUnlockSuccess(): void {
         $settingsRouter.setNeedsUnlock(false)
