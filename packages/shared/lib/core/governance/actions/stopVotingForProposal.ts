9f3d060f87aa2659add3c41e068f5ac3819b771f
Matthew Maxwell <maxwellmattryan@gmail.com>
2022-12-16 11:51:06 -0600

    fix: add return await

--- packages/shared/lib/core/governance/actions/stopVotingForProposal.ts
+++ packages/shared/lib/core/governance/actions/stopVotingForProposal.ts
@@ -1,7 +1,9 @@
 import { get } from 'svelte/store'
 
+import { Transaction } from '@iota/wallet'
+
 import { selectedAccount } from '@core/account/stores'
 
-export async function stopVotingForProposal(eventId: string): Promise<void> {
-    await get(selectedAccount)?.stopParticipating(eventId)
+export function stopVotingForProposal(eventId: string): Promise<Transaction> {
+    return get(selectedAccount)?.stopParticipating(eventId)
 }
