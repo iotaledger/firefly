518c0f2719f0bd7bdb8ec6d47bcd6bc6ac297faa
Matthew Maxwell <44885822+maxwellmattryan@users.noreply.github.com>
2022-12-16 11:56:11 -0600

    feat: flow to stop voting for a proposal (#5432)
    * feat: add basic UI
    
    * feat: add logic if voting for proposal
    
    * feat: add logic to stop voting
    
    * chore: disable feature
    
    * Co-authored-by: Jean Ribeiro <contact@jeanribeiro.dev>
    
    * fix: add return await
    
    * chore: disable feature flag
    
    Co-authored-by: Tuditi <45079109+Tuditi@users.noreply.github.com>

--- packages/shared/components/modals/ProposalDetailsMenu.svelte
+++ packages/shared/components/modals/ProposalDetailsMenu.svelte
@@ -1,28 +1,58 @@
 <script lang="typescript">
-    import { Icon } from '@auxiliary/icon'
-    import { openPopup } from '@auxiliary/popup'
-    import { localize } from '@core/i18n'
+    import { onMount } from 'svelte'
     import { Modal, MenuItem } from 'shared/components'
+    import { Icon } from '@auxiliary/icon'
+    import { openPopup } from '@auxiliary/popup/actions'
+    import { handleError } from '@core/error/handlers'
+    import { isVotingForSelectedProposal } from '@core/governance/utils'
+    import { localize } from '@core/i18n'
 
     export let modal: Modal = undefined
 
-    function handleRemove(): void {
+    let isVotingForProposal: boolean
+
+    function onStopVotingClick(): void {
+        openPopup({
+            type: 'stopVoting',
+        })
+        modal.close()
+    }
+
+    function onRemoveProposalClick(): void {
         openPopup({
             type: 'removeProposal',
         })
         modal.close()
     }
 
-    // TODO: User can only remove a proposal when he is not voting for it
+    async function onMountHelper(): Promise<void> {
+        try {
+            isVotingForProposal = await isVotingForSelectedProposal()
+        } catch (err) {
+            handleError(err)
+        }
+    }
+
+    onMount(() => void onMountHelper())
 </script>
 
 <Modal bind:this={modal} {...$$restProps}>
     <div class="flex flex-col">
-        <MenuItem
-            icon={Icon.Delete}
-            iconProps={{ width: '16', height: '19' }}
-            title={localize('actions.removeProposal')}
-            onClick={handleRemove}
-        />
+        {#if isVotingForProposal}
+            <MenuItem
+                icon={Icon.Close}
+                iconProps={{ width: '16', height: '16' }}
+                title={localize('actions.stopVoting')}
+                onClick={onStopVotingClick}
+            />
+        {:else}
+            <MenuItem
+                icon={Icon.Delete}
+                iconProps={{ width: '16', height: '19' }}
+                title={localize('actions.removeProposal')}
+                onClick={onRemoveProposalClick}
+                variant="error"
+            />
+        {/if}
     </div>
 </Modal>
