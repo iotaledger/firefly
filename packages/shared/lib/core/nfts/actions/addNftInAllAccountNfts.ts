74f472aaba4283a9eb6230f8feabb15d03fc9b72
Mark Nardi <mark.nardi@iota.org>
2022-12-22 11:49:44 +0100

    fix: fix incoming nfts for newly created accounts

--- packages/shared/lib/core/nfts/actions/addNftInAllAccountNfts.ts
+++ packages/shared/lib/core/nfts/actions/addNftInAllAccountNfts.ts
@@ -3,6 +3,9 @@ import { INft } from '../interfaces'
 
 export function addNftInAllAccountNfts(accountIndex: number, nft: INft): void {
     allAccountNfts.update((state) => {
+        if (!state[accountIndex]) {
+            state[accountIndex] = []
+        }
         const alreadyExists = state[accountIndex].some((_nft) => _nft.id === nft.id)
         if (!alreadyExists) {
             state[accountIndex].push(nft)
