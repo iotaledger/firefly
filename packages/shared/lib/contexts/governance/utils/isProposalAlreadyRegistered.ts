b8ccdb879a4e2ddc9057b2c2e55163794354826e
Tuditi <daviddetroch@pm.me>
2022-12-23 15:42:11 +0100

    fix: check if proposal was registered before

--- packages/shared/lib/contexts/governance/utils/isProposalAlreadyRegistered.ts
+++ packages/shared/lib/contexts/governance/utils/isProposalAlreadyRegistered.ts
@@ -1,7 +1,8 @@
 import { get } from 'svelte/store'
 
 import { proposalsState } from '../stores'
+import { activeProfileId } from '@core/profile'
 
 export function isProposalAlreadyRegistered(proposalId: string): boolean {
-    return proposalId in get(proposalsState)
+    return proposalId in get(proposalsState)?.[get(activeProfileId)]
 }
