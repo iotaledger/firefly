b4b18f7034b583f4842619e79abe00d5c3ed0c69
Begoña Álvarez de la Cruz <balvarez@boxfish.studio>
2022-12-27 09:46:33 +0100

    feat: add profile actions buttons and use it for settings (#5489)
    Co-authored-by: MarkNerdi <105642810+MarkNerdi@users.noreply.github.com>

--- packages/mobile/components/buttons/index.js
+++ packages/mobile/components/buttons/index.js
@@ -1,4 +1,5 @@
 export { default as AccountActionsButton } from './AccountActionsButton.svelte'
 export { default as AccountSwitcherButton } from './AccountSwitcherButton.svelte'
-export { default as ProfileActionsButton } from './ProfileActionsButton.svelte'
+export { default as DashboardProfileButton } from './DashboardProfileButton.svelte'
+export { default as ProfileActionButton } from './ProfileActionButton.svelte'
 export { default as ProfileButton } from './ProfileButton.svelte'
