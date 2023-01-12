e9dbc74557a3d741c688412c6f6912bcc3b8a055
Jean Ribeiro <contact@jeanribeiro.dev>
2023-01-11 21:59:21 -0300

    feat: adds Form component

---
+++ packages/shared/components/molecules/Form.svelte
@@ -0,0 +1,18 @@
+<script lang="typescript">
+    export let id: string
+    export let autocomplete: 'on' | 'off' = 'off'
+    export let classes: string
+    export let onSubmit: () => Promise<void> = async () => {}
+
+    let isBusy = false
+
+    async function handleSubmit(): Promise<void> {
+        isBusy = true
+        await onSubmit()
+        isBusy = false
+    }
+</script>
+
+<form {id} {autocomplete} class={classes} on:submit|preventDefault|trusted={handleSubmit}>
+    <slot {isBusy} />
+</form>
