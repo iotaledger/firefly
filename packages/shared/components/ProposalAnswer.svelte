ecc6bb8313b5d4b17dcab1b00f1adba357c10fa9
Tuditi <daviddetroch@pm.me>
2023-01-12 14:55:53 +0100

    feat: disable event handler if question is answer is hidden

--- packages/shared/components/ProposalAnswer.svelte
+++ packages/shared/components/ProposalAnswer.svelte
@@ -24,7 +24,7 @@
     const dispatch = createEventDispatcher()
 
     function handleClick(): void {
-        if (!disabled) {
+        if (!disabled && !hidden) {
             dispatch('click')
         }
     }
