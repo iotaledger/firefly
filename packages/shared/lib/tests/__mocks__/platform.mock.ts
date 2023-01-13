a7c2d10283c6207374fda95c6511cd886d4469d6
Jean Ribeiro <iamjeanribeiro@gmail.com>
2023-01-13 15:41:00 -0300

    feat: feature flag platform method (#5560)
    * feat: adds "isFeatureFlagEnabled" method in platforms
    
    * feat: adds example
    
    * chore: add method to mock
    
    * fix: format electronApi
    
    * fix: format capacitorApi
    
    * fix: adds brackets wrapping the method code
    
    * chore: adds missing types
    
    * fix: removes arrow from function
    
    * fix
    
    * fix: add fetch to default module property
    
    Co-authored-by: Matthew Maxwell <44885822+maxwellmattryan@users.noreply.github.com>
    Co-authored-by: Matthew Maxwell <maxwellmattryan@gmail.com>

--- packages/shared/lib/tests/__mocks__/platform.mock.ts
+++ packages/shared/lib/tests/__mocks__/platform.mock.ts
@@ -76,6 +76,9 @@ const Platform: IPlatform = {
     copyFile(copyFilePath: string, destinationFilePath: string): Promise<void> {
         return Promise.resolve()
     },
+    isFeatureFlagEnabled(keyPath: string): boolean {
+        return false
+    },
 }
 
 window['__CAPACITOR__'] = Platform
