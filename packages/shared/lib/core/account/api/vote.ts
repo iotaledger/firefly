542e1447c796dd32385c16094892a9108f7edbe0
Tuditi <daviddetroch@pm.me>
2023-01-16 17:16:38 +0100

    fix: make accountIndex obligatory for vote()

--- packages/shared/lib/core/account/api/vote.ts
+++ packages/shared/lib/core/account/api/vote.ts
@@ -2,6 +2,6 @@ import { Transaction } from '@iota/wallet'
 
 import { getAccount } from '@core/profile-manager/api'
 
-export async function vote(index?: number, eventId?: string, answers?: number[]): Promise<Transaction> {
-    return (await getAccount(index))?.vote(eventId, answers)
+export async function vote(accountIndex: number, eventId?: string, answers?: number[]): Promise<Transaction> {
+    return (await getAccount(accountIndex))?.vote(eventId, answers)
 }
