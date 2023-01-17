0149eb0170404b46be48736873731dd9dbdd6a41
Mark Nardi <mark.nardi@iota.org>
2023-01-17 09:03:05 +0100

    enhancement: add proposal name to details popup

--- packages/shared/components/molecules/GovernanceActivityDetails.svelte
+++ packages/shared/components/molecules/GovernanceActivityDetails.svelte
@@ -1,11 +1,14 @@
 <script lang="typescript">
-    import { AmountBox, ActivityInclusionStatusPill } from 'shared/components'
+    import { AmountBox, ActivityInclusionStatusPill, Text, FontWeight } from 'shared/components'
     import { formatTokenAmountDefault, getAssetFromPersistedAssets } from '@core/wallet'
     import { GovernanceActivity } from '@core/wallet'
     import { BASE_TOKEN, COIN_TYPE } from '@core/network'
     import { activeProfile } from '@core/profile'
+    import { getVotingEvent } from '@core/profile-manager'
 
     export let activity: GovernanceActivity
+
+    let proposalName: string
 
     $: asset = getAssetFromPersistedAssets(String(COIN_TYPE[$activeProfile.networkProtocol]))
     $: amount = activity.votingPowerDifference
@@ -16,6 +19,17 @@
           )
         : 0
     $: localizationKey = 'governance.' + activity.governanceAction
+    $: activity.participation?.eventId, setProposalName()
+
+    async function setProposalName(): Promise<void> {
+        try {
+            if (activity?.participation?.eventId) {
+                proposalName = (await getVotingEvent(activity.participation.eventId)).data.name
+            }
+        } catch (err) {
+            proposalName = activity.participation.eventId
+        }
+    }
 </script>
 
 <main-content class="flex flex-auto w-full flex-col items-center justify-center space-y-3">
@@ -25,4 +39,9 @@
     <governance-status class="flex flex-row w-full space-x-2 justify-center">
         <ActivityInclusionStatusPill {localizationKey} inclusionState={activity.inclusionState} />
     </governance-status>
+    {#if proposalName}
+        <Text color="gray-800" darkColor="gray-500" fontWeight={FontWeight.semibold} fontSize="text-15">
+            {proposalName}
+        </Text>
+    {/if}
 </main-content>
