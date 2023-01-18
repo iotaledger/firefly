5cdffe7819643e73f5d3c5bda42ac318753d0b49
Tuditi <daviddetroch@pm.me>
2023-01-18 15:39:37 +0100

    fix: set isTransferring when voting

--- packages/shared/lib/core/account/api/vote.ts
+++ packages/shared/lib/core/account/api/vote.ts
@@ -1,9 +1,23 @@
 import { get } from 'svelte/store'
 import { Transaction } from '@iota/wallet'
 
-import { getAccount } from '@core/profile-manager/api'
-import { selectedAccountIndex } from '@core/account/stores'
+import { selectedAccount, updateSelectedAccount } from '@core/account/stores'
+import { activeProfile } from '@core/profile/stores'
+import { ProfileType } from '@core/profile'
+import { handleLedgerError } from '@core/ledger/utils'
 
 export async function vote(eventId?: string, answers?: number[]): Promise<Transaction> {
-    return (await getAccount(get(selectedAccountIndex)))?.vote(eventId, answers)
+    try {
+        const account = get(selectedAccount)
+        updateSelectedAccount({ isTransferring: true })
+        await account.vote(eventId, answers)
+    } catch (err) {
+        const _activeProfile = get(activeProfile)
+        if (_activeProfile.type === ProfileType.Ledger) {
+            handleLedgerError(err?.error)
+        }
+        return Promise.reject(err)
+    } finally {
+        updateSelectedAccount({ isTransferring: false })
+    }
 }
