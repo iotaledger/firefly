5cc735ee86d3a343fad6a18cad2483937d26eb0b
Tuditi <daviddetroch@pm.me>
2023-01-19 11:54:53 +0100

    fix: return Transaction from stopVote()

--- packages/shared/lib/contexts/governance/actions/stopVotingForProposal.ts
+++ packages/shared/lib/contexts/governance/actions/stopVotingForProposal.ts
@@ -1,20 +1,22 @@
 import { get } from 'svelte/store'
+import type { Transaction } from '@iota/wallet'
 
 import { selectedAccount, updateSelectedAccount } from '@core/account/stores'
 import { showAppNotification } from '@auxiliary/notification/actions'
 import { localize } from '@core/i18n'
 import { handleError } from '@core/error/handlers'
 
-export async function stopVotingForProposal(eventId: string): Promise<void> {
+export async function stopVotingForProposal(eventId: string): Promise<Transaction> {
     try {
         updateSelectedAccount({ isTransferring: true })
         const account = get(selectedAccount)
-        await account?.stopParticipating(eventId)
+        const transaction = await account?.stopParticipating(eventId)
         showAppNotification({
             type: 'success',
             message: localize('notifications.stopVoting.success'),
             alert: true,
         })
+        return transaction
     } catch (err) {
         handleError(err)
     } finally {
