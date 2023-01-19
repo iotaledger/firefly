19304165f21748c2f0de4c3ebc9e8328018937b3
Tuditi <45079109+Tuditi@users.noreply.github.com>
2023-01-20 00:25:34 +0100

    feat: manage voting power while voting (#5593)
    * feat: add revotePopup
    
    * fix: make accountIndex obligatory for vote()
    
    * feat: add locales
    
    * fix: add optional chaining
    
    * feat: add votingPowerTransactionState
    
    * fix: revote logic
    
    * fix: pr review
    
    * chore: info popup instead of success
    
    * chore: simplify logic to open revote
    
    * feat: rename confirm to revote
    
    * fix: disable change in voting power if same amount
    
    * fix: pass disabled prop to unitInput
    
    * fix: show new voting power when reopening
    
    * feat: only reopen popup if has to revote
    
    * chore: add default parameter to sync
    
    * chore: dont update participationOverview to speed up vote
    
    * fix: correctly set disable
    
    * fix: set isTransferring when voting
    
    * refactor: consistent vote/revote/stopVote logic
    
    * feat: add hint text if has to revote
    
    * feat: use isTransferring for disabled state
    
    * chore: address review comments on PR
    
    * fix: use error handler function
    
    * feat: handle all errors within vote()
    
    * refactor: remove redundant parameter
    
    * fix: isVoting logic
    
    * fix: align warning text with design
    
    * fix: return Transaction from stopVote()
    
    * fix: reactively update if availableBalance changes
    
    * chore: adjust locales
    
    Co-authored-by: Matthew Maxwell <44885822+maxwellmattryan@users.noreply.github.com>
    Co-authored-by: Matthew Maxwell <maxwellmattryan@gmail.com>

--- packages/shared/lib/contexts/governance/utils/isVotingForProposal.ts
+++ packages/shared/lib/contexts/governance/utils/isVotingForProposal.ts
@@ -1,13 +1,21 @@
 import { get } from 'svelte/store'
 
-import { TrackedParticipationOverview } from '@iota/wallet'
+import type { ParticipationOverview, TrackedParticipationOverview } from '@iota/wallet'
 
-import { selectedAccount } from '@core/account/stores'
-import { getParticipationOverview } from '@core/account/api'
+import { participationOverview } from '@contexts/governance/stores'
+import { getParticipationOverview, selectedAccountIndex } from '@core/account'
 
-export async function isVotingForProposal(proposalId: string, accountIndex?: number): Promise<boolean> {
-    const _accountIndex = accountIndex ?? get(selectedAccount)?.index
-    const overview = await getParticipationOverview(_accountIndex)
+export async function isVotingForProposal(
+    proposalId: string,
+    accountIndex = get(selectedAccountIndex)
+): Promise<boolean> {
+    let overview: ParticipationOverview
+    if (accountIndex === get(selectedAccountIndex)) {
+        overview = get(participationOverview)
+    } else {
+        overview = await getParticipationOverview(accountIndex)
+    }
+
     const participations = overview?.participations?.[proposalId] ?? {}
     const participationOutputs: TrackedParticipationOverview[] = Object.values(participations)
     return participationOutputs.some((output) => output?.endMilestoneIndex === 0)
