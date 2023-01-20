d83f78f83552d7dc7ba3519b37996e5961cebe6d
Tuditi <daviddetroch@pm.me>
2023-01-20 15:07:38 +0100

    fix: resetParticipationOverview when switching accountIndex

--- packages/shared/lib/core/account/actions/setSelectedAccount.ts
+++ packages/shared/lib/core/account/actions/setSelectedAccount.ts
@@ -2,6 +2,7 @@ import { get } from 'svelte/store'
 
 import { activeAccounts, updateActiveProfile } from '@core/profile/stores'
 import { resetSendOptionIndex } from '@core/wallet/stores'
+import { resetParticipationOverview } from '@contexts/governance/stores'
 
 import { selectedAccount, selectedAccountIndex } from '../stores'
 
@@ -11,6 +12,7 @@ export function setSelectedAccount(index: number): void {
         selectedAccountIndex.set(index)
         selectedAccount.set(account)
         updateActiveProfile({ lastUsedAccountIndex: index })
+        resetParticipationOverview()
         resetSendOptionIndex()
     } else {
         throw new Error(`Account with ID ${index} cannot be found!`)
