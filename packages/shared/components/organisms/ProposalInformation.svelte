a6dd8d238a1aad0689832aa1d1c279de517c9374
Mark Nardi <mark.nardi@iota.org>
2023-01-23 14:28:06 +0100

    enhancement: make event id copyable

--- packages/shared/components/organisms/ProposalInformation.svelte
+++ packages/shared/components/organisms/ProposalInformation.svelte
@@ -6,13 +6,19 @@
     import { DATE_FORMAT, milestoneToDate, truncateString } from '@core/utils'
     import { proposalsState, selectedProposal } from '@contexts/governance/stores'
 
-    const proposalInformation = {
-        countingEnds: formatDate(
-            milestoneToDate($networkStatus.currentMilestone, $selectedProposal.milestones?.ended),
-            DATE_FORMAT
-        ),
-        eventId: truncateString($selectedProposal?.id, 9, 9),
-        nodeUrl: $proposalsState[$activeProfileId]?.[$selectedProposal?.id].nodeUrl,
+    const proposalInformation: { [key in string]: { data: string; isCopyable?: boolean; copyValue?: string } } = {
+        countingEnds: {
+            data: formatDate(
+                milestoneToDate($networkStatus.currentMilestone, $selectedProposal.milestones?.ended),
+                DATE_FORMAT
+            ),
+        },
+        eventId: {
+            data: truncateString($selectedProposal?.id, 9, 9),
+            isCopyable: true,
+            copyValue: $selectedProposal?.id,
+        },
+        nodeUrl: { data: $proposalsState[$activeProfileId]?.[$selectedProposal?.id].nodeUrl, isCopyable: true },
     }
 </script>
 
@@ -25,7 +31,9 @@
             <li>
                 <KeyValueBox
                     keyText={localize(`views.governance.details.proposalInformation.${counterKey}`)}
-                    valueText={proposalInformation[counterKey]}
+                    valueText={proposalInformation[counterKey].data}
+                    isCopyable={proposalInformation[counterKey].isCopyable}
+                    copyValue={proposalInformation[counterKey].copyValue}
                 />
             </li>
         {/each}
