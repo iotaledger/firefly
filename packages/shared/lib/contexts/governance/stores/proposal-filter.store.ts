078474fb7a6f28c2e4ee6b67f0d6c6a9f5d5998f
Mark Nardi <mark.nardi@iota.org>
2023-01-23 18:06:57 +0100

    enhancement: add dropdown enums + translation

--- packages/shared/lib/contexts/governance/stores/proposal-filter.store.ts
+++ packages/shared/lib/contexts/governance/stores/proposal-filter.store.ts
@@ -1,21 +1,21 @@
 import { BooleanFilterOption } from '@core/utils/enums/filters'
 import { writable, Writable } from 'svelte/store'
-import { ProposalFilter } from '..'
+import { ProposalFilter, ProposalStatus, ProposalType } from '..'
 
 export const proposalFilter: Writable<ProposalFilter> = writable({
     phase: {
         active: false,
         type: 'selection',
         localeKey: 'filters.phase',
-        selected: BooleanFilterOption.Yes,
-        choices: [BooleanFilterOption.Yes, BooleanFilterOption.No],
+        selected: ProposalStatus.Commencing,
+        choices: [ProposalStatus.Commencing, ProposalStatus.Upcoming, ProposalStatus.Holding, ProposalStatus.Ended],
     },
     type: {
         active: false,
         type: 'selection',
-        localeKey: 'filters.type',
-        selected: BooleanFilterOption.Yes,
-        choices: [BooleanFilterOption.Yes, BooleanFilterOption.No],
+        localeKey: 'filters.proposalType',
+        selected: ProposalType.Official,
+        choices: [ProposalType.Official, ProposalType.Custom],
     },
     participated: {
         active: false,
