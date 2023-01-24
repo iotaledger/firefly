0e2619b046b093fabe984017d4789f18f20a02bb
Amadeo Marchioni <amadeo.marchioni@iota.org>
2023-01-24 18:15:11 +0100

    chore: adapt new routes
    Signed-off-by: Amadeo Marchioni <amadeo.marchioni@iota.org>

--- packages/mobile/views/onboarding/views/profile-recovery/ProfileRecoveryRouter.svelte
+++ packages/mobile/views/onboarding/views/profile-recovery/ProfileRecoveryRouter.svelte
@@ -1,10 +1,10 @@
 <script lang="typescript">
     import { onMount } from 'svelte'
-    import { ImportMnemonicPhraseView, SuccessView } from './views'
+    import { BackupPasswordView, ImportMnemonicPhraseView, ImportStrongholdBackupView, SuccessView } from './views'
     import { localize } from '@core/i18n'
     import { profileRecoveryRoute, profileRecoveryRouter, ProfileRecoveryRoute } from '../../../../lib/routers'
     import { showAppNotification } from '@auxiliary/notification'
-
+    
     let busy = false
     let error = ''
 
@@ -38,6 +38,10 @@
 
 {#if $profileRecoveryRoute === ProfileRecoveryRoute.ImportMnemonicPhrase}
     <ImportMnemonicPhraseView on:next={next} on:previous={previous} />
+{:else if $profileRecoveryRoute === ProfileRecoveryRoute.ImportStrongholdBackup}
+    <ImportStrongholdBackupView on:next={next} on:previous={previous} />
+{:else if $profileRecoveryRoute === ProfileRecoveryRoute.BackupPassword}
+    <BackupPasswordView on:next={next} on:previous={previous} />
 {:else if $profileRecoveryRoute === ProfileRecoveryRoute.Success}
     <SuccessView on:next={next} on:previous={previous} />
 {/if}
