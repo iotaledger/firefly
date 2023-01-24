3f7d57d97049200569f9a8a3b3bab78670570909
Paul Bögelsack <pboegelsack@googlemail.com>
2023-01-24 13:33:42 +0100

    feat-mobile: show toast container only if notifications exist

--- packages/mobile/components/ToastContainer.svelte
+++ packages/mobile/components/ToastContainer.svelte
@@ -16,22 +16,24 @@
     }))
 </script>
 
-<toast-container class="fixed top-0 flex flex-col z-10 w-full {$notifications.length > 0 && 'p-5'}">
-    <ul class="space-y-2">
-        {#each toasts as toast}
-            <li in:fade|local={{ duration: 100 }} out:fade|local={{ duration: 100 }}>
-                {#if toast.alert}
-                    <Alert type={toast.type} message={toast.message} />
-                {:else}
-                    <Toast
-                        type={toast.type}
-                        message={toast.message}
-                        subMessage={toast.subMessage}
-                        progress={toast.progress}
-                        actions={toast.actions}
-                    />
-                {/if}
-            </li>
-        {/each}
-    </ul>
-</toast-container>
+{#if $notifications.length > 0}
+    <toast-container class="fixed top-0 flex flex-col z-10 w-full p-5">
+        <ul class="space-y-2">
+            {#each toasts as toast}
+                <li in:fade|local={{ duration: 100 }} out:fade|local={{ duration: 100 }}>
+                    {#if toast.alert}
+                        <Alert type={toast.type} message={toast.message} />
+                    {:else}
+                        <Toast
+                            type={toast.type}
+                            message={toast.message}
+                            subMessage={toast.subMessage}
+                            progress={toast.progress}
+                            actions={toast.actions}
+                        />
+                    {/if}
+                </li>
+            {/each}
+        </ul>
+    </toast-container>
+{/if}
