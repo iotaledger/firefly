f307e2b2662e012add7a30a212cb5a943e0e0692
Tuditi <daviddetroch@pm.me>
2023-01-24 15:40:55 +0100

    enhancement: consistent naming + setingEventIds

--- packages/shared/lib/core/profile/actions/active-profile/logout.ts
+++ packages/shared/lib/core/profile/actions/active-profile/logout.ts
@@ -1,9 +1,9 @@
 import { closePopup } from '@auxiliary/popup'
 import { resetSelectedAccount } from '@core/account'
-import { clearPollGovernanceDataInterval } from '@contexts/governance'
+import { clearGovernancePollAndData } from '@contexts/governance'
 import { isPollingLedgerDeviceStatus, stopPollingLedgerNanoStatus } from '@core/ledger'
-import { clearPollMarketPrices } from '@core/market/actions'
-import { clearPollNetworkInterval } from '@core/network'
+import { clearMarketPricesPoll } from '@core/market/actions'
+import { clearNetworkPoll } from '@core/network'
 import {
     activeAccounts,
     activeProfile,
@@ -31,9 +31,9 @@ export function logout(clearActiveProfile: boolean = true, _lockStronghold: bool
             get(isPollingLedgerDeviceStatus) && stopPollingLedgerNanoStatus()
         }
 
-        clearPollNetworkInterval()
-        clearPollMarketPrices()
-        clearPollGovernanceDataInterval()
+        clearNetworkPoll()
+        clearMarketPricesPoll()
+        clearGovernancePollAndData()
         const _activeProfile = get(activeProfile)
         if (_activeProfile) {
             const manager = get(profileManager)
