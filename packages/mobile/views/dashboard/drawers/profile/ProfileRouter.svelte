de712636c59322e79d0e89ec5449fe3880767b82
paul-boegelsack <paul.boegelsack@iota.org>
2023-01-26 18:31:01 +0100

    feat-mobile: add backup profile action screen (#5637)
    * feat-mobile: add backup profile button
    
    * feat-mobile: setup profile backup view
    
    * feat-mobile: adjust styling
    
    * feat-mobile: implement complete backup routing
    
    * feat-mobile: move export result handling into profile router
    
    * fix-mobile: error notification message
    
    * fix-mobile: isBusy
    
    Co-authored-by: Begoña Alvarez <balvarez@boxfish.studio>

--- packages/mobile/views/dashboard/drawers/profile/ProfileRouter.svelte
+++ packages/mobile/views/dashboard/drawers/profile/ProfileRouter.svelte
@@ -1,17 +1,23 @@
 <script lang="typescript">
     import { Drawer, StrongholdUnlock } from '../../../../components'
     import { ProfileRoute, profileRoute, profileRouter } from '../../../../lib/routers'
-    import { ActionsView, NetworkStatusView, SettingsView } from './views'
+    import { ActionsView, BackupProfileView, NetworkStatusView, SettingsView } from './views'
 
     $: needsUnlockStore = $profileRouter?.getNeedsUnlockStore()
-
-    function closeStrongholdUnlock(): void {
+    $: needsUnlockStoreCallbackStore = $profileRouter?.getNeedsUnlockCallbackStore()
+    $: returnPasswordUnlockCallbackStore = $profileRouter?.getReturnPasswordUnlockCallbackStore()
+    function onUnlockSuccess(password?: string): void {
         $profileRouter.setNeedsUnlock(false, undefined)
+        if ($needsUnlockStoreCallbackStore && typeof $needsUnlockStoreCallbackStore === 'function') {
+            $needsUnlockStoreCallbackStore(password)
+        }
     }
 </script>
 
 {#if $profileRoute === ProfileRoute.Actions}
     <ActionsView />
+{:else if $profileRoute === ProfileRoute.Backup}
+    <BackupProfileView />
 {:else if $profileRoute === ProfileRoute.NetworkStatus}
     <NetworkStatusView />
 {:else if $profileRoute === ProfileRoute.Settings}
@@ -19,7 +25,11 @@
 {/if}
 
 {#if $needsUnlockStore}
-    <Drawer onClose={closeStrongholdUnlock}>
-        <StrongholdUnlock onSuccess={closeStrongholdUnlock} onCancel={closeStrongholdUnlock} />
+    <Drawer onClose={() => $profileRouter.setNeedsUnlock(false)}>
+        <StrongholdUnlock
+            onSuccess={onUnlockSuccess}
+            onCancel={() => $profileRouter.setNeedsUnlock(false, undefined)}
+            returnPassword={$returnPasswordUnlockCallbackStore}
+        />
     </Drawer>
 {/if}
