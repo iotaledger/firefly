34f2c14d1a34d0ab86a10f4bc931f8be6b279961
Jean Ribeiro <iamjeanribeiro@gmail.com>
2023-01-26 14:11:08 -0300

    feat: governance polishing (#5655)
    * feat: adds hover styling to ProposalAnswer
    
    * feat: go to proposal details after registering it
    
    * fix: cursor pointer only if not disabled
    
    * fix: height of proposal info in DetailsView
    
    * fix: fixed width to dashboard pane
    
    * fix: adds truncation to answer text
    
    * enhancement: adds shadow effects and keyboard selectability to ProposalCard
    
    * fix: ProposalCard height
    
    * enhancement: adds focus style to MeatballMenuButton
    
    * feat: adds additional info to IProposal and add it below the title in DetailsView
    
    * chore: changes register proposal to add proposal
    
    * feat: improve empty ProposalsView UX
    
    * feat: adds total proposals number to proposals details
    
    * fix: limit question text to 1 line but fully show once question is opened
    
    * fix: limit answer text to 1 line but fully show once question is opened
    
    * fix: first question opened as default
    
    * feat: when question is answered, open and scroll to next one
    
    * fix: scrollbar spacing
    
    * fix: prevent pill status to show when the status is undefined
    
    * fixes from review
    
    * fix: absence of margin top in proposal answers when some answer is selected

--- packages/shared/lib/contexts/governance/utils/isProposalAlreadyAdded.ts
+++ packages/shared/lib/contexts/governance/utils/isProposalAlreadyAdded.ts
@@ -3,6 +3,6 @@ import { get } from 'svelte/store'
 import { proposalsState } from '../stores'
 import { activeProfileId } from '@core/profile'
 
-export function isProposalAlreadyRegistered(proposalId: string): boolean {
+export function isProposalAlreadyAdded(proposalId: string): boolean {
     return proposalId in get(proposalsState)?.[get(activeProfileId)]
 }
