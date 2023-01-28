d6bdb490d764cff5489664df054416f623500316
Mark Nardi <mark.nardi@iota.org>
2023-01-28 21:35:32 +0100

    feat: add filter to proposals

--- packages/shared/components/Proposals.svelte
+++ packages/shared/components/Proposals.svelte
@@ -1,24 +1,40 @@
 <script lang="typescript">
-    import { Text, ProposalCard, Filter } from 'shared/components'
+    import { Text, ProposalCard, Filter, SearchInput } from 'shared/components'
     import { localize } from '@core/i18n'
     import { FontWeight } from './enums'
     import { IProposal } from '@contexts/governance/interfaces'
-    import { proposalFilter } from '@contexts/governance/stores'
-    import { isVisibleProposal, sortProposals } from '@contexts/governance/utils'
+    import { proposalFilter, sortProposals } from '@contexts/governance'
+    import { isVisibleProposal } from '@contexts/governance/utils/isVisibleProposal'
 
     export let proposals: IProposal[] = []
 
-    $: visibleProposals = proposals.filter((proposal) => isVisibleProposal(proposal, $proposalFilter))
+    let searchTerm = ''
+
+    $: visibleProposals = proposals
+        .filter((proposal) => isVisibleProposal(proposal, $proposalFilter))
+        .filter((proposal) => {
+            if (!searchTerm) {
+                return true
+            } else if (
+                proposal.title.toLowerCase().includes(searchTerm.toLowerCase()) ||
+                proposal.id.includes(searchTerm.toLowerCase())
+            ) {
+                return true
+            }
+            return false
+        })
+        .sort((a, b) => (a.id < b.id ? -1 : 1))
     $: sortedProposals = sortProposals(visibleProposals, $proposalFilter)
 </script>
 
 <proposals-container class="flex flex-col h-full">
-    <header-container class="flex justify-between items-center mb-4">
+    <header-container class="flex justify-between items-center mb-4 h-9">
         <Text fontSize="14" fontWeight={FontWeight.semibold}>
             {localize('views.governance.proposals.title')}
         </Text>
-        <div class="flex flex-row">
+        <div class="flex flex-row space-x-2 items-center">
             <Filter filterStore={proposalFilter} />
+            <SearchInput bind:value={searchTerm} />
         </div>
     </header-container>
     <ul class="grid grid-cols-2 auto-rows-min gap-6 flex-1 overflow-y-scroll">
