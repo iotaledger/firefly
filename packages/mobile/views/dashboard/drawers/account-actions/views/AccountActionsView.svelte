f6c5a0e53ce5f1b3c39060f4c7291914a6d723a1
Begoña Álvarez de la Cruz <balvarez@boxfish.studio>
2023-01-30 13:37:48 +0100

    feat-mobile: add customize account (#5687)
    * feat: kickoff account actions drawer
    
    * fix: features
    
    * feat: add customize account
    
    ---------
    
    Co-authored-by: paul-boegelsack <paul.boegelsack@iota.org>

--- packages/mobile/views/dashboard/drawers/account-actions/views/AccountActionsView.svelte
+++ packages/mobile/views/dashboard/drawers/account-actions/views/AccountActionsView.svelte
@@ -3,13 +3,17 @@
     import features from '@features/features'
     import { Icon } from '@lib/auxiliary/icon/enums'
     import { Button } from '@ui'
+    import { AccountAction } from '../../../../../lib/contexts/dashboard'
+    import { accountActionsRouter } from '../../../../../lib/routers'
 
-    export let onClose: () => unknown
+    function handleCustomizeAccountClick(): void {
+        $accountActionsRouter.next({ action: AccountAction.Customize })
+    }
 </script>
 
 <div class="flex flex-col space-y-4">
     {#if features?.dashboard?.accountActions?.customize?.enabled}
-        <Button outline icon={Icon.Customize}>
+        <Button outline onClick={handleCustomizeAccountClick} icon={Icon.Customize}>
             {localize('actions.customizeAcount')}
         </Button>
     {/if}
