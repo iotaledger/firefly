0328c234c82bed5a21ecb8424ea984ee8f32bfa8
Mark Nardi <mark.nardi@iota.org>
2023-01-31 10:31:21 +0100

    ui: top align right text

--- packages/shared/components/atoms/tiles/tileContents/ActivityTileContent.svelte
+++ packages/shared/components/atoms/tiles/tileContents/ActivityTileContent.svelte
@@ -32,16 +32,14 @@
             </Text>
         {/if}
     </div>
-    <div class="flex flex-col space-y-0.5 justify-center overflow-hidden">
-        {#if rightText && rightText.text}
-            <Text
-                fontWeight={FontWeight.semibold}
-                lineHeight="140"
-                color={rightText.color}
-                classes="text-right {rightText.classes}"
-            >
-                {rightText.text}
-            </Text>
-        {/if}
+    <div class="flex flex-col space-y-0.5 justify-between overflow-hidden">
+        <Text
+            fontWeight={FontWeight.semibold}
+            lineHeight="140"
+            color={rightText?.color}
+            classes="text-right {rightText?.classes}"
+        >
+            {rightText?.text ?? ''}
+        </Text>
     </div>
 </div>
