0c9e6824ac52f9589f0317077b62c66550af3c30
Matthew Maxwell <44885822+maxwellmattryan@users.noreply.github.com>
2023-02-01 07:40:34 -0600

    fix: require at least one answer (#5718)
    * fix: adjust logic to automatically abstain
    
    * fix: PR fixes
    
    ---------
    
    Co-authored-by: Tuditi <45079109+Tuditi@users.noreply.github.com>
    Co-authored-by: Mark Nardi <mark.nardi@iota.org>

--- packages/shared/components/popups/VoteForProposalPopup.svelte
+++ packages/shared/components/popups/VoteForProposalPopup.svelte
@@ -1,15 +1,16 @@
 <script lang="ts">
     import { Button, Text, FontWeight, TextHint, TextType, KeyValueBox } from 'shared/components'
     import { HTMLButtonType } from 'shared/components/enums'
+    import { closePopup } from '@auxiliary/popup/actions'
     import { selectedAccount } from '@core/account/stores'
     import { localize } from '@core/i18n'
     import { BASE_TOKEN } from '@core/network/constants'
-    import { activeProfile } from '@core/profile/stores'
     import { checkActiveProfileAuth } from '@core/profile/actions'
+    import { activeProfile } from '@core/profile/stores'
     import { formatTokenAmountBestMatch } from '@core/wallet/utils'
-    import { selectedProposal } from '@contexts/governance/stores'
-    import { closePopup } from '@auxiliary/popup/actions'
     import { vote } from '@contexts/governance/actions'
+    import { ABSTAIN_VOTE_VALUE } from '@contexts/governance/constants'
+    import { selectedProposal } from '@contexts/governance/stores'
 
     export let selectedAnswerValues: number[]
 
@@ -20,6 +21,9 @@
     $: hasVotingPower = Number($selectedAccount?.votingPower) > 0
 
     $: isTransferring = $selectedAccount?.isTransferring
+
+    $: numberOfAbstainedQuestions =
+        selectedAnswerValues?.filter((answerValue) => answerValue === ABSTAIN_VOTE_VALUE).length ?? 0
 
     async function handleSubmit(): Promise<void> {
         await checkActiveProfileAuth(async () => {
@@ -48,6 +52,13 @@
         <KeyValueBox keyText={localize('popups.voteForProposal.key')} valueText={formattedVotingPower} />
         {#if !hasVotingPower}
             <TextHint danger text={localize('popups.voteForProposal.noVotingPower')} />
+        {:else if numberOfAbstainedQuestions > 0}
+            <TextHint
+                warning
+                text={localize('popups.voteForProposal.hasAbstained', {
+                    values: { numberOfQuestions: numberOfAbstainedQuestions },
+                })}
+            />
         {/if}
     </div>
     <popup-buttons class="flex flex-row flex-nowrap w-full space-x-4">
