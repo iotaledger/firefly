b911d6163173fc720b83e0fa9c13681600ea91a9
Jean Ribeiro <contact@jeanribeiro.dev>
2023-01-31 22:52:15 -0300

    feat: add a decimal place to the proposal answers percentages where is needed

--- packages/shared/lib/contexts/governance/utils/getPercentagesFromAnswerStatuses.ts
+++ packages/shared/lib/contexts/governance/utils/getPercentagesFromAnswerStatuses.ts
@@ -1,3 +1,4 @@
+import { round } from '@core/utils/number'
 import type { AnswerStatus } from '@iota/wallet/out/types'
 import { IProposalAnswerPercentages } from '../interfaces'
 
@@ -13,7 +14,7 @@ export function getPercentagesFromAnswerStatuses(answerStatuses: AnswerStatus[])
             const divisionResult = (answerStatus.accumulated ?? 0) / totalVotes
             percentages = {
                 ...percentages,
-                [answerStatus.value]: Number.isNaN(divisionResult) ? '0%' : `${Math.round(divisionResult * 100)}%`,
+                [answerStatus.value]: Number.isNaN(divisionResult) ? '0%' : `${round(divisionResult * 100, 1)}%`,
             }
         }
     })
