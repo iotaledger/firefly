c08d546084a745d5c1988041332331494ebbfbc4
MarkNerdi <105642810+MarkNerdi@users.noreply.github.com>
2023-02-01 18:00:05 +0100

    fix: fix details view if account with proposal gets deleted (#5739)
    Co-authored-by: Tuditi <45079109+Tuditi@users.noreply.github.com>

--- packages/shared/lib/contexts/governance/utils/isAnyAccountVotingForSelectedProposal.ts
+++ packages/shared/lib/contexts/governance/utils/isAnyAccountVotingForSelectedProposal.ts
@@ -1,11 +1,9 @@
-import { activeProfile } from '@core/profile'
+import { activeAccounts } from '@core/profile'
 import { get } from 'svelte/store'
 import { isVotingForSelectedProposal } from './isVotingForSelectedProposal'
 
 export async function isAnyAccountVotingForSelectedProposal(): Promise<boolean> {
-    const accountIndexes = Object.values(get(activeProfile)?.accountMetadata).map(
-        (accountMetadata) => accountMetadata.index
-    )
+    const accountIndexes = get(activeAccounts).map((account) => account.index)
     const isVotingPromises = accountIndexes.map(isVotingForSelectedProposal)
     const results = await Promise.all(isVotingPromises)
     return results.some((bool) => bool === true)
