2d842efbdfb213a3a7826941b4f87a3a67479af9
Tuditi <daviddetroch@pm.me>
2023-02-06 11:48:56 +0100

    refactor: logout function

--- packages/shared/lib/core/profile/actions/active-profile/logout.ts
+++ packages/shared/lib/core/profile/actions/active-profile/logout.ts
@@ -21,48 +21,40 @@ import { get } from 'svelte/store'
 /**
  * Logout from active profile
  */
-export async function logout(clearActiveProfile: boolean = true, _lockStronghold: boolean = true): Promise<void> {
-    const { lastActiveAt, loggedIn, hasLoadedAccounts, type } = get(activeProfile)
-    await unsubscribeFromWalletApiEvents()
+export async function logout(clearActiveProfile = true, _lockStronghold = true): Promise<void> {
+    if (get(isSoftwareProfile)) {
+        _lockStronghold && lockStronghold()
+    } else if (isLedgerProfile(get(activeProfile).type)) {
+        get(isPollingLedgerDeviceStatus) && stopPollingLedgerNanoStatus()
+    }
 
-    // (TODO): Figure out why we are using a promise here?
-    return new Promise((resolve) => {
-        if (get(isSoftwareProfile)) {
-            _lockStronghold && lockStronghold()
-        } else if (isLedgerProfile(type)) {
-            get(isPollingLedgerDeviceStatus) && stopPollingLedgerNanoStatus()
-        }
+    clearNetworkPoll()
+    clearMarketPricesPoll()
+    clearGovernancePollAndData()
 
-        clearNetworkPoll()
-        clearMarketPricesPoll()
-        clearGovernancePollAndData()
-        const _activeProfile = get(activeProfile)
-        if (_activeProfile) {
-            const manager = get(profileManager)
+    const _activeProfile = get(activeProfile)
+    if (_activeProfile) {
+        const manager = get(profileManager)
+        await manager?.stopBackgroundSync()
+        await unsubscribeFromWalletApiEvents()
+        destroyProfileManager()
+    }
 
-            // stop background sync
-            // TODO: Make sure we need this. Would destroying the profile manager also stop background syncing automatically?
-            manager?.stopBackgroundSync()
+    cleanupProfileState(clearActiveProfile)
+}
 
-            // Unsubscribe to listeners
-            // https://github.com/iotaledger/wallet.rs/issues/1133
+function cleanupProfileState(clearActiveProfile: boolean): void {
+    const { lastActiveAt, loggedIn, hasLoadedAccounts } = get(activeProfile)
 
-            destroyProfileManager()
-        }
-
-        // TODO: clean up the state management
-        lastActiveAt.set(new Date())
-        closePopup(true)
-        loggedIn.set(false)
-        hasLoadedAccounts.set(false)
-        resetSelectedAccount()
-        resetPendingGovernanceTransactionIds()
-        activeAccounts.set([])
-        if (clearActiveProfile) {
-            resetActiveProfile()
-        }
-        get(routerManager).resetRouters()
-
-        resolve()
-    })
+    lastActiveAt.set(new Date())
+    closePopup(true)
+    loggedIn.set(false)
+    hasLoadedAccounts.set(false)
+    resetSelectedAccount()
+    resetPendingGovernanceTransactionIds()
+    activeAccounts.set([])
+    if (clearActiveProfile) {
+        resetActiveProfile()
+    }
+    get(routerManager).resetRouters()
 }
