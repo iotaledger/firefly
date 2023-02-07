1c475b1eb9dbf3fc648737960ae4eed6cb3f966d
Jean Ribeiro <contact@jeanribeiro.dev>
2023-02-07 15:20:56 -0300

    fixes from review

--- packages/mobile/components/inputs/SearchInput.svelte
+++ packages/mobile/components/inputs/SearchInput.svelte
@@ -13,12 +13,22 @@
     })()
 </script>
 
-<input-container class="flex items-center py-2 px-4 rounded-lg bg-gray-50">
+<input-container class="flex items-center py-2 px-4 rounded-lg bg-gray-50 dark:bg-gray-850">
     <Icon icon={IconEnum.Search} classes="mr-2 text-gray-500 dark:text-white" width={21} height={21} />
     <input
         bind:value={searchValue}
         placeholder={localize('general.search')}
         type="text"
-        class="text-sm bg-transparent text-gray-500 dark:text-white"
+        class="flex-1 text-sm bg-transparent text-gray-500 dark:text-white"
     />
+    {#if searchValue}
+        <button on:click={() => (searchValue = '')} slot="right">
+            <Icon
+                icon={IconEnum.Close}
+                classes="cursor-pointer ml-2 text-gray-500 dark:text-white hover:text-gray-600 dark:hover:text-gray-200"
+                width={21}
+                height={21}
+            />
+        </button>
+    {/if}
 </input-container>
