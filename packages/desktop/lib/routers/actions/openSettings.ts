ec3e4bbc62bfa230d7bb5907573c71f6d1ac1adf
Jean Ribeiro <contact@jeanribeiro.dev>
2023-02-09 17:47:59 -0300

    fix that supposedly causes the type check CI to success

--- packages/desktop/lib/routers/actions/openSettings.ts
+++ packages/desktop/lib/routers/actions/openSettings.ts
@@ -3,7 +3,7 @@ import { get } from 'svelte/store'
 import { DashboardRoute } from '@core/router/enums'
 import { dashboardRouter, settingsRouter } from '@core/router/routers'
 
-export function openSettings(): string {
+export function openSettings(): void {
     get(dashboardRouter).goTo(DashboardRoute.Settings)
     get(settingsRouter).reset()
 }
