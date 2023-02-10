fe54b4951b388bb52dd8e3f5cd11d5a1d01e0c1b
Nicole O'Brien <nicole.obrien@iota.org>
2023-02-10 09:33:09 +0000

    refactor: speed up initialising events
    Co-authored-by: MarkNerdi <MarkNerdi@users.noreply.github.com>

--- packages/shared/lib/contexts/governance/actions/getGovernanceData.ts
+++
@@ -1,5 +0,0 @@
-import { initializeRegisteredProposals } from './initializeRegisteredProposals'
-
-export async function getGovernanceData(): Promise<void> {
-    await initializeRegisteredProposals()
-}
