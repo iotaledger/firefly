77f9acf35db9d40b83e148aa18e5e4acc97240d5
Jean Ribeiro <iamjeanribeiro@gmail.com>
2023-02-10 15:15:56 -0300

    feat-mobile: filter assets (#5750)
    * feat-mobile: adds filter and its functionality to assets
    
    * fixes from review
    
    ---------
    
    Co-authored-by: Begoña Álvarez de la Cruz <balvarez@boxfish.studio>

---
+++ packages/shared/lib/core/wallet/constants/default-asset-filter.constant.ts
@@ -0,0 +1,30 @@
+import { AssetOrderOption, BooleanFilterOption, OrderOption } from '@core/utils/enums/filters'
+import { NotVerifiedStatus, VerifiedStatus } from '../enums'
+import { AssetFilter } from '../interfaces'
+
+export const DEFAULT_ASSET_FILTER: AssetFilter = {
+    verificationStatus: {
+        active: false,
+        type: 'selection',
+        localeKey: 'filters.verificationStatus',
+        selected: 'new',
+        choices: Object.values(NotVerifiedStatus)
+            .map((status) => String(status))
+            .concat(Object.values(VerifiedStatus).map((status) => String(status))),
+    },
+    showHidden: {
+        active: false,
+        type: 'selection',
+        localeKey: 'filters.showHidden',
+        selected: BooleanFilterOption.Yes,
+        choices: [BooleanFilterOption.Yes, BooleanFilterOption.No],
+    },
+    order: {
+        active: false,
+        type: 'order',
+        localeKey: 'filters.assetOrder',
+        selected: AssetOrderOption.Name,
+        ascDesc: OrderOption.Asc,
+        choices: [AssetOrderOption.Name, AssetOrderOption.Amount],
+    },
+}
