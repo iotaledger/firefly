1d7310ee722e37572b8e25628cf1053d06084d07
Jean Ribeiro <contact@jeanribeiro.dev>
2023-02-09 21:31:13 -0300

    feat: adds search in activities

--- packages/mobile/components/ActivityList.svelte
+++ packages/mobile/components/ActivityList.svelte
@@ -1,44 +1,92 @@
 <script lang="ts">
-    import { time } from '@core/app'
-    import { localize } from '@core/i18n'
-    import { getMonthYear } from '@core/utils'
-    import { Activity, queriedActivities, setAsyncStatusOfAccountActivities } from '@core/wallet'
     import VirtualList from '@sveltejs/svelte-virtual-list'
     import { FontWeight, Text } from 'shared/components'
-    import { ActivityTile } from '../../mobile/components'
+    import { ActivityTile, SearchInput } from '../../mobile/components'
+    import { time } from '@core/app'
+    import { getGroupSeparator, localize } from '@core/i18n'
+    import { getMonthYear } from '@core/utils'
+    import {
+        Activity,
+        ActivityType,
+        getActivityTileTitle,
+        getFormattedAmountFromActivity,
+        queriedActivities,
+        setAsyncStatusOfAccountActivities,
+    } from '@core/wallet'
+
+    type ActivityTileData = {
+        title: string | undefined
+        amount: number | undefined
+        activity: Activity
+    }
 
     export let onTileClick: (activity: Activity) => unknown = () => {}
     export let onReject: (activity: Activity) => unknown = () => {}
     export let onClaim: (activity: Activity) => unknown = () => {}
 
+    let searchValue = ''
+
     $: setAsyncStatusOfAccountActivities($time)
-
-    $: activityListWithTitles = $queriedActivities.map((activity, index) => {
-        const currentTitle = getActivityGroupTitleForTimestamp(activity.time)
-        const previousTitle = $queriedActivities[index - 1]
-            ? getActivityGroupTitleForTimestamp($queriedActivities[index - 1]?.time)
-            : undefined
-        if (currentTitle !== previousTitle || index === 0) {
-            const amount = $queriedActivities.filter(
-                (activity) => getActivityGroupTitleForTimestamp(activity.time) === currentTitle
-            ).length
-
-            return { title: currentTitle, amount, activity }
-        } else {
-            return { title: undefined, amount: undefined, activity }
-        }
-    })
+    $: activityTileDataList = searchActivities(getActivityTileDataList($queriedActivities), searchValue)
 
     function getActivityGroupTitleForTimestamp(time: Date): string {
         const dateString = getMonthYear(time)
         return dateString === getMonthYear(new Date()) ? localize('general.thisMonth') : dateString
     }
+
+    function getActivityTileDataList(activities: Activity[]): ActivityTileData[] {
+        return activities.map((activity, index) => {
+            const currentTitle = getActivityGroupTitleForTimestamp(activity.time)
+            const previousTitle =
+                activities[index - 1] && getActivityGroupTitleForTimestamp(activities[index - 1]?.time)
+            if (currentTitle !== previousTitle || index === 0) {
+                const amount = activities.filter(
+                    (activity) => getActivityGroupTitleForTimestamp(activity.time) === currentTitle
+                ).length
+
+                return { title: currentTitle, amount, activity }
+            } else {
+                return { title: undefined, amount: undefined, activity }
+            }
+        })
+    }
+
+    function searchActivities(activities: ActivityTileData[], searchValue: string): ActivityTileData[] {
+        if (!activities || !searchValue) {
+            return activities
+        }
+
+        const searchTerm = searchValue.toLowerCase()
+        const filteredActivities = activities?.filter((activity) => {
+            const title = localize(getActivityTileTitle(activity?.activity))?.toLowerCase()
+            const transactionId = activity?.activity?.transactionId?.toLowerCase()
+
+            const addressOrName =
+                activity.activity.subject.type === 'address'
+                    ? activity.activity.subject.address.toLowerCase()
+                    : activity.activity.subject.account.name.toLowerCase()
+
+            const amount = getFormattedAmountFromActivity(
+                activity.activity.type === ActivityType.Basic || activity.activity.type === ActivityType.Foundry
+                    ? activity.activity
+                    : undefined
+            )
+                .toLowerCase()
+                .replace(getGroupSeparator(), '')
+
+            return [title, transactionId, addressOrName, amount].some((item) => item.includes(searchTerm))
+        })
+        return filteredActivities
+    }
 </script>
 
-<div class="asset-list h-full flex flex-auto flex-col flex-grow flex-shrink-0">
-    {#if activityListWithTitles.length > 0}
-        <VirtualList items={activityListWithTitles} let:item>
-            <div class="mb-2">
+<activity-list-container class="asset-list h-full flex flex-auto flex-col flex-grow flex-shrink-0">
+    <activity-list-header class="sticky pb-4">
+        <SearchInput bind:value={searchValue} />
+    </activity-list-header>
+    {#if activityTileDataList.length > 0}
+        <VirtualList items={activityTileDataList} let:item>
+            <activity-tile-container class="mb-2">
                 {#if item.title}
                     <Text fontWeight={FontWeight.semibold} color="gray-600" classes="my-2">
                         {item.title} • {item.amount}
@@ -50,11 +98,11 @@
                     onClaim={() => onClaim(item.activity)}
                     onReject={() => onReject(item.activity)}
                 />
-            </div>
+            </activity-tile-container>
         </VirtualList>
     {:else}
-        <div class="h-full flex flex-col items-center justify-center text-center">
+        <text-container class="h-full flex flex-col items-center justify-center text-center">
             <Text secondary>{localize('general.noRecentHistory')}</Text>
-        </div>
+        </text-container>
     {/if}
-</div>
+</activity-list-container>
