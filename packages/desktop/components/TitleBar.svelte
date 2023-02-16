88c61932b9f983916ab9c71515eca42460b189fc
Jean Ribeiro <contact@jeanribeiro.dev>
2023-02-16 16:28:40 -0300

    refactor: move app body outside of title bar

--- packages/desktop/components/TitleBar.svelte
+++ packages/desktop/components/TitleBar.svelte
@@ -33,50 +33,42 @@
     })
 </script>
 
-<title-bar class="block h-full w-full">
-    <nav
-        class:dark
-        class:with-borders={isDashboardVisible}
-        class="flex flex-row justify-between fixed z-50 top-0 left-0 right-0 w-full h-9 transition-none bg-transparent"
-    >
-        {#if isWindows}
-            <!-- We need to add this element to allow fix the windows resize area issue due to -webkit-app-region: drag -->
-            <windows-resize-area />
+<nav
+    class:dark
+    class:with-borders={isDashboardVisible}
+    class="flex flex-row justify-between fixed z-50 top-0 left-0 right-0 w-full h-9 transition-none bg-transparent"
+>
+    {#if isWindows}
+        <!-- We need to add this element to allow fix the windows resize area issue due to -webkit-app-region: drag -->
+        <windows-resize-area />
+        <button
+            on:click={() => Platform.popupMenu()}
+            class="flex justify-center items-center p-3 text-gray-500 dark:text-gray-100 w-20"
+            type="button"
+        >
+            <Icon icon={IconEnum.Hamburger} />
+        </button>
+        <window-control-buttons class="flex flex-row justify-end space-x-4 w-36 pr-6">
+            <button on:click={() => Platform.minimize()} class="p-2 text-gray-500 dark:text-gray-100" type="button">
+                <Icon icon={IconEnum.Minimize} />
+            </button>
             <button
-                on:click={() => Platform.popupMenu()}
-                class="flex justify-center items-center p-3 text-gray-500 dark:text-gray-100 w-20"
+                on:click={async () => (isMaximized = await Platform.maximize())}
+                class="p-2 text-gray-500 dark:text-gray-100"
                 type="button"
             >
-                <Icon icon={IconEnum.Hamburger} />
+                {#if isMaximized}
+                    <Icon icon={IconEnum.RestoreSize} />
+                {:else}
+                    <Icon icon={IconEnum.Maximize} />
+                {/if}
             </button>
-            <window-control-buttons class="flex flex-row justify-end space-x-4 w-36 pr-6">
-                <button on:click={() => Platform.minimize()} class="p-2 text-gray-500 dark:text-gray-100" type="button">
-                    <Icon icon={IconEnum.Minimize} />
-                </button>
-                <button
-                    on:click={async () => (isMaximized = await Platform.maximize())}
-                    class="p-2 text-gray-500 dark:text-gray-100"
-                    type="button"
-                >
-                    {#if isMaximized}
-                        <Icon icon={IconEnum.RestoreSize} />
-                    {:else}
-                        <Icon icon={IconEnum.Maximize} />
-                    {/if}
-                </button>
-                <button on:click={() => Platform.close()} class="p-2 text-gray-500 dark:text-gray-100" type="button">
-                    <Icon icon={IconEnum.CloseThin} />
-                </button>
-            </window-control-buttons>
-        {/if}
-    </nav>
-    <slot-container
-        class="block fixed left-0 right-0 bottom-0 z-50 top-0"
-        class:top-placement={isWindows || isDashboardVisible}
-    >
-        <slot />
-    </slot-container>
-</title-bar>
+            <button on:click={() => Platform.close()} class="p-2 text-gray-500 dark:text-gray-100" type="button">
+                <Icon icon={IconEnum.CloseThin} />
+            </button>
+        </window-control-buttons>
+    {/if}
+</nav>
 
 <style lang="scss">
     windows-resize-area {
