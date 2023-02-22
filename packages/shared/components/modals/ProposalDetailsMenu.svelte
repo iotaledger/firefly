f2326ad33524d552e1e2d4f0ae92f0228efc5986
Mark Nardi <mark.nardi@iota.org>
2023-02-22 15:07:45 +0100

    refactor: refactor menuitem

--- packages/shared/components/modals/ProposalDetailsMenu.svelte
+++ packages/shared/components/modals/ProposalDetailsMenu.svelte
@@ -1,7 +1,6 @@
 <script lang="ts">
     import { onMount } from 'svelte'
-    import { Modal, MenuItem, Text, Tooltip, Spinner } from 'shared/components'
-    import { Position } from 'shared/components/enums'
+    import { Modal, MenuItem } from 'shared/components'
     import { Icon } from '@auxiliary/icon'
     import { openPopup } from '@auxiliary/popup/actions'
     import { handleError } from '@core/error/handlers'
@@ -16,13 +15,24 @@
     export let modal: Modal = undefined
 
     let isVotingForProposal: boolean
-    let deleteMenuItem: HTMLDivElement
-    let isTooltipVisible = false
     let isBusy = true // starts in a busy state because data needs to be fetched before displaying selectable options
 
     $: isTransferring = $selectedAccount?.isTransferring
     $: isTransferring, $participationOverviewForSelectedAccount, void updateIsVoting() // vote/stop vote changes the isTransferring value. Relying on this requires less updates than relying on proposalsState
     $: isBusy = isVotingForProposal === undefined
+
+    $: buttons = [
+        {
+            icon: Icon.Delete,
+            title: isBusy ? localize('views.governance.details.fetching') : localize('actions.removeProposal'),
+            onClick: onRemoveProposalClick,
+            variant: 'error',
+            isLoading: isBusy,
+            disabled: !features.governance.removeProposals.enabled || isVotingForProposal,
+            enableTooltipVisible: isVotingForProposal !== undefined,
+            tooltip: localize('tooltips.governance.removeProposalWarning'),
+        },
+    ]
 
     function onRemoveProposalClick(): void {
         openPopup({
@@ -39,40 +49,13 @@
         }
     }
 
-    function showTooltip(show: boolean): void {
-        isTooltipVisible = isVotingForProposal !== undefined && show
-    }
-
     onMount(() => void updateIsVoting())
 </script>
 
 <Modal bind:this={modal} {...$$restProps}>
     <div class="flex flex-col">
-        {#if isBusy}
-            <div class="flex items-center p-3 space-x-3 bg-gray-200">
-                <Spinner busy width={16} height={16} />
-                <Text type="p" secondary>{localize('views.governance.details.fetching')}</Text>
-            </div>
-        {:else}
-            <div
-                bind:this={deleteMenuItem}
-                on:mouseenter={() => showTooltip(true)}
-                on:mouseleave={() => showTooltip(false)}
-            >
-                <MenuItem
-                    icon={Icon.Delete}
-                    iconProps={{ width: '16', height: '19' }}
-                    title={localize('actions.removeProposal')}
-                    onClick={onRemoveProposalClick}
-                    variant="error"
-                    disabled={!features.governance.removeProposals.enabled || isVotingForProposal}
-                />
-            </div>
-            {#if isTooltipVisible}
-                <Tooltip anchor={deleteMenuItem} position={Position.Right}>
-                    <Text smaller>{localize('tooltips.governance.removeProposalWarning')}</Text>
-                </Tooltip>
-            {/if}
-        {/if}
+        {#each buttons as button}
+            <MenuItem {...button} />
+        {/each}
     </div>
 </Modal>
