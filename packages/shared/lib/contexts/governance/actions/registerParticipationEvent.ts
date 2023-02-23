c2fb5a8542873c067a15c949d9f686c546944ac4
Mark Nardi <mark.nardi@iota.org>
2023-02-23 19:03:23 +0100

    fix: fix tests

--- packages/shared/lib/contexts/governance/actions/registerParticipationEvent.ts
+++ packages/shared/lib/contexts/governance/actions/registerParticipationEvent.ts
@@ -1,6 +1,6 @@
 import type { Node, ParticipationEventWithNodes, ParticipationEventId } from '@iota/wallet'
 import { IAccountState } from '@core/account'
-import { addProposalToRegisteredProposals } from '../stores'
+import { addOrUpdateProposalToRegisteredProposals } from '../stores'
 import { createProposalFromEvent } from '@contexts/governance'
 
 export async function registerParticipationEvent(
@@ -10,7 +10,7 @@ export async function registerParticipationEvent(
 ): Promise<ParticipationEventWithNodes> {
     const event = await account.registerParticipationEvent(eventId, [node])
     const proposal = createProposalFromEvent(event)
-    addProposalToRegisteredProposals(proposal, account.index)
+    addOrUpdateProposalToRegisteredProposals(proposal, account.index)
 
     return event
 }
