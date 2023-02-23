8d09286b3c1a7ada693c81f56efab335db8de7dd
Begoña Álvarez de la Cruz <balvarez@boxfish.studio>
2023-02-23 15:19:39 +0100

    Update packages/mobile/components/drawers/EnterPasswordDrawer.svelte
    Co-authored-by: Jean Ribeiro <iamjeanribeiro@gmail.com>

--- packages/mobile/components/drawers/EnterPasswordDrawer.svelte
+++ packages/mobile/components/drawers/EnterPasswordDrawer.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
+    import { Button, HTMLButtonType, PasswordInput, Text, TextType } from '@ui'
     import { localize } from '@core/i18n'
     import { unlockStronghold } from '@core/profile'
-    import { Button, HTMLButtonType, PasswordInput, Text, TextType } from '@ui'
 
     export let busyMessage: string = ''
     export let onSuccess: (password?: string) => unknown
