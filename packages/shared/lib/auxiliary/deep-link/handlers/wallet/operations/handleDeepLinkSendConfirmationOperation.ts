3f933f60d75f6c23a2972108c3064cb1edcc0915
Jean Ribeiro <iamjeanribeiro@gmail.com>
2023-02-27 13:07:03 -0300

    adds Mark's suggestion
    Co-authored-by: MarkNerdi <105642810+MarkNerdi@users.noreply.github.com>

--- packages/shared/lib/auxiliary/deep-link/handlers/wallet/operations/handleDeepLinkSendConfirmationOperation.ts
+++ packages/shared/lib/auxiliary/deep-link/handlers/wallet/operations/handleDeepLinkSendConfirmationOperation.ts
@@ -90,7 +90,7 @@ function parseSendConfirmationOperation(searchParams: URLSearchParams): NewTrans
 
     return {
         type: NewTransactionType.TokenTransfer,
-        ...(assetId ? { assetId } : { assetId: baseAsset.id }),
+        assetId: asset.id,
         ...(recipient && { recipient }),
         ...(rawAmount && { rawAmount }),
         ...(unit && { unit }),
