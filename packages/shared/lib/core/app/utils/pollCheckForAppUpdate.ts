8963bd23a85aeabb912fc4fcc49cb46b9568055a
Mark Nardi <mark.nardi@iota.org>
2023-02-28 11:48:11 +0100

    chore: revert polling

--- packages/shared/lib/core/app/utils/pollCheckForAppUpdate.ts
+++ packages/shared/lib/core/app/utils/pollCheckForAppUpdate.ts
@@ -1,9 +1,9 @@
+import { DEFAULT_APP_UPDATER_POLL_INTERVAL } from '../constants'
 import { checkForAppUpdate } from './checkForAppUpdate'
 
 /**
  * Polls checking for an application update.
  */
 export function pollCheckForAppUpdate(): void {
-    void checkForAppUpdate()
-    // setInterval(() => checkForAppUpdate(), DEFAULT_APP_UPDATER_POLL_INTERVAL)
+    setInterval(() => checkForAppUpdate(), DEFAULT_APP_UPDATER_POLL_INTERVAL)
 }
