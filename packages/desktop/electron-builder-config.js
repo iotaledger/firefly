8508ef3ef6838ec366c1cc61e095de11eb99dd47
Mark Nardi <mark.nardi@iota.org>
2023-03-01 11:01:57 +0100

    chore: test release

--- packages/desktop/electron-builder-config.js
+++ packages/desktop/electron-builder-config.js
@@ -95,7 +95,7 @@ const prodConfig = () => ({
         provider: 'generic',
         url: 'https://dl.firefly.iota.org/',
         publishAutoUpdate: true,
-        channel: 'alpha',
+        channel: 'test',
     },
 })
 
