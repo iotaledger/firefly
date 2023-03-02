b17943311d5e91a6908809b579dc2858c4125f47
Tuditi <daviddetroch@pm.me>
2023-03-02 21:40:31 +0100

    fix: revote option always on, correctly build accountState

--- packages/shared/components/modals/ProposalsDetailsMenu.svelte
+++ packages/shared/components/modals/ProposalsDetailsMenu.svelte
@@ -1,15 +1,10 @@
 <script lang="typescript">
     import { Icon } from '@auxiliary/icon'
     import { openPopup, PopupId } from '@auxiliary/popup'
-    import { allParticipationOverviews, isAccountVoting } from '@contexts/governance'
-    import { selectedAccountIndex } from '@core/account'
     import { localize } from '@core/i18n'
     import { Modal, MenuItem } from 'shared/components'
 
     export let modal: Modal = undefined
-
-    let disabled = true
-    $: $allParticipationOverviews, (disabled = !isAccountVoting($selectedAccountIndex))
 
     function onRevoteClick(): void {
         openPopup({
@@ -26,7 +21,6 @@
             iconProps={{ width: '16', height: '19' }}
             title={localize('actions.revote')}
             onClick={onRevoteClick}
-            {disabled}
         />
     </div>
 </Modal>
