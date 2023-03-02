3f56ab1b161d06a55c9309867dfa2a6568615f56
Jean Ribeiro <contact@jeanribeiro.dev>
2023-03-01 21:46:07 -0300

    refactor: mobile VerifyMnemonicView

--- packages/mobile/views/onboarding/views/profile-backup/views/VerifyMnemonicView.svelte
+++ packages/mobile/views/onboarding/views/profile-backup/views/VerifyMnemonicView.svelte
@@ -1,23 +1,29 @@
 <script lang="ts">
     import { onMount } from 'svelte'
+
     import { OnboardingLayout } from '@components'
     import { Icon, Text, TextType, RecoveryPhrase } from '@ui'
-    import { english } from '@auxiliary/wordlists'
+
     import { localize } from '@core/i18n'
-    import { profileBackupRouter } from '@/routers'
+
     import { Mnemonic, onboardingProfile, verifyAndStoreMnemonic } from '@contexts/onboarding'
 
-    export let busy = false
+    import { Icon as IconEnum } from '@auxiliary/icon'
+    import { english } from '@auxiliary/wordlists'
+
+    import { profileBackupRouter } from '@/routers'
+
+    export let busy: boolean = false
 
     const verifyRecoveryPhrase: Mnemonic = []
 
-    let wordChoices = ['', '', '']
-    let verifyIndex = 0
-    let verified = false
+    let wordChoices: string[] = ['', '', '']
+    let verifyIndex: number = 0
+    let isVerified: boolean = false
 
     const title = localize('views.onboarding.profileBackup.verifyMnemonic.title')
 
-    $: if (verified) {
+    $: if (isVerified) {
         completeVerification()
     }
 
@@ -49,7 +55,7 @@
         verifyRecoveryPhrase[verifyIndex] = word
         if ($onboardingProfile?.mnemonic[verifyIndex] === word) {
             if (verifyIndex === $onboardingProfile?.mnemonic.length - 1) {
-                verified = true
+                isVerified = true
             } else {
                 verifyIndex++
                 fillChoices()
@@ -72,38 +78,40 @@
 </script>
 
 <OnboardingLayout {onBackClick} {busy} {title}>
-    <div slot="content" class="w-full h-full flex flex-col p-0">
-        {#if !verified}
-            <Text type={TextType.p} secondary fontSize="15" classes="mb-4">
+    <content-container slot="content" class="w-full h-full flex flex-col p-0">
+        {#if !isVerified}
+            <Text secondary fontSize="15" classes="mb-4">
                 {localize('views.onboarding.profileBackup.verifyMnemonic.body')}
             </Text>
         {:else}
             <div class="flex flex-col items-center bg-gray-100 dark:bg-gray-900 rounded-2xl mt-10 p-5">
                 <div class="bg-green-500 rounded-2xl relative -top-10">
-                    <Icon icon="success-check" classes="text-white" />
+                    <Icon icon={IconEnum.SuccessCheck} classes="text-white" />
                 </div>
-                <Text type={TextType.h2} classes="mb-5 text-center"
-                    >{localize('views.onboarding.profileBackup.verifyMnemonic.verified')}</Text
-                >
-                <Text type={TextType.p} secondary fontSize="15" classes="mb-2"
-                    >{localize('views.onboarding.profileBackup.verifyMnemonic.verifiedBody')}</Text
-                >
+                <Text type={TextType.h2} classes="mb-5 text-center">
+                    {localize('views.onboarding.profileBackup.verifyMnemonic.verified')}
+                </Text>
+                <Text secondary fontSize="15" classes="mb-2">
+                    {localize('views.onboarding.profileBackup.verifyMnemonic.verifiedBody')}
+                </Text>
             </div>
         {/if}
         <RecoveryPhrase recoveryPhrase={$onboardingProfile?.mnemonic} {verifyRecoveryPhrase} boxed />
-    </div>
-    <div slot="footer">
-        {#if !verified}
+    </content-container>
+    <footer-container slot="footer" class="block">
+        {#if !isVerified}
             {#each wordChoices as word}
                 <button
                     type="button"
-                    class="w-full flex flex-row p-4 mb-4 rounded-2xl border border-1 border-solid items-center justify-between border-gray-300 dark:border-gray-700 hover:border-gray-500 dark:hover:border-gray-700 focus:border-gray-500 dark:focus:border-gray-700"
+                    class="w-full flex flex-row p-4 mb-4 rounded-2xl border border-1 border-solid items-center
+                    justify-between border-gray-300 dark:border-gray-700 hover:border-gray-500
+                    dark:hover:border-gray-700 focus:border-gray-500 dark:focus:border-gray-700"
                     on:click={() => handleChoice(word)}
                 >
                     <Text smaller fontSize="15" classes="ml-3" bold>{word}</Text>
-                    <Icon icon="chevron-right" classes="text-gray-800 dark:text-white" />
+                    <Icon icon={IconEnum.ChevronRight} classes="text-gray-800 dark:text-white" />
                 </button>
             {/each}
         {/if}
-    </div>
+    </footer-container>
 </OnboardingLayout>
