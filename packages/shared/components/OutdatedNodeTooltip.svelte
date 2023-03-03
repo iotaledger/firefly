b9bc3cd04c1b31c70b0bbe18033e0e82dffa09c0
Matthew Maxwell <maxwellmattryan@gmail.com>
2023-03-03 03:35:19 -0600

    chore: add tooltip

---
+++ packages/shared/components/OutdatedNodeTooltip.svelte
@@ -0,0 +1,19 @@
+<script lang="ts">
+    import { FontWeight, Text, TextType, Tooltip } from 'shared/components'
+    import { Position } from './enums'
+    import { localize } from '@core/i18n'
+
+    export let anchor: HTMLElement
+    export let position: Position = Position.Right
+</script>
+
+<Tooltip {anchor} {position}>
+    <div class="flex flex-col text-left">
+        <Text type={TextType.p} fontWeight={FontWeight.semibold} fontSize="16" classes="mb-2">
+            {localize('tooltips.outdatedNode.title')}
+        </Text>
+        <Text type={TextType.p} fontWeight={FontWeight.normal}>
+            {localize('tooltips.outdatedNode.body')}
+        </Text>
+    </div>
+</Tooltip>
--- packages/shared/components/ProposalStatusInfo.svelte
+++ packages/shared/components/ProposalStatusInfo.svelte
@@ -1,11 +1,13 @@
 <script lang="ts">
     import { IProposal } from '@contexts/governance/interfaces'
-    import { ProposalStatusPill, ProposalStatusTimelineTooltip } from 'shared/components'
+    import { ProposalStatusPill, ProposalStatusTimelineTooltip, OutdatedNodeTooltip } from 'shared/components'
     import { Position } from 'shared/components/enums'
     import { Icon } from '../lib/auxiliary/icon'
 
     export let proposal: IProposal
     export let position: Position = Position.Right
+
+    const isNodeOutdated = proposal?.isNodeOutdated
 
     let anchor: HTMLElement
     let isTooltipVisible = false
@@ -19,5 +21,14 @@
     <ProposalStatusPill status={proposal?.status} isNodeOutdated={proposal?.isNodeOutdated} icon={Icon.StatusError} />
 </div>
 {#if isTooltipVisible}
-    <ProposalStatusTimelineTooltip bind:anchor milestones={proposal.milestones} status={proposal?.status} {position} />
+    {#if isNodeOutdated}
+        <OutdatedNodeTooltip bind:anchor {position} />
+    {:else}
+        <ProposalStatusTimelineTooltip
+            bind:anchor
+            milestones={proposal.milestones}
+            status={proposal?.status}
+            {position}
+        />
+    {/if}
 {/if}
