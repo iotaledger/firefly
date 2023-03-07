8cb0b3a6502639122e3a1ddff0816077b21c7a44
Jean Ribeiro <iamjeanribeiro@gmail.com>
2023-03-07 11:01:45 -0300

    feat: add stronghold badge (#6084)
    * feat: add stronghold version to profile
    
    * fix: fix default profile
    
    * fix. fix typo
    
    * feat: add feature flag
    
    * chore: revert
    
    * fix: move stronghold version to frontend
    
    * fix: move stronghold version to congratulations view
    
    * enhancement: add remove whitespace functionality in Icon component
    
    * feat: adds StrongholdBadge
    
    * feat: adds StrongholdBadge in Profile
    
    * refactor: Profile.svelte
    
    * fix: remove dummy value
    
    * feat: add feature flag
    
    ---------
    
    Co-authored-by: Mark Nardi <mark.nardi@iota.org>
    Co-authored-by: MarkNerdi <105642810+MarkNerdi@users.noreply.github.com>

--- packages/shared/lib/core/app/utils/isStrongholdUpdated.ts
+++ packages/shared/lib/core/app/utils/isStrongholdUpdated.ts
@@ -1,6 +1,6 @@
-import { IProfile } from '@core/profile/interfaces'
+import { IPersistedProfile } from '@core/profile/interfaces'
 import { STRONGHOLD_VERSION } from '@core/stronghold/constants'
 
-export function isStrongholdUpdated(profile: IProfile): boolean {
+export function isStrongholdUpdated(profile: IPersistedProfile): boolean {
     return profile.strongholdVersion === STRONGHOLD_VERSION
 }
