54bbefde619e4e4cc6029581c5ebec6bb5f8e784
Mark Nardi <mark.nardi@iota.org>
2023-03-07 12:04:22 +0100

    chore: revert

--- packages/shared/lib/core/app/utils/isStrongholdUpdated.ts
+++ packages/shared/lib/core/app/utils/isStrongholdUpdated.ts
@@ -1,7 +1,6 @@
 import { IProfile } from '@core/profile/interfaces'
 import { STRONGHOLD_VERSION } from '@core/stronghold/constants'
-import features from '@features/features'
 
 export function isStrongholdUpdated(profile: IProfile): boolean {
-    return profile.strongholdVersion === STRONGHOLD_VERSION || !features.onboarding.strongholdVersionCheck.enabled
+    return profile.strongholdVersion === STRONGHOLD_VERSION
 }
