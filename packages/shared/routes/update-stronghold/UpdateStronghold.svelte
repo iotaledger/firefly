77ba59b792f8afb9d4d5a463a0bd986d47a362f4
Begoña Alvarez <balvarez@boxfish.studio>
2023-03-07 17:36:46 +0100

    feat: add update stronghold view

---
+++ packages/shared/routes/update-stronghold/UpdateStronghold.svelte
@@ -0,0 +1,23 @@
+<script lang="typescript">
+    import { updateStrongholdRoute, UpdateStrongholdRoute, UpdateStrongholdRouter } from '@core/router'
+    import { Transition } from 'shared/components'
+    import { onMount } from 'svelte'
+    import { Update } from './views'
+
+    export let locale: Locale
+
+    let updateStrongholdRouter: UpdateStrongholdRouter
+
+    onMount(() => {
+        updateStrongholdRouter = new UpdateStrongholdRouter()
+    })
+
+    const next = (): void => updateStrongholdRouter.next()
+    const previous = (): void => updateStrongholdRouter.previous()
+</script>
+
+{#if $updateStrongholdRoute === UpdateStrongholdRoute.Update}
+    <Transition>
+        <Update on:next={next} on:previous={previous} {locale} />
+    </Transition>
+{/if}
---
+++ packages/shared/routes/update-stronghold/views/Update.svelte
@@ -0,0 +1,109 @@
+<script lang="typescript">
+    import { Locale, localize } from '@core/i18n'
+    import { tabFormWithEnterKey } from '@lib/keyboard'
+    import { Animation, Button, OnboardingLayout, Password, Text } from 'shared/components'
+    import { getKeyboardTransitionSpeed, isKeyboardOpened, keyboardHeight, mobile } from 'shared/lib/app'
+    import { api } from 'shared/lib/wallet'
+    import { createEventDispatcher } from 'svelte'
+    import zxcvbn from 'zxcvbn'
+
+    export let locale: Locale
+
+    const dispatch = createEventDispatcher()
+
+    let password = ''
+    let lastCheckedPassword = ''
+    let error = ''
+    const busy = false
+    let passwordContainer: HTMLElement
+
+    $: passwordStrength = checkPasswordStrength(password) ?? passwordStrength
+    $: password, (error = '')
+    $: if ($isKeyboardOpened || error) {
+        setTimeout(() => {
+            passwordContainer?.parentElement?.scrollTo(0, passwordContainer?.parentElement?.scrollHeight)
+        }, getKeyboardTransitionSpeed($isKeyboardOpened))
+    }
+
+    function checkPasswordStrength(password: string): any {
+        const NUMBER_OF_STRENGTH_VALIDATION_CHARS = 64
+        const limitedPassword = password.substring(0, NUMBER_OF_STRENGTH_VALIDATION_CHARS - 1)
+        const hasCheckedPasswordChanged = lastCheckedPassword !== limitedPassword
+        if (hasCheckedPasswordChanged) {
+            lastCheckedPassword = limitedPassword
+            return zxcvbn(limitedPassword)
+        }
+    } // zxcvbn lib recommends to not validate long passwords because of performance issues https://github.com/dropbox/zxcvbn#user-content-performance
+
+    function onKeyPress(e) {
+        tabFormWithEnterKey(e, document, 'update-stronghold-form')
+    }
+
+    function handleContinueClick() {
+        api.setStrongholdPassword(password, {
+            onSuccess() {
+                dispatch('next')
+            },
+            onError(err) {
+                error = locale(err.error)
+            },
+        })
+    }
+    function handleBackClick() {
+        dispatch('previous')
+    }
+</script>
+
+<OnboardingLayout onBackClick={handleBackClick}>
+    <div slot="title">
+        <Text type="h2">{localize('views.updateStronghold.update.title')}</Text>
+    </div>
+    <div
+        slot="leftpane__content"
+        style="flex-auto margin-bottom: {$mobile && $isKeyboardOpened
+            ? $keyboardHeight
+            : 0}px; transition: margin-bottom {getKeyboardTransitionSpeed($isKeyboardOpened) +
+            'ms'} var(--transition-scroll)"
+        bind:this={passwordContainer}
+    >
+        <Text type="p" secondary classes="mb-4">{localize('views.updateStronghold.update.body')}</Text>
+        <form on:submit|preventDefault={handleContinueClick} on:keypress={onKeyPress} id="update-stronghold-form">
+            <Password
+                {error}
+                classes="mb-4"
+                bind:value={password}
+                strengthLevels={4}
+                showRevealToggle
+                showStrengthLevel
+                strength={passwordStrength?.score}
+                {locale}
+                autofocus={!$mobile}
+                disabled={busy}
+            />
+        </form>
+    </div>
+    <div
+        slot="leftpane__action"
+        style="padding-bottom: {$mobile && $isKeyboardOpened
+            ? $keyboardHeight
+            : 0}px; transition: padding-bottom {getKeyboardTransitionSpeed($isKeyboardOpened) +
+            'ms'} var(--transition-scroll)"
+    >
+        <Button type="submit" form="update-stronghold-form" classes="w-full" disabled={!password || busy}>
+            {locale('actions.continue')}
+        </Button>
+    </div>
+    <div
+        slot="rightpane"
+        class="w-full h-full flex justify-center {$mobile ? 'overflow-hidden' : 'bg-pastel-yellow dark:bg-gray-900'}"
+        style="margin-top: {$mobile && $isKeyboardOpened
+            ? -$keyboardHeight
+            : 0}px; transition: margin-top {getKeyboardTransitionSpeed($isKeyboardOpened) +
+            'ms'} var(--transition-scroll)"
+    >
+        <Animation
+            classes="setup-anim-aspect-ratio {$mobile ? 'transform ' : ''}"
+            animation="backup-recovery-phrase-desktop"
+        />
+    </div>
+</OnboardingLayout>
