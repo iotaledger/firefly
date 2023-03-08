7db1923f6bcd820cf094c0b85e9786fa3cb790e2
Jean Ribeiro <iamjeanribeiro@gmail.com>
2023-03-08 05:14:08 -0300

    feat: adds update stronghold view (#6087)
    * feat: adds update stronghold router and route
    
    * feat: adds update stronghold view
    
    * enhancement: replace dummy prop
    
    * fix: fix tests
    
    * feat: adds update stronghold view
    
    * fix: add login option to avoid next route call in login function
    
    * fix: removes unused constant
    
    ---------
    
    Co-authored-by: Mark Nardi <mark.nardi@iota.org>

---
+++ packages/desktop/views/login/views/update-stronghold/UpdateStrongholdRouter.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+    import { UpdateStrongholdView } from './views'
+    import { UpdateStrongholdRoute } from '@core/router/enums/login'
+    import { updateStrongholdRoute } from '@core/router/subrouters/login'
+</script>
+
+{#if $updateStrongholdRoute === UpdateStrongholdRoute.Update}
+    <UpdateStrongholdView />
+{/if}
