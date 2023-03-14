157d40d02c275ca33368463c09b04602cdba4896
Mark Nardi <mark.nardi@iota.org>
2023-03-14 13:06:07 +0100

    fix: download all nfts at one

--- packages/shared/lib/core/nfts/utils/downloadAllNftMedia.ts
+++ packages/shared/lib/core/nfts/utils/downloadAllNftMedia.ts
@@ -2,10 +2,10 @@ import { get } from 'svelte/store'
 import { allAccountNfts } from '../stores'
 import { downloadNftMedia } from '../actions/downloadNftMedia'
 
-export async function downloadAllNftMedia(): Promise<void> {
+export function downloadAllNftMedia(): void {
     for (const [accountIndex, accountNfts] of get(allAccountNfts).entries()) {
         for (const nft of accountNfts) {
-            await downloadNftMedia(nft, accountIndex)
+            void downloadNftMedia(nft, accountIndex)
         }
     }
 }
