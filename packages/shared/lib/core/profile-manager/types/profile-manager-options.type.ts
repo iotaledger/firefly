a76ead85b87fcffc1b681c8ed8fb128f923dfe80
Tuditi <45079109+Tuditi@users.noreply.github.com>
2023-03-17 08:16:55 +0100

    Release: desktop 2.1.0 (#6119)
    * fix: add error catch on update check
    
    * chore: increment version
    
    * feat: update proposal card color (#6118)
    
    * chore: hide edges of percentage bar
    
    * fix: only show dismiss button by default in mobile app (#6114)
    
    Co-authored-by: MarkNerdi <105642810+MarkNerdi@users.noreply.github.com>
    
    * chore: pass showDismiss reactively
    
    * chore: update wallet.rs (#6125)
    
    * enhancement: remove placeholder on images (#6124)
    
    * enhancement: remove placeholder on nft images
    
    * fix: fix background png nfts
    
    * chore: increment desktop version to `2.1.0-beta-3`
    
    * fix: fix royalty validations (#6127)
    
    * fix: fix royalty validations
    
    * chore: fix errors & show error without overlap
    
    ---------
    
    Co-authored-by: Tuditi <daviddetroch@pm.me>
    
    * feat: New Crowdin translations by Github Action (#6111)
    
    Co-authored-by: Crowdin Bot <support+bot@crowdin.com>
    Co-authored-by: Tuditi <45079109+Tuditi@users.noreply.github.com>
    
    * fix: fix toasts (#6129)
    
    * fix: fix toasts
    
    * chore: imporve code of toasts
    
    * fix: fix toasts again
    
    * chore: update wallet.rs (#6131)
    
    * chore: update version
    
    * chore: update version
    
    * chore: update wallet.rs (#6174)
    
    * chore: update version
    
    * chore: enable mainnet profiles in feature flags
    
    * chore: increment desktop version to `2.1.0`
    
    * fix: recover from empty client options
    
    * fix: update active profile instead of only the persisted profile
    
    * fix: use 5 sec milestone interval
    
    * fix: remove menu item from electron menu
    
    Co-authored-by: MarkNerdi <MarkNerdi@users.noreply.github.com>
    
    * fix: test
    
    ---------
    
    Co-authored-by: Mark Nardi <mark.nardi@iota.org>
    Co-authored-by: Matthew Maxwell <44885822+maxwellmattryan@users.noreply.github.com>
    Co-authored-by: Matthew Maxwell <maxwellmattryan@gmail.com>
    Co-authored-by: Jean Ribeiro <iamjeanribeiro@gmail.com>
    Co-authored-by: MarkNerdi <105642810+MarkNerdi@users.noreply.github.com>
    Co-authored-by: Nicole O'Brien <nicole.obrien@iota.org>
    Co-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>
    Co-authored-by: Crowdin Bot <support+bot@crowdin.com>
    Co-authored-by: MarkNerdi <MarkNerdi@users.noreply.github.com>

--- packages/shared/lib/core/profile-manager/types/profile-manager-options.type.ts
+++ packages/shared/lib/core/profile-manager/types/profile-manager-options.type.ts
@@ -1,3 +1,6 @@
+import { IClientOptions } from '@core/network/interfaces'
 import { AccountManagerOptions } from '@iota/wallet'
 
-export type ProfileManagerOptions = AccountManagerOptions
+export type ProfileManagerOptions = Omit<AccountManagerOptions, 'clientOptions'> & {
+    clientOptions: IClientOptions
+}
