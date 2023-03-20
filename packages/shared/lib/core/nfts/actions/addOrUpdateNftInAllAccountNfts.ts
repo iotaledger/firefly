d94aa7aa58b0b75c301912a9f463ff59d092d749
Jean Ribeiro <contact@jeanribeiro.dev>
2023-03-20 18:51:37 -0300

    fix: move updateNftInAllAccountNfts function to its own file

--- packages/shared/lib/core/nfts/actions/addOrUpdateNftInAllAccountNfts.ts
+++ packages/shared/lib/core/nfts/actions/addOrUpdateNftInAllAccountNfts.ts
@@ -15,16 +15,3 @@ export function addOrUpdateNftInAllAccountNfts(accountIndex: number, newNft: INf
         return state
     })
 }
-
-export function updateNftInAllAccountNfts(accountIndex: number, nftId: string, partialNft: Partial<INft>): void {
-    allAccountNfts.update((state) => {
-        if (!state[accountIndex]) {
-            state[accountIndex] = []
-        }
-        const nft = state[accountIndex].find((_nft) => _nft.id === nftId)
-        if (nft) {
-            Object.assign(nft, { ...nft, ...partialNft })
-        }
-        return state
-    })
-}
