b5f2708f74897aef46f7ba2fdc2d5535608cd060
Nicole O'Brien <nicole.obrien@iota.org>
2023-03-21 17:58:23 +0000

    refactor: help and info settings category

--- packages/desktop/views/dashboard/settings/views/advanced/index.js
+++ packages/desktop/views/dashboard/settings/views/advanced/index.js
@@ -1,6 +1,3 @@
-export { default as Diagnostics } from './Diagnostics.svelte'
-export { default as ErrorLog } from './ErrorLog.svelte'
 export { default as HiddenAccounts } from './HiddenAccounts.svelte'
-export { default as MigrateLedgerIndex } from './MigrateLedgerIndex.svelte'
 export { default as NetworkConfiguration } from './NetworkConfiguration.svelte'
 export { default as WalletFinder } from './WalletFinder.svelte'
