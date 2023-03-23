d4a34df9fb71a2d3aa28ea29878b49acc7ef82f8
Mark Nardi <mark.nardi@iota.org>
2023-03-23 08:41:52 +0100

    enhancement: validate nfts at the same time

---
+++ packages/shared/lib/core/nfts/utils/downloadAllNftMediaForAccount.ts
@@ -0,0 +1,72 @@
+import { get } from 'svelte/store'
+import { allAccountNfts, downloadingNftId } from '../stores'
+import { sleep } from '@core/utils'
+import { validateNftMedia } from './validateNftMedia'
+import { Platform } from '@core/app'
+import { INft } from '../interfaces'
+import {
+    CHECK_CURRENTLY_DOWNLOADING_INTERVAL,
+    CHECK_CURRENTLY_DOWNLOADING_MAX_COUNT,
+} from '../constants/check-currently-downloading.constants'
+import { activeProfile } from '@core/profile'
+import { updateNftInAllAccountNfts } from '../actions'
+
+interface DownloadQueueItem {
+    nft: INft
+    downloadUrl: string
+    path: string
+    accountIndex: number
+}
+
+export async function downloadAllNftMediaForAccount(accountIndex: number): Promise<void> {
+    const downloadQueuePromises: Promise<DownloadQueueItem>[] = []
+    for (const nft of get(allAccountNfts)[accountIndex]) {
+        const isAlreadyValidated =
+            nft.downloadMetadata.isLoaded || nft.downloadMetadata.error || nft.downloadMetadata.warning
+        if (!isAlreadyValidated) {
+            downloadQueuePromises.push(validateNft(accountIndex, nft))
+        }
+    }
+    const downloadQueue: DownloadQueueItem[] = await Promise.all(downloadQueuePromises)
+
+    for (const { nft, accountIndex, downloadUrl, path } of downloadQueue.filter((item) => !!item)) {
+        if (!get(activeProfile)?.loggedIn) {
+            break
+        }
+        try {
+            await waitForDownloadToBeFinished()
+            downloadingNftId.set(nft.id)
+            await Platform.downloadFile(downloadUrl, path, nft.id, accountIndex)
+        } catch (error) {
+            downloadingNftId.set(undefined)
+        }
+    }
+}
+
+function validateNft(accountIndex, nft): Promise<DownloadQueueItem> {
+    const promise = new Promise<DownloadQueueItem>((resolve) => {
+        void validateNftMedia(nft).then(({ needsDownload, downloadMetadata, downloadUrl }) => {
+            let downloadQueueItem: DownloadQueueItem = undefined
+            if (needsDownload) {
+                nft.downloadMetadata = { isLoaded: false }
+                downloadQueueItem = { nft: nft, downloadUrl, path: nft.filePath, accountIndex }
+            } else {
+                nft.downloadMetadata = downloadMetadata
+            }
+            updateNftInAllAccountNfts(accountIndex, nft.id, { downloadMetadata })
+            resolve(downloadQueueItem)
+        })
+    })
+
+    return promise
+}
+
+async function waitForDownloadToBeFinished(): Promise<void> {
+    for (let count = 0; count < CHECK_CURRENTLY_DOWNLOADING_MAX_COUNT; count++) {
+        if (!get(downloadingNftId)) {
+            return Promise.resolve()
+        }
+        await sleep(CHECK_CURRENTLY_DOWNLOADING_INTERVAL)
+    }
+    return Promise.reject()
+}
