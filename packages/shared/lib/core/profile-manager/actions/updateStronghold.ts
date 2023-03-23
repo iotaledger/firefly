f1ed294cae4c34d2ba85170983f1621440ff51ff
Jean Ribeiro <contact@jeanribeiro.dev>
2023-03-23 12:34:26 -0300

    fix: adds temporary stronghold directory to updateStronghold

--- packages/shared/lib/core/profile-manager/actions/updateStronghold.ts
+++ packages/shared/lib/core/profile-manager/actions/updateStronghold.ts
@@ -3,17 +3,18 @@ import { get } from 'svelte/store'
 import { getStorageDirectoryOfProfile } from '@core/profile'
 import { api, getSecretManagerPath } from '@core/profile-manager'
 
-import { updateOnboardingProfile } from '@contexts/onboarding/stores'
 import { activeProfileId, updateActiveProfile } from '@core/profile/stores'
 import { STRONGHOLD_VERSION } from '@core/stronghold'
 
+import { getTemporaryProfileManagerStorageDirectory } from '@contexts/onboarding/helpers'
+import { updateOnboardingProfile } from '@contexts/onboarding/stores'
+
 export async function updateStronghold(password: string, isRecovery: boolean = false): Promise<void> {
-    const activeProfileStorageDirectory = await getStorageDirectoryOfProfile(get(activeProfileId))
-    const strongholdDestination = getSecretManagerPath(activeProfileStorageDirectory)
+    const storageDirectory = isRecovery
+        ? await getTemporaryProfileManagerStorageDirectory()
+        : await getStorageDirectoryOfProfile(get(activeProfileId))
+    const strongholdDestination = getSecretManagerPath(storageDirectory)
     await api.migrateStrongholdSnapshotV2ToV3(strongholdDestination, password, strongholdDestination, password)
-    if (isRecovery) {
-        updateOnboardingProfile({ strongholdVersion: STRONGHOLD_VERSION })
-    } else {
-        updateActiveProfile({ strongholdVersion: STRONGHOLD_VERSION })
-    }
+    const updateProfile = isRecovery ? updateOnboardingProfile : updateActiveProfile
+    updateProfile({ strongholdVersion: STRONGHOLD_VERSION })
 }
