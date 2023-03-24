309617bc4360cc20c432a2cd5d0575e95b1a3c87
Nicole O'Brien <nicole.obrien@iota.org>
2023-03-24 10:44:45 +0000

    refactor: simplify settings navigator

--- packages/desktop/views/dashboard/settings/views/SettingsViewer.svelte
+++ packages/desktop/views/dashboard/settings/views/SettingsViewer.svelte
@@ -1,70 +1,9 @@
-<script lang="ts" context="module">
-    import {
-        AdvancedSettingsRoute,
-        CollectiblesSettingsRoute,
-        GeneralSettingsRoute,
-        HelpAndInfoRoute,
-        NetworkSettingsRoute,
-        ProfileSettingsRoute,
-        SecuritySettingsRoute,
-    } from '@core/router'
-    export namespace SettingsNavigatorTypes {
-        export type Settings = {
-            general: typeof GeneralSettingsRoute
-            profile?: typeof ProfileSettingsRoute
-            collectibles?: typeof CollectiblesSettingsRoute
-            network?: typeof NetworkSettingsRoute
-            security?: typeof SecuritySettingsRoute
-            advanced?: typeof AdvancedSettingsRoute
-            helpAndInfo: typeof HelpAndInfoRoute
-        }
-    }
-</script>
-
 <script lang="ts">
-    import { Scroller, SettingsNavigator } from '@components'
+    import { Scroller } from '@components'
     import { _ } from '@core/i18n'
-    import { activeProfile } from '@core/profile'
-    import { settingsRoute, SettingsRoute, SettingsRouteNoProfile, settingsRouter } from '@core/router'
-    import features from '@features/features'
+    import { settingsRoute, settingsRouter } from '@core/router'
     import { onMount } from 'svelte'
-    import { SettingsListForCategory } from './'
-
-    const { loggedIn } = $activeProfile
-
-    const routes = Object.values($loggedIn ? SettingsRoute : SettingsRouteNoProfile)
-
-    let settings: SettingsNavigatorTypes.Settings
-
-    $: if ($loggedIn) {
-        settings = {
-            general: GeneralSettingsRoute,
-            profile: ProfileSettingsRoute,
-            collectibles: CollectiblesSettingsRoute,
-            network: NetworkSettingsRoute,
-            security: SecuritySettingsRoute,
-            advanced: AdvancedSettingsRoute,
-            helpAndInfo: HelpAndInfoRoute,
-        }
-    } else {
-        settings = {
-            general: GeneralSettingsRoute,
-            helpAndInfo: HelpAndInfoRoute,
-        }
-    }
-
-    $: Object.keys(features?.settings)?.forEach((group) => {
-        if (features?.settings?.[group]?.enabled) {
-            Object.keys(features?.settings?.[group])?.forEach((setting) => {
-                if (!features?.settings?.[group]?.[setting]?.enabled) {
-                    const settingName = setting[0].toUpperCase() + setting.slice(1)
-                    delete settings?.[group]?.[settingName]
-                }
-            })
-        } else {
-            delete settings?.[group]
-        }
-    })
+    import { SettingsListForCategory, SettingsNavigator } from './'
 
     function scrollIntoView(id: string, options = null): void {
         if (id) {
@@ -87,12 +26,7 @@
 
 {#key $_}
     <settings-viewer class="flex flex-1 flex-row items-start">
-        <SettingsNavigator
-            {routes}
-            onSettingClick={(id) => scrollIntoView(id)}
-            {settings}
-            bind:route={$settingsRoute}
-        />
+        <SettingsNavigator bind:currentCategory={$settingsRoute} onSettingClick={(id) => scrollIntoView(id)} />
         <div class="h-full w-full">
             <Scroller classes="w-full md:w-3/4 h-full md:pr-100" threshold={100}>
                 <div class="md:w-11/12">
--- packages/desktop/views/dashboard/settings/views/index.js
+++ packages/desktop/views/dashboard/settings/views/index.js
@@ -1,4 +1,5 @@
 export { default as SettingsViewer } from './SettingsViewer.svelte'
 export { default as SettingsListForCategory } from './SettingsListForCategory.svelte'
+export { default as SettingsNavigator } from './SettingsNavigator.svelte'
 
 export * from './settings.constant.js'
