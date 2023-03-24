75a20b94c1e547eded95b8d8dfa8899d00f42806
Nicole O'Brien <nicole.obrien@iota.org>
2023-03-24 14:24:13 +0000

    enhancement: fiat balance on switcher

--- packages/shared/components/molecules/menu-items/AccountSwitcherMenuItem.svelte
+++ packages/shared/components/molecules/menu-items/AccountSwitcherMenuItem.svelte
@@ -1,14 +1,18 @@
 <script lang="ts">
-    import { Text } from 'shared/components'
+    import { Text, FontWeight } from 'shared/components'
     import { AccountLabel } from 'shared/components/atoms'
     import { IAccountState, selectedAccount, setSelectedAccount } from '@core/account'
     import { BASE_TOKEN } from '@core/network'
     import { activeProfile } from '@core/profile'
-    import { formatTokenAmountBestMatch } from '@core/wallet'
+    import { formatTokenAmountBestMatch, selectedAccountAssets } from '@core/wallet'
+    import { getMarketAmountFromAssetValue } from '@core/market/utils'
+    import { formatCurrency } from '@core/i18n'
 
     export let account: IAccountState
     export let onClick: () => unknown
     export let id: string = ''
+
+    $: ({ baseCoin } = $selectedAccountAssets)
 
     function onAccountClick(accountIndex: number): void {
         setSelectedAccount(accountIndex)
@@ -24,10 +28,25 @@
     <div class="flex flex-row items-center space-x-4">
         <AccountLabel selected={account.index === $selectedAccount?.index} {account} />
     </div>
-    <Text classes={account.index === $selectedAccount?.index ? '' : 'opacity-50'} type="h5">
-        {formatTokenAmountBestMatch(
-            Number(account.balances.baseCoin.total),
-            BASE_TOKEN[$activeProfile.networkProtocol]
-        )}
-    </Text>
+    <div class="flex flex-row items-end space-x-1">
+        <Text
+            fontSize="14"
+            fontWeight={FontWeight.semibold}
+            classes="{account.index === $selectedAccount?.index ? '' : 'opacity-50'} text-right"
+        >
+            {formatTokenAmountBestMatch(
+                Number(account.balances.baseCoin.total),
+                BASE_TOKEN[$activeProfile.networkProtocol]
+            )}
+        </Text>
+        <Text
+            fontSize="12"
+            fontWeight={FontWeight.semibold}
+            lineHeight="20"
+            color="blue-500"
+            classes="{account.index === $selectedAccount?.index ? '' : 'opacity-50'} text-right"
+        >
+            {formatCurrency(getMarketAmountFromAssetValue(Number(account.balances.baseCoin.total), baseCoin))}
+        </Text>
+    </div>
 </button>
