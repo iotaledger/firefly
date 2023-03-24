bdeaba9de4e74466f24d69563adf329d6072b83f
Amadeo Marchioni <amadeo.marchioni@iota.org>
2023-03-24 12:48:06 +0000

    feat: implement capacitor import
    Signed-off-by: Amadeo Marchioni <amadeo.marchioni@iota.org>

--- packages/mobile/views/onboarding/views/profile-recovery/views/ImportStrongholdBackupView.svelte
+++ packages/mobile/views/onboarding/views/profile-recovery/views/ImportStrongholdBackupView.svelte
@@ -1,6 +1,7 @@
 <script lang="typescript">
     import { OnboardingLayout } from '@components'
     import { Button, Text, TextType } from '@ui'
+    import { Platform } from '@core/app'
     import { localize } from '@core/i18n'
     import { profileRecoveryRouter } from '@/routers'
     import {
@@ -11,20 +12,14 @@
     } from '@contexts/onboarding'
     import { onMount } from 'svelte'
 
-    // TODO: Not needed for native mobile
-    let fakeSelectionInput: HTMLElement
-
-    let importFile: ImportFile = null
+    const importFile: ImportFile = null
     let importFileName = ''
     let importFilePath = ''
     const title = localize('views.onboarding.profileRecovery.importStrongholdBackup.title')
 
-    function onChooseFileClick(): void {
-        // TODO: implement share functionality here for native mobile
-        fakeSelectionInput.click()
-    }
-
-    function onFileSelected() {
+    async function onFileSelected() {
+        importFilePath = await Platform.getStrongholdBackupDestination(null)
+        importFileName = importFilePath.split('/').pop()
         validateBackupFile(importFileName)
         setProfileRecoveryTypeFromFilename(importFileName)
         updateOnboardingProfile({ importFile, importFilePath })
@@ -35,30 +30,10 @@
         $profileRecoveryRouter.previous()
     }
 
-    // TODO: Remove fake selection for native mobile
-    function fakeSelection(event): void {
-        const fileWithPath = event.target.files[0]
-        const ext = /\.([0-9a-z]+)$/i.exec(fileWithPath.name)
-        if (!ext || !['stronghold'].includes(ext[1])) {
-            return
-        }
-        const reader = new FileReader()
-        reader.onload = (e): void => {
-            importFile = e.target.result
-            importFileName = fileWithPath?.name
-            importFilePath = fileWithPath?.path
-            onFileSelected()
-        }
-        reader.readAsArrayBuffer(fileWithPath)
-    }
-
     onMount(() => {
         updateOnboardingProfile({ importFile: null, importFilePath: null, strongholdPassword: null })
     })
 </script>
-
-<!-- TODO: Not needed for native mobile -->
-<input bind:this={fakeSelectionInput} type="file" class="hidden" on:change={fakeSelection} />
 
 <OnboardingLayout {onBackClick} {title} animation="import-from-file-desktop">
     <div slot="content">
@@ -67,7 +42,7 @@
         >
     </div>
     <div slot="footer" class="flex flex-row flex-wrap justify-between items-center space-x-4">
-        <Button classes="flex-1" onClick={onChooseFileClick}>
+        <Button classes="flex-1" onClick={onFileSelected}>
             {localize('actions.chooseFile')}
         </Button>
     </div>
