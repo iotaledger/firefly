9a4c8f609571a22c6916f9468a73ff6c4de75bef
Nicole O'Brien <nicole.obrien@iota.org>
2023-03-24 13:09:38 +0000

    fix: use constant instead of magic number

--- packages/shared/lib/core/profile/constants/default-persisted-profile-object.constant.ts
+++ packages/shared/lib/core/profile/constants/default-persisted-profile-object.constant.ts
@@ -2,6 +2,7 @@ import { DEFAULT_MARKET_CURRENCY } from '@core/market'
 import { NetworkProtocol, NetworkType } from '@core/network/enums'
 import { ProfileType } from '../enums'
 import { IPersistedProfile } from '../interfaces'
+import { DEFAULT_STRONGHOLD_PASSWORD_TIMEOUT_IN_MINUTES } from './default_stronghold_password_timeout_in_minutes.constant'
 
 export const DEFAULT_PERSISTED_PROFILE_OBJECT: IPersistedProfile = {
     id: '',
@@ -13,7 +14,7 @@ export const DEFAULT_PERSISTED_PROFILE_OBJECT: IPersistedProfile = {
     settings: {
         marketCurrency: DEFAULT_MARKET_CURRENCY,
         lockScreenTimeoutInMinutes: 5,
-        strongholdPasswordTimeoutInMinutes: 2,
+        strongholdPasswordTimeoutInMinutes: DEFAULT_STRONGHOLD_PASSWORD_TIMEOUT_IN_MINUTES,
         maxMediaSizeInMegaBytes: 50,
         hideNetworkStatistics: true,
     },
