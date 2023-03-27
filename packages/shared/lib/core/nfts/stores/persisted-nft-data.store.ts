0686c52f6d927992d69bc6a6e8b6028c2c861968
Mark Nardi <mark.nardi@iota.org>
2023-03-27 12:13:08 +0200

    enhancement: add persisted nft data store

---
+++ packages/shared/lib/core/nfts/stores/persisted-nft-data.store.ts
@@ -0,0 +1,22 @@
+import { derived, get } from 'svelte/store'
+
+import { activeProfile } from '@core/profile'
+import { persistent } from '@core/utils/store'
+import { IPersistedNftData, IPersistedNftStore } from '../interfaces'
+
+export const persistedNfts = persistent<IPersistedNftStore>('persistedNfts', {})
+
+export const persistedNftForActiveProfile = derived(
+    [persistedNfts],
+    ($persistedNfts) => $persistedNfts[get(activeProfile)?.id]
+)
+
+export function addPersistedNftData(nftId: string, newPersistedNft: IPersistedNftData, accountIndex: number): void {
+    persistedNfts.update((state) => {
+        if (!state[accountIndex]) {
+            state[accountIndex] = {}
+        }
+        state[accountIndex][nftId] = newPersistedNft
+        return state
+    })
+}
