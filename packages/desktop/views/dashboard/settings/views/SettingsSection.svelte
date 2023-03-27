8ced939b962d733287471e4e9c7e53717ed24a80
Nicole O'Brien <nicole.obrien@iota.org>
2023-03-27 13:15:44 +0100

    refactor: create settings section to be reused

---
+++ packages/desktop/views/dashboard/settings/views/SettingsSection.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+    import { localize } from '@core/i18n'
+    import { Text, TextType } from '@ui'
+
+    export let setting: string
+</script>
+
+<settings-section class="space-y-5">
+    <div class="space-y-3">
+        <Text type={TextType.h4}>{localize(`views.settings.${setting}.title`)}</Text>
+        <Text type={TextType.p} secondary>{localize(`views.settings.${setting}.description`)}</Text>
+    </div>
+    <slot />
+</settings-section>
--- packages/desktop/views/dashboard/settings/views/index.js
+++ packages/desktop/views/dashboard/settings/views/index.js
@@ -1,5 +1,6 @@
-export { default as SettingsViewer } from './SettingsViewer.svelte'
+export * from './settings.constant.js'
+
 export { default as SettingsListForCategory } from './SettingsListForCategory.svelte'
 export { default as SettingsNavigator } from './SettingsNavigator.svelte'
-
-export * from './settings.constant.js'
+export { default as SettingsSection } from './SettingsSection.svelte'
+export { default as SettingsViewer } from './SettingsViewer.svelte'
