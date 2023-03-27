89f11ebf7be20816bf0adc043883d84254f6143d
MarkNerdi <105642810+MarkNerdi@users.noreply.github.com>
2023-03-27 16:05:31 +0200

    enhancement: persist nft head data (#6249)
    * enhancement: add persisted nft data store
    
    * enhancement: use persisted data
    
    * fix: fix persisted store
    
    * enhancement: add persisted nft data store
    
    * enhancement: use persisted data
    
    * fix: fix persisted store
    
    * fix: add correct type to IPersistedNftData props
    
    * refactor: extract get nft persisted data info logic
    
    ---------
    
    Co-authored-by: Jean Ribeiro <contact@jeanribeiro.dev>

--- packages/shared/lib/core/nfts/stores/index.ts
+++ packages/shared/lib/core/nfts/stores/index.ts
@@ -1,4 +1,5 @@
 export * from './all-account-nfts.store'
 export * from './downloading-nft.store'
+export * from './persisted-nft-data.store'
 export * from './selected-account-nfts.store'
 export * from './selected-nft.store'
---
+++ packages/shared/lib/core/nfts/stores/persisted-nft-data.store.ts
@@ -0,0 +1,22 @@
+import { derived, get, Readable } from 'svelte/store'
+
+import { activeProfile } from '@core/profile'
+import { persistent } from '@core/utils/store'
+import { IPersistedNftData, IPersistedNftStore } from '../interfaces'
+
+export const persistedNfts = persistent<IPersistedNftStore>('persistedNfts', {})
+
+export const persistedNftForActiveProfile: Readable<{ [nftId: string]: IPersistedNftData }> = derived(
+    [persistedNfts, activeProfile],
+    ([$persistedNfts, $activeProfile]) => $persistedNfts[$activeProfile?.id]
+)
+
+export function addPersistedNftData(nftId: string, newPersistedNft: IPersistedNftData): void {
+    persistedNfts.update((state) => {
+        if (!state[get(activeProfile)?.id]) {
+            state[get(activeProfile)?.id] = {}
+        }
+        state[get(activeProfile)?.id][nftId] = newPersistedNft
+        return state
+    })
+}
