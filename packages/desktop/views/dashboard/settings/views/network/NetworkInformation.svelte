84de07c9c1a1075d26a4ae6085a2fec126446916
Mark Nardi <mark.nardi@iota.org>
2023-03-28 09:26:16 +0200

    chore: remove redundant prop

--- packages/desktop/views/dashboard/settings/views/network/NetworkInformation.svelte
+++ packages/desktop/views/dashboard/settings/views/network/NetworkInformation.svelte
@@ -8,21 +8,21 @@
         nodeInfo,
     } from '@core/network'
     import { NetworkSettingsRoute } from '@core/router'
-    import { Text, TextType } from 'shared/components'
+    import { Text } from 'shared/components'
     import SettingsSection from '../SettingsSection.svelte'
 </script>
 
 <SettingsSection setting={NetworkSettingsRoute.NetworkInformation}>
     <div class="flex flex-row justify-between">
         <div class="flex flex-col space-y-1">
-            <Text type={TextType.p} secondary>
+            <Text secondary>
                 {localize('views.settings.networkInformation.connectedTo')}:
             </Text>
-            <Text type={TextType.p} highlighted>{$nodeInfo?.protocol?.networkName}</Text>
+            <Text highlighted>{$nodeInfo?.protocol?.networkName}</Text>
         </div>
         <div class="flex flex-col space-y-1">
-            <Text type={TextType.p} secondary>{localize('views.dashboard.network.status')}:</Text>
-            <Text type={TextType.p} color="{NETWORK_HEALTH_COLORS[$networkStatus.health || 0]}-500">
+            <Text secondary>{localize('views.dashboard.network.status')}:</Text>
+            <Text color="{NETWORK_HEALTH_COLORS[$networkStatus.health || 0]}-500">
                 {localize(
                     `views.dashboard.network.${
                         $networkStatus.description || NETWORK_STATUS_DESCRIPTION[NetworkHealth.Disconnected]
