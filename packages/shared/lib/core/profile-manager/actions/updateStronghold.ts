835f039522311e8c98ecdf91f8118dd2bd6fd9d4
Jean Ribeiro <contact@jeanribeiro.dev>
2023-03-28 10:55:45 -0300

    try-fix: reset profile manager after migration

--- packages/shared/lib/core/profile-manager/actions/updateStronghold.ts
+++ packages/shared/lib/core/profile-manager/actions/updateStronghold.ts
@@ -1,24 +1,51 @@
 import { get } from 'svelte/store'
 
 import { getStorageDirectoryOfProfile } from '@core/profile'
-import { api, getSecretManagerPath } from '@core/profile-manager'
+import {
+    api,
+    buildProfileManagerOptionsFromProfileData,
+    destroyProfileManager,
+    getSecretManagerPath,
+    initialiseProfileManager,
+    profileManager,
+} from '@core/profile-manager'
 
-import { activeProfileId, updateActiveProfile } from '@core/profile/stores'
+import { activeProfile, activeProfileId, updateActiveProfile } from '@core/profile/stores'
 import { STRONGHOLD_VERSION } from '@core/stronghold'
 
 import { copyStrongholdFileToProfileDirectory } from '@contexts/onboarding/helpers'
 import { onboardingProfile, updateOnboardingProfile } from '@contexts/onboarding/stores'
+import { initialiseProfileManagerFromOnboardingProfile } from '@contexts/onboarding'
 
 export async function updateStronghold(password: string, isRecovery: boolean = false): Promise<void> {
     const profileId = isRecovery ? get(onboardingProfile)?.id : get(activeProfileId)
     const profileDirectory = await getStorageDirectoryOfProfile(profileId)
     const secretManagerPath = getSecretManagerPath(profileDirectory)
+
     if (isRecovery) {
         const { importFilePath } = get(onboardingProfile) ?? {}
         await copyStrongholdFileToProfileDirectory(profileDirectory, importFilePath)
         updateOnboardingProfile({ importFilePath: secretManagerPath, importFile: null })
     }
+
     await api.migrateStrongholdSnapshotV2ToV3(secretManagerPath, password, secretManagerPath, password)
     const updateProfile = isRecovery ? updateOnboardingProfile : updateActiveProfile
     updateProfile({ strongholdVersion: STRONGHOLD_VERSION })
+
+    get(profileManager) && (await destroyProfileManager())
+    if (isRecovery) {
+        await initialiseProfileManagerFromOnboardingProfile()
+    } else {
+        const profileManagerOptions = await buildProfileManagerOptionsFromProfileData(get(activeProfile))
+        const { storagePath, coinType, clientOptions, secretManager } = profileManagerOptions
+        updateActiveProfile({ clientOptions })
+        const manager = await initialiseProfileManager(
+            storagePath,
+            coinType,
+            clientOptions,
+            secretManager,
+            get(activeProfileId)
+        )
+        profileManager.set(manager)
+    }
 }
