9cf041e29cf2f4af42d3e15ca5332ee505621ed0
Mark Nardi <mark.nardi@iota.org>
2023-03-29 12:27:05 +0200

    enhancement: improve error handling

--- packages/shared/lib/core/error/constants/wallet-rs-error-parameters.constant.ts
+++ packages/shared/lib/core/error/constants/wallet-rs-error-parameters.constant.ts
@@ -2,12 +2,12 @@ import { ClientError, WalletRsError, IotaClientError } from '../enums'
 import { IErrorParameters } from '../interfaces'
 
 export const WALLET_RS_ERROR_PARAMETERS: Readonly<{
-    [WalletRsError.ClientError]?: { [key in ClientError]?: Partial<IErrorParameters> }
+    [WalletRsError.Client]?: { [key in ClientError]?: Partial<IErrorParameters> }
     [WalletRsError.InsufficientFunds]?: Partial<IErrorParameters>
     [WalletRsError.NoOutputsToConsolidate]?: Partial<IErrorParameters>
     [WalletRsError.IotaClientError]?: { [key in IotaClientError]?: Partial<IErrorParameters> }
 }> = {
-    [WalletRsError.ClientError]: {
+    [WalletRsError.Client]: {
         [ClientError.NoSyncedNode]: {
             localizationKey: `error.node.${ClientError.NoSyncedNode}`,
             logToConsole: true,
@@ -16,6 +16,12 @@ export const WALLET_RS_ERROR_PARAMETERS: Readonly<{
         },
         [ClientError.TimeNotSynced]: {
             localizationKey: `error.node.${ClientError.TimeNotSynced}`,
+            logToConsole: true,
+            saveToErrorLog: true,
+            showNotification: true,
+        },
+        [ClientError.InsufficientAmount]: {
+            localizationKey: 'error.send.insufficientFundsStorageDeposit',
             logToConsole: true,
             saveToErrorLog: true,
             showNotification: true,
