2d297d6b1e176b18b008f4d3e43d97838b1485a4
Amadeo Marchioni <amadeo.marchioni@iota.org>
2023-03-30 10:59:16 +0100

    chore: modify font-size according Firgma
    Signed-off-by: Amadeo Marchioni <amadeo.marchioni@iota.org>

--- packages/shared/components/atoms/labels/AccountLabel.svelte
+++ packages/shared/components/atoms/labels/AccountLabel.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
     import { IAccountState } from '@core/account'
+    import { mobile } from '@core/app'
     import { Text, FontWeight, ColoredCircle } from 'shared/components/'
 
     export let account: IAccountState
@@ -8,7 +9,7 @@
 
 <div class="flex flex-row items-center space-x-3">
     <ColoredCircle color={account?.color} />
-    <Text fontSize="14" fontWeight={FontWeight.semibold} classes={selected ? '' : 'opacity-50'}>
+    <Text fontSize={$mobile ? '16' : '14'} fontWeight={FontWeight.semibold} classes={selected ? '' : 'opacity-50'}>
         {account?.name}
     </Text>
 </div>
