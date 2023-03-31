14683b9d05ee59b5f258cc0820b72dd9d14450cf
Tuditi <daviddetroch@pm.me>
2023-03-31 14:04:31 +0200

    fix: optional chaining of nftId

--- packages/shared/lib/core/wallet/utils/send/validateSendConfirmation.ts
+++ packages/shared/lib/core/wallet/utils/send/validateSendConfirmation.ts
@@ -13,7 +13,7 @@ export function validateSendConfirmation(outputOptions: OutputOptions, outputTyp
     const { storageDeposit, giftedStorageDeposit } = getStorageDepositFromOutput(outputTypes)
     const expirationDateTime = convertUnixTimestampToDate(outputOptions?.unlocks?.expirationUnixTime)
 
-    const isNft = !!outputOptions.assets.nftId
+    const isNft = !!outputOptions?.assets?.nftId
     if (!isNft && (balance < amount + storageDeposit || balance < amount + giftedStorageDeposit)) {
         throw new InsufficientFundsForStorageDepositError()
     } else if (expirationDateTime && !isValidExpirationDateTime(expirationDateTime)) {
