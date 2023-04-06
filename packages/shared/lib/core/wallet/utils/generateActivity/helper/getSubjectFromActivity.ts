508f8c65ca6112ffe147c39c6be393319315bd02
Tuditi <daviddetroch@pm.me>
2023-04-06 22:21:18 +0200

    chore: deduplicate getSubjectLocale function

--- packages/shared/lib/core/wallet/utils/generateActivity/helper/getSubjectFromActivity.ts
+++ packages/shared/lib/core/wallet/utils/generateActivity/helper/getSubjectFromActivity.ts
@@ -1,3 +1,6 @@
+import { localize } from '@core/i18n'
+import { truncateString } from '@core/utils'
+import { ActivityType } from '@core/wallet/enums'
 import type { Activity, Subject } from '@core/wallet/types'
 
 export function getSubjectFromActivity(activity: Activity): Subject {
@@ -10,5 +13,22 @@ export function getSubjectFromActivity(activity: Activity): Subject {
         }
     } else {
         return activity.subject
+    }
+}
+
+export function getSubjectLocaleFromActivity(activity: Activity): string {
+    const { subject } = activity
+
+    if (activity.type === ActivityType.Basic && activity?.isShimmerClaiming) {
+        return localize('general.shimmerGenesis')
+    } else if (subject?.type === 'account') {
+        return truncateString(subject?.account?.name, 13, 0)
+    } else if (subject?.type === 'address') {
+        const address = activity?.parsedLayer2Metadata?.ethereumAddress ?? subject?.address
+        return truncateString(address, 6, 6)
+    } else if (subject?.type === 'network') {
+        return subject?.network
+    } else {
+        return localize('general.unknownAddress')
     }
 }
