37946a9138927f8280e7f9ea21b7f81eab4d9495
Nicole O'Brien <nicole.obrien@iota.org>
2023-04-06 15:17:11 +0100

    refactor: use networkId on chooseNetworkView

--- packages/desktop/features/onboarding.features.ts
+++ packages/desktop/features/onboarding.features.ts
@@ -1,252 +1,177 @@
-const onboardingFeatures = {
+import { IFeatureFlag } from './feature-flag.interface'
+
+interface IOnboardingFeaturesForNetwork extends IFeatureFlag {
+    newProfile: IFeatureFlag & {
+        softwareProfile: IFeatureFlag
+        ledgerProfile: IFeatureFlag
+    }
+    restoreProfile: IFeatureFlag & {
+        recoveryPhrase: IFeatureFlag
+        strongholdBackup: IFeatureFlag
+        ledgerBackup: IFeatureFlag
+    }
+    claimRewards: IFeatureFlag & {
+        recoveryPhrase: IFeatureFlag
+        strongholdBackup: IFeatureFlag
+        ledgerBackup: IFeatureFlag
+    }
+}
+
+interface IOnboardingFeatures extends IFeatureFlag {
+    iota: IOnboardingFeaturesForNetwork
+    shimmer: IOnboardingFeaturesForNetwork
+    testnet: IOnboardingFeaturesForNetwork
+    custom: IOnboardingFeaturesForNetwork
+    strongholdVersionCheck: IFeatureFlag
+}
+
+const onboardingFeatures: IOnboardingFeatures = {
     enabled: true,
     iota: {
-        enabled: false,
-        mainnet: {
+        enabled: true,
+        newProfile: {
             enabled: false,
-            claimRewards: {
+            softwareProfile: {
                 enabled: false,
-                hidden: true,
-                recoveryPhrase: {
-                    enabled: false,
-                },
-                strongholdBackup: {
-                    enabled: false,
-                },
-                ledgerBackup: {
-                    enabled: false,
-                },
             },
-            newProfile: {
+            ledgerProfile: {
                 enabled: false,
-                softwareProfile: {
-                    enabled: false,
-                },
-                ledgerProfile: {
-                    enabled: false,
-                },
-            },
-            restoreProfile: {
-                enabled: false,
-                migrateSeed: {
-                    enabled: false,
-                },
-                recoveryPhrase: {
-                    enabled: false,
-                },
-                strongholdBackup: {
-                    enabled: false,
-                },
-                ledgerBackup: {
-                    enabled: false,
-                },
             },
         },
-        devnet: {
+        restoreProfile: {
+            enabled: false,
+            recoveryPhrase: {
+                enabled: false,
+            },
+            strongholdBackup: {
+                enabled: false,
+            },
+            ledgerBackup: {
+                enabled: false,
+            },
+        },
+        claimRewards: {
             enabled: false,
             hidden: true,
-            claimRewards: {
+            recoveryPhrase: {
                 enabled: false,
-                hidden: true,
-                recoveryPhrase: {
-                    enabled: false,
-                },
-                strongholdBackup: {
-                    enabled: false,
-                },
-                ledgerBackup: {
-                    enabled: false,
-                },
             },
-            newProfile: {
+            strongholdBackup: {
                 enabled: false,
-                softwareProfile: {
-                    enabled: false,
-                },
-                ledgerProfile: {
-                    enabled: false,
-                },
             },
-            restoreProfile: {
+            ledgerBackup: {
                 enabled: false,
-                migrateSeed: {
-                    enabled: false,
-                },
-                recoveryPhrase: {
-                    enabled: false,
-                },
-                strongholdBackup: {
-                    enabled: false,
-                },
-                ledgerBackup: {
-                    enabled: false,
-                },
-            },
-        },
-        'private-net': {
-            enabled: false,
-            claimRewards: {
-                enabled: false,
-                hidden: true,
-                recoveryPhrase: {
-                    enabled: false,
-                },
-                strongholdBackup: {
-                    enabled: false,
-                },
-                ledgerBackup: {
-                    enabled: false,
-                },
-            },
-            newProfile: {
-                enabled: false,
-                softwareProfile: {
-                    enabled: false,
-                },
-                ledgerProfile: {
-                    enabled: false,
-                },
-            },
-            restoreProfile: {
-                enabled: false,
-                migrateSeed: {
-                    enabled: false,
-                },
-                recoveryPhrase: {
-                    enabled: false,
-                },
-                strongholdBackup: {
-                    enabled: false,
-                },
-                ledgerBackup: {
-                    enabled: false,
-                },
             },
         },
     },
     shimmer: {
         enabled: true,
-        mainnet: {
-            enabled: false,
-            claimRewards: {
+        claimRewards: {
+            enabled: true,
+            hidden: false,
+            recoveryPhrase: {
                 enabled: true,
-                hidden: false,
-                recoveryPhrase: {
-                    enabled: true,
-                },
-                strongholdBackup: {
-                    enabled: true,
-                },
-                ledgerBackup: {
-                    enabled: true,
-                },
             },
-            newProfile: {
+            strongholdBackup: {
                 enabled: true,
-                softwareProfile: {
-                    enabled: true,
-                },
-                ledgerProfile: {
-                    enabled: true,
-                },
             },
-            restoreProfile: {
+            ledgerBackup: {
                 enabled: true,
-                migrateSeed: {
-                    enabled: false,
-                    hidden: true,
-                },
-                recoveryPhrase: {
-                    enabled: true,
-                },
-                strongholdBackup: {
-                    enabled: true,
-                },
-                ledgerBackup: {
-                    enabled: true,
-                },
             },
         },
-        devnet: {
+        newProfile: {
             enabled: true,
-            claimRewards: {
+            softwareProfile: {
                 enabled: true,
-                hidden: false,
-                recoveryPhrase: {
-                    enabled: true,
-                },
-                strongholdBackup: {
-                    enabled: true,
-                },
-                ledgerBackup: {
-                    enabled: true,
-                },
             },
-            newProfile: {
+            ledgerProfile: {
                 enabled: true,
-                softwareProfile: {
-                    enabled: true,
-                },
-                ledgerProfile: {
-                    enabled: true,
-                },
-            },
-            restoreProfile: {
-                enabled: true,
-                migrateSeed: {
-                    enabled: false,
-                    hidden: true,
-                },
-                recoveryPhrase: {
-                    enabled: true,
-                },
-                strongholdBackup: {
-                    enabled: true,
-                },
-                ledgerBackup: {
-                    enabled: true,
-                },
             },
         },
-        'private-net': {
+        restoreProfile: {
             enabled: true,
-            claimRewards: {
+            recoveryPhrase: {
                 enabled: true,
-                hidden: false,
-                recoveryPhrase: {
-                    enabled: true,
-                },
-                strongholdBackup: {
-                    enabled: true,
-                },
-                ledgerBackup: {
-                    enabled: true,
-                },
             },
-            newProfile: {
+            strongholdBackup: {
                 enabled: true,
-                softwareProfile: {
-                    enabled: true,
-                },
-                ledgerProfile: {
-                    enabled: true,
-                },
             },
-            restoreProfile: {
+            ledgerBackup: {
                 enabled: true,
-                migrateSeed: {
-                    enabled: false,
-                    hidden: true,
-                },
-                recoveryPhrase: {
-                    enabled: true,
-                },
-                strongholdBackup: {
-                    enabled: true,
-                },
-                ledgerBackup: {
-                    enabled: true,
-                },
+            },
+        },
+    },
+    testnet: {
+        enabled: true,
+        claimRewards: {
+            enabled: true,
+            hidden: false,
+            recoveryPhrase: {
+                enabled: true,
+            },
+            strongholdBackup: {
+                enabled: true,
+            },
+            ledgerBackup: {
+                enabled: true,
+            },
+        },
+        newProfile: {
+            enabled: true,
+            softwareProfile: {
+                enabled: true,
+            },
+            ledgerProfile: {
+                enabled: true,
+            },
+        },
+        restoreProfile: {
+            enabled: true,
+            recoveryPhrase: {
+                enabled: true,
+            },
+            strongholdBackup: {
+                enabled: true,
+            },
+            ledgerBackup: {
+                enabled: true,
+            },
+        },
+    },
+    custom: {
+        enabled: true,
+        claimRewards: {
+            enabled: true,
+            hidden: false,
+            recoveryPhrase: {
+                enabled: true,
+            },
+            strongholdBackup: {
+                enabled: true,
+            },
+            ledgerBackup: {
+                enabled: true,
+            },
+        },
+        newProfile: {
+            enabled: true,
+            softwareProfile: {
+                enabled: true,
+            },
+            ledgerProfile: {
+                enabled: true,
+            },
+        },
+        restoreProfile: {
+            enabled: true,
+            recoveryPhrase: {
+                enabled: true,
+            },
+            strongholdBackup: {
+                enabled: true,
+            },
+            ledgerBackup: {
+                enabled: true,
             },
         },
     },
