50fc1c566425a54162e70ca6a99f9a42193405c8
Begoña Alvarez <balvarez@boxfish.studio>
2023-04-06 18:18:41 +0200

    fix: add patch for debug mode to enable devtools in iOS 16
    Signed-off-by: Begoña Alvarez <balvarez@boxfish.studio>

--- packages/mobile/ios/App/App/AppDelegate.swift
+++ packages/mobile/ios/App/App/AppDelegate.swift
@@ -9,6 +9,20 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
   func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
     // Override point for customization after application launch.
+    
+    // --> start of patch, source https://github.com/ionic-team/capacitor/issues/6441#issuecomment-1491092025
+    // When running from Xcode in debug mode, enable webView inspection 5 seconds after startup
+    #if DEBUG
+      if #available(macOS 13.3, iOS 16.4, tvOS 16.4, *) {
+            DispatchQueue.main.asyncAfter(deadline: .now() + 5.0) {
+                  if let vc = self.window?.rootViewController as? CAPBridgeViewController {
+                      vc.bridge?.webView?.isInspectable = true;
+                  }
+            }
+      }
+    #endif
+    // <-- end of patch
+    
     return true
   }
 
