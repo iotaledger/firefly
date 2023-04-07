d7ab3c252b70fd24737fdfe3da19dd337394c88a
Tuditi <45079109+Tuditi@users.noreply.github.com>
2023-04-07 11:13:01 +0200

    Update packages/shared/lib/core/layer-2/utils/getLayer2NetworkFromAddress.ts
    Co-authored-by: MarkNerdi <105642810+MarkNerdi@users.noreply.github.com>

--- packages/shared/lib/core/layer-2/utils/getLayer2NetworkFromAddress.ts
+++ packages/shared/lib/core/layer-2/utils/getLayer2NetworkFromAddress.ts
@@ -3,10 +3,6 @@ import { NETWORK_ADDRESS } from '@core/layer-2/constants'
 import { activeProfile } from '@core/profile/stores'
 
 export function getLayer2NetworkFromAddress(address: string): string {
-    for (const network of Object.entries(NETWORK_ADDRESS[get(activeProfile)?.networkType])) {
-        if (network[1] === address) {
-            return network[0]
-        }
-    }
-    return undefined
-}
+    const entry = Object.entries(NETWORK_ADDRESS[get(activeProfile)?.networkType]).find(network => network[1] === address)
+
+return entry?.[0]
