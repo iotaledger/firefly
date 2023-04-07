ca10438f4e075797c22fb0ecbb9bb085e04d0b9d
Mark Nardi <mark.nardi@iota.org>
2023-04-07 10:37:05 +0200

    feature: refresh nft media

--- packages/desktop/views/dashboard/settings/views/collectibles/collectibles-settings.constant.js
+++ packages/desktop/views/dashboard/settings/views/collectibles/collectibles-settings.constant.js
@@ -1,4 +1,13 @@
 import { CollectiblesSettingsRoute } from '@core/router'
-import { MaxMediaSize } from '.'
+import { MaxMediaSize, RefreshNftMedia } from '.'
 
-export const COLLECTIBLES_SETTINGS = [{ component: MaxMediaSize, childRoute: CollectiblesSettingsRoute.MaxMediaSize }]
+export const COLLECTIBLES_SETTINGS = [
+    {
+        component: MaxMediaSize,
+        childRoute: CollectiblesSettingsRoute.MaxMediaSize,
+    },
+    {
+        component: RefreshNftMedia,
+        childRoute: CollectiblesSettingsRoute.RefreshNftMedia,
+    },
+]
--- packages/shared/lib/core/router/enums/settings/collectibles-settings-route.enum.ts
+++ packages/shared/lib/core/router/enums/settings/collectibles-settings-route.enum.ts
@@ -1,3 +1,4 @@
 export enum CollectiblesSettingsRoute {
     MaxMediaSize = 'maxMediaSize',
+    RefreshNftMedia = 'refreshNftMedia',
 }
