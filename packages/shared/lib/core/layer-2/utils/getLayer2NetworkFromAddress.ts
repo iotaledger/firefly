5ae0b31d0a1dd8e0b4180b3579d6a6a73cb17c4d
Tuditi <daviddetroch@pm.me>
2023-04-07 11:19:06 +0200

    fix: missing bracket

--- packages/shared/lib/core/layer-2/utils/getLayer2NetworkFromAddress.ts
+++ packages/shared/lib/core/layer-2/utils/getLayer2NetworkFromAddress.ts
@@ -3,6 +3,7 @@ import { NETWORK_ADDRESS } from '@core/layer-2/constants'
 import { activeProfile } from '@core/profile/stores'
 
 export function getLayer2NetworkFromAddress(address: string): string {
-    const entry = Object.entries(NETWORK_ADDRESS[get(activeProfile)?.networkType]).find(network => network[1] === address)
-
-return entry?.[0]
+    const networkType = get(activeProfile)?.networkType
+    const entry = Object.entries(NETWORK_ADDRESS[networkType]).find((network) => network[1] === address)
+    return entry?.[0]
+}
