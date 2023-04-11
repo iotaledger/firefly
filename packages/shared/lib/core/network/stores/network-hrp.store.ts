f0ed055f01b5075a7698483e5bda449b4ce5b4c9
Nicole O'Brien <nicole.obrien@iota.org>
2023-04-11 22:33:28 +0100

    refactor: only get networkHrp from activeProfile

--- packages/shared/lib/core/network/stores/network-hrp.store.ts
+++ packages/shared/lib/core/network/stores/network-hrp.store.ts
@@ -1,11 +1,4 @@
 import { activeProfile } from '@core/profile'
 import { derived } from 'svelte/store'
-import { nodeInfo } from './node-info.store'
 
-export const networkHrp = derived([nodeInfo, activeProfile], ([$nodeInfo, $activeProfile]) => {
-    if ($nodeInfo) {
-        return $nodeInfo?.protocol?.bech32Hrp ?? $activeProfile?.network?.protocol?.bech32Hrp ?? ''
-    } else {
-        return $activeProfile?.network?.protocol?.bech32Hrp ?? ''
-    }
-})
+export const networkHrp = derived([activeProfile], ([$activeProfile]) => $activeProfile?.network?.protocol?.bech32Hrp)
