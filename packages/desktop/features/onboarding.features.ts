427f1126971798bb461e9f11fe72878f47628b09
Nicole O'Brien <nicole.obrien@iota.org>
2023-04-11 15:49:12 +0100

    fix: use correct onboarding interface

--- packages/desktop/features/onboarding.features.ts
+++ packages/desktop/features/onboarding.features.ts
@@ -1,35 +1,4 @@
-import { IFeatureFlag } from './feature-flag.interface'
-
-interface INewProfileFeatures extends IFeatureFlag {
-    softwareProfile: IFeatureFlag
-    ledgerProfile: IFeatureFlag
-}
-
-interface IRestoreProfileFeatures extends IFeatureFlag {
-    recoveryPhrase: IFeatureFlag
-    strongholdBackup: IFeatureFlag
-    ledgerBackup: IFeatureFlag
-}
-
-interface IClaimRewardsFeatures extends IFeatureFlag {
-    recoveryPhrase: IFeatureFlag
-    strongholdBackup: IFeatureFlag
-    ledgerBackup: IFeatureFlag
-}
-
-interface IOnboardingFeaturesForNetwork extends IFeatureFlag {
-    newProfile: INewProfileFeatures
-    restoreProfile: IRestoreProfileFeatures
-    claimRewards: IClaimRewardsFeatures
-}
-
-interface IOnboardingFeatures extends IFeatureFlag {
-    iota: IOnboardingFeaturesForNetwork
-    shimmer: IOnboardingFeaturesForNetwork
-    testnet: IOnboardingFeaturesForNetwork
-    custom: IOnboardingFeaturesForNetwork
-    strongholdVersionCheck: IFeatureFlag
-}
+import { IOnboardingFeatures } from '@lib/features/interfaces'
 
 const onboardingFeatures: IOnboardingFeatures = {
     enabled: true,
