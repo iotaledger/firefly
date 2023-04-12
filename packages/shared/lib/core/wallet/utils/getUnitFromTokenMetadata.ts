cfd98a019419628c20036e599df8aa3c1937bbe3
Nicole O'Brien <nicole.obrien@iota.org>
2023-04-12 14:30:51 +0100

    fix: getUnitFromTokenMetadata

--- packages/shared/lib/core/wallet/utils/getUnitFromTokenMetadata.ts
+++ packages/shared/lib/core/wallet/utils/getUnitFromTokenMetadata.ts
@@ -1,13 +1,7 @@
-import { IBaseToken, IIrc30Metadata } from '../interfaces'
+import { TokenStandard } from '../enums'
 import { TokenMetadata } from '../types'
-import { isBaseToken } from './isBaseToken'
 
 export function getUnitFromTokenMetadata(tokenMetadata: TokenMetadata): string {
-    if (isBaseToken(tokenMetadata)) {
-        const baseToken = tokenMetadata as IBaseToken
-        return baseToken?.useMetricPrefix ? baseToken?.unit : baseToken?.subunit
-    } else {
-        const irc30Metadata = tokenMetadata as IIrc30Metadata
-        return irc30Metadata?.symbol
-    }
+    const isBaseToken = tokenMetadata.standard === TokenStandard.BaseCoin
+    return isBaseToken ? tokenMetadata?.unit : tokenMetadata?.symbol
 }
--- packages/shared/lib/core/wallet/utils/isBaseToken.ts
+++
@@ -1,6 +0,0 @@
-import { TokenStandard } from '../enums'
-import { TokenMetadata } from '../types'
-
-export function isBaseToken(tokenMetadata: TokenMetadata): boolean {
-    return tokenMetadata?.standard === TokenStandard.BaseCoin
-}
