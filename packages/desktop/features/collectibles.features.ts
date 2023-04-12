3d648383c48c0cdbc9197843dd07bb373db0260b
Nicole O'Brien <nicole.obrien@iota.org>
2023-04-12 02:10:48 +0100

    refactor: type feature flags (#6456)
    * refactor: add initial types
    
    * refactor: type settings feature flags
    
    * refactor: type wallet feature flags
    
    * refactor: type wallet feature flags
    
    * fix: use wallet features interface
    
    * refactor: type governance feature flags
    
    * refactor: type developer feature flags
    
    * refactor: type electron feature flags
    
    * fix: adds type in constants and some missing fields
    
    * fix: tests
    
    ---------
    
    Co-authored-by: Jean Ribeiro <iamjeanribeiro@gmail.com>
    Co-authored-by: Jean Ribeiro <contact@jeanribeiro.dev>

--- packages/desktop/features/collectibles.features.ts
+++ packages/desktop/features/collectibles.features.ts
@@ -1,4 +1,6 @@
-const collectiblesFeatures = {
+import { ICollectiblesFeatures } from '@lib/features/interfaces'
+
+const collectiblesFeatures: ICollectiblesFeatures = {
     enabled: true,
     useCaching: {
         enabled: true, // If this is set to false, `https://*` needs to be added to `img-src` and `media-src` in `public/index.html`
