31faa3a9c41600790143ed4312002d6f58907736
Nicole O'Brien <nicole.obrien@iota.org>
2023-04-12 19:47:07 +0100

    refactor: rename SetupPrivateNetwork to SetupCustomNetwork

--- packages/desktop/views/onboarding/views/network-setup/views/index.js
+++ packages/desktop/views/onboarding/views/network-setup/views/index.js
@@ -1,2 +1,2 @@
 export { default as ChooseNetworkView } from './ChooseNetworkView.svelte'
-export { default as SetupPrivateNetworkConnectionView } from './SetupPrivateNetworkConnectionView.svelte'
+export { default as SetupCustomNetworkConnectionView } from './SetupCustomNetworkConnectionView.svelte'
