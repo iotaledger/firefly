c6087a44641185a4e8d12bdd9db4732883d1cb1c
Nicole O'Brien <nicole.obrien@iota.org>
2023-04-12 10:57:07 +0100

    refactor: remove protocol selection view/routes (#6465)
    * refactor: remove protocol selection
    
    * feat: remove protocol selection from mobile onboarding
    
    ---------
    
    Co-authored-by: MarkNerdi <105642810+MarkNerdi@users.noreply.github.com>
    Co-authored-by: Begoña Alvarez <balvarez@boxfish.studio>

--- packages/desktop/views/onboarding/views/network-setup/views/index.js
+++ packages/desktop/views/onboarding/views/network-setup/views/index.js
@@ -1,3 +1,2 @@
 export { default as ChooseNetworkView } from './ChooseNetworkView.svelte'
-export { default as ChooseProtocolView } from './ChooseProtocolView.svelte'
 export { default as SetupPrivateNetworkConnectionView } from './SetupPrivateNetworkConnectionView.svelte'
