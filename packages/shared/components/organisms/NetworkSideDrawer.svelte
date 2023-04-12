f9bad66ad7d5d92fb932096f4a39bd6c40308c48
Mark Nardi <mark.nardi@iota.org>
2023-04-12 12:12:27 +0200

    enhancement: open side drawer

--- packages/shared/components/organisms/NetworkSideDrawer.svelte
+++ packages/shared/components/organisms/NetworkSideDrawer.svelte
@@ -1,8 +1,23 @@
 <script lang="ts">
-    import { isNetworkSideDrawerOpen } from '@core/network/stores/network-side-drawer.store'
-    import SideDrawer from 'shared/components/atoms/SideDrawer.svelte'
+    import { localize } from '@core/i18n'
+    import { isNetworkSideDrawerOpen } from '@core/network/stores'
+    import { SideDrawer, Text, TextType, Icon } from 'shared/components/'
+    import { Icon as IconEnum } from '@lib/auxiliary/icon'
+
+    function closeDrawer(): void {
+        $isNetworkSideDrawerOpen = false
+    }
 </script>
 
 {#if $isNetworkSideDrawerOpen}
-    <SideDrawer>test</SideDrawer>
+    <SideDrawer onClose={closeDrawer}>
+        <div class="flex flex-row justify-between mb-6">
+            <div class="flex justify-center content-center">
+                <Text type={TextType.h4} classes="text-center">{localize('views.dashboard.network.drawer.title')}</Text>
+            </div>
+            <button type="button" on:click={closeDrawer}>
+                <Icon width="24" height="24" icon={IconEnum.Close} classes="text-gray-500" />
+            </button>
+        </div>
+    </SideDrawer>
 {/if}
--- packages/shared/lib/core/network/stores/index.ts
+++ packages/shared/lib/core/network/stores/index.ts
@@ -1,3 +1,4 @@
 export * from './network-hrp.store'
+export * from './network-side-drawer.store'
 export * from './network-status.store'
 export * from './node-info.store'
