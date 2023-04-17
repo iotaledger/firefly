cfbde56df3206af1cf2b1f85209414db516459f4
Mark Nardi <mark.nardi@iota.org>
2023-04-17 16:58:40 +0200

    enhancement: precalculate storage deposit for nfts

--- packages/shared/lib/core/nfts/utils/buildNftFromNftOutput.ts
+++ packages/shared/lib/core/nfts/utils/buildNftFromNftOutput.ts
@@ -1,31 +1,44 @@
 import { activeProfileId } from '@core/profile/stores/active-profile-id.store'
 import {
-    ADDRESS_TYPE_NFT,
     getBech32AddressFromAddressTypes,
     getIssuerFromNftOutput,
     getMetadataFromNftOutput,
     getNftId,
-} from '@core/wallet'
-import { INftOutput } from '@iota/types'
+} from '@core/wallet/utils'
+import { IWrappedOutput } from '@core/wallet/interfaces'
+import type { INftOutput } from '@iota/types'
 import { get } from 'svelte/store'
 import { DEFAULT_NFT_NAME } from '../constants'
 import { INft } from '../interfaces'
 import { parseNftMetadata } from './parseNftMetadata'
 import { composeUrlFromNftUri } from './composeUrlFromNftUri'
+import { getSpendableStatusFromUnspentNftOutput } from './getSpendableStatusFromUnspentNftOutput'
+import { ADDRESS_TYPE_NFT } from '@core/wallet/constants'
 
 export function buildNftFromNftOutput(
-    nftOutput: INftOutput,
-    outputId: string,
-    isSpendable: boolean,
-    timelockTime?: string
+    wrappedOutput: IWrappedOutput,
+    accountAddress: string,
+    calculateStatus: boolean = true
 ): INft {
-    const id = getNftId(nftOutput.nftId, outputId)
+    const nftOutput = wrappedOutput.output as INftOutput
+
+    let isSpendable = false
+    let timeLockTime = undefined
+
+    if (calculateStatus) {
+        const status = getSpendableStatusFromUnspentNftOutput(accountAddress, nftOutput)
+        isSpendable = status.isSpendable
+        timeLockTime = status.timeLockTime
+    }
+
+    const id = getNftId(nftOutput.nftId, wrappedOutput.outputId)
     const address = getBech32AddressFromAddressTypes({ type: ADDRESS_TYPE_NFT, nftId: id })
     const issuer = getIssuerFromNftOutput(nftOutput)
     const metadata = getMetadataFromNftOutput(nftOutput)
     const parsedMetadata = parseNftMetadata(metadata)
     const composedUrl = composeUrlFromNftUri(parsedMetadata?.uri)
     const filePath = `${get(activeProfileId)}/nfts/${id}`
+    const storageDeposit = Number(nftOutput.amount)
 
     return {
         id,
@@ -33,13 +46,14 @@ export function buildNftFromNftOutput(
         name: parsedMetadata?.name ?? DEFAULT_NFT_NAME,
         issuer,
         isSpendable,
-        timelockTime: timelockTime ? Number(timelockTime) : undefined,
+        timelockTime: timeLockTime ? Number(timeLockTime) : undefined,
         metadata,
         parsedMetadata,
-        latestOutputId: outputId,
+        latestOutputId: wrappedOutput.outputId,
         composedUrl,
         downloadUrl: composedUrl,
         filePath,
+        storageDeposit,
         downloadMetadata: {
             error: undefined,
             warning: undefined,
