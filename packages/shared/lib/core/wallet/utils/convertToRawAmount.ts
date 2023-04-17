5a5debb37a93f52f77cdce5a5880c6f386e153e5
Nicole O'Brien <nicole.obrien@iota.org>
2023-04-17 15:06:05 +0100

    refactor: clean up convertToRawAmountFromMetadata

--- packages/shared/lib/core/wallet/utils/convertToRawAmount.ts
+++ packages/shared/lib/core/wallet/utils/convertToRawAmount.ts
@@ -16,16 +16,22 @@ export function convertToRawAmount(amount: string, tokenMetadata: TokenMetadata,
     }
 }
 
-function convertToRawAmountFromMetadata(amount: number, tokenMetadata: TokenMetadata, unit: string): Big {
-    const isBaseToken = tokenMetadata?.standard === TokenStandard.BaseToken
-    if (isBaseToken && tokenMetadata.useMetricPrefix) {
-        return Big(amount * IOTA_UNIT_MAP?.[unit?.substring(0, 1)] ?? 0)
-    } else {
-        if (unit && isBaseToken && unit === tokenMetadata.unit) {
-            const decimals = Math.min(tokenMetadata.decimals, MAX_SUPPORTED_DECIMALS)
-            return Big(amount).mul(Big(10).pow(decimals))
-        } else if (isBaseToken && unit === tokenMetadata.subunit) {
-            return Big(amount)
+function convertToRawAmountFromMetadata(amount: number, tokenMetadata: TokenMetadata, selectedUnit: string): Big {
+    if (tokenMetadata?.standard === TokenStandard.BaseToken) {
+        if (tokenMetadata.useMetricPrefix) {
+            return Big(amount * IOTA_UNIT_MAP?.[selectedUnit?.substring(0, 1)] ?? 0)
+        } else {
+            if (selectedUnit === tokenMetadata.unit) {
+                const decimals = Math.min(tokenMetadata.decimals, MAX_SUPPORTED_DECIMALS)
+                return Big(amount).mul(Big(10).pow(decimals))
+            } else if (selectedUnit === tokenMetadata.subunit) {
+                return Big(amount)
+            }
         }
+    } else if (tokenMetadata?.standard === TokenStandard.Irc30) {
+        const decimals = Math.min(tokenMetadata.decimals, MAX_SUPPORTED_DECIMALS)
+        return Big(amount).mul(Big(10).pow(decimals))
+    } else {
+        throw new Error('convertToRawAmountFromMetadata: Invalid token standard')
     }
 }
