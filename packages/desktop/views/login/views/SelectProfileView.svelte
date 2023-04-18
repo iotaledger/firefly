0c7e0642ec9edfbbed81100fb50efb2ed1dd0cee
Nicole O'Brien <nicole.obrien@iota.org>
2023-04-18 23:00:42 +0100

    fix: initialiseOnboardingFlow call

--- packages/desktop/views/login/views/SelectProfileView.svelte
+++ packages/desktop/views/login/views/SelectProfileView.svelte
@@ -24,7 +24,7 @@
         const isDeveloperProfile = shouldBeDeveloperProfile()
         await initialiseOnboardingFlow({
             isDeveloperProfile,
-            ...(!isDeveloperProfile && { networkId: NetworkId.Testnet }),
+            ...(!isDeveloperProfile && { networkId: NetworkId.Shimmer }),
         })
         $routerManager.goToAppContext(AppContext.Onboarding)
         $onboardingRouter.goTo(isDeveloperProfile ? OnboardingRoute.NetworkSetup : OnboardingRoute.ProfileSetup)
