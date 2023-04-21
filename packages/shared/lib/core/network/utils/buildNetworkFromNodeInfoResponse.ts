ea7bd982bdb033132c31aab7ccc2f301b4777aa7
Nicole O'Brien <nicole.obrien@iota.org>
2023-04-21 09:29:18 +0100

    enhancement: correct coin type for testnet and custom coin type (#6613)
    * refactor: use network id in COIN_TYPE
    
    * enhancement: add coinType to network in onboarding
    
    * refactor: add coin type in profile migration
    
    * refactor: set correct coin type for Testnet
    
    * refactor: use network.coinType in profileManagerOptions
    
    * fix: getCoinType to use network.coinType
    
    * fix: custom network to use custom coin type
    
    * enhancement: add validation for coin type
    
    * refactor: use coinType from onboarding in claim flow
    
    * fix: set default network based on features
    
    * fix: shimmer claiming
    
    * fix: migration
    
    ---------
    
    Co-authored-by: Begoña Alvarez <balvarez@boxfish.studio>

--- packages/shared/lib/core/network/utils/buildNetworkFromNodeInfoResponse.ts
+++ packages/shared/lib/core/network/utils/buildNetworkFromNodeInfoResponse.ts
@@ -1,14 +1,16 @@
-import { NETWORK } from '../constants'
+import { COIN_TYPE, NETWORK } from '../constants'
 import { NetworkId } from '../enums'
 import { INetwork, INodeInfoResponse } from '../interfaces'
 import { getNetworkIdFromNetworkName } from './getNetworkIdFromNetworkName'
 
-export function buildNetworkFromNodeInfoResponse(nodeInfoResponse: INodeInfoResponse): INetwork {
+export function buildNetworkFromNodeInfoResponse(nodeInfoResponse: INodeInfoResponse, coinType?: number): INetwork {
     const networkName = nodeInfoResponse?.nodeInfo?.protocol?.networkName
     const networkId = getNetworkIdFromNetworkName(networkName)
+    const _coinType = coinType ?? COIN_TYPE[networkId] ?? 1
     return {
         id: networkId,
         name: networkId === NetworkId.Custom ? networkName : NETWORK?.[networkId]?.name,
+        coinType: _coinType,
         protocol: nodeInfoResponse?.nodeInfo?.protocol,
         baseToken: nodeInfoResponse?.nodeInfo?.baseToken,
         chains: [],
