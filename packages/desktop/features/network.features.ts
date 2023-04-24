ef71202c86da212cbf839aeb51cbb01e18d83314
Matthew Maxwell <maxwellmattryan@gmail.com>
2023-04-24 10:16:45 -0500

    chore: add flag

--- packages/desktop/features/network.features.ts
+++ packages/desktop/features/network.features.ts
@@ -6,7 +6,10 @@ const networkFeatures: INetworkFeatures = {
         enabled: true,
     },
     config: {
-        enabled: false,
+        enabled: true,
+        addChain: {
+            enabled: false,
+        },
     },
 }
 
