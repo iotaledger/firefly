bfd48bf92907b24d390480ab31973fb167dbb305
Jean Ribeiro <contact@jeanribeiro.dev>
2023-04-27 10:54:53 -0300

    fix: removes unnecessary crasis from error regexes

--- packages/shared/lib/core/error/constants/error-regex.constant.ts
+++ packages/shared/lib/core/error/constants/error-regex.constant.ts
@@ -1,10 +1,10 @@
 import { ClientError, IotaClientError } from '../enums'
 
 export const CLIENT_ERROR_REGEXES = {
-    [ClientError.InvalidStrongholdPassword]: /`invalid stronghold password`/,
-    [ClientError.NoSyncedNode]: /`No synced node available`/,
-    [ClientError.InsufficientAmount]: /`insufficient amount: found \d*, required \d*`/,
-    [ClientError.TimeNotSynced]: /`local time \d* doesn't match the time of the latest milestone timestamp: \d*`/,
+    [ClientError.InvalidStrongholdPassword]: /invalid stronghold password/,
+    [ClientError.NoSyncedNode]: /No synced node available/,
+    [ClientError.InsufficientAmount]: /insufficient amount: found \d*, required \d*/,
+    [ClientError.TimeNotSynced]: /local time \d* doesn't match the time of the latest milestone timestamp: \d*/,
     [IotaClientError.NoInputs]: /no inputs found/,
     [IotaClientError.NotEnoughBalance]:
         /the wallet account doesn't have enough balance. It only has \d*, required is \d*/,
