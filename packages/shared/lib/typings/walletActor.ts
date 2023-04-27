3851155dcdba2571f2b99c1f993060fb2b2d543f
Thibault Martinez <thibault.martinez.30@gmail.com>
2023-04-28 00:18:04 +0200

    feat: Stronghold version migration (#6630)
    * Use wallet.rs-private
    
    * Add nodejs migrate_stronghold_snapshot_v2_to_v3
    
    * chore: expose method to TS side
    
    * chore: remove test code
    
    * chore: add mock for tests
    
    ---------
    
    Co-authored-by: Matthew Maxwell <maxwellmattryan@gmail.com>
    Co-authored-by: Matthew Maxwell <44885822+maxwellmattryan@users.noreply.github.com>

--- packages/shared/lib/typings/walletActor.ts
+++ packages/shared/lib/typings/walletActor.ts
@@ -8,4 +8,11 @@ export interface IWalletActor {
     init(id: string, storagePath: string, sendCrashReports: boolean, machineId: string): IActorHandler
     onMessage(callback: (payload: unknown) => void): void
     initLogger(config: LoggerConfig): void
+
+    migrateStrongholdSnapshotV2ToV3(
+        currentPath: string,
+        currentPassword: string,
+        newPath: string,
+        newPassword: string
+    ): void
 }
