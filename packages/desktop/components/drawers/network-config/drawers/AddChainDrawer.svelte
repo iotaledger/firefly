d4367d4502ff8b86db131f90200edf29d62aa86f
Mark Nardi <mark.nardi@iota.org>
2023-04-27 08:41:49 +0200

    enhancement: add chain validation

--- packages/desktop/components/drawers/network-config/drawers/AddChainDrawer.svelte
+++ packages/desktop/components/drawers/network-config/drawers/AddChainDrawer.svelte
@@ -2,10 +2,16 @@
     import { localize } from '@core/i18n'
     import { IIscpChainMetadata } from '@core/network'
     import { ChainType } from '@core/network/enums'
+    import { isValidUrl } from '@core/utils'
     import { Button, HTMLButtonType, Input } from '@ui'
 
     const isBusy = false
+    const localeKey = 'views.dashboard.drawers.networkConfig.addChain'
 
+    let nameError = ''
+    let aliasAddressError = ''
+    let iscpEndpointError = ''
+    let explorerUrlError = ''
     $: submitDisabled = !chain.name || !chain.aliasAddress || !chain.iscpEndpoint
 
     const chain: IIscpChainMetadata = {
@@ -17,28 +23,53 @@
         iscpEndpoint: '',
     }
 
+    function validate(): boolean {
+        if (!chain.name) {
+            nameError = localize(`${localeKey}.errors.cannotBeEmpty`)
+        }
+
+        const isHex = chain.aliasAddress.startsWith('0x')
+        if (!isHex || !/^(0x08)?[0-9a-f]{64}?(?:0[1-9]|[1-5][0-9]|6[0-4])?0{8}$/i.test(chain.aliasAddress)) {
+            aliasAddressError = localize(`${localeKey}.errors.aliasAddressMustBeHex`)
+        }
+
+        if (!isValidUrl(chain.iscpEndpoint)) {
+            iscpEndpointError = localize(`${localeKey}.errors.invalidUrl`)
+        }
+
+        if (chain.explorerUrl && !isValidUrl(chain.explorerUrl)) {
+            explorerUrlError = localize(`${localeKey}.errors.invalidUrl`)
+        }
+
+        return !!nameError || !!aliasAddressError || !!iscpEndpointError || !!explorerUrlError
+    }
+
     function handleSubmit(): void {
+        validate()
         // TODO: Fetch chainId from ISCP node before adding it to profile
     }
 </script>
 
 <add-chain-drawer class="h-full flex flex-col justify-between">
     <form id="add-chain-form" class="flex flex-col gap-3" on:submit|preventDefault={handleSubmit}>
-        <Input bind:value={chain.name} placeholder={localize('general.name')} disabled={isBusy} />
+        <Input bind:value={chain.name} placeholder={localize('general.name')} disabled={isBusy} error={nameError} />
         <Input
             bind:value={chain.aliasAddress}
-            placeholder={localize('views.dashboard.drawers.networkConfig.addChain.aliasAddress')}
+            placeholder={localize(`${localeKey}.aliasAddress`)}
             disabled={isBusy}
+            error={aliasAddressError}
         />
         <Input
             bind:value={chain.iscpEndpoint}
-            placeholder={localize('views.dashboard.drawers.networkConfig.addChain.iscpEndpoint')}
+            placeholder={localize(`${localeKey}.iscpEndpoint`)}
             disabled={isBusy}
+            error={iscpEndpointError}
         />
         <Input
             bind:value={chain.explorerUrl}
-            placeholder={localize('views.dashboard.drawers.networkConfig.addChain.explorerEndpoint')}
+            placeholder={localize(`${localeKey}.explorerEndpoint`)}
             disabled={isBusy}
+            error={explorerUrlError}
         />
     </form>
     <Button
@@ -48,6 +79,6 @@
         disabled={submitDisabled || isBusy}
         {isBusy}
     >
-        {localize('views.dashboard.drawers.networkConfig.addChain.title')}
+        {localize(`${localeKey}.title`)}
     </Button>
 </add-chain-drawer>
