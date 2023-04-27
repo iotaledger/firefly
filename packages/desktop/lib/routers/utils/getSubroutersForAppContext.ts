e4326fb6891403c9fec25b63296952a7bf22abc8
Nicole O'Brien <nicole.obrien@iota.org>
2023-04-27 17:43:28 +0100

    fix: check types

--- packages/desktop/lib/routers/utils/getSubroutersForAppContext.ts
+++ packages/desktop/lib/routers/utils/getSubroutersForAppContext.ts
@@ -1,16 +1,6 @@
 import { AppContext } from '@core/app/enums'
 import { IRouter } from '@core/router/interfaces'
-import {
-    ledgerSetupRouter,
-    networkSetupRouter,
-    profileBackupRouter,
-    profileRecoveryRouter,
-    profileSetupRouter,
-    shimmerClaimingRouter,
-    storageProtectionSetupRouter,
-    strongholdSetupRouter,
-    updateStrongholdRouter,
-} from '@core/router/subrouters'
+import { updateStrongholdRouter } from '@core/router/subrouters'
 import { get } from 'svelte/store'
 
 export function getSubroutersForAppContext(context: AppContext): IRouter[] {
@@ -20,16 +10,7 @@ export function getSubroutersForAppContext(context: AppContext): IRouter[] {
         case AppContext.Login:
             return [get(updateStrongholdRouter)]
         case AppContext.Onboarding:
-            return [
-                get(ledgerSetupRouter),
-                get(networkSetupRouter),
-                get(strongholdSetupRouter),
-                get(profileBackupRouter),
-                get(profileRecoveryRouter),
-                get(profileSetupRouter),
-                get(storageProtectionSetupRouter),
-                get(shimmerClaimingRouter),
-            ]
+            return []
         case AppContext.Settings:
             return []
         default:
