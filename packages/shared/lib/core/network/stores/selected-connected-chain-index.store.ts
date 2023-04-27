df4bb6329aa86a42ff29860d3d6d529c03491915
Mark Nardi <mark.nardi@iota.org>
2023-04-27 08:35:08 +0200

    feature: update chain deposit address drawer

--- packages/shared/lib/core/network/stores/selected-connected-chain-index.store.ts
+++ packages/shared/lib/core/network/stores/selected-connected-chain-index.store.ts
@@ -1,3 +1,24 @@
-import { Writable, writable } from 'svelte/store'
+import { activeProfile } from '@core/profile'
+import { Readable, Writable, derived, writable } from 'svelte/store'
+import { buildChainFromNetwork } from '../utils'
+import { ConnectedChain } from '../interfaces'
+import { NetworkHealth } from '../enums'
 
 export const selectedConnectedChainIndex: Writable<number> = writable(undefined)
+
+export const selectedConnectedChain: Readable<ConnectedChain> = derived(
+    [activeProfile, selectedConnectedChainIndex],
+    ([$activeProfile, $selectedConnectedChainIndex]) => {
+        if ($selectedConnectedChainIndex === 0) {
+            return buildChainFromNetwork()
+        } else {
+            const index = $selectedConnectedChainIndex - 1
+            const chain = $activeProfile.network.chains[index]
+            return {
+                name: chain.name,
+                address: chain.name, // TODO
+                status: NetworkHealth.Operational, // TODO
+            }
+        }
+    }
+)
