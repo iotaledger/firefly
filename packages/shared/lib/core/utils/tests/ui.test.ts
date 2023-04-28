d735483d415c71b03e4a1192a5c201a249f314fb
Jason Kraft <developer.kraft@gmail.com>
2023-04-28 15:04:12 -0400

    Add tests for hex2rgb function

--- packages/shared/lib/core/utils/tests/ui.test.ts
+++ packages/shared/lib/core/utils/tests/ui.test.ts
@@ -1,4 +1,4 @@
-import { debounce } from '@core/utils'
+import { debounce, hex2rgb } from '@core/utils'
 
 describe('File: ui.ts', () => {
     describe('Function: debounce', () => {
@@ -27,6 +27,17 @@ describe('File: ui.ts', () => {
             debounced()
             jest.advanceTimersByTime(100)
             expect(callback).toHaveBeenCalledTimes(2)
+        })
+    })
+    describe('Function: hex2rgb', () => {
+        it('should convert hex to rgb', () => {
+            expect(hex2rgb('#000000')).toEqual('0,0,0')
+        })
+        it('should return RGB values for white if input string has length less than 7', () => {
+            expect(hex2rgb('')).toEqual('255,255,255')
+        })
+        it('should return RGB values of initial 7 chars if input string has length greater than 7', () => {
+            expect(hex2rgb('#0F0F0F9')).toEqual('15,15,15')
         })
     })
 })
