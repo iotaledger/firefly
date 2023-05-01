025b0c0f5c62f702ba377b64a26441d397434367
MarkNerdi <105642810+MarkNerdi@users.noreply.github.com>
2023-05-01 15:49:25 +0200

    chore: replace deepcopy with structuredClone (#6672)
    Co-authored-by: Jean Ribeiro <iamjeanribeiro@gmail.com>

--- packages/shared/lib/core/utils/object.ts
+++ packages/shared/lib/core/utils/object.ts
@@ -5,7 +5,3 @@ export function resolveObjectPath(object: Record<string, unknown>, path: string,
 
     return path.split('.').reduce((o, p) => o && o[p], object) ?? defaultValue
 }
-
-export function deepCopy<T>(object: T): T {
-    return JSON.parse(JSON.stringify(object)) as T
-}
