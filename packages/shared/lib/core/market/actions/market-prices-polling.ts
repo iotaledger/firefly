6e63f359bbb89283f01e0864f25cb25d0a42c660
Matthew Maxwell <maxwellmattryan@gmail.com>
2023-05-01 16:41:05 -0500

    chore: use window setInterval method

--- packages/shared/lib/core/market/actions/market-prices-polling.ts
+++ packages/shared/lib/core/market/actions/market-prices-polling.ts
@@ -1,14 +1,14 @@
 import { MARKET_POLL_INTERVAL } from '../constants'
 import { getAndUpdateMarketPrices } from './getAndUpdateMarketPrices'
 
-let pollInterval: ReturnType<typeof setInterval>
+let pollInterval: number
 
 /**
  * Poll the network status at an interval.
  */
 export async function pollMarketPrices(): Promise<void> {
     await getAndUpdateMarketPrices()
-    pollInterval = setInterval(() => void getAndUpdateMarketPrices(), MARKET_POLL_INTERVAL)
+    pollInterval = window.setInterval(() => void getAndUpdateMarketPrices(), MARKET_POLL_INTERVAL)
 }
 
 export function clearMarketPricesPoll(): void {
--- packages/shared/lib/core/network/actions/chain-polling.ts
+++ packages/shared/lib/core/network/actions/chain-polling.ts
@@ -1,11 +1,11 @@
 import { CHAIN_STATUSES_POLL_INTERVAL } from '../constants'
 import { updateChainStatuses } from './updateChainStatuses'
 
-let pollInterval: ReturnType<typeof setInterval>
+let pollInterval: number
 
 export async function pollChainStatuses(): Promise<void> {
     await updateChainStatuses()
-    pollInterval = setInterval(() => void updateChainStatuses(), CHAIN_STATUSES_POLL_INTERVAL)
+    pollInterval = window.setInterval(() => void updateChainStatuses(), CHAIN_STATUSES_POLL_INTERVAL)
 }
 
 export function clearChainStatusesPoll(): void {
