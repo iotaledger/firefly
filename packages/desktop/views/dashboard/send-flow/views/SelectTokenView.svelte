8bca9890f9f7797495441749e3bed2789bc663a1
Mark Nardi <mark.nardi@iota.org>
2023-05-02 17:34:40 +0200

    enhancement: add search

--- packages/desktop/views/dashboard/send-flow/views/SelectTokenView.svelte
+++ packages/desktop/views/dashboard/send-flow/views/SelectTokenView.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-    import { AssetTile, Button, FontWeight, Text, TextType } from '@ui'
+    import { AssetTile, Button, FontWeight, Icon, Text, TextInput, TextType } from '@ui'
     import { localize } from '@core/i18n'
     import { marketCoinPrices } from '@core/market'
     import {
@@ -13,22 +13,27 @@
     import { sendFlowRouter } from '../send-flow-router'
     import { closePopup } from '@auxiliary/popup'
     import { get } from 'svelte/store'
+    import { Icon as IconEnum } from '@auxiliary/icon'
 
     const transactionDetails = get(newTransactionDetails)
 
     let selectedAssetId: string =
         transactionDetails?.type === NewTransactionType.TokenTransfer ? transactionDetails.assetId : undefined
     let assetList: IAsset[]
+    let searchValue: string = ''
 
     let assets: IAccountAssets
     $: assets = getAccountAssetsForSelectedAccount($marketCoinPrices)
-    $: assets, setFilteredAssetList()
+    $: assets, searchValue, setFilteredAssetList()
 
     function setFilteredAssetList(): void {
         if (!assets) {
             assetList = []
         } else {
-            assetList = [assets.baseCoin, ...assets.nativeTokens]
+            assetList = [assets.baseCoin, ...assets.nativeTokens].filter(
+                (asset) =>
+                    asset?.metadata?.name && asset.metadata.name.toLowerCase().includes(searchValue.toLowerCase())
+            )
         }
     }
 
@@ -54,7 +59,18 @@
     <Text type={TextType.h3} fontWeight={FontWeight.semibold} classes="text-left">
         {localize('popups.transaction.selectToken')}
     </Text>
-    <select-token-buttons class="flex flex-row flex-nowrap w-full space-x-4">
+    <select-token-buttons class="w-full space-y-4">
+        <TextInput
+            bind:value={searchValue}
+            placeholder={localize('general.search')}
+            fontSize="15"
+            clearPadding
+            containerClasses="py-2 px-3"
+            fontWeight={FontWeight.medium}
+            color="gray-500"
+        >
+            <Icon slot="left" icon={IconEnum.Search} classes="text-gray-500 dark:text-white mr-2" />
+        </TextInput>
         <div class="asset-list w-full flex flex-col -mr-1 pr-1.5 gap-2">
             {#each assetList as asset}
                 <AssetTile
