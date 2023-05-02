931994d83893c1e373845466ec6f7643c95d1fea
Nicole O'Brien <nicole.obrien@iota.org>
2023-05-02 10:34:43 +0100

    fix: changes to actions that would have broke mobile

--- packages/shared/lib/contexts/onboarding/actions/restoreBackupFromStrongholdFile.ts
+++ packages/shared/lib/contexts/onboarding/actions/restoreBackupFromStrongholdFile.ts
@@ -1,13 +1,18 @@
 import { getStorageDirectoryOfProfile } from '@core/profile'
 import { profileManager, restoreBackup } from '@core/profile-manager'
 import { get } from 'svelte/store'
-import { restoreBackupByCopyingFile } from '../helpers'
-import { onboardingProfile } from '../stores'
+import { restoreBackupByCopyingFile, validateStrongholdCoinType } from '../helpers'
+import { onboardingProfile, updateOnboardingProfile } from '../stores'
+import { mobile } from '@core/app'
 
 export async function restoreBackupFromStrongholdFile(strongholdPassword: string): Promise<void> {
-    const { id, importFilePath, clientOptions } = get(onboardingProfile)
+    const { id, importFilePath, clientOptions, network } = get(onboardingProfile)
     try {
         await restoreBackup(importFilePath, strongholdPassword)
+        if (mobile) {
+            await validateStrongholdCoinType(profileManager, network?.id)
+            updateOnboardingProfile({ lastStrongholdBackupTime: new Date() })
+        }
     } catch (err) {
         const storageDirectory = await getStorageDirectoryOfProfile(id)
         await restoreBackupByCopyingFile(
---
+++ packages/shared/lib/contexts/onboarding/helpers/validateStrongholdCoinType.ts
@@ -0,0 +1,24 @@
+import { localize } from '@core/i18n'
+import { COIN_TYPE, NetworkId } from '@core/network'
+import { createAccount, getAccounts, IProfileManager } from '@core/profile-manager'
+import { get, Writable } from 'svelte/store'
+import { OnboardingType } from '../enums'
+import { CannotRestoreWithMismatchedCoinTypeError } from '../errors'
+import { onboardingProfile } from '../stores'
+
+export async function validateStrongholdCoinType(
+    profileManager: Writable<IProfileManager>,
+    networkId: NetworkId
+): Promise<void> {
+    const accounts = await getAccounts(profileManager)
+    if (accounts?.length === 0) {
+        const alias = `${localize('general.account')} 1`
+        const account = await createAccount({ alias }, profileManager)
+        accounts.push(account)
+    }
+
+    if (accounts[0]?.getMetadata()?.coinType !== COIN_TYPE[networkId]) {
+        const isClaiming = get(onboardingProfile)?.onboardingType === OnboardingType.Claim
+        throw new CannotRestoreWithMismatchedCoinTypeError(isClaiming)
+    }
+}
