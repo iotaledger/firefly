71eba477847e94dd82ad3aae4bcb00d31c1e6968
Tuditi <daviddetroch@pm.me>
2023-05-04 12:05:46 +0200

    chore: use enums

--- packages/shared/lib/core/network/constants/default-chain-configurations.constant.ts
+++ packages/shared/lib/core/network/constants/default-chain-configurations.constant.ts
@@ -1,17 +1,18 @@
+import { DestinationNetwork } from '@core/layer-2'
 import { ChainType, NetworkId } from '../enums'
 import { ChainConfiguration } from '../types'
 
 export const DEFAULT_CHAIN_CONFIGURATIONS: Readonly<{ [id in NetworkId]?: ChainConfiguration }> = {
     [NetworkId.Shimmer]: {
         type: ChainType.Iscp,
-        name: 'ShimmerEVM',
+        name: DestinationNetwork.Shimmer,
         chainId: 1071,
         aliasAddress: '', // 'rms1prwgvvw472spqusqeufvlmp8xdpyxtrnmvt26jnuk6sxdcq2hk8scku26h7',
         iscpEndpoint: '', // 'https://json-rpc.evm.testnet.shimmer.network',
     },
     [NetworkId.Testnet]: {
         type: ChainType.Iscp,
-        name: 'ShimmerEVM',
+        name: DestinationNetwork.ShimmerEvmTestnet,
         chainId: 1071,
         aliasAddress: 'rms1prwgvvw472spqusqeufvlmp8xdpyxtrnmvt26jnuk6sxdcq2hk8scku26h7',
         iscpEndpoint: 'https://json-rpc.evm.testnet.shimmer.network',
