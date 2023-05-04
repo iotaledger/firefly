ae6ea1d1fea5160cc7df13f4b0e4af85d6c14333
Nicole O'Brien <nicole.obrien@iota.org>
2023-05-04 14:19:51 +0100

    fix: checkAndUpdateActiveProfileNetwork

--- packages/shared/lib/core/profile/actions/active-profile/checkAndUpdateActiveProfileNetwork.ts
+++ packages/shared/lib/core/profile/actions/active-profile/checkAndUpdateActiveProfileNetwork.ts
@@ -1,20 +1,17 @@
-import { NetworkId, buildPersistedNetworkFromNodeInfoResponse, getNetworkIdFromNetworkName } from '@core/network'
+import { NetworkId, buildPersistedNetworkFromNodeInfoResponse } from '@core/network'
 import { getAndUpdateNodeInfo } from '@core/network/actions'
 import { activeProfile, updateActiveProfile } from '@core/profile/stores'
 import { get } from 'svelte/store'
 
 export async function checkAndUpdateActiveProfileNetwork(): Promise<void> {
     const $activeProfile = get(activeProfile)
+    const existingNetworkId = $activeProfile?.network?.id
     const nodeInfoResponse = await getAndUpdateNodeInfo(true)
-    const networkId = $activeProfile?.network?.id
-    if (
-        networkId !== NetworkId.Custom &&
-        networkId !== getNetworkIdFromNetworkName(nodeInfoResponse?.nodeInfo?.protocol?.networkName)
-    ) {
-        throw new Error('error.node.networkIdMismatch')
-    } else {
-        const network = buildPersistedNetworkFromNodeInfoResponse(nodeInfoResponse)
+    const network = buildPersistedNetworkFromNodeInfoResponse(nodeInfoResponse)
+    if (existingNetworkId === NetworkId.Custom || existingNetworkId === network.id) {
         network.chainConfigurations = $activeProfile.network?.chainConfigurations || []
         updateActiveProfile({ network })
+    } else {
+        throw new Error('error.node.networkIdMismatch')
     }
 }
