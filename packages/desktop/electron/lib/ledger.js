1780a2177c2a8cbdb461a5ecd64e201abee9e3fd
Tuditi <daviddetroch@pm.me>
2023-05-05 10:58:42 +0200

    --wip-- [skip ci]

--- packages/desktop/electron/lib/ledger.js
+++ packages/desktop/electron/lib/ledger.js
@@ -1,6 +1,6 @@
-// const TransportNodeHid = require('@ledgerhq/hw-transport-node-hid').default
-// const AppEth = require('@ledgerhq/hw-app-eth').default
-// const { listen } = require('@ledgerhq/logs');
+const TransportNodeHid = require('@ledgerhq/hw-transport-node-hid').default
+const AppEth = require('@ledgerhq/hw-app-eth').default
+const { listen } = require('@ledgerhq/logs');
 
 // This a very basic example
 // Ideally you should not run this code in main thread
@@ -8,22 +8,22 @@
 export function getEthereumInfo(verify) {
     /* eslint-disable no-console */
     console.log('VERIFY: ', verify)
-    // return TransportNodeHid.open('')
-    // .then(transport => {
-    //     listen(log => console.log(log))
-    //     const appEth = new AppEth(transport);
-    //     return appEth.getAddress('44\'/60\'/0\'/0/0').then(r =>
-    //         transport
-    //             .close()
-    //             .catch(e => {})
-    //             .then(() => r)
-    //         );
-    // })
-    // .catch(e => {
-    //     console.warn(e);
-    //     // try again until success!
-    //     return new Promise(s => setTimeout(s, 1000)).then(() =>
-    //         getEthereumInfo(verify)
-    //     );
-    // });
+    return TransportNodeHid.open('')
+    .then(transport => {
+        listen(log => console.log(log))
+        const appEth = new AppEth(transport);
+        return appEth.getAddress('44\'/60\'/0\'/0/0').then(r =>
+            transport
+                .close()
+                .catch(e => {})
+                .then(() => r)
+            );
+    })
+    .catch(e => {
+        console.warn(e);
+        // try again until success!
+        return new Promise(s => setTimeout(s, 1000)).then(() =>
+            getEthereumInfo(verify)
+        );
+    });
 }
