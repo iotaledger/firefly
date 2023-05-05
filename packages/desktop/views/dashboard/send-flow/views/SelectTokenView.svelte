ff7db0e485cd20d369cbf883b198e19883f5697d
Mark Nardi <mark.nardi@iota.org>
2023-05-05 14:24:51 +0200

    refactor: refactor send flow component

--- packages/desktop/views/dashboard/send-flow/views/SelectTokenView.svelte
+++ packages/desktop/views/dashboard/send-flow/views/SelectTokenView.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-    import { AssetTile, Button, FontWeight, Icon, Text, TextInput, TextType } from '@ui'
+    import { AssetTile, FontWeight, Icon, TextInput } from '@ui'
     import { localize } from '@core/i18n'
     import { marketCoinPrices } from '@core/market'
     import {
@@ -15,6 +15,7 @@
     import { get } from 'svelte/store'
     import { Icon as IconEnum } from '@auxiliary/icon'
     import { sendFlowRouter } from '../send-flow.router'
+    import SendFlowTemplate from './SendFlowTemplate.svelte'
 
     const transactionDetails = get(newTransactionDetails)
 
@@ -68,45 +69,33 @@
     }
 </script>
 
-<select-asset-popup class="w-full h-full space-y-6 flex flex-auto flex-col flex-shrink-0">
-    <Text type={TextType.h3} fontWeight={FontWeight.semibold} classes="text-left">
-        {localize('popups.transaction.selectToken')}
-    </Text>
-    <select-token-buttons class="w-full space-y-4">
-        <TextInput
-            bind:value={searchValue}
-            placeholder={localize('general.search')}
-            fontSize="15"
-            clearPadding
-            containerClasses="py-2 px-3"
-            fontWeight={FontWeight.medium}
-            color="gray-500"
-        >
-            <Icon slot="left" icon={IconEnum.Search} classes="text-gray-500 dark:text-white mr-2" />
-        </TextInput>
-        <div class="-mr-3">
-            <div class="asset-list w-full flex flex-col -mr-1 pr-1.5 gap-2">
-                {#each assetList as asset}
-                    <AssetTile
-                        {asset}
-                        onClick={() => (selectedAssetId = asset.id)}
-                        classes="border-2 border-solid {selectedAssetId === asset.id
-                            ? 'border-blue-500 dark:border-gray-500'
-                            : 'border-transparent'}"
-                    />
-                {/each}
-            </div>
+<SendFlowTemplate
+    title={localize('popups.transaction.selectToken')}
+    leftButton={{ text: localize('actions.cancel'), onClick: onCancelClick }}
+    rightButton={{ text: localize('actions.continue'), onClick: onContinueClick, disabled: !selectedAssetId }}
+>
+    <TextInput
+        bind:value={searchValue}
+        placeholder={localize('general.search')}
+        fontWeight={FontWeight.medium}
+        color="gray-500"
+    >
+        <Icon slot="left" icon={IconEnum.Search} classes="text-gray-500 dark:text-white mr-2" />
+    </TextInput>
+    <div class="-mr-3">
+        <div class="asset-list w-full flex flex-col -mr-1 pr-1.5 gap-2">
+            {#each assetList as asset}
+                <AssetTile
+                    {asset}
+                    onClick={() => (selectedAssetId = asset.id)}
+                    classes="border-2 border-solid {selectedAssetId === asset.id
+                        ? 'border-blue-500 dark:border-gray-500'
+                        : 'border-transparent'}"
+                />
+            {/each}
         </div>
-    </select-token-buttons>
-    <popup-buttons class="flex flex-row flex-nowrap w-full space-x-4">
-        <Button classes="w-full" outline onClick={onCancelClick}>
-            {localize('actions.cancel')}
-        </Button>
-        <Button classes="w-full" onClick={onContinueClick} disabled={!selectedAssetId}>
-            {localize('actions.continue')}
-        </Button>
-    </popup-buttons>
-</select-asset-popup>
+    </div>
+</SendFlowTemplate>
 
 <style lang="scss">
     .asset-list {
