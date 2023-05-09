2f93d245162fb470c9801e373435dae9293aa45f
Tuditi <daviddetroch@pm.me>
2023-05-09 10:41:11 +0200

    fix: use correct enum

--- packages/shared/lib/core/network/constants/default-chain-configurations.constant.ts
+++ packages/shared/lib/core/network/constants/default-chain-configurations.constant.ts
@@ -5,7 +5,7 @@ import { ChainConfiguration } from '../types'
 export const DEFAULT_CHAIN_CONFIGURATIONS: Readonly<{ [id in NetworkId]?: ChainConfiguration }> = {
     [NetworkId.Shimmer]: {
         type: ChainType.Iscp,
-        name: DestinationNetwork.Shimmer,
+        name: DestinationNetwork.ShimmerEvm,
         chainId: 1071,
         aliasAddress: '', // 'rms1prwgvvw472spqusqeufvlmp8xdpyxtrnmvt26jnuk6sxdcq2hk8scku26h7',
         iscpEndpoint: '', // 'https://json-rpc.evm.testnet.shimmer.network',
