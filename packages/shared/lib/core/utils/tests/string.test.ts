417df841bf2672b1a53e16be2ee42f3ae55bc310
Mark Nardi <mark.nardi@iota.org>
2023-05-10 08:31:21 +0200

    test: add tests for truncate string

--- packages/shared/lib/core/utils/tests/string.test.ts
+++ packages/shared/lib/core/utils/tests/string.test.ts
@@ -1,4 +1,4 @@
-import { isStringTrue, getByteLengthOfString, stripTrailingSlash, stripSpaces } from '@core/utils'
+import { isStringTrue, getByteLengthOfString, stripTrailingSlash, stripSpaces, truncateString } from '@core/utils'
 
 describe('File: string.ts', () => {
     describe('Function: isStringTrue', () => {
@@ -46,6 +46,30 @@ describe('File: string.ts', () => {
         })
         it('should do nothing if no spaces exist', () => {
             expect(stripSpaces('test')).toBe('test')
+        })
+    })
+
+    describe('Function: truncateString', () => {
+        it('should truncate the string correclty if "start" and "end" are greater than 0', () => {
+            expect(truncateString('Lorem ipsum', 3, 3)).toBe('Lor...sum')
+        })
+        it('should show no letters at the end if "end" is 0', () => {
+            expect(truncateString('Lorem ipsum', 0, 3)).toBe('...sum')
+        })
+        it('should show no letters at the start if "start" is 0', () => {
+            expect(truncateString('Lorem ipsum', 3, 0)).toBe('Lor...')
+        })
+        it('should do nothing if start and end would exactly include the word', () => {
+            expect(truncateString('Lorem ipsum', 4, 7)).toBe('Lorem ipsum')
+        })
+        it('should do nothing if start and end would exactly include the word', () => {
+            expect(truncateString('Lorem ipsum', 4, 7)).toBe('Lorem ipsum')
+        })
+        it('should do nothing if start and end would be overlapping', () => {
+            expect(truncateString('Lorem ipsum', 7, 7)).toBe('Lorem ipsum')
+        })
+        it('should do nothing if the added dots would make the resulting string longer', () => {
+            expect(truncateString('Lorem ipsum', 4, 6)).toBe('Lorem ipsum')
         })
     })
 })
