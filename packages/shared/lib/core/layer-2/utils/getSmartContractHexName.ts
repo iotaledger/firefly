ce27d9d1cdf1e0e027d93b19bae478751427e761
Begoña Alvarez <balvarez@boxfish.studio>
2023-05-17 12:50:38 +0200

    chore: remove unnecessary code

--- packages/shared/lib/core/layer-2/utils/getSmartContractHexName.ts
+++ packages/shared/lib/core/layer-2/utils/getSmartContractHexName.ts
@@ -7,13 +7,11 @@ export function getSmartContractHexName(name: string): number {
     const SMART_CONTRACT_HEX_NAME_LENGTH = 4
     const stringBytes = Converter.utf8ToBytes(name)
     const hash = Blake2b.sum256(stringBytes)
-
-    for (let i = 0; i < hash.length; i += SMART_CONTRACT_HEX_NAME_LENGTH) {
-        const slice = hash.slice(i, i + SMART_CONTRACT_HEX_NAME_LENGTH)
+    if (name?.length) {
+        const slice = hash.slice(0, SMART_CONTRACT_HEX_NAME_LENGTH)
         const cursor = new SimpleBufferCursor(Buffer.from(slice))
-
-        return cursor.readUInt32LE()
+        return cursor?.readUInt32LE()
+    } else {
+        return 0
     }
-
-    return 0
 }
