30a368766616ff8dca26a63375379de87effef98
Matthew Maxwell <maxwellmattryan@gmail.com>
2023-05-17 16:58:17 -0500

    chore: fix error handler function

--- packages/shared/lib/core/ledger/actions/registerLedgerDeviceEventHandlers.ts
+++ packages/shared/lib/core/ledger/actions/registerLedgerDeviceEventHandlers.ts
@@ -1,8 +1,8 @@
 import { Platform } from '@core/app/classes'
-import { addError, IError } from '@core/error'
+import { addError } from '@core/error'
 
 export function registerLedgerDeviceEventHandlers(): void {
     Platform.onEvent('ledger-error', (error) => {
-        addError(<IError>{ ...(error && { error }) })
+        addError(error)
     })
 }
