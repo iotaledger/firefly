3902f0e2d8fce8578448c1652ea1e2e8679a2f5e
Matthew Maxwell <maxwellmattryan@gmail.com>
2023-05-17 13:44:45 -0500

    chore: change save method

--- packages/shared/lib/core/ledger/actions/registerLedgerDeviceEventHandlers.ts
+++ packages/shared/lib/core/ledger/actions/registerLedgerDeviceEventHandlers.ts
@@ -3,6 +3,6 @@ import { addError, IError } from '@core/error'
 
 export function registerLedgerDeviceEventHandlers(): void {
     Platform.onEvent('ledger-error', (error) => {
-        addError(<IError>{ ...error })
+        addError(<IError>{ ...(error && { error }) })
     })
 }
