b1129c2c697b3b8145f933aab2f3ebd17b75620a
Mark Nardi <mark.nardi@iota.org>
2023-05-19 17:13:31 +0200

    chore: better naming

--- packages/shared/lib/core/app/interfaces/evm-address-event.interface.ts
+++ packages/shared/lib/core/app/interfaces/evm-address-event.interface.ts
@@ -1,4 +1,4 @@
-export type IEvmAddress = {
+export interface IEvmAddressEvent {
     coinType: number
     evmAddress: string
     accountIndex: number
--- packages/shared/lib/core/app/interfaces/platform-event-map.interface.ts
+++ packages/shared/lib/core/app/interfaces/platform-event-map.interface.ts
@@ -1,4 +1,4 @@
-import { IAppUpdateDownloadProgress, IAppVersionDetails, IEvmAddress, INFTDownloadState } from '.'
+import { IAppUpdateDownloadProgress, IAppVersionDetails, IEvmAddressEvent, INFTDownloadState } from '.'
 
 export interface IPlatformEventMap {
     'menu-logout': void
@@ -17,5 +17,5 @@ export interface IPlatformEventMap {
     'notification-activated': unknown
     'nft-download-done': INFTDownloadState
     'nft-download-interrupted': INFTDownloadState
-    'evm-address': IEvmAddress
+    'evm-address': IEvmAddressEvent
 }
