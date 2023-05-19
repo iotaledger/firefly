f5f1c08ffbd170509ef53df89d39827c40957a67
MarkNerdi <105642810+MarkNerdi@users.noreply.github.com>
2023-05-19 14:20:54 +0200

    chore: mint token details to strict mode (#6856)
    * chore: change mint token details to strict mode
    
    Co-authored-by: Jason Kraft <jason.kraft@iota.org>
    
    * chore: update default value for native token form
    
    * fix: fix prettier
    
    * fix: fix prettier
    
    * fix: remove random string
    
    * delete unused IEmptyMintNftDetails interface from token details file
    
    ---------
    
    Co-authored-by: Jason Kraft <jason.kraft@iota.org>
    Co-authored-by: Jason Kraft <developer.kraft@gmail.com>

--- packages/shared/lib/core/wallet/stores/mint-token-details.store.ts
+++ packages/shared/lib/core/wallet/stores/mint-token-details.store.ts
@@ -1,32 +1,12 @@
 import { writable } from 'svelte/store'
 import { IMintTokenDetails } from '../interfaces'
 
-export const mintTokenDetails = writable<IMintTokenDetails>({
-    name: undefined,
-    totalSupply: undefined,
-    circulatingSupply: undefined,
-    decimals: 0,
-    symbol: undefined,
-    description: undefined,
-    url: undefined,
-    logoUrl: undefined,
-    aliasId: undefined,
-})
+export const mintTokenDetails = writable<IMintTokenDetails | undefined>(undefined)
 
 export function setMintTokenDetails(payload: IMintTokenDetails): void {
     mintTokenDetails.set(payload)
 }
 
 export function resetMintTokenDetails(): void {
-    mintTokenDetails.set({
-        name: undefined,
-        totalSupply: undefined,
-        circulatingSupply: undefined,
-        decimals: 0,
-        symbol: undefined,
-        description: undefined,
-        url: undefined,
-        logoUrl: undefined,
-        aliasId: undefined,
-    })
+    mintTokenDetails.set(undefined)
 }
