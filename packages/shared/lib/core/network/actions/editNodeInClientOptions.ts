b81c93dfbb57c05851b5573a4fffabb7a16a1f57
Jason Kraft <developer.kraft@gmail.com>
2023-05-19 10:45:25 -0400

    Add strict coverage for editNodeInClientOptions

--- packages/shared/lib/core/network/actions/editNodeInClientOptions.ts
+++ packages/shared/lib/core/network/actions/editNodeInClientOptions.ts
@@ -3,8 +3,10 @@ import { activeProfile } from '@core/profile/stores'
 import { get } from 'svelte/store'
 
 export async function editNodeInClientOptions(currentNode: INode, node: INode): Promise<void> {
-    const clientOptions = get(activeProfile)?.clientOptions
-    const newNode = clientOptions.nodes.find((_node) => _node.url === currentNode.url)
-    Object.assign(newNode, node)
-    await updateClientOptions(clientOptions)
+    const clientOptions = get(activeProfile).clientOptions
+    const newNode = clientOptions.nodes?.find((_node) => _node.url === currentNode.url)
+    if (newNode) {
+        Object.assign(newNode, node)
+        await updateClientOptions(clientOptions)
+    }
 }
