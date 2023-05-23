ddb4c7612d19972f657da88e860fef702ca5c543
Begoña Alvarez <balvarez@boxfish.studio>
2023-05-23 11:32:44 +0200

    chore: remove debris

--- packages/shared/lib/core/account/interfaces/index.ts
+++ packages/shared/lib/core/account/interfaces/index.ts
@@ -1,4 +1,3 @@
 export * from './account-state.interface'
 export * from './account.interface'
 export * from './persisted-account-data.interface'
-export * from './persisted-evm-addresses.interface'
--- packages/shared/lib/core/account/interfaces/persisted-evm-addresses.interface.ts
+++
@@ -1,3 +0,0 @@
-export interface IPersistedEvmAddresses {
-    [profileId: string]: (string | undefined)[]
-}
