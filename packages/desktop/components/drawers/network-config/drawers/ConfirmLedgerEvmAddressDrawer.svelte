e80773b70bd9d1c54dadb1b388ae5566fa14dffb
Begoña Alvarez <balvarez@boxfish.studio>
2023-05-23 13:59:36 +0200

    fix: improve loading

--- packages/desktop/components/drawers/network-config/drawers/ConfirmLedgerEvmAddressDrawer.svelte
+++ packages/desktop/components/drawers/network-config/drawers/ConfirmLedgerEvmAddressDrawer.svelte
@@ -1,9 +1,8 @@
 <script lang="ts">
-    import { selectedAccount } from '@core/account'
+    import { selectedAccount, updateSelectedAccountPersistedData } from '@core/account'
     import { localize } from '@core/i18n'
     import { loadEvmAddressForSelectedAccount } from '@core/layer-2'
     import { selectedChain } from '@core/network'
-    import { updateSelectedAccountPersistedData } from '@core/account'
     import { Router } from '@core/router'
     import { DrawerRoute, NetworkConfigRoute, networkConfigRouter } from '@desktop/routers'
     import { Animation, Button, CopyableBox, FontWeight, Pane, Text, TextType } from '@ui'
@@ -43,22 +42,26 @@
     <confirm-ledger-evm-address-drawer class="flex flex-col justify-between w-full h-full">
         <div class="flex flex-col self-center">
             <Animation animation="ledger-prompt-confirmed-desktop" />
-            <Text type={TextType.h4}
-                >{localize('views.dashboard.drawers.networkConfig.confirmLedgerEvmAddress.header')}</Text
-            >
-            <Pane classes="mt-6">
-                <CopyableBox value={address ?? '---'} classes="bg-transparent w-full">
-                    <div class="w-full text-left">
-                        <Text fontWeight={FontWeight.medium} fontSize="13" color="gray-600"
-                            >{localize('general.evmAddress')}</Text
-                        >
-                        <Text type={TextType.pre} fontWeight={FontWeight.medium} fontSize="15" classes="break-words"
-                            >{address ?? '---'}</Text
-                        >
-                    </div>
-                </CopyableBox>
-            </Pane>
+            {#if address}
+                <Text type={TextType.h4}
+                    >{localize('views.dashboard.drawers.networkConfig.confirmLedgerEvmAddress.header')}</Text
+                >
+                <Pane classes="mt-6">
+                    <CopyableBox value={address ?? '---'} classes="bg-transparent w-full">
+                        <div class="w-full text-left">
+                            <Text fontWeight={FontWeight.medium} fontSize="13" color="gray-600"
+                                >{localize('general.evmAddress')}</Text
+                            >
+                            <Text type={TextType.pre} fontWeight={FontWeight.medium} fontSize="15" classes="break-words"
+                                >{address ?? '---'}</Text
+                            >
+                        </div>
+                    </CopyableBox>
+                </Pane>
+            {/if}
         </div>
-        <Button outline disabled={!address} onClick={onContinueClick}>{localize('actions.continue')}</Button>
+        <Button outline disabled={!address} isBusy={!address} onClick={onContinueClick}>
+            {localize('actions.continue')}
+        </Button>
     </confirm-ledger-evm-address-drawer>
 </DrawerTemplate>
