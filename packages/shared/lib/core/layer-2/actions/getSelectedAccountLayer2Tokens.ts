8defaf5351c45a06e3e1d6aa569d12e026e8f52f
Tuditi <daviddetroch@pm.me>
2023-05-26 13:51:43 +0200

    chore: add return type

--- packages/shared/lib/core/layer-2/actions/getSelectedAccountLayer2Tokens.ts
+++ packages/shared/lib/core/layer-2/actions/getSelectedAccountLayer2Tokens.ts
@@ -1,6 +1,6 @@
 import { getSelectedAccountWrappedNativeTokens } from './getSelectedAccountWrappedNativeTokens'
 
-export async function getSelectedAccountLayer2Tokens(): Promise<unknown[]> {
+export async function getSelectedAccountLayer2Tokens(): Promise<{ amount: bigint; id: string }[]> {
     const wrappedNativeTokens = await getSelectedAccountWrappedNativeTokens()
     return [...wrappedNativeTokens]
 }
