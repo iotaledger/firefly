a449fd5e2b1c775cc3535d5a6e6e684615ff73b3
Jean Ribeiro <iamjeanribeiro@gmail.com>
2023-06-21 10:45:11 -0300

    fix: Mark's suggestion
    Co-authored-by: MarkNerdi996 <105642810+MarkNerdi996@users.noreply.github.com>

--- packages/shared/lib/core/wallet/utils/getOutputParameters.ts
+++ packages/shared/lib/core/wallet/utils/getOutputParameters.ts
@@ -97,10 +97,10 @@ function getAssetFromTransactionDetails(transactionDetails: NewTransactionDetail
     return assets
 }
 
-function getMetadata(transactionDetails: NewTransactionDetails): Promise<string> {
+async function getMetadata(transactionDetails: NewTransactionDetails): Promise<string> {
     if (transactionDetails.layer2Parameters) {
-        return getLayer2MetadataForTransfer(transactionDetails)
+        return await getLayer2MetadataForTransfer(transactionDetails)
     } else {
-        return Promise.resolve(Converter.utf8ToHex(transactionDetails?.metadata))
+        return Converter.utf8ToHex(transactionDetails?.metadata)
     }
 }
