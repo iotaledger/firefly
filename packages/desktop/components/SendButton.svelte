2aaab1dea06ea5f21705fb75eb6f47552bb36533
Begoña Alvarez <balvarez@boxfish.studio>
2023-06-21 18:18:57 +0200

    remove: new send flow

--- packages/desktop/components/SendButton.svelte
+++ packages/desktop/components/SendButton.svelte
@@ -4,16 +4,13 @@
     import { resetNewTokenTransactionDetails } from '@core/wallet'
     import { openPopup, PopupId } from '@desktop/auxiliary/popup'
     import { OnboardingButton } from '@ui'
-    import { SendFlowRouter, sendFlowRouter } from '@views/dashboard/send-flow'
-    import features from '@features/features'
 
     function onSendClick(): void {
         resetNewTokenTransactionDetails()
         resetLedgerPreparedOutput()
         resetShowInternalVerificationPopup()
-        sendFlowRouter.set(new SendFlowRouter(undefined))
         openPopup({
-            id: features.wallet.newSendFlow.enabled ? PopupId.SendFlow : PopupId.SendForm,
+            id: PopupId.SendForm,
             overflow: true,
         })
     }
