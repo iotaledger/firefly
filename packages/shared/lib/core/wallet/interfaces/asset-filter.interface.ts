52c06abbf87ae2e5fbf4b18e718c46e7ab3d7a21
Begoña Alvarez <balvarez@boxfish.studio>
2023-06-21 20:32:17 +0200

    remove: network filter

--- packages/shared/lib/core/wallet/interfaces/asset-filter.interface.ts
+++ packages/shared/lib/core/wallet/interfaces/asset-filter.interface.ts
@@ -1,12 +1,7 @@
-import {
-    NetworkFilterUnit,
-    OrderFilterUnit,
-    SelectionFilterUnit,
-} from '@core/utils/interfaces/filter/filter-unit.interface'
+import { OrderFilterUnit, SelectionFilterUnit } from '@core/utils/interfaces/filter/filter-unit.interface'
 
 export interface AssetFilter {
     verificationStatus: SelectionFilterUnit
-    network: NetworkFilterUnit
     showHidden: SelectionFilterUnit
     order: OrderFilterUnit
 }
