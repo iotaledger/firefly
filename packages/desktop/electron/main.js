4d987ce582a146ef03dc92fc228beca467aaaa51
cpl121 <cpeon@boxfish.studio>
2023-06-29 13:10:23 +0200

    feat: lock firefly when user locks OS

--- packages/desktop/electron/main.js
+++ packages/desktop/electron/main.js
@@ -16,6 +16,7 @@ const {
     session,
     utilityProcess,
     nativeTheme,
+    powerMonitor,
 } = require('electron')
 const path = require('path')
 const fs = require('fs')
@@ -354,6 +355,11 @@ app.on('window-all-closed', () => {
     if (process.platform !== 'darwin') {
         app.quit()
     }
+})
+
+powerMonitor.on('lock-screen', () => {
+    // MacOS and Windows
+    windows.main.webContents.send('lock-screen')
 })
 
 app.once('ready', () => {
