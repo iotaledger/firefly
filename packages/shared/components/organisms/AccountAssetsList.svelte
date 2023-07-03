4415d69489eb6850929963b263f44e89a43e554b
Jean Carlo Noguera <jcnoguera@boxfish.studio>
2023-07-03 18:35:11 +0200

    refactor: remove AccountAssetsList

--- packages/shared/components/organisms/AccountAssetsList.svelte
+++
@@ -1,6 +0,0 @@
-<script lang="ts">
-    import { AssetList } from 'shared/components'
-    import { selectedAccountAssets } from '@core/wallet'
-</script>
-
-<AssetList assets={$selectedAccountAssets} />
