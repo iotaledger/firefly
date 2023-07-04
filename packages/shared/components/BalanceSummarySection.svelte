499cc41adc2a8b53207619ef05b9c48358de89a6
evavirseda <evirseda@boxfish.studio>
2023-07-04 15:06:37 +0200

    refactor: move BalanceSummarySection out of atoms and cleanup

--- packages/shared/components/BalanceSummarySection.svelte
+++ packages/shared/components/BalanceSummarySection.svelte
@@ -1,28 +1,28 @@
 <script lang="ts">
     import { formatCurrency, localize } from '@core/i18n'
     import { getMarketAmountFromAssetValue } from '@core/market/utils'
-    import { formatTokenAmountBestMatch, selectedAccountAssets } from '@core/wallet'
+    import { IAsset, TokenMetadata, formatTokenAmountBestMatch, selectedAccountAssets } from '@core/wallet'
     import { BalanceSummaryRow, Icon } from 'shared/components'
     import { Icon as IconEnum } from '@auxiliary/icon'
     import { activeProfile } from '@core/profile'
 
     export let titleKey: string
     export let subtitleKey: string = ''
-    export let subBreakdown: { [key: string]: { amount: number } } = undefined
+    export let subBreakdown: { [key: string]: { amount: number } } = {}
     export let amount: number
     export let bold: boolean = false
 
-    let expanded = false
+    let expanded: boolean = false
 
     $: hasChildren = !!Object.keys(subBreakdown ?? {}).length
     $: ({ baseCoin } = $selectedAccountAssets?.[$activeProfile?.network?.id] ?? {})
 
     function getAmount(amount: number): string {
-        return formatTokenAmountBestMatch(amount, baseCoin.metadata)
+        return formatTokenAmountBestMatch(amount, baseCoin?.metadata as TokenMetadata)
     }
 
     function getCurrencyAmount(amount: number): string {
-        return formatCurrency(getMarketAmountFromAssetValue(amount, baseCoin))
+        return formatCurrency(getMarketAmountFromAssetValue(amount, baseCoin as IAsset))
     }
 
     function toggleExpandedView(): void {
@@ -34,7 +34,7 @@
     <div
         class="w-full flex flex-row flex-grow justify-between space-x-2 {hasChildren ? 'cursor-pointer ' : ''}"
         on:click={toggleExpandedView}
-        on:keydown={() => {}}
+        on:keydown={toggleExpandedView}
     >
         {#if hasChildren}
             <Icon
@@ -54,13 +54,14 @@
     </div>
     {#if expanded}
         {#each Object.keys(subBreakdown ?? {}) as breakdownKey}
-            <BalanceSummaryRow
-                title={localize(`popups.balanceBreakdown.${breakdownKey}.title`)}
-                subtitle={localize(`popups.balanceBreakdown.${breakdownKey}.subtitle`)}
-                amount={getAmount(subBreakdown[breakdownKey].amount)}
-                convertedAmount={getCurrencyAmount(subBreakdown[breakdownKey].amount)}
-                classes="ml-8"
-            />
+            <balance-summary-row-expanded class="ml-8">
+                <BalanceSummaryRow
+                    title={localize(`popups.balanceBreakdown.${breakdownKey}.title`)}
+                    subtitle={localize(`popups.balanceBreakdown.${breakdownKey}.subtitle`)}
+                    amount={getAmount(subBreakdown[breakdownKey].amount)}
+                    convertedAmount={getCurrencyAmount(subBreakdown[breakdownKey].amount)}
+                />
+            </balance-summary-row-expanded>
         {/each}
     {/if}
 </div>
