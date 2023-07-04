8c000b1a40b5ab13af00dcfb9e38c113a8b24411
evavirseda <evirseda@boxfish.studio>
2023-07-04 13:33:27 +0200

    fix: rename files

--- packages/shared/components/interfaces/index.ts
+++ packages/shared/components/interfaces/index.ts
@@ -1,3 +1,3 @@
 export * from './network-recipient-selector-option.interface'
 export * from './option.interface'
-export * from './node.interface'
+export * from './node-configuration-form.interface'
