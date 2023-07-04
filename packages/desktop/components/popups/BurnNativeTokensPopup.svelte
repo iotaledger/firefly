bedfcc2c85a26cae1551dd9fd3fac328ed5194b5
Jean Carlo Noguera <jcnoguera@boxfish.studio>
2023-07-04 17:29:53 +0200

    chore: move auxiliary popup back to the shared folder

--- packages/desktop/components/popups/BurnNativeTokensPopup.svelte
+++ packages/desktop/components/popups/BurnNativeTokensPopup.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
     import { Button, Text, TextHint, FontWeight, TextType, AssetAmountInput } from 'shared/components'
     import { localize } from '@core/i18n'
-    import { closePopup, openPopup, PopupId } from '@desktop/auxiliary/popup'
+    import { closePopup, openPopup, PopupId } from '@auxiliary/popup'
     import { IAsset } from '@core/wallet'
 
     export let asset: IAsset
