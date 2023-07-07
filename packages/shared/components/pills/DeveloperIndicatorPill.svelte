20999d9d3d528489989f6985af65c6f77a80aecf
evavirseda <evirseda@boxfish.studio>
2023-07-07 12:15:56 +0200

    fix: add missing enum

--- packages/shared/components/pills/DeveloperIndicatorPill.svelte
+++ packages/shared/components/pills/DeveloperIndicatorPill.svelte
@@ -1,5 +1,6 @@
 <script lang="ts">
     import { localize } from '@core/i18n'
+    import { PillBorderRadius } from 'components/enums'
     import { Pill } from 'shared/components'
 </script>
 
@@ -9,5 +10,5 @@
     darkTextColor="white"
     backgroundColor="bg-gray-100 dark:bg-opacity-20"
     uppercase
-    rounded="rounded-full"
+    rounded={PillBorderRadius.Full}
 />
