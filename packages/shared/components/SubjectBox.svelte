35de6fbf88979f0cf3e8702dc10f671be11cd21e
evavirseda <evirseda@boxfish.studio>
2023-07-11 15:18:52 +0200

    feat: cleanup SubjectBox

--- packages/shared/components/SubjectBox.svelte
+++ packages/shared/components/SubjectBox.svelte
@@ -3,15 +3,20 @@
     import { Subject } from '@core/wallet'
     import { Box, AddressBox, Text, AccountLabel, TextType, FontWeight } from 'shared/components'
 
-    export let subject: Subject = null
+    export let subject: Subject | null = null
+
+    enum SubjectType {
+        Account = 'account',
+        Address = 'address',
+    }
 </script>
 
-{#if subject.type === 'account'}
+{#if subject?.type === SubjectType.Account}
     <Box row clearBackground clearPadding classes="justify-center">
-        <AccountLabel account={subject.account} />
+        <AccountLabel account={subject?.account} />
     </Box>
-{:else if subject.type === 'address'}
-    <AddressBox clearBackground clearPadding isCopyable address={subject.address} />
+{:else if subject?.type === SubjectType.Address}
+    <AddressBox clearBackground clearPadding isCopyable address={subject?.address} />
 {:else}
     <Box row clearBackground clearPadding classes="justify-center">
         <Text type={TextType.pre} fontSize="base" fontWeight={FontWeight.medium}>
