3c9cbf61a5b31570e9d474e0fd03d5aed6e67a7f
evavirseda <evirseda@boxfish.studio>
2023-07-13 16:11:51 +0200

    feat: cleanup

--- packages/shared/components/molecules/AssetIcon.svelte
+++ packages/shared/components/molecules/AssetIcon.svelte
@@ -1,41 +1,36 @@
 <script lang="ts">
     import { Icon as IconEnum, NETWORK_ICON_SVG } from '@auxiliary/icon'
     import { getIconColorFromString } from '@core/account'
-    import { COIN_TYPE, IPersistedNetwork, NetworkId } from '@core/network'
-    import { activeProfile } from '@core/profile'
+    import { COIN_TYPE, NetworkId } from '@core/network'
     import { isBright } from '@core/utils'
     import { ANIMATED_TOKEN_IDS, getAssetInitials, IPersistedAsset, TokenStandard } from '@core/wallet'
     import { Animation, Icon, VerificationBadge } from 'shared/components'
 
     export let asset: IPersistedAsset
-    export let large = false
-    export let small = false
+    export let large: boolean = false
+    export let small: boolean = false
 
     let icon: IconEnum | null
-    let assetIconColor: string
+    let assetIconColor: string = 'text-blue-500'
     let assetIconBackgroundColor: string
     let assetInitials: string
     let assetIconWrapperWidth: number
     let assetLogoUrl: string
-    let network: IPersistedNetwork
 
-    $: network = $activeProfile.network
     $: isAnimation = asset.id in ANIMATED_TOKEN_IDS
     $: asset, updateAssetIcon()
 
     function updateAssetIcon(): void {
         switch (asset.id) {
             case String(COIN_TYPE[NetworkId.Iota]):
-                assetInitials = ''
                 assetIconBackgroundColor = '#6E82A4'
-                assetIconColor = isBright(assetIconBackgroundColor) ? 'gray-800' : 'white'
+                assetIconColor = isBright(assetIconBackgroundColor) ? 'text-gray-800' : 'text-white'
                 icon = NETWORK_ICON_SVG[NetworkId.Iota]
                 break
             case String(COIN_TYPE[NetworkId.Shimmer]):
             case String(COIN_TYPE[NetworkId.Testnet]):
-                assetInitials = ''
                 assetIconBackgroundColor = '#25DFCA'
-                assetIconColor = isBright(assetIconBackgroundColor) ? 'gray-800' : 'white'
+                assetIconColor = isBright(assetIconBackgroundColor) ? 'text-gray-800' : 'text-white'
                 icon = NETWORK_ICON_SVG[NetworkId.Shimmer]
                 break
             default:
@@ -44,26 +39,19 @@
                     shades: ['500', '600', '700', '800'],
                     colorsToExclude: ['gray'],
                 })
-                assetIconColor = isBright(assetIconBackgroundColor) ? 'gray-800' : 'white'
+                assetIconColor = isBright(assetIconBackgroundColor) ? 'text-gray-800' : 'text-white'
                 assetLogoUrl = asset.metadata?.standard === TokenStandard.Irc30 ? asset.metadata?.logoUrl ?? '' : ''
                 icon = null
         }
     }
 </script>
 
-<div
-    class="
-        relative flex
-        {large ? 'w-12 h-12' : small ? 'w-6 h-6' : 'w-8 h-8'}
-    "
->
-    <div
-        class="
-        rounded-full flex justify-center items-center transition-none
-        {isAnimation ? 'p-0' : 'p-1'}
-        {large ? 'w-12 h-12' : small ? 'w-6 h-6' : 'w-8 h-8'}
-        {assetIconBackgroundColor ? 'icon-bg' : 'bg-blue-500'}
-    "
+<asset-icon-wrapper class:large class:small>
+    <asset-icon
+        class:isAnimation
+        class:large
+        class:small
+        class:icon-bg={assetIconBackgroundColor}
         style={assetIconBackgroundColor ? `--icon-bg-color: ${assetIconBackgroundColor}` : ''}
         bind:clientWidth={assetIconWrapperWidth}
     >
@@ -75,27 +63,62 @@
                 renderer="canvas"
             />
         {:else if icon}
-            <Icon {icon} width="80%" height="80%" classes="text-{assetIconColor ?? 'blue-500'} text-center" />
+            <Icon {icon} width="80%" height="80%" classes="{assetIconColor} text-center" />
         {:else if assetLogoUrl}
-            <img src={assetLogoUrl} on:error={() => (assetLogoUrl = '')} alt="" class="w-full h-full" />
+            <img src={assetLogoUrl} on:error={() => (assetLogoUrl = '')} alt={assetLogoUrl} class="w-full h-full" />
         {:else}
             <p
-                style={`font-size: ${Math.floor(
+                style:--font-size={Math.floor(
                     Math.min(large ? 20 : 12, assetIconWrapperWidth / assetInitials?.length)
-                )}px;`}
-                class="transition-none font-600 text-{assetIconColor ?? 'blue-500'} text-center"
+                ) + 'px'}
+                class=" {assetIconColor}"
             >
                 {assetInitials?.toUpperCase() ?? '-'}
             </p>
         {/if}
-    </div>
-    <span class="absolute flex justify-center items-center bottom-0 right-0">
+    </asset-icon>
+    <verification-badge-wrapper class="absolute flex justify-center items-center bottom-0 right-0">
         <VerificationBadge status={asset.verification?.status} width={14} height={14} />
-    </span>
-</div>
+    </verification-badge-wrapper>
+</asset-icon-wrapper>
 
 <style lang="scss">
-    .icon-bg {
-        background-color: var(--icon-bg-color);
+    asset-icon-wrapper {
+        @apply relative;
+        @apply flex;
+        @apply w-8 h-8;
+        &.large {
+            @apply w-12 h-12;
+        }
+        &.small {
+            @apply w-6 h-6;
+        }
+        asset-icon {
+            @apply p-1;
+            @apply rounded-full;
+            @apply flex justify-center items-center;
+            @apply transition-none;
+            @apply bg-blue-500;
+            @apply w-8 h-8;
+            &.isAnimation {
+                @apply p-0;
+            }
+            &.large {
+                @apply w-12 h-12;
+            }
+            &.small {
+                @apply w-6 h-6;
+            }
+
+            &.icon-bg {
+                background-color: var(--icon-bg-color);
+            }
+            p {
+                @apply transition-none;
+                @apply font-600;
+                @apply text-center;
+                font-size: var(--font-size);
+            }
+        }
     }
 </style>
