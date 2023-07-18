71569cd559a2cdbc24e6520f1ea66523c81d0c0f
Branko Bosnic <brankobosnic1@gmail.com>
2023-07-18 11:37:10 +0200

    fix: incomingTransactions signature

--- packages/shared/lib/core/account/interfaces/account.interface.ts
+++ packages/shared/lib/core/account/interfaces/account.interface.ts
@@ -84,7 +84,7 @@ export interface IAccount {
     getParticipationEventStatus(eventId: string): Promise<ParticipationEventStatus>
     getParticipationOverview(eventIds?: string[]): Promise<ParticipationOverview>
     getTransaction(transactionId: string): Promise<Transaction>
-    incomingTransactions(): Promise<[string, Transaction][]>
+    incomingTransactions(): Promise<Transaction[]>
     increaseNativeTokenSupply(
         tokenId: string,
         mintAmount: HexEncodedAmount,
