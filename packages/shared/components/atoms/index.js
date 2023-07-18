28dd56c19b80effcdd74b2304db4c80129556d1c
evavirseda <evirseda@boxfish.studio>
2023-07-18 17:30:54 +0200

    refactor: remove unnecessary ProposalDetailsButton (#7158)
    Co-authored-by: Begoña Álvarez de la Cruz <balvarez@boxfish.studio>

--- packages/shared/components/atoms/index.js
+++ packages/shared/components/atoms/index.js
@@ -1,4 +1,3 @@
 export { default as Pane } from './Pane.svelte'
-export { default as ProposalsDetailsButton } from './ProposalsDetailsButton.svelte'
 export { default as SubjectBox } from './SubjectBox.svelte'
 export { default as Tabs } from './Tabs.svelte'
