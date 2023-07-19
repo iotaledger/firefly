2dc7d4a6d86fcadbf0226884b428eaa3b0b1a646
evavirseda <evirseda@boxfish.studio>
2023-07-19 14:37:44 +0200

    refactor: cleanup component

--- packages/desktop/views/dashboard/settings/views/security/ExportStronghold.svelte
+++ packages/desktop/views/dashboard/settings/views/security/ExportStronghold.svelte
@@ -2,8 +2,8 @@
     import { ExportStrongholdButton, Spinner, Text, TextType } from 'shared/components'
     import { localize } from '@core/i18n'
 
-    let exportBusy = false
-    let exportMessage = ''
+    let exportBusy: boolean = false
+    let exportMessage: string = ''
 </script>
 
 <Text type={TextType.h4} classes="mb-3">{localize('views.settings.exportStronghold.title')}</Text>
