770e99aa1cbbc39b2b7e5843d88129c4a7c7e5c5
Jean Carlo Noguera <jcnoguera@boxfish.studio>
2023-07-24 12:53:59 +0200

    refactor: cleanup Spinner component

--- packages/shared/components/Spinner.svelte
+++ packages/shared/components/Spinner.svelte
@@ -2,27 +2,14 @@
     import { Icon, Text } from 'shared/components'
     import { Icon as IconEnum } from '@auxiliary/icon'
 
-    export let busy = false
-    export let message = ''
-    export let width = 24
-    export let height = 24
-    export let color = 'gray-500'
-    export let darkColor = 'white'
-    export let classes = ''
+    export let message: string = ''
+    export let size: number = 24
+    export let color: string = 'text-gray-500 dark:text-white'
 </script>
 
-{#if busy || message}
-    <spinner-container class={`flex flex-row items-center ${classes}`}>
-        {#if busy}
-            <Icon
-                icon={IconEnum.Refresh}
-                {width}
-                {height}
-                classes={`animate-spin-reverse text-${color} dark:text-${darkColor}`}
-            />
-        {/if}
-        {#if message}
-            <Text type="p" classes="ml-2" secondary>{message}</Text>
-        {/if}
-    </spinner-container>
-{/if}
+<spinner-container class="flex flex-row items-center justify-center">
+    <Icon icon={IconEnum.Refresh} width={size} height={size} classes="animate-spin-reverse {color}" />
+    {#if message}
+        <Text type="p" classes="ml-2" secondary>{message}</Text>
+    {/if}
+</spinner-container>
