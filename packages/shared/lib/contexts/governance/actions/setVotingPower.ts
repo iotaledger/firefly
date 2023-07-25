1299cab927bb674f1a4f4014bad951274ef0ca15
Jean Carlo Noguera <jcnoguera@boxfish.studio>
2023-07-25 16:21:17 +0200

    fix: avoid error when voting power stays the same

--- packages/shared/lib/contexts/governance/actions/setVotingPower.ts
+++ packages/shared/lib/contexts/governance/actions/setVotingPower.ts
@@ -7,19 +7,23 @@ import { handleError } from '@core/error/handlers'
 export async function setVotingPower(rawAmount: string): Promise<void> {
     const account = get(selectedAccount)
     try {
+        if (!account) return
+
         const votingPower = parseInt(account.votingPower, 10)
         const amount = parseInt(rawAmount, 10)
 
         updateSelectedAccount({ hasVotingPowerTransactionInProgress: true, isTransferring: true })
 
         let transaction: Transaction
-        if (amount > votingPower) {
-            const amountToIncrease = amount - votingPower
-            transaction = await account.increaseVotingPower(amountToIncrease.toString())
-        } else if (amount < votingPower) {
+
+        if (amount < votingPower || amount === votingPower) {
             const amountToDecrease = votingPower - amount
             transaction = await account.decreaseVotingPower(amountToDecrease.toString())
+        } else {
+            const amountToIncrease = amount - votingPower
+            transaction = await account.increaseVotingPower(amountToIncrease.toString())
         }
+
         await processAndAddToActivities(transaction, account)
     } catch (err) {
         handleError(err)
