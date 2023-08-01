76ecb2f5fd4169f836388dad1b6f33042bc802f0
evavirseda <evirseda@boxfish.studio>
2023-08-01 13:08:48 +0200

    refactor: remove unused `NetworkRecipientItem` and `NetworkRecipientSelector` components (#7215)
    Co-authored-by: Begoña Alvarez <balvarez@boxfish.studio>

--- packages/shared/components/molecules/index.js
+++ packages/shared/components/molecules/index.js
@@ -1,4 +1,2 @@
 export { default as MarkdownBlock } from './MarkdownBlock.svelte'
-export { default as NetworkRecipientItem } from './NetworkRecipientItem.svelte'
-export { default as NetworkRecipientSelector } from './NetworkRecipientSelector.svelte'
 export { default as SelectorInput } from './SelectorInput.svelte'
