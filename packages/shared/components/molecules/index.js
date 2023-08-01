097887fb8260b0f4e64a000ccd02c6272cf9e970
JCNoguera <88061365+VmMad@users.noreply.github.com>
2023-08-01 14:31:21 +0200

    refactor: cleanup `SelectorInput` (#7228)
    * refactor: cleanup `SelectorInput`
    
    * styles: remove class
    
    ---------
    
    Co-authored-by: Begoña Alvarez <balvarez@boxfish.studio>

--- packages/shared/components/molecules/index.js
+++
@@ -1,4 +0,0 @@
-export { default as ActivityInformation } from './ActivityInformation.svelte'
-export { default as AliasActivityDetails } from './AliasActivityDetails.svelte'
-export { default as AssetIcon } from './AssetIcon.svelte'
-export { default as SelectorInput } from './SelectorInput.svelte'
