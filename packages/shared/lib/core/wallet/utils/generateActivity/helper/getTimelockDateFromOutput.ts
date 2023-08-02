043c24d376c150b524bc1655711d942fc0da2340
cpl121 <100352899+cpl121@users.noreply.github.com>
2023-08-02 14:00:38 +0200

    feat: add util function detect time locked outputs (#51)
    * feat: extend IAccountState to include all addresses with unspent outputs
    
    * fix: check
    
    * feat: update syncing WIP
    
    * feat: update syncing during wallet finding in settings
    
    * feat: update syncing
    
    * feat: store unspent outputs
    
    Co-authored-by: Marc Espín <mespinsanz@gmail.com>
    
    * fix: remove unnecessary addressesIdsWithUnspentOutputs
    
    * feat: add function to filter all unlocked outputs
    
    * feat: modify IAccountState with addressesWithOutputs interface
    
    * feat: get addressesWithOutputs
    
    * feat: update addressesWithOutputs
    
    * feat: add derived function to filter all unlocked outputs
    
    * fix: remove logic in setAsyncStatusOfAccountActivities
    
    * fix: remove derived stores
    
    * feat: create isUnlockedOutput boolean util
    
    * feat: update logic and move files around
    
    * refactor: reverse logic to positive
    
    * chore: rename var
    
    ---------
    
    Co-authored-by: Marc Espín <mespinsanz@gmail.com>
    Co-authored-by: Begoña Alvarez <balvarez@boxfish.studio>

--- packages/shared/lib/core/wallet/utils/generateActivity/helper/getTimelockDateFromOutput.ts
+++ packages/shared/lib/core/wallet/utils/generateActivity/helper/getTimelockDateFromOutput.ts
@@ -2,7 +2,7 @@ import { MILLISECONDS_PER_SECOND } from '@core/utils'
 import { Output } from '@core/wallet/types'
 import { UNLOCK_CONDITION_TIMELOCK } from '../../../constants'
 
-export function getTimelockDateFromOutput(output: Output): Date {
+export function getTimelockDateFromOutput(output: Output): Date | undefined {
     for (const unlockCondition of output.unlockConditions) {
         if (unlockCondition?.type === UNLOCK_CONDITION_TIMELOCK) {
             return unlockCondition?.unixTime ? new Date(unlockCondition?.unixTime * MILLISECONDS_PER_SECOND) : undefined
---
+++ packages/shared/lib/core/wallet/utils/outputs/isOutputTimeLocked.ts
@@ -0,0 +1,11 @@
+import { OUTPUT_TYPE_TREASURY, Output, getTimelockDateFromOutput } from '@core/wallet'
+import { OutputData } from '@iota/wallet'
+
+export function isOutputTimeLocked(outputData: OutputData): boolean {
+    const output = outputData.output
+    if (output.type !== OUTPUT_TYPE_TREASURY) {
+        const unlockTime = getTimelockDateFromOutput(output as Output)
+        return unlockTime !== undefined && unlockTime.getTime() < Date.now()
+    }
+    return false
+}
