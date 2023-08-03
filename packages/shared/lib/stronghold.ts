8863cf51c06daea572611bb0e278a3c3700d4006
Begoña Alvarez <balvarez@boxfish.studio>
2023-08-03 14:53:17 +0200

    fix: error string

--- packages/shared/lib/stronghold.ts
+++ packages/shared/lib/stronghold.ts
@@ -15,7 +15,7 @@ import { Profile, ProfileType } from '@lib/typings/profile'
 
 export const STRONGHOLD_VERSION = 3
 
-export const STRONGHOLD_VERSION_ERROR = 'Unsupported snapshot version, migration required'
+export const STRONGHOLD_VERSION_ERROR = 'unsupported snapshot version, migration required'
 
 export const STRONGHOLD_DECRYPTION_ERROR = 'Failed to decrypt snapshot: incorrect password or corrupt data'
 
