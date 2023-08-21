49e956203ad4a6b3176cf8e1df2af39c0048b995
JCNoguera <88061365+VmMad@users.noreply.github.com>
2023-08-21 18:19:53 +0200

    refactor: cleanup `NodeActionsMenu` (#7263)
    * refactor: cleanup `NodeActionsMenu`
    
    * refactor: cleanup `ProfileActions`
    
    * chore: revert previous commit
    
    * chore: change tag name
    
    ---------
    
    Co-authored-by: Begoña Álvarez de la Cruz <balvarez@boxfish.studio>

--- packages/shared/components/modals/index.js
+++ packages/shared/components/modals/index.js
@@ -1,5 +1,4 @@
 export { default as AssetActionsModal } from './AssetActionsModal.svelte'
 export { default as CollectibleActionsModal } from './CollectibleActionsModal.svelte'
 export { default as Modal } from './Modal.svelte'
-export { default as NodeActionsMenu } from './NodeActionsMenu.svelte'
 export { default as ProfileActionsModal } from './ProfileActions.svelte'
