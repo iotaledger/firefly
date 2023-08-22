7c028eac224457b651f2d200212258dbdb5e7d80
JCNoguera <88061365+VmMad@users.noreply.github.com>
2023-08-22 17:46:03 +0200

    feat: add pulsing badge to unvisited new tabs (#80)
    * feat: add pulsating badge to unvisited new tabs
    
    * fix: update logic
    
    Co-authored-by: evavirseda <evirseda@boxfish.studio>
    
    * fix: rename store file & remove lint warnings
    
    * update store logic
    
    Co-authored-by: Marc Espín <mespinsanz@gmail.com>
    
    * feat: update initial persistent visited tab store
    
    ---------
    
    Co-authored-by: evavirseda <evirseda@boxfish.studio>
    Co-authored-by: Marc Espín <mespinsanz@gmail.com>
    Co-authored-by: Begoña Álvarez de la Cruz <balvarez@boxfish.studio>

--- packages/desktop/components/SidebarTab.svelte
+++ packages/desktop/components/SidebarTab.svelte
@@ -2,30 +2,37 @@
     import { Icon, PingingBadge, Position, InformationTooltip } from '@ui'
     import { dashboardRoute } from '@core/router'
     import { ISidebarTab } from '@desktop/routers'
+    import { addToVisitedTabs, visitedTabsStore } from '@contexts/dashboard/stores'
 
     export let tab: ISidebarTab = undefined
 
-    let tooltipAnchor: HTMLButtonElement
-    let showTooltip = false
+    let anchor: HTMLButtonElement
+    let showTooltip: boolean = false
 
     function onClick(): void {
-        _showTooltip(false)
+        handleHideTooltip()
         tab?.onClick()
+        addToVisitedTabs(tab?.route)
     }
 
-    function _showTooltip(show: boolean): void {
-        showTooltip = show
+    function handleHideTooltip(): void {
+        showTooltip = false
+    }
+
+    function handleShowTooltip(): void {
+        showTooltip = true
     }
 </script>
 
 <button
-    on:mouseenter={() => _showTooltip(true)}
-    on:mouseleave={() => _showTooltip(false)}
-    bind:this={tooltipAnchor}
+    on:mouseenter={handleShowTooltip}
+    on:mouseleave={handleHideTooltip}
+    bind:this={anchor}
     class="{$dashboardRoute === tab?.route ? 'text-blue-500' : 'text-gray-500'} relative"
     on:click={onClick}
 >
     <Icon width="24" height="24" icon={tab?.icon} />
+
     {#if tab?.notificationType}
         <PingingBadge
             innerColor={tab?.notificationType === 'warning' ? 'yellow-600' : 'red-500'}
@@ -33,8 +40,12 @@
             classes="absolute -top-2 -left-2"
         />
     {/if}
+
+    {#if tab?.showNotificationBadge || !$visitedTabsStore.includes(tab?.route)}
+        <PingingBadge innerColor="red-500" outerColor="red-300" classes="absolute -top-2 -left-2" />
+    {/if}
 </button>
 
 {#if showTooltip}
-    <InformationTooltip anchor={tooltipAnchor} position={Position.Right} size="small" body={tab?.label} />
+    <InformationTooltip {anchor} position={Position.Right} size="small" body={tab?.label} />
 {/if}
