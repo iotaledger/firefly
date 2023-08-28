4e5c8ff8df1199a79223eb14c01382e4bbc2f76f
Mario Sarcevic <mario.sarcevic@iota.org>
2023-08-28 20:48:04 +0200

    fix: Fix method not found on FoundryOutput (use readonly field directly)

--- packages/shared/lib/core/wallet/utils/getMetadataFromFoundryOutput.ts
+++ packages/shared/lib/core/wallet/utils/getMetadataFromFoundryOutput.ts
@@ -1,16 +1,17 @@
 import { FeatureType, FoundryOutput, MetadataFeature } from '@iota/sdk/out/types'
 
 export function getMetadataFromFoundryOutput(foundry: FoundryOutput): string {
-    for (const feature of foundry?.getImmutableFeatures() ?? []) {
+    for (const feature of foundry?.immutableFeatures ?? []) {
         if (feature?.type === FeatureType.Metadata) {
             return (feature as MetadataFeature).data
         }
     }
 
-    for (const feature of foundry?.getFeatures() ?? []) {
+    for (const feature of foundry?.features ?? []) {
         if (feature?.type === FeatureType.Metadata) {
             return (feature as MetadataFeature).data
         }
     }
+
     return undefined
 }
