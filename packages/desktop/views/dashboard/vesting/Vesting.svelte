bd6d68b17eced6de47b2e0a678eb1333244c6de2
evavirseda <evirseda@boxfish.studio>
2023-08-28 15:25:31 +0200

    feat: vesting Tab: Vesting stats - UI (#138)
    * feat: add vesting grid
    
    * feat: improve grid
    
    * feat: add locales
    
    * feat: add vesting stats ui
    
    * fix: update props
    
    * fix: rename coinValue
    
    * fix: add missing locale to button
    
    * fix: rename
    
    * fix: rename
    
    * feat: add scrollbar if too many outputs in the grid
    
    * styles: make table responsive
    
    * fix: table container overflowing
    
    * feat: add locales
    
    * styles: make placeholder lighter in dark mode
    
    * fix: remove unnecessary component
    
    * fix: update tag
    
    * refactor: simplify code & apply recommended names
    
    * fix: remove debris
    
    ---------
    
    Co-authored-by: Jean Carlo Noguera <jcnoguera@boxfish.studio>
    Co-authored-by: JCNoguera <88061365+VmMad@users.noreply.github.com>

--- packages/desktop/views/dashboard/vesting/Vesting.svelte
+++ packages/desktop/views/dashboard/vesting/Vesting.svelte
@@ -3,7 +3,7 @@
     import { localize } from '@core/i18n'
     import { selectedAccount } from '@core/account/stores'
     import { VestingSchedule } from '@components'
-    import { IVestingOutput, getBestTimeDuration, VestingOutputStatus } from '@core/utils'
+    import { IVestingOutput, VestingOutputStatus } from '@core/utils'
 
     const MOCKED_OUTPUTS: IVestingOutput[] = Array.from({ length: 120 }, (_, i) => {
         const amount = Math.floor(Math.random() * 100)
@@ -16,31 +16,53 @@
         }
     })
 
-    $: sortedVestingOutputsByUnlockTime = MOCKED_OUTPUTS.sort((a, b) => a.unlockTime.getTime() - b.unlockTime.getTime())
+    const MOCKED_VESTING_STATS = [
+        {
+            title: localize('views.vesting.stats.airdrop'),
+            iotaAmount: '--- IOTA',
+            convertedAmount: '$--',
+        },
+        {
+            title: localize('views.vesting.stats.remaining'),
+            iotaAmount: '--- IOTA',
+            convertedAmount: '$--',
+        },
+        {
+            title: localize('views.vesting.stats.totalRewards'),
+            iotaAmount: '--- IOTA',
+            convertedAmount: '$--',
+            asmbAmount: '--- ASMB',
+        },
+    ]
 
-    $: nextVestingDate =
-        MOCKED_OUTPUTS?.filter((output) => output?.unlockTime.getTime() > Date.now())
-            ?.sort((a, b) => a?.unlockTime?.getTime() - b?.unlockTime?.getTime())?.[0]
-            ?.unlockTime?.getTime() - Date.now()
+    $: sortedVestingOutputsByUnlockTime = MOCKED_OUTPUTS.sort((a, b) => a.unlockTime.getTime() - b.unlockTime.getTime())
 </script>
 
 {#if $selectedAccount}
     <vesting-container class="w-full h-full flex flex-col flex-nowrap p-8 relative flex-1 bg-gray-50 dark:bg-gray-900">
         {#key $selectedAccount?.index}
             <div class="h-full flex flex-row space-x-4">
-                <Pane classes="flex flex-col p-6 w-1/4">
-                    <Text type={TextType.h4} classes="mb-4">Pending vesting amount</Text>
-                    <Text type={TextType.h1} classes="mb-4">200 SMR</Text>
-                    <div class="mt-auto w-full">
-                        {#if nextVestingDate}
-                            <Text classes="mb-4">Next reward available in {getBestTimeDuration(nextVestingDate)}</Text>
-                        {/if}
-                        <div class="flex space-x-4">
-                            <Button classes="w-full">Collect</Button>
+                <Pane classes="flex flex-col p-6 w-1/3 justify-between">
+                    <div class="flex flex-col space-y-4">
+                        <Text type={TextType.h1}>{localize('views.vesting.title')}</Text>
+                        <div class="flex flex-col space-y-4">
+                            {#each MOCKED_VESTING_STATS as { title, iotaAmount, convertedAmount, asmbAmount }}
+                                <div class="flex flex-col space-y-2">
+                                    <Text color="gray-600" darkColor="gray-400" fontSize="12">{title}</Text>
+                                    <Text type={TextType.h1} classes="mt-4 mb-2">{iotaAmount}</Text>
+                                    <Text darkColor="gray-100" fontSize="12">{convertedAmount}</Text>
+                                    {#if asmbAmount}
+                                        <Text darkColor="gray-100" fontSize="12">{asmbAmount}</Text>
+                                    {/if}
+                                </div>
+                            {/each}
                         </div>
                     </div>
+                    <div class="w-full flex space-x-4">
+                        <Button classes="w-full">{localize('views.vesting.collect')}</Button>
+                    </div>
                 </Pane>
-                <Pane classes="h-full min-h-96 flex-1 py-8 px-12">
+                <Pane classes="h-full min-h-96 flex-1 py-8 px-12 w-2/3">
                     <Text type={TextType.h4}>{localize('views.vesting.airdrops.title')}</Text>
                     <div class="h-full flex justify-center items-center">
                         <VestingSchedule outputs={sortedVestingOutputsByUnlockTime} />
