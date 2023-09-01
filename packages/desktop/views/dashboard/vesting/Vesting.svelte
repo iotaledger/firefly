c371ff937af1730e905dde5da75c1c42e9a099f3
evavirseda <evirseda@boxfish.studio>
2023-09-01 16:37:07 +0200

    feat: add time until next airdrop (UI and logic) (#151)
    * feat: add vesting grid
    
    * feat: improve grid
    
    * feat: add locales
    
    * feat: add vesting stats ui
    
    * fix: update props
    
    * fix: rename coinValue
    
    * fix: add missing locale to button
    
    * fix: rename
    
    * fix: rename
    
    * feat: add scrollbar if too many outputs in the grid
    
    * styles: make table responsive
    
    * fix: table container overflowing
    
    * feat: add locales
    
    * styles: make placeholder lighter in dark mode
    
    * fix: remove unnecessary component
    
    * feat: Vesting stats - logic WIP
    
    * fix: update tag
    
    * refactor: simplify code & apply recommended names
    
    * feat: vesting stats - logic WIP
    
    * fix: rename constants and comments
    
    * fix: wrong logic to get total rewards
    
    * fix isStaker
    
    * fix: add index in vesting context
    
    * fix: use selectedAccountVestingOutputs store in getVestingStats
    
    * fix: make getVestingStats reactive
    
    * perf: improve code readability
    
    * feat: add locale
    
    * feat: add timeUntilNextTime
    
    * fix: rename variable
    
    * fix: remove calculateRemainingGridSpaces
    
    * fix: export IVestingStats to interface folder
    
    * fix: rename and move getFiatAmount function
    
    * fix: rename vestingStats and check baseCoin
    
    * perf: rename stats by overview
    
    * perf: make remainingPayouts singular
    
    * perf: create vestingOutputsOverview store and modfy logic
    
    * fix check
    
    * fix: selectedAccountVestingOutputs order
    
    * fix format
    
    * fix ??
    
    * perf: improvements
    
    * fix isInvestor
    
    * fix: update countdown
    
    * fix: format
    
    * fix: make reusable function and add return type
    
    * style: update text color in light theme
    
    * chore: update code to fit new sdk bindings
    
    * chore: cleanup code
    
    * fix: prevent crashes
    
    * feat: use real vesting information & reduce code duplication
    
    * chore: remove code debris
    
    * fix: remove duplicated comment
    
    ---------
    
    Co-authored-by: Jean Carlo Noguera <jcnoguera@boxfish.studio>
    Co-authored-by: JCNoguera <88061365+VmMad@users.noreply.github.com>
    Co-authored-by: cpl121 <cpeon@boxfish.studio>
    Co-authored-by: Begoña Álvarez de la Cruz <balvarez@boxfish.studio>

--- packages/desktop/views/dashboard/vesting/Vesting.svelte
+++ packages/desktop/views/dashboard/vesting/Vesting.svelte
@@ -1,17 +1,29 @@
 <script lang="ts">
+    import { Icon as IconEnum } from '@auxiliary/icon'
     import { VestingSchedule } from '@components'
     import {
         getLockedPayouts,
         getTotalPayouts,
         getUnlockedPayouts,
+        selectedAccountVestingOutputs,
         selectedAccountVestingOverview,
     } from '@contexts/vesting'
     import { selectedAccount } from '@core/account/stores'
+    import { time } from '@core/app'
     import { formatCurrency, localize } from '@core/i18n'
     import { getMarketAmountFromAssetValue } from '@core/market/utils'
     import { activeProfile } from '@core/profile'
-    import { formatTokenAmountBestMatch, selectedAccountAssets } from '@core/wallet'
-    import { Button, Pane, Text, TextType } from '@ui'
+    import { getBestTimeDuration } from '@core/utils'
+    import {
+        formatTokenAmountBestMatch,
+        getTimelockDateFromOutput,
+        isOutputTimeLocked,
+        selectedAccountAssets,
+    } from '@core/wallet'
+    import { CommonOutput } from '@iota/sdk/out/types'
+    import { Button, FontWeight, Icon, Pane, Text, TextType, Tile } from '@ui'
+
+    const DEFAULT_EMPTY_VALUE_STRING = '----'
 
     $: ({ baseCoin } = $selectedAccountAssets[$activeProfile?.network?.id])
 
@@ -23,7 +35,6 @@
         ? getUnlockedPayouts(totalPayouts, lockedPayouts, $selectedAccountVestingOverview.payoutAmount)
         : []
     $: allPayouts = [...unlockedPayouts, ...lockedPayouts]
-
     $: vestingOverview = [
         {
             title: localize('views.vesting.overview.airdrop'),
@@ -36,12 +47,29 @@
         {
             title: localize('views.vesting.overview.totalRewards'),
             iotaAmount: $selectedAccountVestingOverview?.totalRewards || 0,
-            asmbAmount: '--- ASMB',
+            asmbAmount: DEFAULT_EMPTY_VALUE_STRING,
         },
     ]
 
+    // TODO: reduce to every minute or so
+    let timeUntilNextAirdrop = DEFAULT_EMPTY_VALUE_STRING
+    $: $time, (timeUntilNextAirdrop = getTimeUntilNextAirdrop())
+
     function getFiatAmount(amount: number): string {
         return baseCoin ? formatCurrency(getMarketAmountFromAssetValue(amount, baseCoin)) : ''
+    }
+
+    function getTimeUntilNextAirdrop(): string {
+        // Note: we can look at the time of the first output, since all outputs along the addresses are unlocked at the same time
+        const upComingPayoutOutput = $selectedAccountVestingOutputs
+            ?.find((addressWithOutputs) => addressWithOutputs.outputs?.length)
+            ?.outputs.filter(isOutputTimeLocked)?.[0]?.output
+        if (!upComingPayoutOutput) {
+            return DEFAULT_EMPTY_VALUE_STRING
+        }
+        return getBestTimeDuration(
+            getTimelockDateFromOutput(upComingPayoutOutput as CommonOutput)?.getTime() - $time.getTime()
+        )
     }
 </script>
 
@@ -59,10 +87,10 @@
                                     <Text type={TextType.h1} classes="mt-4 mb-2">
                                         {baseCoin
                                             ? formatTokenAmountBestMatch(Math.round(iotaAmount), baseCoin.metadata)
-                                            : '----'}
+                                            : DEFAULT_EMPTY_VALUE_STRING}
                                     </Text>
                                     <Text darkColor="gray-100" fontSize="12">
-                                        {getFiatAmount(iotaAmount) ?? '----'}
+                                        {getFiatAmount(iotaAmount) ?? DEFAULT_EMPTY_VALUE_STRING}
                                     </Text>
                                     {#if asmbAmount}
                                         <Text darkColor="gray-100" fontSize="12">{asmbAmount}</Text>
@@ -71,7 +99,18 @@
                             {/each}
                         </div>
                     </div>
-                    <div class="w-full flex space-x-4">
+                    <div class="flex flex-col space-y-4">
+                        <div class="flex flex-col space-y-2">
+                            <Text color="gray-600" darkColor="gray-400" fontSize="12">
+                                {localize('views.vesting.timeUntilNextAirdrop')}
+                            </Text>
+                            <Tile classes="flex flex-row items-center space-x-2">
+                                <Icon icon={IconEnum.Timelock} classes="text-gray-800 dark:text-gray-100" />
+                                <Text color="gray-800" darkColor="gray-400" fontWeight={FontWeight.semibold}>
+                                    {timeUntilNextAirdrop}
+                                </Text>
+                            </Tile>
+                        </div>
                         <Button classes="w-full">{localize('views.vesting.collect')}</Button>
                     </div>
                 </Pane>
