8dcbbe25a88659fabe902eded153dfbbe5d745b6
Mario <mario.sarcevic@iota.org>
2023-09-07 18:09:57 +0200

    Fix: Improve tx history reminder logic to consider all known addresses (#214)
    Co-authored-by: Begoña Álvarez de la Cruz <balvarez@boxfish.studio>

--- packages/shared/lib/core/wallet/utils/transactions/getNonRemainderBasicOutputsFromTransaction.ts
+++ packages/shared/lib/core/wallet/utils/transactions/getNonRemainderBasicOutputsFromTransaction.ts
@@ -2,19 +2,21 @@ import { getRecipientAddressFromOutput } from '..'
 import { IWrappedOutput } from '@core/wallet/interfaces'
 import { ActivityDirection } from '@core/wallet/enums'
 import { CommonOutput } from '@iota/sdk/out/types'
+import { AddressWithOutputs } from '@core/account'
 
 export function getNonRemainderBasicOutputsFromTransaction(
     wrappedOutputs: IWrappedOutput[],
-    accountAddress: string,
+    accountAddressesWithOutputs: AddressWithOutputs[],
     direction: ActivityDirection
 ): IWrappedOutput[] {
     return wrappedOutputs.filter((outputData) => {
         const recipientAddress = getRecipientAddressFromOutput(outputData.output as CommonOutput)
+        const accountAddresses = accountAddressesWithOutputs.map((addressWithOutputs) => addressWithOutputs.address)
 
         if (direction === ActivityDirection.Incoming || direction === ActivityDirection.SelfTransaction) {
-            return accountAddress === recipientAddress
+            return accountAddresses.includes(recipientAddress)
         } else {
-            return accountAddress !== recipientAddress
+            return !accountAddresses.includes(recipientAddress)
         }
     })
 }
