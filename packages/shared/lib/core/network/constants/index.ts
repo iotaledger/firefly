6b437131bfa97f729345b0df9b704d3e7fe5fe88
Mario <mario.sarcevic@iota.org>
2023-09-12 18:39:38 +0200

    feat: Add maxParallelApiRequests to ClientOptions (#7415)
    * feat: Add maxParallelApiRequests config to ClientOptions (with migration to existing profiles)
    
    * fix: Fix failing network defaults test
    
    * fix: avoid crash for devs
    
    ---------
    
    Co-authored-by: Begoña Alvarez <balvarez@boxfish.studio>

--- packages/shared/lib/core/network/constants/index.ts
+++ packages/shared/lib/core/network/constants/index.ts
@@ -1,6 +1,7 @@
 export * from './coin-type.constant'
 export * from './default-base-token.constant'
 export * from './default-chain-configurations.constant'
+export * from './default-max-parallel-api-requests'
 export * from './default-network-metadata.constant'
 export * from './empty-node.constant'
 export * from './explorer-urls.constant'
