41ee777fad7dc0aee7e2554dd64a190b7a076f7a
Mario <mario.sarcevic@iota.org>
2023-09-13 11:14:41 +0200

    chore: Update Iota network node URL (#238)

--- packages/shared/lib/core/network/constants/official-node-urls.constant.ts
+++ packages/shared/lib/core/network/constants/official-node-urls.constant.ts
@@ -1,7 +1,7 @@
 import { NetworkId } from '../enums'
 
 export const OFFICIAL_NODE_URLS: Readonly<{ [key in NetworkId]?: string[] }> = {
-    [NetworkId.Iota]: ['https://api.lb-0.h.stardust-mainnet.iotaledger.net:8443'],
+    [NetworkId.Iota]: ['https://api.stardust-mainnet.iotaledger.net:8443'],
     [NetworkId.IotaAlphanet]: ['https://api.iota-alphanet.iotaledger.net'],
     [NetworkId.Shimmer]: ['https://api.shimmer.network'],
     [NetworkId.Testnet]: ['https://api.testnet.shimmer.network'],
--- packages/shared/lib/core/network/tests/network.test.ts
+++ packages/shared/lib/core/network/tests/network.test.ts
@@ -21,7 +21,7 @@ describe('File: network.ts', () => {
     }
 
     const EXPECTED_NODE_URLS: Readonly<{ [key in NetworkId]?: string[] }> = {
-        [NetworkId.Iota]: ['https://api.lb-0.h.stardust-mainnet.iotaledger.net:8443'],
+        [NetworkId.Iota]: ['https://api.stardust-mainnet.iotaledger.net:8443'],
         [NetworkId.IotaAlphanet]: ['https://api.iota-alphanet.iotaledger.net'],
         [NetworkId.Shimmer]: ['https://api.shimmer.network'],
         [NetworkId.Testnet]: ['https://api.testnet.shimmer.network'],
