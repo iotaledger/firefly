3fd61fec077346d3cfcafbba65728e7e02112c8f
cpl121 <100352899+cpl121@users.noreply.github.com>
2023-09-15 17:22:43 +0200

    feat: update network information when creating a ledger profile using iota (#237)
    * feat: update network information when creating a ledger profile using iota
    
    * fix: update network names to be compatible with shimmer code too
    
    * fix: remove debris
    
    * fix: determineLedgerConnectionState choose network depending on the profile
    
    * feat: update network information
    
    * fix: test
    
    * feat: add ledgerAppName to store
    
    * feat: add ledger app name in literal
    
    * fix: ledgerAppName store
    
    * feat: improve enableLedgerBlindSigning network name
    
    * feat: update locale to not harcode "IOTA" namings
    
    ---------
    
    Co-authored-by: Begoña Álvarez de la Cruz <balvarez@boxfish.studio>
    Co-authored-by: Marc Espín <mespinsanz@gmail.com>

--- packages/desktop/views/onboarding/views/create-from-ledger/views/InstallLedgerView.svelte
+++ packages/desktop/views/onboarding/views/create-from-ledger/views/InstallLedgerView.svelte
@@ -2,8 +2,8 @@
     import { PopupId, openPopup } from '@auxiliary/popup'
     import { OnboardingLayout } from '@components'
     import { localize } from '@core/i18n'
-    import { LedgerAppName, pollLedgerNanoStatus, stopPollingLedgerNanoStatus } from '@core/ledger'
-    import { Button, Icon, LedgerAnimation, Link, Text } from '@ui'
+    import { ledgerAppName, pollLedgerNanoStatus, stopPollingLedgerNanoStatus } from '@core/ledger'
+    import { Button, Icon, LedgerAnimation, Link, Text, TextType } from '@ui'
     import { onMount } from 'svelte'
     import { createFromLedgerRouter } from '../create-from-ledger-router'
     import { Icon as IconEnum } from '@auxiliary/icon'
@@ -31,30 +31,30 @@
 
 <OnboardingLayout {onBackClick}>
     <div slot="leftpane__content">
-        <Text type="h2" classes="mb-5"
-            >{localize('views.ledgerInstallationGuide.title', { values: { network: LedgerAppName.Shimmer } })}</Text
+        <Text type={TextType.h2} classes="mb-5"
+            >{localize('views.ledgerInstallationGuide.title', { values: { network: $ledgerAppName } })}</Text
         >
-        <Text type="p" secondary classes="mb-5">
-            {localize('views.ledgerInstallationGuide.body1', { values: { network: LedgerAppName.Shimmer } })}
+        <Text type={TextType.p} secondary classes="mb-5">
+            {localize('views.ledgerInstallationGuide.body1', { values: { network: $ledgerAppName } })}
         </Text>
-        <Text type="p" secondary classes="mb-5">{localize('views.ledgerInstallationGuide.body2')}</Text>
+        <Text type={TextType.p} secondary classes="mb-5">{localize('views.ledgerInstallationGuide.body2')}</Text>
         <div class="flex flex-row flex-nowrap items-center justify-center space-x-4 text-center mt-28">
             <div class="flex flex-col flex-wrap space-y-2">
                 <div class="bg-blue-400 rounded-2xl w-20 h-20 flex justify-center items-center">
-                    <Icon icon={IconEnum.Shimmer} width="32" height="32" classes="text-white" />
+                    <Icon icon={IconEnum.Iota} width="32" height="32" classes="text-white" />
                 </div>
-                <Text type="p" secondary>{LedgerAppName.Shimmer}</Text>
+                <Text type={TextType.p} secondary>{$ledgerAppName}</Text>
             </div>
         </div>
     </div>
     <div slot="leftpane__action">
         <span class="flex justify-center mb-10">
             <Link icon={IconEnum.Info} on:click={onPopupOpenClick}
-                >{localize('popups.ledgerAppGuide.title', { values: { legacy: LedgerAppName.Shimmer } })}</Link
+                >{localize('popups.ledgerAppGuide.title', { values: { legacy: $ledgerAppName } })}</Link
             >
         </span>
         <Button classes="w-full" onClick={onContinueClick}
-            >{localize('views.ledgerInstallationGuide.action', { values: { network: LedgerAppName.Shimmer } })}</Button
+            >{localize('views.ledgerInstallationGuide.action', { values: { network: $ledgerAppName } })}</Button
         >
     </div>
     <LedgerAnimation slot="rightpane" illustration={IllustrationEnum.LedgerInstallNewAppDesktop} />
