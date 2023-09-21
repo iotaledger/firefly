9c0b791bd143b1120a5aaf7feb07fd655d7015e4
cpl121 <cpeon@boxfish.studio>
2023-09-21 16:03:09 +0200

    feat: add the ShimmerEVM details

--- packages/shared/lib/core/network/constants/default-chain-configurations.constant.ts
+++ packages/shared/lib/core/network/constants/default-chain-configurations.constant.ts
@@ -4,10 +4,17 @@ import { ChainId, ChainType, NetworkId } from '../enums'
 import { ChainMetadata } from '../types'
 
 export const DEFAULT_CHAIN_CONFIGURATIONS: Readonly<{ [id in NetworkId]?: ChainMetadata }> = {
+    [NetworkId.Shimmer]: {
+        type: ChainType.Iscp,
+        name: DestinationNetwork.ShimmerEvm,
+        chainId: ChainId.ShimmerEVM,
+        aliasAddress: 'rms1pr75wa5xuepg2hew44vnr28wz5h6n6x99zptk2g68sp2wuu2karywgrztx3', // TODO: pending modification
+        iscpEndpoint: 'https://json-rpc.evm.testnet.shimmer.network', // TODO: pending modification
+    },
     [NetworkId.Testnet]: {
         type: ChainType.Iscp,
         name: DestinationNetwork.ShimmerEvmTestnet,
-        chainId: ChainId.ShimmerEVM,
+        chainId: ChainId.ShimmerEVMTesnet,
         aliasAddress: 'rms1pr75wa5xuepg2hew44vnr28wz5h6n6x99zptk2g68sp2wuu2karywgrztx3',
         iscpEndpoint: 'https://json-rpc.evm.testnet.shimmer.network',
     },
