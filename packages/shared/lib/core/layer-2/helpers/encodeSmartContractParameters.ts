e607182372829544e6e3b69aee3c14ceece65619
Begoña Alvarez <balvarez@boxfish.studio>
2023-09-22 16:21:37 +0200

    feat: add new encoding

--- packages/shared/lib/core/layer-2/helpers/encodeSmartContractParameters.ts
+++ packages/shared/lib/core/layer-2/helpers/encodeSmartContractParameters.ts
@@ -1,7 +1,7 @@
 import { Converter } from '@core/utils'
 import { SpecialStream } from '../classes'
 
-export function encodeSmartContractParameters(parameters: [string, string][]): Uint8Array {
+export function encodeSmartContractParameters(parameters: [string, Uint8Array][]): Uint8Array {
     const encodedParameters = new SpecialStream()
     encodedParameters.writeUInt32SpecialEncoding('parametersLength', parameters.length)
 
@@ -12,29 +12,8 @@ export function encodeSmartContractParameters(parameters: [string, string][]): U
         encodedParameters.writeUInt32SpecialEncoding('keyLength', key.length)
         encodedParameters.writeBytes('keyBytes', keyBytes.length, keyBytes)
 
-        // const valueBytes = Converter.hexToBytes(value)
-        const evmAddressToAgentIdBuffer = evmAddressToAgentID(value)
-
-        encodedParameters.writeUInt32SpecialEncoding('valueLength', evmAddressToAgentIdBuffer.length)
-        encodedParameters.writeUint8Array('valueBytes', evmAddressToAgentIdBuffer)
+        encodedParameters.writeUInt32SpecialEncoding('valueLength', value.length)
+        encodedParameters.writeBytes('valueBytes', value.length, value)
     }
     return encodedParameters.finalBytes()
-}
-
-function evmAddressToAgentID(evmStoreAccount: string): Uint8Array {
-    // This function constructs an AgentID that is required to be used with contracts
-    // Wasp understands different AgentID types and each AgentID needs to provide a certain ID that describes it's address type.
-    // In the case of EVM addresses it's ID 3.
-    const agentIDKindEthereumAddress = 3
-
-    // Note: we need the evmStoreAccount to be in lower case,
-    // otherwise fetching balances using the iscmagic contract will fail,
-    // because evm addresses are case-insensitive but hexToBytes is not.
-    const receiverAddrBinary = Converter.hexToBytes(evmStoreAccount?.toLowerCase())
-    const aliasAddress = Converter.hexToBytes('rms1ppnkvsjctdg53v2x89uzhuxg89s073jmn2nuzcw44tggjy8rzzgzq2rg0qp')
-    const aliasAddressInBytes = new Uint8Array([0, ...aliasAddress])
-
-    const agentIdBytes = new Uint8Array([agentIDKindEthereumAddress, ...aliasAddressInBytes, ...receiverAddrBinary])
-
-    return agentIdBytes
 }
--- packages/shared/lib/core/layer-2/utils/getLayer2MetadataForTransfer.ts
+++ packages/shared/lib/core/layer-2/utils/getLayer2MetadataForTransfer.ts
@@ -1,10 +1,11 @@
-import { encodeAddress, encodeAssetAllowance, encodeSmartContractParameters } from '../helpers'
-import BigInteger from 'big-integer'
 import type { NewTransactionDetails } from '@core/wallet/types'
 import { getAddressFromSubject } from '@core/wallet/utils'
+import BigInteger from 'big-integer'
 import { SpecialStream } from '../classes'
 import { ACCOUNTS_CONTRACT, EXTERNALLY_OWNED_ACCOUNT, GAS_BUDGET, TRANSFER_ALLOWANCE } from '../constants'
+import { encodeAddress, encodeAssetAllowance, encodeSmartContractParameters } from '../helpers'
 import { getEstimatedGasForTransferFromTransactionDetails } from './getEstimatedGasForTransferFromTransactionDetails'
+import { evmAddressToAgentID } from './'
 // import { getActiveNetworkId } from '@core/network/utils/getNetworkId'
 // import { NetworkId } from '@core/network'
 // import { Converter } from '@iota/util.js'
@@ -16,17 +17,16 @@ export async function getLayer2MetadataForTransfer(transactionDetails: NewTransa
     const encodedAddress = encodeAddress(address.toLowerCase())
 
     const estimatedGas = await getEstimatedGasForTransferFromTransactionDetails()
-    // if (getActiveNetworkId() === NetworkId.Shimmer) {
-    metadataStream.writeUInt8('senderContract', EXTERNALLY_OWNED_ACCOUNT)
 
-    // } else {
-    //     metadataStream.writeUInt32('senderContract', EXTERNALLY_OWNED_ACCOUNT)
-    // }
+    metadataStream.writeUInt8('senderContract', EXTERNALLY_OWNED_ACCOUNT)
     metadataStream.writeUInt32('targetContract', ACCOUNTS_CONTRACT)
     metadataStream.writeUInt32('contractFunction', TRANSFER_ALLOWANCE)
     metadataStream.writeUInt64SpecialEncoding('gasBudget', BigInteger(estimatedGas) ?? GAS_BUDGET)
 
-    const smartContractParameters = Object.entries({ a: encodedAddress })
+    const aliasAddress = 'rms1ppnkvsjctdg53v2x89uzhuxg89s073jmn2nuzcw44tggjy8rzzgzq2rg0qp'
+    const evmAddressToAgentIdBuffer = evmAddressToAgentID(encodedAddress, aliasAddress)
+
+    const smartContractParameters = Object.entries({ a: evmAddressToAgentIdBuffer })
     const parameters = encodeSmartContractParameters(smartContractParameters)
     metadataStream.writeBytes('smartContractParameters', parameters.length, parameters)
 
