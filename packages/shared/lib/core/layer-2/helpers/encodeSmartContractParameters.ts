7e1d50f155a739cb26cbb5cca1f241e1b8f35f91
cpl121 <cpeon@boxfish.studio>
2023-09-22 13:31:08 +0200

    feat: add new encoding WIP

--- packages/shared/lib/core/layer-2/helpers/encodeSmartContractParameters.ts
+++ packages/shared/lib/core/layer-2/helpers/encodeSmartContractParameters.ts
@@ -12,9 +12,29 @@ export function encodeSmartContractParameters(parameters: [string, string][]): U
         encodedParameters.writeUInt32SpecialEncoding('keyLength', key.length)
         encodedParameters.writeBytes('keyBytes', keyBytes.length, keyBytes)
 
-        const valueBytes = Converter.hexToBytes(value)
-        encodedParameters.writeUInt32SpecialEncoding('valueLength', valueBytes.length)
-        encodedParameters.writeBytes('valueBytes', valueBytes.length, valueBytes)
+        // const valueBytes = Converter.hexToBytes(value)
+        const evmAddressToAgentIdBuffer = evmAddressToAgentID(value)
+
+        encodedParameters.writeUInt32SpecialEncoding('valueLength', evmAddressToAgentIdBuffer.length)
+        encodedParameters.writeUint8Array('valueBytes', evmAddressToAgentIdBuffer)
     }
     return encodedParameters.finalBytes()
+}
+
+function evmAddressToAgentID(evmStoreAccount: string): Uint8Array {
+    // This function constructs an AgentID that is required to be used with contracts
+    // Wasp understands different AgentID types and each AgentID needs to provide a certain ID that describes it's address type.
+    // In the case of EVM addresses it's ID 3.
+    const agentIDKindEthereumAddress = 3
+
+    // Note: we need the evmStoreAccount to be in lower case,
+    // otherwise fetching balances using the iscmagic contract will fail,
+    // because evm addresses are case-insensitive but hexToBytes is not.
+    const receiverAddrBinary = Converter.hexToBytes(evmStoreAccount?.toLowerCase())
+    const aliasAddress = Converter.hexToBytes('rms1ppnkvsjctdg53v2x89uzhuxg89s073jmn2nuzcw44tggjy8rzzgzq2rg0qp')
+    const aliasAddressInBytes = new Uint8Array([0, ...aliasAddress])
+
+    const agentIdBytes = new Uint8Array([agentIDKindEthereumAddress, ...aliasAddressInBytes, ...receiverAddrBinary])
+
+    return agentIdBytes
 }
--- packages/shared/lib/core/layer-2/utils/getLayer2MetadataForTransfer.ts
+++ packages/shared/lib/core/layer-2/utils/getLayer2MetadataForTransfer.ts
@@ -5,6 +5,9 @@ import { getAddressFromSubject } from '@core/wallet/utils'
 import { SpecialStream } from '../classes'
 import { ACCOUNTS_CONTRACT, EXTERNALLY_OWNED_ACCOUNT, GAS_BUDGET, TRANSFER_ALLOWANCE } from '../constants'
 import { getEstimatedGasForTransferFromTransactionDetails } from './getEstimatedGasForTransferFromTransactionDetails'
+// import { getActiveNetworkId } from '@core/network/utils/getNetworkId'
+// import { NetworkId } from '@core/network'
+// import { Converter } from '@iota/util.js'
 
 export async function getLayer2MetadataForTransfer(transactionDetails: NewTransactionDetails): Promise<string> {
     const metadataStream = new SpecialStream()
@@ -13,8 +16,12 @@ export async function getLayer2MetadataForTransfer(transactionDetails: NewTransa
     const encodedAddress = encodeAddress(address.toLowerCase())
 
     const estimatedGas = await getEstimatedGasForTransferFromTransactionDetails()
+    // if (getActiveNetworkId() === NetworkId.Shimmer) {
+    metadataStream.writeUInt8('senderContract', EXTERNALLY_OWNED_ACCOUNT)
 
-    metadataStream.writeUInt32('senderContract', EXTERNALLY_OWNED_ACCOUNT)
+    // } else {
+    //     metadataStream.writeUInt32('senderContract', EXTERNALLY_OWNED_ACCOUNT)
+    // }
     metadataStream.writeUInt32('targetContract', ACCOUNTS_CONTRACT)
     metadataStream.writeUInt32('contractFunction', TRANSFER_ALLOWANCE)
     metadataStream.writeUInt64SpecialEncoding('gasBudget', BigInteger(estimatedGas) ?? GAS_BUDGET)
