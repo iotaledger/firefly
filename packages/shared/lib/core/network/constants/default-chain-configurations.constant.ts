19a02184a4460c6644a17a2c3ee4ea44b539b202
Begoña Alvarez <balvarez@boxfish.studio>
2023-09-26 12:38:43 +0200

    chore: undo what shouldnt go to develop

--- packages/shared/lib/core/network/constants/default-chain-configurations.constant.ts
+++ packages/shared/lib/core/network/constants/default-chain-configurations.constant.ts
@@ -4,20 +4,11 @@ import { ChainId, ChainType, NetworkId } from '../enums'
 import { ChainMetadata } from '../types'
 
 export const DEFAULT_CHAIN_CONFIGURATIONS: Readonly<{ [id in NetworkId]?: ChainMetadata }> = {
-    [NetworkId.Shimmer]: {
-        type: ChainType.Iscp,
-        name: DestinationNetwork.ShimmerEvm,
-        chainId: ChainId.ShimmerEVM,
-        aliasAddress: 'rms1ppnkvsjctdg53v2x89uzhuxg89s073jmn2nuzcw44tggjy8rzzgzq2rg0qp', // TODO: pending modification
-        iscpEndpoint:
-            'https://7-teamnet.chrysalis2.com/wasp/api/v1/chains/rms1ppnkvsjctdg53v2x89uzhuxg89s073jmn2nuzcw44tggjy8rzzgzq2rg0qp', // TODO: pending modification
-    },
     [NetworkId.Testnet]: {
         type: ChainType.Iscp,
         name: DestinationNetwork.ShimmerEvmTestnet,
-        chainId: ChainId.ShimmerEVMTesnet,
-        aliasAddress: 'rms1ppnkvsjctdg53v2x89uzhuxg89s073jmn2nuzcw44tggjy8rzzgzq2rg0qp', // TODO: pending modification
-        iscpEndpoint:
-            'https://7-teamnet.chrysalis2.com/wasp/api/v1/chains/rms1ppnkvsjctdg53v2x89uzhuxg89s073jmn2nuzcw44tggjy8rzzgzq2rg0qp', // TODO: pending modification
+        chainId: ChainId.ShimmerEVM,
+        aliasAddress: 'rms1pr75wa5xuepg2hew44vnr28wz5h6n6x99zptk2g68sp2wuu2karywgrztx3',
+        iscpEndpoint: 'https://json-rpc.evm.testnet.shimmer.network',
     },
 }
