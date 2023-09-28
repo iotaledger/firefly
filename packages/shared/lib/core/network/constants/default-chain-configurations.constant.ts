2b96525b66d9419fa469c0708a46e9e1e7546e82
Branko Bosnic <brankobosnic1@gmail.com>
2023-09-28 11:07:56 +0200

    chore: add shimmer evm

--- packages/shared/lib/core/network/constants/default-chain-configurations.constant.ts
+++ packages/shared/lib/core/network/constants/default-chain-configurations.constant.ts
@@ -1,4 +1,15 @@
-import { NetworkId } from '../enums'
+import { DestinationNetwork } from '@core/layer-2/enums'
+
+import { ChainId, ChainType, NetworkId } from '../enums'
 import { ChainMetadata } from '../types'
 
-export const DEFAULT_CHAIN_CONFIGURATIONS: Readonly<{ [id in NetworkId]?: ChainMetadata }> = {}
+export const DEFAULT_CHAIN_CONFIGURATIONS: Readonly<{ [id in NetworkId]?: ChainMetadata }> = {
+    [NetworkId.Shimmer]: {
+        type: ChainType.Iscp,
+        name: DestinationNetwork.ShimmerEvm,
+        chainId: ChainId.ShimmerEVM,
+        aliasAddress: 'smr1prxvwqvwf7nru5q5xvh5thwg54zsm2y4wfnk6yk56hj3exxkg92mx20wl3s',
+        iscpEndpoint:
+            'https://api.evm.shimmer.network/v1/chains/smr1prxvwqvwf7nru5q5xvh5thwg54zsm2y4wfnk6yk56hj3exxkg92mx20wl3s',
+    },
+}
