952a6795775905aa16ac711c21f83fde03caec9a
marc2332 <mespinsanz@gmail.com>
2023-10-10 09:00:54 +0200

    fmt

--- packages/desktop/electron.vite.config.ts
+++ packages/desktop/electron.vite.config.ts
@@ -90,7 +90,7 @@ export default defineConfig({
         },
         build: {
             target: 'esnext',
-            minify: false
+            minify: false,
         },
         define,
         plugins: [
