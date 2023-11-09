b3204a5cf84728bc8da04ee5749ba5e8dba319c4
marc2332 <mespinsanz@gmail.com>
2023-11-09 10:59:41 +0100

    fix: Proper evm address decoding

--- packages/shared/lib/core/layer-2/utils/parseLayer2MetadataForTransfer.ts
+++ packages/shared/lib/core/layer-2/utils/parseLayer2MetadataForTransfer.ts
@@ -11,8 +11,7 @@ export function parseLayer2MetadataForTransfer(metadata: Uint8Array): ILayer2Tra
     const targetContract = readStream.readUInt32('targetContract')
     const contractFunction = readStream.readUInt32('contractFunction')
     const gasBudget = readStream.readUInt64SpecialEncoding('gasBudget')
-    const smartContractParameters = parseSmartContractParameters(readStream)
-    const ethereumAddress = '0x' + smartContractParameters['a'].substring(4)
+    const ethereumAddress = parseEvmAddressFromAgentId(readStream)
     const allowance = parseAssetAllowance(readStream)
 
     return {
@@ -25,6 +24,11 @@ export function parseLayer2MetadataForTransfer(metadata: Uint8Array): ILayer2Tra
         nativeTokens: allowance?.nativeTokens,
         nfts: allowance?.nfts,
     }
+}
+
+function parseEvmAddressFromAgentId(readStream: ReadSpecialStream): string {
+    const smartContractParameters = parseSmartContractParameters(readStream)
+    return '0x' + smartContractParameters['a'].slice(-40)
 }
 
 function parseSmartContractParameters(readStream: ReadSpecialStream): Record<string, string> {
