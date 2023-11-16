a57d574205e449ba9998b2040fd72047912b8a76
Begoña Alvarez <balvarez@boxfish.studio>
2023-11-16 10:31:31 +0100

    test: add long base token test

--- packages/shared/lib/core/layer-2/tests/parseLayer2MetadataForTransfer.test.ts
+++ packages/shared/lib/core/layer-2/tests/parseLayer2MetadataForTransfer.test.ts
@@ -20,6 +20,24 @@ describe('Function: parseLayer2MetadataForTransfer.ts', () => {
         expect(parsedMetadata).toEqual(expected)
     })
 
+    it('should correctly parse metadata with long base token', () => {
+        const metadata =
+            '0x00025e4b3ca1e3f423914e010161350342f7da9bdb55b3ec87e5ac1a1e6d88e16768663fde5eca3429eb6f579cc538acb82a77d6f89dae4611b81eac279fbf96d322001f80ff9f94a58d1d'
+        const metadataByteArray = Converter.hexToBytes(metadata)
+        const expected = {
+            senderContract: '0x0',
+            targetContract: 'Accounts',
+            contractFunction: 'transferAllowanceTo',
+            gasBudget: '10000',
+            ethereumAddress: '0xb82a77d6f89dae4611b81eac279fbf96d322001f',
+            baseTokens: '999999999999',
+            nativeTokens: [],
+            nfts: [],
+        }
+        const parsedMetadata = parseLayer2MetadataForTransfer(metadataByteArray)
+        expect(parsedMetadata).toEqual(expected)
+    })
+
     it('should correctly parse metadata with native tokens', () => {
         const metadata =
             '0x00025e4b3ca1e3f423914e010161350342f7da9bdb55b3ec87e5ac1a1e6d88e16768663fde5eca3429eb6f579cc538acb82a77d6f89dae4611b81eac279fbf96d322001f4001086ac702fcfdc37b437e7ebb7a87d8acfb875be6b1ae3823bc61aa7896b852a6d5010000000001fa'
