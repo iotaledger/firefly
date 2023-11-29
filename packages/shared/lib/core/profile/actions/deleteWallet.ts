3fd299af9771b082dc6ca91096dcd5e43c868602
marc2332 <mespinsanz@gmail.com>
2023-11-29 10:24:15 +0100

    clean up `deleteWallet`

--- packages/shared/lib/core/profile/actions/deleteWallet.ts
+++ packages/shared/lib/core/profile/actions/deleteWallet.ts
@@ -8,18 +8,18 @@ import { CannotRemoveWalletError, RemoveNotLastWalletError, setSelectedWallet } 
 
 // TODO(2.0): replace all its usage, before it was numeric index, now it's id
 export async function deleteWallet(id: string): Promise<void> {
-    const accounts = get(visibleActiveWallets)
-    const accountToBeDeleted = accounts.find((account) => account?.id === id)
+    const wallets = get(visibleActiveWallets)
+    const walletToBeDeleted = wallets.find((wallet) => wallet?.id === id)
  
 
-    if (accountToBeDeleted !== accounts[accounts.length - 1]) {
+    if (walletToBeDeleted !== wallets[wallets.length - 1]) {
         throw new RemoveNotLastWalletError()
     }
 
     try {
         await api.deleteWallet(id)
         // TODO(2.0) do we need this?: removeAccountFromActiveAccounts(id)
-        setSelectedWallet(accounts[0]?.id ?? null)
+        setSelectedWallet(wallets[0]?.id ?? null)
         get(routerManager).resetRouterForAppContext(AppContext.Dashboard)
     } catch (err) {
         throw new CannotRemoveWalletError()
