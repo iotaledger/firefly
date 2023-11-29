4577abfc6bcbf174cde3cd8420d25e5cb146489e
Mario Sarcevic <mario.sarcevic@iota.org>
2023-11-29 11:26:49 +0100

    fix: Fix the chronicle URL constants for all networks

--- packages/shared/lib/core/network/constants/chronicle-urls.constant.ts
+++ packages/shared/lib/core/network/constants/chronicle-urls.constant.ts
@@ -2,8 +2,8 @@ import { NetworkId } from '../enums'
 
 export const CHRONICLE_URLS: Readonly<{ [key in NetworkId]?: string[] }> = {
     [NetworkId.Iota]: ['https://chronicle.stardust-mainnet.iotaledger.net/'],
-    [NetworkId.IotaAlphanet]: ['https://chronicle.alphanet.iotaledger.net'],
-    [NetworkId.Shimmer]: ['https://chronicle.shimmer.network'],
+    [NetworkId.IotaAlphanet]: ['https://chronicle.alphanet.iotaledger.net/'],
+    [NetworkId.Shimmer]: ['https://chronicle.shimmer.network/'],
     [NetworkId.Testnet]: ['https://chronicle.testnet.shimmer.network/'],
 }
 
