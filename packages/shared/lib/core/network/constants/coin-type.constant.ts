1072ef2e8a1536dea5f5886ad397cbdcbed56a3a
Begoña Alvarez <balvarez@boxfish.studio>
2023-12-01 12:15:39 +0100

    test: apply changes to test legacy migration

--- packages/shared/lib/core/network/constants/coin-type.constant.ts
+++ packages/shared/lib/core/network/constants/coin-type.constant.ts
@@ -2,7 +2,7 @@ import { NetworkId } from '../enums'
 
 export const COIN_TYPE: Readonly<{ [key in NetworkId]: number }> = {
     [NetworkId.Iota]: 4218,
-    [NetworkId.IotaAlphanet]: 1,
+    [NetworkId.IotaAlphanet]: 4218,
     [NetworkId.Shimmer]: 4219,
     [NetworkId.Testnet]: 1,
     [NetworkId.Custom]: 1,
