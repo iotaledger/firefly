027b69d3fb8723312c658ec5b8d66466a84a3a08
marc2332 <mespinsanz@gmail.com>
2023-12-22 13:35:02 +0100

    clean up and fix

--- packages/shared/lib/contexts/onboarding/stores/onboarding-secret-manager.store.ts
+++ packages/shared/lib/contexts/onboarding/stores/onboarding-secret-manager.store.ts
@@ -1,6 +1,7 @@
-import { onboardingProfile } from '@contexts/onboarding/stores'
+import { onboardingProfile, updateOnboardingProfile } from '@contexts/onboarding/stores'
 import { api } from '@core/api'
 import { SecretManager } from '@iota/sdk'
+import { verifyMnemonic } from 'shared/lib/core/secret-manager'
 import { get, writable, Writable } from 'svelte/store'
 
 export const onboardingProfileSecretManager: Writable<SecretManager | null> = writable(null)
@@ -8,18 +9,29 @@ export const onboardingProfileSecretManager: Writable<SecretManager | null> = wr
 export async function buildOnboardingSecretManager(): Promise<void> {
     const profile = get(onboardingProfile)
     if (profile) {
-        const { strongholdPassword, secretManagerOptions } = profile
-        console.log(secretManagerOptions, strongholdPassword)
-        // Create secret manager with persisted options
-        const secretManager = await api.createSecretManager(secretManagerOptions!)
+        const { strongholdPassword, secretManagerOptions, mnemonic } = profile
+        const mnemonicStringified = mnemonic?.join(' ') ?? ''
 
-        console.log(secretManager)
-        // Load the stronghold password specified in the onboarding if necessary
+        // 1. Create SecretManager
+        const secretManager = await api.createSecretManager(secretManagerOptions!)
+        
+        // 2. Load the stronghold password specified in the onboarding if necessary
         if(strongholdPassword){
             await secretManager.setStrongholdPassword(strongholdPassword);
-            console.log("setted strongholdPassword")
         }
+      
+        // 3. Verify Mnemonic
+        await verifyMnemonic(mnemonicStringified);
 
+        // 4. Store Mnemonic
+        await secretManager.storeMnemonic(mnemonicStringified)
+
+        // 5. Generate address
+        const address = (await secretManager.generateEd25519Addresses({}))[0];
+
+        updateOnboardingProfile({
+            address
+        })
         onboardingProfileSecretManager.set(secretManager)
     } else {
         onboardingProfileSecretManager.set(null)
