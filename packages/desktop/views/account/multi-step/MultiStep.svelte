c3d054a472b895c6778d7da3b6f6dd4004516322
evavirseda <evirseda@boxfish.studio>
2023-12-27 11:53:17 +0100

    feat: prepare UI step 2

--- packages/desktop/views/account/multi-step/MultiStep.svelte
+++ packages/desktop/views/account/multi-step/MultiStep.svelte
@@ -24,14 +24,14 @@
 
 {#if activeComponent}
     <svelte:component this={activeComponent} {activeState} {onNext} />
-    <!-- TODO: Remove this button, it's just to allow go to next step -->
-    {#if currentStep < IMPLICIT_ACCOUNT_STEPS.length - 1}
-        <button on:click={handleNext}>Next</button>
-    {/if}
     <!-- Dots to show progress -->
     <div class="flex flex-row justify-center space-x-2.5">
         {#each IMPLICIT_ACCOUNT_STEPS as step, index}
-            <div class="w-2.5 h-2.5 rounded-full {activeState === step ? 'bg-blue-500' : 'bg-blue-200'}" />
+            <!-- svelte-ignore a11y-click-events-have-key-events -->
+            <div
+                on:click={handleNext}
+                class="w-2.5 h-2.5 rounded-full {activeState === step ? 'bg-blue-500' : 'bg-blue-200'}"
+            />
         {/each}
     </div>
 {/if}
