f4a90f55332dd528544a42eaa7288f1e92c5be9d
evavirseda <evirseda@boxfish.studio>
2024-01-11 15:30:08 +0100

    fix: move if/else and key block

--- packages/desktop/views/dashboard/wallet/Wallet.svelte
+++ packages/desktop/views/dashboard/wallet/Wallet.svelte
@@ -1,16 +1,21 @@
 <script lang="ts">
+    import { selectedWallet } from '@core/wallet'
     import { ImplicitAccountCreationView, WalletView } from './views'
 
     const hasAccount: boolean = false
 </script>
 
-<wallet-container
-    class="w-full h-full flex flex-nowrap p-8 relative flex-1
+{#if $selectedWallet}
+    {#key $selectedWallet?.index}
+        <wallet-container
+            class="w-full h-full flex flex-nowrap p-8 relative flex-1
 bg-gray-50 dark:bg-gray-900 justify-center items-center"
->
-    {#if hasAccount}
-        <WalletView />
-    {:else}
-        <ImplicitAccountCreationView />
-    {/if}
-</wallet-container>
+        >
+            {#if hasAccount}
+                <WalletView />
+            {:else}
+                <ImplicitAccountCreationView />
+            {/if}
+        </wallet-container>
+    {/key}
+{/if}
