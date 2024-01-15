071bffcfc396ddd157884539007be793411f079b
marc2332 <mespinsanz@gmail.com>
2024-01-15 18:33:09 +0100

    format

--- packages/shared/lib/core/wallet/actions/setStrongholdPassword.ts
+++ packages/shared/lib/core/wallet/actions/setStrongholdPassword.ts
@@ -3,13 +3,13 @@ import { getSelectedWallet } from '../stores/selected-wallet.store'
 
 export async function setStrongholdPassword(password: string): Promise<void> {
     // Set in Wallet
-    const wallet = getSelectedWallet();
+    const wallet = getSelectedWallet()
     // Otherwise error is thrown, if password is still present in memory
     await wallet?.clearStrongholdPassword()
     await wallet?.setStrongholdPassword(password)
 
     // Set in SecretManager
-    const secretManager = getSecretManager();
-    await secretManager.setStrongholdPassword(password);
+    const secretManager = getSecretManager()
+    await secretManager.setStrongholdPassword(password)
     // await secretManager?.clearStrongholdPassword() // TODO(2.0)
 }
