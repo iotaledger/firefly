4ca6d8be85f020ba3d336e3e7123724dca500410
cpl121 <cpeon@boxfish.studio>
2024-01-16 13:38:52 +0100

    fix: move onDestroy to the last view of implicit account creation

--- packages/desktop/views/dashboard/wallet/views/implicit-account-creation-multistep/AccountCreationView.svelte
+++ packages/desktop/views/dashboard/wallet/views/implicit-account-creation-multistep/AccountCreationView.svelte
@@ -1,7 +1,9 @@
 <script lang="ts">
+    import { onDestroy } from 'svelte'
     import { Button, FontWeight, PasswordInput, Text, TextType } from 'shared/components'
     import { localize } from '@core/i18n'
     import { showAppNotification } from '@auxiliary/notification'
+    import { unsubscribeFromWalletApiEvents } from '@core/wallet'
 
     let error = ''
     let strongholdPassword = ''
@@ -15,6 +17,8 @@
             message: localize('error.global.generic'),
         })
     }
+
+    onDestroy(unsubscribeFromWalletApiEvents)
 </script>
 
 <step-content class="flex flex-col items-center justify-between h-full pt-28 w-full">
