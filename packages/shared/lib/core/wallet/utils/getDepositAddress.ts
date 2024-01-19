faca71107e6f073b59822df98372baf28ee5deb2
evavirseda <evirseda@boxfish.studio>
2024-01-19 13:21:46 +0100

    fix: update depositAddress

--- packages/shared/lib/core/wallet/utils/getDepositAddress.ts
+++ packages/shared/lib/core/wallet/utils/getDepositAddress.ts
@@ -1,21 +1,14 @@
-import { AccountOutput, BlockIssuerFeature, FeatureType } from '@iota/sdk'
-import type { Ed25519BlockIssuerKey } from '@iota/sdk/out/types/block/output/block-issuer-key'
-import { selectedWallet } from '../stores'
 import { get } from 'svelte/store'
+import { selectedWallet } from '../stores'
+import { AccountOutput } from '@iota/sdk'
 
 // TODO(2.0): Update the implementation to handle multiple accounts and use the selected account's address.
 export async function getDepositAddress(): Promise<string> {
     const wallet = get(selectedWallet)
     const accounts = await wallet?.accounts()
     if (accounts?.length) {
-        const depositAddress =
-            (
-                (
-                    (accounts[0]?.output as AccountOutput).features?.find(
-                        (feat) => feat?.type === FeatureType.BlockIssuer
-                    ) as BlockIssuerFeature
-                )?.blockIssuerKeys[0] as Ed25519BlockIssuerKey
-            )?.publicKey ?? ''
+        // TODO: when we have selectedAccount in the store,  we need to find the account with the blockIssuer feature
+        const depositAddress = (accounts[0]?.output as AccountOutput).accountId ?? ''
         return depositAddress
     } else {
         return ''
