e1b9b88395b25d2100e9f169632c0075980eee5e
Marc Espin <mespinsanz@gmail.com>
2024-02-01 18:47:54 +0100

    feat: Profile-centric wallet file scheme for persisted data (#7942)
    * refactor(2.0): Dynamic password
    
    * refactor(2.0): Remove Profile Manager (wip)
    
    * clean up and fix
    
    * clean up
    
    * clean up more code
    
    * clean up
    
    * adapt and remove for profile-manager related code
    
    * clean up more code
    
    * fmt
    
    * more cleanup
    
    * more cleanup
    
    * small tweaks
    
    * more cleanup
    
    * format
    
    * update
    
    * clean up
    
    * https://api.testnet.shimmer.network
    
    * revert Wallet.svelte
    
    * clean up
    
    * remove account debris
    
    * feat: Profile-centric file scheme for wallets
    
    * chore: Clean up 2.0 comments
    
    * more
    
    * more
    
    * more
    
    * works
    
    * clean up
    
    * clean up
    
    * clean up
    
    * chore: Adapt to sdk changes
    
    * fmt
    
    ---------
    
    Co-authored-by: Begoña Álvarez de la Cruz <balvarez@boxfish.studio>

--- packages/shared/lib/core/wallet/actions/createNewWallet.ts
+++ packages/shared/lib/core/wallet/actions/createNewWallet.ts
@@ -4,27 +4,33 @@ import {
     addWalletPersistedDataToActiveProfile,
     addWalletToActiveWallets,
     createWallet,
+    getActiveProfile,
 } from '@core/profile'
 import { get } from 'svelte/store'
-
 import { DEFAULT_SYNC_OPTIONS } from '@core/wallet/constants'
 import { IWalletState } from '@core/wallet/interfaces'
-
 import { buildWalletStateAndPersistedData } from './buildWalletStateAndPersistedData'
 import { addEmptyWalletActivitiesToAllWalletActivities } from '../stores'
 
 export async function createNewWallet(name?: string, color?: string): Promise<IWalletState> {
+    const activeProfile = getActiveProfile()
+
     // 1. Get the wallet name
     const walletName = name || `${localize('general.wallet')} ${(get(activeWallets)?.length ?? 0) + 1}`
 
     // 2. Create the wallet instance
-    const wallet = await createWallet()
+    const wallet = await createWallet(activeProfile)
 
     // 3. Sync the wallet with the Node
     await wallet.sync(DEFAULT_SYNC_OPTIONS)
 
     // 4. Create a wrapper over the wallet instance and the persisted data
-    const [walletState, walletPersistedData] = await buildWalletStateAndPersistedData(wallet, walletName, color)
+    const [walletState, walletPersistedData] = await buildWalletStateAndPersistedData(
+        activeProfile.id,
+        wallet,
+        walletName,
+        color
+    )
 
     addWalletToActiveWallets(walletState)
     addWalletPersistedDataToActiveProfile(walletState.id, walletPersistedData)
