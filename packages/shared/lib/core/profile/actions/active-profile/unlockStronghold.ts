a4d2b7cecce1d175fa6c1aee6cb936d700ee94d1
cpl121 <cpeon@boxfish.studio>
2024-02-06 11:45:48 +0100

    feat: improve unlockStronghold to locked when unable to unlock

--- packages/shared/lib/core/profile/actions/active-profile/unlockStronghold.ts
+++ packages/shared/lib/core/profile/actions/active-profile/unlockStronghold.ts
@@ -1,4 +1,4 @@
-import { activeProfile, IProfile } from '@core/profile'
+import { activeProfile, IProfile, lockStronghold } from '@core/profile'
 import { get } from 'svelte/store'
 import { setTimeStrongholdLastUnlocked } from '@core/profile/stores'
 import { setStrongholdPassword } from '@core/wallet/actions'
@@ -10,6 +10,11 @@ export async function unlockStronghold(password: string, profile: IProfile = get
         isStrongholdLocked.set(false)
         setTimeStrongholdLastUnlocked()
     } catch (err) {
+        try {
+            await lockStronghold()
+        } catch (err) {
+            console.error(err)
+        }
         console.error(err)
         throw new Error('error.password.incorrect')
     }
