2c2607cd3549140c75fe4139f46d47501899d905
cpl121 <100352899+cpl121@users.noreply.github.com>
2024-02-06 11:47:47 +0100

    feat: refactor stronghold unlock (#7971)
    * feat: refactor stronghold unlock
    
    * feat: refactor account creation logic
    
    * feat: improve unlockStronghold to locked when unable to unlock

--- packages/shared/lib/core/profile/actions/active-profile/unlockStronghold.ts
+++ packages/shared/lib/core/profile/actions/active-profile/unlockStronghold.ts
@@ -1,18 +1,21 @@
-import { activeProfile, IProfile } from '@core/profile'
+import { activeProfile, IProfile, lockStronghold } from '@core/profile'
 import { get } from 'svelte/store'
 import { setTimeStrongholdLastUnlocked } from '@core/profile/stores'
 import { setStrongholdPassword } from '@core/wallet/actions'
 
 export async function unlockStronghold(password: string, profile: IProfile = get(activeProfile)): Promise<void> {
     const { isStrongholdLocked } = profile
-    if (get(isStrongholdLocked)) {
+    try {
+        await setStrongholdPassword(password)
+        isStrongholdLocked.set(false)
+        setTimeStrongholdLastUnlocked()
+    } catch (err) {
         try {
-            await setStrongholdPassword(password)
-            isStrongholdLocked.set(false)
-            setTimeStrongholdLastUnlocked()
+            await lockStronghold()
         } catch (err) {
             console.error(err)
-            throw new Error('error.password.incorrect')
         }
+        console.error(err)
+        throw new Error('error.password.incorrect')
     }
 }
