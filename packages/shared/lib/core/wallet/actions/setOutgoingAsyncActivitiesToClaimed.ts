079b2a022f215aebfc0c72b5c5e7d5baa7eb1bdf
cpl121 <cpeon@boxfish.studio>
2024-02-07 20:07:08 +0100

    feat: update getExpirationDateFromOutput uses

--- packages/shared/lib/core/wallet/actions/setOutgoingAsyncActivitiesToClaimed.ts
+++ packages/shared/lib/core/wallet/actions/setOutgoingAsyncActivitiesToClaimed.ts
@@ -4,6 +4,7 @@ import { get } from 'svelte/store'
 import { ActivityAsyncStatus, ActivityDirection } from '../enums'
 import { allWalletActivities, updateAsyncDataByActivityId } from '../stores'
 import { getExpirationDateFromOutput } from '../utils'
+import { getUnixTimestampFromNodeInfoAndSlotIndex, nodeInfoProtocolParameters } from '../../network'
 
 export async function setOutgoingAsyncActivitiesToClaimed(wallet: IWalletState): Promise<void> {
     const walletActivities = get(allWalletActivities)[wallet.id]
@@ -15,12 +16,18 @@ export async function setOutgoingAsyncActivitiesToClaimed(wallet: IWalletState):
     for (const activity of activities) {
         try {
             const detailedOutput = await wallet.getOutput(activity.outputId)
-            const isClaimed = detailedOutput && isOutputClaimed(detailedOutput)
-            if (isClaimed) {
-                updateAsyncDataByActivityId(wallet.id, activity.id, {
-                    asyncStatus: ActivityAsyncStatus.Claimed,
-                    claimedDate: new Date(), // TODO(2.0) Fix and use: new Date(detailedOutput.metadata.milestoneTimestampSpent * MILLISECONDS_PER_SECOND),
-                })
+            const nodeProtocolParameters = get(nodeInfoProtocolParameters)
+            if (nodeProtocolParameters && detailedOutput.metadata.spent) {
+                const claimedDate = new Date(
+                    getUnixTimestampFromNodeInfoAndSlotIndex(nodeProtocolParameters, detailedOutput.metadata.spent.slot)
+                )
+                const isClaimed = detailedOutput && isOutputClaimed(detailedOutput, claimedDate)
+                if (isClaimed && claimedDate) {
+                    updateAsyncDataByActivityId(wallet.id, activity.id, {
+                        asyncStatus: ActivityAsyncStatus.Claimed,
+                        claimedDate,
+                    })
+                }
             }
         } catch (err) {
             console.error(err)
@@ -28,16 +35,11 @@ export async function setOutgoingAsyncActivitiesToClaimed(wallet: IWalletState):
     }
 }
 
-function isOutputClaimed(output: OutputData): boolean {
+function isOutputClaimed(output: OutputData, claimedDate: Date): boolean {
     const expirationDate = getExpirationDateFromOutput(output?.output as BasicOutput)
-
     if (expirationDate) {
-        return (
-            output.isSpent &&
-            new Date().getTime() /* TODO(2.0) Fix and use: output.metadata.milestoneTimestampSpent * MILLISECONDS_PER_SECOND */ <
-                expirationDate.getTime()
-        )
+        return !!output.metadata.spent && claimedDate.getTime() < expirationDate.getTime()
     } else {
-        return output?.isSpent
+        return !!output?.metadata.spent
     }
 }
