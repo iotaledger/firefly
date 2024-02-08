ab0d61b5aab2ca6b2c821ea80d46eb561ffd78f4
evavirseda <evirseda@boxfish.studio>
2024-02-08 11:21:39 +0100

    feat: update features

--- packages/shared/lib/features/interfaces/delegation-features.interface.ts
+++ packages/shared/lib/features/interfaces/delegation-features.interface.ts
@@ -2,4 +2,6 @@ import { IFeatureFlag } from './feature-flag.interface'
 
 export interface IDelegationFeatures extends IFeatureFlag {
     delegationList?: IFeatureFlag
+    delegateOutput?: IFeatureFlag
+    claimRewards?: IFeatureFlag
 }
