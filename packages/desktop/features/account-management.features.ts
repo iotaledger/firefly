222e9418ae52dd2d149f42dbc4fd35a003fff156
evavirseda <evirseda@boxfish.studio>
2024-02-08 10:21:54 +0100

    feat: add features flag

--- packages/desktop/features/account-management.features.ts
+++ packages/desktop/features/account-management.features.ts
@@ -2,6 +2,12 @@ import { IAccountManagementFeatures } from '@lib/features/interfaces'
 
 const accountManagementFeatures: IAccountManagementFeatures = {
     enabled: false,
+    accountList: {
+        enabled: true,
+    },
+    accountDetails: {
+        enabled: false,
+    },
 }
 
 export default accountManagementFeatures
--- packages/desktop/views/dashboard/account-management/AccountManagement.svelte
+++ packages/desktop/views/dashboard/account-management/AccountManagement.svelte
@@ -6,6 +6,7 @@
     import { onMount } from 'svelte'
     import { localize } from '@core/i18n'
     import { getBech32AddressFromAddressTypes } from '@core/wallet/utils'
+    import features from '@features/features'
 
     let allAccounts: OutputData[] = []
     let accounts: OutputData[] = []
@@ -52,53 +53,59 @@
     >
         <div class="flex space-x-4 max-w-7xl justify-center w-full">
             {#key $selectedWallet?.id}
-                <left-pane class="flex flex-col w-1/3">
-                    <Pane height={Height.Full}>
-                        <left-pane-container class="flex flex-col space-y-10 h-full">
-                            <Text type={TextType.h2}>{localize('views.accountManagement.list.title')}</Text>
-                            <list-wrapper class="flex flex-col space-y-2">
-                                {#each allAccounts as account, index}
-                                    <ClickableTile onClick={handleAccountClick}>
-                                        <div class="flex flex-col space-y-1">
-                                            <div class="flex space-x-2">
+                {#if features.accountManagement.accountList.enabled}
+                    <left-pane class="flex flex-col w-1/3">
+                        <Pane height={Height.Full}>
+                            <left-pane-container class="flex flex-col space-y-10 h-full">
+                                <Text type={TextType.h2}>{localize('views.accountManagement.list.title')}</Text>
+                                <list-wrapper class="flex flex-col space-y-2">
+                                    {#each allAccounts as account, index}
+                                        <ClickableTile onClick={handleAccountClick}>
+                                            <div class="flex flex-col space-y-1">
+                                                <div class="flex space-x-2">
+                                                    <Text
+                                                        type={TextType.h5}
+                                                        color="gray-800"
+                                                        darkColor="gray-500"
+                                                        fontWeight={FontWeight.semibold}
+                                                    >
+                                                        {localize('views.accountManagement.list.tile.title')}
+                                                        {index + 1}
+                                                    </Text>
+                                                    {#if isAnAccount(account)}
+                                                        <Pill backgroundColor="blue-200" textColor="blue-600"
+                                                            >{localize(
+                                                                'views.accountManagement.list.tile.pill.main'
+                                                            )}</Pill
+                                                        >
+                                                    {/if}
+                                                    {#if isAnImplicitAccount(account)}
+                                                        <Pill backgroundColor="yellow-200" textColor="yellow-900"
+                                                            >{localize(
+                                                                'views.accountManagement.list.tile.pill.pending'
+                                                            )}</Pill
+                                                        >
+                                                    {/if}
+                                                </div>
                                                 <Text
-                                                    type={TextType.h5}
-                                                    color="gray-800"
-                                                    darkColor="gray-500"
-                                                    fontWeight={FontWeight.semibold}
+                                                    type={TextType.p}
+                                                    fontSize="12"
+                                                    lineHeight="leading-140"
+                                                    color="gray-600">{formatAndTruncateAccount(account.output)}</Text
                                                 >
-                                                    {localize('views.accountManagement.list.tile.title')}
-                                                    {index + 1}
-                                                </Text>
-                                                {#if isAnAccount(account)}
-                                                    <Pill backgroundColor="blue-200" textColor="blue-600"
-                                                        >{localize('views.accountManagement.list.tile.pill.main')}</Pill
-                                                    >
-                                                {/if}
-                                                {#if isAnImplicitAccount(account)}
-                                                    <Pill backgroundColor="yellow-200" textColor="yellow-900"
-                                                        >{localize(
-                                                            'views.accountManagement.list.tile.pill.pending'
-                                                        )}</Pill
-                                                    >
-                                                {/if}
                                             </div>
-                                            <Text
-                                                type={TextType.p}
-                                                fontSize="12"
-                                                lineHeight="leading-140"
-                                                color="gray-600">{formatAndTruncateAccount(account.output)}</Text
-                                            >
-                                        </div>
-                                    </ClickableTile>
-                                {/each}
-                            </list-wrapper>
-                        </left-pane-container>
-                    </Pane>
-                </left-pane>
-                <right-pane class="w-full h-full min-h-96 flex-1 space-y-4 flex flex-col">
-                    <Pane height={Height.Full}>Account Details</Pane>
-                </right-pane>
+                                        </ClickableTile>
+                                    {/each}
+                                </list-wrapper>
+                            </left-pane-container>
+                        </Pane>
+                    </left-pane>
+                {/if}
+                {#if features.accountManagement.accountDetails.enabled}
+                    <right-pane class="w-full h-full min-h-96 flex-1 space-y-4 flex flex-col">
+                        <Pane height={Height.Full}>Account Details</Pane>
+                    </right-pane>
+                {/if}
             {/key}
         </div>
     </account-management-container>
