aeb41a8e0c7407fbc99ef3b5a7038e60a50d01df
evavirseda <evirseda@boxfish.studio>
2024-02-13 13:18:21 +0100

    feat: add account info UI (#8018)
    * feat: add account management router
    
    * feat: update icon
    
    * feat: add account list
    
    * fix: display as isBusy if hasImplicitAccountCreationTransactionInProgress and disable the password input
    
    * feat: format and truncate address
    
    * feat: cleanup code
    
    * feat: add features flag
    
    * feat: add account management details ui
    
    * feat: add locales
    
    * feat: enable features
    
    * feat: update accountManagement
    
    * feat: fix wrong merge
    
    * feat: add isImplicitAccountAccountOutput function
    
    * fix import
    
    * fix: improve function
    
    * feat: cleanup
    
    * feat: cleanup
    
    * feat: cleanup
    
    * fix type
    
    * fix warnings
    
    * fix: cleanup

--- packages/desktop/views/dashboard/account-management/AccountManagement.svelte
+++ packages/desktop/views/dashboard/account-management/AccountManagement.svelte
@@ -1,39 +1,7 @@
 <script lang="ts">
-    import { truncateString } from '@core/utils'
     import { selectedWallet } from '@core/wallet/stores'
-    import { AccountAddress, AccountOutput, ImplicitAccountCreationAddress, OutputData } from '@iota/sdk/out/types'
-    import { Height, Pane, TextType, Text, ClickableTile, FontWeight, Pill } from '@ui'
-    import { localize } from '@core/i18n'
-    import { getBech32AddressFromAddressTypes, isImplicitAccountOutput } from '@core/wallet/utils'
     import features from '@features/features'
-
-    const allAccounts: OutputData[] = [...$selectedWallet.accountOutputs, ...$selectedWallet.implicitAccountOutputs]
-    function isAnAccount(output: OutputData) {
-        return $selectedWallet.accountOutputs.find((account) => account.outputId === output.outputId)
-    }
-    function isAnImplicitAccount(output: OutputData) {
-        return $selectedWallet.implicitAccountOutputs.find((account) => account.outputId === output.outputId)
-    }
-
-    function formatAndTruncateAccount(output) {
-        let address: string = ''
-        if (isImplicitAccountOutput(output)) {
-            address = getBech32AddressFromAddressTypes(
-                new ImplicitAccountCreationAddress(output.unlockConditions[0].address.pubKeyHash).address()
-            )
-        } else {
-            const accountId = (output as AccountOutput).accountId
-            if (accountId) {
-                address = getBech32AddressFromAddressTypes(new AccountAddress(accountId))
-            }
-        }
-
-        return truncateString(address, 7, 5)
-    }
-
-    function handleAccountClick() {
-        // TODO: Implement account details
-    }
+    import { AccountManagementDetails, AccountManagementList } from '@components'
 </script>
 
 {#if $selectedWallet}
@@ -42,59 +10,12 @@
     >
         <div class="flex space-x-4 max-w-7xl justify-center w-full">
             {#key $selectedWallet?.id}
-                <left-pane class="flex flex-col w-1/3">
-                    {#if features.accountManagement.accountList.enabled}
-                        <Pane height={Height.Full}>
-                            <left-pane-container class="flex flex-col space-y-10 h-full">
-                                <Text type={TextType.h2}>{localize('views.accountManagement.list.title')}</Text>
-                                <list-wrapper class="flex flex-col space-y-2">
-                                    {#each allAccounts as account, index}
-                                        <ClickableTile onClick={handleAccountClick}>
-                                            <div class="flex flex-col space-y-1">
-                                                <div class="flex space-x-2">
-                                                    <Text
-                                                        type={TextType.h5}
-                                                        color="gray-800"
-                                                        darkColor="gray-500"
-                                                        fontWeight={FontWeight.semibold}
-                                                    >
-                                                        {localize('views.accountManagement.list.tile.title')}
-                                                        {index + 1}
-                                                    </Text>
-                                                    {#if isAnAccount(account)}
-                                                        <Pill backgroundColor="blue-200" textColor="blue-600"
-                                                            >{localize(
-                                                                'views.accountManagement.list.tile.pill.main'
-                                                            )}</Pill
-                                                        >
-                                                    {/if}
-                                                    {#if isAnImplicitAccount(account)}
-                                                        <Pill backgroundColor="yellow-200" textColor="yellow-900"
-                                                            >{localize(
-                                                                'views.accountManagement.list.tile.pill.pending'
-                                                            )}</Pill
-                                                        >
-                                                    {/if}
-                                                </div>
-                                                <Text
-                                                    type={TextType.p}
-                                                    fontSize="12"
-                                                    lineHeight="leading-140"
-                                                    color="gray-600">{formatAndTruncateAccount(account.output)}</Text
-                                                >
-                                            </div>
-                                        </ClickableTile>
-                                    {/each}
-                                </list-wrapper>
-                            </left-pane-container>
-                        </Pane>
-                    {/if}
-                </left-pane>
-                <right-pane class="w-full h-full min-h-96 flex-1 space-y-4 flex flex-col">
-                    {#if features.accountManagement.accountDetails.enabled}
-                        <Pane height={Height.Full}>Account Details</Pane>
-                    {/if}
-                </right-pane>
+                {#if features.accountManagement.accountList.enabled}
+                    <AccountManagementList />
+                {/if}
+                {#if features.accountManagement.accountDetails.enabled}
+                    <AccountManagementDetails />
+                {/if}
             {/key}
         </div>
     </account-management-container>
