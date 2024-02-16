46e0b3b33b85c19f27277eeeabffa0842a3097b1
evavirseda <evirseda@boxfish.studio>
2024-02-16 13:06:03 +0100

    feat: add account info logic (#8032)
    * feat: add account management router
    
    * feat: update icon
    
    * feat: add account list
    
    * fix: display as isBusy if hasImplicitAccountCreationTransactionInProgress and disable the password input
    
    * feat: format and truncate address
    
    * feat: cleanup code
    
    * feat: add features flag
    
    * feat: add account management details ui
    
    * feat: add locales
    
    * feat: enable features
    
    * feat: update accountManagement
    
    * feat: use real data
    
    * feat: fix wrong merge
    
    * feat: add isImplicitAccountAccountOutput function
    
    * fix import
    
    * fix: improve function
    
    * feat: cleanup
    
    * feat: cleanup
    
    * feat: cleanup
    
    * minor fix
    
    * feat: show address
    
    * fix type
    
    * feat: cleanup
    
    * feat: hide implicitAccount address, clenaup and display transition button
    
    * feat: update main account id and persist
    
    * feat: cleanup
    
    * feat: add locales and menuItem
    
    * feat: update details and cleanup
    
    * fix warnings
    
    * fix: format
    
    * fix: minor improvements
    
    ---------
    
    Co-authored-by: cpl121 <cpeon@boxfish.studio>

--- packages/desktop/components/AccountManagementDetails.svelte
+++ packages/desktop/components/AccountManagementDetails.svelte
@@ -12,95 +12,127 @@
         CopyableBox,
         Icon,
         Pill,
+        Button,
+        ButtonSize,
     } from '@ui'
     import { Icon as IconEnum } from '@auxiliary/icon'
     import { AccountManagementMenu } from './modals'
+    import {
+        formatTokenAmountBestMatch,
+        getBech32AddressFromAddressTypes,
+        isAccountOutput,
+        isImplicitAccountOutput,
+    } from '@core/wallet'
+    import { AccountAddress, AccountOutput, CommonOutput, OutputData } from '@iota/sdk/out/types'
+    import { openUrlInBrowser } from '@core/app'
+    import { ExplorerEndpoint, getOfficialExplorerUrl } from '@core/network'
+    import { activeProfile, getBaseToken } from '@core/profile'
+
+    export let selectedOutput: OutputData
+    export let index: number
 
     let modal: Modal
+    let address: string = ''
 
-    const DUMMY_ACCOUNT_DATA = {
-        id: '1',
-        tiles: [
-            {
-                title: localize('views.accountManagement.details.balance'),
-                amount: '12 Gi',
-            },
-            {
-                title: localize('views.accountManagement.details.staked'),
-                amount: '0i',
-            },
-        ],
-        address: {
-            title: localize('views.accountManagement.details.address'),
-            value: '0xba6a556cfdb7c6a66a45ee520e529c2ea6c526dd27f364865bc0d1167f41c819',
-        },
-        info: [
-            {
-                title: localize('views.accountManagement.details.mana'),
-                value: '120000',
-            },
-            {
-                title: localize('views.accountManagement.details.key'),
-                value: '7f364865bc0d1167f41c819',
-            },
-        ],
+    const explorerUrl = getOfficialExplorerUrl($activeProfile?.network?.id)
+
+    $: isImplicitAccount = isImplicitAccountOutput(selectedOutput.output as CommonOutput)
+    $: accountId = isAccountOutput(selectedOutput) ? (selectedOutput?.output as AccountOutput)?.accountId : null
+    $: address = accountId ? getBech32AddressFromAddressTypes(new AccountAddress(accountId)) : null
+
+    function onExplorerClick(): void {
+        const url = `${explorerUrl}/${ExplorerEndpoint.Output}/${selectedOutput.outputId.toString()}`
+        openUrlInBrowser(url)
     }
 </script>
 
 <right-pane class="w-full h-full min-h-96 flex-1 space-y-4 flex flex-col">
     <Pane height={Height.Full}>
-        <right-pane-container class="flex flex-col space-y-10 h-full">
-            <title-container class="flex justify-between w-full">
-                <title-wrapper class="flex items-center space-x-2">
-                    <Text type={TextType.h2}>{localize('views.accountManagement.list.tile.title')}</Text>
-                    <Pill backgroundColor="blue-200" textColor="blue-600"
-                        >{localize('views.accountManagement.list.tile.pill.main')}
-                    </Pill>
-                </title-wrapper>
-                <wallet-actions-button class="block relative">
-                    <MeatballMenuButton onClick={modal?.toggle} />
-                    <AccountManagementMenu bind:modal position={{ right: '0' }} classes="mt-1.5" />
-                </wallet-actions-button>
-            </title-container>
-            <div class="flex flex-row space-x-2 w-1/2">
-                {#each DUMMY_ACCOUNT_DATA.tiles as tile}
-                    <Tile>
-                        <div class="flex flex-col space-y-2 items-center justify-center w-full">
-                            <Text type={TextType.h3}>{tile.amount}</Text>
-                            <Text color="gray-600" fontWeight={FontWeight.medium} fontSize="12" type={TextType.p}
-                                >{tile.title}</Text
+        <right-pane-container class="flex flex-col space-y-8 h-full">
+            <right-pane-title class="flex flex-col space-y-1">
+                <title-container class="flex justify-between w-full items-center">
+                    <title-wrapper class="flex items-center space-x-2 py-1">
+                        <Text type={TextType.h2}>{localize('views.accountManagement.list.tile.title')} {index}</Text>
+                        {#if isImplicitAccount}
+                            <Pill backgroundColor="yellow-200" textColor="yellow-900"
+                                >{localize('views.accountManagement.list.tile.pill.pending')}</Pill
                             >
-                        </div>
-                    </Tile>
-                {/each}
-            </div>
-            <div class="flex flex-col space-y-2 w-1/2">
-                <Text color="gray-600" fontWeight={FontWeight.medium} fontSize="12" type={TextType.p}
-                    >{DUMMY_ACCOUNT_DATA.address.title}</Text
+                        {:else}
+                            <Pill backgroundColor="blue-200" textColor="blue-600"
+                                >{localize('views.accountManagement.list.tile.pill.main')}
+                            </Pill>
+                        {/if}
+                    </title-wrapper>
+
+                    {#if accountId}
+                        <wallet-actions-button class="block relative">
+                            <MeatballMenuButton onClick={modal?.toggle} />
+                            <AccountManagementMenu bind:modal position={{ right: '0' }} classes="mt-1.5" {accountId} />
+                        </wallet-actions-button>
+                    {/if}
+                    {#if isImplicitAccount}
+                        <Button size={ButtonSize.Small}
+                            >{localize('views.implicit-account-creation.steps.step2.view.action')}</Button
+                        >
+                    {/if}
+                </title-container>
+                <button
+                    class="action w-max flex justify-start text-center font-medium text-14 text-blue-500"
+                    on:click={onExplorerClick}
                 >
-                <CopyableBox
-                    clearBackground
-                    clearBoxPadding
-                    isCopyable
-                    value={DUMMY_ACCOUNT_DATA.address.value}
-                    classes="flex space-x-2 items-center"
-                >
-                    <Text type={TextType.pre} fontSize="13" lineHeight="leading-120" classes="text-start w-[260px]"
-                        >{DUMMY_ACCOUNT_DATA.address.value}</Text
-                    >
-                    <Icon icon={IconEnum.Copy} classes="text-blue-500" width={24} height={24} />
-                </CopyableBox>
+                    {localize('general.viewOnExplorer')}
+                </button>
+            </right-pane-title>
+            <div class="flex flex-row space-x-2 w-1/2">
+                <Tile>
+                    <div class="flex flex-col space-y-2 items-center justify-center w-full">
+                        <!-- TODO: Replace this with the actual balance for accountOutputs-->
+                        <Text type={TextType.h3}
+                            >{isImplicitAccount
+                                ? formatTokenAmountBestMatch(Number(selectedOutput.output.amount), getBaseToken())
+                                : 0 + ' Gi'}</Text
+                        >
+                        <Text color="gray-600" fontWeight={FontWeight.medium} fontSize="12" type={TextType.p}
+                            >{localize('views.accountManagement.details.balance')}</Text
+                        >
+                    </div>
+                </Tile>
+
+                <Tile>
+                    <div class="flex flex-col space-y-2 items-center justify-center w-full">
+                        <!-- TODO: Replace this with the actual staked amount -->
+                        <Text type={TextType.h3}>0i</Text>
+                        <Text color="gray-600" fontWeight={FontWeight.medium} fontSize="12" type={TextType.p}
+                            >{localize('views.accountManagement.details.staked')}</Text
+                        >
+                    </div>
+                </Tile>
             </div>
-            {#each DUMMY_ACCOUNT_DATA.info as info}
+            {#if accountId}
                 <div class="flex flex-col space-y-2 w-1/2">
-                    <Text color="gray-600" fontWeight={FontWeight.medium} fontSize="12" type={TextType.p}
-                        >{info.title}</Text
+                    <Text color="gray-600" fontWeight={FontWeight.medium} fontSize="12" type={TextType.p}>Address</Text>
+                    <CopyableBox
+                        clearBackground
+                        clearBoxPadding
+                        isCopyable
+                        value={address}
+                        classes="flex space-x-2 items-center"
                     >
+                        <Text type={TextType.pre} fontSize="13" lineHeight="leading-120" classes="text-start w-[260px]"
+                            >{address}</Text
+                        >
+                        <Icon icon={IconEnum.Copy} classes="text-blue-500" width={24} height={24} />
+                    </CopyableBox>
+                </div>
+            {/if}
+            {#if isImplicitAccount}
+                <div class="flex flex-col space-y-2 w-1/2">
+                    <Text color="gray-600" fontWeight={FontWeight.medium} fontSize="12" type={TextType.p}>Mana</Text>
                     <Text type={TextType.pre} fontSize="13" lineHeight="leading-120" classes="text-start w-[260px]"
-                        >{info.value}</Text
+                        >{selectedOutput?.output?.mana}</Text
                     >
                 </div>
-            {/each}
+            {/if}
         </right-pane-container>
     </Pane>
 </right-pane>
