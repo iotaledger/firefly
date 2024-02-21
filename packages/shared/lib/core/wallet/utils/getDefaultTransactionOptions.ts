243517645cef4dfb1a51d1893c9fe5658e0f096f
cpl121 <cpeon@boxfish.studio>
2024-02-21 08:58:48 +0100

    feat: update getDefaultTransactionOptions interface

--- packages/shared/lib/core/wallet/utils/getDefaultTransactionOptions.ts
+++ packages/shared/lib/core/wallet/utils/getDefaultTransactionOptions.ts
@@ -1,23 +1,17 @@
-import { AddressType, TransactionOptions } from '@iota/sdk/out/types'
+import { AccountAddress, TransactionOptions } from '@iota/sdk/out/types'
 import { getSelectedWallet } from '../stores'
 
 export function getDefaultTransactionOptions(accountId?: string): TransactionOptions | undefined {
     if (!accountId) {
         const wallet = getSelectedWallet()
-        if (!wallet) return
+        if (!wallet || !wallet.mainAccountId) return
         accountId = wallet.mainAccountId
     }
 
-    // TODO: update interface when https://github.com/iotaledger/iota-sdk/issues/1975 is merged
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    const value: any = {
-        type: AddressType.Account,
-        accountId,
-    }
     return {
         remainderValueStrategy: {
             strategy: 'CustomAddress',
-            value,
+            value: new AccountAddress(accountId),
         },
         allowMicroAmount: true,
     }
