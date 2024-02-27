bef38a976dbed61f7a226f27d3f6fdbc3c546b33
Begoña Alvarez <balvarez@boxfish.studio>
2024-02-27 12:25:41 +0100

    chore: cleanup code

--- packages/shared/lib/core/network/utils/getPassiveManaForOutput.ts
+++ packages/shared/lib/core/network/utils/getPassiveManaForOutput.ts
@@ -1,10 +1,15 @@
+import { api } from '@core/api'
 import { OutputData } from '@iota/sdk/out/types'
 import { get } from 'svelte/store'
 import { nodeInfo } from '../stores'
 import { getManaBalance } from './getManaBalance'
-import { api } from '@core/api'
 
-export function getTotalManaForOutput(output: OutputData): number | undefined {
+/**
+ * Get the passive mana for an output, it does not include BIC nor staking/delegation rewards
+ * @param output The output to get the mana for.
+ * @returns The passive mana for the output.
+ */
+export function getPassiveManaForOutput(output: OutputData): number | undefined {
     const _nodeInfo = get(nodeInfo)
     if (!_nodeInfo || !_nodeInfo.protocolParameters?.[0]?.parameters || !output) return
     const isOutputSpent = output.metadata.spent !== undefined
