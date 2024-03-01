0fcedd79cc718efbac05b6c19a209105a71433ed
marc2332 <mespinsanz@gmail.com>
2024-03-01 13:45:51 +0100

    fix: Sync with SDK types

--- packages/shared/lib/core/network/interfaces/client-options.interface.ts
+++ packages/shared/lib/core/network/interfaces/client-options.interface.ts
@@ -1,8 +1,8 @@
-import { IClientOptions } from '@iota/sdk/out/types'
+import { ClientOptions as SdkClientOptions } from '@iota/sdk/out/types'
 
 import { INode } from './node.interface'
 
-export interface ClientOptions extends Omit<IClientOptions, 'nodes' | 'primaryNode'> {
+export interface ClientOptions extends Omit<SdkClientOptions, 'nodes' | 'primaryNode'> {
     primaryNode?: INode
     nodes?: INode[]
 }
