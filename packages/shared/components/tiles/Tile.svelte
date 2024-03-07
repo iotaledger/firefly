269840e048bf12209192dfd8d5a6c39947b1163a
cpl121 <cpeon@boxfish.studio>
2024-03-07 01:03:07 +0100

    feat: selected account in the left pane has no UI hint that is selected

--- packages/shared/components/tiles/Tile.svelte
+++ packages/shared/components/tiles/Tile.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
     export let isGhost = false
     export let fullWidth = true
+    export let selected = false
     export let classes = ''
 </script>
 
@@ -13,8 +14,16 @@
         : 'bg-gray-50 dark:bg-gray-900'}
         {classes}
     "
+    class:selected
     on:click
     {...$$restProps}
 >
     <slot />
 </tile>
+
+<style lang="scss">
+    .selected {
+        @apply border border-solid border-blue-500 bg-blue-100;
+        @apply dark:bg-blue-300 dark:bg-opacity-10;
+    }
+</style>
