baaebe8f0d5ed27e640c5b81c70345e4ba8c0afc
cpl121 <cpeon@boxfish.studio>
2024-03-12 16:56:22 +0100

    fix: reactivity error when activating account

--- packages/desktop/components/popups/ActivateAccountPopup.svelte
+++ packages/desktop/components/popups/ActivateAccountPopup.svelte
@@ -1,6 +1,15 @@
 <script lang="ts">
+    import { DashboardRoute, dashboardRouter } from '@core/router'
+    import { selectedWallet } from '@core/wallet'
     import { ImplicitAccountCreationView } from '@views'
     export let outputId: string | undefined
+
+    function handleOutdatedOutput() {
+        $dashboardRouter.goTo(DashboardRoute.Wallet)
+    }
+
+    $: existOutputInUnspentOutputs = $selectedWallet.walletUnspentOutputs.some((output) => outputId === output.outputId)
+    $: !existOutputInUnspentOutputs && handleOutdatedOutput()
 </script>
 
 <ImplicitAccountCreationView {outputId} />
