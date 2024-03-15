135471706ebbb9ede7aa395aac177e14325b2539
cpl121 <cpeon@boxfish.studio>
2024-03-15 11:10:59 +0100

    fix: crash-accounts-if-not-selected-output

--- packages/desktop/views/dashboard/account-management/AccountManagement.svelte
+++ packages/desktop/views/dashboard/account-management/AccountManagement.svelte
@@ -11,7 +11,7 @@
         ) || []
 
     let selectedOutput =
-        $selectedWallet.walletUnspentOutputs.find(
+        $selectedWallet?.walletUnspentOutputs?.find(
             (output) => (output.output as AccountOutput)?.accountId === $selectedWallet.mainAccountId
         ) || allAccountOutputs?.[0]
 
@@ -28,19 +28,21 @@
     <account-management-container
         class="w-full h-full flex flex-nowrap p-8 relative flex-1 bg-gray-50 dark:bg-gray-900 space-x-4 justify-center"
     >
-        <div class="flex space-x-4 max-w-7xl justify-center w-full">
-            {#key $selectedWallet?.id}
-                {#if features.accountManagement.accountList.enabled}
-                    <AccountManagementList
-                        onAccountClick={handleAccountClick}
-                        allOutputs={allAccountOutputs}
-                        {selectedOutput}
-                    />
-                {/if}
-                {#if features.accountManagement.accountDetails.enabled}
-                    <AccountManagementDetails {selectedOutput} index={setAccountOutputIndex(selectedOutput)} />
-                {/if}
-            {/key}
-        </div>
+        {#if selectedOutput}
+            <div class="flex space-x-4 max-w-7xl justify-center w-full">
+                {#key $selectedWallet?.id}
+                    {#if features.accountManagement.accountList.enabled}
+                        <AccountManagementList
+                            onAccountClick={handleAccountClick}
+                            allOutputs={allAccountOutputs}
+                            {selectedOutput}
+                        />
+                    {/if}
+                    {#if features.accountManagement.accountDetails.enabled}
+                        <AccountManagementDetails {selectedOutput} index={setAccountOutputIndex(selectedOutput)} />
+                    {/if}
+                {/key}
+            </div>
+        {/if}
     </account-management-container>
 {/if}
