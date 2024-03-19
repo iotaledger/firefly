c9c480063056aeedb4196524b9f350b8c601966b
Marc Espin <mespinsanz@gmail.com>
2024-03-19 10:46:49 +0100

    fix: Remove PoW and debris (#8177)
    * fix: Remove local pow and debris
    
    * fix
    
    * debris
    
    ---------
    
    Co-authored-by: Begoña Álvarez de la Cruz <balvarez@boxfish.studio>

--- packages/shared/lib/core/network/actions/toggleLocalPowInClientOptions.ts
+++
@@ -1,8 +0,0 @@
-import { updateClientOptions } from '@core/network'
-import { activeProfile } from '@core/profile'
-import { get } from 'svelte/store'
-
-export async function toggleLocalPowInClientOptions(): Promise<void> {
-    const clientOptions = get(activeProfile)?.clientOptions
-    await updateClientOptions({ localPow: !clientOptions?.localPow })
-}
