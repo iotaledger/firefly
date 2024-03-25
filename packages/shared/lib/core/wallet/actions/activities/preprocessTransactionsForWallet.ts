d5ed5c7eb97c89b23ac2a2eba01c2436a9c4c845
cpl121 <100352899+cpl121@users.noreply.github.com>
2024-03-25 18:29:34 +0100

    fix: tx history: include account output in average txs (#8253)
    * feat: Sync SDK types
    
    * feat: stop generating txs out of outputs
    
    * fix: tx history: include account output in average txs
    
    ---------
    
    Co-authored-by: marc2332 <mespinsanz@gmail.com>
    Co-authored-by: Begoña Álvarez de la Cruz <balvarez@boxfish.studio>

--- packages/shared/lib/core/wallet/actions/activities/preprocessTransactionsForWallet.ts
+++ packages/shared/lib/core/wallet/actions/activities/preprocessTransactionsForWallet.ts
@@ -1,15 +1,23 @@
 import { IWalletState } from '@core/wallet/interfaces'
-import { preprocessOutgoingTransaction } from '../../utils'
+import { preprocessIncomingTransaction, preprocessOutgoingTransaction } from '../../utils'
 import { IProcessedTransaction } from '../../interfaces/processed-transaction.interface'
 
 export async function preprocessTransactionsForWallet(wallet: IWalletState): Promise<IProcessedTransaction[]> {
-    const transactions = await wallet.transactions()
-
     const processedTransactions: IProcessedTransaction[] = []
+    const transactions = await wallet.transactions()
 
     for (const transaction of transactions) {
         try {
             const processedTransaction = await preprocessOutgoingTransaction(transaction, wallet)
+            processedTransactions.push(processedTransaction)
+        } catch (err) {
+            console.error(err)
+        }
+    }
+    const incomingTransactions = await wallet.incomingTransactions()
+    for (const incomingTransaction of incomingTransactions) {
+        try {
+            const processedTransaction = preprocessIncomingTransaction(incomingTransaction)
             processedTransactions.push(processedTransaction)
         } catch (err) {
             console.error(err)
