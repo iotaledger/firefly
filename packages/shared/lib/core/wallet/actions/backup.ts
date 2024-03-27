ad32f78b961eda7f064aea1746285d93c73cb63a
evavirseda <evirseda@boxfish.studio>
2024-03-27 09:11:34 +0100

    feat: update stronghold export (#8264)

--- packages/shared/lib/core/wallet/actions/backup.ts
+++ packages/shared/lib/core/wallet/actions/backup.ts
@@ -2,5 +2,5 @@ import { getSelectedWallet } from '../stores'
 
 export async function backup(dest: string, password: string): Promise<void> {
     const wallet = getSelectedWallet()
-    await wallet.backup(dest, password)
+    await wallet.backupToStrongholdSnapshot(dest, password)
 }
