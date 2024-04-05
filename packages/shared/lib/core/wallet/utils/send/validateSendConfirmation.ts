39f28be54123a77913db8b9317296a79cc7d4e1f
cpl121 <100352899+cpl121@users.noreply.github.com>
2024-04-05 14:38:50 +0200

    fix: remove ExpirationUnlockConditionTemp and TimelockUnlockConditionTemp (#8322)

--- packages/shared/lib/core/wallet/utils/send/validateSendConfirmation.ts
+++ packages/shared/lib/core/wallet/utils/send/validateSendConfirmation.ts
@@ -1,24 +1,19 @@
 import { InvalidExpirationDateTimeError } from '@contexts/wallet'
 import { convertUnixTimestampToDate, isValidExpirationDateTime } from '@core/utils'
-import { CommonOutput, ExpirationUnlockCondition, SlotIndex, UnlockConditionType } from '@iota/sdk/out/types'
+import { CommonOutput, ExpirationUnlockCondition, UnlockConditionType } from '@iota/sdk/out/types'
 import { getUnixTimestampFromNodeInfoAndSlotIndex, nodeInfoProtocolParameters } from '@core/network'
 import { get } from 'svelte/store'
-
-// TODO: Remove temp interface -> https://github.com/iotaledger/firefly/issues/8305
-interface ExpirationUnlockConditionTemp extends ExpirationUnlockCondition {
-    slot: SlotIndex
-}
 
 export function validateSendConfirmation(output: CommonOutput): void {
     const expirationUnlockCondition = output.unlockConditions.find(
         (c) => c.type === UnlockConditionType.Expiration
-    ) as ExpirationUnlockConditionTemp
+    ) as ExpirationUnlockCondition
 
     const nodeProtocolParameters = get(nodeInfoProtocolParameters)
     if (!nodeProtocolParameters || !expirationUnlockCondition) return
     const expirationUnixTime = getUnixTimestampFromNodeInfoAndSlotIndex(
         nodeProtocolParameters,
-        expirationUnlockCondition.slotIndex ?? expirationUnlockCondition.slot
+        expirationUnlockCondition.slot
     )
     const expirationDateTime = expirationUnixTime ? convertUnixTimestampToDate(expirationUnixTime) : undefined
 
