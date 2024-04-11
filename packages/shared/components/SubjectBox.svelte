fa593e51c08ffd011ed50c00a9f213e69d22980c
cpl121 <cpeon@boxfish.studio>
2024-04-11 11:44:22 +0200

    fix: include a feature flag

--- packages/shared/components/SubjectBox.svelte
+++ packages/shared/components/SubjectBox.svelte
@@ -2,16 +2,13 @@
     import { localize } from '@core/i18n'
     import { Subject, SubjectType } from '@core/wallet'
     import { Box, AddressBox, Text, WalletLabel, TextType, FontWeight } from '@ui'
+    import features from '@features/features'
 
     export let subject: Subject | null = null
-
-    // TODO: update when multiwallet is implemented
-    // https://github.com/iotaledger/firefly/issues/8362
-    const isMultiwalletSupported = false
 </script>
 
 {#if subject?.type === SubjectType.Wallet}
-    {#if isMultiwalletSupported}
+    {#if features?.wallet?.multiWallet?.enabled}
         <Box row clearBackground clearPadding classes="justify-center">
             <WalletLabel wallet={subject?.wallet} />
         </Box>
