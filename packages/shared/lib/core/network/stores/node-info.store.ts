0fed75eb3b548ae79dfcf50e1de38e8abae7d55a
cpl121 <cpeon@boxfish.studio>
2024-04-17 11:41:40 +0200

    fix: remove nodeInfo deriveds

--- packages/shared/lib/core/network/stores/node-info.store.ts
+++ packages/shared/lib/core/network/stores/node-info.store.ts
@@ -1,12 +1,8 @@
 import { INodeInfo } from '@iota/sdk/out/types'
-import { writable, derived } from 'svelte/store'
+import { writable } from 'svelte/store'
 
 export const nodeInfo = writable<INodeInfo | undefined>(undefined)
 
 export function setNodeInfo(newNodeInfo: INodeInfo | undefined): void {
     return nodeInfo.set(newNodeInfo)
 }
-
-export const nodeInfoBaseToken = derived(nodeInfo, ($nodeInfo) => $nodeInfo?.baseToken)
-
-export const nodeInfoProtocol = derived(nodeInfo, ($nodeInfo) => $nodeInfo?.protocol)
--- packages/shared/lib/core/profile/actions/active-profile/getBaseToken.ts
+++ packages/shared/lib/core/profile/actions/active-profile/getBaseToken.ts
@@ -1,12 +1,12 @@
 import { IBaseToken, TokenStandard } from '@core/wallet'
-import { nodeInfoBaseToken } from '@core/network'
+import { nodeInfo } from '@core/network'
 import { get } from 'svelte/store'
 
 export function getBaseToken(): IBaseToken {
-    const $nodeInfoBaseToken = get(nodeInfoBaseToken)
+    const $nodeInfo = get(nodeInfo)
 
     return {
         standard: TokenStandard.BaseToken,
-        ...$nodeInfoBaseToken,
+        ...$nodeInfo?.baseToken,
     }
 }
