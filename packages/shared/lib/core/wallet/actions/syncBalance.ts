99780f0e545bfd9d59532b44ce8f710ccb8dea74
marc2332 <mespinsanz@gmail.com>
2024-04-19 13:07:47 +0200

    clean up

--- packages/shared/lib/core/wallet/actions/syncBalance.ts
+++ packages/shared/lib/core/wallet/actions/syncBalance.ts
@@ -2,20 +2,22 @@ import { get } from 'svelte/store'
 import { selectedWalletId, updateSelectedWallet } from '../stores/'
 import { getWalletById, updateActiveWallet } from '@core/profile/stores'
 import { IBalance } from '../interfaces'
-import { IWalletState, getTotalWalletBalance } from '..'
+import { getTotalWalletBalance } from '..'
 import { OutputData } from '@iota/sdk/out/types'
 
 export async function syncBalance(walletId: string, syncCongestion: boolean): Promise<void> {
     let accountOutputs: OutputData[] = []
-    const wallet = getWalletById(walletId) as IWalletState
-    if (syncCongestion) {
-        accountOutputs = wallet?.accountOutputs ?? []
-    }
-    const totalBalance: IBalance = await getTotalWalletBalance(wallet, accountOutputs)
-    if (get(selectedWalletId) === walletId) {
-        updateSelectedWallet({ balances: totalBalance })
-    } else {
-        updateActiveWallet(walletId, { balances: totalBalance })
+    const wallet = getWalletById(walletId)
+    if (wallet) {
+        if (syncCongestion) {
+            accountOutputs = wallet.accountOutputs ?? []
+        }
+        const totalBalance: IBalance = await getTotalWalletBalance(wallet, accountOutputs)
+        if (get(selectedWalletId) === walletId) {
+            updateSelectedWallet({ balances: totalBalance })
+        } else {
+            updateActiveWallet(walletId, { balances: totalBalance })
+        }
     }
     return
 }
