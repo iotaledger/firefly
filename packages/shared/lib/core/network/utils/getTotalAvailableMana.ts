f5dc0c2bc908a34f395b5799bf7d44540a666469
cpl121 <cpeon@boxfish.studio>
2024-04-22 11:56:39 +0200

    fix: error to calculate total available mana

--- packages/shared/lib/core/network/utils/getTotalAvailableMana.ts
+++ packages/shared/lib/core/network/utils/getTotalAvailableMana.ts
@@ -4,7 +4,7 @@ import { getManaBalance } from './getManaBalance'
 
 export function getTotalAvailableMana(_selectedWallet: IWalletState, outputId: string | undefined = undefined): number {
     return (
-        getManaBalance(_selectedWallet?.balances?.mana?.available) +
+        getManaBalance(_selectedWallet?.balances?.mana?.available) -
         getImplicitAccountsMana(_selectedWallet?.implicitAccountOutputs, outputId ? [outputId] : [])
     )
 }
