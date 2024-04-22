1a1ee632f752dc0cb4227cdd73d6295c38d5ab84
Marc Espin <mespinsanz@gmail.com>
2024-04-22 16:58:24 +0200

    fix: Prevent wallet destroyed error (#8338)
    * fix: Prevent wallet destroyed error
    
    * some improvements
    
    * improve import
    
    * clean up
    
    * fix I think
    
    * chore: Update to @iota/sdk alpha-7
    
    * clean up
    
    * fix
    
    ---------
    
    Co-authored-by: cpl121 <100352899+cpl121@users.noreply.github.com>
    Co-authored-by: Begoña Álvarez de la Cruz <balvarez@boxfish.studio>

--- packages/shared/lib/core/wallet/actions/getBalance.ts
+++
@@ -1,6 +0,0 @@
-import { Balance } from '@iota/sdk/out/types'
-import { getWallet } from '@core/profile/actions'
-
-export async function getBalance(walletId: string): Promise<Balance> {
-    return (await getWallet(walletId))?.getBalance()
-}
--- packages/shared/lib/core/wallet/actions/getVotingPower.ts
+++
@@ -1,6 +0,0 @@
-import { getBalance } from './getBalance'
-
-export async function getVotingPower(walletId: string): Promise<string> {
-    const balance = await getBalance(walletId)
-    return balance.baseCoin.votingPower.toString()
-}
