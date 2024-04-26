26af0da73b7631705062bcf69c07cd13595129d1
cpl121 <cpeon@boxfish.studio>
2024-04-26 16:17:23 +0200

    feat: add new consolidate outputs popup

---
+++ packages/desktop/components/popups/ConsolidateOutputsPopup.svelte
@@ -0,0 +1,102 @@
+<script lang="ts">
+    import { Button, Text, TextHint, FontWeight, TextType, ButtonVariant } from '@ui'
+    import { localize } from '@core/i18n'
+    import { closePopup } from '@auxiliary/popup'
+    import { handleError } from '@core/error/handlers'
+    import { onMount } from 'svelte'
+    import { selectedWallet } from '@core/wallet'
+    import { TextHintVariant } from '@ui/enums'
+    import { ITransactionInfoToCalculateManaCost } from '@core/network'
+    import { checkActiveProfileAuth } from '@core/profile'
+    import { ManaBox } from '@components'
+    import { consolidateOutputs } from '@core/wallet/actions/consolidateOutputs'
+
+    export let _onMount: (..._: any[]) => Promise<void> = async () => {}
+
+    const transactionInfo: ITransactionInfoToCalculateManaCost = {}
+
+    let isBusy = false
+    let hasError = false
+    let hasEnoughMana = false
+
+    $: confirmAllowed =
+        !$selectedWallet?.isTransferring &&
+        !isBusy &&
+        hasEnoughMana &&
+        !hasError &&
+        transactionInfo.preparedTransaction &&
+        !transactionInfo.preparedTransactionError
+
+    $: displayManaBox = !hasError && !$selectedWallet?.isTransferring
+
+    async function prepareConsolidateTransaction(): Promise<void> {
+        try {
+            transactionInfo.preparedTransaction = await $selectedWallet.prepareConsolidateOutputs({
+                force: false,
+                outputThreshold: 2,
+                targetAddress: $selectedWallet.depositAddress,
+            })
+        } catch (error) {
+            if (error.message.includes('slots remaining until enough mana')) {
+                transactionInfo.preparedTransactionError = error
+            } else {
+                hasError = true
+                handleError(error)
+            }
+        }
+    }
+
+    async function onConsolidateClick(): Promise<void> {
+        try {
+            isBusy = true
+            await checkActiveProfileAuth(
+                async () => {
+                    await consolidateOutputs()
+                    closePopup()
+                },
+                { stronghold: true }
+            )
+        } catch (err) {
+            handleError(err)
+        } finally {
+            isBusy = false
+        }
+    }
+
+    function onCancelClick(): void {
+        closePopup()
+    }
+
+    onMount(async () => {
+        try {
+            await _onMount()
+            await prepareConsolidateTransaction()
+        } catch (err) {
+            handleError(err)
+        }
+    })
+</script>
+
+<div class="w-full h-full space-y-6 flex flex-auto flex-col shrink-0">
+    <Text type={TextType.h3} fontWeight={FontWeight.semibold} classes="text-left">
+        {localize('popups.minimizeStorageDeposit.title')}
+    </Text>
+    <div class="space-y-4">
+        <TextHint variant={TextHintVariant.Info} text={localize('popups.minimizeStorageDeposit.description')} />
+        {#if displayManaBox}
+            <ManaBox {transactionInfo} bind:hasEnoughMana />
+        {/if}
+    </div>
+    <popup-buttons class="flex flex-row flex-nowrap w-full space-x-4">
+        <Button classes="w-full" outline onClick={onCancelClick}>{localize('actions.cancel')}</Button>
+        <Button
+            classes="w-full"
+            variant={ButtonVariant.Primary}
+            disabled={!confirmAllowed}
+            isBusy={$selectedWallet?.isTransferring || isBusy}
+            onClick={onConsolidateClick}
+        >
+            {localize('popups.minimizeStorageDeposit.confirmButton')}
+        </Button>
+    </popup-buttons>
+</div>
