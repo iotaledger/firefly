3b08c06d0ea7d0c07f68f8263d82868fcaec5db9
marc2332 <mespinsanz@gmail.com>
2024-04-30 16:08:13 +0200

    fix: Smarter network ID matcher

--- packages/shared/lib/core/network/utils/getNetworkIdFromNetworkName.ts
+++ packages/shared/lib/core/network/utils/getNetworkIdFromNetworkName.ts
@@ -1,13 +1,13 @@
 import { NetworkId } from '../enums'
 
 export function getNetworkIdFromNetworkName(networkName: string): NetworkId {
+    if(networkName.startsWith('iota-alphanet')){
+        return NetworkId.IotaAlphanet
+    }
     switch (networkName) {
         case 'iota-mainnet':
         case 'stardust-mainnet-pre1':
             return NetworkId.Iota
-        case 'iota-alphanet':
-        case 'iota-alphanet-2':
-            return NetworkId.IotaAlphanet
         case 'shimmer':
             return NetworkId.Shimmer
         case 'testnet':
