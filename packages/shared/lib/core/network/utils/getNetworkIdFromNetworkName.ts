77cf9c84535b92bce4c800c29da58a7da731a9a5
marc2332 <mespinsanz@gmail.com>
2024-04-30 16:09:32 +0200

    fix: Smarter network ID matcher

--- packages/shared/lib/core/network/utils/getNetworkIdFromNetworkName.ts
+++ packages/shared/lib/core/network/utils/getNetworkIdFromNetworkName.ts
@@ -8,13 +8,14 @@ export function getNetworkIdFromNetworkName(networkName: string): NetworkId {
         case 'iota-mainnet':
         case 'stardust-mainnet-pre1':
             return NetworkId.Iota
+        case 'iota-alphanet':
+        case 'iota-alphanet-2':
+            return NetworkId.IotaAlphanet
         case 'shimmer':
             return NetworkId.Shimmer
         case 'testnet':
         case 'testnet-1':
         case 'testnet-2':
-        case 'docker':
-        case 'iota2-alphanet':
             return NetworkId.Testnet
         default:
             return NetworkId.Custom
