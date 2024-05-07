6334d145804ecf057899486fcc3586d54641ede8
evavirseda <evirseda@boxfish.studio>
2024-05-07 12:21:16 +0200

    feat: fix available balance toggle on tesnet and custom network (#8501)
    * fix available balance toggle on tesnet and custom network
    
    * fix: move to variable
    
    * fix import
    
    ---------
    
    Co-authored-by: Begoña Álvarez de la Cruz <balvarez@boxfish.studio>

--- packages/shared/components/labels/TogglableAssetBalanceLabel.svelte
+++ packages/shared/components/labels/TogglableAssetBalanceLabel.svelte
@@ -3,7 +3,8 @@
     import { formatTokenAmountBestMatch, IAsset } from '@core/wallet'
     import { formatCurrency, localize } from '@core/i18n'
     import { getMarketAmountFromAssetValue } from '@core/market/utils'
-    import { DEFAULT_MANA } from '@core/network'
+    import { DEFAULT_MANA, NetworkId } from '@core/network'
+    import { activeProfile } from '@core/profile'
 
     export let asset: IAsset
     export let mana: IAsset
@@ -11,6 +12,8 @@
     $: availableMarketValue = getMarketAmountFromAssetValue(asset?.balance?.available, asset)
     $: totalMarketValue = getMarketAmountFromAssetValue(asset?.balance?.total, asset)
     $: disabled = Number.isNaN(totalMarketValue) || Number.isNaN(availableMarketValue)
+    $: isNotTestnetOrCustomNetwork =
+        $activeProfile?.network?.id !== NetworkId.Testnet && $activeProfile?.network?.id !== NetworkId.Custom
 
     let isToggled = false
     function toggle(): void {
@@ -22,16 +25,17 @@
     <button on:click={toggle} type="button" {disabled}>
         <div class="flex flex-col flex-wrap items-start space-y-1">
             <Text type={TextType.h1} fontWeight={FontWeight.semibold}>
-                {isToggled
+                {isToggled && isNotTestnetOrCustomNetwork
                     ? formatCurrency(totalMarketValue)
                     : formatTokenAmountBestMatch(asset?.balance?.total, asset?.metadata)}
             </Text>
             <Text type={TextType.p} fontWeight={FontWeight.medium} color="gray-600" darkColor="gray-500">
                 {localize('general.availableBalanceWithValue', {
                     values: {
-                        balance: isToggled
-                            ? formatCurrency(availableMarketValue)
-                            : formatTokenAmountBestMatch(asset?.balance?.available, asset?.metadata),
+                        balance:
+                            isToggled && isNotTestnetOrCustomNetwork
+                                ? formatCurrency(availableMarketValue)
+                                : formatTokenAmountBestMatch(asset?.balance?.available, asset?.metadata),
                     },
                 })}
             </Text>
