2ce0e4fda4d1ec05140a88d997e5ff997bf2480d
marc2332 <mespinsanz@gmail.com>
2024-05-13 11:50:57 +0200

    chore: Update Node and Faucet URLs

--- packages/shared/lib/core/network/constants/explorer-urls.constant.ts
+++ packages/shared/lib/core/network/constants/explorer-urls.constant.ts
@@ -4,5 +4,5 @@ export const EXPLORER_URLS: Readonly<{ [key in NetworkId]?: string }> = {
     [NetworkId.Iota]: 'https://explorer.iota.org/mainnet',
     [NetworkId.IotaAlphanet]: 'https://explorer.iota-alphanet.iotaledger.net/devnet',
     [NetworkId.Shimmer]: 'https://explorer.shimmer.network/shimmer',
-    [NetworkId.Testnet]: 'https://explorer.shimmer.network/testnet',
+    [NetworkId.Testnet]: '<UPDATE>',
 }
