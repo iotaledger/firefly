8e1bfb86e4e5ea10081d8fee1a4cd2472a045790
marc2332 <mespinsanz@gmail.com>
2024-05-13 11:57:38 +0200

    chore: Update getNetworkIdFromNetworkName again

--- packages/shared/lib/core/network/utils/getNetworkIdFromNetworkName.ts
+++ packages/shared/lib/core/network/utils/getNetworkIdFromNetworkName.ts
@@ -1,6 +1,9 @@
 import { NetworkId } from '../enums'
 
 export function getNetworkIdFromNetworkName(networkName: string): NetworkId {
+    if (networkName.startsWith('nova-testnet')) {
+        return NetworkId.Testnet
+    }
     switch (networkName) {
         case 'iota-mainnet':
         case 'stardust-mainnet-pre1':
@@ -13,7 +16,6 @@ export function getNetworkIdFromNetworkName(networkName: string): NetworkId {
         case 'testnet':
         case 'testnet-1':
         case 'testnet-2':
-        case 'nova-testnet':
             return NetworkId.Testnet
         default:
             return NetworkId.Custom
